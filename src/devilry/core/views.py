fd2922780e8de3ad0d4c302460d2764552f95540
Espen A. Kristiansen <post@espenak.net>
2010-01-30 03:57:26 +0100

    Improved delivery form.

--- src/devilry/core/views.py
+++ src/devilry/core/views.py
@@ -1,25 +1,34 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django import forms
-from models import DeliveryCandidate, FileMeta
+from models import DeliveryCandidate, FileMeta, Delivery
 from django.forms.models import inlineformset_factory
 from datetime import datetime
+from django.contrib.auth.decorators import login_required
 
 
 def dashboard(request):
     return HttpResponse('Should display a listing of courses and stuff like that!')
 
 
-class DeliveryCandidateForm(forms.ModelForm):
-    class Meta:
-        model = DeliveryCandidate
-        exclude = ['time_of_delivery']
-
-
 FileMetaForm = inlineformset_factory(DeliveryCandidate, FileMeta, extra=1)
 
 
+@login_required
 def deliver(request):
+
+    print request.user
+    print dir(Delivery.objects.all()[0])
+    print Delivery.objects.filter(deliverystudent__student=request.user)
+    class DeliveryCandidateForm(forms.ModelForm):
+        delivery = forms.ModelChoiceField(
+                queryset=Delivery.objects.all())
+                #queryset=Delivery.objects.filter(students=request.user))
+        
+        class Meta:
+            model = DeliveryCandidate
+            exclude = ['time_of_delivery']
+
     if request.method == 'POST':
         if 'add_file' in request.POST:
             print "Add file..."
--- src/devilry/urls.py
+++ src/devilry/urls.py
@@ -8,6 +8,8 @@ urlpatterns = patterns('',
     # Example:
     # (r'^devilry/', include('devilry.foo.urls')),
 
+    (r'^accounts/login/$', 'django.contrib.auth.views.login'),
+
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
@@ -16,4 +18,5 @@ urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
 
     (r'^core/', include('devilry.core.urls')),
+
 )
