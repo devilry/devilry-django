0319a7c12fc1fff08511aa0258074a2b78344412
Espen A. Kristiansen <post@espenak.net>
2010-02-14 19:33:51 +0100

    Some messing about with file uploads.

--- src/devilry/core/views.py
+++ src/devilry/core/views.py
@@ -17,18 +17,16 @@ FileMetaForm = inlineformset_factory(DeliveryCandidate, FileMeta, extra=1)
 @login_required
 def deliver(request):
 
-    print request.user
-    print dir(Delivery.objects.all()[0])
-    print Delivery.objects.filter(deliverystudent__student=request.user)
     class DeliveryCandidateForm(forms.ModelForm):
-        delivery = forms.ModelChoiceField(
-                queryset=Delivery.objects.all())
-                #queryset=Delivery.objects.filter(students=request.user))
+        #delivery = forms.ModelChoiceField(
+                #queryset=Delivery.objects.all())
+                ##queryset=Delivery.objects.filter(students=request.user))
         
         class Meta:
             model = DeliveryCandidate
             exclude = ['time_of_delivery']
 
+    filemetas = []
     if request.method == 'POST':
         if 'add_file' in request.POST:
             print "Add file..."
@@ -37,20 +35,22 @@ def deliver(request):
 
         form = DeliveryCandidateForm(request.POST)
         formset = FileMetaForm(request.POST, request.FILES, instance=form.instance)
-        print dir(formset)
 
         if form.is_valid() and formset.is_valid():
             form.instance.time_of_delivery = datetime.now()
-            form.save()
-            formset.save()
-            return HttpResponse('thanks')
-            #return HttpResponseRedirect('thanks')
+            deliverycandidate = form.save()
+            filemetas = formset.save()
+            if 'deliver' in request.POST:
+                return HttpResponse('thanks')
+                #return HttpResponseRedirect('thanks')
+            else:
+                form = DeliveryCandidateForm(deliverycandidate)
+                formset = FileMetaForm(request.POST, request.FILES, instance=deliverycandidate)
     else:
         form = DeliveryCandidateForm()
-        #print dir(form.instance)
         formset = FileMetaForm()
-        #print dir(formset)
     return render_to_response('core/delivery.html', {
         'form': form,
         'formset': formset,
+        'filemetas': filemetas
     })
