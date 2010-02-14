7d7465240f273a65d90e0db8734d27aa65377336
Espen A. Kristiansen <post@espenak.net>
2010-02-15 00:38:50 +0100

    Made a working delivery form.

--- src/devilry/core/urls.py
+++ src/devilry/core/urls.py
@@ -1,6 +1,12 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('devilry.core',
-	(r'^dashboard/$', 'views.dashboard'),
-	(r'^deliver/$', 'views.deliver'),
+    (r'^dashboard/$', 'views.dashboard'),
+    #(r'^deliver/(?P<deliveryid>\d+)/form$', 'views.deliver'),
+    (r'^deliver/(?P<deliveryid>\d+)/start$', 'views.start_delivery'),
+    url(r'^deliver/(?P<deliveryid>\d+)/create$', 'views.create_delivery',
+        name='create-delivery'),
+    url(r'^deliver/(?P<deliveryid>\d+)/(?P<deliverycand_id>\d+)/edit$',
+        'views.create_delivery', name='edit-delivery'),
+    #(r'^deliver/(?P<deliveryid>\d+)/edit$', 'views.edit_delivery'),
 )
--- src/devilry/core/views.py
+++ src/devilry/core/views.py
@@ -1,10 +1,13 @@
+from datetime import datetime
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 from django import forms
+from django.db import models
 from models import DeliveryCandidate, FileMeta, Delivery
 from django.forms.models import inlineformset_factory
-from datetime import datetime
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
+from django.core.urlresolvers import reverse
 
 
 def dashboard(request):
@@ -13,44 +16,82 @@ def dashboard(request):
 
 FileMetaForm = inlineformset_factory(DeliveryCandidate, FileMeta, extra=1)
 
+class DeliveryCandidateForm(forms.ModelForm):
+    class Meta:
+        model = DeliveryCandidate
+        exclude = ['time_of_delivery', 'delivery']
+
 
 @login_required
-def deliver(request):
-
-    class DeliveryCandidateForm(forms.ModelForm):
-        #delivery = forms.ModelChoiceField(
-                #queryset=Delivery.objects.all())
-                ##queryset=Delivery.objects.filter(students=request.user))
-        
-        class Meta:
-            model = DeliveryCandidate
-            exclude = ['time_of_delivery']
-
+def deliver(request, deliveryid):
     filemetas = []
     if request.method == 'POST':
-        if 'add_file' in request.POST:
-            print "Add file..."
-        else:
-            print 'Deliver...'
-
+        delivery = Delivery.objects.get(id=deliveryid)
+        #dc = DeliveryCandidate(time_of_delivery = datetime.now(),
+            #delivery=delivery)
         form = DeliveryCandidateForm(request.POST)
+        form.instance.delivery = delivery
         formset = FileMetaForm(request.POST, request.FILES, instance=form.instance)
 
         if form.is_valid() and formset.is_valid():
             form.instance.time_of_delivery = datetime.now()
-            deliverycandidate = form.save()
+            dc = form.save()
             filemetas = formset.save()
             if 'deliver' in request.POST:
                 return HttpResponse('thanks')
-                #return HttpResponseRedirect('thanks')
+               #return HttpResponseRedirect('thanks')
             else:
-                form = DeliveryCandidateForm(deliverycandidate)
-                formset = FileMetaForm(request.POST, request.FILES, instance=deliverycandidate)
+                form = DeliveryCandidateForm(request.POST, instance=dc)
+                print form.instance.id
+                formset = FileMetaForm(request.POST, request.FILES,
+                    instance=dc)
     else:
         form = DeliveryCandidateForm()
         formset = FileMetaForm()
     return render_to_response('core/delivery.html', {
         'form': form,
         'formset': formset,
-        'filemetas': filemetas
     })
+
+
+
+def deliveryform(form, formset, url):
+    return render_to_response('core/delivery.html', {
+        'form': form,
+        'formset': formset,
+        'url': url
+    })
+
+@login_required
+def start_delivery(request, deliveryid):
+    form = DeliveryCandidateForm()
+    formset = FileMetaForm()
+    url = reverse('create-delivery', args=[deliveryid])
+    return deliveryform(form, formset, url)
+
+@login_required
+def create_delivery(request, deliveryid, deliverycand_id=None):
+    if request.method != 'POST' and deliverycand_id==None:
+        raise Http404()
+
+    delivery = get_object_or_404(Delivery, pk=deliveryid)
+    form = DeliveryCandidateForm(request.POST)
+    form = DeliveryCandidateForm(request.POST)
+    if deliverycand_id != None:
+        deliverycand = get_object_or_404(DeliveryCandidate, pk=deliverycand_id)
+        form.instance = deliverycand
+    form.instance.delivery = delivery
+    formset = FileMetaForm(request.POST, request.FILES, instance=form.instance)
+
+    if form.is_valid() and formset.is_valid():
+        form.instance.time_of_delivery = datetime.now()
+        form.save()
+        formset.save()
+        if 'deliver' in request.POST:
+            return HttpResponse('thanks')
+        else:
+            print dir(form.instance.filemeta_set)
+            url = reverse('edit-delivery', args=[deliveryid, form.instance.pk])
+            return deliveryform(form, formset, url)
+    else:
+        return deliveryform(form, formset)
