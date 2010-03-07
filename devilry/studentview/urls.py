c95ad84673d7e15cea71033679aa361b269e3da6
Bendik <bro@bro-laptop.(none)>
2010-03-07 16:58:11 +0100

    Moved history into separate template

--- devilry/studentview/urls.py
+++ devilry/studentview/urls.py
@@ -19,4 +19,6 @@ urlpatterns = patterns('devilry.studentview',
         'views.add_delivery', name='add-delivery'),
     url(r'^successful-delivery/(?P<delivery_id>\d+)$',
         'views.successful_delivery', name='successful-delivery'),
+    url(r'^show-history/$',
+        'views.show_history', name='show-history'),
 )
