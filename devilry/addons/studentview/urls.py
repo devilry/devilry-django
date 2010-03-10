8ac071fefa8f41849ff50244e079a09016c73f96
Espen A. Kristiansen <post@espenak.net>
2010-03-10 01:44:26 +0100

    Fixed studentview template urls.

--- devilry/addons/studentview/urls.py
+++ devilry/addons/studentview/urls.py
@@ -3,22 +3,13 @@ from django.conf.urls.defaults import *
 
 
 urlpatterns = patterns('devilry.addons.studentview',
-    url(r'^$',
-        'views.main', name='main'),
+    (r'^$', 'views.main'),
+    (r'^list-assignmentgroups/$', 'views.list_assignmentgroups'),
+    (r'^show-assignment-group/(?P<assignmentgroup_id>\d+)$', 'views.show_assignmentgroup'),
 
-    url(r'^list-assignmentgroups/$',
-        'views.list_assignmentgroups', name='list-assignmentgroups'),
-    url(r'^show-assignment-group/(?P<assignmentgroup_id>\d+)$',
-        'views.show_assignmentgroup', name='show-assignment-group'),
-
-    url(r'^list-deliveries/$',
-        'views.list_deliveries', name='list-deliveries'),
-    url(r'^show-delivery/(?P<delivery_id>\d+)$',
-        'views.show_delivery', name='show-delivery'),
-    url(r'^add-delivery/(?P<assignment_group_id>\d+)$',
-        'views.add_delivery', name='add-delivery'),
-    url(r'^successful-delivery/(?P<delivery_id>\d+)$',
-        'views.successful_delivery', name='successful-delivery'),
-    url(r'^show-history/$',
-        'views.show_history', name='show-history'),
+    (r'^list-deliveries/$', 'views.list_deliveries'),
+    (r'^show-delivery/(?P<delivery_id>\d+)$', 'views.show_delivery'),
+    (r'^add-delivery/(?P<assignment_group_id>\d+)$', 'views.add_delivery'),
+    (r'^successful-delivery/(?P<delivery_id>\d+)$', 'views.successful_delivery'),
+    (r'^show-history/$', 'views.show_history'),
 )
