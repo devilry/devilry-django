6443953405f3ce1d5318e6399cacfa236fd99761
Espen A. Kristiansen <post@espenak.net>
2010-03-10 21:39:02 +0100

    Fixed successful delivery.

--- devilry/addons/studentview/urls.py
+++ devilry/addons/studentview/urls.py
@@ -6,6 +6,7 @@ urlpatterns = patterns('devilry.addons.studentview',
     (r'^choose-assignment/$', 'views.choose_assignment'),
     (r'^show-delivery/(?P<delivery_id>\d+)$', 'views.show_delivery'),
     (r'^add-delivery/(?P<assignment_group_id>\d+)$', 'views.add_delivery'),
+    (r'^successful-delivery/(?P<assignment_group_id>\d+)$', 'views.successful_delivery'),
 
     (r'^show-history/$', 'views.show_history'),
     (r'^show-assignment-group/(?P<assignmentgroup_id>\d+)$', 'views.show_assignmentgroup'),
