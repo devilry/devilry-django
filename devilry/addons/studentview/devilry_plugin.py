fcfd75f2416d7681c5e941dbb19f49ee90f59189
Bendik <bro@bro-laptop.(none)>
2010-03-10 21:35:26 +0100

    Fixed dashbaord

--- devilry/addons/studentview/devilry_plugin.py
+++ devilry/addons/studentview/devilry_plugin.py
@@ -1,5 +1,15 @@
+from django.utils.translation import ugettext as _
 from devilry.addons.dashboard.dashboardplugin_registry import register
 
+register('add_delivery_key', 
+         'Add Delivery', 
+         '/studentview/choose-assignment/', 
+         'ikon.png', 
+         description = _('Add delivery.'))
 
-register('stuff', 'Stuff', '/tull', 'ikon.png', 'Saker')
-print 'hei'
+
+register('show_history_key', 
+         'Show History', 
+         '/studentview/show-history/', 
+         'ikon.png', 
+         description = _('Show History.'))
