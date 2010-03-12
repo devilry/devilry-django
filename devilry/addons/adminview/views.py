7616479a8aff7306e2645271dfdca11992222c5f
Espen A. Kristiansen <post@espenak.net>
2010-03-12 03:58:19 +0100

    Turned edit form in adminview into a class.

--- devilry/addons/adminview/views.py
+++ devilry/addons/adminview/views.py
@@ -21,83 +21,103 @@ def main(request):
         }, context_instance=RequestContext(request))
 
 
+class EditNodeBase(object):
+    VIEW_NAME = None
+    MODEL_CLASS = None
 
-def edit_node_generic(request, nodecls, parentnodecls, view_name,
-        form_cls, node_id=None):
+    def __init__(self, request, node_id):
+        self.request = request
 
-    message = None
-    if node_id == None:
-        node = nodecls()
-        post_url = reverse('add-' + view_name)
-    else:
-        node = get_object_or_404(nodecls, pk=node_id)
-        post_url = reverse('edit-' + view_name, args=(str(node.pk)))
+        if node_id == None:
+            self.node = self.MODEL_CLASS()
+        else:
+            self.node = get_object_or_404(self.MODEL_CLASS, pk=node_id)
+        if not self.node.can_save(request.user):
+            return HttpResponseForbidden("Forbidden")
 
-    if not node.can_save(request.user):
-        return HttpResponseForbidden("Forbidden")
+        if self.node.pk == None:
+            self.post_url = reverse('add-' + self.VIEW_NAME)
+        else:
+            self.post_url = reverse('edit-' + self.VIEW_NAME, args=(str(self.node.pk)))
 
-    if request.POST:
-        nodeform = form_cls(request.POST, instance=node)
-        if nodeform.is_valid():
-            nodeform.save()
-            message = nodecls._meta.verbose_name + ' saved'
-    else:
-        nodeform = form_cls(instance=node)
 
-    d = {'model_name': nodecls._meta.verbose_name}
-    if node_id:
-        title = _('Edit %(model_name)s' % d)
-    else:
-        title = _('New %(model_name)s') % d
+    def create_form(self):
+        class NodeForm(forms.ModelForm):
+            parentnode = forms.ModelChoiceField(required=False,
+                    queryset = self.MODEL_CLASS.where_is_admin(self.request.user))
+            class Meta:
+                model = self.MODEL_CLASS
+        return NodeForm
 
-    return render_to_response('devilry/adminview/edit_node.django.html', {
-        'title': title,
-        'model_plural_name': nodecls._meta.verbose_name_plural,
-        'nodeform': nodeform,
-        'message': message,
-        'post_url': post_url,
-        }, context_instance=RequestContext(request))
 
+    def create_view(self):
+        model_name = self.MODEL_CLASS._meta.verbose_name
+        form_cls = self.create_form()
+        message = None
+        if self.request.POST:
+            nodeform = form_cls(self.request.POST, instance=self.node)
+            if nodeform.is_valid():
+                nodeform.save()
+                message = model_name + ' saved'
+        else:
+            nodeform = form_cls(instance=self.node)
+
+        d = {'model_name': model_name}
+        if self.node.id == None:
+            title = _('New %(model_name)s') % d
+        else:
+            title = _('Edit %(model_name)s' % d)
+
+        return render_to_response('devilry/adminview/edit_node.django.html', {
+            'title': title,
+            'model_plural_name': self.MODEL_CLASS._meta.verbose_name_plural,
+            'nodeform': nodeform,
+            'message': message,
+            'post_url': self.post_url,
+            }, context_instance=RequestContext(self.request))
+
+
+class EditNode(EditNodeBase):
+    VIEW_NAME = 'node'
+    MODEL_CLASS = Node
+
+    def create_form(self):
+        class NodeForm(forms.ModelForm):
+            parentnode = forms.ModelChoiceField(required=False,
+                    queryset = Node.where_is_admin(self.request.user))
+            class Meta:
+                model = Node
+        return NodeForm
+
+class EditSubject(EditNodeBase):
+    VIEW_NAME = 'subject'
+    MODEL_CLASS = Subject
+
+class EditPeriod(EditNodeBase):
+    VIEW_NAME = 'period'
+    MODEL_CLASS = Period
+
+class EditAssignment(EditNodeBase):
+    VIEW_NAME = 'assignment'
+    MODEL_CLASS = Assignment
 
 
 @login_required
 def edit_node(request, node_id=None):
-    class NodeForm(forms.ModelForm):
-        parentnode = forms.ModelChoiceField(required=False,
-                queryset = Node.where_is_admin(request.user))
-        class Meta:
-            model = Node
-    return edit_node_generic(request, Node, Node, 'node', NodeForm, node_id)
+    return EditNode(request, node_id).create_view()
+
 
 @login_required
 def edit_subject(request, node_id=None):
-    class SubjectForm(forms.ModelForm):
-        parentnode = forms.ModelChoiceField(
-                queryset = Node.where_is_admin(request.user))
-        class Meta:
-            model = Subject
-    return edit_node_generic(request, Subject, Node, 'subject', SubjectForm,
-            node_id)
+    return EditSubject(request, node_id).create_view()
 
 @login_required
 def edit_period(request, node_id=None):
-    class PeriodForm(forms.ModelForm):
-        parentnode = forms.ModelChoiceField(
-                queryset = Subject.where_is_admin(request.user))
-        class Meta:
-            model = Period
-    return edit_node_generic(request, Period, Subject, 'period', PeriodForm,
-            node_id)
+    return EditPeriod(request, node_id).create_view()
 
 @login_required
 def edit_assignment(request, node_id=None):
-    class AssignmentForm(forms.ModelForm):
-        parentnode = forms.ModelChoiceField(
-                queryset = Period.where_is_admin(request.user))
-        class Meta:
-            model = Assignment
-    return edit_node_generic(request, Assignment, Period, 'assignment',
-            AssignmentForm, node_id)
+    return EditAssignment(request, node_id).create_view()
 
 
 
