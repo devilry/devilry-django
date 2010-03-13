f409d1c43ce415389bb1d83179fd9c910e5b8a08
Espen A. Kristiansen <post@espenak.net>
2010-03-13 22:04:19 +0100

    Named all admin views.

--- devilry/addons/adminview/urls.py
+++ devilry/addons/adminview/urls.py
@@ -4,9 +4,12 @@ from django.conf.urls.defaults import *
 generic_urls = []
 for x in ('node', 'subject', 'period', 'assignment'):
     generic_urls += [
-        (r'^%ss/edit/(?P<node_id>\d+)$' % x, 'views.edit_%s' % x),
-        (r'^%ss/add/$' % x, 'views.edit_%s' % x),
-        (r'^%ss/$' % x, 'views.list_%ss' % x)]
+        url(r'^%ss/edit/(?P<node_id>\d+)$' % x, 'views.edit_%s' % x,
+            name='devilry-admin-edit_%s' % x),
+        url(r'^%ss/add/$' % x, 'views.edit_%s' % x,
+            name='devilry-admin-add_%s' % x),
+        url(r'^%ss/$' % x, 'views.list_%ss' % x,
+            name='devilry-admin-list_%ss' % x)]
 
 urlpatterns = patterns('devilry.addons.adminview',
     (r'^$', 'views.main'),
