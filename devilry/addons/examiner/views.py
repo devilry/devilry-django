1df5d51963ef09d5f43b8c55a78edfb3b16f5e50
Espen A. Kristiansen <post@espenak.net>
2010-05-29 11:41:07 +0200

    Cleaned up some imports.

--- devilry/addons/examiner/views.py
+++ devilry/addons/examiner/views.py
@@ -1,20 +1,12 @@
-from os.path import basename
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response, get_object_or_404
-from django.http import HttpResponseRedirect, HttpResponseForbidden
-from django.core.urlresolvers import reverse
+from django.http import HttpResponseForbidden
 from django.template import RequestContext
-from django import forms
-from django.forms.formsets import formset_factory
 from devilry.core.models import (Delivery, Feedback, AssignmentGroup,
         Node, Subject, Period, Assignment, FileMeta)
 from devilry.core import gradeplugin_registry
-from django.db import transaction
 
 from devilry.core.utils.GroupNodes import group_assignments, group_assignmentgroups 
-
-from django.http import HttpResponse
-from django.core.servers.basehttp import FileWrapper
 
 
 @login_required
