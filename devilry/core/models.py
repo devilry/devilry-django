8762a3db859c958911262be0839a7731fa85e4cd
Espen A. Kristiansen <post@espenak.net>
2010-06-29 20:38:25 +0200

    Some cleanup.

--- devilry/core/models.py
+++ devilry/core/models.py
@@ -1,9 +1,7 @@
 from datetime import datetime
 
-from django.db.models.signals import post_save, post_delete
 from django.db import models
-from django.contrib.auth.models import User, Permission
-from django.conf import settings
+from django.contrib.auth.models import User
 from django.db.models import Q
 from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext as _
