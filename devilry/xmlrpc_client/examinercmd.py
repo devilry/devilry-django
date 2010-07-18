85d0f4a364498c92138b9ab0a8fc165e49d69d1c
Espen A. Kristiansen <post@espenak.net>
2010-07-18 15:44:21 +0200

    Made cli sync with root as cwd.

--- devilry/xmlrpc_client/examinercmd.py
+++ devilry/xmlrpc_client/examinercmd.py
@@ -55,8 +55,8 @@ class Sync(Command):
     urlpath = '/xmlrpc_examiner/'
 
     def command(self):
-        AssignmentSync(self.get_cookiepath(), self.get_server(),
-                self.get_url())
+        AssignmentSync(self.get_configdir(), self.get_cookiepath(),
+                self.get_server(), self.get_url())
 
 class Feedback(Command):
     name = 'feedback'
