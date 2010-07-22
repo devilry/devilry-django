38c0b7c29ec64b4f0917bd22c0fc8dac676e7c38
Espen A. Kristiansen <post@espenak.net>
2010-07-22 21:19:43 +0200

    Added GradeConf to approved-grade.

--- devilry/addons/grade_default/devilry_plugin.py
+++ devilry/addons/grade_default/devilry_plugin.py
@@ -2,13 +2,14 @@ from django.utils.translation import ugettext as _
 
 from devilry.core import gradeplugin_registry
 from devilry.xmlrpc.gradeconf import GradeConf
+
 from gradeviews import view
 from models import CharFieldGrade
 
 
 gradeplugin_registry.register(
         view = view,
-        xmlrpc_conf = GradeConf(),
+        xmlrpc_gradeconf = GradeConf(),
         model_cls = CharFieldGrade,
         label = _('Manual grade handling'),
         description = _('Examiners type in grades manually in a text field ' \
