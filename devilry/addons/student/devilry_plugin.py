1ce609ea10abdda5834d43018cd0a0bc15142fc5
bendikro <you@example.com>
2010-08-03 15:46:46 +0200

    Fixed dashboard link

--- devilry/addons/student/devilry_plugin.py
+++ devilry/addons/student/devilry_plugin.py
@@ -4,7 +4,7 @@ from django.core.urlresolvers import reverse
 
 register('show_assignments_key', 
          'Show Assignments', 
-         reverse('devilry-student-show_assignments'),
+         reverse('devilry-student-show-assignments'),
          description = _('Show Assignments.'),
          icon='ikon.png', 
          student_access=True,
