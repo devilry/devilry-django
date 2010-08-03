0514209e6559e2893654fbe4b599ab7193c3fb45
bendikro <you@example.com>
2010-08-03 15:45:23 +0200

    Removed crap

--- devilry/addons/student/devilry_plugin.py
+++ devilry/addons/student/devilry_plugin.py
@@ -2,23 +2,6 @@ from django.utils.translation import ugettext as _
 from devilry.addons.dashboard.dashboardplugin_registry import register
 from django.core.urlresolvers import reverse
 
-register('add_delivery_key', 
-         'Add Delivery', 
-         reverse('devilry-student-add_delivery_choose_assignment'),
-         description = _('Add delivery.'),
-         icon='ikon.png',
-         student_access=True,
-         )
-
-
-register('show_history_key', 
-         'Show History', 
-         reverse('devilry-student-show_history'),
-         description = _('Show History.'),
-         icon='ikon.png', 
-         student_access=True,
-         )
-
 register('show_assignments_key', 
          'Show Assignments', 
          reverse('devilry-student-show_assignments'),
