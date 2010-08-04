2c68e790029370ea44787ec5d5fe649f014cd807
Espen A. Kristiansen <post@espenak.net>
2010-08-04 19:33:35 +0200

    Some changes

--- devilry/addons/admin/templates/devilry/admin/autocomplete-nodename.js
+++ devilry/addons/admin/templates/devilry/admin/autocomplete-nodename.js
@@ -1,18 +1,19 @@
 {% comment %} vim: set ts=4 sts=4 et sw=4: {% endcomment %}
 
 $(function() {
-    $("#autocomplete-{{ clsname }}name").keyup(function(e) {
+    $("#autocomplete-{{ clsname }}").keyup(function(e) {
         if (e.ctrlKey || e.altKey || e.shiftKey || e.metaKey) {
             return;
         }
         var value = $(this).val();
-        var result = $("#autocomplete-{{ clsname }}name-result");
+        var result = $("#autocomplete-{{ clsname }}-result");
         result.empty();
         if(value.length > 1 || value.length == 0) {
             $.getJSON('{{ jsonurl }}', {'term':value}, function(data) {
                 $.each(data, function(i, item) {
+                    var path = item.path.replace(value, "<strong>" + value + "</strong>");
                     $("<li></li>").append(
-                        $("<a>" + item.path + "</a>")
+                        $("<a>" + path + "</a>")
                         .attr("href", item.editurl))
                     .appendTo(result);
                 });
