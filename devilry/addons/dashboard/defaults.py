2845ba1962ac7f32df87465da8c2b972c5f424f7
bendikro <you@example.com>
2010-08-29 19:38:28 +0200

    Readded the DATTIME_FORMAT

--- devilry/addons/dashboard/defaults.py
+++ devilry/addons/dashboard/defaults.py
@@ -1,8 +1,16 @@
 """ Default values for dashboard plugins
 
+.. attr:: DATETIME_FORMAT
+
+    Used to format dates in the dashboard.
+
 .. attr:: DEFAULT_DISPLAYNUM
 
     Default number of items to display in a list/table in the dashboard.
 """
 
+DATETIME_FORMAT = "%Y-%m-%d %H:%M"
+
 DEFAULT_DISPLAYNUM = 7
+
+
