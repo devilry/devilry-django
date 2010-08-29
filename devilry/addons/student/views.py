79228376518673fc5ba4e6e63d1ff2af0b5ee8d8
bendikro <you@example.com>
2010-08-29 18:54:16 +0200

    Now verifies that the file names on a delivery are unique

--- devilry/addons/student/views.py
+++ devilry/addons/student/views.py
@@ -13,13 +13,12 @@ from django.utils.translation import ugettext as _
 from devilry.ui.messages import UiMessages
 from devilry.core.utils.GroupNodes import group_assignmentgroups, print_tree
 from devilry.core.models import Delivery, AssignmentGroup
-
-
+from devilry.ui.defaults import DATETIME_FORMAT
+from devilry.core.utils.verify_unique_entries import verify_unique_entries
 
 class UploadFileForm(forms.Form):
     file = forms.FileField()
 UploadFileFormSet = formset_factory(UploadFileForm, extra=10)
-
 
 
 @login_required
@@ -31,14 +30,19 @@ def add_delivery(request, assignment_group_id, messages=None):
     if request.method == 'POST':
         formset = UploadFileFormSet(request.POST, request.FILES)
         if formset.is_valid():
-            delivery = Delivery.begin(assignment_group, request.user)
-            for f in request.FILES.values():
-                filename = basename(f.name) # do not think basename is needed, but at least we *know* we only get the filename.
-                delivery.add_file(filename, f.chunks())
-            delivery.finish()
-            return HttpResponseRedirect(
+            if not verify_unique_entries(request.FILES.values()):
+                if not messages:
+                    messages = UiMessages()
+                messages.add_warning(_("The filenames are not unique."))
+            else:
+                delivery = Delivery.begin(assignment_group, request.user)
+                for f in request.FILES.values():
+                    filename = basename(f.name) # do not think basename is needed, but at least we *know* we only get the filename.
+                    delivery.add_file(filename, f.chunks())
+                delivery.finish()
+                return HttpResponseRedirect(
                     reverse('devilry-student-successful_delivery',
-                        args=[assignment_group_id]))
+                            args=[assignment_group_id]))
     else:
         formset = UploadFileFormSet()
 
@@ -48,10 +52,38 @@ def add_delivery(request, assignment_group_id, messages=None):
         'messages': messages,
         }, context_instance=RequestContext(request))
 
+from devilry.core.devilry_email import DevilryEmail
+
 def successful_delivery(request, assignment_group_id):
     messages = UiMessages()
     messages.add_info(_('Successful delivery'))
-    #return add_delivery(request, assignment_group_id, messages)
+    
+    assignment_group = get_object_or_404(AssignmentGroup, pk=assignment_group_id)
+    period = assignment_group.parentnode.parentnode
+    subject = period.parentnode
+    
+    latest = assignment_group.deliveries.all()[0]
+    print "time of del:", latest.time_of_delivery.strftime(DATETIME_FORMAT)
+
+    email_message = "This is a receipt for your delivery."
+    email_message += '\n\n'
+    email_message += "Subject: %s - %s\n" % (subject.long_name, period.long_name)
+    email_message += "Time of delivery: %s\n" % latest.time_of_delivery.strftime(DATETIME_FORMAT)
+    email_message += "Files:\n"
+
+    for fm in latest.filemetas.all():
+        email_message += " - %s (%d bytes)\n" % (fm.filename, fm.size)
+        
+    email_message += '\n\n'
+
+    mail = DevilryEmail()
+    mail.send_email(request.user, 
+                    "Receipt for your delivery on %s" % (subject.short_name), 
+                    email_message)
+    
+    
+    latest.time_of_delivery.strftime(DATETIME_FORMAT)
+
     return show_assignmentgroup(request, assignment_group_id, messages)
 
 
