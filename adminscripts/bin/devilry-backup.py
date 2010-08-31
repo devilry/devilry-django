0aceb154c5af8d18ccdc712dcd2438b1003c676e
Espen A. Kristiansen <post@espenak.net>
2010-08-31 21:49:13 +0200

    Working backup script

--- adminscripts/bin/devilry-backup.py
+++ adminscripts/bin/devilry-backup.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 
-from optparse import OptionParser
 import os
 import sys
 import os.path
+import logging
+import textwrap
+from shutil import rmtree
 from subprocess import call, Popen, PIPE, STDOUT
 from datetime import datetime
-import logging
-from shutil import rmtree
+from optparse import OptionParser
 
 from django.utils.importlib import import_module
 
@@ -33,7 +34,7 @@ def callexp(cmd):
                 "Excecution of %(cmd)s failed: %(e)s" % vars())
 
 def fsdeliverystore_backup(tarfile, filesroot):
-    cmd = "tar -cvjf %(tarfile)s --exclude \"lost+found\" %(filesroot)s" % vars()
+    cmd = "tar -cvjf %(tarfile)s -C %(filesroot)s ./" % vars()
     callexp(cmd)
 
 def fsdeliverystore_restore(tarfile, filesroot):
@@ -48,10 +49,99 @@ def fsdeliverystore_clear(filesroot):
             rmtree(os.path.join(filesroot, dirname))
 
 
+def backup(settings, backupdir, djangoadmin):
+    os.makedirs(backupdir)
+    logging.info("Creating backup in %(backupdir)s." % vars())
+
+    dbdumpfile = os.path.join(backupdir, "db.json")
+    fsdeliverystore_tarfile = os.path.join(backupdir, 'fsdeliverystore.tar.bz2')
+
+    # DB backup
+    f = open(dbdumpfile, 'wb')
+    logging.info("Backing up the database to %(dbdumpfile)s..." % vars())
+    p = Popen([djangoadmin, 'dumpdata', '--indent=4',
+            '--settings', opt.settings],
+        stdout=f, stderr=PIPE)
+    stderr = p.communicate()[1].strip()
+    f.close()
+    if stderr:
+        logging.warning("Error messages from db dump: %s" % stderr)
+    logging.info("... database backup complete")
+
+    # File backup
+    logging.info("Backing up the files ...")
+    if settings.DELIVERY_STORE_BACKEND == 'devilry.core.deliverystore.FsDeliveryStore':
+        logging.info("Storing files in %(fsdeliverystore_tarfile)s." %
+                vars())
+        try:
+            fsdeliverystore_backup(fsdeliverystore_tarfile,
+                    settings.DELIVERY_STORE_ROOT)
+        except BackupError, e:
+            logging.error(str(e))
+        logging.info("... file backup complete")
+    else:
+        logging.error(
+                "The backup script only supports FsDeliveryStoreBackend")
+        raise SystemExit()
+
+    logging.info("*** Successful backup to: %(backupdir)s" % vars())
+
+def restore(settings, restoredir, djangoadmin):
+    print
+    q = textwrap.fill("We are about to to a restore of a backup. " \
+            "This action will destroy all data in the database " \
+            "and clear all delivered files! Are you sure you want to " \
+            "continue? [yes/no]")
+    inp = raw_input(q + " ")
+    if inp != 'yes':
+        raise SystemExit("Restore aborted")
+
+    dbdumpfile = os.path.join(restoredir, "db.json")
+    fsdeliverystore_tarfile = os.path.join(restoredir, 'fsdeliverystore.tar.bz2')
+
+    # Clear db
+    logging.info("Clearing the database")
+    for app in settings.INSTALLED_APPS:
+        appname = app.split('.')[-1]
+        cmd = "%(djangoadmin)s reset --noinput %(appname)s" % vars()
+        callexp(cmd)
+
+    # DB restore
+    f = open(dbdumpfile, 'rb')
+    logging.info("Restoring the database from %(dbdumpfile)s..." % vars())
+    p = Popen([djangoadmin, 'loaddata', '--settings', opt.settings],
+        stdout=PIPE, stderr=STDOUT, stdin=f)
+    stdout = p.communicate()[0]
+    f.close()
+    logging.info(stdout)
+    logging.info("... database restore complete")
+
+    # Remove existing files
+    logging.info("Removing old the delivery-files ...")
+
+    # File restore
+    logging.info("Restoring the files ...")
+    if settings.DELIVERY_STORE_BACKEND == 'devilry.core.deliverystore.FsDeliveryStore':
+        logging.info("Restoring delivery-files from %(fsdeliverystore_tarfile)s." %
+                vars())
+        try:
+            fsdeliverystore_clear(settings.DELIVERY_STORE_ROOT)
+            fsdeliverystore_restore(fsdeliverystore_tarfile,
+                    settings.DELIVERY_STORE_ROOT)
+        except BackupError, e:
+            logging.error(str(e))
+        logging.info("... delivery-files restore complete")
+    else:
+        logging.error(
+                "The backup script only supports FsDeliveryStoreBackend")
+        raise SystemExit()
+
+    logging.info("*** Successful restore from: %(restoredir)s" % vars())
+
 
 if __name__ == "__main__":
     p = OptionParser(
-            usage = "%prog [options] <backup|restore> <backupdir>")
+            usage = "%prog [options] <backup|restore> <backupdir> [restoredir]")
     p.add_option("--settings", dest="settings", default='devilry.settings',
             help="Django settings file.", metavar="SETTINGS")
     p.add_option("--djangoadmin-script", dest="djangoadmin",
@@ -62,11 +152,17 @@ if __name__ == "__main__":
     p.add_option("-q", "--quiet", action="store_const",
         const=logging.ERROR, dest="loglevel", default=logging.INFO,
         help="Don't show extra information (only errors).")
+    p.add_option("--no-backup-on-restore", action="store_false",
+        dest="backup_on_restore", default=True,
+        help="Normally a backup is made before a restore (because restore "\
+            "destroys data). Use this to skip the backup.")
     (opt, args) = p.parse_args()
 
-    if len(args) != 2:
+
+    def exit_help():
         p.print_help()
         raise SystemExit()
+
 
     logging.basicConfig(level=opt.loglevel)
     try:
@@ -77,95 +173,18 @@ if __name__ == "__main__":
     action = args[0]
     djangoadmin = opt.djangoadmin
 
-    if action == "backup":
+    if not action in ("backup", "restore"):
+        exit_help()
+    if action == "backup" and len(args) != 2:
+        exit_help()
+    if action == "restore" and len(args) != 3:
+        exit_help()
+
+    if action == "backup" or opt.backup_on_restore:
         backupdir = os.path.join(args[1], opt.settings,
                 datetime.now().isoformat())
-    elif action == "restore":
-        backupdir = args[1]
-    else:
-        p.print_help()
-        raise SystemExit()
+        backup(settings, backupdir, djangoadmin)
 
-    dbdumpfile = os.path.join(backupdir, "db.json")
-    fsdeliverystore_tarfile = os.path.join(backupdir, 'fsdeliverystore.tar.bz2')
-
-
-    if action == "backup":
-        os.makedirs(backupdir)
-        logging.info("Creating backup in %(backupdir)s." % vars())
-
-        # DB backup
-        f = open(dbdumpfile, 'wb')
-        logging.info("Backing up the database to %(dbdumpfile)s..." % vars())
-        p = Popen([djangoadmin, 'dumpdata', '--indent=4',
-                '--settings', opt.settings],
-            stdout=f, stderr=PIPE)
-        stderr = p.communicate()[1].strip()
-        f.close()
-        if stderr:
-            logging.warning("Error messages from db dump: %s" % stderr)
-        logging.info("... database backup complete")
-
-        # File backup
-        logging.info("Backing up the files ...")
-        if settings.DELIVERY_STORE_BACKEND == 'devilry.core.deliverystore.FsDeliveryStore':
-            logging.info("Storing files in %(fsdeliverystore_tarfile)s." %
-                    vars())
-            try:
-                fsdeliverystore_backup(fsdeliverystore_tarfile,
-                        settings.DELIVERY_STORE_ROOT)
-            except BackupError, e:
-                logging.error(str(e))
-            logging.info("... file backup complete")
-        else:
-            logging.error(
-                    "The backup script only supports FsDeliveryStoreBackend")
-            raise SystemExit()
-
-        logging.info("Successful backup to: %(backupdir)s" % vars())
-
-    elif action == "restore":
-        inp = raw_input("This action will destroy all data in the database " \
-                "and clear all delivered files! Are you sure you want to " \
-                "continue? [yes/no] ")
-        if inp != 'yes':
-            raise SystemExit("Restore aborted")
-
-        # Clear db
-        logging.info("Clearing the database")
-        for app in settings.INSTALLED_APPS:
-            appname = app.split('.')[-1]
-            cmd = "%(djangoadmin)s reset --noinput %(appname)s" % vars()
-            callexp(cmd)
-
-        # DB restore
-        f = open(dbdumpfile, 'rb')
-        logging.info("Restoring the database from %(dbdumpfile)s..." % vars())
-        p = Popen([djangoadmin, 'loaddata', '--settings', opt.settings],
-            stdout=PIPE, stderr=STDOUT, stdin=f)
-        stdout = p.communicate()[0]
-        f.close()
-        logging.info(stdout)
-        logging.info("... database restore complete")
-
-        # Remove existing files
-        logging.info("Removing old the delivery-files ...")
-
-        # File restore
-        logging.info("Restoring the files ...")
-        if settings.DELIVERY_STORE_BACKEND == 'devilry.core.deliverystore.FsDeliveryStore':
-            logging.info("Restoring delivery-files from %(fsdeliverystore_tarfile)s." %
-                    vars())
-            try:
-                fsdeliverystore_clear(settings.DELIVERY_STORE_ROOT)
-                fsdeliverystore_restore(fsdeliverystore_tarfile,
-                        settings.DELIVERY_STORE_ROOT)
-            except BackupError, e:
-                logging.error(str(e))
-            logging.info("... delivery-files restore complete")
-        else:
-            logging.error(
-                    "The backup script only supports FsDeliveryStoreBackend")
-            raise SystemExit()
-
-        logging.info("Successful restore from: %(backupdir)s" % vars())
+    if action == "restore":
+        restoredir = args[2]
+        restore(settings, restoredir, djangoadmin)
