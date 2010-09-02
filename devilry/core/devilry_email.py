836802164329b2069a9d7e79f52f55d12291932b
bendikro <you@example.com>
2010-09-02 17:51:20 +0200

    Send a list of receivers to send_mail

--- devilry/core/devilry_email.py
+++ devilry/core/devilry_email.py
@@ -2,12 +2,15 @@ from django.utils.translation import ugettext as _
 from django.core.mail import send_mail
 from devilry.settings import email_subject_prefix
 
-def send_email(user, subject, message):
+def send_email(user_objects_to_send_to, subject, message):
     message += "\n\n"
     message += _("This is a message from the Devilry assignment delivery system. " \
                      "Please do not respond to this email.")
-    send_mail(email_subject_prefix + subject, message, 'devilry@example.com',
-              [user.email], fail_silently=False)
+	emails = []
+	for u in user_objects_to_send_to:
+        emails.append(u.email)
+	send_mail(email_subject_prefix + subject, message, 'devilry@example.com',
+             emails, fail_silently=False)
 
         
         
