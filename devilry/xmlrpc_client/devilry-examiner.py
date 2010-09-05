4727145020577f794c39934284d8343c4266377a
Espen A. Kristiansen <post@espenak.net>
2010-09-05 18:17:05 +0200

    Fixed import errors in cli when installed globally.

--- devilry/xmlrpc_client/devilry-examiner.py
+++ devilry/xmlrpc_client/devilry-examiner.py
@@ -1,13 +1,17 @@
-#!/usr/bin/env python
-
-from cli import Cli, Login, Init, FormLogin
-from examinercmd import ListAssignmentGroups, Sync, ListAssignments, \
-        Feedback, UnpublishFeedback, PublishFeedback, InfoCmd, Guide
+try:
+    from devilry_xmlrpc_client.cli import (Cli, Login, Init, FormLogin)
+    from devilry_xmlrpc_client.examinercmd import (ListAssignmentGroups,
+            Sync, ListAssignments, Feedback, UnpublishFeedback,
+            PublishFeedback, InfoCmd, Guide)
+except ImportError:
+    from devilry.xmlrpc_client.cli import (Cli, Login, Init, FormLogin)
+    from devilry.xmlrpc_client.examinercmd import (ListAssignmentGroups,
+            Sync, ListAssignments, Feedback, UnpublishFeedback,
+            PublishFeedback, InfoCmd, Guide)
 
 
 # TODO: make sure SESSION_COOKIE_SECURE is enabled by default or something
 #       see: http://docs.djangoproject.com/en/dev/topics/http/sessions/#settings
-
 
 if __name__ == '__main__':
     Cli([
--- devilry/xmlrpc_client/setup.py
+++ devilry/xmlrpc_client/setup.py
@@ -2,16 +2,16 @@
 
 import ez_setup
 ez_setup.use_setuptools()
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(name = 'devilry_xmlrpc_client',
-      version = '0.1.3',
-      description = 'The devilry xmlrpc client and shell scripts.',
-      url = 'http://devilry.github.com',
-      author = 'Devilry',
-      author_email = 'devilry-support@ifi.uio.no',
-      package_dir = {'devilry_xmlrpc_client': ''},
-      packages = ['devilry_xmlrpc_client'],
-      scripts = ['devilry-examiner.py']
-     )
+    version = '0.1.4',
+    description = 'The devilry xmlrpc client and shell scripts.',
+    url = 'http://devilry.github.com',
+    author = 'Devilry',
+    author_email = 'devilry-support@ifi.uio.no',
+    package_dir = {'devilry_xmlrpc_client': ''},
+    packages = ['devilry_xmlrpc_client'],
+    scripts = ['devilry-examiner.py']
+)
