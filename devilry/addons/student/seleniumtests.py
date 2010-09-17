3367b98b27b264ee2d60dc5849e149953620fbff
Espen A. Kristiansen <post@espenak.net>
2010-09-17 14:01:31 +0200

    Selenium tests for student, especially the forbidden parts.

--- devilry/addons/student/seleniumtests.py
+++ devilry/addons/student/seleniumtests.py
@@ -1,39 +1,69 @@
-"""
-Here are selenium tests for student
-"""
-
-from django.test import TestCase
 from selenium import selenium
 import unittest, time, re
 
-class StudentDeliveryTest(unittest.TestCase):
+class TestDashboard(unittest.TestCase):
     def setUp(self):
         self.verificationErrors = []
         self.selenium = selenium("localhost", 4444, "*chrome", "http://localhost:8000/")
         self.selenium.start()
-    
-    def test_delivery(self):
         sel = self.selenium
-        sel.open("/ui/login?next=/")
-        sel.type("id_username", "student1")
+        sel.open("/ui/login")
+        sel.type("id_username", "may")
         sel.type("id_password", "test")
         sel.click("login")
         sel.wait_for_page_to_load("30000")
-        try: self.failUnless(sel.is_text_present("Add Delivery"))
+    
+    def assert403(self, f, *args, **kw):
+        try:
+            f(*args, **kw)
+        except Exception, e:
+            self.assertTrue("403" in str(e))
+            self.assertTrue("FORBIDDEN" in str(e))
+        else:
+            self.fail("403 not raised for %s, %s, %s" % (f, args, kw))
+    
+    def test_normal_workflow(self):
+        sel = self.selenium
+        try: self.failUnless(sel.is_text_present("Dashboard"))
         except AssertionError, e: self.verificationErrors.append(str(e))
-        sel.click("link=Add Delivery")
-        sel.wait_for_page_to_load("30000")
-        sel.click("link=Week 1 (student1)")
-        sel.wait_for_page_to_load("30000")
-        sel.type("id_form-0-file", "/home/bro/a.txt")
-        sel.click("deliver")
-        sel.wait_for_page_to_load("30000")
-        try: self.failUnless(sel.is_text_present("Successful delivery"))
+        try: self.failUnless(sel.is_text_present("Assignments"))
         except AssertionError, e: self.verificationErrors.append(str(e))
+        try: self.failUnless(sel.is_text_present("Money making basics"))
+        except AssertionError, e: self.verificationErrors.append(str(e))
+
+        sel.click("link=Effective habits for lazy people")
+        sel.wait_for_page_to_load("30000")
+        try: self.failUnless(sel.is_text_present(
+            "tull101.21century.avoiding-work (may)"))
+        except AssertionError, e: self.verificationErrors.append(str(e))
+
+        sel.click("//div[@id='content']/table[2]/tbody/tr[4]/td[5]/a")
+        sel.wait_for_page_to_load("30000")
+        try: self.failUnless(sel.is_text_present("Still not good enough!"))
+        except AssertionError, e: self.verificationErrors.append(str(e))
+
+        sel.click("link=tull101.21century.avoiding-work (may)")
+        sel.wait_for_page_to_load("30000")
+        sel.click("link=Show feedback")
+        sel.wait_for_page_to_load("30000")
+        try:
+            self.failUnless(sel.is_text_present(
+                "Ok i will pass you, but you have to do better on the next assignment."))
+        except AssertionError, e: self.verificationErrors.append(str(e))
+
+
+    def test_forbidden(self):
+        sel = self.selenium
+        self.assert403(sel.open, "/student/1")
+        self.assert403(sel.open, "/student/delivery/1")
+        self.assert403(sel.open, "/student/add-delivery/1")
+        self.assert403(sel.open, "/student/successful-delivery/1")
+        self.assert403(sel.open, "/ui/download-file/3")
     
     def tearDown(self):
         self.selenium.stop()
         self.assertEqual([], self.verificationErrors)
+
 
 if __name__ == "__main__":
     unittest.main()
--- devilry/selenium-readme.txt
+++ devilry/selenium-readme.txt
@@ -1,11 +1,33 @@
-Needs robotframework:
-easy_install robotframework
+Install the selenium server
+###########################
 
-SeleniumLibrary (http://code.google.com/p/robotframework-seleniumlibrary/wiki)
-hg clone https://robotframework-seleniumlibrary.googlecode.com/hg/ robotframework-seleniumlibrary
-python setup.py install
+Download selenium RC from http://seleniumhq.org/download/. Put
+``selenium-server.jar`` somewhere of your choice.
 
-To run tests:
-robotframework-seleniumlibrary/demo$ python rundemo.py selenium start
 
-devilry$ make selenium-test
+Install the selenium python library
+###################################
+
+Instructions here: http://pypi.python.org/pypi/selenium/2.0a5
+
+Note that the instructions say to use ``pip install -U selenium``, but::
+
+    ~$ easy_install -U selenium
+
+Works as well.
+
+
+Running the tests
+#################
+
+1. Start the selenium server with (from where you installed it)::
+
+    ~$ java -jar selenium-server.jar
+
+2. Start the django server (from this directory)::
+
+    ~$ python manage.py runserver
+
+3. Run the tests with::
+
+    ~$ make selenium-tests
