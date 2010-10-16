cf0b69ac0f0fde7bd6866fdcf1186fa85f5cc544
Espen A. Kristiansen <post@espenak.net>
2010-10-16 23:01:49 +0200

    Simple statistics for a single user.

--- devilry/addons/grade_rstschema/views.py
+++ devilry/addons/grade_rstschema/views.py
@@ -111,32 +111,36 @@ def subject_summary(request, subject_id):
     #period = get_object_or_404(Period, pk=subject_id)
     #if not subject.can_save(request.user):
         #return HttpResponseForbidden("Forbidden")
-    
+    from devilry.core.gradeplugin import registry
+
     def iter():
         assignment_groups = AssignmentGroup.active_where_is_candidate(
                 request.user)
-        assignment_groups = assignment_groups.filter(
-               parentnode__grade_plugin='grade_rstschema:rstschemagrade')
-        assignment_groups = assignment_groups.order_by("parentnode__parentnode")
+        assignment_groups = assignment_groups.order_by(
+                "parentnode__parentnode", "parentnode__grade_plugin")
         current_period = None
-        groups = []
+        plugins = None
         for group in assignment_groups:
-            if current_period == None:
+            if current_period == None \
+                    or current_period.id != group.parentnode.parentnode.id:
+                if current_period != None:
+                    yield current_period, plugins
                 current_period = group.parentnode.parentnode
-            if current_period.id != group.parentnode.parentnode.id:
-                yield current_period, groups
-                current_period = group.parentnode.parentnode
-                groups = []
+                current_gradeplugin = None
+                plugins = []
+            if current_gradeplugin != group.parentnode.grade_plugin:
+                current_gradeplugin = group.parentnode.grade_plugin
+                plugins.append((registry.getitem(current_gradeplugin), []))
             d = group.get_latest_delivery_with_published_feedback()
-            if d:
-                groups.append((
-                        group,
-                        d.feedback.get_grade_as_short_string()))
-        yield current_period, groups
 
-    for x in iter():
-        print x
-    
+            value = None
+            if d:
+                value = d.feedback.get_grade_as_short_string()
+            else:
+                value = group.get_localized_status()
+            plugins[-1][1].append((group, value))
+        yield current_period, plugins
+
     return render_to_response(
         'devilry/grade_rstschema/subject_summary.django.html', {
             'periods': iter(),
