e4288fa5dbe8a773e69ca7f72dcc6023744f8535
Espen A. Kristiansen <post@espenak.net>
2010-10-19 19:22:25 +0200

    Improved gradestats overview.

--- devilry/addons/gradestats/dashboardviews.py
+++ devilry/addons/gradestats/dashboardviews.py
@@ -2,6 +2,7 @@ from django.template.loader import render_to_string
 from django.template import RequestContext
 
 from devilry.core.models import Period
+from devilry.core.utils.GroupNodes import group_nodes
 
 
 class GroupPeriodsBySubject(object):
@@ -34,14 +35,18 @@ def overview(request):
     where_is_student = Period.objects.filter(
             assignments__assignmentgroups__candidates__student=request.user)
     where_is_student = where_is_student.distinct().order_by(*order)
-    if where_is_student.count() == 0 \
-            and where_is_admin_or_superadmin.count() == 0:
+    studcount = where_is_student.count()
+    admcount = where_is_admin_or_superadmin.count()
+    if studcount == 0 and admcount == 0:
         return None
+
+
+    adm = group_nodes(where_is_admin_or_superadmin, 0)
+    stud = group_nodes(where_is_student, 0)
 
     return render_to_string(
         'devilry/gradestats/overview.django.html', {
-            'where_is_admin_or_superadmin':
-                GroupPeriodsBySubject(where_is_admin_or_superadmin),
-            'where_is_student':
-                GroupPeriodsBySubject(where_is_student),
+            'showboth': studcount and admcount,
+            'where_is_admin_or_superadmin': adm,
+            'where_is_student': stud,
         }, context_instance=RequestContext(request))
