bdd1324b2a020b14e7bef51b21b950b6919cedc2
Espen A. Kristiansen <post@espenak.net>
2010-10-25 16:17:14 +0200

    Made a js file for general utilities.

--- devilry/resources/ui/js/jquery.filtertable.js
+++ devilry/resources/ui/js/jquery.filtertable.js
@@ -429,49 +429,4 @@
         });
 
     };
-
-
-
-
-    /* This is independent of the filtertable, and could be moved elsewhere. */
-    $.fn.boxWithLabel = function(args) {
-      args.type = args.type || "checkbox";
-
-      var box = $(this);
-      var button = $("<input></input>")
-        .attr("type", args.type)
-        .appendTo(box);
-      if(typeof(args.value) != undefined) {
-        button.val(args.value);
-      }
-      if(args.checked) {
-        button.attr("checked", "checked");
-      }
-      var lbl = $("<a></a>")
-        .attr("href", "#")
-        .html(args.label)
-        .appendTo(box);
-
-      // Events
-      lbl.click(function() {
-          button.click();
-          return false;
-        });
-      if(args.click) {
-        button.click(args.click);
-      }
-
-      return box;
-    };
-
-
-
-
-    $.log = function(message) {
-      if(window.console) {
-        console.debug(message);
-      } else {
-        alert(message);
-      }
-    };
   })(jQuery);
