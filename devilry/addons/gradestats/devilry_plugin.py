96c82bed810a74d49153fba13ea1d863d5827c31
Espen A. Kristiansen <post@espenak.net>
2010-10-27 02:37:42 +0200

    Minor changes

--- devilry/addons/gradestats/devilry_plugin.py
+++ devilry/addons/gradestats/devilry_plugin.py
@@ -1,6 +1,10 @@
+from django.utils.translation import ugettext as _
 from django.core.urlresolvers import reverse
 
 from devilry.addons.admin.actionregistry import periodactions
+from devilry.addons.quickdash.dashboardplugin_registry import (
+        DashboardItem, personalgroup)
+from devilry.core.models import Candidate
 
 
 periodactions.add_action(
@@ -8,3 +12,14 @@ periodactions.add_action(
     urlcallback = lambda p:
         reverse('devilry-gradestats-periodstats', args=[str(p.id)])
 )
+
+
+#def is_student(user):
+    #return Candidate.objects.filter(student=user).count > 0
+
+#personalgroup.additems(
+    #DashboardItem(
+        #title = _('Grade statistics'),
+        #url = "http://example.com",
+        #check = lambda r: is_student(r.user)),
+#)
