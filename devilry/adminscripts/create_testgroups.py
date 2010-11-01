25d3790cf7b6542d1fecb1b19909acb3c579d95d
Espen A. Kristiansen <post@espenak.net>
2010-11-01 22:04:18 +0100

    Started working on a test group creation script.

---
+++ devilry/adminscripts/create_testgroups.py
@@ -0,0 +1,37 @@
+from optparse import OptionParser
+import logging
+
+from common import (setup_logging, load_devilry_plugins,
+    add_settings_option, set_django_settings_module, add_quiet_opt,
+    add_debug_opt)
+
+
+p = OptionParser(
+        usage = "%prog [options] <assignment-id>")
+add_settings_option(p)
+add_quiet_opt(p)
+add_debug_opt(p)
+(opt, args) = p.parse_args()
+setup_logging(opt)
+
+# Django must be imported after setting DJANGO_SETTINGS_MODULE
+set_django_settings_module(opt)
+load_devilry_plugins()
+from devilry.core.models import Assignment, AssignmentGroup
+from django.contrib.auth.models import User
+
+def exit_help():
+    p.print_help()
+    raise SystemExit()
+if len(args) != 1:
+    exit_help()
+setup_logging(opt)
+
+
+assignment_id = int(args[0])
+students = ['stud%d' % d for d in xrange(30)]
+assignment = Assignment.objects.get(id=assignment_id)
+for username in students:
+    student = User.objects.get(username=username)
+    g = assignment.assignmentgroups.create()
+    g.
