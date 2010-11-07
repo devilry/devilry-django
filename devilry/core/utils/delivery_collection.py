840385476196fc88d7f34f16a59c656b2ed31c3c
bendikro <you@example.com>
2010-11-08 00:39:40 +0100

    Tar-streaming finished and working

--- devilry/core/utils/delivery_collection.py
+++ devilry/core/utils/delivery_collection.py
@@ -5,96 +5,26 @@ from django.http import HttpResponse
 from devilry.core.models import AssignmentGroup, Assignment
 from django.utils.formats import date_format
 from ui.defaults import DATETIME_FORMAT
-
-
-def get_assignmentgroup_name(assigmentgroup):
-     cands = assigmentgroup.get_candidates()
-     cands = cands.replace(", ", "-")
-     return cands
-
-def get_dictionary_with_name_matches(assignmentgroups):
-    matches = {}
-    for assigmentgroup in assignmentgroups:
-        name = get_assignmentgroup_name(assigmentgroup)
-        if matches.has_key(name):
-            matches[name] =  matches[name] + 1
-        else:
-            matches[name] = 1
-    return matches
-
-def create_zip_from_assignmentgroups2(request, assignment, assignmentgroups):
-    name_matches = get_dictionary_with_name_matches(assignmentgroups)
-
-    from ui.defaults import DATETIME_FORMAT
-
-    in_memory = StringIO()  
-    zip = ZipFile(in_memory, "a")
-
-    for ass_group in assignmentgroups:
-        ass_group_name = get_assignmentgroup_name(ass_group)
-        # If multiple groups with the same members exists,
-        # postfix the name with asssignmengroup ID.
-        if name_matches[ass_group_name] > 1:
-            ass_group_name = "%s+%d" % (ass_group_name, ass_group.id)
-
-        include_delivery_explanation = False
-        deliveries = ass_group.deliveries.all()
-        if len(deliveries) > 1:
-            include_delivery_explanation = True
-            multiple_deliveries_content = "Delivery-ID    File count    Total size     Delivery time  \r\n"
-            
-        for delivery in deliveries:
-            metas = delivery.filemetas.all()
-            delivery_size = 0
-            for f in metas:
-                delivery_size += f.size
-                bytes = f.read_open().read(f.size)
-                if include_delivery_explanation:
-                    zip.writestr("%s/%s/%d/%s" % (assignment.get_path(), ass_group_name,
-                                                  delivery.number, f.filename), bytes)
-                else:
-                    zip.writestr("%s/%s/%s" % (assignment.get_path(), ass_group_name,
-                                               f.filename), bytes)
-            if include_delivery_explanation:
-                multiple_deliveries_content += "  %3d            %3d          %5d        %s\r\n" % \
-                                               (delivery.number, len(metas), delivery_size,
-                                                date_format(delivery.time_of_delivery, "DATETIME_FORMAT"))
-        # Adding file explaining multiple deliveries
-        if include_delivery_explanation:
-            zip.writestr("%s/%s/%s" %
-                         (assignment.get_path(), ass_group_name,
-                          "Student has multiple deliveries.txt"),
-                         multiple_deliveries_content.encode("ascii"))
-
-    response = HttpResponse(mimetype="application/zip")  
-    response["Content-Disposition"] = "attachment; filename=%s.zip" % assignment.get_path()  
-    zip.close()         
-    in_memory.seek(0)      
-    response.write(in_memory.read())
-    return response  
-
+from tarfile import TarFile
+import copy
 
 def create_zip_from_assignmentgroups(request, assignment, assignmentgroups):
-    zip = False
+    it = iter_streamable_archive(StreamableZip(), assignment, assignmentgroups)
+    response = HttpResponse(it, mimetype="application/zip")
+    response["Content-Disposition"] = "attachment; filename=%s.zip" % assignment.get_path()  
+    return response
 
-    if zip:
-        it = iter_streamable_archive(StreamableZip(), assignment, assignmentgroups)
-        response = HttpResponse(it, mimetype="application/zip")
-        response["Content-Disposition"] = "attachment; filename=%s.zip" % assignment.get_path()  
-        return response
-    else:
-        it = iter_streamable_archive(StreamableTar(), assignment, assignmentgroups)
-        response = HttpResponse(it, mimetype="application/tar")  
-        response["Content-Disposition"] = "attachment; filename=%s.tar" % assignment.get_path()  
-        return response
-
+def create_tar_from_assignmentgroups(request, assignment, assignmentgroups):
+    it = iter_streamable_archive(StreamableTar(), assignment, assignmentgroups)
+    response = HttpResponse(it, mimetype="application/tar")  
+    response["Content-Disposition"] = "attachment; filename=%s.tar" % assignment.get_path()  
+    return response
 
 class StreamableTar():
     def __init__(self):
         self.in_memory = StringIO()
         self.tar = DevilryTarfile.open(name=None, mode='w', fileobj=self.in_memory)
-        #self.tar = tarfile.open(name=None, mode='w', fileobj=self.in_memory)
-        
+    
     def add_file(self, filename, bytes):
         tarinfo = tarfile.TarInfo(filename)
         tarinfo.size = len(bytes)
@@ -143,7 +73,7 @@ class StreamableZip():
 def inclusive_range(start, stop, step=1):
     """
     A range() clone, but this includes the right limit
-    as is if it doesn't divide on stop
+    as is if the last step doesn't divide on stop
     """
     l = []
     x = start
@@ -153,6 +83,32 @@ def inclusive_range(start, stop, step=1):
     if x > stop:
         l.append(stop)
     return l
+
+def get_assignmentgroup_name(assigmentgroup):
+    """
+    Returns a string containing the group member of the
+    assignmentgroup separated by '-'.
+    """
+    cands = assigmentgroup.get_candidates()
+    cands = cands.replace(", ", "-")
+    return cands
+
+def get_dictionary_with_name_matches(assignmentgroups):
+    """
+    Takes a list of assignmentgroups and returns
+    a dictionary containing the count of groups
+    with similar names sa values in the dictionary.
+    """
+    matches = {}
+    for assigmentgroup in assignmentgroups:
+        name = get_assignmentgroup_name(assigmentgroup)
+        if matches.has_key(name):
+            matches[name] =  matches[name] + 1
+        else:
+            matches[name] = 1
+    return matches
+
+max_archive_chunk_size = 1000000
 
 def iter_streamable_archive(archive, assignment, assignmentgroups):
     name_matches = get_dictionary_with_name_matches(assignmentgroups)
@@ -188,18 +144,16 @@ def iter_streamable_archive(archive, assignment, assignmentgroups):
                     filename = "%s/%s/%d/%s" % (assignment.get_path(), ass_group_name,
                                                 delivery.number, f.filename)
 
-                print "Adding file ", filename
-
-                #greater than 1000 bytes, write chuncks of 100
-                if f.size > 1000000 and archive.can_write_chunks():
-                    chunk_size = 100000
+                # File size i greater than max_archive_chunk_size bytes
+                # Add only chunks of data to the archive
+                if f.size > max_archive_chunk_size and archive.can_write_chunks():
+                    chunk_size = max_archive_chunk_size
                     start_index = 0
-                    print "file size is bigger than 1000 bytes:", f.size
                     archive.start_filestream(filename, f.size)
                     for i in inclusive_range(chunk_size, f.size, chunk_size):
-                        print "append   chunk: byte[%d:%d]" % (start_index, i)
                         archive.append_file_chunk(bytes[start_index:i])
                         start_index += chunk_size
+                        yield archive.get_bytes()
                     archive.close_filestream()
                 else:
                     archive.add_file(filename, bytes)
@@ -219,52 +173,15 @@ def iter_streamable_archive(archive, assignment, assignmentgroups):
     archive.close()
     yield archive.get_bytes()
 
-from tarfile import TarFile
-import copy
-
 class DevilryTarfile(TarFile):
-
     def __init__(self, name=None, mode="r", fileobj=None):
         TarFile.__init__(self, name, mode, fileobj)
         self.filestream_active = False
-        
     
-    def addfile(self, tarinfo, fileobj):
-        """Add the TarInfo object `tarinfo' to the archive. If `fileobj' is
-        given, tarinfo.size bytes are read from it and added to the archive.
-        You can create TarInfo objects using gettarinfo().
-           On Windows platforms, `fileobj' should always be opened with mode
-           'rb' to avoid irritation about the file size.
-           """
-        self._check("aw")
-
-        if self.filestream_active:
-            raise Exception("Cannot add new file in the middle of a filestream")
-        
-        tarinfo = copy.copy(tarinfo)
-
-        buf = tarinfo.tobuf(self.posix)
-        self.fileobj.write(buf)
-        self.offset += len(buf)
-
-        # If there's data to follow, append it.
-        if fileobj is not None:
-            tarfile.copyfileobj(fileobj, self.fileobj, tarinfo.size)
-            blocks, remainder = divmod(tarinfo.size, tarfile.BLOCKSIZE)
-            if remainder > 0:
-                self.fileobj.write(tarfile.NUL * (tarfile.BLOCKSIZE - remainder))
-                blocks += 1
-            self.offset += blocks * tarfile.BLOCKSIZE
-
-        self.members.append(tarinfo)
-
-
     def start_filestream(self, tarinfo):
-        """Add the TarInfo object `tarinfo' to the archive. If `fileobj' is
-        given, tarinfo.size bytes are read from it and added to the archive.
-        You can create TarInfo objects using gettarinfo().
-           On Windows platforms, `fileobj' should always be opened with mode
-           'rb' to avoid irritation about the file size.
+        """Add the TarInfo object `tarinfo' to the archive and opens a filestream.
+        Data chunks are appended to the filestream with append_file_chunk, and
+        the filestream must be closed with close_filestream.
            """
         self._check("aw")
         if self.filestream_active:
@@ -286,15 +203,15 @@ class DevilryTarfile(TarFile):
         Appends a chunk to the current active filestream started with start_filestream
         """
         self._check("aw")
-
         if not self.filestream_active:
             raise Exception("Cannot append file chunk without an active filestream."\
                             "Start a filestream first.")
-        # If there's data to follow, append it.
-        if fileobj is not None:
-            tarfile.copyfileobj(fileobj, self.fileobj, chunk_size)
-            self.offset += chunk_size
-            self.filestream_sum += chunk_size
+        if fileobj is None:
+            raise Exception("fileobj cannot be None.")
+
+        tarfile.copyfileobj(fileobj, self.fileobj, chunk_size)
+        self.offset += chunk_size
+        self.filestream_sum += chunk_size
     
     def close_filestream(self):
         """
