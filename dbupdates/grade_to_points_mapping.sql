d9ace36934463a9c740eced98e776b4957b71869
bendiko <bendikro@gmail.com>
2011-04-06 16:47:10 +0200

    Now only one sql update file

--- dbupdates/grade_to_points_mapping.sql
+++
@@ -1,1 +0,0 @@
-alter table grade_rstschema_rstschemadefinition add column "grade_to_points_mapping" text DEFAULT NULL;
--- dbupdates/student_can_see_grade.sql
+++
@@ -1,1 +0,0 @@
-alter table core_assignment add column "students_can_see_points" boolean NOT NULL DEFAULT true;
