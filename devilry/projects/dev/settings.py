1f9e55f1eb978398733bde972dcf84fb1920e94c
Espen A. Kristiansen <post@espenak.net>
2011-05-28 23:44:58 +0200

    Started rearranging for better structure

--- devilry/projects/dev/settings.py
+++ devilry/projects/dev/settings.py
@@ -10,7 +10,7 @@ TEMPLATE_DEBUG = DEBUG
 
 # If no admins are set, no emails are sent to admins
 ADMINS = (
-     ('Devilry admin', 'admin@example.com'),
+     #('Devilry admin', 'admin@example.com'),
 )
 
 MANAGERS = ADMINS
@@ -26,48 +26,16 @@ DATABASES = {
     }
 }
 
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
 TIME_ZONE = 'Europe/Oslo'
-
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en-us'
-
 SITE_ID = 1
-
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
 USE_I18N = True
-
-# Absolute path to the directory that holds media.
-# Example: "/home/media/media.lawrence.com/"
 MEDIA_ROOT = join(this_dir, "filestore")
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash if there is a path component (optional in other cases).
-# Examples: "http://media.lawrence.com", "http://example.com/media/"
 MEDIA_URL = ''
-
-# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
-# trailing slash.
-# Examples: "http://foo.com/media/", "/media/".
 ADMIN_MEDIA_PREFIX = '/media/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '+g$%**q(w78xqa_2)(_+%v8d)he-b_^@d*pqhq!#2p*a7*9e9h'
-
-## List of callables that know how to import templates from various sources.
-# For production (see  http://docs.djangoproject.com/en/dev/ref/templates/api/#template-loaders)
-#TEMPLATE_LOADERS = (
-    #('django.template.loaders.cached.Loader', (
-        #'django.template.loaders.filesystem.Loader',
-        #'django.template.loaders.app_directories.Loader',
-    #)),
-#)
 
 
 
@@ -76,51 +44,14 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.middleware.transaction.TransactionMiddleware',
-    'devilry.core.utils.profile.ProfilerMiddleware'
+    'devilry.apps.core.utils.profile.ProfilerMiddleware'
 ]
 
 
 ROOT_URLCONF = 'devilry.urls'
 
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    join(this_dir, 'templates'),
-)
-
-
 LOGIN_URL = '/ui/login'
 DEVILRY_LOGOUT_URL = '/ui/logout'
-
-
-#DEVILRY_APPS = [
-    #'devilry.core',
-    #'devilry.ui',
-    #'devilry.adminscripts',
-    #'devilry.addons.student',
-    #'devilry.addons.examiner',
-    #'devilry.addons.admin',
-    #'devilry.addons.aboutme',
-    #'devilry.addons.grade_approved',
-    #'devilry.addons.grade_default',
-    #'devilry.addons.grade_rstschema',
-    #'devilry.addons.gradestats',
-    #'devilry.addons.xmlrpc_examiner',
-    #'devilry.addons.quickdash',
-    #'devilry.xmlrpc',
-    #'devilry.xmlrpc_client'
-#]
-
-#INSTALLED_APPS = [
-    #'django.contrib.webdesign', 
-    #'django.contrib.markup', 
-    #'django.contrib.sessions',
-    #'django.contrib.sites',
-    #'django.contrib.admin',
-    #'django.contrib.auth',
-    #'django.contrib.contenttypes'
-#] + DEVILRY_APPS
 
 
 INSTALLED_APPS = (
@@ -128,28 +59,31 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.admin',
-    'devilry.addons.student',
-    'devilry.addons.examiner',
-    'devilry.addons.admin',
-    'devilry.addons.grade_approved',
-    'devilry.addons.grade_default',
-    'devilry.addons.grade_schema',
-    'devilry.addons.grade_rstschema',
-    'devilry.addons.quickdash',
-    'devilry.addons.xmlrpc_examiner',
-    'devilry.xmlrpc',
-    'devilry.xmlrpc_client',
+    'django.contrib.webdesign',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'devilry.core',
-    'devilry.ui',
-    'devilry.adminscripts',
-    'devilry.addons.gradestats',
-    'devilry.gui',
-    'devilry.addons.guiexamples',
-    'devilry.simplified',
-    'devilry.restful',
-    'django.contrib.webdesign',
+
+    'devilry.apps.core',
+    'devilry.apps.ui',
+    'devilry.apps.gui',
+    'devilry.apps.xmlrpc',
+
+    'devilry.apps.simplified',
+    'devilry.apps.restful',
+
+    'devilry.apps.student',
+    'devilry.apps.examiner',
+    'devilry.apps.xmlrpc_examiner',
+    'devilry.apps.admin',
+    'devilry.apps.grade_approved',
+    'devilry.apps.grade_default',
+    'devilry.apps.grade_schema',
+    'devilry.apps.grade_rstschema',
+    'devilry.apps.quickdash',
+    'devilry.apps.gradestats',
+
+    'devilry.apps.xmlrpc_client',
+    'devilry.apps.guiexamples',
 )
 
 
@@ -158,7 +92,6 @@ INSTALLED_APPS = (
 DEVILRY_RESOURCES_ROOT = join(this_dir, 'resources')
 DEVILRY_RESOURCES_URL = '/resources'
 DEVILRY_THEME_URL = '/resources/gui/themes/devilry'
-#DEVILRY_DOJO_URL = '/resources/dojo/dojo/dojo.js'
 DEVILRY_EXTJS_URL = '/resources/ext-4.0.1'
 TEMPLATE_CONTEXT_PROCESSORS = (
     "django.core.context_processors.auth", 
@@ -166,14 +99,14 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.core.context_processors.i18n",
     "django.core.context_processors.media",
     "django.core.context_processors.debug",
-    'devilry.core.templatecontext.template_variables',
+    'devilry.apps.core.templatecontext.template_variables',
 )
 
 
 
-DELIVERY_STORE_BACKEND = 'devilry.core.deliverystore.FsDeliveryStore'
+DELIVERY_STORE_BACKEND = 'devilry.apps.core.deliverystore.FsDeliveryStore'
 DELIVERY_STORE_ROOT = join(this_dir, 'deliverystore')
-#DELIVERY_STORE_BACKEND = 'devilry.core.deliverystore.DbmDeliveryStore'
+#DELIVERY_STORE_BACKEND = 'devilry.apps.core.deliverystore.DbmDeliveryStore'
 #DELIVERY_STORE_DBM_FILENAME = join(this_dir, 'deliverystore', 'deliverystore.dbm')
 
 # Make sure this does not end with / (i.e. '' means / is the main page).
@@ -183,9 +116,6 @@ DEVILRY_MAIN_PAGE = ''
 BASE_TEMPLATE = 'devilry/base.django.html'
 
 MEDIA_ICONS_URL = 'media/icons/'
-#JQUERY_UI_THEME = 'devilry'
-#JQUERY_UI_THEME = 'dot-luv'
-#JQUERY_UI_THEME = 'ui-darkness'
 JQUERY_UI_THEME = 'devilry-blue'
 
 SEND_EMAIL_TO_USERS = False
