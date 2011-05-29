c4477ae1bb01212735143f032866f5c1e81cf2d9
Espen A. Kristiansen <post@espenak.net>
2011-05-29 02:15:29 +0200

    Separated urls to make the app urls reusable

--- devilry/projects/dev/urls.py
+++ devilry/projects/dev/urls.py
@@ -1,43 +1,26 @@
-from django.conf.urls.defaults import *
-from django.conf import settings
-from devilry.apps.core import pluginloader
-
 from django.contrib import admin
+from django.conf.urls.defaults import patterns, include
+
+from devilry.apps.core import pluginloader
+from devilry.apps.default_urls import devilry_urls, fileserve_url
+
 admin.autodiscover()
 
 
-debugpatterns = []
-if settings.DEBUG:
-    debugpatterns = [
-        (r'^guiexamples/', include('devilry.apps.guiexamples.urls'))]
-
 urlpatterns = patterns('',
-    # Example:
-    # (r'^devilry/', include('devilry.foo.urls')),
+    # Custom urls for this project
+    (r'^guiexamples/', include('devilry.apps.guiexamples.urls')),
 
+    # Django admin interface and login page. The admin interface is only
+    # needed for user administration.
+    (r'^superadmin/', include(admin.site.urls)),
     (r'^accounts/login/$', 'django.contrib.auth.views.login'),
 
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    #(r'^superadmin/doc/', include('django.contrib.admindocs.urls')),
-    (r'^superadmin/', include(admin.site.urls)),
+    # Include the url to serve static files (../../static) at /static/
+    fileserve_url,
 
-    (r'^restful/', include('devilry.apps.restful.urls')),
-    (r'^ui/', include('devilry.apps.ui.urls')),
-    (r'^student/', include('devilry.apps.student.urls')),
-    (r'^examiner/', include('devilry.apps.examiner.urls')),
-    (r'^admin/', include('devilry.apps.admin.urls')),
-    (r'^grade_schema/', include('devilry.apps.grade_schema.urls')),
-    (r'^grade_rstschema/', include('devilry.apps.grade_rstschema.urls')),
-    (r'^gradestats/', include('devilry.apps.gradestats.urls')),
-    (r'^adminscripts/', include('devilry.apps.adminscripts.urls')),
-    (r'^xmlrpc/', include('devilry.apps.xmlrpc.urls')),
-    (r'^xmlrpc_examiner/', include('devilry.apps.xmlrpc_examiner.urls')),
-    (r'^', include('devilry.apps.quickdash.urls')),
-
-    (r'^static/(?P<path>.*)$', 'django.views.static.serve',
-        {'document_root': settings.DEVILRY_STATIC_ROOT}),
-    *debugpatterns
+    # Include the urls of all devilry apps
+    *devilry_urls
 )
 
 
