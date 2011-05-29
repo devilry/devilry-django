5a87c07c805ef90828939c67d5d0961f63ed138d
Espen A. Kristiansen <post@espenak.net>
2011-05-29 03:38:43 +0200

    Cleaned up installed apps

--- devilry/apps/default_settings.py
+++ devilry/apps/default_settings.py
@@ -12,46 +12,22 @@ ADMIN_MEDIA_PREFIX = '/media/'
 DATETIME_FORMAT = "N j, Y, H:i"
 LOGIN_URL = '/ui/login'
 
-
-
-#INSTALLED_APPS = (
-    #'django.contrib.markup', 
-    #'django.contrib.sessions',
-    #'django.contrib.sites',
-    #'django.contrib.admin',
-    #'django.contrib.webdesign',
-    #'django.contrib.auth',
-    #'django.contrib.contenttypes',
-
-    #'devilry.apps.core',
-    #'devilry.apps.ui',
-    #'devilry.apps.gui',
-    #'devilry.apps.xmlrpc',
-
-    #'devilry.apps.simplified',
-    #'devilry.apps.restful',
-
-    #'devilry.apps.student',
-    #'devilry.apps.examiner',
-    #'devilry.apps.xmlrpc_examiner',
-    #'devilry.apps.admin',
-    #'devilry.apps.grade_approved',
-    #'devilry.apps.grade_default',
-    #'devilry.apps.grade_schema',
-    #'devilry.apps.grade_rstschema',
-    #'devilry.apps.quickdash',
-    #'devilry.apps.gradestats',
-
-    #'devilry.apps.xmlrpc_client',
-    #'devilry.apps.guiexamples',
-#)
-
-
-INSTALLED_APPS = (
+INSTALLED_APPS = [
     'django.contrib.markup', 
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.admin',
+    'django.contrib.webdesign',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+
+    'devilry.apps.core',
+    'devilry.apps.ui',
+    'devilry.apps.gui',
+    'devilry.apps.simplified',
+
+    'devilry.apps.restful',
+
     'devilry.apps.student',
     'devilry.apps.examiner',
     'devilry.apps.admin',
@@ -60,25 +36,12 @@ INSTALLED_APPS = (
     'devilry.apps.grade_schema',
     'devilry.apps.grade_rstschema',
     'devilry.apps.quickdash',
-    'devilry.apps.xmlrpc_examiner',
-    'devilry.apps.xmlrpc',
-    'devilry.apps.xmlrpc_client',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'devilry.apps.core',
-    'devilry.apps.ui',
-    'devilry.apps.adminscripts',
     'devilry.apps.gradestats',
 
-    'devilry.apps.gui',
-    'devilry.apps.guiexamples',
-    'devilry.apps.simplified',
-    'devilry.apps.restful',
-    'django.contrib.webdesign',
-)
-
-
-
+    'devilry.apps.xmlrpc',
+    'devilry.apps.xmlrpc_examiner',
+    'devilry.apps.xmlrpc_client',
+]
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     "django.contrib.auth.context_processors.auth",
