797c9a20a9736ccccd8398afdff993f58a819ba8
Vegard Angell <vegarang@ifi.uio.no>
2011-06-09 21:58:55 +0200

    added more tests for simplified/administrator.py

--- devilry/apps/simplified/tests/administrator.py
+++ devilry/apps/simplified/tests/administrator.py
@@ -161,30 +161,59 @@ class TestAdministratorNode(TestCase):
 
 class TestAdministratorSubject(TestCase):
     fixtures = ["simplified/data.json"]
-    
+
     def setUp(self):
         self.grandma = User.objects.get(username='grandma') # superuser
         self.duck1100 = models.Subject.objects.get(short_name='duck1100')
         self.clarabelle = User.objects.get(username="clarabelle")
         self.univ = models.Node.objects.get(short_name='univ')
+        self.duckburgh = models.Node.objects.get(short_name='duckburgh')
         self.univ.admins.add(self.clarabelle)
-
+        self.duckburgh.admins.add(self.clarabelle)
         self.daisy = User.objects.get(username="daisy")
-        self.assertEquals(0,
-                models.Node.where_is_admin_or_superadmin(self.daisy).count())
-
-        self.invalidid = 100000
-        self.assertRaises(models.Node.DoesNotExist, models.Node.objects.get,
-                id=self.invalidid)
 
 
     def test_read_model(self):
         subject = Subject.read_model(self.clarabelle, id=self.duck1100.id)
         subject = Subject.read_model(self.grandma, self.duck1100.id) # superuser allowed
-        with self.assertRaises(PermissionDenied):
-            node = Subject.read_model(self.daisy, self.duck1100.id) # superuser allowed
         subject = Subject.read_model(self.grandma, id=self.duck1100.id)
         self.assertEquals(subject.short_name, 'duck1100')
         subject = Subject.read_model(self.grandma,
                 dict(short_name=self.duck1100.short_name))
         self.assertEquals(subject.short_name, 'duck1100')
+
+        with self.assertRaises(PermissionDenied):
+            node = Subject.read_model(self.daisy, self.duck1100.id) # superuser allowed
+
+    def test_read(self):
+        subject = Subject.read(self.grandma, self.duck1100.id)
+        self.assertEquals(subject, dict(
+                short_name = 'duck1100',
+                long_name = self.duck1100.long_name,
+                id = self.duck1100.id))
+
+    def test_search(self):
+        subjects = models.Subject.where_is_admin_or_superadmin(self.grandma).order_by("short_name")
+        qryset = Subject.search(self.grandma).qryset
+        self.assertEquals(len(qryset), len(subjects))
+        self.assertEquals(qryset[0].short_name, subjects[0].short_name)
+
+        # query
+        qryset = Subject.search(self.grandma, query="duck1").qryset
+        self.assertEquals(len(qryset), 2)
+        qryset = Subject.search(self.grandma, query="duck").qryset
+        self.assertEquals(len(qryset), len(subjects))
+        qryset = Subject.search(self.grandma, query="1100").qryset
+        self.assertEquals(len(qryset), 1)
+
+    def test_create(self):
+        kw = dict(long_name='Test',
+                parentnode_id=self.univ.id)
+        subject = Subject.create(self.clarabelle, short_name='test1', **kw)
+        self.assertEquals(subject.short_name, 'test1')
+        self.assertEquals(subject.long_name, 'Test')
+        self.assertEquals(subject.parentnode, self.univ)
+
+        subject = Node.create(self.grandma, short_name='test2', **kw) # superuser allowed
+        with self.assertRaises(PermissionDenied):
+            subject = Node.create(self.daisy, short_name='test3', **kw)
