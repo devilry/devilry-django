0f54d75ea8685a41f874f33fe64f30c16682e0e6
Espen A. Kristiansen <post@espenak.net>
2011-06-09 21:56:00 +0200

    Simplified javascript for tutorial.

--- devilry/apps/tutorialstats/templates/devilry/tutorialstats/main.js
+++ devilry/apps/tutorialstats/templates/devilry/tutorialstats/main.js
@@ -1,15 +1,39 @@
-Ext.require('devilry.tutorialstats.store.StatConfig');
-
 
 function comboBox()
 {
+    Ext.define('StatConfig', {
+        extend: 'Ext.data.Model',
+        fields: [
+            {name:'id', type:'int'},
+            {name:'name', type:'string'},
+            {name:'period__id', type:'int'},
+            {name:'user__id', type:'int'}
+        ],
+        idProperty: 'id'
+    });
+
+    var store = Ext.create('Ext.data.Store', {
+        model: 'StatConfig',
+        autoLoad: true,
+        autoSync: true,
+        proxy: {
+            type: 'rest',
+            url: '/tutorialstats/rest/',
+            reader: {
+                type: 'json',
+                root: 'items'
+            },
+            writer: 'json'
+        }
+    });
+
     var simpleCombo = Ext.create('Ext.form.field.ComboBox', {
         fieldLabel: 'Select statistics config',
         renderTo: 'simpleCombo',
         displayField: 'name',
         width: 400,
         labelWidth: 130,
-        store: Ext.create('devilry.tutorialstats.store.StatConfig'),
+        store: store,
         queryMode: 'local',
         typeAhead: true
     });
