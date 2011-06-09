fb28ce5d66b386142f834e6cc765161e996fc279
Vegard Angell <vegarang@ifi.uio.no>
2011-06-09 20:28:05 +0200

    added more tests for simplified/administrator.py

--- devilry/apps/simplified/tests/administrator.py
+++ devilry/apps/simplified/tests/administrator.py
@@ -59,6 +59,8 @@ class TestAdministratorNode(TestCase):
             node = Node.read_model(self.daisy, self.univ.id) # superuser allowed
         node = Node.read_model(self.grandma, dict(short_name=self.univ.short_name))
         self.assertEquals(node.short_name, 'univ')
+        node = Node.read_model(self.grandma, id=self.univ.id)
+        self.assertEquals(node.short_name, 'univ')
 
 
     def test_update(self):
@@ -162,10 +164,27 @@ class TestAdministratorSubject(TestCase):
     
     def setUp(self):
         self.grandma = User.objects.get(username='grandma') # superuser
-        self.univ = models.Node.objects.get(short_name='univ')
         self.duck1100 = models.Subject.objects.get(short_name='duck1100')
+        self.clarabelle = User.objects.get(username="clarabelle")
+        self.univ = models.Node.objects.get(short_name='univ')
+        self.univ.admins.add(self.clarabelle)
+
+        self.daisy = User.objects.get(username="daisy")
+        self.assertEquals(0,
+                models.Node.where_is_admin_or_superadmin(self.daisy).count())
+
+        self.invalidid = 100000
+        self.assertRaises(models.Node.DoesNotExist, models.Node.objects.get,
+                id=self.invalidid)
 
 
     def test_read_model(self):
+        subject = Subject.read_model(self.clarabelle, id=self.duck1100.id)
+        subject = Subject.read_model(self.grandma, self.duck1100.id) # superuser allowed
+        with self.assertRaises(PermissionDenied):
+            node = Subject.read_model(self.daisy, self.duck1100.id) # superuser allowed
         subject = Subject.read_model(self.grandma, id=self.duck1100.id)
+        self.assertEquals(subject.short_name, 'duck1100')
+        subject = Subject.read_model(self.grandma,
+                dict(short_name=self.duck1100.short_name))
         self.assertEquals(subject.short_name, 'duck1100')
