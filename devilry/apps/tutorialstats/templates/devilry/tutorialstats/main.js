55a65b20bffd879b04828a7534d8ffdf14ccceb6
Espen A. Kristiansen <post@espenak.net>
2011-06-10 14:14:41 +0200

    Some commentds

--- devilry/apps/tutorialstats/templates/devilry/tutorialstats/main.js
+++ devilry/apps/tutorialstats/templates/devilry/tutorialstats/main.js
@@ -1,8 +1,13 @@
 
 function comboBox()
 {
+    // Get the extjs model generated from the datamodel
     {{ RestStatConfig.extjs_model_class|safe }}
+
+    // Get the extjs store generated from the rest config
     var store = {{ RestStatConfig.get_extjs_store_object|safe }}
+
+    // Use them to create a simple combobox.
     var simpleCombo = Ext.create('Ext.form.field.ComboBox', {
         fieldLabel: 'Select statistics config',
         renderTo: 'simpleCombo',
