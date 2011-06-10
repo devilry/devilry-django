3b4452751f1e0bebe8166e8f5dce4b146a8f1168
Espen A. Kristiansen <post@espenak.net>
2011-06-10 14:34:17 +0200

    Simple bar chart

--- devilry/apps/tutorialstats/templates/devilry/tutorialstats/main.js
+++ devilry/apps/tutorialstats/templates/devilry/tutorialstats/main.js
@@ -18,6 +18,100 @@ function comboBox()
         queryMode: 'local',
         typeAhead: true
     });
+
+
+    window.generateData = function(n, floor) {
+        var data = [],
+            p = (Math.random() * 11) + 1,
+            i;
+
+        floor = (!floor && floor !== 0) ? 20 : floor;
+
+        for (i = 0; i < (n || 12); i++) {
+            data.push({
+                name: Ext.Date.monthNames[i % 12],
+                data1: Math.floor(Math.max((Math.random() * 100), floor)),
+            });
+        }
+        return data;
+    };
+
+
+    var chartstore = Ext.create('Ext.data.JsonStore', {
+        fields: ['name', 'data1'],
+        data: generateData()
+    });
+
+
+    var win = Ext.create('Ext.Window', {
+        width: 800,
+        height: 600,
+        hidden: false,
+        maximizable: true,
+        title: 'Bar Chart',
+        renderTo: Ext.getBody(),
+        layout: 'fit',
+        items: {
+            id: 'chartCmp',
+            xtype: 'chart',
+            animate: true,
+            shadow: true,
+            store: chartstore,
+            axes: [{
+                type: 'Numeric',
+                position: 'bottom',
+                fields: ['data1'],
+                label: {
+                    renderer: Ext.util.Format.numberRenderer('0,0')
+                },
+                title: 'Number of Hits',
+                grid: true,
+                minimum: 0
+            }, {
+                type: 'Category',
+                position: 'left',
+                fields: ['name'],
+                title: 'Month of the Year'
+            }],
+            background: {
+                gradient: {
+                    id: 'backgroundGradient',
+                    angle: 45,
+                    stops: {
+                        0: {
+                            color: '#ffffff'
+                        },
+                        100: {
+                            color: '#eaf1f8'
+                        }
+                    }
+                }
+            },
+            series: [{
+                type: 'bar',
+                axis: 'bottom',
+                highlight: true,
+                tips: {
+                    trackMouse: true,
+                    width: 140,
+                    height: 28,
+                    renderer: function(storeItem, item) {
+                        this.setTitle(storeItem.get('name') + ': ' + storeItem.get('data1') + ' views');
+                    }
+                },
+                label: {
+                    display: 'insideEnd',
+                    field: 'data1',
+                    renderer: Ext.util.Format.numberRenderer('0'),
+                    orientation: 'horizontal',
+                    color: '#333',
+                    'text-anchor': 'middle'
+                },
+                xField: 'name',
+                yField: ['data1']
+            }]
+        }
+    });
 }
 
 
