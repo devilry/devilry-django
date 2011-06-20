2bc2bf66c911e1f9676e3dae7274dce6ab4bcb92
Espen A. Kristiansen <post@espenak.net>
2011-06-20 12:58:22 +0200

    Moved create_dotgraph into devilryadmin.

--- devilry/devilryadmin/cmd_create_dotgraph.py
+++ devilry/devilryadmin/cmd_create_dotgraph.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
+# Create graphviz (dot) compatible graphs from django models.
 
 from optparse import OptionParser
-from sys import argv
+from common import getprogname
 
-from common import (setup_logging,
-    add_settings_option, set_django_settings_module, add_quiet_opt,
-    add_debug_opt)
 
 class CustOptParser(OptionParser):
     def format_epilog(self, formatter):
@@ -17,26 +15,24 @@ Examples
 --------
 
 Create PDF class diagram:
-   %(prog)s --cls -o cls.dot && dot -Tpng -o cls.png cls.dot
+   {prog} --cls -o cls.dot && dot -Tpng -o cls.png cls.dot
 
 Create PDF database diagram with all details:
-   %(prog)s --show-fields -o db.dot && dot -Tpdf -o db.pdf db.dot
+   {prog} --show-fields -o db.dot && dot -Tpdf -o db.pdf db.dot
 
 Create PNG class diagram with all details:
-   %(prog)s --cls --show-fields -o cls.dot && dot -Tpng -o cls.png cls.dot
+   {prog} --cls --show-fields -o cls.dot && dot -Tpng -o cls.png cls.dot
 
 Create PNG and choose maximum size:
-    %(prog)s -o db.dot --height 2000 --width 550 && dot -Tpng -o db.png db.dot
-""" % dict(prog=argv[0])
+    {prog} -o db.dot --height 2000 --width 550 && dot -Tpng -o db.png db.dot
+""".format(prog=getprogname())
 
 
 default_filter = '^(devilry\.apps\.core\.models\.|django\.contrib\.auth\.models\.User).*$'
 p = CustOptParser(
         usage = "%prog [options] <outfile>",
+        prog = getprogname(),
         epilog = help_epilog)
-add_settings_option(p)
-add_quiet_opt(p)
-add_debug_opt(p)
 p.add_option("--sphinx-format", dest="sphinx_format",
         default=False, action='store_const', const=True,
         help="Format for sphinx.")
@@ -51,7 +47,7 @@ p.add_option('-o', "--out-file", dest="outfile",
         help='Output file. Defaults to printing to standard output.')
 p.add_option("--filter", dest="filter",
         default=default_filter,
-        help='Filter the used models using a regex. Default: "%s"' % default_filter)
+        help='Filter the used models using a regex. Default: "{0}"'.format(default_filter))
 p.add_option("--width", dest="width",
         default=None, type='int',
         help='Output width in pixels. Only used when --height is supplied.')
@@ -60,10 +56,8 @@ p.add_option("--height", dest="height",
         help='Output height in pixels. Only used when --width is supplied.')
 
 (opt, args) = p.parse_args()
-setup_logging(opt)
 
 # Django must be imported after setting DJANGO_SETTINGS_MODULE
-set_django_settings_module(opt)
 from devilry.utils.graphviz.dot import Graph
 from devilry.utils.graphviz.sphinx import sphinx_format
 from devilry.utils.graphviz.djangomodels import (ModelSet,
@@ -72,7 +66,6 @@ from devilry.utils.graphviz.djangomodels import (ModelSet,
 def exit_help():
     p.print_help()
     raise SystemExit()
-setup_logging(opt)
 
 models = ModelSet(opt.filter)
 models.add_installed_apps_models()
--- devilry/devilryadmin/common.py
+++ devilry/devilryadmin/common.py
@@ -1,11 +1,17 @@
-from os import listdir, system, getcwd
+from os import listdir, getcwd
 from os.path import dirname, abspath, join, exists
+from subprocess import call
+from sys import argv
+from os import environ
 
 def getdir(filename):
     return abspath(dirname(filename))
 
 def getthisdir():
     return getdir(__file__)
+
+def getprogname():
+    return 'devilryadmin.py {0}'.format(environ['DEVILRYADMIN_COMMANDNAME'])
 
 def getreporoot():
     return abspath(dirname(dirname(getthisdir())))
@@ -15,22 +21,22 @@ def getscriptsdir():
     return join(dirname(thisdir), 'scripts')
 
 def getcommands():
-    return [fn for fn in listdir(getdir(__file__)) \
-            if fn.startswith('cmd_') and fn.endswith('.py')]
+    return [filename for filename in listdir(getdir(__file__)) \
+            if filename.startswith('cmd_') and filename.endswith('.py')]
 
 def getcommandnames():
-    return [fn[4:-3] for fn in getcommands()]
+    return [filename[4:-3] for filename in getcommands()]
 
 def checkcommands(allcommands, *cmdnames):
     for cmd in cmdnames:
         if not cmd in allcommands:
             raise SystemExit('{0} is not a valid command name.'.format(cmd))
 
-def cmdname_to_filename(cmdname):
-    return 'cmd_{0}.py'.format(cmdname)
+def cmdname_to_filename(commandname):
+    return 'cmd_{0}.py'.format(commandname)
 
-def gethelp(cmdname):
-    filename = cmdname_to_filename(cmdname)
+def gethelp(commandname):
+    filename = cmdname_to_filename(commandname)
     filepath = join(getthisdir(), filename)
     f = open(filepath)
     f.readline()
@@ -38,8 +44,11 @@ def gethelp(cmdname):
     f.close()
     return hlp
 
-def execcommand(cmdname):
-    system(join(getthisdir(), cmdname_to_filename(cmdname)))
+def execcommand(commandname):
+    commandpath = join(getthisdir(), cmdname_to_filename(commandname))
+    command = [commandpath] + list(argv[2:])
+    environ['DEVILRYADMIN_COMMANDNAME'] = commandname
+    call(command)
 
 def depends(*cmdnames):
     allcommands = getcommandnames()
