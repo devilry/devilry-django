6894107ea3b3d82e7e528f0082ed100fc76b0cf7
Espen A. Kristiansen <post@espenak.net>
2011-06-20 02:54:00 +0200

    Made todolist show relative paths

--- devilry/devilryadmin/cmd_todolist.py
+++ devilry/devilryadmin/cmd_todolist.py
@@ -4,7 +4,8 @@
 if __name__ == '__main__':
     from subprocess import Popen, PIPE
     from common import getreporoot
-    from os.path import join
+    from os import getcwd, chdir
+    from os.path import join, relpath, abspath
     import re
 
     print __file__
@@ -23,11 +24,18 @@ if __name__ == '__main__':
     p = Popen(['git', 'grep', '-l', 'TODO:'], stdout=PIPE, cwd=reporoot)
     stdout, stderr = p.communicate()
     matchingfiles = stdout.split()
-    for fn in matchingfiles:
-        if ignore(fn):
+
+    cwd = getcwd()
+    chdir(reporoot)
+    absmatchinfiles = [abspath(fn) for fn in matchingfiles]
+    chdir(cwd)
+
+    for rootrelpath, path in zip(matchingfiles, absmatchinfiles):
+        if ignore(rootrelpath):
             continue
-        print '{0}:'.format(fn)
-        for lineno, line in enumerate(open(join(reporoot, fn))):
+        print
+        print '{0}:'.format(relpath(path))
+        for lineno, line in enumerate(open(join(reporoot, path))):
             line = line.strip()
             if 'TODO:' in line:
                 print '  Line {0:>5}: {1}'.format(lineno, line)
