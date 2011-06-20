b34409270714854b34fe22765ee1926852d138be
Espen A. Kristiansen <post@espenak.net>
2011-06-20 02:23:40 +0200

    Started replacing Makefile with devilryadmin.

---
+++ devilry/devilryadmin/common.py
@@ -0,0 +1,49 @@
+from os import listdir, system, getcwd
+from os.path import dirname, abspath, join, exists
+
+def getdir(filename):
+    return abspath(dirname(filename))
+
+def getthisdir():
+    return getdir(__file__)
+
+def getscriptsdir():
+    thisdir = getthisdir()
+    return join(dirname(thisdir), 'scripts')
+
+def getcommands():
+    return [fn for fn in listdir(getdir(__file__)) \
+            if fn.startswith('cmd_') and fn.endswith('.py')]
+
+def getcommandnames():
+    return [fn[4:-3] for fn in getcommands()]
+
+def checkcommands(allcommands, *cmdnames):
+    for cmd in cmdnames:
+        if not cmd in allcommands:
+            raise SystemExit('{0} is not a valid command name.'.format(cmd))
+
+def cmdname_to_filename(cmdname):
+    return 'cmd_{0}.py'.format(cmdname)
+
+def gethelp(cmdname):
+    filename = cmdname_to_filename(cmdname)
+    filepath = join(getthisdir(), filename)
+    f = open(filepath)
+    f.readline()
+    hlp = f.readline()[1:].strip()
+    f.close()
+    return hlp
+
+def execcommand(cmdname):
+    system(join(getthisdir(), cmdname_to_filename(cmdname)))
+
+def depends(*cmdnames):
+    allcommands = getcommandnames()
+    checkcommands(allcommands, *cmdnames)
+    for cmd in cmdnames:
+        execcommand(cmd)
+
+def require_djangoproject():
+    if not exists(join(getcwd(), 'manage.py')):
+        raise SystemExit('This command requires CWD to be a django project (a directory containing manage.py).')
---
+++ devilry/devilryadmin/devilryadmin.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+
+if __name__ == '__main__':
+    from os import linesep
+    from sys import argv
+    from common import getdir, getcommandnames, execcommand, gethelp
+
+    thisdir = getdir(__file__)
+    commandnames = getcommandnames()
+
+    def exit_help():
+        commandlist = '{linesep} - '.format(linesep=linesep).join(commandnames)
+        print 'Usage: {progname} <command>'.format(progname=argv[0])
+        print
+        print 'Where command is one of:'
+        for cmdname in commandnames:
+            print ' - {0:<24} {1}'.format(cmdname, gethelp(cmdname))
+        raise SystemExit()
+
+    if len(argv) < 2:
+        exit_help()
+    cmdname = argv[1]
+
+    if not cmdname in commandnames:
+        exit_help()
+    execcommand(cmdname)
