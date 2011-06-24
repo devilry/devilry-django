e8526324d458091f79f78ca3fa79b7a3516f0779
Christian Tryti <cotryti@ifi.uio.no>
2011-06-24 14:11:28 +0200

    apps: student: simplified.py: Added Delivery class and tests

---
+++ devilry/apps/student/simplified.py
@@ -0,0 +1,46 @@
+from ...simplified import simplified_api, PermissionDenied
+from ..core import models
+
+class PublishedWhereIsCandidateMixin(object):
+
+    @classmethod
+    def create_searchqryset(cls, user, **kwargs):
+        return cls._meta.model.published_where_is_candidate(user)
+
+    @classmethod
+    def read_authorize(cls, user, obj):
+        if not cls._meta.model.published_where_is_candidate(user).filter(id=obj.id):
+            raise PermissionDenied()
+
+
+@simplified_api
+class Delivery(PublishedWhereIsCandidateMixin):
+
+    class Meta:
+
+        _subject_long     = 'assignment_group__parentnode__parentnode__parentnode__long_name'
+        _subject_short    = 'assignment_group__parentnode__parentnode__parentnode__short_name'
+        _subject_id       = 'assignment_group__parentnode__parentnode__parentnode__id'
+
+        _period_long      = 'assignment_group__parentnode__parentnode__long_name'
+        _period_short     = 'assignment_group__parentnode__parentnode__short_name'
+        _period_id        = 'assignment_group__parentnode__parentnode__id'
+
+        _assignment_long  = 'assignment_group__parentnode__long_name'
+        _assignment_short = 'assignment_group__parentnode__short_name'
+        _assignment_id    = 'assignment_group__parentnode__id'
+
+        model = models.Delivery
+        resultfields = {
+            '__BASE__'   : ['time_of_delivery', 'number', 'successful'],
+            'period'     : [_period_short, _period_long, _period_id],
+            'subject'    : [_subject_long, _subject_short, _subject_id],
+            'assignment' : [_assignment_short, _assignment_long, _assignment_id],
+            }
+        searchfields = [_subject_short,
+                        _subject_long,
+                        _period_short,
+                        _period_long,
+                        _assignment_long,
+                        _assignment_short]
+        methods = ['search', 'read', 'create', 'update', 'delete']
---
+++ devilry/apps/student/tests/simplified.py
@@ -0,0 +1,74 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+
+from ....simplified import PermissionDenied
+from ...core import models
+from ...core import pluginloader
+from ..simplified import Delivery
+
+import datetime
+
+class SimplifiedDeliveryTestCase(TestCase):
+    fixtures = ["simplified/data.json"]
+
+    # def setUp(self):
+    #      self.duck1100_core = models.Subject.objects.get(short_name='duck1100')
+    #      self.duck1080_core = models.Subject.objects.get(short_name='duck1080')
+    #      self.duck3580_core = models.Subject.objects.get(short_name='duck3580')
+    #
+    #      self.duck1100user = User.objects.create(username='duck1100student')
+    #      ag = self.duck1100_core.periods.all()[0].assignments.all()[0].assignmentgroups.all()[0]
+    #      self.duck1100candidate = models.Candidate.objects.create(student=self.duck1100user, assignment_group=ag)
+    #
+    #      self.duck1100_core.periods.all()[0].assignments.all()[0].assignmentgroups.all()[0].candidates.add(self.duck1100candidate)
+    #
+    #      self.duck1080user = User.objects.create(username='duck1080student')
+    #      self.duck1080candidate = models.Candidate.objects.create(student=self.duck1080user)
+    #      self.duck1080_core.periods.all()[0].assignments.all()[0].assignmentgroups.all()[0].candidate.add(self.duck1080candidate)
+    #
+    #      self.duck3580user = User.objects.create(username='duck3580student')
+    #      self.duck3580candidate = models.Candidate.objects.create(student=self.duck3580user)
+    #      self.duck3580_core.periods.all()[0].assignments.all()[0].assignmentgroups.all()[0].candidate.add(self.duck1080candidate)
+
+
+class TestSimplifiedDelivery(SimplifiedDeliveryTestCase):
+
+    def test_search(self):
+        
+        candidate0 = User.objects.get(username="student0")
+
+        # student0 has 11 deliveries total
+        #               4 deliveries in duck1100
+        #               4 deliveries in duck3580
+        
+        # check that:
+        #   all deliveries are returned 
+        self.assertEquals(Delivery.search(candidate0).qryset.count(), 11)
+
+        #   all deliveries in subject duck1100 are returned 
+        self.assertEquals(Delivery.search(candidate0, query="duck1100").qryset.count(), 4)
+
+        #   all deliveries in subject duck3580 are returned 
+        self.assertEquals(Delivery.search(candidate0, query="duck3580").qryset.count(), 2)
+
+        #   all deliveries from period "h01" are returned 
+        self.assertEquals(Delivery.search(candidate0, query="h01").qryset.count(), 11)
+
+        #   all deliveries from assignment "week1" are returned 
+        self.assertEquals(Delivery.search(candidate0, query="week1").qryset.count(), 5)
+
+        
+    def test_read(self):
+        
+        candidate0 = User.objects.get(username="student0")
+
+        #
+        deliveries = models.Delivery.objects.filter(assignment_group__candidates__student=candidate0).all()
+        delivery = Delivery.read(candidate0, Delivery.search(candidate0).qryset[0].id)
+        self.assertEquals(delivery['number'], 1)
+        self.assertEquals(delivery['successful'], 1)
+        # TODO: test time_of_delivery in some way
+        # self.assertEquals(delivery['time_of_delivery'], some_date)
+        
+        
+        
