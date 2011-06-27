919856d32e5951f3450d7af7e897e19143fecd6d
Christian Tryti <cotryti@ifi.uio.no>
2011-06-27 17:54:24 +0200

    apps: student: simplified: Added Feedback-class and tests for it

--- devilry/apps/student/simplified.py
+++ devilry/apps/student/simplified.py
@@ -33,7 +33,7 @@ class Delivery(PublishedWhereIsCandidateMixin):
 
         model = models.Delivery
         resultfields = {
-            '__BASE__'   : ['time_of_delivery', 'number', 'successful'],
+            '__BASE__'   : ['id', 'time_of_delivery', 'number', 'successful'],
             'period'     : [_period_short, _period_long, _period_id],
             'subject'    : [_subject_long, _subject_short, _subject_id],
             'assignment' : [_assignment_short, _assignment_long, _assignment_id],
@@ -43,5 +43,61 @@ class Delivery(PublishedWhereIsCandidateMixin):
                         _period_short,
                         _period_long,
                         _assignment_long,
-                        _assignment_short]
-        methods = ['search', 'read', 'create', 'update', 'delete']
+                        _assignment_short,
+                        'successful']
+        methods = ['search', 'read', 'delete']
+
+
+@simplified_api
+class Feedback(PublishedWhereIsCandidateMixin):
+
+    class Meta:
+
+        _subject_long     = 'delivery__assignment_group__parentnode__parentnode__parentnode__long_name'
+        _subject_short    = 'delivery__assignment_group__parentnode__parentnode__parentnode__short_name'
+        _subject_id       = 'delivery__assignment_group__parentnode__parentnode__parentnode__id'
+
+        _period_long      = 'delivery__assignment_group__parentnode__parentnode__long_name'
+        _period_short     = 'delivery__assignment_group__parentnode__parentnode__short_name'
+        _period_id        = 'delivery__assignment_group__parentnode__parentnode__id'
+
+        _assignment_long  = 'delivery__assignment_group__parentnode__long_name'
+        _assignment_short = 'delivery__assignment_group__parentnode__short_name'
+        _assignment_id    = 'delivery__assignment_group__parentnode__id'
+
+        _delivery_time    = 'delivery__time_of_delivery'
+        _delivery_number  = 'delivery__number'
+        _delivery_success = 'delivery__successful'
+
+        model = models.Feedback
+        resultfields = {
+            '__BASE__'   : ['id', 'format', 'text'],
+            'period'     : [_period_short, _period_long, _period_id],
+            'subject'    : [_subject_long, _subject_short, _subject_id],
+            'assignment' : [_assignment_short, _assignment_long, _assignment_id],
+            'delivery'   : [_delivery_time, _delivery_number, _delivery_success],
+            }
+        searchfields = [_subject_short,
+                        _subject_long,
+                        _period_short,
+                        _period_long,
+                        _assignment_long,
+                        _assignment_short,
+                        _delivery_success,
+                        _delivery_number]
+        methods = ['search', 'read']
+
+
+@simplified_api
+class Subject(PublishedWhereIsCandidateMixin):
+
+    class Meta:
+
+        _subject_long     = 'delivery__assignment_group__parentnode__parentnode__parentnode__long_name'
+        _subject_short    = 'delivery__assignment_group__parentnode__parentnode__parentnode__short_name'
+        _subject_id       = 'delivery__assignment_group__parentnode__parentnode__parentnode__id'
+
+        model = models.Subject
+        resultfields = ['id', 'short_name', 'long_name', 'periods']
+        searchfields = ['short_name', 'long_name', 'periods']
+        methods = ['search', 'read']
