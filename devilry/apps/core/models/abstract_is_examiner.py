3b6613825fe89ffdc2e723ac396d71ffdf0f1bd5
Vegard Angell <vegarang@ifi.uio.no>
2011-07-01 14:21:38 +0200

    apps.core.models.abstract_is_examiner: commented out q_published(...)

--- devilry/apps/core/models/abstract_is_examiner.py
+++ devilry/apps/core/models/abstract_is_examiner.py
@@ -2,18 +2,19 @@ class AbstractIsExaminer(object):
     """ Abstract class implemented by all classes where it is natural to
     need to check if a user is examiner. """
 
-    @classmethod
-    def q_published(cls, old=True, active=True):
-        """
-        Return a django.models.Q object which matches all items of this type
-        where :attr:`Assignment.publishing_time` is in the future.
+    #TODO delete this?
+    #@classmethod
+    #def q_published(cls, old=True, active=True):
+        #"""
+        #Return a django.models.Q object which matches all items of this type
+        #where :attr:`Assignment.publishing_time` is in the future.
 
-        :param old: Include assignments where :attr:`Period.end_time`
-            is in the past?
-        :param active: Include assignments where :attr:`Period.end_time`
-            is in the future?
-        """
-        raise NotImplementedError()
+        #:param old: Include assignments where :attr:`Period.end_time`
+            #is in the past?
+        #:param active: Include assignments where :attr:`Period.end_time`
+            #is in the future?
+        #"""
+        #raise NotImplementedError()
 
 
     @classmethod
