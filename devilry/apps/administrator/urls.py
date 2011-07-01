41ae11e3e75b2dd7074d67652d6a6c4601881829
Espen A. Kristiansen <post@espenak.net>
2011-07-01 13:12:41 +0200

    apps.administrator: Boilerplate code for main view.

--- devilry/apps/administrator/urls.py
+++ devilry/apps/administrator/urls.py
@@ -1,5 +1,8 @@
-from django.conf.urls.defaults import patterns
+from django.conf.urls.defaults import patterns, url
+from django.views.generic.simple import direct_to_template
+
 import restful
 
 urlpatterns = patterns('devilry.apps.administrator',
-                      restful.RestNode.create_rest_url())
+                      restful.RestNode.create_rest_url(),
+                      url(r'^$', direct_to_template, dict(template='administrator/main.django.html')))
