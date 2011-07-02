316133e7fd2e42c684f29f49562307d85ef8b204
Espen A. Kristiansen <post@espenak.net>
2011-07-02 21:40:00 +0200

    restful: Added result_fieldgroups and search_fieldgroups SearchForm.

--- devilry/restful/restful_api.py
+++ devilry/restful/restful_api.py
@@ -12,12 +12,13 @@ class UrlMapping(object):
 
 def _create_seachform(cls):
     class SearchForm(forms.Form):
-        #format = fields.FormatField()
         query = forms.CharField(required=False)
         limit = fields.PositiveIntegerWithFallbackField(fallbackvalue=50)
         start = fields.PositiveIntegerWithFallbackField()
         orderby = fields.CharListWithFallbackField(
                 fallbackvalue=cls._meta.simplified._meta.ordering)
+        result_fieldgroups = fields.CharListWithFallbackField()
+        search_fieldgroups = fields.CharListWithFallbackField()
     cls.SearchForm = SearchForm
 
 
