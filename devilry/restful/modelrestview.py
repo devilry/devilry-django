0d5b762ff1b9b32562175abe0da2360c75758e9a
Espen A. Kristiansen <post@espenak.net>
2011-07-03 17:11:21 +0200

    restful: Cleanup and docs.

---
+++ devilry/restful/modelrestview.py
@@ -0,0 +1,108 @@
+from django.shortcuts import get_object_or_404
+
+from errors import InvalidRequestDataError
+from restview import RestView
+from serializers import serializers, RestfulResult
+from django.http import HttpResponseBadRequest
+
+
+class ModelRestView(RestView):
+    """
+    Autogenerate RESTful interfaces for simplified classes.
+    """
+
+    @classmethod
+    def _searchform_to_kwargs(cls, getdata):
+        """
+        Converts the ``data`` to a validated :class:`SearchForm`.
+
+        Throws :class:`errors.InvalidRequestDataError` if the form does not
+        validate.
+        """
+        form = cls.SearchForm(getdata)
+        if form.is_valid():
+            return form.cleaned_data
+        else:
+            raise InvalidRequestDataError(form)
+
+    @classmethod
+    def filter_urlmap(cls, itemdct):
+        if not hasattr(cls._meta, 'urlmap'):
+            return itemdct
+        for fieldname, mapping in cls._meta.urlmap.iteritems():
+            url = mapping.restfulcls.get_rest_url(itemdct[mapping.idfield])
+            itemdct[fieldname] = url
+        return itemdct
+
+    @classmethod
+    def filter_resultitem(cls, itemdct):
+        return cls.filter_urlmap(itemdct)
+
+    def restultqry_to_list(self, qryresultwrapper):
+        return [self.__class__.filter_resultitem(itemdct) \
+                for itemdct in qryresultwrapper]
+
+    def _create_or_replace(self, instance=None):
+        data = serializers.deserialize(self.comformat, self.request.raw_post_data)
+        form = self.__class__.EditForm(data, instance=instance)
+        result = None
+        if form.is_valid():
+            form.save()
+            result = dict(success=True, id=form.instance.id)
+        else:
+            fielderrors = dict(form.errors)
+            non_field_errors = list(form.non_field_errors())
+            result = dict(success=False, fielderrors=fielderrors, non_field_errors=non_field_errors)
+        return RestfulResult(result)
+
+
+
+    def crud_search(self, request, **kwargs):
+        """ Maps to the ``search`` method of the simplified class. """
+        if 'getdata_in_qrystring' in self.request.GET:
+            data = self.request.GET
+        else:
+            try:
+                data = serializers.deserialize(self.comformat, self.request.raw_post_data)
+            except ValueError, e:
+                return RestfulResult(
+                        result=dict(error='Bad request data: {0}. Perhaps you ment to'\
+                                'send GET data as a querystring? In that case, add ' \
+                                'getdata_in_qrystring=1 to your querystring.'.format(e)),
+                        httpresponsecls=HttpResponseBadRequest)
+        try:
+            form = self.__class__._searchform_to_kwargs(data)
+        except InvalidRequestDataError, e:
+            return RestfulResult(
+                    result=dict(error="Bad request: %s" % e),
+                    httpresponsecls=HttpResponseBadRequest)
+
+        form.update(**kwargs) # add variables from url PATH
+        qryresultwrapper = self._meta.simplified.search(self.request.user, **form)
+        resultList = self.restultqry_to_list(qryresultwrapper)
+        result = dict(total=len(resultList), success=True, items=resultList)
+        return RestfulResult(result)
+
+
+    def crud_read(self, request, id):
+        """ Maps to the ``read`` method of the simplified class. """
+        data = self._meta.simplified.read(self.request.user, id)
+        if 'wrap_in_items' in self.request.GET:
+            data = dict(items=data, total=1, success=True)
+        return RestfulResult(data)
+
+
+    def crud_create(self, request):
+        """ Maps to the ``create`` method of the simplified class. """
+        return self._create_or_replace()
+
+    def crud_update(self, request, id):
+        """ Maps to the ``update`` method of the simplified class. """
+        instance = get_object_or_404(self._meta.simplified._meta.model,
+                pk=id)
+        return self._create_or_replace(instance)
+
+    def crud_delete(self, request, id):
+        """ Maps to the ``delete`` method of the simplified class. """
+        self._meta.simplified.delete(request.user, id)
+        return RestfulResult(dict(pk=id))
---
+++ devilry/restful/serializers.py
@@ -0,0 +1,61 @@
+from functools import wraps
+import json
+from django.db.models.query import ValuesQuerySet
+from django.http import HttpResponseBadRequest, HttpResponse
+
+
+# TODO: Rename to SerializableResult
+class RestfulResult(object):
+    """ Stores Python objects for serialization with :class:`SerializerRegistry`. """
+    def __init__(self, result, httpresponsecls=HttpResponse, encoding='utf-8'):
+        self.result = result
+        self.httpresponsecls = httpresponsecls
+        self.encoding = encoding
+
+
+class SerializerRegistryItem(object):
+    def __init__(self, serializer, deserializer):
+        self.serializer = serializer
+        self.deserializer = deserializer
+
+class SerializerRegistry(dict):
+    def create_response(self, result, comformat):
+        i = self[comformat]
+        return result.httpresponsecls(i.serializer(result.result),
+                content_type='%s; encoding=%s' % (comformat, result.encoding))
+
+    def deserialize(self, comformat, data):
+        i = self[comformat]
+        return i.deserializer(data)
+
+
+def json_serialize_handler(obj):
+    #print type(obj)
+    if isinstance(obj, ValuesQuerySet):
+        return list(obj)
+    if hasattr(obj, 'isoformat'):
+        return obj.isoformat()
+    else:
+        raise TypeError('Object of type %s with value of %s is not JSON serializable' % (
+            type(obj), repr(obj)))
+
+def json_serialize(s):
+    return json.dumps(s, default=json_serialize_handler, indent=2)
+
+serializers = SerializerRegistry()
+serializers['application/json'] = SerializerRegistryItem(json_serialize, json.loads)
+
+
+
+def serialize(f):
+    @wraps(f)
+    def wrapper(self, request, *args, **kwargs):
+        comformat = request.META.get('Accept', 'application/json')
+        if not comformat in serializers:
+            return HttpResponseBadRequest(
+                "Bad request: %s" % comformat,
+                format='text/plain; encoding=utf-8')
+        self.comformat = comformat
+        result = f(self, request, *args, **kwargs) # returns a RestfulResult object
+        return serializers.create_response(result, comformat)
+    return wrapper
