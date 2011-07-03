77eb8d8c777f55edd009a63592924058b7f85769
Espen A. Kristiansen <post@espenak.net>
2011-07-04 00:00:35 +0200

    apps.theme: Added version, version name and admin link to base template.

--- devilry/apps/administrator/urls.py
+++ devilry/apps/administrator/urls.py
@@ -6,7 +6,9 @@ from views import MainView
 import editorviews
 
 urlpatterns = patterns('devilry.apps.administrator',
-                      (r'^$', login_required(MainView.as_view())),
+                       url(r'^$',
+                           login_required(MainView.as_view()),
+                           name='administrator'),
                       url(r'^editors/node/(?P<id>\d+)?',
                           editorviews.NodeEditor.as_view(),
                           name='administrator-editors-node'))
