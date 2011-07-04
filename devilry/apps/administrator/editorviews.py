6e8d9730d6b236043008482dba21f3960e11a427
Espen A. Kristiansen <post@espenak.net>
2011-07-04 03:31:03 +0200

    apps.administrator: Create support in GUI.

--- devilry/apps/administrator/editorviews.py
+++ devilry/apps/administrator/editorviews.py
@@ -1,10 +1,20 @@
 from django.views.generic import View
 from django.shortcuts import render
+from django.http import HttpResponseBadRequest
 
 import restful
 
 class NodeEditor(View):
-    def get(self, request, id):
-        templatevars =  {'id': id,
+    def get(self, request, id=None):
+        if id == None:
+            mode = "browse"
+        elif id == "create":
+            mode = "create"
+        elif id.isdigit():
+            mode = "edit"
+        else:
+            return HttpResponseBadRequest()
+        templatevars =  {'record_id': id,
+                         'mode': mode,
                          'RestfulSimplifiedNode': restful.RestfulSimplifiedNode}
         return render(request, 'administrator/editors/node.django.html', templatevars)
