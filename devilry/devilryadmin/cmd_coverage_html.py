bf35675b9f4db57bd74dd0a3ec11ae0780e63678
Vegard Angell <vegarang@ifi.uio.no>
2011-07-05 18:23:51 +0200

    devilryadmin: modified cmd_coverage.py
    changed the coverage-script so its not OS-specific and so it can run
    from anywhere. It now creates, and deletes, the .coveragerc-file needed
    to specify folders coverage should test

--- devilry/devilryadmin/cmd_coverage_html.py
+++ devilry/devilryadmin/cmd_coverage_html.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 # Check test coverage and create a html-report of the results
 
-from common import depends, Command
+from common import depends, Command, require_djangoproject
 from subprocess import call
 from sys import argv
 
+require_djangoproject()
 depends(Command('coverage', *argv[1:]))
 
-command = ["coverage", "html"]
+command = ["coverage", "html", "-d", "coverage_html_report"]
 call(command)
