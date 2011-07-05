b6e27481eb50fcd74b225e2c66b5ad6aa3b1e6c3
bendiko <bendikro@gmail.com>
2011-07-05 19:35:57 +0200

    apps.core.tests: Implemented tests with the new testhelper
    Implemented tests in basenode, node, subject and period.

--- devilry/apps/core/tests/assignment.py
+++ devilry/apps/core/tests/assignment.py
@@ -5,13 +5,20 @@ from django.core.exceptions import ValidationError
 from datetime import datetime, timedelta
 
 from ..models import Period, Assignment, AssignmentGroup
+from ..testhelper import TestHelper
 
-class TestAssignment(TestCase):
-    fixtures = ['core/deprecated_users.json', 'core/core.json']
+class TestAssignment(TestCase, TestHelper):
+
+    def setUp(self):
+        self.add(nodes="uio:admin(uioadmin).ifi",
+                 subjects=["inf1100"],
+                 periods=["old:begins(-2):ends(1)", "looong"],
+                 assignments=["assignment1", "assignment2"],
+                 assignmentgroups=["g1:examiner(examiner1)", "g2:examiner(examiner2)"])
 
     def test_unique(self):
         n = Assignment(parentnode=Period.objects.get(short_name='looong'),
-                short_name='oblig1', long_name='O1',
+                short_name='assignment1', long_name='O1',
                 publishing_time=datetime.now())
         self.assertRaises(IntegrityError, n.save)
 
@@ -23,7 +30,7 @@ class TestAssignment(TestCase):
         examiner1 = User.objects.get(username='examiner1')
         q = Assignment.where_is_examiner(examiner1)
         self.assertEquals(q.count(), 1)
-        self.assertEquals(q[0].short_name, 'oblig1')
+        self.assertEquals(q[0].short_name, 'assignment1')
         ag = AssignmentGroup.objects.get(pk=4)
         ag.examiners.add(examiner1)
         ag.save()
@@ -39,7 +46,7 @@ class TestAssignment(TestCase):
 
         q = Assignment.published_where_is_examiner(examiner1)
         self.assertEquals(q.count(), 1)
-        self.assertEquals(q[0].short_name, 'oblig1')
+        self.assertEquals(q[0].short_name, 'assignment1')
 
         ag = AssignmentGroup.objects.get(pk=4)
         ag.examiners.add(examiner1)
@@ -55,17 +62,20 @@ class TestAssignment(TestCase):
     def test_active_where_is_examiner(self):
         future = datetime.now() + timedelta(10)
         examiner1 = User.objects.get(username='examiner1')
-        q = Assignment.active_where_is_examiner(examiner1)
-        self.assertEquals(q.count(), 1)
-        self.assertEquals(q[0].short_name, 'oblig1')
+        q = Assignment.active_where_is_examiner(examiner1).order_by('short_name')
+        self.assertEquals(q.count(), 2)
+        self.assertEquals(q[0].short_name, 'assignment1')
 
-        ag = AssignmentGroup.objects.get(pk=4)
-        ag.examiners.add(examiner1)
-        ag.save()
+        #Create group2 with examiner1 as examiner
+        self.add_to_path('uio.ifi;inf1010.spring10.oblig1.group2:examiner(examiner1)')
+        #ag = AssignmentGroup.objects.get(pk=4)
+        #ag.examiners.add(examiner1)
+        #ag.save()
+        #print "self.inf1010_spring10.end_time:", self.inf1010_spring10.end_time
         q = Assignment.active_where_is_examiner(examiner1)
-        self.assertEquals(q.count(), 1)
-        ag.parentnode.parentnode.end_time = future
-        ag.parentnode.parentnode.save()
+        self.assertEquals(q.count(), 3)
+        self.inf1010_spring10.end_time = future
+        self.inf1010_spring10.save()
         self.assertEquals(q.count(), 2)
 
         ag.parentnode.publishing_time = future
@@ -73,6 +83,29 @@ class TestAssignment(TestCase):
         q = Assignment.active_where_is_examiner(examiner1)
         self.assertEquals(q.count(), 1)
 
+
+#    def test_active_where_is_examiner(self):
+#        future = datetime.now() + timedelta(10)
+#        examiner1 = User.objects.get(username='examiner1')
+#        q = Assignment.active_where_is_examiner(examiner1)
+#
+#        self.assertEquals(q.count(), 1)
+#        self.assertEquals(q[0].short_name, 'assignment1')
+#
+#        ag = AssignmentGroup.objects.get(pk=4)
+#        ag.examiners.add(examiner1)
+#        ag.save()
+#        q = Assignment.active_where_is_examiner(examiner1)
+#        self.assertEquals(q.count(), 1)
+#        ag.parentnode.parentnode.end_time = future
+#        ag.parentnode.parentnode.save()
+#        self.assertEquals(q.count(), 2)
+#
+#        ag.parentnode.publishing_time = future
+#        ag.parentnode.save()
+#        q = Assignment.active_where_is_examiner(examiner1)
+#        self.assertEquals(q.count(), 1)
+#
     def test_old_where_is_examiner(self):
         past = datetime.now() - timedelta(10)
         examiner3 = User.objects.get(username='examiner3')
@@ -93,58 +126,58 @@ class TestAssignment(TestCase):
     def test_assignmentgroups_where_is_examiner(self):
         examiner1 = User.objects.get(username='examiner1')
         examiner2 = User.objects.get(username='examiner2')
-        oblig1 = Assignment.objects.get(id=1)
+        assignment1 = Assignment.objects.get(id=1)
         self.assertEquals(3,
-                oblig1.assignment_groups_where_is_examiner(examiner2)[0].id)
+                assignment1.assignment_groups_where_is_examiner(examiner2)[0].id)
         self.assertEquals(2,
-                oblig1.assignment_groups_where_is_examiner(examiner1).count())
+                assignment1.assignment_groups_where_is_examiner(examiner1).count())
 
     def test_assignmentgroups_where_is_examiner_or_admin(self):
         examiner1 = User.objects.get(username='examiner1')
         ifiadmin = User.objects.get(username='ifiadmin')
 
-        oblig1 = Assignment.objects.get(id=1)
+        assignment1 = Assignment.objects.get(id=1)
         self.assertEquals(1,
-                oblig1.assignment_groups_where_can_examine(examiner1)[0].id)
+                assignment1.assignment_groups_where_can_examine(examiner1)[0].id)
         self.assertEquals(2,
-                oblig1.assignment_groups_where_can_examine(examiner1).count())
+                assignment1.assignment_groups_where_can_examine(examiner1).count())
 
         self.assertEquals(1,
-                oblig1.assignment_groups_where_can_examine(ifiadmin)[0].id)
+                assignment1.assignment_groups_where_can_examine(ifiadmin)[0].id)
         self.assertEquals(4,
-                oblig1.assignment_groups_where_can_examine(ifiadmin).count())
+                assignment1.assignment_groups_where_can_examine(ifiadmin).count())
 
     def test_clean_publishing_time_before(self):
-        oblig1 = Assignment.objects.get(id=1)
-        oblig1.parentnode.start_time = datetime(2010, 1, 1)
-        oblig1.parentnode.end_time = datetime(2011, 1, 1)
-        oblig1.publishing_time = datetime(2010, 1, 2)
-        oblig1.clean()
-        oblig1.publishing_time = datetime(2009, 1, 1)
-        self.assertRaises(ValidationError, oblig1.clean)
+        assignment1 = Assignment.objects.get(id=1)
+        assignment1.parentnode.start_time = datetime(2010, 1, 1)
+        assignment1.parentnode.end_time = datetime(2011, 1, 1)
+        assignment1.publishing_time = datetime(2010, 1, 2)
+        assignment1.clean()
+        assignment1.publishing_time = datetime(2009, 1, 1)
+        self.assertRaises(ValidationError, assignment1.clean)
 
     def test_clean_publishing_time_after(self):
-        oblig1 = Assignment.objects.get(id=1)
-        oblig1.parentnode.start_time = datetime(2010, 1, 1)
-        oblig1.parentnode.end_time = datetime(2011, 1, 1)
-        oblig1.publishing_time = datetime(2010, 1, 2)
-        oblig1.clean()
-        oblig1.publishing_time = datetime(2012, 1, 1)
-        self.assertRaises(ValidationError, oblig1.clean)
+        assignment1 = Assignment.objects.get(id=1)
+        assignment1.parentnode.start_time = datetime(2010, 1, 1)
+        assignment1.parentnode.end_time = datetime(2011, 1, 1)
+        assignment1.publishing_time = datetime(2010, 1, 2)
+        assignment1.clean()
+        assignment1.publishing_time = datetime(2012, 1, 1)
+        self.assertRaises(ValidationError, assignment1.clean)
 
     def test_get_path(self):
-        oblig1 = Assignment.objects.get(id=1)
-        self.assertEquals(oblig1.get_path(), 'inf1100.looong.oblig1')
+        assignment1 = Assignment.objects.get(id=1)
+        self.assertEquals(assignment1.get_path(), 'inf1100.looong.assignment1')
 
     def test_get_full_path(self):
-        oblig1 = Assignment.objects.get(id=1)
-        self.assertEquals(oblig1.get_full_path(),
-                'uio.ifi.inf1100.looong.oblig1')
+        assignment1 = Assignment.objects.get(id=1)
+        self.assertEquals(assignment1.get_full_path(),
+                'uio.ifi.inf1100.looong.assignment1')
 
     def test_get_by_path(self):
         self.assertEquals(
-                Assignment.get_by_path('inf1100.looong.oblig1').short_name,
-                'oblig1')
+                Assignment.get_by_path('inf1100.looong.assignment1').short_name,
+                'assignment1')
         self.assertRaises(Assignment.DoesNotExist, Assignment.get_by_path,
                 'does.not.exist')
         self.assertRaises(ValueError, Assignment.get_by_path, 'does.not')
--- devilry/apps/core/tests/node.py
+++ devilry/apps/core/tests/node.py
@@ -7,25 +7,13 @@ from ..models import Node
 from ..testhelper import TestHelper
 
 class TestNode(TestCase, TestHelper):
-    #fixtures = ['core/deprecated_users.json', 'core/core.json']
 
     def setUp(self):
-        #self.th = TestHelper
         self.add(nodes="uio:admin(uioadmin).ifi:admin(ifiadmin)")
+        self.add(nodes="uio.phys")
         self.add(nodes="uio.deepdummy1.deepdummy2.deepdummy3")
 
-        
-        #self.uioadmin = User.objects.get(username='uioadmin')
-        #self.ifiadmin = User.objects.get(username='ifiadmin')
-
-        #self.uio = Node.objects.get(pk=1)
-        #self.ifi = Node.objects.get(pk=2)
-        #self.deepdummy1 = Node.objects.get(pk=4)
-        #self.deepdummy3 = Node.objects.get(pk=6)
-
     def test_unique(self):
-        #print "uio:", self.uio
-        #print "deepdummy1:", self.deepdummy1
         n = Node(parentnode=self.deepdummy1, short_name='ifi', long_name='Ifi')
         n.save()
         n.parentnode = self.uio
@@ -68,9 +56,8 @@ class TestNode(TestCase, TestHelper):
         self.assertEquals(
                 [n.short_name for n in self.deepdummy1.iter_childnodes()],
                 [u'deepdummy2', u'deepdummy3'])
-
         s = set([n.short_name for n in self.uio.iter_childnodes()])
-        self.assertEquals(s, set([u'deepdummy1', u'deepdummy2', u'deepdummy3', u'fys', u'ifi']))
+        self.assertEquals(s, set([u'deepdummy1', u'deepdummy2', u'deepdummy3', u'phys', u'ifi']))
 
     def test_clean_parent_is_child(self):
         """ Can not be child of it's own child. """
@@ -118,131 +105,22 @@ class TestNode(TestCase, TestHelper):
                 'does.not.exist')
 
     def test_get_nodepks_where_is_admin(self):
-        #self.add(nodes="uio.matnat.math;math101.spring01(testadmin)")
-        #self.add(nodes="uio.matnat.phys;phys101(testadmin).spring10")
-        #self.add(nodes="uio.matnat.chem;chem101(testadmin).fall12")
-        #self.add(nodes="uio.matnat.bio(testadmin);bio1.fall11")
-        #self.add(nodes="uio.matnat.bio;bio1.fall12")
-        #self.add(nodes="uio.matnat.bio;bio2.fall13")
-
         self.add(nodes="uio.matnat.math:admin(testadmin)")
         self.add(nodes="uio.matnat.phys.test1:admin(testadmin)")
         self.add(nodes="uio.matnat.chem:admin(testadmin)")
         self.add(nodes="uio.matnat.bio:admin(testadmin)")
         self.add(nodes="uio.matnat.bio.test1")
-        #self.add(nodes="uio.matnat.bio.test2")
-
-        print "test1:", self.test1
-        print "MATH", Node.get_by_path("uio.matnat.math") 
-
-        phys = Node.get_by_path("uio.matnat.phys")
-        for c in phys.iter_childnodes():
-            print "c:", c
-
-        phys = Node.get_by_path("uio.matnat.phys.test1")
-        #print "phys:", phys
-        bio = Node.get_by_path("uio.matnat.bio.test1")
-        #get_by_path_kw(cls, pathlist):
-        #print "bio:", bio
-
-        t1 = Node.objects.get(short_name="test1")
-        #print "t1____:", t1
-
-        pk_list = []
-        pk_list.append(Node.get_by_path("uio.matnat.math").id)
-        pk_list.append(Node.get_by_path("uio.matnat.phys").id)
-        pk_list.append(Node.get_by_path("uio.matnat.phys.test1").id)
-
-        pk_list.append(Node.get_by_path("uio.matnat.chem").id)
-        pk_list.append(Node.get_by_path("uio.matnat.bio").id)
-                        
-        uioadmin = User.objects.get(username='testadmin')
-        pks = Node._get_nodepks_where_isadmin(uioadmin)
-
-        print "pks:", pks
-        print "pk_list:", pk_list
+                
+        pk_verify = []
+        pk_verify.append(Node.get_by_path("uio.matnat.math").id)
+        pk_verify.append(Node.get_by_path("uio.matnat.phys.test1").id)
+        pk_verify.append(Node.get_by_path("uio.matnat.chem").id)
+        pk_verify.append(Node.get_by_path("uio.matnat.bio").id)
+        pk_verify.append(Node.get_by_path("uio.matnat.bio.test1").id)
         
-    #Pks = Node._get_nodepks_where_isadmin(uioadmin)
-        #self.assertEquals(set(pks), set([1,2,3,4,5,6]))
+        testadmin = User.objects.get(username='testadmin')
+        pks = Node._get_nodepks_where_isadmin(testadmin)
+        pks.sort()
+        pk_verify.sort()
+        self.assertEquals(set(pks), set(pk_verify))
         
-
-
-    def test_get_nodepks_where_is_admin2(self):
-        #self.add(nodes="uio.matnat.math;math101.spring01(testadmin)")
-        #self.add(nodes="uio.matnat.phys;phys101(testadmin).spring10")
-        #self.add(nodes="uio.matnat.chem;chem101(testadmin).fall12")
-        #self.add(nodes="uio.matnat.bio(testadmin);bio1.fall11")
-        #self.add(nodes="uio.matnat.bio;bio1.fall12")
-        #self.add(nodes="uio.matnat.bio;bio2.fall13")
-        
-        #uio = Node(short_name='uio', long_name="UIO", parentnode=None)
-        #uio.clean()
-        #uio.save()
-
-        try:
-            matnat = Node(short_name='matnat', long_name="MATNAT", parentnode=self.uio)
-            matnat.clean()
-            matnat.save()
-
-            math = Node(short_name='math', long_name="MATH", parentnode=matnat)
-            math.clean()
-            math.save()
-            
-            phys = Node(short_name='phys', long_name="PHYS", parentnode=matnat)
-            phys.clean()
-            phys.save()
-            
-            test1_1 = Node(short_name='test1', long_name="TEST1", parentnode=phys)
-            test1_1.clean()
-            test1_1.save()
-            print "created test1_1 with id:", test1_1.id
-            
-            bio = Node(short_name='bio', long_name="BIO", parentnode=matnat)
-            bio.clean()
-            bio.save()
-            
-            test1_2 = Node(short_name='test1', long_name="TEST1", parentnode=bio)
-            test1_2.clean()
-            test1_2.save()
-            print "created test1_2 with id:", test1_2.id
-        except Exception as e:
-            print "Exception:", e
-        
-        #self.add(nodes="uio.matnat.math:admin(testadmin)")
-        #self.add(nodes="uio.matnat.phys.test1:admin(testadmin)")
-        #self.add(nodes="uio.matnat.chem:admin(testadmin)")
-        #self.add(nodes="uio.matnat.bio:admin(testadmin)")
-        #self.add(nodes="uio.matnat.bio.test1")
-        #self.add(nodes="uio.matnat.bio.test2")
-
-        #print "test1:", self.test1
-        #print "MATH", Node.get_by_path("uio.matnat.math") 
-        #
-        #phys = Node.get_by_path("uio.matnat.phys")
-        #for c in phys.iter_childnodes():
-        #    print "c:", c
-        #
-        #phys = Node.get_by_path("uio.matnat.phys.test1")
-        #print "phys:", phys
-        #bio = Node.get_by_path("uio.matnat.bio.test1")
-        #get_by_path_kw(cls, pathlist):
-        #print "bio:", bio
-
-        #t1 = Node.objects.get(short_name="test1")
-        #print "t1____:", t1
-
-        #pk_list = []
-        #pk_list.append(Node.get_by_path("uio.matnat.math").id)
-        #pk_list.append(Node.get_by_path("uio.matnat.phys").id)
-        #pk_list.append(Node.get_by_path("uio.matnat.phys.test1").id)
-        #
-        #pk_list.append(Node.get_by_path("uio.matnat.chem").id)
-        #pk_list.append(Node.get_by_path("uio.matnat.bio").id)
-        #                
-        #uioadmin = User.objects.get(username='testadmin')
-        #pks = Node._get_nodepks_where_isadmin(uioadmin)
-        #
-        #print "pks:", pks
-        #print "pk_list:", pk_list
-        
-  
