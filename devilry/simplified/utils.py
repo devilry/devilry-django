62243227c35f1f225a42d483cd44c3b520f0aeb0
Christian Tryti <ctryti@gmail.com>
2011-07-07 14:36:40 +0200

    simplified.qryresultwrapper: Changed how 'list' results are represented
    This change is related to candidates in anonymous assignments, and
    assignmentgroups with multiple candidates.

--- devilry/simplified/utils.py
+++ devilry/simplified/utils.py
@@ -1,16 +1,22 @@
 """ General purpose utilities used by the simplified API. If any functions here
 proves useful outside this module, they should me moved to ``devilry.utils``. """
+from django.db.models.fields import related
 
 
 def _recurse_getmodelattr(instance, path):
+    pathseg = path.pop(0)
     try:
-        cur = getattr(instance, path.pop(0))
+        cur = getattr(instance, pathseg)
     except AttributeError, e:
-        return None # If the foreign relationship we are following is null, we return None
+        # NOTE: Dirty hack specifically for candidates
+        if repr(type(instance)) == "<class 'django.db.models.fields.related.RelatedManager'>":
+            return [getattr(obj, pathseg) for obj in instance.all()]
+        return None  # If the foreign relationship we are following is null, we return None
     if not path:
         return cur
     else:
         return _recurse_getmodelattr(cur, path)
+
 
 def modelinstance_to_dict(instance, fields):
     """ Convert the given django model instance into a dict where
