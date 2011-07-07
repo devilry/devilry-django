ea216948c7d6ba0662a6b92005512a58103688be
Espen A. Kristiansen <post@espenak.net>
2011-07-07 04:25:23 +0200

    apps.extjshelpers: Fixed bug which prevented correct fieldgroups.

--- devilry/apps/extjshelpers/storeintegration.py
+++ devilry/apps/extjshelpers/storeintegration.py
@@ -2,9 +2,9 @@ from modelintegration import get_extjs_modelname, restfulcls_to_extjsmodel
 
 def restfulcls_to_extjsstore(restfulcls, integrateModel=False, modelkwargs={}):
     if integrateModel:
-        model = restfulcls_to_extjsmodel(restfulcls)
+        model = restfulcls_to_extjsmodel(restfulcls, **modelkwargs)
     else:
-        model = "'{modelname}'".format(modelname=get_extjs_modelname(restfulcls, **modelkwargs))
+        model = "'{modelname}'".format(modelname=get_extjs_modelname(restfulcls))
     return """Ext.create('Ext.data.Store', {{
             model: {model},
             remoteFilter: true,
