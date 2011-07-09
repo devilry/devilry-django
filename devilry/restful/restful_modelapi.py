928ebf403512e4ba85c51f9b5ee2ad076642e2fc
Espen A. Kristiansen <post@espenak.net>
2011-07-09 17:34:41 +0200

    restful: Split EditForm and SearchForm into their own files.

--- devilry/restful/restful_modelapi.py
+++ devilry/restful/restful_modelapi.py
@@ -1,61 +1,13 @@
 from types import MethodType
 from inspect import getmodule
-from django import forms
-from django.db.models import DateTimeField
 
 from ..simplified.modelapi import _require_metaattr
 from restful_api import restful_api
-import fields
+from searchform import _create_seachform
+from editform import _create_editform
 
 
-def _create_seachform(cls):
-    class SearchForm(forms.Form):
-        query = forms.CharField(required=False)
-        limit = fields.PositiveIntegerWithFallbackField(fallbackvalue=50)
-        start = fields.PositiveIntegerWithFallbackField()
-        orderby = fields.CharListWithFallbackField(
-                fallbackvalue=cls._meta.simplified._meta.ordering)
-        result_fieldgroups = fields.CharListWithFallbackField()
-        search_fieldgroups = fields.CharListWithFallbackField()
-    cls.SearchForm = SearchForm
 
-
-def _create_editform(cls):
-    """
-    Generates an inner class named EditForm from ``Meta.resultfields``::
-
-        class EditForm(django.forms.ModelForm):
-            class Meta:
-                model = cls._meta.simplified._meta.model
-                fields = fields_deduced_from_resultfields
-    """
-
-
-    formfields = []
-    extra_classattributes = {}
-    model = cls._meta.simplified._meta.model
-    for fieldname in cls._meta.simplified._meta.editablefields:
-        formfields.append(fieldname)
-
-        field = model._meta.get_field_by_name(fieldname)[0]
-        if isinstance(field, DateTimeField):
-            input_formats = input_formats=['%Y-%m-%dT%H:%M:%S',
-                                           '%Y-%m-%d %H:%M:%S'] # TODO: More datetime input formats
-            required = not field.blank
-            extra_classattributes[fieldname] = forms.DateTimeField(input_formats=input_formats,
-                                                                   required=required,
-                                                                   label=field.verbose_name,
-                                                                   help_text=field.help_text,
-                                                                   initial=field.default)
-
-    #print cls.__name__, formfields
-    class Meta:
-        model = cls._meta.simplified._meta.model
-        fields = formfields
-
-    extra_classattributes['Meta'] = Meta
-    EditForm = type('EditForm', (forms.ModelForm,), extra_classattributes)
-    cls.EditForm = EditForm
 
 
 def _copy_supports_metaattrs_from_simplified(cls):
