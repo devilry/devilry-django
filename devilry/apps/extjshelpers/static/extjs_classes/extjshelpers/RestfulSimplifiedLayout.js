20bf2d0010c8e85c9f6ca0975f9c5d30b1386986
Espen A. Kristiansen <post@espenak.net>
2011-07-10 22:49:22 +0200

    apps.extjshelpers: Removed the overview from RestfulSimplifiedLayout.

--- devilry/apps/extjshelpers/static/extjs_classes/extjshelpers/RestfulSimplifiedLayout.js
+++ devilry/apps/extjshelpers/static/extjs_classes/extjshelpers/RestfulSimplifiedLayout.js
@@ -44,8 +44,7 @@ Ext.define('devilry.extjshelpers.RestfulSimplifiedLayout', {
                     submitEmptyText: true,
                     waitMsg: 'Saving item...',
                     success: function() {
-                        me.editform.disable();
-                        Ext.getCmp(me.getChildIdBySuffix('buttoncarddeck')).getLayout().setActiveItem(me.getChildIdBySuffix('readonlybuttons'));
+                        me.readonly();
                     }
                 });
             }
@@ -56,54 +55,43 @@ Ext.define('devilry.extjshelpers.RestfulSimplifiedLayout', {
             flex: 0,
             text: 'Edit',
             handler: function() {
-                me.editform.enable();
-                Ext.getCmp(me.getChildIdBySuffix('buttoncarddeck')).getLayout().setActiveItem(me.getChildIdBySuffix('editbuttons'));
+                me.editable();
             }
         };
 
 
-
         Ext.apply(this, {
-            layout: 'card',
-            items: [{
-                xtype: 'panel',
-                id: me.getChildIdBySuffix('overview-card'),
-                html: '<h2>Overview</h2><p><strong>Search</strong>, <em>button</em> to create, .....</p>'
-            }, {
-                xtype: 'panel',
-                id: me.getChildIdBySuffix('edit-card'),
-                items: [editformargs, {
+            items: [editformargs, {
+                xtype: 'container',
+                id: me.getChildIdBySuffix('buttoncarddeck'),
+                layout: 'card',
+                items: [{
                     xtype: 'container',
-                    id: me.getChildIdBySuffix('buttoncarddeck'),
-                    layout: 'card',
+                    id: me.getChildIdBySuffix('readonlybuttons'),
+                    height: 40,
+                    layout: {
+                        type: 'hbox',
+                        pack: 'start',
+                        align: 'stretch'
+                    },
+                    items: [
+                        deletebuttonargs,
+                        { xtype: 'tbspacer', flex: 1 }, 
+                        editbuttonargs
+                    ]
+                }, {
+                    xtype: 'container',
+                    id: me.getChildIdBySuffix('editbuttons'),
+                    height: 40,
+                    layout: {
+                        type: 'hbox',
+                        pack: 'start',
+                        align: 'stretch'
+                    },
                     items: [{
-                        xtype: 'container',
-                        id: me.getChildIdBySuffix('readonlybuttons'),
-                        height: 40,
-                        layout: {
-                            type: 'hbox',
-                            pack: 'start',
-                            align: 'stretch'
-                        },
-                        items: [
-                            deletebuttonargs,
-                            { xtype: 'tbspacer', flex: 1 }, 
-                            editbuttonargs
-                        ]
-                    }, {
-                        xtype: 'container',
-                        id: me.getChildIdBySuffix('editbuttons'),
-                        height: 40,
-                        layout: {
-                            type: 'hbox',
-                            pack: 'start',
-                            align: 'stretch'
-                        },
-                        items: [{
-                            xtype: 'tbspacer',
-                            flex: 1
-                        }, savebuttonargs ]
-                    }]
+                        xtype: 'tbspacer',
+                        flex: 1
+                    }, savebuttonargs ]
                 }]
             }]
         });
@@ -111,11 +99,6 @@ Ext.define('devilry.extjshelpers.RestfulSimplifiedLayout', {
 
         this.editform = Ext.getCmp(editformargs.id);
     },
-
-    loadOverviewMode: function() {
-        this.getLayout().setActiveItem(this.getChildIdBySuffix('overview-card'));
-    },
-
 
     getChildIdBySuffix: function(idsuffix) {
         return this.id + '-' + idsuffix;
@@ -128,8 +111,8 @@ Ext.define('devilry.extjshelpers.RestfulSimplifiedLayout', {
         Ext.ModelManager.getModel(this.model).load(record_id, {
             success: function(record) {
                 me.editform.loadRecord(record);
-                var title = Ext.String.format('{0} ({1})', record.data.long_name, record.data.short_name);
-                me.editform.setTitle(title);
+                //var title = Ext.String.format('Edit', record.data.long_name, record.data.short_name);
+                //me.editform.setTitle(title);
                 var fields = me.editform.getForm().getFields();
                 Ext.each(me.foreignkeys, function(fieldname) {
                     var field = fields.filter('name', fieldname).items[0];
@@ -141,24 +124,24 @@ Ext.define('devilry.extjshelpers.RestfulSimplifiedLayout', {
         });
     },
 
+    readonly: function() {
+        this.editform.disable();
+        Ext.getCmp(this.getChildIdBySuffix('buttoncarddeck')).getLayout().setActiveItem(this.getChildIdBySuffix('readonlybuttons'));
+    },
+    editable: function() {
+        this.editform.enable();
+        Ext.getCmp(this.getChildIdBySuffix('buttoncarddeck')).getLayout().setActiveItem(this.getChildIdBySuffix('editbuttons'));
+    },
+
     loadUpdateMode: function(record_id) {
-        var editcard = Ext.getCmp(this.getChildIdBySuffix('edit-card'));
-        this.getLayout().setActiveItem(editcard);
         this.loadRecordFromStore(record_id);
     },
-
     loadCreateMode: function() {
-        var editcard = Ext.getCmp(this.getChildIdBySuffix('edit-card'));
-        this.getLayout().setActiveItem(editcard);
-        this.editform.enable();
+        this.editable();
     },
-
-
     loadMode: function(mode, record_id) {
         if(mode == "update") {
             this.loadUpdateMode(record_id);
-        } else if(mode == "overview") {
-            this.loadOverviewMode();
         } else if(mode == "create") {
             this.loadCreateMode();
         }
