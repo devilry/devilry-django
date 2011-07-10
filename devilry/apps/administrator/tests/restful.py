6e99810dd3398797169a08141b60d0c4f0445065
Espen A. Kristiansen <post@espenak.net>
2011-07-10 14:14:49 +0200

    apps.administrator.tests: Re-enabled and fixed restful tests.

--- devilry/apps/administrator/tests/restful.py
+++ devilry/apps/administrator/tests/restful.py
@@ -1,52 +1,54 @@
-from django.contrib.auth.models import User
 from django.test import TestCase
 from django.test.client import Client
 from django.utils import simplejson as json
 
 from ..restful import RestfulSimplifiedNode
-from ...core import models
+from ...core import models, testhelper
 
 
 
 class TestAdministratorRestfulSimplifiedNodeNoFixture(TestCase):
     def test_getdata_to_kwargs(self):
         kw = RestfulSimplifiedNode._searchform_to_kwargs({})
-        self.assertEquals(kw, dict(
-                limit=50, start=0, orderby=["short_name"], query=''))
-        #print RestfulSimplifiedNode.extjs_model
+        self.assertEquals(kw, {'orderby': ['short_name'],
+                               'start': 0,
+                               'limit': 50,
+                               'result_fieldgroups': None,
+                               'query': u'',
+                               'search_fieldgroups': None})
 
 
-class TestAdministratorRestfulSimplifiedNode(TestCase):
-    fixtures = ["simplified/data.json"]
-
+class TestAdministratorRestfulSimplifiedNode(TestCase, testhelper.TestHelper):
     def setUp(self):
+        self.add(nodes='uni:admin(admin1)',
+                 subjects=['inf101', 'inf110'],
+                 periods=['firstSem', 'secondSem:admin(admin2)'],
+                 assignments=['a1', 'a2'])
         self.client = Client()
-        self.client.login(username="clarabelle", password="test")
-        clarabelle = User.objects.get(username="clarabelle")
-        self.univ = models.Node.objects.get(short_name='univ')
-        self.univ.admins.add(clarabelle)
+        self.client.login(username="admin1", password="test")
 
     def test_search(self):
         url = RestfulSimplifiedNode.get_rest_url()
         r = self.client.get(url, data={'getdata_in_qrystring': True},
-                content_type='application/json')
-        data = json.loads(r.content)['items']
+                            Accept='application/json')
+        self.assertEquals(r.status_code, 200)
+        data = json.loads(r.content)
         first = data[0]
         self.assertEquals(first, {
-            u'id': 2,
-            u'short_name': u'univ',
-            u'long_name': u'Univ',
-            u'parentnode__id': None
+            u'id': self.uni.id,
+            u'short_name': self.uni.short_name,
+            u'long_name': self.uni.long_name,
+            u'parentnode': None
             })
 
     def test_create(self):
         self.assertEquals(models.Node.objects.filter(short_name='testnode').count(), 0)
-        url = RestfulSimplifiedNode.get_rest_url(self.univ.id)
+        url = RestfulSimplifiedNode.get_rest_url(self.uni.id)
         data = dict(short_name='testnode', long_name='Test SimplifiedNode', parentnode=None)
         r = self.client.post(url, data=json.dumps(data),
                 content_type='application/json')
+        self.assertEquals(r.status_code, 200)
         response = json.loads(r.content)
-        self.assertTrue(response['success'])
         self.assertEquals(models.Node.objects.filter(short_name='testnode').count(), 1)
         fromdb = models.Node.objects.get(id=response['id'])
         self.assertEquals(fromdb.short_name, 'testnode')
@@ -54,40 +56,40 @@ class TestAdministratorRestfulSimplifiedNode(TestCase):
         self.assertEquals(fromdb.parentnode, None)
 
     def test_create_errors(self):
-        url = RestfulSimplifiedNode.get_rest_url(self.univ.id)
+        url = RestfulSimplifiedNode.get_rest_url(self.uni.id)
         data = dict(short_name='uniV', long_name='Univ', parentnode=None)
         r = self.client.post(url, data=json.dumps(data),
                 content_type='application/json')
+        self.assertEquals(r.status_code, 400)
         response = json.loads(r.content)
         self.assertEquals(response, dict(
-            success = False,
             fielderrors = {u'short_name': [u"Can only contain numbers, lowercase letters, '_' and '-'. "]},
-            non_field_errors = []))
+            errormessages = []))
 
     def test_update(self):
-        url = RestfulSimplifiedNode.get_rest_url(self.univ.id)
+        url = RestfulSimplifiedNode.get_rest_url(self.uni.id)
         data = dict(id=2, short_name='univ', long_name='Univ', parentnode=None)
         r = self.client.put(url, data=json.dumps(data),
                 content_type='application/json')
         response = json.loads(r.content)
-        self.assertEquals(response, dict(success=True, id=2))
+        self.assertEquals(response, {'id': self.uni.id,
+                                     'short_name': 'univ',
+                                     'long_name': 'Univ',
+                                     'parentnode': None})
 
     def test_update_errors(self):
-        url = RestfulSimplifiedNode.get_rest_url(self.univ.id)
+        url = RestfulSimplifiedNode.get_rest_url(self.uni.id)
         data = dict(id=2, short_name='uniV', long_name='Univ', parentnode=None)
         r = self.client.put(url, data=json.dumps(data),
                 content_type='application/json')
         response = json.loads(r.content)
         self.assertEquals(response, dict(
-            success = False,
             fielderrors = {u'short_name': [u"Can only contain numbers, lowercase letters, '_' and '-'. "]},
-            non_field_errors = []))
+            errormessages = []))
 
 
     def test_delete(self):
-        url = RestfulSimplifiedNode.get_rest_url(self.univ.id)
-
-        # TODO: delete should not be recursive
-        self.assertEquals(models.Node.objects.filter(id=self.univ.id).count(), 1)
+        url = RestfulSimplifiedNode.get_rest_url(self.uni.id)
+        self.assertEquals(models.Node.objects.filter(id=self.uni.id).count(), 1)
         r = self.client.delete(url, content_type='application/json')
-        self.assertEquals(models.Node.objects.filter(id=self.univ.id).count(), 0)
+        self.assertEquals(models.Node.objects.filter(id=self.uni.id).count(), 0)
