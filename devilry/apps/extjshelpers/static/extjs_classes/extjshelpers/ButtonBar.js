5e5b98c669f68270ac4eec96d7a5b4003410dda4
Espen A. Kristiansen <post@espenak.net>
2011-07-11 04:13:31 +0200

    apps.extjshelpers: Tooltips for ButtonBar.

--- devilry/apps/extjshelpers/static/extjs_classes/extjshelpers/ButtonBar.js
+++ devilry/apps/extjshelpers/static/extjs_classes/extjshelpers/ButtonBar.js
@@ -15,25 +15,38 @@ Ext.define('devilry.extjshelpers.ButtonBar', {
         this.callParent(arguments);
 
         Ext.each(this.buttons, function(button) {
-            me.addButton(button.text, button.url);
+            me.addButton(button.text, button.url, button.tooltip);
         });
     },
 
-    addButton: function(text, url) {
+    addButton: function(text, url, tooltip) {
         if(this.items.items.size != 0) {
             this.add({
                 xtype: 'component',
                 width: 20
             });
         }
-        this.add({
+        var button = Ext.create('Ext.button.Button', {
             xtype: 'button',
             iconCls: 'icon-add-32',
             scale: 'large',
             text: text,
             handler: function() {
                 window.location = url;
+            },
+            listeners: {
+                render: function() {
+                    console.log(tooltip);
+                    Ext.create('Ext.tip.ToolTip', {
+                        target: button.id,
+                        anchor: 'top',
+                        dismissDelay: 30000,
+                        html: Ext.String.format('<div class="tooltip-title">{0}</div><p>{1}</p>',
+                                                tooltip.title, tooltip.body)
+                    });
+                }
             }
         });
+        this.add(button);
     }
 });
