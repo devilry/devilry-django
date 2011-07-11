b7d6bc270506c6053a1ec6ebfe88da0539c090e0
Christian Tryti <ctryti@gmail.com>
2011-07-12 00:18:19 +0200

    apps.administrator.simplified: Adapted to the new MetaMixins
    MetaMixins described in 9fbac8509815ea0128ce88e47b14cc85f8842d2e

--- devilry/apps/administrator/simplified.py
+++ devilry/apps/administrator/simplified.py
@@ -3,8 +3,13 @@ from ...simplified import (SimplifiedModelApi, simplified_modelapi,
                            FilterSpecs, FilterSpec, ForeignFilterSpec, PatternFilterSpec)
 from ..core import models
 
-__all__ = ('SimplifiedNode', 'SimplifiedSubject', 'SimplifiedPeriod', 'SimplifiedAssignment')
+from ..student.simplified import (SimplifiedNodeMetaMixin, SimplifiedSubjectMetaMixin,
+                                  SimplifiedPeriodMetaMixin, SimplifiedAssignmentMetaMixin,
+                                  SimplifiedAssignmentGroupMetaMixin, SimplifiedDeadlineMetaMixin,
+                                  SimplifiedDeliveryMetaMixin, SimplifiedStaticFeedbackMetaMixin,
+                                  SimplifiedFileMetaMetaMixin)
 
+__all__ = ('SimplifiedNode', 'SimplifiedSubject', 'SimplifiedPeriod', 'SimplifiedAssignment')
 
 
 class CanSaveBase(SimplifiedModelApi):
@@ -26,208 +31,50 @@ class CanSaveBase(SimplifiedModelApi):
 @simplified_modelapi
 class SimplifiedNode(CanSaveBase):
     """ Simplified wrapper for :class:`devilry.apps.core.models.Node`. """
-    class Meta:
-        model = models.Node
-        resultfields = FieldSpec('id', 'short_name', 'long_name', 'parentnode')
-        searchfields = FieldSpec('short_name', 'long_name')
+    class Meta(SimplifiedNodeMetaMixin):
         methods = ['create', 'insecure_read_model', 'read', 'update', 'delete', 'search']
-        filters = FilterSpecs(FilterSpec('parentnode'),
-                              FilterSpec('short_name'),
-                              FilterSpec('long_name'),
-                              PatternFilterSpec('parentnode__*short_name'),
-                              PatternFilterSpec('parentnode__*long_name'),
-                              PatternFilterSpec('parentnode__*id'))
 
     @classmethod
     def create_searchqryset(cls, user):
-        qryset = models.Node.where_is_admin_or_superadmin(user)
-        return qryset
+        return models.Node.where_is_admin_or_superadmin(user)
 
 
 @simplified_modelapi
 class SimplifiedSubject(CanSaveBase):
     """ Simplified wrapper for :class:`devilry.apps.core.models.Subject`. """
-    class Meta:
-        model = models.Subject
-        resultfields = FieldSpec('id', 'short_name', 'long_name', 'parentnode')
-        searchfields = FieldSpec('short_name', 'long_name')
+    class Meta(SimplifiedSubjectMetaMixin):
         methods = ['create', 'insecure_read_model', 'read', 'update', 'delete', 'search']
-        filters = FilterSpecs(FilterSpec('parentnode'),
-                              FilterSpec('short_name'),
-                              FilterSpec('long_name'),
-                              ForeignFilterSpec('parentnode', # Node
-                                                FilterSpec('paretnode'),
-                                                FilterSpec('short_name'),
-                                                FilterSpec('long_name')))
 
 
 @simplified_modelapi
 class SimplifiedPeriod(CanSaveBase):
     """ Simplified wrapper for :class:`devilry.apps.core.models.Period`. """
-    class Meta:
-        model = models.Period
-        resultfields = FieldSpec('id', 'short_name', 'long_name', 'parentnode',
-                                 'start_time', 'end_time',
-                                 subject = ['parentnode__short_name', 'parentnode__long_name'])
-        searchfields = FieldSpec('short_name', 'long_name', 'parentnode__short_name',
-                                 'parentnode__long_name')
+    class Meta(SimplifiedPeriodMetaMixin):
         methods = ['create', 'insecure_read_model', 'read', 'update', 'delete', 'search']
-        filters = FilterSpecs(FilterSpec('parentnode'),
-                              FilterSpec('short_name'),
-                              FilterSpec('long_name'),
-                              ForeignFilterSpec('parentnode', # Subject
-                                                FilterSpec('paretnode'),
-                                                FilterSpec('short_name'),
-                                                FilterSpec('long_name')))
 
 
 @simplified_modelapi
 class SimplifiedAssignment(CanSaveBase):
     """ Simplified wrapper for :class:`devilry.apps.core.models.Assignment`. """
-    class Meta:
-        model = models.Assignment
-        resultfields = FieldSpec('id', 'short_name', 'long_name', 'parentnode', 'publishing_time',
-                                 period = ['parentnode__short_name',
-                                           'parentnode__long_name',
-                                           'parentnode__parentnode'],
-                                 subject = ['parentnode__parentnode__short_name',
-                                            'parentnode__parentnode__long_name'],
-                                 pointfields = ['anonymous', 'must_pass', 'maxpoints',
-                                                'attempts'])
-        searchfields = FieldSpec('short_name', 'long_name',
-                                 'parentnode__short_name',
-                                 'parentnode__long_name',
-                                 'parentnode__parentnode__short_name',
-                                 'parentnode__parentnode__long_name')
+    class Meta(SimplifiedAssignmentMetaMixin):
         methods = ['create', 'insecure_read_model', 'read', 'update', 'delete', 'search']
-        filters = FilterSpecs(FilterSpec('parentnode'),
-                              FilterSpec('short_name'),
-                              FilterSpec('long_name'),
-                              ForeignFilterSpec('parentnode', # Period
-                                                FilterSpec('paretnode'),
-                                                FilterSpec('short_name'),
-                                                FilterSpec('long_name')),
-                              ForeignFilterSpec('parentnode__parentnode', # Subject
-                                                FilterSpec('paretnode'),
-                                                FilterSpec('short_name'),
-                                                FilterSpec('long_name')))
 
 
 @simplified_modelapi
 class SimplifiedAssignmentGroup(CanSaveBase):
-    class Meta:
-        model = models.AssignmentGroup
-        resultfields = FieldSpec('id', 'name', 'is_open', 'status', 'parentnode',
-                                 users=['examiners__username', 'candidates__identifier'],
-                                 assignment=['parentnode__long_name',
-                                             'parentnode__short_name'],
-                                 period=['parentnode__parentnode',
-                                         'parentnode__parentnode__long_name',
-                                         'parentnode__parentnode__short_name'],
-                                 subject=['parentnode__parentnode__parentnode',
-                                          'parentnode__parentnode__parentnode__long_name',
-                                          'parentnode__parentnode__parentnode__short_name']
-                                 )
-        searchfields = FieldSpec('name',
-                                 'examiners__username',
-                                 'candidates__identifier',
-                                 # assignment
-                                 'parentnode__long_name',
-                                 'parentnode__short_name',
-                                 # period
-                                 'parentnode__parentnode__long_name',
-                                 'parentnode__parentnode__short_name',
-                                 # subject
-                                 'parentnode__parentnode__parentnode__long_name',
-                                 'parentnode__parentnode__parentnode__short_name',
-                                 )
+    class Meta(SimplifiedAssignmentGroupMetaMixin):
         methods = ['create', 'insecure_read_model', 'read', 'update', 'delete', 'search']
-        filters = FilterSpecs(FilterSpec('id'),
-                              FilterSpec('parentnode'),
-                              FilterSpec('short_name'),
-                              FilterSpec('long_name'),
-                              ForeignFilterSpec('parentnode', # Assignment
-                                                FilterSpec('paretnode'),
-                                                FilterSpec('short_name'),
-                                                FilterSpec('long_name')),
-                              ForeignFilterSpec('parentnode__parentnode', # Period
-                                                FilterSpec('paretnode'),
-                                                FilterSpec('short_name'),
-                                                FilterSpec('long_name')),
-                              ForeignFilterSpec('parentnode__parentnode__parentnode', # Subject
-                                                FilterSpec('parentnode'),
-                                                FilterSpec('short_name'),
-                                                FilterSpec('long_name')))
 
 
 @simplified_modelapi
 class SimplifiedDelivery(CanSaveBase):
-    class Meta:
-        model = models.Delivery
-        resultfields = FieldSpec('id', 'number', 'time_of_delivery', 'assignment_group',
-                                 assignment_group=['assignment_group', 'assignment_group__name'],
-                                 assignment=['assignment_group__parentnode',
-                                             'assignment_group__parentnode__long_name',
-                                             'assignment_group__parentnode__short_name'],
-                                 period=['assignment_group__parentnode__parentnode',
-                                         'assignment_group__parentnode__parentnode__long_name',
-                                         'assignment_group__parentnode__parentnode__short_name'],
-                                 subject=['assignment_group__parentnode__parentnode__parentnode',
-                                          'assignment_group__parentnode__parentnode__parentnode__long_name',
-                                          'assignment_group__parentnode__parentnode__parentnode__short_name'])
-        searchfields = FieldSpec('number',
-                                 # assignmentgroup
-                                 'assignment_group__name',
-                                 'assignment_group__examiners__username',
-                                 'assignment_group__candidates__identifier',
-                                 'assignment_group__examiners__username',
-                                 'assignment_group__candidates__identifier',
-                                 # assignment
-                                 'assignment_group__parentnode__long_name',
-                                 'assignment_group__parentnode__short_name',
-                                 # period
-                                 'assignment_group__parentnode__parentnode__long_name',
-                                 'assignment_group__parentnode__parentnode__short_name',
-                                 # subject
-                                 'assignment_group__parentnode__parentnode__parentnode__long_name',
-                                 'assignment_group__parentnode__parentnode__parentnode__short_name')
+    class Meta(SimplifiedDeliveryMetaMixin):
         methods = ['search', 'read']
 
 
 @simplified_modelapi
 class SimplifiedStaticFeedback(SimplifiedModelApi):
-    class Meta:
-        _subject_long     = 'delivery__assignment_group__parentnode__parentnode__parentnode__long_name'
-        _subject_short    = 'delivery__assignment_group__parentnode__parentnode__parentnode__short_name'
-        _subject_id       = 'delivery__assignment_group__parentnode__parentnode__parentnode__id'
-
-        _period_long      = 'delivery__assignment_group__parentnode__parentnode__long_name'
-        _period_short     = 'delivery__assignment_group__parentnode__parentnode__short_name'
-        _period_id        = 'delivery__assignment_group__parentnode__parentnode__id'
-
-        _assignment_long  = 'delivery__assignment_group__parentnode__long_name'
-        _assignment_short = 'delivery__assignment_group__parentnode__short_name'
-        _assignment_id    = 'delivery__assignment_group__parentnode__id'
-
-        _delivery_time    = 'delivery__time_of_delivery'
-        _delivery_number  = 'delivery__number'
-        _delivery_delivered_by = 'delivery__delivered_by'
-        _delivery_after_deadline = 'delivery__after_deadline'
-
-        model = models.StaticFeedback
-        resultfields = FieldSpec('id', 'grade', 'points', 'is_passing_grade',
-                                 period=[_period_short, _period_long, _period_id],
-                                 subject=[_subject_long, _subject_short, _subject_id],
-                                 assignment=[_assignment_short, _assignment_long, _assignment_id],
-                                 delivery=[_delivery_time, _delivery_number, ])
-        searchfields = FieldSpec(_subject_short,
-                                 _subject_long,
-                                 _period_short,
-                                 _period_long,
-                                 _assignment_long,
-                                 _assignment_short,
-                                 _delivery_number,
-                                 )
+    class Meta(SimplifiedStaticFeedbackMetaMixin):
         methods = ['search', 'read']
 
     @classmethod
@@ -244,37 +91,7 @@ class SimplifiedStaticFeedback(SimplifiedModelApi):
 
 @simplified_modelapi
 class SimplifiedDeadline(SimplifiedModelApi):
-    class Meta:
-
-        subject = ['assignment_group__parentnode__parentnode__parentnode',
-                   'assignment_group__parentnode__parentnode__parentnode__long_name',
-                   'assignment_group__parentnode__parentnode__parentnode__short_name']
-        period = ['assignment_group__parentnode__parentnode',
-                  'assignment_group__parentnode__parentnode__long_name',
-                  'assignment_group__parentnode__parentnode__short_name']
-        assignment = ['assignment_group__parentnode',
-                      'assignment_group__parentnode__long_name',
-                      'assignment_group__parentnode__short_name']
-        assignmentgroup = ['assignment_group',
-                           'assignment_group__candidates__identifier',
-                           'assignment_group__examiners__username',
-                           'assignment_group__status']
-
-        model = models.Deadline
-        resultfields = FieldSpec('id', 'status', 'deadline', 'text',
-                                 'deliveries_available_before_deadline',
-                                 'feedbacks_published', 'is_head',
-                                 assignmentgroup=assignmentgroup,
-                                 assignment=assignment,
-                                 period=period,
-                                 subject=subject
-                                 )
-        searchfields = FieldSpec('status', 'deadline',
-                                 subject[1], subject[2],
-                                 period[1], period[2],
-                                 assignment[1], assignment[2],
-                                 assignmentgroup[1], assignmentgroup[2], assignmentgroup[3]
-                                 )
+    class Meta(SimplifiedDeadlineMetaMixin):
         methods = ['search', 'read', 'create', 'delete']
 
     @classmethod
@@ -296,31 +113,7 @@ class SimplifiedDeadline(SimplifiedModelApi):
 
 @simplified_modelapi
 class SimplifiedFileMeta(SimplifiedModelApi):
-    class Meta:
-        model = models.FileMeta
-        resultfields = FieldSpec('filename', 'size', 'id',
-                                 subject=['delivery__assignment_group__parentnode__parentnode__parentnode__long_name',
-                                          'delivery__assignment_group__parentnode__parentnode__parentnode__short_name',
-                                          'delivery__assignment_group__parentnode__parentnode__parentnode__id'],
-                                 period=['delivery__assignment_group__parentnode__parentnode__long_name',
-                                         'delivery__assignment_group__parentnode__parentnode__short_name',
-                                         'delivery__assignment_group__parentnode__parentnode__id'],
-                                 assignment=['delivery__assignment_group__parentnode__long_name',
-                                             'delivery__assignment_group__parentnode__short_name',
-                                             'delivery__assignment_group__parentnode__id']
-                                 )
-        searchfields = FieldSpec(
-            # delivery__delivered_by
-            'delivery__assignment_group__parentnode__parentnode__parentnode__long_name',  # subject
-            'delivery__assignment_group__parentnode__parentnode__parentnode__short_name',  # subject
-            'delivery__assignment_group__parentnode__parentnode__long_name',  # period
-            'delivery__assignment_group__parentnode__parentnode__short_name',  # period
-            'delivery__assignment_group__parentnode__long_name',  # assignment
-            'delivery__assignment_group__parentnode__short_name',  # assignment
-            'delivery__assignment_group__candidates__identifier',  # assignment
-            'delivery__assignment_group__examiners__username',  # assignmen
-            )
-
+    class Meta(SimplifiedFileMetaMetaMixin):
         methods = ['search', 'read']
 
     @classmethod
