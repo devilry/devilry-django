2bdeaaae162a574d28394effe4ca34053f2ac5a9
Christian Tryti <ctryti@gmail.com>
2011-07-12 17:54:40 +0200

    apps.examiner.simplified: Examiners need more resultfields than the base mixin gives, so it now adds the fields it needs (points)

--- devilry/apps/examiner/simplified.py
+++ devilry/apps/examiner/simplified.py
@@ -1,4 +1,4 @@
-from ...simplified import SimplifiedModelApi, simplified_modelapi, PermissionDenied
+from ...simplified import SimplifiedModelApi, simplified_modelapi, PermissionDenied, FieldSpec
 from ..core import models
 from ..student.simplifiedmetabases import (SimplifiedSubjectMetaMixin, SimplifiedFileMetaMetaMixin,
                                            SimplifiedPeriodMetaMixin, SimplifiedAssignmentMetaMixin,
@@ -64,6 +64,9 @@ class SimplifiedDelivery(PublishedWhereIsExaminerMixin):
 class SimplifiedStaticFeedback(PublishedWhereIsExaminerMixin):
     class Meta(SimplifiedStaticFeedbackMetaMixin):
         methods = ['search', 'read', 'create']
+        # Examiners need a few more fields than is given by
+        # default in SimplifiedStaticFeedbackMetaMixin. Addition them in!
+        resultfields = SimplifiedStaticFeedbackMetaMixin.resultfields + FieldSpec('points')
 
     @classmethod
     def write_authorize(cls, user, obj):
