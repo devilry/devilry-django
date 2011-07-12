86e01c19776b25b19d5404c4b98ccfb00d863a48
Espen A. Kristiansen <post@espenak.net>
2011-07-12 16:18:44 +0200

    restful: unused import

--- devilry/restful/restview.py
+++ devilry/restful/restview.py
@@ -3,7 +3,6 @@ from django.core.urlresolvers import reverse
 from django.views.generic import View
 from django.http import HttpResponseBadRequest, HttpResponseForbidden
 from django.conf.urls.defaults import url
-from django.contrib.auth.decorators import login_required
 
 from serializers import serialize, SerializableResult
 
