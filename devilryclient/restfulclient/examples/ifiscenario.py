a4b08b8a5b47b48adcde53a44f29d791b0668105
Espen A. Kristiansen <post@espenak.net>
2011-07-14 10:46:51 +0200

    devilryclient.restfulclient: Fixed missing / in examples.

--- devilryclient/restfulclient/examples/ifiscenario.py
+++ devilryclient/restfulclient/examples/ifiscenario.py
@@ -21,11 +21,11 @@ logincookie = login(login_url, username='grandma', password='test')
 # Create proxy classes which makes it natural to program against the
 # RESTful interface using python.
 restful_factory = RestfulFactory(devilry_url)
-SimplifiedNode = restful_factory.make("administrator/restfulsimplifiednode/")
-SimplifiedSubject = restful_factory.make("administrator/restfulsimplifiedsubject/")
-SimplifiedPeriod = restful_factory.make("administrator/restfulsimplifiedperiod/")
-SimplifiedAssignment = restful_factory.make("administrator/restfulsimplifiedassignment/")
-SimplifiedAssignmentGroup = restful_factory.make("administrator/restfulsimplifiedassignmentgroup/")
+SimplifiedNode = restful_factory.make("/administrator/restfulsimplifiednode/")
+SimplifiedSubject = restful_factory.make("/administrator/restfulsimplifiedsubject/")
+SimplifiedPeriod = restful_factory.make("/administrator/restfulsimplifiedperiod/")
+SimplifiedAssignment = restful_factory.make("/administrator/restfulsimplifiedassignment/")
+SimplifiedAssignmentGroup = restful_factory.make("/administrator/restfulsimplifiedassignmentgroup/")
 
 uio = {}
 matnat = {}
