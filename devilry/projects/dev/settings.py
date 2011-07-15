8e899c099dbc82e6513be8e1273a5a112354e42f
Espen A. Kristiansen <post@espenak.net>
2011-07-15 19:37:32 +0200

    projects.dev: Added postgresql config.

--- devilry/projects/dev/settings.py
+++ devilry/projects/dev/settings.py
@@ -4,14 +4,22 @@ from devilry.defaults.settings import *
 this_dir = dirname(abspath(__file__))
 
 DATABASES = {
-    "default": {
-        'ENGINE': 'django.db.backends.sqlite3',  # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': join(this_dir, 'db.sqlite3'),    # Or path to database file if using sqlite3.
-        'USER': '',             # Not used with sqlite3.
-        'PASSWORD': '',         # Not used with sqlite3.
-        'HOST': '',             # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',             # Set to empty string for default. Not used with sqlite3.
-    }
+             "default": {
+                         'ENGINE': 'django.db.backends.sqlite3',  # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+                         'NAME': join(this_dir, 'db.sqlite3'),    # Or path to database file if using sqlite3.
+                         'USER': '',             # Not used with sqlite3.
+                         'PASSWORD': '',         # Not used with sqlite3.
+                         'HOST': '',             # Set to empty string for localhost. Not used with sqlite3.
+                         'PORT': '',             # Set to empty string for default. Not used with sqlite3.
+                        },
+             "postgres": {
+                          'ENGINE': 'postgresql_psycopg2',  # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+                          'NAME': 'devilry',
+                          'USER': 'devilrydev',
+                          'PASSWORD': 'secret',
+                          'HOST': '',             # Set to empty string for localhost. Not used with sqlite3.
+                          'PORT': '',             # Set to empty string for default. Not used with sqlite3.
+                         }
 }
 
 INSTALLED_APPS = INSTALLED_APPS + [
