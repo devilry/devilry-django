f01b6ac34042d8de6e41531654e266e5774751ee
Espen A. Kristiansen <post@espenak.net>
2011-07-16 12:17:34 +0200

    projects.dev.urls: Redirect to /administrator/ by default.

--- devilry/projects/dev/urls.py
+++ devilry/projects/dev/urls.py
@@ -1,13 +1,24 @@
 from django.conf.urls.defaults import patterns, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.core.urlresolvers import reverse
+from django.shortcuts import redirect
+from django.views.generic import View
 
 from devilry.apps.core import pluginloader
 from devilry.defaults.urls import devilry_urls
 
 
+class RedirectToFrontpage(View):
+    def get(self, request):
+        return redirect(reverse('administrator'))
+
+
 urlpatterns = patterns('',
                        # Custom urls for this project
                        (r'^tutorialstats/', include('devilry.projects.dev.apps.tutorialstats.urls')),
+                       (r'^$', RedirectToFrontpage.as_view()),
+
+                       # Add the default Devilry urls
                        *devilry_urls
 ) + staticfiles_urlpatterns()
 
