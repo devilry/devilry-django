c94824c290ac2aae11f8a72aad08313e947e7bcd
Espen A. Kristiansen <post@espenak.net>
2011-07-18 00:13:38 +0200

    apps.examiner: Feedback editor with file display, however no feedback editing yet.

--- devilry/apps/examiner/feedbackeditorviews.py
+++ devilry/apps/examiner/feedbackeditorviews.py
@@ -2,11 +2,13 @@ from django.views.generic import View
 from django.shortcuts import render
 
 
-from restful import RestfulSimplifiedDelivery
+from restful import RestfulSimplifiedDelivery, RestfulSimplifiedFileMeta
 
 
 class FeedbackEditorView(View):
     def get(self, request, deliveryid):
         return render(request,
                       'examiner/feedbackeditor.django.html',
-                      {'RestfulSimplifiedDelivery': RestfulSimplifiedDelivery})
+                      {'RestfulSimplifiedDelivery': RestfulSimplifiedDelivery,
+                       'RestfulSimplifiedFileMeta': RestfulSimplifiedFileMeta,
+                       'deliveryid': deliveryid})
