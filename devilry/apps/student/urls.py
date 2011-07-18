832b2f0760537ee3efe9134da77b710f4fc93abf
Tor Ivar Johansen <toij@ifi.uio.no>
2011-07-19 00:32:13 +0200

    devilry/apps/student Added delivery view

--- devilry/apps/student/urls.py
+++ devilry/apps/student/urls.py
@@ -2,10 +2,13 @@ from django.conf.urls.defaults import patterns, url
 from django.contrib.auth.decorators import login_required
 
 from restful import student_restful
-from views import MainView
+from views import (MainView, AddDeliveryView)
 
 urlpatterns = patterns('devilry.apps.student',
                        url(r'^$', login_required(MainView.as_view()), name='student'),
-                           )
+                       url(r'^add-delivery/(?P<deliveryid>\d+)$', 
+                           login_required(AddDeliveryView.as_view()), 
+                           name='add-delivery'),
+                       )
 
 urlpatterns += student_restful
