42c12681db7701bdb26d9528bd9abc378ae1908f
Espen A. Kristiansen <post@espenak.net>
2011-07-18 20:05:12 +0200

    apps.extjshelpers: Added buttons to SearchResultItem.

--- devilry/apps/extjshelpers/static/extjs_classes/extjshelpers/searchwidget/SearchResultItem.js
+++ devilry/apps/extjshelpers/static/extjs_classes/extjshelpers/searchwidget/SearchResultItem.js
@@ -6,6 +6,7 @@ Ext.define('devilry.extjshelpers.searchwidget.SearchResultItem', {
     extend: 'Ext.container.Container',
     alias: 'widget.searchresultitem',
     cls: 'searchresultitem',
+    overCls: 'searchresultitem-hover',
     config: {
         /**
          * @cfg
@@ -13,14 +14,50 @@ Ext.define('devilry.extjshelpers.searchwidget.SearchResultItem', {
          */
         tpl: undefined
     },
+    layout: {
+        type: 'hbox',
+        align: 'top'
+    },
+
+    frame: false,
 
     initComponent: function() {
+
         var template = Ext.create('Ext.XTemplate', this.tpl);
         Ext.apply(this, {
-            frame: false,
             items: [{
                 xtype: 'component',
+                flex: 4,
                 html: template.apply(this.record.data)
+            }, {
+                xtype: 'container',
+                layout: {
+                    type:'vbox',
+                    padding:'5',
+                    align:'stretch'
+                },
+                height: 70,
+                width: 140,
+                frame: false,
+                //style: {"background-color": "red"},
+                flex: 0,
+                padding: {
+                    left: 20 // Avoid text pressing against buttons
+                },
+                items: [{
+                    xtype: 'button',
+                    text: 'Default stuff'
+                }, {
+                    xtype: 'button',
+                    text: 'More actions',
+                    margin: {top: 5},
+                    menu: [
+                        {text: 'Item 1'},
+                        {text: 'Item 2'},
+                        {text: 'Item 3'},
+                        {text: 'Item 4'}
+                    ]
+                }]
             }]
         });
         this.callParent(arguments);
