5c1f40b24c0c40d9b2c626f9514153daae8fd930
Espen A. Kristiansen <post@espenak.net>
2011-07-20 01:52:15 +0200

    apps.core.models.filemeta: Better docs for deliverystore.

--- devilry/apps/core/models/filemeta.py
+++ devilry/apps/core/models/filemeta.py
@@ -16,6 +16,24 @@ class FileMeta(models.Model, AbstractIsAdmin, AbstractIsExaminer, AbstractIsCand
     """
     Represents the metadata for a file belonging to a `Delivery`_.
 
+    A file meta is just information about a single file, which is stored in a
+    :attr:`deliverystore`. Use the :attr:`deliverystore` to manage the file
+    stored in its physical location. Example::
+
+        filemeta = FileMeta.objects.get(pk=0)
+        if filemeta.deliverystore.exists(filemeta):
+            filemeta.deliverystore.remove(filemeta)
+
+        # Write or read just as with the builtin open()
+        fobj = filemeta.deliverystore.write_open(filemeta)
+        fobj.write('Hello')
+        fobj.write('World')
+        fobj.close()
+        fobj = filemeta.deliverystore.read_open(filemeta)
+        print fobj.read()
+
+    See :ref:`DeliveryStore <devilry.apps.core.deliverystore>` for more details on deliverystores.
+
     .. attribute:: delivery
 
     A django.db.models.ForeignKey_ that points to the `Delivery`_ of
@@ -32,6 +50,7 @@ class FileMeta(models.Model, AbstractIsAdmin, AbstractIsExaminer, AbstractIsCand
     .. attribute:: deliverystore
 
         The current :ref:`DeliveryStore <devilry.apps.core.deliverystore>`.
+        *Class variable*.
     """
     delivery = models.ForeignKey("Delivery", related_name='filemetas')
     filename = models.CharField(max_length=255, help_text=_('Name of the file.'))
@@ -46,13 +65,13 @@ class FileMeta(models.Model, AbstractIsAdmin, AbstractIsExaminer, AbstractIsCand
         unique_together = ('delivery', 'filename')
         ordering = ['filename']
 
-    def remove_file(self):
-        """
-        Remove the file using the
-        :meth:`~devilry.core.deliverystore.DeliveryStoreInterface.remove`-method
-        of the :attr:`deliverystore`.
-        """
-        return self.deliverystore.remove(self)
+    #def remove_file(self):
+        #"""
+        #Remove the file using the
+        #:meth:`~devilry.core.deliverystore.DeliveryStoreInterface.remove`-method
+        #of the :attr:`deliverystore`.
+        #"""
+        #return self.deliverystore.remove(self)
 
     #TODO delete this?
     #def file_exists(self):
@@ -104,7 +123,7 @@ class FileMeta(models.Model, AbstractIsAdmin, AbstractIsExaminer, AbstractIsCand
 def filemeta_deleted_handler(sender, **kwargs):
     filemeta = kwargs['instance']
     try:
-        filemeta.remove_file()
+        filemeta.deliverystore.remove_file(filemeta)
     except FileNotFoundError, e:
         # TODO: We should have some way of cleaning files which have no
         # corresponding FileMeta from DeliveryStores (could happen if the
