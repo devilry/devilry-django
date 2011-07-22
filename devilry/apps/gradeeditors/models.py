e5167e11e511f5708a48dc7c1068483eb4b6fcf0
Espen A. Kristiansen <post@espenak.net>
2011-07-23 01:05:34 +0200

    apps.gradeeditors: Data model.

--- devilry/apps/gradeeditors/models.py
+++ devilry/apps/gradeeditors/models.py
@@ -1,1 +1,25 @@
-# Only here to make Django see this as an app.
+from django.db import models
+from django.contrib.auth.models import User
+
+from devilry.apps.core.models import Delivery, Assignment
+
+
+class Config(models.Model):
+    """
+    Stored by admins.
+    """
+    assignment = models.OneToOne(Assignment)
+    config = models.TextField()
+
+
+class FeedbackDraft(models.Model):
+    """
+    Stored by examiners.
+    """
+    delivery = models.ForeignKey(Delivery)
+    saved_by = models.ForeignKey(User)
+    shared = models.BooleanField()
+    draft = models.TextField()
+
+    class Meta:
+        unique_together = ('delivery', 'saved_by')
