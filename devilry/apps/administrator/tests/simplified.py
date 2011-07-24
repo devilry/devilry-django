67b44ccd81d0ebc8ff2963d826e9a065df5da67e
Vegard Angell <vegarang@ifi.uio.no>
2011-07-24 17:09:48 +0200

    apps.administrator.simplified: added tests for Period
    Added tests for everything except filters in SimplifiedPeriod for admin

--- devilry/apps/administrator/tests/simplified.py
+++ devilry/apps/administrator/tests/simplified.py
@@ -327,28 +327,140 @@ class TestSimplifiedSubject(SimplifiedAdminTestBase):
             SimplifiedSubject.delete(self.exam1, self.inf101.id)
 
 class TestSimplifiedPeriod(SimplifiedAdminTestBase):
+    allExtras = SimplifiedSubject._meta.resultfields.additional_aslist()
     def setUp(self):
         super(TestSimplifiedPeriod, self).setUp()
 
+    def test_create_asadmin(self):
+        kw = dict(
+                long_name='TestOne',
+                parentnode = self.inf110,
+                start_time = self.inf110_firstsem.start_time,
+                end_time = self.inf110_firstsem.end_time)
+
+        newpk = SimplifiedPeriod.create(self.admin1, short_name='test1', **kw)
+        create_res = models.Period.objects.get(pk=newpk)
+        self.assertEquals(create_res.short_name, 'test1')
+        self.assertEquals(create_res.long_name, 'TestOne')
+        self.assertEquals(create_res.parentnode, self.inf110)
+
+    def test_create_assuperadmin(self):
+        kw = dict(
+                long_name='TestOne',
+                parentnode = self.inf110,
+                start_time = self.inf110_firstsem.start_time,
+                end_time = self.inf110_firstsem.end_time)
+
+        newpk = SimplifiedPeriod.create(self.superadminuser, short_name='test1', **kw)
+        create_res = models.Period.objects.get(pk=newpk)
+        self.assertEquals(create_res.short_name, 'test1')
+        self.assertEquals(create_res.long_name, 'TestOne')
+        self.assertEquals(create_res.parentnode, self.inf110)
+
+    def test_read(self):
+        # do a read with no extra fields
+        read_res = SimplifiedPeriod.read(self.admin1, self.inf110_firstsem.id)
+        expected_res = modelinstance_to_dict(self.inf110_firstsem, SimplifiedPeriod._meta.resultfields.aslist())
+        self.assertEquals(read_res, expected_res)
+
+        # do a read with all extras
+        read_res = SimplifiedPeriod.read(self.admin1, self.inf110_firstsem.id, result_fieldgroups=self.allExtras)
+        expected_res = modelinstance_to_dict(self.inf110_firstsem, SimplifiedPeriod._meta.resultfields.aslist(self.allExtras))
+        self.assertEquals(read_res, expected_res)
+
+    def test_update(self):
+        self.assertEquals(self.inf110_firstsem.short_name, 'firstsem')
+
+        kw = dict(short_name = 'testsem',
+                    long_name = 'TestPeriod')
+
+        pk = SimplifiedPeriod.update(self.admin1,
+                            pk = self.inf110_firstsem.id,
+                            **kw)
+        update_res = models.Period.objects.get(pk=pk)
+        self.assertEquals(update_res.short_name, 'testsem')
+
+        self.assertEquals(self.inf110_firstsem.short_name, 'firstsem')
+        self.refresh_var(self.inf110_firstsem)
+        self.assertEquals(self.inf110_firstsem.short_name, 'testsem')
+
+    def test_search_noextras(self):
+        # search with no query and no extra fields
+        search_res = SimplifiedPeriod.search(self.admin1)
+        expected_res = [modelinstance_to_dict(self.inf110_firstsem,SimplifiedPeriod._meta.resultfields.aslist()),
+                        modelinstance_to_dict(self.inf110_secondsem,SimplifiedPeriod._meta.resultfields.aslist()),
+                        modelinstance_to_dict(self.inf101_firstsem,SimplifiedPeriod._meta.resultfields.aslist()),
+                        modelinstance_to_dict(self.inf101_secondsem,SimplifiedPeriod._meta.resultfields.aslist())]
+
+        # assert that all search results are as expected
+        self.assertEquals(search_res.count(), len(expected_res))
+        for s in search_res:
+            self.assertTrue(s in expected_res)
+
+    def test_search_allextras(self):
+        # search with no querys, but all extra fields
+        search_res = SimplifiedPeriod.search(self.admin1, result_fieldgroups=self.allExtras)
+        expected_res = [modelinstance_to_dict(self.inf110_firstsem,SimplifiedPeriod._meta.resultfields.aslist(self.allExtras)),
+                        modelinstance_to_dict(self.inf110_secondsem,SimplifiedPeriod._meta.resultfields.aslist(self.allExtras)),
+                        modelinstance_to_dict(self.inf101_firstsem,SimplifiedPeriod._meta.resultfields.aslist(self.allExtras)),
+                        modelinstance_to_dict(self.inf101_secondsem,SimplifiedPeriod._meta.resultfields.aslist(self.allExtras))]
+
+        # assert that all search results are as expected
+        self.assertEquals(search_res.count(), len(expected_res))
+        for s in search_res:
+            self.assertTrue(s in expected_res)
+
+    def test_search_withquery(self):
+        # search with query
+        search_res = SimplifiedPeriod.search(self.admin1, query='11')
+        expected_res = [modelinstance_to_dict(self.inf110_firstsem,SimplifiedPeriod._meta.resultfields.aslist()),
+                        modelinstance_to_dict(self.inf110_secondsem,SimplifiedPeriod._meta.resultfields.aslist())]
+
+        # assert that all search results are as expected
+        self.assertEquals(search_res.count(), len(expected_res))
+        for s in search_res:
+            self.assertTrue(s in expected_res)
+
+    def test_search_queryandextras(self):
+        # with query and extra fields.
+        search_res = SimplifiedPeriod.search(self.admin1, query='11', result_fieldgroups=self.allExtras)
+        expected_res = [modelinstance_to_dict(self.inf110_firstsem,SimplifiedPeriod._meta.resultfields.aslist(self.allExtras)),
+                        modelinstance_to_dict(self.inf110_secondsem,SimplifiedPeriod._meta.resultfields.aslist(self.allExtras))]
+
+        # assert that all search results are as expected
+        self.assertEquals(search_res.count(), len(expected_res))
+        for s in search_res:
+            self.assertTrue(s in expected_res)
+
+    def test_search_security(self):
+        self.add_to_path('uni;inf110.firstsem.a2.g1:candidate(testPerson)')
+
+        search_res = SimplifiedPeriod.search(self.testPerson)
+        self.assertEquals(search_res.count(), 0)
+
     def test_delete_asnodeadmin(self):
-        self.add_to_path('uni;inf101.firstSem:admin(testadmin)')
-        SimplifiedPeriod.delete(self.testadmin, self.inf101.firstSem.id)
+        self.add_to_path('uni;test111.testsem:admin(testadmin)')
+        SimplifiedPeriod.delete(self.testadmin, self.test111_testsem.id)
 
         with self.assertRaises(PermissionDenied):
-            SimplifiedPeriod.delete(self.testadmin, self.inf101.firstSem.id)
+            SimplifiedPeriod.delete(self.testadmin, self.test111_testsem.id)
+
+        # this node has children so this should raise PermissionDenied
+        with self.assertRaises(PermissionDenied):
+            SimplifiedPeriod.delete(self.admin1, self.inf101_firstsem.id)
 
     def test_delete_assuperadmin(self):
-        SimplifiedPeriod.delete(self.superadminuser, self.inf101.firstSem.id)
+        SimplifiedPeriod.delete(self.superadminuser, self.inf101_firstsem.id)
 
         with self.assertRaises(PermissionDenied):
-            SimplifiedPeriod.delete(self.superadminuser, self.inf101.firstSem.id)
+            SimplifiedPeriod.delete(self.superadminuser, self.inf101_firstsem.id)
 
     def test_delete_noperm(self):
         with self.assertRaises(PermissionDenied):
-            SimplifiedPeriod.delete(self.firstStud, self.inf101.firstSem.id)
+            SimplifiedPeriod.delete(self.firstStud, self.inf101_firstsem.id)
 
         with self.assertRaises(PermissionDenied):
-            SimplifiedPeriod.delete(self.exam1, self.inf101.firstSem.id)
+            SimplifiedPeriod.delete(self.exam1, self.inf101_firstsem.id)
 
 class TestSimplifiedAssignment(SimplifiedAdminTestBase):
 
