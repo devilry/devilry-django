72a180eaf3a7a41dd4582216a08b000d25d1b587
Vegard Angell <vegarang@ifi.uio.no>
2011-07-24 15:44:44 +0200

    apps.administrator.simplified: added tests for Node
    added tests for everything except filters in SimplifiedNode

--- devilry/apps/administrator/tests/simplified.py
+++ devilry/apps/administrator/tests/simplified.py
@@ -39,9 +39,14 @@ class SimplifiedAdminTestBase(TestCase, testhelper.TestHelper):
 
 
 class TestSimplifiedNode(SimplifiedAdminTestBase):
+    allExtras = SimplifiedNode._meta.resultfields.additional_aslist()
+
     def setUp(self):
         self.add(nodes='uni:admin(admin1).mat.inf')
         self.add(nodes='uni.fys')
+        self.create_superuser('superadminuser')
+
+        #methods = ['create', 'read', 'update', 'delete', 'search'
 
     def test_search_filters(self):
         qrywrap = SimplifiedNode.search(self.admin1)
@@ -75,18 +80,115 @@ class TestSimplifiedNode(SimplifiedAdminTestBase):
         with self.assertRaises(InvalidNumberOfResults):
             SimplifiedNode.search(self.admin1, exact_number_of_results=0)
 
-    def test_delete_asnodeadmin(self):
-        #TODO - can a normal admin for a node delete?
-        # according to simplified.modelapi -> no he/she/it can't..  
-        SimplifiedNode.delete(self.admin1, self.uni.id)
+    def test_create_asadmin(self):
+        kw = dict(
+                long_name='TestOne',
+                parentnode = self.uni)
 
+        newpk = SimplifiedNode.create(self.admin1, short_name='test1', **kw)
+        create_res = models.Node.objects.get(pk=newpk)
+        self.assertEquals(create_res.short_name, 'test1')
+        self.assertEquals(create_res.long_name, 'TestOne')
+        self.assertEquals(create_res.parentnode, self.uni)
+
+    def test_create_assuperadmin(self):
+        kw = dict(
+                long_name='TestOne',
+                parentnode = None)
+
+        newpk = SimplifiedNode.create(self.superadminuser, short_name='test1', **kw)
+        create_res = models.Node.objects.get(pk=newpk)
+        self.assertEquals(create_res.short_name, 'test1')
+        self.assertEquals(create_res.long_name, 'TestOne')
+        self.assertEquals(create_res.parentnode, None)
+
+    def test_read(self):
+        # do a read with no extra fields
+        read_res = SimplifiedNode.read(self.admin1, self.uni.id)
+        expected_res = modelinstance_to_dict(self.uni, SimplifiedNode._meta.resultfields.aslist())
+        self.assertEquals(read_res, expected_res)
+
+        # do a read with all extras
+        read_res = SimplifiedNode.read(self.admin1, self.uni.id, result_fieldgroups=self.allExtras)
+        expected_res = modelinstance_to_dict(self.uni, SimplifiedNode._meta.resultfields.aslist(self.allExtras))
+        self.assertEquals(read_res, expected_res)
+
+    def test_update(self):
+        self.assertEquals(self.uni.short_name, 'uni')
+
+        kw = dict(short_name = 'testuni',
+                    long_name = 'Test')
+
+        pk = SimplifiedNode.update(self.admin1,
+                            pk = self.uni.id,
+                            **kw)
+        update_res = models.Node.objects.get(pk=pk)
+        self.assertEquals(update_res.short_name, 'testuni')
+
+        self.assertEquals(self.uni.short_name, 'uni')
+        self.refresh_var(self.uni)
+        self.assertEquals(self.uni.short_name, 'testuni')
+
+    def test_search_noextras(self):
+        # search with no query and no extra fields
+        search_res = SimplifiedNode.search(self.admin1)
+        expected_res = [modelinstance_to_dict(self.uni, SimplifiedNode._meta.resultfields.aslist()),
+                        modelinstance_to_dict(self.uni_mat, SimplifiedNode._meta.resultfields.aslist()),
+                        modelinstance_to_dict(self.uni_fys, SimplifiedNode._meta.resultfields.aslist()),
+                        modelinstance_to_dict(self.uni_mat_inf, SimplifiedNode._meta.resultfields.aslist())]
+
+        # assert that all search results are as expected
+        self.assertEquals(search_res.count(), len(expected_res))
+        for s in search_res:
+            self.assertTrue(s in expected_res)
+
+    def test_search_allextras(self):
+        # search with no querys, but all extra fields
+        search_res = SimplifiedNode.search(self.admin1, result_fieldgroups=self.allExtras)
+        expected_res = [modelinstance_to_dict(self.uni, SimplifiedNode._meta.resultfields.aslist(self.allExtras)),
+                        modelinstance_to_dict(self.uni_mat, SimplifiedNode._meta.resultfields.aslist(self.allExtras)),
+                        modelinstance_to_dict(self.uni_fys, SimplifiedNode._meta.resultfields.aslist(self.allExtras)),
+                        modelinstance_to_dict(self.uni_mat_inf, SimplifiedNode._meta.resultfields.aslist(self.allExtras))]
+
+        # assert that all search results are as expected
+        self.assertEquals(search_res.count(), len(expected_res))
+        for s in search_res:
+            self.assertTrue(s in expected_res)
+
+    def test_search_withquery(self):
+        # search with query
+        search_res = SimplifiedNode.search(self.admin1, query='inf')
+        expected_res = [modelinstance_to_dict(self.uni_mat_inf, SimplifiedNode._meta.resultfields.aslist())]
+
+        # assert that all search results are as expected
+        self.assertEquals(search_res.count(), len(expected_res))
+        for s in search_res:
+            self.assertTrue(s in expected_res)
+
+    def test_search_queryandextras(self):
+        # with query and extra fields.
+        search_res = SimplifiedNode.search(self.admin1, query='inf', result_fieldgroups = self.allExtras)
+        expected_res = [modelinstance_to_dict(self.uni_mat_inf, SimplifiedNode._meta.resultfields.aslist(self.allExtras))]
+
+        # assert that all search results are as expected
+        self.assertEquals(search_res.count(), len(expected_res))
+        for s in search_res:
+            self.assertTrue(s in expected_res)
+
+    def test_search_security(self):
+        self.add_to_path('uni;inf110.firstsem.a2.g1:candidate(testPerson)')
+
+        search_res = SimplifiedNode.search(self.testPerson)
+        self.assertEquals(search_res.count(), 0)
+
+    def test_delete_asnodeadmin(self):
+        # this node has children and should raise permissiondenied
         with self.assertRaises(PermissionDenied):
             SimplifiedNode.delete(self.admin1, self.uni.id)
 
     def test_delete_assuperadmin(self):
-        self.create_superuser('superadminuser')
         SimplifiedNode.delete(self.superadminuser, self.uni.id)
-        
+
         with self.assertRaises(PermissionDenied):
             SimplifiedNode.delete(self.superadminuser, self.uni.id)
 
@@ -104,13 +206,13 @@ class TestSimplifiedSubject(SimplifiedAdminTestBase):
     def test_delete_asnodeadmin(self):
         self.add_to_path('uni;inf101:admin(testadmin)')
         SimplifiedSubject.delete(self.testadmin, self.inf101.id)
-        
+
         with self.assertRaises(PermissionDenied):
             SimplifiedSubject.delete(self.testadmin, self.inf101.id)
 
     def test_delete_assuperadmin(self):
         SimplifiedSubject.delete(self.superadminuser, self.inf101.id)
-        
+
         with self.assertRaises(PermissionDenied):
             SimplifiedSubject.delete(self.superadminuser, self.inf101.id)
 
@@ -120,7 +222,7 @@ class TestSimplifiedSubject(SimplifiedAdminTestBase):
 
         with self.assertRaises(PermissionDenied):
             SimplifiedSubject.delete(self.exam1, self.inf101.id)
-        
+
 class TestSimplifiedPeriod(SimplifiedAdminTestBase):
     def setUp(self):
         super(TestSimplifiedPeriod, self).setUp()
@@ -128,13 +230,13 @@ class TestSimplifiedPeriod(SimplifiedAdminTestBase):
     def test_delete_asnodeadmin(self):
         self.add_to_path('uni;inf101.firstSem:admin(testadmin)')
         SimplifiedPeriod.delete(self.testadmin, self.inf101.firstSem.id)
-        
+
         with self.assertRaises(PermissionDenied):
             SimplifiedPeriod.delete(self.testadmin, self.inf101.firstSem.id)
 
     def test_delete_assuperadmin(self):
         SimplifiedPeriod.delete(self.superadminuser, self.inf101.firstSem.id)
-        
+
         with self.assertRaises(PermissionDenied):
             SimplifiedPeriod.delete(self.superadminuser, self.inf101.firstSem.id)
 
@@ -158,7 +260,6 @@ class TestSimplifiedAssignment(SimplifiedAdminTestBase):
         self.assertEquals(read_res, expected_res)
 
     def test_read_period(self):
-        # do a read with all extras
         read_res = SimplifiedAssignment.read(self.admin1,
                 self.inf101_firstsem_a1.id, result_fieldgroups='period')
         expected_res = modelinstance_to_dict(self.inf101_firstsem_a1,
@@ -463,7 +564,7 @@ class TestSimplifiedAdminAssignmentGroup(SimplifiedAdminTestBase):
         # We know secondStud hasn't signed up for firstsem.inf101.
         with self.assertRaises(PermissionDenied):
             SimplifiedAssignmentGroup.read(self.secondStud, self.inf101_firstsem_a1_g1.id)
-    
+
     def test_delete_asnodeadmin(self):
         self.add_to_path('uni;inf101.firstsem:admin(testadmin)')
         SimplifiedAssignmentGroup.delete(self.testadmin, self.inf101_firstsem_a1_g1.id)
@@ -490,9 +591,9 @@ class TestSimplifiedAdminAssignmentGroup(SimplifiedAdminTestBase):
 
 
 class TestSimplifiedAdminstratorStaticFeedback(SimplifiedAdminTestBase):
-    
+
     allExtras = SimplifiedStaticFeedback._meta.resultfields.additional_aslist()
-    
+
     def setUp(self):
         super(TestSimplifiedAdminstratorStaticFeedback, self).setUp()
         # we need to add some deliveries here! Use the admin of uni as
@@ -507,7 +608,7 @@ class TestSimplifiedAdminstratorStaticFeedback(SimplifiedAdminTestBase):
                 group.examiners.add(self.exam1)
                 self.add_delivery(group)
                 self.add_feedback(group)
-    
+
     def test_search(self):
         # search with no query and no extra fields
         search_res = SimplifiedStaticFeedback.search(self.admin1)
