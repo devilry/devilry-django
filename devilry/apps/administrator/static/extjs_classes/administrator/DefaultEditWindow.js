3adef4bf5f58ba1b6122c2c531aa0cd7c6ae5f66
Espen A. Kristiansen <post@espenak.net>
2011-07-24 07:06:36 +0200

    apps.administrator: Improved DefaultEditWindow.

--- devilry/apps/administrator/static/extjs_classes/administrator/DefaultEditWindow.js
+++ devilry/apps/administrator/static/extjs_classes/administrator/DefaultEditWindow.js
@@ -1,20 +1,10 @@
-/** Default config for the Edit window, which is opened to edit an item and the
+/** Default config for the Edit window, which is opened to edit an item in the
  * admin interface. */
 Ext.define('devilry.administrator.DefaultEditWindow', {
-    extend: 'Ext.window.Window',
+    extend: 'devilry.administrator.DefaultEditWindowBase',
     title: 'Edit',
-    width: 800,
-    height: 600,
-    layout: 'fit',
-    maximizable: true,
     
     config: {
-        /**
-         * @cfg
-         * The {@link devilry.administrator.EditPanel} to use for editing.
-         */
-        editpanel: undefined,
-
         /**
          * @cfg
          * The {@link devilry.administrator.PrettyView} to refresh when a save succeeds.
@@ -22,21 +12,8 @@ Ext.define('devilry.administrator.DefaultEditWindow', {
         prettyview: undefined
     },
 
-    constructor: function(config) {
-        this.callParent([config]);
-        this.initConfig(config);
-    },
-    
-    initComponent: function() {
-        var me = this;
-        this.editpanel.addListener('saveSucess', function(record) {
-            me.close();
-            me.prettyview.setRecord(record);
-        });
-
-        Ext.apply(this, {
-            items: this.editpanel
-        });
-        this.callParent(arguments);
+    onSaveSuccess: function(record) {
+        this.prettyview.setRecord(record);
+        this.close();
     }
 });
