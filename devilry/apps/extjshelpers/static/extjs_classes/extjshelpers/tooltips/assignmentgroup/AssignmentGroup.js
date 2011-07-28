c89e826fb375612eb05e479146341ac2a1b80608
Tor Ivar Johansen <toij@ifi.uio.no>
2011-07-28 23:01:26 +0200

    devilry/apps/extjshelpers/static/extjs_classes/extjshelpers/tooltips Added tooltips classes for AssigmentgroupView for Examiner. Only basic HTML-rendered help text.

---
+++ devilry/apps/extjshelpers/static/extjs_classes/extjshelpers/tooltips/assignmentgroup/AssignmentGroup.js
@@ -0,0 +1,43 @@
+/**
+ * Tooltip classes for Assignmentgroup.
+ * 
+ */
+Ext.define('devilry.extjshelpers.tooltips.assignmentgroup.StaticFeedbackNext', {
+    extend: 'Ext.tip.ToolTip',
+    html: 'Click to get to the next feedback'
+});
+
+Ext.define('devilry.extjshelpers.tooltips.assignmentgroup.StaticFeedbackPrevious', {
+    extend: 'Ext.tip.ToolTip',
+    html: 'Click to get to the previous feedback'
+});
+
+Ext.define('devilry.extjshelpers.tooltips.assignmentgroup.BrowseFiles', {
+    extend: 'Ext.tip.ToolTip',
+    html: 'Click to view the files in this delivery'
+}); 
+
+Ext.define('devilry.extjshelpers.tooltips.assignmentgroup.AboutTheDelivery', {
+    extend: 'Ext.tip.ToolTip',
+    html: 'Click to get more info about the delivery'
+});
+
+Ext.define('devilry.extjshelpers.tooltips.assignmentgroup.CreateNewDeadline', {
+    extend: 'Ext.tip.ToolTip',
+    html: 'Click to create a new deadline for deliveries'
+});
+
+Ext.define('devilry.extjshelpers.tooltips.assignmentgroup.OtherDeliveries', {
+    extend: 'Ext.tip.ToolTip',
+    html: 'Click to view this groups other deliveries'
+});
+
+Ext.define('devilry.extjshelpers.tooltips.assignmentgroup.SearchField', {
+    extend: 'Ext.tip.ToolTip',
+    html: 'Search within this assignment'
+});
+
+Ext.define('devilry.extjshelpers.tooltips.assignmentgroup.WarningField', {
+    extend: 'Ext.tip.ToolTip',
+    html: 'Information about the current delivery'
+});
