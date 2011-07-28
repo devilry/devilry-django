29ab242c7808d1aa5c5b95c898ef11a42c60ecff
Espen A. Kristiansen <post@espenak.net>
2011-07-29 01:16:59 +0200

    apps.administrator.simplified: Made SimplifiedDelivery not inherit from CanSaveBase.

--- devilry/apps/administrator/simplified.py
+++ devilry/apps/administrator/simplified.py
@@ -148,11 +148,34 @@ class SimplifiedAssignmentGroup(CanSaveBase):
 
 
 @simplified_modelapi
-class SimplifiedDelivery(CanSaveBase):
+class SimplifiedDelivery(SimplifiedModelApi):
     """ Simplified wrapper for :class:`devilry.apps.core.models.Delivery`. """
     class Meta(SimplifiedDeliveryMetaMixin):
         """ Defines what methods an Administrator can use on a Delivery object using the Simplified API """
         methods = ['search', 'read', 'delete']
+
+    @classmethod
+    def create_searchqryset(cls, user, **kwargs):
+        """ Returns all objects of this type that matches arguments
+        given in ``\*\*kwargs`` where ``user`` is admin or superadmin.
+
+        :param user: A django user object.
+        :param \*\*kwargs: A dict containing search-parameters.
+        :rtype: a django queryset
+        """
+        return cls._meta.model.where_is_admin_or_superadmin(user)
+
+    @classmethod
+    def write_authorize(cls, user, obj):
+        """ Check if the given ``user`` can save changes to the given
+        ``obj``, and raise ``PermissionDenied`` if not.
+
+        :param user: A django user object.
+        :param obj: A object of the type this method is used in.
+        :throws PermissionDenied:
+        """
+        if not obj.deadline.assignment_group.can_save(user):
+            raise PermissionDenied()
 
 
 @simplified_modelapi
