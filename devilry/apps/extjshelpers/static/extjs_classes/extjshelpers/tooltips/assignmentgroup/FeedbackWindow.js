1b8af9f8e630789be3ad8e3e3d1fd4114ff8d392
Espen A. Kristiansen <post@espenak.net>
2011-07-29 06:40:25 +0200

    apps.extjshelpers: Fixed FeedBack to Feedback.

--- devilry/apps/extjshelpers/static/extjs_classes/extjshelpers/tooltips/assignmentgroup/FeedbackWindow.js
+++ devilry/apps/extjshelpers/static/extjs_classes/extjshelpers/tooltips/assignmentgroup/FeedbackWindow.js
@@ -1,4 +1,4 @@
-Ext.define('devilry.extjshelpers.tooltips.assignmentgroup.FeedBackWindow', {
+Ext.define('devilry.extjshelpers.tooltips.assignmentgroup.FeedbackWindow', {
             extend: 'devilry.extjshelpers.tooltips.assignmentgroup.CommonConfig',
             target: 'tooltip-feedback-window',
             html: 'Click to create a new feedback for this delivery'
