ff2061da8b1a52577cf0d37b55bfcddfaab8b4b2
Espen A. Kristiansen <post@espenak.net>
2011-07-31 13:34:09 +0200

    apps.extjshelpers: Subject form.

---
+++ devilry/apps/extjshelpers/static/extjs_classes/extjshelpers/forms/administrator/Subject.js
@@ -0,0 +1,48 @@
+Ext.define('devilry.extjshelpers.forms.administrator.Subject', {
+    extend: 'Ext.form.Panel',
+    alias: 'widget.administrator_subjectform',
+    cls: 'widget-nodeform',
+    requires: [
+        'devilry.extjshelpers.formfields.ForeignKeySelector'
+    ],
+
+    suggested_windowsize: {
+        width: 600,
+        height: 400
+    },
+
+    flex: 8,
+
+    layout: {
+        type: 'vbox',
+        align: 'stretch'
+    },
+
+    fieldDefaults: {
+        labelAlign: 'top',
+        labelWidth: 100,
+        labelStyle: 'font-weight:bold'
+    },
+
+    items: [{
+        name: "short_name",
+        fieldLabel: "Short name",
+        xtype: 'textfield'
+    }, {
+        name: "long_name",
+        fieldLabel: "Long name",
+        xtype: 'textfield'
+    }, {
+        name: "parentnode",
+        fieldLabel: "Parent",
+        xtype: 'foreignkeyselector',
+        model: 'devilry.apps.administrator.simplified.SimplifiedNode',
+        emptyText: 'Select a parent node, or leave blank for no parent',
+        displayTpl: '{long_name} ({short_name})',
+        dropdownTpl: '<div class="important">{short_name}</div><div class="unimportant">{long_name}</div>'
+    }],
+
+    help: '<p><strong>Short name</strong> is a short name used when the long name takes to much space. Short name can only contain english lower-case letters, numbers and underscore (_).</p>' +
+        '<p><strong>Long name</strong> is a longer descriptive name which can contain any character.</p>' +
+        '<p><strong>Parent</strong> is the node where this subject belongs.</p>'
+});
