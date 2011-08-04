ab4809f2fe36d8c447cdd5eeb68b1a8c875f44ee
Espen A. Kristiansen <post@espenak.net>
2011-08-04 15:58:31 +0200

    devilryadmin.create_random_exampledata: Update for default gradeeditor_config.

--- devilry/devilryadmin/create_random_exampledata.py
+++ devilry/devilryadmin/create_random_exampledata.py
@@ -261,12 +261,15 @@ def create_assignment(period, deadlines, **assignment_kwargs):
     """ Create an assignment from path. """
     assignment = period.assignments.create(publishing_time = deadlines[0] - timedelta(14),
                                            **assignment_kwargs)
-    gradeconfig = Config(assignment=assignment,
-                         gradeeditorid='asminimalaspossible',
-                         config=json.dumps({'defaultvalue': True,
-                                            'fieldlabel': 'Is the assignment approved?'}))
-    gradeconfig.full_clean()
-    gradeconfig.save()
+    #gradeconfig = Config(assignment=assignment,
+                         #gradeeditorid='asminimalaspossible',
+                         #config=json.dumps({'defaultvalue': True,
+                                            #'fieldlabel': 'Is the assignment approved?'}))
+    assignment.gradeeditor_config.gradeeditorid = 'asminimalaspossible'
+    assignment.gradeeditor_config.config = json.dumps({'defaultvalue': True,
+                                                     'fieldlabel': 'Is the assignment approved?'})
+    assignment.gradeeditor_config.full_clean()
+    assignment.gradeeditor_config.save()
     #assignment.save()
     return assignment
 
