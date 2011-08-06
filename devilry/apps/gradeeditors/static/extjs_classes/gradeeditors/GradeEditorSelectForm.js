8a622507b3038598ce2300f53261dfdb278f1250
Espen A. Kristiansen <post@espenak.net>
2011-08-06 16:58:36 +0200

    apps.gradeeditors: Docs.

--- devilry/apps/gradeeditors/static/extjs_classes/gradeeditors/GradeEditorSelectForm.js
+++ devilry/apps/gradeeditors/static/extjs_classes/gradeeditors/GradeEditorSelectForm.js
@@ -32,13 +32,13 @@ Ext.define('devilry.gradeeditors.GradeEditorSelectForm', {
         'A <strong>grade editor</strong> is what examiners use to give feedback to students.',
 
         'Internally in Devilry, a grade is:<ul>' +
-        '   <li>The number of points. Any grade in devilry is represented as a number, however this number is mainly for statistical purposes.</li>' +
+        '   <li>The number of points. Any grade in Devilry is represented as a number, however this number is mainly for statistical purposes.</li>' +
         '   <li>A very short text that students view. Usually something like: <em>Approved</em>, <em>B</em> or <em>7/10</em>.</li>' +
         '   <li>A longer text that students can view.</li>' +
         '</ul>',
 
         'To make it easy for examiners to create all the information related to a <em>grade</em>, ' +
-        'Devilry use <em>grade editors</em>. Grade editors give examiners a unified user ' +
-        'interfaced tailored for different kinds of grading systems.'
+        'Devilry use <em>grade editors</em>. Grade editors give examiners a unified user-' +
+        'interface tailored for different kinds of grading systems.'
     ]
 });
