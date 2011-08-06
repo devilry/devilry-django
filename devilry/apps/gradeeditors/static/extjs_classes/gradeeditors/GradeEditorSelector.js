f22d5c2b09449111f4c46df9c2ac6c7c4421dcf0
Espen A. Kristiansen <post@espenak.net>
2011-08-06 12:38:27 +0200

    apps.gradeeditors: GradeEditorSelector load current value into combo.

--- devilry/apps/gradeeditors/static/extjs_classes/gradeeditors/GradeEditorSelector.js
+++ devilry/apps/gradeeditors/static/extjs_classes/gradeeditors/GradeEditorSelector.js
@@ -6,12 +6,32 @@ Ext.define('devilry.gradeeditors.GradeEditorSelector', {
 
     valueField: 'gradeeditorid',
     displayField: 'title',
+    queryMode: 'local',
 
     initComponent: function() {
+        this.neverLoaded = true;
         this.store = Ext.create('Ext.data.Store', {
             model: 'devilry.gradeeditors.GradeEditorModel',
             autoSync: true
         });
         this.callParent(arguments);
+        this.store.load({
+            scope: this,
+            callback: function(records, op, success) {
+                if(success) {
+                    this.onLoadSuccess(records);
+                } else {
+                    this.onLoadFailure(records);
+                }
+            }
+        });
+    },
+
+    onLoadSuccess: function(records) {
+        this.setValue(this.value);
+    },
+
+    onLoadFailure: function() {
+        console.error('Failed to load records');
     }
 });
