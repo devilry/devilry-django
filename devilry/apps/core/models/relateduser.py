44ad151b38fc7faf4b5f81245f37ba2acbf84eed
Espen A. Kristiansen <post@espenak.net>
2011-08-06 23:44:18 +0200

    apps.core.models.relateduser: Simplified #144.
    To get a version of related users that we can improve through iteration,
    I simplified RelatedUser by removing tag, and changing user from
    ForeignKey to a username SlugField. For a more complex implementation
    using user as a foreign key, we need to be able to set foreign keys
    by other values than the primary key in simplified.

--- devilry/apps/core/models/relateduser.py
+++ devilry/apps/core/models/relateduser.py
@@ -1,25 +1,31 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils.translation import ugettext as _
+from django.db.models import Q
 
 from period import Period
+from node import Node
+from abstract_is_admin import AbstractIsAdmin
 
 
 
-class RelatedUserBase(models.Model):
-    """
-    .. attribute:: tag
-
-        A string with less than 50 characters.
-    """
-    tag = models.CharField(max_length=50, verbose_name=_('Tag'),
-                          help_text=_('A string with less than 50 characters.'))
-    user = models.ForeignKey(User, verbose_name=_('User'))
+class RelatedUserBase(models.Model, AbstractIsAdmin):
+    username = models.SlugField(max_length=30, help_text=_('A username.'))
 
     class Meta:
         abstract = True # This model will then not be used to create any database table. Instead, when it is used as a base class for other models, its fields will be added to those of the child class.
-        unique_together = ('tag', 'user')
+        unique_together = ('period', 'username')
         app_label = 'core'
+
+    @classmethod
+    def q_is_admin(cls, user_obj):
+        return Q(admins=user_obj) | \
+                Q(period__admins=user_obj) | \
+                Q(period__parentnode__admins=user_obj) | \
+                Q(period__parentnode__parentnode__pk__in=Node._get_nodepks_where_isadmin(user_obj))
+
+    def __unicode__(self):
+        return '{0}:{1}'.format(self.period, self.username)
 
 
 class RelatedExaminer(RelatedUserBase):
@@ -40,7 +46,3 @@ class RelatedStudent(RelatedUserBase):
     """
     period = models.ForeignKey(Period, related_name='relatedstudents',
                                verbose_name=_('Period'))
-
-
-    def __unicode__(self):
-        return '{0}:{1}:{2}'.format(self.tag, self.user, self.period)
