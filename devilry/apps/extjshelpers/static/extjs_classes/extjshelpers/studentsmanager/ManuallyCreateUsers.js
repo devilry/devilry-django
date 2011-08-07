792f05e9e8aa994c8a012c7630a99033985bfc26
Espen A. Kristiansen <post@espenak.net>
2011-08-07 22:58:37 +0200

    apps.extjshelpers: ManuallyCreateUsers docs.

--- devilry/apps/extjshelpers/static/extjs_classes/extjshelpers/studentsmanager/ManuallyCreateUsers.js
+++ devilry/apps/extjshelpers/static/extjs_classes/extjshelpers/studentsmanager/ManuallyCreateUsers.js
@@ -20,18 +20,32 @@ Ext.define('devilry.extjshelpers.studentsmanager.ManuallyCreateUsers', {
     helptext:
         '<section class="helpsection">' +
         '   <h1>Help</h1>' +
-        '   <p>Students are organized in <em>assignment groups</em>.</p>' +
-        '   <p>Very often, an assignment requires <strong>individual</strong> deliveries and feedback. In this case, each <em>assignment group</em> should contain a single student.</p>' +
-        '   <p>When students are supposed to <strong>cooperate</strong> on the same assignment, they should be in the same <em>assignment group</em>.</p>' +
-        '   <p>It is often useful to give an <em>assignment group</em> a <strong>name</strong>. The name is primarily intended for project assignments, where it is useful to name a group after their project. However, the name can be used for other purposes, such as <em>tagging</em> of groups of special interest. Since you can search for groups by name, you can name multiple groups with tags, such as <em>exceptional</em>, and find these groups using the search field.</p>' +
-        '   <h2>Input format</h2>' +
+        '   <p>Students are organized in <em>assignment groups</em>. You should specify <strong>one</strong> <em>assignment group</em> on each line in the input box on the right hand side.</p>' +
+        '   <h2>Common usage examples</h2>' +
+        '   <h3>Individual deliveries</h3>' +
+        '   <p>Very often, an assignment requires <strong>individual</strong> deliveries and feedback. In this case, each <em>assignment group</em> should contain a single student. In this case, the input box should contain something similar to this:</p>' +
+        '   <pre style="margin-left:30px; border: 1px solid #999; padding: 5px;">bob\nalice\neve\ndave</pre>' +
+
+        '   <h3>Group deliveries</h3>' +
+        '   <p>When students are supposed to <strong>cooperate</strong> on the same assignment, they should be in the same <em>assignment group</em>. In this case, the input box should contain something similar to this:</p>' +
+        '   <pre style="margin-left:30px; border: 1px solid #999; padding: 5px;">bob, alice\neve, dave, charlie</pre>' +
+
+        '   <h3>Anonymous assignments</h3>' +
+        '   <p>When you have specified the the assignment is anonymous, you may want to give each student a <em>candidate-id</em> that the examiners will see instead of the username. Specify candidate-id with a colon at the end of each username like this:</p>' +
+        '   <pre style="margin-left:30px; border: 1px solid #999; padding: 5px;">bob:10, alice:232\ncharlie:4X23</pre>' +
+
+        '   <h3>Project groups and group naming</h3>' +
+        '   <p>It is often useful to give an <em>assignment group</em> a <strong>name</strong>. The name is primarily intended for project assignments, where it is useful to name a group after their project. However, the name can be used for other purposes, such as <em>tagging</em> of groups of special interest. Since you can search for groups by name, you can name multiple groups with tags, such as <em>exceptional</em>, and find these groups using the search field. You specify group name like this:</p>' +
+        '   <pre style="margin-left:30px; border: 1px solid #999; padding: 5px;">Secret project:: bob, alice\nTake over world:: eve, dave, charlie</pre>' +
+
+        '   <h2>Input format explained in detail</h2>' +
         '   <p>The format used to create the assignment groups is:</p>' +
         '   <ul>' +
         '       <li>One assignment group on each line.</li>' +
         '       <li>Each username is separated by a comma.</li>' +
         '       <li>Group name is identified by two colons at the end of the name, and must be placed at the beginning of the line.</li>' +
         '       <li>A group name or at least one username is required for each group.</li>' +
-        '       <li>An optional candidate id for a candidate is denoted by a colon and the candidate id after the username.</li>' +
+        '       <li>An optional <em>candidate-id</em> for a candidate is denoted by a colon and the <em>candidate-id</em> after the username.</li>' +
         '   </ul>' +
         '</section>',
 
