ab86dbe84616de5ed16a467f2e165b8954a79df5
Vegard Angell <vegarang@ifi.uio.no>
2011-08-12 20:10:22 +0200

    apps.autograde_gradeeditor: added first version of a new gradeeditor.
    autograde is a gradeeditor where grade/status is automatically set based
    on points. configeditor is still missing

---
+++ devilry/apps/autograde_gradeeditor/devilry_plugin.py
@@ -0,0 +1,69 @@
+import json
+from django.conf import settings
+
+from devilry.apps.gradeeditors import (gradeeditor_registry, JsonRegistryItem,
+                                       DraftValidationError, ConfigValidationError)
+
+
+
+class Manual(JsonRegistryItem):
+    """
+    Serves as a minimal example of a grade editor, and as a well suited grade
+    editor for use in test cases.
+    """
+    gradeeditorid = 'autograde'
+    title = 'Automatig grading'
+    description = 'A gradeeditor where the examiner sets the number of points earned, and the grade is automatically set.'
+    config_editor_url = settings.DEVILRY_STATIC_URL + '/autograde_gradeeditor/configeditor.js'
+    draft_editor_url = settings.DEVILRY_STATIC_URL + '/autograde_gradeeditor/drafteditor.js'
+
+    @classmethod
+    def validate_config(cls, configstring):
+        config = cls.decode_configstring(configstring)
+        cls.validate_dict(config, ConfigValidationError, {'maxpoints': int,
+                                                          'approvedlimit': int,
+                                                          'usegrades': bool,
+                                                          'A': int,
+                                                          'B': int,
+                                                          'C': int,
+                                                          'D': int,
+                                                          'E': int,
+                                                          'pointlabel': basestring,
+                                                          'feedbacklabel': basestring})
+
+    @classmethod
+    def validate_draft(cls, draftstring):
+        buf = json.loads(draftstring)
+        points = buf[0]
+        grade = buf[1]
+        feedback = buf[2]
+        is_approved = buf[3]
+
+        if not isinstance(points, int):
+            raise DraftValidationError('The points-field must be an integer-value.')
+
+        if not isinstance(grade, basestring):
+            raise DraftValidationError('The grade-field must be a string-value.')
+
+        if not isinstance(feedback, basestring):
+            raise DraftValidationError('The feedback-field must contain a string-value.')
+
+        if not isinstance(is_approved, bool):
+            raise DraftValidationError('is_approved must be a boolean value.')
+
+    @classmethod
+    def draft_to_staticfeedback_kwargs(cls, draftstring):
+        buf = json.loads(draftstring)
+        points = buf[0]
+        grade = buf[1]
+        feedback = buf[2]
+        is_approved = buf[3]
+
+        return dict(is_passing_grade=is_approved,
+                    grade=grade,
+                    points=points,
+                    rendered_view='You got {0} points on your delivery and your grade is: {1}. Feedback from examiner: "{2}"'.format(points, grade, feedback))
+
+        #TODO: .format() crashes when using non-ascii symbols.. need to find a fix since norwegian characters are bound to be used in feedbacks!!
+
+gradeeditor_registry.register(Manual)
---
+++ devilry/apps/autograde_gradeeditor/static/autograde_gradeeditor/drafteditor.js
@@ -0,0 +1,145 @@
+{
+    padding: 20,
+    border: false,
+    frame: false,
+    xtype: 'form',
+    //items: [{
+        //xtype: 'checkboxfield',
+        //boxLabel: 'Approved',
+        //id: 'approved-checkbox'
+    //}],
+
+    /**
+     * Called by the grade-editor main window just before calling
+     * setDraftstring() for the first time.
+     *
+     * @param config Get the grade editor configuration that is stored on the
+     *      current assignment.
+     */
+    initializeEditor: function(config) {
+        this.editorConfig = Ext.JSON.decode(config.config);
+
+        this.points = Ext.widget('numberfield', {
+            fieldLabel: this.editorConfig.pointlabel,
+            minValue: 0,
+            maxValue: this.editorConfig.maxpoints
+        });
+        this.textarea = Ext.widget('textareafield', {
+            fieldLabel: this.editorConfig.feedbacklabel,
+            anchor: '-5 -45'
+        });
+        this.add(this.points);
+        this.add(this.textarea);
+    },
+
+    /**
+     * Called by the grade-editor main window to set the current draft. Used
+     * both on initialization and when selecting a draft from history (rolling
+     * back to a previous draft).
+     *
+     * @param draftstring The current draftstring, or ``undefined`` if no
+     *      drafts have been saved yet.
+     */
+    setDraftstring: function(draftstring) {
+        if(draftstring != undefined) {
+        //} else{
+            var buf = Ext.JSON.decode(draftstring);
+            var points = buf[0];
+            var feedback = buf[2];
+            this.points.setValue(points);
+            this.textarea.setValue(feedback);
+        }
+        this.getEl().unmask(); // Unmask the loading mask (set by the main window).
+    },
+
+    /**
+     * Called when the 'save draft' button is clicked.
+     */
+    onSaveDraft: function() {
+        if (this.getForm().isValid()) {
+            var draft = this.createDraft();
+            this.getMainWin().saveDraft(draft, this.onFailure);
+        }
+    },
+
+    /**
+     * Called when the publish button is clicked.
+     */
+    onPublish: function() {
+        if (this.getForm().isValid()) {
+            var draft = this.createDraft();
+            this.getMainWin().saveDraftAndPublish(draft, this.onFailure);
+        }
+    },
+
+    /**
+     * @private
+     * Get the grade draft editor main window.
+     */
+    getMainWin: function() {
+        return this.up('gradedrafteditormainwin');
+    },
+
+    /**
+     * @private
+     * Used by onSaveDraft and onPublish to handle save-failures.
+     */
+    onFailure: function() {
+        console.error('Failed!');
+    },
+
+    /**
+     * @private
+     * Create a draft (used in onSaveDraft and onPublish)
+     */
+    createDraft: function() {
+        var points = this.points.getValue();
+        var feedback = this.textarea.getValue();
+        var grade = '';
+        var passing = true;
+        if (this.editorConfig.usegrades) {
+            grade = this.getGrade(points);
+            if (grade == 'F') {
+                passing = false;
+            }
+        } else {
+            passing = this.getApproved(points);
+            if (passing) {
+                grade = 'approved';
+            } else {
+                grade = 'not approved';
+            }
+        }
+        
+        var retval = new Array();
+        retval[0] = points;
+        retval[1] = grade;
+        retval[2] = feedback;
+        retval[3] = passing;
+
+        var draft = Ext.JSON.encode(retval);
+        return draft;
+    },
+
+    getGrade: function(points) {
+        if (points > this.editorConfig.A) {
+            return 'A';
+        } else if (points >= this.editorConfig.B) {
+            return 'B';
+        } else if (points >= this.editorConfig.C) {
+            return 'C';
+        } else if (points >= this.editorConfig.D) {
+            return 'D';
+        } else if (points >= this.editorConfig.E) {
+            return 'E';
+        }
+        return 'F';
+    },
+
+    getApproved: function(points) {
+        if (points >= this.editorConfig.approvedlimit) {
+            return true;
+        }
+        return false;
+    }
+}
