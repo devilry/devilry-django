65ab2db0db7212bcf864fe3f8cc4bbf15cd48cae
Espen A. Kristiansen <post@espenak.net>
2011-08-13 15:22:00 +0200

    coreutils.restful.metabases.deadline: Use the new combobox_tpl format.

--- devilry/coreutils/restful/metabases/deadline.py
+++ devilry/coreutils/restful/metabases/deadline.py
@@ -1,10 +1,21 @@
 class DeadlineExtjsModelMeta:
     """ Metadata for Deadline javascript. """
-    combobox_fieldgroups = ['assignment', 'period', 'subject']
-    combobox_tpl = ('<div class="important">{deadline:date}</div>'
-                    '<div class="unimportant">'
-                        '{assignment_group__parentnode__parentnode__parentnode__short_name}.'
-                        '{assignment_group__parentnode__parentnode__short_name}.'
-                        '{assignment_group__parentnode__short_name}'
-                        '&mdash; Group id: {assignment_group}</div>')
+    combobox_fieldgroups = ['assignment', 'period', 'subject',
+                            'assignment_group', 'assignment_group_users']
+    combobox_tpl = ('<section class="popuplistitem">'
+                    '    <table>'
+                    '        <tr>'
+                    '            <td>'
+                    '               <h1>{deadline:date}</h1>'
+                    '               <ul class="useridlist"><tpl for="assignment_group__candidates__identifier"><li>{.}</li></tpl></ul>'
+                    '               <p><tpl if="assignment_group__name"> &ndash; {assignment_group__name}</tpl><p>'
+                    '            </td>'
+                    '            <td class="rightaligned">'
+                    '               <h1>{assignment_group__parentnode__parentnode__parentnode__long_name:ellipsis(60)}</h1>'
+                    '               <h2>{assignment_group__parentnode__parentnode__long_name:ellipsis(60)}</h2>'
+                    '               <h3>{assignment_group__parentnode__long_name:ellipsis(60)}</h3>'
+                    '            </td>'
+                    '        </tr>'
+                    '    </table>'
+                   '</section>')
     combobox_displayfield = 'id'
