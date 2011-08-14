e403f117fb71aa477a6c8c516870192197dc72af
Espen A. Kristiansen <post@espenak.net>
2011-08-14 16:03:10 +0200

    apps.extjshelpers: Track latest delivery in DeliveryInfo.

--- devilry/apps/extjshelpers/static/extjs_classes/extjshelpers/assignmentgroup/DeliveryInfo.js
+++ devilry/apps/extjshelpers/static/extjs_classes/extjshelpers/assignmentgroup/DeliveryInfo.js
@@ -44,16 +44,28 @@ Ext.define('devilry.extjshelpers.assignmentgroup.DeliveryInfo', {
 
     tpl: Ext.create('Ext.XTemplate',
         '<tpl if="time_of_delivery">', // If time_of_delivery is false, the record is not loaded yet
-        '    <section class="info-small">',
-        '       <h1>Delivery number {number}</h1>',
-        '       <p>This delivery was made <em>{time_of_delivery:date}</em>. Choose <span class="menuref">Browse files</span> in the toolbar to download the delivered files.</p>',
-        '    </section>',
         '    <tpl if="time_of_delivery &gt; deadline__deadline">',
         '        <section class="warning-small">',
         '           <h1>After deadline</h1>',
         '           <p>This delivery was made <strong>after</strong> the deadline (<em>{deadline__deadline:date}</em>).</p>',
         '        </section>',
         '    </tpl>',
+        '    <tpl if="!islatestDelivery">',
+        '        <section class="warning-small">',
+        '           <h1>Not the latest delivery</h1>',
+        '           <p>',
+        '               The group has made one or more deliveries after the one you are currently viewing. <em>Normally</em> the latest delivery is corrected.',
+        '               <tpl if="time_of_delivery &gt; deadline__deadline">',
+        '                   However since this delivery was made after the deadline, an earlier delivery may be corrected instead.',
+        '               </tpl>',
+        '               Choose <span class="menuref">Other deliveries</span> in the toolbar to view other deliveries.',
+        '           </p>',
+        '        </section>',
+        '    </tpl>',
+        '    <section class="info-small">',
+        '       <h1>Delivery number {number}</h1>',
+        '       <p>This delivery was made <em>{time_of_delivery:date}</em>. Choose <span class="menuref">Browse files</span> in the toolbar to download the delivered files.</p>',
+        '    </section>',
         '</tpl>'
     ),
 
@@ -70,20 +82,67 @@ Ext.define('devilry.extjshelpers.assignmentgroup.DeliveryInfo', {
         });
         this.callParent(arguments);
 
+        this.deliverystore = this.createDeliveryStore();
+
+        if(this.assignmentgroup_recordcontainer.record) {
+            this.loadDeliveryStore();
+        }
+        this.assignmentgroup_recordcontainer.addListener('setRecord', this.loadDeliveryStore, this);
+
         if(this.delivery_recordcontainer.record) {
             this.onLoadDelivery();
         }
-        this.delivery_recordcontainer.addListener('setRecord', this.onLoadDelivery, this);
+        this.delivery_recordcontainer.addListener('setRecord', this.onLoadSomething, this);
     },
 
     /**
      * @private
      */
-    onLoadDelivery: function() {
+    onLoadSomething: function() {
+        if(this.delivery_recordcontainer.record && this.deliverystoreLoaded) {
+            this.onLoadingComplete();
+        }
+    },
+
+    /**
+     * @private
+     * Reload all deliveries on this assignmentgroup.
+     * */
+    loadDeliveryStore: function() {
+        //this.deliverystore.pageSize = 3;
+        this.deliverystore.proxy.extraParams.filters = Ext.JSON.encode([{
+            field: 'deadline__assignment_group',
+            comp: 'exact',
+            value: this.assignmentgroup_recordcontainer.record.data.id
+        }]);
+        this.deliverystore.load({
+            scope: this,
+            callback: function(records, op, success) {
+                if(success) {
+                    this.deliverystoreLoaded = true;
+                    this.onLoadSomething(records);
+                } else {
+                    throw "Failed to load delivery store.";
+                }
+            }
+        });
+    },
+
+    /**
+     * @private
+     */
+    onLoadingComplete: function() {
         var delivery = this.delivery_recordcontainer.record.data;
+        var islatestDelivery = this.deliverystore.currentPage == 1 && this.deliverystore.data.items[0].data.id == delivery.id;
+
         this.show();
         this.toolbar.removeAll();
-        this.update(delivery);
+
+        var data = {
+            islatestDelivery: islatestDelivery
+        };
+        Ext.apply(data, delivery);
+        this.update(data);
 
         this.toolbar.add({
             xtype: 'button',
@@ -117,6 +176,22 @@ Ext.define('devilry.extjshelpers.assignmentgroup.DeliveryInfo', {
 
     /**
      * @private
+     * */
+    createDeliveryStore: function() {
+        var deliverystore = Ext.create('Ext.data.Store', {
+            model: this.deliverymodel,
+            remoteFilter: true,
+            remoteSort: true,
+            autoSync: true,
+            groupField: 'deadline__deadline'
+        });
+
+        deliverystore.proxy.extraParams.orderby = Ext.JSON.encode(['-deadline__deadline', '-number']);
+        return deliverystore;
+    },
+
+    /**
+     * @private
      */
     onOtherDeliveries: function(button) {
         if(!this.deliveriesWindow) {
@@ -129,11 +204,9 @@ Ext.define('devilry.extjshelpers.assignmentgroup.DeliveryInfo', {
                 closeAction: 'hide',
                 items: {
                     xtype: 'deliveriesonsinglegrouplisting',
-                    assignmentgroup_recordcontainer: this.assignmentgroup_recordcontainer,
+                    store: this.deliverystore,
                     delivery_recordcontainer: this.delivery_recordcontainer,
-                    deliverymodel: this.deliverymodel,
-                    deadlinemodel: this.deadlinemodel,
-                    enableDeadlineCreation: this.canExamine
+                    deliverymodel: this.deliverymodel
                 },
 
                 //listeners: {
