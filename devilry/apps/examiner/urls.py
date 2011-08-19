f2e3724d18176dff75cb55872accbd5add5e2871
Tor Ivar Johansen <toij@ifi.uio.no>
2011-08-19 15:55:12 +0200

    devilry/apps/examiner/templates/examiner/main.django.html Text improvement

--- devilry/apps/examiner/urls.py
+++ devilry/apps/examiner/urls.py
@@ -7,7 +7,7 @@ from views import MainView, AssignmentGroupView
 urlpatterns = patterns('devilry.apps.examiner',
                        url(r'^$', login_required(MainView.as_view()), name='examiner'),
                        url(r'^assignmentgroup/(?P<assignmentgroupid>\d+)$',
-                           login_required(AssignmentGroupView.as_view()))
+                           login_required(AssignmentGroupView.as_view()), name='examiner-agroup-view')
                       )
 
 urlpatterns += examiner_restful
