c3e0ce17865ddc8f5a5050f0efc78b0c96e9d28f
Espen A. Kristiansen <post@espenak.net>
2011-08-24 17:14:18 +0200

    apps.administrator: Change group members gui.

--- devilry/apps/administrator/static/extjs_classes/administrator/studentsmanager/StudentsManagerManageGroups.js
+++ devilry/apps/administrator/static/extjs_classes/administrator/studentsmanager/StudentsManagerManageGroups.js
@@ -78,5 +78,81 @@ Ext.define('devilry.administrator.studentsmanager.StudentsManagerManageGroups', 
             this.onNotSingleSelected();
             return;
         }
+
+        var record = this.getSelection()[0];
+        var candidates = this.statics().getCandidateInfoFromGroupRecord(record);
+
+        var candidatestrings = [];
+        Ext.each(candidates, function(candidate, index) {
+            if(candidate.candidate_id == undefined || candidate.candidate_id == "candidate-id missing") {
+                candidatestrings.push(candidate.username);
+            } else {
+                candidatestrings.push(Ext.String.format('{0}:{1}', candidate.username, candidate.candidate_id));
+            }
+        });
+
+        var win = Ext.widget('window', {
+            title: 'Select members',
+            modal: true,
+            width: 500,
+            height: 400,
+            maximizable: true,
+            layout: 'fit',
+            items: {
+                xtype: 'setlistofusers',
+                usernames: candidatestrings,
+                helptext: '<p>The username of a single student on each line. Example:</p>',
+                listeners: {
+                    scope: this,
+                    saveClicked: this.changeGroupMembers
+                }
+            },
+        });
+        win.show();
+    },
+
+    changeGroupMembers: function(setlistofusersobj, candidateSpecs) {
+        var record = this.getSelection()[0];
+        var editRecord = this.createRecordFromStoreRecord(record);
+        editRecord.data.fake_candidates = [];
+        Ext.Array.each(candidateSpecs, function(candidateSpec) {
+            editRecord.data.fake_candidates.push(devilry.administrator.studentsmanager.StudentsManagerManageGroups.parseCandidateSpec(candidateSpec));
+        }, this);
+
+        setlistofusersobj.getEl().mask("Changing group members");
+        editRecord.save({
+            scope: this,
+            failure: function() {
+                setlistofusersobj.getEl().unmask();
+                Ext.MessageBox.alert('Failed to change group members', 'Please try again');
+            },
+            success: function() {
+                setlistofusersobj.up('window').close();
+                this.loadFirstPage();
+            }
+        });
+    },
+
+    statics: {
+        getCandidateInfoFromGroupRecord: function(record) {
+            var candidates = [];
+            Ext.each(record.data.candidates__student__username, function(username, index) {
+                candidate = {username: username};
+                if(record.data.parentnode__anonymous) {
+                    candidate.candidate_id = record.data.candidates__identifier[index];
+                }
+                candidates.push(candidate);
+            });
+            return candidates;
+        },
+
+        parseCandidateSpec: function(candidateSpec) {
+            var asArray = candidateSpec.split(/\s*:\s*/);
+            var candidate_id = asArray.length > 1? asArray[1]: null;
+            return {
+                username: asArray[0],
+                candidate_id: candidate_id
+            };
+        },
     }
 });
