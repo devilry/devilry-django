e307263dd38080518dcfd2db89d864f85239768b
Espen A. Kristiansen <post@espenak.net>
2011-08-24 10:09:16 +0200

    example-productionenv: Cleanup

--- example-productionenv/settings/apache_settings.py
+++ example-productionenv/settings/apache_settings.py
@@ -4,12 +4,11 @@ from settings import *
 ## Alternative log out url. Depends on how you authenticate
 #DEVILRY_LOGOUT_URL='/authenticate/logout'
 
-# Authentication against the REMOTE_USER environment variable.
+## Authentication against the REMOTE_USER environment variable.
 MIDDLEWARE_CLASSES = MIDDLEWARE_CLASSES + [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.RemoteUserMiddleware',
 ]
-
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.RemoteUserBackend'
 ]
