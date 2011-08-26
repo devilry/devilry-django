b713fe59adca8339a79372cddcc7a262219a43d0
Espen A. Kristiansen <post@espenak.net>
2011-08-26 16:58:48 +0200

    apps.superadmin: Useradd and usermod working.

--- devilry/apps/superadmin/management/commands/useradd.py
+++ devilry/apps/superadmin/management/commands/useradd.py
@@ -2,21 +2,24 @@ from django.core.management.base import BaseCommand, CommandError
 from django.contrib.auth.models import User
 from optparse import make_option
 
-class Command(BaseCommand):
+from usermod import UserModCommand
+
+
+class Command(UserModCommand):
     args = '<username>'
     help = 'Create new user.'
     option_list = BaseCommand.option_list + (
         make_option('--email',
             dest='email',
-            default=False,
+            default='',
             help='Email address'),
         make_option('--first_name',
             dest='first_name',
-            default=False,
+            default='',
             help='First name'),
         make_option('--last_name',
             dest='last_name',
-            default=False,
+            default='',
             help='Last name'),
         make_option('--superuser',
             action='store_true',
@@ -28,17 +31,15 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         if len(args) != 1:
             raise CommandError('Username is required. See --help.')
+        verbosity = int(options.get('verbosity', '1'))
         username = args[0]
         kw = {}
-        for attrname in ('email', 'first_name', 'last_name'):
-            kw[attrname] = options[attrname] or ''
-        kw['is_superuser'] = options['is_superuser']
-        self._add_user(username, **kw)
+        for attrname in ('email', 'first_name', 'last_name', 'is_superuser'):
+            kw[attrname] = options[attrname]
 
-    def _add_user(self, username, **kw):
         if User.objects.filter(username=username).count() == 0:
-            u = User(username=username, **kw)
-            u.save()
-            print 'User "{0}" created successfully.'.format(username)
+            user = User(username=username, **kw)
+            user.set_unusable_password()
+            self.save_user(user, verbosity)
         else:
             raise CommandError('User "{0}" already exists.'.format(username))
