5dc544ccc283e79d9a8aa84dc2f483cc4727e17e
Espen A. Kristiansen <post@espenak.net>
2011-08-28 05:48:53 +0200

    markup: CodeHilite for pygments support.

--- devilry/apps/markup/parse_markdown.py
+++ devilry/apps/markup/parse_markdown.py
@@ -4,5 +4,5 @@ import markdown
 def markdown_full(inputMarkdown):
     md = markdown.Markdown(output_format='xhtml1',
                            safe_mode="escape",
-                           extensions=['fenced_code'])
+                           extensions=['codehilite'])
     return md.convert(inputMarkdown)
