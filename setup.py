ae9d082e344611ec9bc375946a585154e09933ca
Espen A. Kristiansen <post@espenak.net>
2011-08-28 18:47:07 +0200

    setup: Specific Pygments version.

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(
     description = "A system for handling electronic deliveries.",
     author = 'The Devilry developers',
     packages = find_packages(),
-    install_requires = ['setuptools', 'Django==1.3', 'Markdown>=2.0.3', 'Pygments']
+    install_requires = ['setuptools', 'Django==1.3', 'Markdown>=2.0.3', 'Pygments==1.4']
 )
