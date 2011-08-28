e26b958ed2ac35dc87e6c3d2d056368fa1eb1f86
Espen A. Kristiansen <post@espenak.net>
2011-08-28 19:02:55 +0200

    apps.extjshelpers...AssignmentAdvanced: Removed attempts.

--- devilry/apps/extjshelpers/static/extjs_classes/extjshelpers/forms/administrator/AssignmentAdvanced.js
+++ devilry/apps/extjshelpers/static/extjs_classes/extjshelpers/forms/administrator/AssignmentAdvanced.js
@@ -28,22 +28,16 @@ Ext.define('devilry.extjshelpers.forms.administrator.AssignmentAdvanced', {
         name: "must_pass",
         fieldLabel: 'Must pass?',
         //boxLabel: "Check this if the student is required to pass this this subject?",
-        xtype: 'checkbox',
-        checked: true
+        xtype: 'checkbox'
+        //checked: true
     }, {
         name: "anonymous",
         fieldLabel: "Anonymous?",
         xtype: 'checkbox'
-    }, {
-        name: "attempts",
-        fieldLabel: "Attempts",
-        xtype: 'textfield'
     }],
 
     help: [
         'If you check <strong>must pass</strong>, any student registered on a group on this assignment is <em>required</em> to pass this assignment to get a passing grade on this period.',
-        'If you check <strong>anonymous</strong>, examiners see a <em>candidate-id</em> instead of a username. A candidate-id must be set for each student. Normally students can see who their examiner is, however when <em>anonymous</em> is checked students do not get any information about their examiner(s).',
-        'When <strong>attempts</strong> is set to something other than <em>0</em>, examiners is asked to create new deadlines for students as long as they have failed to get a passing grade <em>attempts</em> times. When a student has failed to get a passing grade <em>attempts</em> times, their group is automatically closed, which means they get a failing grade on the assignment.',
-        'Note that <strong>attempts</strong> is just a hint that is used to make the job of the examiners easier. Examiners can re-open closed groups if needed, and they can manually close groups even when they have not failed <em>attempts</em> times.'
+        'If you check <strong>anonymous</strong>, examiners see a <em>candidate-id</em> instead of a username. A candidate-id must be set for each student. Normally students can see who their examiner is, however when <em>anonymous</em> is checked students do not get any information about their examiner(s).'
     ]
 });
