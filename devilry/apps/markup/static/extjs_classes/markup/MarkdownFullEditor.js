e1ba3457bb24b5439f87a7b4e0a4ce06df083e9f
Espen A. Kristiansen <post@espenak.net>
2011-08-28 03:35:17 +0200

    apps.markup...MarkdownFullEditor: Usable but ugly.

--- devilry/apps/markup/static/extjs_classes/markup/MarkdownFullEditor.js
+++ devilry/apps/markup/static/extjs_classes/markup/MarkdownFullEditor.js
@@ -5,13 +5,89 @@ Ext.define('devilry.markup.MarkdownFullEditor', {
 
     initComponent: function() {
         Ext.apply(this, {
-            tbar: [{
-                xtype: 'button',
-                text: 'b',
-                listeners: {
-                    scope: this,
-                    click: this.onBold
-                }
+            dockedItems: [{
+                xtype: 'toolbar',
+                dock: 'top',
+                items: [{
+                    xtype: 'button',
+                    text: 'h1',
+                    listeners: {
+                        scope: this,
+                        click: this.onH1
+                    }
+                }, {
+                    xtype: 'button',
+                    text: 'h2',
+                    listeners: {
+                        scope: this,
+                        click: this.onH2
+                    }
+                }, {
+                    xtype: 'button',
+                    text: 'h3',
+                    listeners: {
+                        scope: this,
+                        click: this.onH3
+                    }
+                }, {xtype: 'box', width: 10}, {
+                    xtype: 'button',
+                    text: 'b',
+                    listeners: {
+                        scope: this,
+                        click: this.onBold
+                    }
+                }, {
+                    xtype: 'button',
+                    text: 'i',
+                    listeners: {
+                        scope: this,
+                        click: this.onItalic
+                    }
+                }, {
+                    xtype: 'button',
+                    text: '{}',
+                    listeners: {
+                        scope: this,
+                        click: this.onCode
+                    }
+                }, {
+                    xtype: 'button',
+                    text: 'a',
+                    listeners: {
+                        scope: this,
+                        click: this.onUrl
+                    }
+                }, {xtype: 'box', width: 10}, {
+                    xtype: 'button',
+                    text: '-',
+                    listeners: {
+                        scope: this,
+                        click: this.onBulletList
+                    }
+                }, {
+                    xtype: 'button',
+                    text: '1.',
+                    listeners: {
+                        scope: this,
+                        click: this.onNumberedList
+                    }
+                }, {
+                    xtype: 'button',
+                    text: '" "',
+                    listeners: {
+                        scope: this,
+                        click: this.onBlockQuote
+                    }
+                }, '->', {
+                    xtype: 'button',
+                    text: '',
+                    iconCls: 'icon-help-32',
+                    scale: 'large',
+                    listeners: {
+                        scope: this,
+                        click: this.onHelp
+                    }
+                }]
             }],
             items: [{
                 xtype: 'textareafield'
@@ -20,10 +96,43 @@ Ext.define('devilry.markup.MarkdownFullEditor', {
         this.callParent(arguments);
     },
 
-    onBold: function() {
-        this.setValue(this.surroundCursorSelection('**'));
+    onH1: function() {
+        this.surroundCursorSelection('\n# ', '');
+    },
+    onH2: function() {
+        this.surroundCursorSelection('\n## ', '');
+    },
+    onH3: function() {
+        this.surroundCursorSelection('\n### ', '');
     },
 
+    onBold: function() {
+        this.surroundCursorSelection('**');
+    },
+    onItalic: function() {
+        this.surroundCursorSelection('_');
+    },
+    onCode: function() {
+        this.surroundCursorSelection('`');
+    },
+    onUrl: function() {
+        this.surroundCursorSelection('[', '](http://)');
+    },
+
+    onBulletList: function() {
+        this.surroundCursorSelection('\n- ', '');
+    },
+    onNumberedList: function() {
+        this.surroundCursorSelection('\n1. ', '');
+    },
+    onBlockQuote: function() {
+        this.surroundCursorSelection('\n> ', '');
+    },
+
+
+    onHelp: function() {
+        console.log('Help meee');
+    },
 
     getValue: function() {
         return this.getField().getValue();
@@ -64,12 +173,18 @@ Ext.define('devilry.markup.MarkdownFullEditor', {
     },
     
     surroundCursorSelection: function(prefix, suffix) {
-        suffix = suffix || prefix;
+        if(suffix == undefined) {
+            suffix = prefix;
+        }
         var selection = this.getCursorSelection();
         var curText = this.getValue();
         var textPrefix = curText.substring(0, selection.start);
         var textSuffix = curText.substring(selection.end, curText.length);
         var selectionText = curText.substring(selection.start, selection.end);
-        return textPrefix + prefix + selectionText + suffix + textSuffix;
+        var result = textPrefix + prefix + selectionText + suffix + textSuffix;
+        this.setValue(result);
+
+        var cursorPosition = selection.start + prefix.length;
+        this.getField().selectText(cursorPosition, cursorPosition);
     }
 });
