46754265c2bc424c800ae7333648c842e31b3ab0
Tor Ivar Johansen <toij@ifi.uio.no>
2011-08-29 02:17:49 +0200

    devilry/apps/examiner/views.py refactored zipfile construction due to stupidity

--- devilry/apps/examiner/views.py
+++ devilry/apps/examiner/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import TemplateView, View
 from django.shortcuts import render
-from tempfile import TemporaryFile
+from tempfile import TemporaryFile, NamedTemporaryFile
 from devilry.apps.gradeeditors.restful import examiner as gradeeditors_restful
 from devilry.utils.module import dump_all_into_dict
 from django.http import HttpResponse
@@ -55,64 +55,42 @@ class CompressedFileDownloadView(View):
                 candidates_as_string += "_"
             else:
                 candidates_as_string += "-"
-        candidates_as_string += str(assignmentgroup_id)
+        candidates_as_string += "group-" + str(assignmentgroup_id)
         return candidates_as_string
-
-    def _add_directory_to_zipfile(self, zip_file, basedir):
-        for root, dirs, files in os.walk(os.path.join(os.getcwd(), "deliveries")):
-            for fn in files:
-                absolute_fn = os.path.join(root, fn)
-                relative_fn = absolute_fn[len(basedir)+len(os.sep):]
-                zip_file.write(absolute_fn, relative_fn)
-        zip_file.close()
-
-    def _copy_file_from_deliverystore(self, filemeta):
-        file_content = filemeta.deliverystore.read_open(filemeta)
-        ut = open(filemeta.filename, 'wb')
-        for data in file_content:
-            ut.write(data)
-        ut.close()
 
     #TODO use temporary directory
     def get(self, request, assignmentid):
         assignment = get_object_or_404(Assignment, id=assignmentid)
-        
-        basedir = os.getcwd()
-        os.mkdir("deliveries")
-        os.chdir(os.path.join(os.getcwd(), "deliveries"))
-        
+
         zip_file_name = assignment.short_name + ".zip"
-        tempfile = TemporaryFile()
-        zip_file = zipfile.ZipFile(tempfile, 'w');        
+        tempfile = NamedTemporaryFile()
+        zip_file = zipfile.ZipFile(tempfile, 'w');
+
+        basedir = "deliveries" + os.sep
+        path = basedir
                 
         for assignmentgroup in assignment.assignmentgroups.all():
             candidates = self._get_candidates_as_string(assignmentgroup.candidates.all(), assignmentgroup.id)
-            
-            for deadline in assignmentgroup.deadlines.all():
-            
-                deadline_dir = os.getcwd()
-                deadline_dir_name = deadline.deadline.strftime("%d-%m-%Y_")
-                deadline_dir_name += str(assignmentgroup.id)
-                os.mkdir(deadline_dir_name)
-                
-                os.chdir(os.path.join(os.getcwd(), deadline_dir_name))
-                os.mkdir(candidates)
-                
-                for delivery in deadline.deliveries.all():
-                    delivery_dir = os.getcwd()
-                    os.chdir(os.path.join(os.getcwd(), candidates))
-                    os.mkdir(str(delivery.number))
-                    os.chdir(os.path.join(os.getcwd(), str(delivery.number)))
-                    
-                    for filemeta in delivery.filemetas.all():                        
-                        self._copy_file_from_deliverystore(filemeta)
-                        
-                    os.chdir(delivery_dir)                        
-                os.chdir(deadline_dir)
-        os.chdir(basedir)
 
-        self._add_directory_to_zipfile(zip_file, basedir)
-        shutil.rmtree(os.path.join(os.getcwd(), "deliveries"))
+            for deadline in assignmentgroup.deadlines.all():
+                deadline_dir_name = deadline.deadline.strftime("%d-%m-%Y_")
+                deadline_dir_name += "group-" + str(assignmentgroup.id)
+                path += deadline_dir_name + os.sep
+                path += candidates + os.sep
+                deadline_root = path
+
+                for delivery in deadline.deliveries.all():
+                    path += str(delivery.number) + os.sep
+                    delivery_root = path
+
+                    for filemeta in delivery.filemetas.all():
+                        path += filemeta.filename
+                        file_content = filemeta.deliverystore.read_open(filemeta)
+                        zip_file.writestr(path, file_content.read())
+                        path = delivery_root
+                    path = deadline_root
+                path = basedir
+        zip_file.close()
 
         tempfile.seek(0)
         response = HttpResponse(FileWrapperWithExplicitClose(tempfile),
@@ -120,4 +98,4 @@ class CompressedFileDownloadView(View):
         response['Content-Disposition'] = "attachment; filename=%s" % \
             zip_file_name.encode("ascii", 'replace')
         response['Content-Length'] = os.stat(tempfile.name).st_size
-        return response #HttpResponse("Hei Verden")
+        return response
