45a0f36d075eee374fc5a6563d0b5dac47118a84
root <root@bro-compal.(none)>
2011-08-31 19:42:57 +0200

    Added logging to settings.py in exxample-production

--- example-productionenv/settings/settings.py
+++ example-productionenv/settings/settings.py
@@ -41,6 +41,105 @@ DATABASES["default"] = {
 #}
 
 
+#
+# Logging
+#
+
+MIDDLEWARE_CLASSES = MIDDLEWARE_CLASSES + [
+    'devilry.utils.logexceptionsmiddleware.TracebackLoggingMiddleware',
+    #'devilry.utils.profile.ProfilerMiddleware' # Enable profiling. Just add ?prof=yes to any url to see a profile report
+]
+
+logdir = join(this_dir, 'log')
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'verbose': {
+            'format': '[%(levelname)s %(asctime)s %(name)s] %(message)s'
+        },
+        'simple': {
+            'format': '[%(levelname)s] %(message)s'
+        },
+    },
+
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'formatter': 'simple',
+            'class': 'logging.StreamHandler'
+        },
+        'allButExceptionTracebacks': {
+            'level': 'ERROR',
+            'formatter': 'verbose',
+            'class': 'logging.FileHandler',
+            'filename': join(logdir, 'all-but-exceptiontracebacks.devilry.log'),
+        },
+        'dbfile': {
+            'level': 'ERROR',
+            'formatter': 'verbose',
+            'class': 'logging.FileHandler',
+            'filename': join(logdir, 'db.devilry.log')
+        },
+        'dbdebugfile': { # Shows the SQL statements
+            'level': 'DEBUG',
+            'formatter': 'verbose',
+            'class': 'logging.FileHandler',
+            'filename': join(logdir, 'debug-containing-sqlstatements.db.devilry.log')
+        },
+        'requestfile': {
+            'level': 'ERROR',
+            'formatter': 'verbose',
+            'class': 'logging.FileHandler',
+            'filename': join(logdir, 'request.devilry.log')
+        },
+        'exceptionTracebacksFile': {
+            'level': 'ERROR',
+            'formatter': 'verbose',
+            'class': 'logging.FileHandler',
+            'filename': join(logdir, 'exception.devilry.log')
+        },
+        'emailfile': {
+            'level': 'ERROR',
+            'formatter': 'verbose',
+            'class': 'logging.FileHandler',
+            'filename': join(logdir, 'email.devilry.log')
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler',
+            'include_html': True # The traceback email includes an HTML attachment containing the full content of the debug Web page that would have been produced if DEBUG were True. 
+        }
+    },
+    'loggers': {
+        'devilry.utils.logexceptionsmiddleware': {
+            'handlers': ['exceptionTracebacksFile', 'mail_admins'],
+            'level': 'ERROR',
+            'propagate': False
+        },
+        'django.request': {
+            'handlers': ['allButExceptionTracebacks',
+                         'requestfile', 'mail_admins'],
+            'level': 'ERROR',
+            'propagate': False
+        },
+        'django.db.backends': {
+            'handlers': ['allButExceptionTracebacks',
+                         'dbfile',
+                         'mail_admins'],
+            'level': 'DEBUG',
+            'propagate': False
+        },
+        'devilry.utils.devilry_email': {
+            'handlers': ['allButExceptionTracebacks', 'mail_admins'],
+            'level': 'DEBUG',
+            'propagate': False
+        },
+    }
+}
+
+
+
 #################################
 ## THESE SETTINGS MUST BE SET
 #################################
