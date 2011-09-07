acb98e2127ae2b74735de683cb5df25ee91437ca
Espen A. Kristiansen <post@espenak.net>
2011-09-07 11:47:27 +0200

    devilryrestfullib: Rolls back 5eeb56e2dd1a587e9283a4c5237e8e7b68e9a9d0 changes in devilryrestfullib.
    This breaks devilryclient, however login and restfulfactory can not have
    the changes introduced by 5eeb56e2dd1a587e9283a4c5237e8e7b68e9a9d0 and
    still be a general and simple reference implementation for the RESTful
    API. These changes must be made in a wrapper of sort in devilryclient
    instead.

--- devilryrestfullib/login.py
+++ devilryrestfullib/login.py
@@ -2,77 +2,50 @@ import urllib
 from urlparse import urlparse
 import httplib
 from Cookie import SimpleCookie
-from devilryclient.utils import findconffolder
-from getpass import getpass
-from os.path import join, exists
-from os import chmod
-import stat
-import ConfigParser
-
 
 class LoginError(Exception):
-    """Raised on login error"""
+    """ Raised on login errors """
 
+def login(url, **logindata):
+    """
+    Send a login request to the given login ``url``, and return the login cookie.
 
-def get_session_cookie():
-    if exists(join(findconffolder(), 'session')):
-        session = open(join(findconffolder(), 'session'), 'r')
-        cookieout = session.read()
-        session.close()
-        return cookieout
-    else:
-        return login()
+    :param url: Url of the login form.
+    :param logindata: The required login data (usually username and
+        password). With the devilry login form, this is::
 
+            {'user':'myuser', 'password':'mysecret'}
 
-def login():
+        However it may be something else (on custom login solutions).
 
-    confdir = findconffolder()
-    conf = ConfigParser.ConfigParser()
-    conf.read(join(confdir, 'config'))
-
-    # make the url and credentials
-    url = join(conf.get('resources', 'url'), 'authenticate/login')
-
-    username = raw_input("Username: ")
-    password = getpass("Password: ")
-
-    creds = urllib.urlencode({'username': username, 'password': password})
-
-    parsed_url = urlparse(url)
-    host = parsed_url.netloc
-
-    if parsed_url.scheme == "https":
+    :return: The login cookie as a string on success.
+    :throw: LoginError if the login fails.
+    """
+    parsedurl = urlparse(url)
+    host = parsedurl.netloc
+    if parsedurl.scheme == "https":
         conn = httplib.HTTPSConnection(host)
     else:
         conn = httplib.HTTPConnection(host)
-
-    response = conn.request('POST', parsed_url.path, creds, {'Content-type': "application/x-www-form-urlencoded"})
-
+    #conn.set_debuglevel(1)
+    conn.request("POST", parsedurl.path, urllib.urlencode(logindata),
+        headers = {
+            "Content-type": "application/x-www-form-urlencoded"})
     response = conn.getresponse()
-    if response.status > 400:
+    if response.status < 400:
+        data = response.read()
+        setcookie = response.getheader("Set-Cookie")
+        if setcookie == None:
+            raise LoginError("Login failed. This is usually because of "
+                    "invalid username/password, but might be "
+                    "caused by wrong login urlprefix or server errors. "
+                    "Technical error message: Login urlprefix did not "
+                    "respond with any authorization cookies.")
+        else:
+            cookie = SimpleCookie()
+            cookie.load(setcookie)
+            cookieout = cookie.output().replace("Set-Cookie: ", "")
+            return cookieout
+    else:
         raise LoginError("Login to %s failed with the following message: %s %s (%s)" % (
                 url, response.status, response.reason, response.msg))
-
-    response.read()
-    setcookie = response.getheader('Set-Cookie')
-    if setcookie == None:
-        raise LoginError("Login failed. This is usually because of "
-                         "invalid username/password, but might be "
-                         "caused by wrong login urlprefix or server errors. "
-                         "Technical error message: Login urlprefix did not "
-                         "respond with any authorization cookies.")
-
-    cookie = SimpleCookie()
-    cookie.load(setcookie)
-    cookieout = cookie.output().replace('Set-Cookie: ', '')
-    session = open(join(confdir, 'session'), 'w')
-    session.write(cookieout)
-    session.close()
-    chmod(join(confdir, 'session'), stat.S_IRUSR | stat.S_IWUSR)
-
-    conf.set('resources', 'user', username)
-
-    with open(join(confdir, 'config'), 'wb') as f:
-        conf.write(f)
-
-    return cookieout
