d79a9d1d4827589972300f2be851eb68744896f9
Espen A. Kristiansen <post@espenak.net>
2011-09-13 13:09:27 +0200

    apps.superadmin: Commands to clear admins on period and subject.

---
+++ devilry/apps/superadmin/management/commands/devilry_periodadminclear.py
@@ -0,0 +1,22 @@
+from django.core.management.base import CommandError
+
+from devilry.apps.core.models import Period
+from devilry_subjectadminadd import AdminAddBase
+
+
+class Command(AdminAddBase):
+    args = '<subject-short_name>'
+    help = 'Clear period admins (removes all subject admins on a subject).'
+
+    def handle(self, *args, **options):
+        if len(args) != 2:
+            raise CommandError('Subject and period is required. See --help.')
+        short_name = args[0]
+        period_short_name = args[1]
+
+        subject = self.get_subject(short_name)
+        try:
+            period = Period.objects.get(short_name=period_short_name, parentnode=subject)
+        except Period.DoesNotExist, e:
+            raise CommandError('Invalid period-short_name.')
+        period.admins.clear()
