3bd7d3cfa41302b4e390481a2195ad2aea66ec39
Espen A. Kristiansen <post@espenak.net>
2011-09-13 20:54:28 +0200

    apps.core.models.deliverytypes: Values first and labels last.

--- devilry/apps/core/models/deliverytypes.py
+++ devilry/apps/core/models/deliverytypes.py
@@ -3,6 +3,6 @@ NON_ELECTRONIC = 1
 ALIAS = 2
 
 def as_choices_tuple():
-    return (('Electronic', ELECTRONIC),
-            ('Non electronic', NON_ELECTRONIC),
-            ('Alias', ALIAS))
+    return ((ELECTRONIC, 'Electronic'),
+            (NON_ELECTRONIC, 'Non electronic'),
+            (ALIAS, 'Alias'))
