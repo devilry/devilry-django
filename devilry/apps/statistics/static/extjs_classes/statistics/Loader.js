350355e8fb817c3fb7c0b18280c91dc2b51fc186
Espen A. Kristiansen <post@espenak.net>
2011-09-15 20:49:02 +0200

    apps.statistics: Loading table of period statistics.

--- devilry/apps/statistics/static/extjs_classes/statistics/Loader.js
+++ devilry/apps/statistics/static/extjs_classes/statistics/Loader.js
@@ -2,7 +2,10 @@ Ext.define('devilry.statistics.Loader', {
     extend: 'Ext.util.Observable',
 
     constructor: function(periodid, config) {
-        this.assignments = {};
+        this.fields = ['username'];
+        this.columns = [{header: 'Username', dataIndex: 'username'}];
+        this.studentsMap = {};
+        this.students = [];
         this.loadPeriod(periodid);
 
         this.addEvents('loaded');
@@ -24,8 +27,35 @@ Ext.define('devilry.statistics.Loader', {
         assignmentgroup_store.load({
             scope: this,
             callback: function(grouprecords, success) {
-                console.log('Loaded assignmentgroups:', grouprecords);
-                this.assignments[assignmentid].groups = grouprecords;
+                //console.log('Loaded assignmentgroups:', grouprecords);
+                Ext.each(grouprecords, function(grouprecord, index) {
+                    Ext.each(grouprecord.data.candidates__student__username, function(username, index) {
+                        var student;
+                        if(this.studentsMap[username]) {
+                            student = this.students[this.studentsMap[username]];
+                        } else {
+                            this.studentsMap[username] = this.students.length;
+                            student = {};
+                            student.username = username;
+                            this.students.push(student);
+                        }
+
+                        var assignment_ident = grouprecord.data.parentnode__short_name;
+                        var pointdataIndex = assignment_ident + '_points';
+                        var passingdataIndex = assignment_ident + '_is_passing_grade';
+                        student[pointdataIndex] = grouprecord.data.feedback__points;
+                        student[passingdataIndex] = grouprecord.data.feedback__is_passing_grade;
+
+                        if(!Ext.Array.contains(this.fields, pointdataIndex)) {
+                            this.fields.push(pointdataIndex);
+                            this.fields.push(passingdataIndex);
+                            this.columns.push({dataIndex: pointdataIndex, header: pointdataIndex});
+                            this.columns.push({dataIndex: passingdataIndex, header: passingdataIndex});
+                            this.columns.push({dataIndex: passingdataIndex, header: passingdataIndex});
+                        }
+                    }, this);
+                }, this);
+
                 this._tmpAssignmentsWithAllGroupsLoaded ++;
                 if(this._tmpAssignmentsWithAllGroupsLoaded == totalAssignments) {
                     this.fireEvent('loaded', this);
@@ -36,8 +66,6 @@ Ext.define('devilry.statistics.Loader', {
 
     loadAssignments: function(periodid) {
         assignment_store.pageSize = 10000; // TODO: avoid UGLY hack
-        console.log(periodid);
-
         assignment_store.proxy.setDevilryFilters([{
             field: 'parentnode',
             comp: 'exact',
@@ -46,14 +74,9 @@ Ext.define('devilry.statistics.Loader', {
         assignment_store.load({
             scope: this,
             callback: function(assignmentrecords, success) {
-                console.log('Loaded assignments:', assignmentrecords);
-
+                //console.log('Loaded assignments:', assignmentrecords);
                 this._tmpAssignmentsWithAllGroupsLoaded = 0;
                 Ext.each(assignmentrecords, function(assignmentrecord, index) {
-                    this.assignments[assignmentrecord.data.id] = {
-                        record: assignmentrecord,
-                        groups: undefined
-                    }
                     this.loadGroups(assignmentrecord.data.id, assignmentrecords.length);
                 }, this);
             }
@@ -64,7 +87,7 @@ Ext.define('devilry.statistics.Loader', {
         period_model.load(periodid, {
             scope: this,
             success: function(record) {
-                console.log("Loaded period:", record);
+                //console.log("Loaded period:", record);
                 this.loadAssignments(record.data.id);
             }
         });
