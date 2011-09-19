f6a1a1e39c9dee309af576fcf6d33445ff8d4749
Espen A. Kristiansen <post@espenak.net>
2011-09-19 22:56:30 +0200

    apps.gradeeditors.restful: Override extra_create_or_replace_responsedata on Draft.
    To provide a staticfeedback in extra_responsedata for previews.

--- devilry/apps/gradeeditors/restful/examiner.py
+++ devilry/apps/gradeeditors/restful/examiner.py
@@ -1,9 +1,11 @@
+from datetime import datetime
 from devilry.restful import restful_modelapi, ModelRestfulView, RestfulManager
 from devilry.apps.extjshelpers import extjs_restful_modelapi
 from devilry.apps.examiner.restful import (RestfulSimplifiedAssignment,
                                            RestfulSimplifiedDelivery)
 from ..simplified.examiner import (SimplifiedConfig,
                                    SimplifiedFeedbackDraft)
+from ..models import FeedbackDraft
 
 
 __all__ = ('RestfulSimplifiedConfig', 'RestfulSimplifiedFeedbackDraft')
@@ -18,10 +20,21 @@ class RestfulSimplifiedConfig(ModelRestfulView):
         foreignkey_fields = {'assignment': RestfulSimplifiedAssignment}
 
 
+class RestfulSimplifiedFeedbackDraftCommon(object):
+    def extra_create_or_replace_responsedata(self, obj_id):
+        draft = FeedbackDraft.objects.get(id=obj_id)
+        staticfeedback = draft.to_staticfeedback()
+        return dict(rendered_view = staticfeedback.rendered_view,
+                    save_timestamp = datetime.now(), # This is used for previews, so a preview of how a date will look makes sense.
+                    grade = staticfeedback.grade,
+                    points = staticfeedback.points,
+                    is_passing_grade = staticfeedback.is_passing_grade)
+
+
 @examiner_restful.register
 @extjs_restful_modelapi
 @restful_modelapi
-class RestfulSimplifiedFeedbackDraft(ModelRestfulView):
+class RestfulSimplifiedFeedbackDraft(RestfulSimplifiedFeedbackDraftCommon, ModelRestfulView):
     class Meta:
         simplified = SimplifiedFeedbackDraft
         foreignkey_fields = {'delivery': RestfulSimplifiedDelivery}
