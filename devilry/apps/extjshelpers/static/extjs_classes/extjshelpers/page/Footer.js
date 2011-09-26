96215debb1212e64ffe583506af8224c4e970d22
Espen A. Kristiansen <post@espenak.net>
2011-09-26 18:14:30 +0200

    apps: Started experimenting with Viewport.

---
+++ devilry/apps/extjshelpers/static/extjs_classes/extjshelpers/page/Footer.js
@@ -0,0 +1,13 @@
+Ext.define('devilry.extjshelpers.page.Footer', {
+    extend: 'Ext.panel.Panel',
+    alias: 'widget.pagefooter',
+    border: false,
+    margins: '0 0 0 0',
+    autoHeight: true,
+
+    html: Ext.create('Ext.XTemplate',
+        '<div class="footer">',
+        '   <a href="http://devilry.org">Devilry</a> is an open source general purpose delivery system.',
+        '</div>'
+    ).apply()
+});
---
+++ devilry/apps/extjshelpers/static/extjs_classes/extjshelpers/page/Header.js
@@ -0,0 +1,10 @@
+Ext.define('devilry.extjshelpers.page.Header', {
+    extend: 'Ext.panel.Panel',
+    alias: 'widget.pageheader',
+    border: false,
+    margins: '0 0 0 0',
+    //height: 70,
+    autoHeight: true,
+
+    html: '<div class="section"><h1>Devilry</h1></div>'
+});
