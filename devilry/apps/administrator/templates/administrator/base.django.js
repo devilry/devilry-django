ca68e9c1e17267f9c36932829533b6a88b5c3946
Espen A. Kristiansen <post@espenak.net>
2011-09-26 21:24:30 +0200

    apps.administrator: Made searchwidget work with viewport.
    However, the body overflows with long results.

--- devilry/apps/administrator/templates/administrator/base.django.js
+++ devilry/apps/administrator/templates/administrator/base.django.js
@@ -1,27 +1,6 @@
 {% extends "theme/common.django.html" %}
 {% load extjs %}
 
-{% block appjs2 %}
-    var DASHBOARD_URL = '{{ DEVILRY_URLPATH_PREFIX }}/administrator/';
-
-    Ext.require('devilry.administrator.AdministratorSearchWidget');
-    Ext.onReady(function() {
-        var searchwidget = Ext.create('devilry.administrator.AdministratorSearchWidget', {
-            //renderTo: 'searchwidget-container',
-            urlPrefix: DASHBOARD_URL,
-            nodeRowTpl: '{{ restfulapi.RestfulSimplifiedNode.ExtjsModelMeta.combobox_tpl|escapejs }}',
-            subjectRowTpl: '{{ restfulapi.RestfulSimplifiedSubject.ExtjsModelMeta.combobox_tpl|escapejs }}',
-            periodRowTpl: '{{ restfulapi.RestfulSimplifiedPeriod.ExtjsModelMeta.combobox_tpl|escapejs }}',
-            assignmentRowTpl: '{{ restfulapi.RestfulSimplifiedAssignment.ExtjsModelMeta.combobox_tpl|escapejs }}',
-            assignmentgroupRowTpl: '{{ restfulapi.RestfulSimplifiedAssignmentGroup.ExtjsModelMeta.combobox_tpl|escapejs }}',
-            deliveryRowTpl: '{{ restfulapi.RestfulSimplifiedDelivery.ExtjsModelMeta.combobox_tpl|escapejs }}'
-        });
-        searchwidget.loadInitialValues();
-        {% block post-searchwidget-javascript %}{% endblock %}
-    });
-
-{% endblock %}
-
 {% block imports %}
     {{ block.super }}
     Ext.require('devilry.extjshelpers.page.Header');
@@ -35,6 +14,23 @@
         {{ RestfulCls|extjs_combobox_model:"Search" }};
         {{ RestfulCls|extjs_store:"Search" }};
     {% endfor %}
+{% endblock %}
+
+{% block onready %}
+    {{ block.super }}
+    Ext.getBody().mask("Loading page", 'page-load-mask');
+
+    var searchwidget = Ext.create('devilry.administrator.AdministratorSearchWidget', {
+        //renderTo: 'searchwidget-container',
+        urlPrefix: DASHBOARD_URL,
+        nodeRowTpl: '{{ restfulapi.RestfulSimplifiedNode.ExtjsModelMeta.combobox_tpl|escapejs }}',
+        subjectRowTpl: '{{ restfulapi.RestfulSimplifiedSubject.ExtjsModelMeta.combobox_tpl|escapejs }}',
+        periodRowTpl: '{{ restfulapi.RestfulSimplifiedPeriod.ExtjsModelMeta.combobox_tpl|escapejs }}',
+        assignmentRowTpl: '{{ restfulapi.RestfulSimplifiedAssignment.ExtjsModelMeta.combobox_tpl|escapejs }}',
+        assignmentgroupRowTpl: '{{ restfulapi.RestfulSimplifiedAssignmentGroup.ExtjsModelMeta.combobox_tpl|escapejs }}',
+        deliveryRowTpl: '{{ restfulapi.RestfulSimplifiedDelivery.ExtjsModelMeta.combobox_tpl|escapejs }}'
+    });
+    searchwidget.loadInitialValues();
 {% endblock %}
 
 
