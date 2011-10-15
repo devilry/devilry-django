eb06b9d1d99aad3a71a96be87f33561da154c070
Espen A. Kristiansen <post@espenak.net>
2011-10-15 12:13:50 +0200

    apps.statistics: Added support for grouping of assignments.
    I.E.: You need to pass one of three assignments.

--- devilry/apps/statistics/static/extjs_classes/statistics/Filter.js
+++ devilry/apps/statistics/static/extjs_classes/statistics/Filter.js
@@ -19,17 +19,29 @@ Ext.define('devilry.statistics.Filter', {
             return true;
         }
         var matches = true;
-        Ext.each(this.must_pass, function(assignment_short_name, index) {
-            var assignment = student.assignments[assignment_short_name];
-            if(!assignment) {
-                throw "Invalid assignment name: " + assignment_short_name;
-            }
-            if(!assignment.is_passing_grade) {
+        Ext.each(this.must_pass, function(assignment_short_names, index) {
+            var one_of_them_is_passing = this._passesOneOfManyAssignments(student, assignment_short_names);
+            if(!one_of_them_is_passing) {
                 matches = false;
                 return false; // Break
             }
         }, this);
         return matches;
+    },
+
+    _passesOneOfManyAssignments: function(student, assignment_short_names) {
+        var one_of_them_is_passing = false;
+        Ext.each(assignment_short_names, function(assignment_short_name, index) {
+            var assignment = student.assignments[assignment_short_name];
+            if(!assignment) {
+                throw "Invalid assignment name: " + assignment_short_name;
+            }
+            if(assignment.is_passing_grade) {
+                one_of_them_is_passing = true;
+                return false; // Break
+            }
+        }, this);
+        return one_of_them_is_passing;
     },
 
     _matchPointspec: function(student) {
