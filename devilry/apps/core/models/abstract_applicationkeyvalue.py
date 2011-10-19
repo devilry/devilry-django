2aa37a4727aa51351744a1153d1c600405081a9a
Espen A. Kristiansen <post@espenak.net>
2011-10-19 12:16:15 +0200

    apps.core.mdoels.abstract_applicationkeyvalue: Value not required.

--- devilry/apps/core/models/abstract_applicationkeyvalue.py
+++ devilry/apps/core/models/abstract_applicationkeyvalue.py
@@ -5,7 +5,7 @@ class AbstractApplicationKeyValue(models.Model):
     """ Abstract model for application+Key/value pair. """
     application = models.SlugField()
     key = models.SlugField()
-    value = models.TextField()
+    value = models.TextField(null=True, blank=True)
 
     class Meta:
         abstract = True # This model will then not be used to create any database table. Instead, when it is used as a base class for other models, its fields will be added to those of the child class.
