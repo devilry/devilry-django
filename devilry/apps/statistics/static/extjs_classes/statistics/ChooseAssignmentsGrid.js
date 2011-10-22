0764db0fd4dd2faf06915a06651bc6875c603bc0
Espen A. Kristiansen <post@espenak.net>
2011-10-22 17:45:40 +0200

    apps.statistics: Approved on subset working.

---
+++ devilry/apps/statistics/static/extjs_classes/statistics/ChooseAssignmentsGrid.js
@@ -0,0 +1,35 @@
+Ext.define('devilry.statistics.ChooseAssignmentsGrid', {
+    extend: 'Ext.grid.Panel',
+    alias: 'widget.statistics-chooseassignmentsgrid',
+    hideHeaders: true,
+    title: 'Choose assignments',
+
+    requires: [
+        'devilry.extjshelpers.GridSelectionModel'
+    ],
+    
+    constructor: function(config) {
+        this.initConfig(config);
+        this.callParent([config]);
+    },
+    
+    initComponent: function() {
+        this.selModel = Ext.create('devilry.extjshelpers.GridSelectionModel', {
+            checkOnly: false
+        });
+        Ext.apply(this, {
+            columns: [{
+                header: 'Long name',  dataIndex: 'long_name', flex: 1
+            }]
+        });
+        this.callParent(arguments);
+    },
+
+    getIdOfSelected: function() {
+        var assignment_ids = [];
+        Ext.each(this.getSelectionModel().getSelection(), function(assignmentRecord, index) {
+            assignment_ids.push(assignmentRecord.get('id'));
+        }, this);
+        return assignment_ids;
+    }
+});
--- devilry/apps/statistics/static/extjs_classes/statistics/sidebarplugin/qualifiesforexam/RequirePassingGradeOnSubset.js
+++ devilry/apps/statistics/static/extjs_classes/statistics/sidebarplugin/qualifiesforexam/RequirePassingGradeOnSubset.js
@@ -1,9 +1,23 @@
 Ext.define('devilry.statistics.sidebarplugin.qualifiesforexam.RequirePassingGradeOnSubset', {
-    extend: 'Ext.container.Container',
+    extend: 'devilry.statistics.sidebarplugin.qualifiesforexam.FilterBase',
+    layout: 'fit',
+    requires: [
+        'devilry.statistics.ChooseAssignmentsGrid'
+    ],
 
-    config: {
-        loader: undefined,
-        aggregatedStore: undefined,
-        labelname: undefined
+    initComponent: function() {
+        Ext.apply(this, {
+            items: [{
+                xtype: 'statistics-chooseassignmentsgrid',
+                margin: {bottom: 10},
+                store: this.loader.assignment_store
+            }, this.defaultButtonPanel]
+        });
+        this.callParent(arguments);
     },
+
+    filter: function(student) {
+        var assignment_ids = this.down('statistics-chooseassignmentsgrid').getIdOfSelected();
+        return student.passesAssignments(assignment_ids);
+    }
 });
