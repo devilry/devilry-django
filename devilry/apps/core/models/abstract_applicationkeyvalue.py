89934254b3ea8b8c9dce2606dc364b292b73122e
Espen A. Kristiansen <post@espenak.net>
2011-10-27 17:43:15 +0200

    apps.core.models.abstract_applicationkeyvalue: Added index to value field.

--- devilry/apps/core/models/abstract_applicationkeyvalue.py
+++ devilry/apps/core/models/abstract_applicationkeyvalue.py
@@ -5,7 +5,7 @@ class AbstractApplicationKeyValue(models.Model):
     """ Abstract model for application+Key/value pair. """
     application = models.CharField(max_length=300, db_index=True)
     key = models.CharField(max_length=300, db_index=True)
-    value = models.TextField(null=True, blank=True)
+    value = models.TextField(null=True, blank=True, db_index=True)
 
     class Meta:
         abstract = True # This model will then not be used to create any database table. Instead, when it is used as a base class for other models, its fields will be added to those of the child class.
