26686dcc11bc835cb438977a6c3dfce68ca9666f
Espen A. Kristiansen <post@espenak.net>
2011-10-27 18:04:06 +0200

    apps.administrator...activeperiods: Basic layout.

---
+++ devilry/apps/administrator/static/extjs_classes/administrator/activeperiods/Overview.js
@@ -0,0 +1,18 @@
+Ext.define('devilry.administrator.activeperiods.Overview', {
+    extend: 'Ext.panel.Panel',
+    alias: 'widget.activeperiods-overview',
+    
+    config: {
+    },
+    
+    constructor: function(config) {
+        this.initConfig(config);
+        this.callParent([config]);
+    },
+    
+    initComponent: function() {
+        Ext.apply(this, {
+        });
+        this.callParent(arguments);
+    }
+});
--- devilry/apps/administrator/static/extjs_classes/administrator/node/PrettyView.js
+++ devilry/apps/administrator/static/extjs_classes/administrator/node/PrettyView.js
@@ -2,6 +2,10 @@ Ext.define('devilry.administrator.node.PrettyView', {
     extend: 'devilry.administrator.PrettyView',
     alias: 'widget.administrator_nodeprettyview',
 
+    requires: [
+        'devilry.administrator.activeperiods.Overview'
+    ],
+
     bodyTpl: Ext.create('Ext.XTemplate',
         '<div class="section help">',
         '    <h1>What is a node?</h1>',
@@ -10,5 +14,37 @@ Ext.define('devilry.administrator.node.PrettyView', {
         '        Nodes are organised in a tree. This is very flexible, and can be used to emulate most administrative hierarchies.',
         '    </p>',
         '</div>'
-    )
+    ),
+
+    initComponent: function() {
+        Ext.apply(this, {
+            relatedButtons: [{
+                xtype: 'button',
+                scale: 'large',
+                text: 'Overview of active periods/semesters',
+                listeners: {
+                    scope: this,
+                    click: this._onActivePeriods
+                }
+            }]
+        });
+        this.callParent(arguments);
+    },
+
+    _onActivePeriods: function() {
+        var node = this.record;
+        var win = Ext.widget('window', {
+            title: Ext.String.format('Overview of active periods/semesters on {0}', node.get('long_name')),
+            modal: true,
+            width: 400,
+            height: 500,
+            maximizable: true,
+            layout: 'fit',
+            items: {
+                xtype: 'activeperiods-overview',
+                node: node
+            }
+        });
+        win.show();
+    }
 });
