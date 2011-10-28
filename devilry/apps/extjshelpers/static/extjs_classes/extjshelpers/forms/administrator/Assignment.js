329375d643e1b42584744c9bdf82b28c29483cd9
Espen A. Kristiansen <post@espenak.net>
2011-10-28 21:07:37 +0200

    apps.extjshelpers: Updated anonymous help.

--- devilry/apps/extjshelpers/static/extjs_classes/extjshelpers/forms/administrator/Assignment.js
+++ devilry/apps/extjshelpers/static/extjs_classes/extjshelpers/forms/administrator/Assignment.js
@@ -97,6 +97,6 @@ Ext.define('devilry.extjshelpers.forms.administrator.Assignment', {
         'You must choose the <strong>period</strong> where this period belongs. A period is normally a semester.',
         'The <strong>publishing time</strong> is the time when students will be able to start adding deliveries on the assignment.',
         'If you only use Devilry to give feedback, but students deliver through an alternative channel, change <strong>how students add deliveries</strong>. This can also be used if the students deliver through an alternative electronic system such as <em>email</em>.',
-        'If you check <strong>anonymous</strong>, examiners see a <em>candidate-id</em> instead of a username. A candidate-id must be set for each student.' //Normally students can see who their examiner is, however when <em>anonymous</em> is checked students do not get any information about their examiner(s).'
+        'If set <strong>anonymous</strong> to <em>yes</em>, examiners see a <em>candidate-id</em> instead of a username. A candidate-id must be set for each student.' //Normally students can see who their examiner is, however when <em>anonymous</em> is checked students do not get any information about their examiner(s).'
     ]
 });
