ea406a8383e4330e666387d2d270af2b7ca943b9
Espen A. Kristiansen <post@espenak.net>
2011-11-03 18:48:12 +0100

    apps.extjshelpers.modelintegration: Added support for OneToMany.

--- devilry/apps/extjshelpers/modelintegration.py
+++ devilry/apps/extjshelpers/modelintegration.py
@@ -1,6 +1,9 @@
 import json
 from django.db.models import fields
 
+from devilry.simplified import OneToMany
+
+
 
 def _djangofield_to_extjstype(field):
     """ Convert django field to extjs  field type. """
@@ -33,7 +36,9 @@ def _recurse_get_fkfield(modelcls, path):
 def _iter_fields(simplifiedcls, result_fieldgroups):
     meta = simplifiedcls._meta
     for fieldname in meta.resultfields.aslist(result_fieldgroups):
-        if "__" in fieldname:
+        if isinstance(fieldname, OneToMany):
+            yield fieldname.related_field, dict(type='auto')
+        elif "__" in fieldname:
             path = fieldname.split('__')
             yield fieldname, _recurse_get_fkfield(meta.model, path)
         else:
