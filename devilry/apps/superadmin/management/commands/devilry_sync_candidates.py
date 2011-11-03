9776ec27127541c1779c333dbc5e3720ef1381b6
Espen A. Kristiansen <post@espenak.net>
2011-11-03 20:14:59 +0100

    apps.superadmin...devilry_sync_candidates: Created a script to sync candidates with User.

---
+++ devilry/apps/superadmin/management/commands/devilry_sync_candidates.py
@@ -0,0 +1,22 @@
+from django.db import transaction
+from django.core.management.base import NoArgsCommand
+
+from devilry.apps.core.models import Candidate
+
+
+class Command(NoArgsCommand):
+    help = "Sync the cached fields in Candidate with the actual data from User."
+
+    def handle_noargs(self, **options):
+        verbosity = int(options.get('verbosity', '1'))
+
+        updates = 0
+        with transaction.commit_manually():
+            for candidate in Candidate.objects.all():
+                candidate.save()
+                if verbosity > 1:
+                    print 'Updated {0}'.format(candidate)
+                updates += 1
+            transaction.commit()
+        if verbosity > 0:
+            print 'Successfully updated {0} candidates.'.format(updates)
