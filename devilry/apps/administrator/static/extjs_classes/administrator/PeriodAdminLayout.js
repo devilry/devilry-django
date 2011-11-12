793f7960be2b0460ba90b3a45120f7d4f788d5d5
Espen A. Kristiansen <post@espenak.net>
2011-11-12 11:48:17 +0100

    apps.administrator...period: Turned the layout into a widget.

---
+++ devilry/apps/administrator/static/extjs_classes/administrator/PeriodAdminLayout.js
@@ -0,0 +1,80 @@
+Ext.define('devilry.administrator.PeriodAdminLayout', {
+    extend: 'Ext.container.Container',
+    alias: 'widget.administrator-periodadminlayout',
+
+    requires: [
+        'devilry.administrator.period.PrettyView',
+        'devilry.extjshelpers.RestfulSimplifiedEditPanel',
+        'devilry.extjshelpers.forms.administrator.Period',
+        'devilry.statistics.PeriodAdminLayout'
+    ],
+    
+    config: {
+        periodid: undefined
+    },
+
+    periodmodel_name: 'devilry.apps.administrator.simplified.SimplifiedPeriod',
+    
+    constructor: function(config) {
+        this.initConfig(config);
+        this.callParent([config]);
+    },
+    
+    initComponent: function() {
+        Ext.apply(this, {
+            layout: {
+                type: 'vbox',
+                align: 'stretch'
+            },
+            items: [this.heading = Ext.ComponentManager.create({
+                xtype: 'component',
+                data: {},
+                cls: 'section treeheading',
+                tpl: [
+                    '<h1>{long_name}</h1>',
+                    '<h2 class="endoflist">{parentnode__long_name}</h2>'
+                ]
+            }), {
+                xtype: 'tabpanel',
+                flex: 1,
+                items: [{
+                    title: 'Students',
+                    xtype: 'statistics-periodadminlayout',
+                    periodid: this.periodid,
+                    hidesidebar: false,
+                    defaultViewClsname: 'devilry.statistics.dataview.MinimalGridView'
+                    //defaultViewClsname: 'devilry.statistics.dataview.FullGridView'
+                }, this.prettyview = Ext.widget('administrator_periodprettyview', {
+                    title: 'Administer',
+                    modelname: this.periodmodel_name,
+                    objectid: this.periodid,
+                    dashboardUrl: DASHBOARD_URL,
+                    listeners: {
+                        scope: this,
+                        loadmodel: this._onLoadModel,
+                        edit: this._onEdit
+                    }
+                })]
+            }]
+        });
+        this.callParent(arguments);
+    },
+
+    _onLoadModel: function(record) {
+        this.heading.update(record.data);
+    },
+
+    _onEdit: function(record, button) {
+        var editpanel = Ext.ComponentManager.create({
+            xtype: 'restfulsimplified_editpanel',
+            model: this.periodmodel_name,
+            editform: Ext.widget('administrator_periodform'),
+            record: record
+        });
+        var editwindow = Ext.create('devilry.administrator.DefaultEditWindow', {
+            editpanel: editpanel,
+            prettyview: this.prettyview
+        });
+        editwindow.show();
+    }
+});
--- devilry/apps/administrator/templates/administrator/period.django.js
+++ devilry/apps/administrator/templates/administrator/period.django.js
@@ -3,10 +3,9 @@
 
 {% block imports %}
     {{ block.super }}
-    Ext.require('devilry.administrator.period.PrettyView');
-    Ext.require('devilry.extjshelpers.RestfulSimplifiedEditPanel');
-    Ext.require('devilry.extjshelpers.forms.administrator.Period');
-    Ext.require('devilry.statistics.PeriodAdminLayout');
+    Ext.require([
+        'devilry.administrator.PeriodAdminLayout'
+    ]);
 {% endblock %}
 
 
@@ -24,49 +23,6 @@
 
 {% block onready %}
     {{ block.super }}
-    var prettyview = Ext.create('devilry.administrator.period.PrettyView', {
-        flex: 1,
-        title: 'Administer',
-        modelname: {{ restfulapi.RestfulSimplifiedPeriod|extjs_modelname }},
-        objectid: {{ objectid }},
-        dashboardUrl: DASHBOARD_URL
-    });
-
-    var heading = Ext.ComponentManager.create({
-        xtype: 'component',
-        data: {},
-        cls: 'section treeheading',
-        tpl: [
-            '<h1>{long_name}</h1>',
-            '<h2 class="endoflist">{parentnode__long_name}</h2>'
-        ]
-    });
-
-    prettyview.addListener('loadmodel', function(record) {
-        heading.update(record.data);
-    });
-
-    prettyview.addListener('edit', function(record, button) {
-        var editpanel = Ext.ComponentManager.create({
-            xtype: 'restfulsimplified_editpanel',
-            model: {{ restfulapi.RestfulSimplifiedPeriod|extjs_modelname }},
-            editform: Ext.widget('administrator_periodform'),
-            record: record
-        });
-        var editwindow = Ext.create('devilry.administrator.DefaultEditWindow', {
-            editpanel: editpanel,
-            prettyview: prettyview,
-            listeners: {
-                scope: this,
-                close: function() {
-                    button.toggle(false);
-                }
-            }
-        });
-        editwindow.show();
-        editwindow.alignTo(button, 'br', [-editwindow.getWidth(), 0]);
-    });
-
 
     Ext.getBody().unmask();
     Ext.create('Ext.container.Viewport', {
@@ -81,24 +37,9 @@
             xtype: 'pagefooter'
         }, {
             region: 'center',
-            xtype: 'container',
-            padding: {left: 20, right: 20},
-            layout: {
-                type: 'vbox',
-                align: 'stretch'
-            },
-            items: [heading, {
-                xtype: 'tabpanel',
-                flex: 1,
-                items: [{
-                    title: 'Students',
-                    xtype: 'statistics-periodadminlayout',
-                    periodid: {{ objectid }},
-                    hidesidebar: false,
-                    defaultViewClsname: 'devilry.statistics.dataview.MinimalGridView'
-                    //defaultViewClsname: 'devilry.statistics.dataview.FullGridView'
-                }, prettyview]
-            }]
+            xtype: 'administrator-periodadminlayout',
+            periodid: {{ objectid }},
+            padding: {left: 20, right: 20}
         }]
     });
 {% endblock %}
