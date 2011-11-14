9d4cd2f1f7929d8fb31baa435abd21571ab025b2
Espen A. Kristiansen <post@espenak.net>
2011-11-14 02:10:14 +0100

    apps.administrator...node.PrettyView: Help.

--- devilry/apps/administrator/static/extjs_classes/administrator/node/PrettyView.js
+++ devilry/apps/administrator/static/extjs_classes/administrator/node/PrettyView.js
@@ -2,10 +2,6 @@ Ext.define('devilry.administrator.node.PrettyView', {
     extend: 'devilry.administrator.PrettyView',
     alias: 'widget.administrator_nodeprettyview',
 
-    requires: [
-        'devilry.statistics.activeperiods.Overview'
-    ],
-
     bodyTpl: Ext.create('Ext.XTemplate',
         '<div class="section help">',
         '    <h1>What is a node?</h1>',
@@ -13,49 +9,21 @@ Ext.define('devilry.administrator.node.PrettyView', {
         '        A Node is a place to organise top-level administrators (administrators responsible for more than one subject).',
         '        Nodes are organised in a tree. This is very flexible, and can be used to emulate most administrative hierarchies.',
         '    </p>',
+        '   <h1>Usage</h1>',
+        '   <p>Use the <span class="menuref">Active periods/semesters</span> to get an overview over the currently running periods, and if the administrators have registered which students qualify for final exams.</p>',
+        '   <p>The <span class="menuref">Direct childnodes</span> and <span class="menuref">Subjects</span> tabs are lists of everything organized directly below this node.</p>',
         '</div>'
     ),
 
     initComponent: function() {
-        Ext.apply(this, {
-            relatedButtons: [{
-                xtype: 'button',
-                scale: 'large',
-                text: 'Active periods/semesters',
-                listeners: {
-                    scope: this,
-                    click: this._onActivePeriods
-                }
-            }]
-        });
-
         if(this.record) {
             this._onLoadRecord();
         } else {
             this.on('loadmodel', this._onLoadRecord, this);
         }
-
         this.callParent(arguments);
     },
 
     _onLoadRecord: function() {
-        //this._onActivePeriods();
-    },
-
-    _onActivePeriods: function() {
-        var node = this.record;
-        var win = Ext.widget('window', {
-            title: Ext.String.format('Active periods/semesters on {0}', node.get('long_name')),
-            modal: true,
-            width: 800,
-            height: 500,
-            maximizable: true,
-            layout: 'fit',
-            items: {
-                xtype: 'activeperiods-overview',
-                node: node
-            }
-        });
-        win.show();
     }
 });
