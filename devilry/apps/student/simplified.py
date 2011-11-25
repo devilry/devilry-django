c9945d2b8df26fc5ae80698f4e693a9c941c27c1
Espen A. Kristiansen <post@espenak.net>
2011-11-25 13:51:17 +0100

    apps.student.simplified...Period: Use relatedstudent instead of candidate to authorize.

--- devilry/apps/student/simplified.py
+++ devilry/apps/student/simplified.py
@@ -145,11 +145,34 @@ class SimplifiedAssignment(PublishedWhereIsCandidateMixin):
 
 
 @simplified_modelapi
-class SimplifiedPeriod(PublishedWhereIsCandidateMixin):
+class SimplifiedPeriod(SimplifiedModelApi):
     """ Simplified wrapper for :class:`devilry.apps.core.models.Period`. """
     class Meta(SimplifiedPeriodMetaMixin):
         """ Defines what methods a Student can use on a Period object using the Simplified API """
         methods = ['search', 'read']
+
+    @classmethod
+    def create_searchqryset(cls, user, **kwargs):
+        """ Returns all objects of this type that matches arguments
+        given in ``\*\*kwargs`` where ``user`` is a related student.
+
+        :param user: A django user object.
+        :param \*\*kwargs: A dict containing search-parameters.
+        :rtype: a django queryset
+        """
+        return cls._meta.model.where_is_relatedstudent(user)
+
+    @classmethod
+    def read_authorize(cls, user, obj):
+        """ Checks if the given ``user`` is an student in the given
+        ``obj``, and raises ``PermissionDenied`` if not.
+
+        :param user: A django user object.
+        :param obj: An object of the type this method is used in.
+        :throws PermissionDenied:
+        """
+        if not cls._meta.model.where_is_relatedstudent(user).filter(id=obj.id):
+            raise PermissionDenied()
 
 
 @simplified_modelapi
