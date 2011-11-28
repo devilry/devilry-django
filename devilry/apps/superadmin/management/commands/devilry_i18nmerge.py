310e8bc7d7b9bca99706864179319f800126cd4d
Espen A. Kristiansen <post@espenak.net>
2011-11-28 21:37:59 +0100

    apps.superadmin...devilry_i18nmerge: Code decoupling (#223).

--- devilry/apps/superadmin/management/commands/devilry_i18nmerge.py
+++ devilry/apps/superadmin/management/commands/devilry_i18nmerge.py
@@ -9,15 +9,9 @@ from django.utils.importlib import import_module
 from django.conf import settings
 
 
-class I18nMerger(object):
-    def __init__(self):
-        self.result = {'messages': {}}
-        i18ndirs = self._find_i18ndirs_in_installedapps()
-        for i18ndir in i18ndirs:
-            self._parse_messages(i18ndir)
-
-    def _find_i18ndirs_in_installedapps(self):
-        i18ndirs = set()
+class I18nBase(object):
+    def find_i18ndirs_in_installedapps(self):
+        i18ndirs = []
         for app in settings.INSTALLED_APPS:
             if not app.startswith('django.'):
                 appname = app.split('.')[-1]
@@ -26,24 +20,48 @@ class I18nMerger(object):
                     moddir = dirname(mod.__file__)
                     i18ndir = join(moddir, 'static', appname, 'i18n')
                     if exists(i18ndir):
-                        i18ndirs.add(i18ndir)
+                        i18ndirs.append([appname, i18ndir])
         return i18ndirs
 
-    def _parse_messages(self, i18ndir):
-        """ Add all messages.yaml to results. """
+    def load_messagefile(self, i18ndir, langcode=None):
+        filename = 'messages'
+        if langcode:
+            filename += '_' + langcode
+        filename += '.yaml'
+        return yaml.load(open(join(i18ndir, filename)).read())
+
+    def load_default_messagefile(self, i18ndir):
+        return self.load_messagefile(i18ndir)
+
+
+class I18nLoader(I18nBase):
+    def __init__(self):
+        self.data = {}
+        self.i18ndirs = self.find_i18ndirs_in_installedapps()
+        self._load_all_defaultmessages()
+
+    def _load_all_defaultmessages(self):
+        for appname, i18ndir in self.i18ndirs:
+            self._load_defaultmessages(appname, i18ndir)
+
+    def iterdata(self):
+        for appname, i18ndir in self.i18ndirs:
+            yield appname, i18ndir, self.data[appname]
+
+    def _load_defaultmessages(self, appname, i18ndir):
         messagesfile = join(i18ndir, 'messages.yaml')
         if not exists(messagesfile):
             raise CommandError('i18n directory, {i18ndir}, does not contain the required messages.yaml.'.format(**vars()))
-        default_messages = self._add_default_messages_to_results(messagesfile)
-        self._parse_all_subsets(i18ndir, default_messages)
+        self.data[appname] = self.load_default_messagefile(i18ndir)
 
-    def _add_default_messages_to_results(self, messagesfile):
-        default_messages = yaml.load(open(messagesfile).read())
-        for key in default_messages:
-            if key in self.result['messages']:
-                raise CommandError("{messagesfile} contains property, '{key}', that is present in another 'messages.yaml'.".format(**vars()))
-        self.result['messages'].update(default_messages)
-        return default_messages
+
+class I18nMerge(I18nBase):
+    """ Merge all translations into a single map that can be exported as a single file. """
+    def __init__(self, loader):
+        self.result = {'messages': {}}
+        for appname, i18ndir, default_messages in loader.iterdata():
+            self.result['messages'].update(default_messages)
+            self._parse_all_subsets(i18ndir, default_messages)
 
     def _parse_all_subsets(self, i18ndir, default_messages):
         """ Add all subsets (I.E: messages_en.yaml, messages_nb.yaml, ...) to results. """
@@ -53,7 +71,7 @@ class I18nMerger(object):
                 setname = match.groupdict()['setname']
                 self._add_to_set(setname, join(i18ndir, filename), default_messages)
 
-    def _add_to_set(self, setname, messagesfile, default_messages):
+    def _add_to_set(self, setname, messagesfile, default_messages=None):
         messages = yaml.load(open(messagesfile).read())
         for key in messages:
             if not key in default_messages:
@@ -75,4 +93,5 @@ class Command(BaseCommand):
         verbosity = int(options.get('verbosity', '1'))
         extjshelpers_dir =  dirname(import_module('devilry.apps.extjshelpers').__file__)
         outdir = join(extjshelpers_dir, 'static')
-        I18nMerger().print_result()
+        loader = I18nLoader()
+        I18nMerge(loader).print_result()
