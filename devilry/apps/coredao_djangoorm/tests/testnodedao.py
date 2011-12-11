925adc1aeebdb6b26f3bb2380334b835cd9eaaa7
Espen A. Kristiansen <post@espenak.net>
2011-12-11 03:59:58 +0100

    apps.coredao_djangoorm: Tests.

--- devilry/apps/coredao_djangoorm/tests/testnodedao.py
+++ devilry/apps/coredao_djangoorm/tests/testnodedao.py
@@ -13,21 +13,33 @@ class TestNodeDao(TestCase):
         self.testhelper.create_superuser("superuser")
         self.nodedao = NodeDao()
 
+    def test_superuser(self):
+        # The only difference for superuser is auth, so the normal user tests cover everything but these sanity tests
+        self.nodedao.read(self.testhelper.superuser, self.testhelper.rootnode.id)
+        self.nodedao.read(self.testhelper.superuser, self.testhelper.rootnode_subnode_subsubnode.id)
+
+    def test_create_normaluser(self):
+        with self.assertRaises(PermissionDeniedError):
+            self.nodedao.create(self.testhelper.normaluser,
+                                'tst', 'Test', parentnode_id=self.testhelper.rootnode.id)
+
+        nodedct = self.nodedao.create(self.testhelper.normaluser,
+                                      'tst', 'Test', parentnode_id=self.testhelper.rootnode_subnode.id)
+        del nodedct['id']
+        self.assertEquals(nodedct, dict(long_name='Test', short_name='tst',
+                                        parentnode_id=self.testhelper.rootnode_subnode.id))
+
     def test_read_normaluser(self):
         self.assertRaises(PermissionDeniedError, self.nodedao.read,
                           self.testhelper.normaluser, self.testhelper.rootnode.id)
         self.nodedao.read(self.testhelper.normaluser, self.testhelper.rootnode_subnode.id)
         self.nodedao.read(self.testhelper.normaluser, self.testhelper.rootnode_subnode_subsubnode.id)
 
-    def test_read_superuser(self):
-        self.nodedao.read(self.testhelper.superuser, self.testhelper.rootnode.id)
-        self.nodedao.read(self.testhelper.superuser, self.testhelper.rootnode_subnode_subsubnode.id)
-
     def test_update_normaluser(self):
-        self.assertRaises(PermissionDeniedError, self.nodedao.update,
-                          self.testhelper.normaluser,
-                          self.testhelper.rootnode_subnode.id,
-                          'tst', 'Test', parentnode_id=self.testhelper.rootnode.id)
+        with self.assertRaises(PermissionDeniedError):
+            self.nodedao.update(self.testhelper.normaluser,
+                                self.testhelper.rootnode_subnode.id,
+                                'tst', 'Test', parentnode_id=self.testhelper.rootnode.id)
 
         nodedct = self.nodedao.update(self.testhelper.normaluser,
                                       self.testhelper.rootnode_subnode_subsubnode.id,
@@ -35,3 +47,8 @@ class TestNodeDao(TestCase):
         self.assertEquals(nodedct, dict(id=self.testhelper.rootnode_subnode_subsubnode.id,
                                         long_name='Test', short_name='tst',
                                         parentnode_id=self.testhelper.rootnode_subnode.id))
+
+    def test_delete_normaluser(self):
+        with self.assertRaises(PermissionDeniedError):
+            self.nodedao.delete(self.testhelper.normaluser, self.testhelper.rootnode_subnode.id)
+        self.nodedao.delete(self.testhelper.normaluser, self.testhelper.rootnode_subnode_subsubnode.id)
