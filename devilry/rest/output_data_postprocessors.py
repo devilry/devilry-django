589053023dd3209b1a6b19139203b9ade39094bc
Espen A. Kristiansen <post@espenak.net>
2011-12-11 16:19:48 +0100

    rest: Extjs header check.

--- devilry/rest/output_data_postprocessors.py
+++ devilry/rest/output_data_postprocessors.py
@@ -1,5 +1,5 @@
 def extjs(request, output_data, successful):
-    if '_devilry_extjs' in request.GET:
+    if '_devilry_extjs' in request.GET or 'HTTP_X_DEVILRY_USE_EXTJS' in request.META:
         if isinstance(output_data, dict):
             output_data['successful'] = successful
         return True, output_data
