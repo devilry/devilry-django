02770e5b46e6eaa709975c911024f55e07f03396
Espen A. Kristiansen <post@espenak.net>
2011-12-11 14:00:12 +0100

    apps.corerest.extjs_classes...Node: Usable model.

---
+++ devilry/apps/corerest/static/extjs_classes/corerest/administrator/Node.js
@@ -0,0 +1,23 @@
+Ext.define('devilry.corerest.administrator.Node', {
+    extend:'Ext.data.Model',
+    idProperty:'id',
+    fields:[
+        {"type":"int", "name":"id"},
+        {"type":"int", "name":"parentnode_id"},
+        {"type":"string", "name":"short_name"},
+        {"type":"string", "name":"long_name"}
+    ],
+    proxy: {
+        type: 'rest',
+        url: DevilrySettings.DEVILRY_URLPATH_PREFIX + '/corerest/administrator/node/',
+        extraParams: {
+            '_devilry_extjs': 'yes',
+            '_devilry_accept': 'application/json'
+        },
+        reader: {
+            type: 'json',
+            root: 'items',
+            record: 'item'
+        }
+    }
+});
