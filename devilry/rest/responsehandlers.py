1527d199acab5c93a5a59083e22f6333dfec7c42
Espen A. Kristiansen <post@espenak.net>
2011-12-11 13:37:15 +0100

    rest: Fixed minor bug.

--- devilry/rest/responsehandlers.py
+++ devilry/rest/responsehandlers.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
 
 def stricthttp(request, restapimethodname, output_content_type, encoded_output):
+    print restapimethodname
     if restapimethodname == "create":
         status = 201
     else:
         status = 200
-    return HttpResponse(encoded_output, content_type=output_content_type, status=201)
+    return HttpResponse(encoded_output, content_type=output_content_type, status=status)
