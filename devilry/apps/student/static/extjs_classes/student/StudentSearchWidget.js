e91d73bd456c5ae110b23ef19e39014752757a3c
Espen A. Kristiansen <post@espenak.net>
2012-01-20 23:54:36 +0100

    apps.student...StudentSearchWidget: Added missing template.

--- devilry/apps/student/static/extjs_classes/student/StudentSearchWidget.js
+++ devilry/apps/student/static/extjs_classes/student/StudentSearchWidget.js
@@ -12,6 +12,27 @@ Ext.define('devilry.student.StudentSearchWidget', {
      */
     urlPrefix: '',
 
+    deadlineRowTpl: [
+        '<div class="section popuplistitem">',
+        '   <p class="path">',
+        '{assignment_group__parentnode__parentnode__parentnode__short_name}.',
+        '{assignment_group__parentnode__parentnode__short_name}.',
+        '{assignment_group__parentnode__short_name}',
+        '   </p>',
+        '   <tpl if="!is_student">',
+        '       <ul class="useridlist"><tpl for="assignment_group__candidates__identifier"><li>{.}</li></tpl></ul>',
+        '   </tpl>',
+        '   <h1>{deadline:date}</h1>',
+        '   <p><tpl if="assignment_group__name"> &ndash; {assignment_group__name}</tpl><p>',
+        '   <div class="section dl_valueimportant">',
+        '       <div class="section">',
+        '           <h1>Deliveries</h1>',
+        '           {number_of_deliveries}',
+        '       </div>',
+        '   </div>',
+        '</div>'
+    ],
+
     initComponent: function() {
         Ext.apply(this, {
             searchResultItems: [{
