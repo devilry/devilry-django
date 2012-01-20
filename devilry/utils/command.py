a080bd7b30e347d5bc34bb75bd5c7ad52ed2b10a
Espen A. Kristiansen <post@espenak.net>
2012-01-20 19:00:21 +0100

    utils.command: Added setup_logging.

---
+++ devilry/utils/command.py
@@ -0,0 +1,17 @@
+"""
+Utility functions for management commands.
+"""
+
+import logging
+
+
+def setup_logging(verbosity):
+    if verbosity < 1:
+        loglevel = logging.ERROR
+    elif verbosity == 1:
+        loglevel = logging.WARNING
+    elif verbosity == 2:
+        loglevel = logging.INFO
+    else:
+        loglevel = logging.DEBUG
+    logging.basicConfig(level=loglevel)
