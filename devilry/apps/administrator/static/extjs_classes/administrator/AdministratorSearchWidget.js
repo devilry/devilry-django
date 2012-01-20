aaeea73c52a101761d2bcace04292350c0e21b66
Espen A. Kristiansen <post@espenak.net>
2012-01-20 23:17:30 +0100

    apps.administrator...AdministratorSearchWidget: Use DashboardSearchWidget.

--- devilry/apps/administrator/static/extjs_classes/administrator/AdministratorSearchWidget.js
+++ devilry/apps/administrator/static/extjs_classes/administrator/AdministratorSearchWidget.js
@@ -4,10 +4,7 @@
  * the current item.
  * */
 Ext.define('devilry.administrator.AdministratorSearchWidget', {
-    extend: 'devilry.extjshelpers.searchwidget.SearchWidget',
-    requires: [
-        'devilry.extjshelpers.searchwidget.FilterConfigDefaults',
-    ],
+    extend: 'devilry.extjshelpers.searchwidget.DashboardSearchWidget',
 
     nodeRowTpl: [
         '<div class="section popuplistitem">',
@@ -25,50 +22,6 @@ Ext.define('devilry.administrator.AdministratorSearchWidget', {
         '<div class="section popuplistitem">',
         '    <p class="path">{parentnode__short_name}</p>',
         '    <h1>{long_name:ellipsis(40)}</h1>',
-        '</div>'
-    ],
-
-    assignmentRowTpl: [
-        '<div class="section popuplistitem">',
-        '    <p class="path">',
-        '{parentnode__parentnode__short_name}.',
-        '{parentnode__short_name}',
-        '   </p>',
-        '   <h1>{long_name:ellipsis(40)}</h1>',
-        '</div>'
-    ],
-
-    assignmentgroupRowTpl: [
-        '<div class="section popuplistitem">',
-        '   <p class="path">',
-        '{parentnode__parentnode__parentnode__short_name:ellipsis(60)}.',
-        '{parentnode__parentnode__short_name:ellipsis(60)}.',
-        '{parentnode__short_name:ellipsis(60)}',
-        '   </p>',
-        '   <tpl if="!is_student">',
-        '       <h1><ul class="useridlist"><tpl for="candidates__identifier"><li>{.}</li></tpl></ul></h1>',
-        '   </tpl>',
-        '   <p><tpl if="name">{name}</tpl><p>',
-        '</div>'
-    ],
-
-    deliveryRowTpl: [
-        '<div class="section popuplistitem">',
-        '   <p class="path">',
-        '{deadline__assignment_group__parentnode__parentnode__parentnode__short_name}.',
-        '{deadline__assignment_group__parentnode__parentnode__short_name}.',
-        '{deadline__assignment_group__parentnode__short_name}',
-        '   </p>',
-        '   <tpl if="!is_student">',
-        '       <ul class="useridlist"><tpl for="deadline__assignment_group__candidates__identifier"><li>{.}</li></tpl></ul>',
-        '   </tpl>',
-        '   <tpl if="deadline__assignment_group__name"> &ndash; {deadline__assignment_group__name}</tpl>',
-        '   <div class="section dl_valueimportant">',
-        '      <div class="section">',
-        '          <h1>Delivery number</h1>',
-        '          {number}',
-        '      </div>',
-        '   </div>',
         '</div>'
     ],
 
