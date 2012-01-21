44d87a96e92bda9bc01caa3886be028ad87a4277
Espen A. Kristiansen <post@espenak.net>
2012-01-21 13:05:40 +0100

    apps.examiner...main.django.js: Remove all extjs_model.

--- devilry/apps/examiner/templates/examiner/main.django.js
+++ devilry/apps/examiner/templates/examiner/main.django.js
@@ -14,23 +14,20 @@
 {% block appjs %}
     {{ block.super }}
 
-    var dashboard_assignment_model = {{ restfulapi.RestfulSimplifiedAssignment|extjs_model:"subject,period" }}
-    var dashboard_delivery_model = {{ restfulapi.RestfulSimplifiedDelivery|extjs_model:"subject,period,assignment,assignment_group,candidates" }}
-    var dashboard_feedback_model = {{ restfulapi.RestfulSimplifiedStaticFeedback|extjs_model:"subject,period,assignment,assignment_group,candidates" }}
     function createActiveAssignmentsView() {
         var activeAssignmentsView = Ext.create('devilry.examiner.ActiveAssignmentsView', {
-            model: dashboard_assignment_model,
+            model: Ext.ModelManager.getModel('devilry.apps.examiner.simplified.SimplifiedAssignment'),
             dashboard_url: DASHBOARD_URL,
             minHeight: 140,
             flex: 1
         });
         var recentDeliveries = Ext.create('devilry.examiner.RecentDeliveriesView', {
-            model: dashboard_delivery_model,
+            model: Ext.ModelManager.getModel('devilry.apps.examiner.simplified.SimplifiedDelivery'),
             dashboard_url: DASHBOARD_URL,
             flex: 1
         });
         var recentFeedbacks = Ext.create('devilry.examiner.RecentFeedbacksView', {
-            model: dashboard_feedback_model,
+            model: Ext.ModelManager.getModel('devilry.apps.examiner.simplified.SimplifiedStaticFeedback'),
             dashboard_url: DASHBOARD_URL,
             flex: 1
         });
@@ -48,38 +45,13 @@
     }
 
 
-    {{ restfulapi.RestfulSimplifiedAssignmentGroup|extjs_model:";PermissionCheck" }};
-    var assignmentgroup_permcheckstore = {{ restfulapi.RestfulSimplifiedAssignmentGroup|extjs_store:"PermissionCheck" }};
-    assignmentgroup_permcheckstore.pageSize = 1;
 
 {% endblock %}
 
 
 {% block onready %}
     {{ block.super }}
-
-
-    function createPermissionChecker() {
-    }
-
-    var permchecker = Ext.create('devilry.extjshelpers.PermissionChecker', {
-        stores: [assignmentgroup_permcheckstore],
-        emptyHtml: '<div class="section info-small extravisible-small"><h1>{{ DEVILRY_EXAMINER_NO_PERMISSION_MSG.title }}</h1>' +
-            '<p>{{ DEVILRY_EXAMINER_NO_PERMISSION_MSG.body }}</p></div>',
-        listeners: {
-            allLoaded: function() {
-                Ext.getBody().unmask();
-            },
-            hasPermission: function() {
-                //Ext.getDom('has-permission-section').style.display = 'block';
-                //Ext.getDom('searchsection').style.display = 'block';
-                //createActiveAssignmentsView();
-                searchwidget.show();
-                createActiveAssignmentsView();
-            }
-        }
-    });
-    assignmentgroup_permcheckstore.load();
+    Ext.getBody().unmask();
 
     Ext.create('Ext.container.Viewport', {
         layout: 'border',
@@ -100,7 +72,7 @@
                 type: 'vbox',
                 align: 'stretch'
             },
-            items: [searchwidget, permchecker, {xtype:'box', height: 20}, {
+            items: [searchwidget, {xtype:'box', height: 20}, {
                 xtype: 'panel',
                 flex: 1,
                 autoScroll: true,
@@ -113,4 +85,6 @@
             }]
         }]
     });
+    searchwidget.show();
+    createActiveAssignmentsView();
 {% endblock %}
