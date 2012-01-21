ad6f5bfd8583b119f15c3828abc5c2c915dcecd8
Espen A. Kristiansen <post@espenak.net>
2012-01-21 16:57:55 +0100

    apps.administrator....assignment.django.js: Removed all unneded code.

--- devilry/apps/administrator/templates/administrator/assignment.django.js
+++ devilry/apps/administrator/templates/administrator/assignment.django.js
@@ -9,22 +9,6 @@
 {% endblock %}
 
 
-{% block appjs %}
-    {{ block.super }}
-    {{ restfulapi.RestfulSimplifiedDelivery|extjs_model }}
-    {{ restfulapi.RestfulSimplifiedDeadline|extjs_model }}
-    {{ restfulapi.RestfulSimplifiedRelatedStudent|extjs_model }}
-    {{ restfulapi.RestfulSimplifiedRelatedExaminer|extjs_model }}
-    {{ restfulapi.RestfulSimplifiedPeriod|extjs_model:"subject" }}
-    {{ restfulapi.RestfulSimplifiedAssignment|extjs_model:"period,subject,admins" }};
-    {{ restfulapi.RestfulSimplifiedAssignmentGroup|extjs_model:"feedback,feedbackdelivery,assignment,period,subject,users,tags,students_full_name" }};
-    {{ gradeeditors.RestfulSimplifiedConfig|extjs_model }};
-    var assignmentgroupstore = {{ restfulapi.RestfulSimplifiedAssignmentGroup|extjs_store }};
-    {{ gradeeditors.RestfulSimplifiedFeedbackDraft|extjs_model }};
-
-    {{ restfulapi.RestfulSimplifiedAssignmentGroup|extjs_model:"assignment,users,tags;Import" }};
-{% endblock %}
-
 {% block onready %}
     {{ block.super }}
     Ext.getBody().unmask();
@@ -42,8 +26,8 @@
         }, {
             region: 'center',
             xtype: 'administrator-assignmentlayout',
-            assignmentgroupstore: assignmentgroupstore,
             assignmentid: {{ objectid }},
+            dashboardUrl: DASHBOARD_URL,
             padding: {left: 20, right: 20}
         }]
     });
