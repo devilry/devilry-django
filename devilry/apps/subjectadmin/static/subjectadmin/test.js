6c1286b1f3c6a74bbc6fa32fc2ab51d55780edd5
Espen A. Kristiansen <post@espenak.net>
2012-01-23 00:38:48 +0100

    apps.guibase: Enable loader namespaces.

--- devilry/apps/subjectadmin/static/subjectadmin/test.js
+++ devilry/apps/subjectadmin/static/subjectadmin/test.js
@@ -2,9 +2,25 @@ Ext.application({
     name: 'subjectadmin',
     appFolder: DevilrySettings.DEVILRY_STATIC_URL + '/subjectadmin/app',
 
+    requires: [
+        'Ext.ux.Router'
+    ],
+
     controllers: [
         'ShortcutsTestMock',
     ],
+
+    /*
+     * Here is where routes are defined.
+     *  key:    URL matcher
+     *  value:  controller + '#' + action to be invoked
+     */
+    routes: {
+        '/': 'home#index',
+        'users': 'users#list',
+        'users/:id/edit': 'users#edit'
+    },
+
 
     launch: function() {
         Ext.create('Ext.container.Viewport', {
