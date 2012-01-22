5bfc197b9c96643dc7b52ea1cea5373dcc84a8f8
Espen A. Kristiansen <post@espenak.net>
2012-01-22 23:14:16 +0100

    apps.subjectadmin: List populated tests.

--- devilry/apps/guibase/seleniumhelpers.py
+++ devilry/apps/guibase/seleniumhelpers.py
@@ -1,21 +1,28 @@
+from selenium.webdriver.support.ui import WebDriverWait
 from unittest import TestCase
 from selenium import webdriver
 
 
 class SeleniumMixin(object):
-    def getBrowser(self):
+    def getDriver(self):
         return getattr(webdriver, self.seleniumbrowser)()
 
     def getJsAppUrl(self, urlprefix):
         return 'http://localhost:8000/{0}/test'.format(urlprefix)
+
+    def driverWaitForCssSelector(self, driver, cssselector, timeout=10):
+        WebDriverWait(driver, timeout).until(lambda driver: driver.find_elements_by_css_selector(cssselector))
 
 
 class SeleniumTestCase(TestCase, SeleniumMixin):
     appname = None
 
     def setUp(self):
-        self.browser = self.getBrowser()
-        self.browser.get(self.getJsAppUrl(self.appname))
+        self.driver = self.getDriver()
+        self.driver.get(self.getJsAppUrl(self.appname))
 
     def tearDown(self):
-        self.browser.close()
+        self.driver.close()
+
+    def waitForCssSelector(self, cssselector, timeout=10):
+        return self.driverWaitForCssSelector(self.driver, cssselector, timeout)
--- devilry/apps/subjectadmin/seleniumtests/shortcuts.py
+++ devilry/apps/subjectadmin/seleniumtests/shortcuts.py
@@ -4,11 +4,9 @@ from devilry.apps.guibase.seleniumhelpers import SeleniumTestCase
 class TestSelenium(SeleniumTestCase):
     appname = 'subjectadmin'
     def test_list_populated(self):
-        self.assertEquals(self.browser.title, 'Devilry')
-        #elem = browser.find_element_by_name("p") # Find the query box
-        #elem.send_keys("seleniumhq" + Keys.RETURN)
-        #time.sleep(0.2) # Let the page load, will be added to the API
-        #try:
-            #browser.find_element_by_xpath("//a[contains(@href,'http://seleniumhq.org')]")
-        #except NoSuchElementException:
-            #assert 0, "can't find seleniumhq"
+        self.assertEquals(self.driver.title, 'Devilry')
+        self.waitForCssSelector('.shortcutlist')
+        self.assertTrue('>2012h.assignment1<' in self.driver.page_source)
+        self.assertTrue('>duck1100<' in self.driver.page_source)
+        self.assertTrue('>week1<' in self.driver.page_source)
+        #print dir(self.driver.find_element_by_css_selector(".shortcutlist>ul"))
