bedcf375d534191f6f05ede54e9a6781362caaa4
Espen A. Kristiansen <post@espenak.net>
2012-01-23 02:32:20 +0100

    apps.subjectadmin...test.js: Some routing.

--- devilry/apps/subjectadmin/static/subjectadmin/test.js
+++ devilry/apps/subjectadmin/static/subjectadmin/test.js
@@ -2,32 +2,43 @@ Ext.application({
     name: 'subjectadmin',
     appFolder: DevilrySettings.DEVILRY_STATIC_URL + '/subjectadmin/app',
 
-    requires: [
-        'Ext.ux.Router'
-    ],
-
     controllers: [
         'ShortcutsTestMock',
     ],
 
-    /*
-     * Here is where routes are defined.
-     *  key:    URL matcher
-     *  value:  controller + '#' + action to be invoked
-     */
-    routes: {
-        '/': 'home#index',
-        'users': 'users#list',
-        'users/:id/edit': 'users#edit'
-    },
-
-
     launch: function() {
+        this.route = Ext.create('guibase.Router', this);
+        this.route.add("^$", 'index');
+        this.route.add(/^\/shortcutlist$/, 'shortcutlist');
+        this.route.add(/^\/period\/(\d+)$/, 'period');
+        this.route.add(/^\/assignment\/(\d+)$/, 'assignment');
+        this.route.start();
+
         Ext.create('Ext.container.Viewport', {
             layout: 'fit',
             items: {
                 xtype: 'shortcutlist'
             }
         });
+    },
+
+    index: function() {
+        console.log('index');
+    },
+
+    shortcutlist: function(action) {
+        console.log(action);
+    },
+
+    period: function(action, id) {
+        console.log('PE', action, id);
+    },
+
+    assignment: function(action, id) {
+        console.log('Assignment!', id);
+    },
+
+    routeNotFound: function() {
+        alert('ROUTE NOT FOUND');
     }
 });
