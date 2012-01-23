bd92670c2a3093a28f0b4f3833a5f228689b60bd
Espen A. Kristiansen <post@espenak.net>
2012-01-23 15:37:07 +0100

    apps.subjectadmin...app: Routes.

--- devilry/apps/subjectadmin/static/subjectadmin/app.js
+++ devilry/apps/subjectadmin/static/subjectadmin/app.js
@@ -8,11 +8,46 @@ Ext.application({
     ],
 
     launch: function() {
-        Ext.create('Ext.container.Viewport', {
-            layout: 'fit',
-            items: {
-                xtype: 'shortcutlist'
-            }
+        this.viewport = Ext.create('Ext.container.Viewport', {
+            layout: 'fit'
         });
+        this.route = Ext.create('guibase.Router', this);
+        this.route.add("", 'shortcutlist');
+        this.route.add("/@@dashboard/shortcutlist", 'shortcutlist');
+        this.route.add("/:subject/:period", 'period_show');
+        this.route.add("/:subject/:period/@@edit", 'period_edit');
+        this.route.add("/:subject/:period/:assignment", 'assignment_show');
+        this.route.start();
+    },
+
+    dashboard: function() {
+        this.setView({xtype: 'component', html: 'dashboard'});
+    },
+
+    shortcutlist: function() {
+        this.setView({xtype: 'shortcutlist'});
+    },
+
+    period_show: function(action, subject, period) {
+        console.log('PE', action, subject, period);
+    },
+    period_edit: function(action, subject, period) {
+        console.log(action, subject, period);
+    },
+
+    assignment_show: function(action, subject, period, assignment) {
+        console.log('Assignment', subject, period, assignment);
+    },
+
+    routeNotFound: function() {
+        this.setView({
+            xtype: 'component',
+            html: '<h1>Not found</h1><p>Route not found.</p>'
+        });
+    },
+
+    setView: function(component) {
+        this.viewport.removeAll();
+        this.viewport.add(component);
     }
 });
