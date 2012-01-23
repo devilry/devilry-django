c3e99eebc4ebc325f8edf1dce5644d31e5bbaaed
Espen A. Kristiansen <post@espenak.net>
2012-01-23 05:20:15 +0100

    apps.guibase.seleniumtests: Added browseTo method.

--- devilry/apps/guibase/seleniumhelpers.py
+++ devilry/apps/guibase/seleniumhelpers.py
@@ -7,8 +7,11 @@ class SeleniumMixin(object):
     def getDriver(self):
         return getattr(webdriver, self.seleniumbrowser)()
 
-    def getJsAppUrl(self, urlprefix):
-        return 'http://localhost:8000/{0}/test'.format(urlprefix)
+    def getJsAppUrl(self, appname):
+        return 'http://localhost:8000/{0}/test'.format(appname)
+
+    def getHashUrl(self, appname, path):
+        return '{0}#{1}'.format(self.getJsAppUrl(appname), path)
 
     def driverWaitForCssSelector(self, driver, cssselector, timeout=10):
         WebDriverWait(driver, timeout).until(lambda driver: driver.find_elements_by_css_selector(cssselector))
@@ -19,10 +22,12 @@ class SeleniumTestCase(TestCase, SeleniumMixin):
 
     def setUp(self):
         self.driver = self.getDriver()
-        self.driver.get(self.getJsAppUrl(self.appname))
 
     def tearDown(self):
         self.driver.close()
 
     def waitForCssSelector(self, cssselector, timeout=10):
         return self.driverWaitForCssSelector(self.driver, cssselector, timeout)
+
+    def browseTo(self, path):
+        self.driver.get(self.getHashUrl(self.appname, path))
