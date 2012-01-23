81ad9f365fd1c828a2a655761d5b3ac27d4ca55e
Espen A. Kristiansen <post@espenak.net>
2012-01-23 19:37:11 +0100

    apps.guibase...RouteNotFound: Use bootstrap error box.

--- devilry/apps/guibase/static/guibase/RouteNotFound.js
+++ devilry/apps/guibase/static/guibase/RouteNotFound.js
@@ -3,7 +3,12 @@ Ext.define('guibase.RouteNotFound', {
     alias: 'widget.routenotfound',
     
     tpl: [
-        '<h1>Route not found</h1>',
-        '<p>The following route is not available: <strong>{route}</strong></p>'
+        '<div class="alert-message block-message error">',
+        '  <h1>Route not found</h1>',
+        '  <p>The following route is not available: <strong>{route}</strong></p>',
+        '  <div class="alert-actions">',
+        '    <a class="btn small" href="#">Go to dashboard</a>',
+        '  </div>',
+        '</div>'
     ]
 });
