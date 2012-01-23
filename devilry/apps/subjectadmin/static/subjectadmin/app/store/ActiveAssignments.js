2cb07d504bcf326988729295b608e7cf103f6ee3
Espen A. Kristiansen <post@espenak.net>
2012-01-23 15:36:40 +0100

    apps.subjectadmin...store.ActiveAssignments: Use generated model.

--- devilry/apps/subjectadmin/static/subjectadmin/app/store/ActiveAssignments.js
+++ devilry/apps/subjectadmin/static/subjectadmin/app/store/ActiveAssignments.js
@@ -1,3 +1,9 @@
 Ext.define('subjectadmin.store.ActiveAssignments', {
     extend: 'Ext.data.Store',
+    model: 'devilry.apps.administrator.simplified.SimplifiedAssignment',
+
+    proxy: Ext.create('devilry.extjshelpers.RestProxy', {
+        url: DevilrySettings.DEVILRY_URLPATH_PREFIX + '/administrator/restfulsimplifiedassignment/',
+        result_fieldgroups: ["period", "subject"]
+    })
 });
