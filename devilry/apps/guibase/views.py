944fe76ba8258a4614c9660164351b25026b7c1a
Espen A. Kristiansen <post@espenak.net>
2012-01-23 20:41:05 +0100

    apps.guibase: CSS support.

--- devilry/apps/guibase/views.py
+++ devilry/apps/guibase/views.py
@@ -8,17 +8,19 @@ from django.conf.urls.defaults import url
 class GuiBaseView(View):
     appname = None
     templatename = None
+    with_css = False
 
     @classonlymethod
-    def as_appview(cls, appname, templatename='production'):
-        return cls.as_view(appname=appname, templatename=templatename)
+    def as_appview(cls, appname, templatename='production', with_css=False):
+        return cls.as_view(appname=appname, templatename=templatename, with_css=with_css)
 
     def get(self, request):
         return render(request, 'guibase/{0}.django.html'.format(self.templatename),
-                      {'appname': self.appname})
+                      {'appname': self.appname,
+                       'with_css': self.with_css})
 
 
 
-def create_urls(appname):
-    return (url(r'^ui$', login_required(GuiBaseView.as_appview(appname=appname))),
-            url(r'^test$', GuiBaseView.as_appview(appname=appname, templatename='test')))
+def create_urls(appname, with_css=False):
+    return (url(r'^ui$', login_required(GuiBaseView.as_appview(appname=appname, with_css=with_css))),
+            url(r'^test$', GuiBaseView.as_appview(appname=appname, templatename='test', with_css=with_css)))
