4c053129f802bb74190d9c48a96fc54975ecd37d
Espen A. Kristiansen <post@espenak.net>
2012-01-27 22:26:41 +0100

    apps.themebase...PrimaryButton: Button intended as the primary button in a view.

---
+++ devilry/apps/themebase/static/themebase/lib/PrimaryButton.js
@@ -0,0 +1,10 @@
+/**
+Button intended as the primary button in a view (the one it is most natural to
+click by default).
+*/ 
+Ext.define('themebase.PrimaryButton', {
+    extend: 'Ext.button.Button',
+    alias: 'widget.primarybutton',
+    cls: 'primarybutton',
+    scale: 'large'
+});
