fb28c260eeac96ad000974e0cc8e5f24fdcabb1b
Espen A. Kristiansen <post@espenak.net>
2012-01-27 21:51:05 +0100

    apps.guibase|subjectadmin: Made libs configurable.

--- devilry/apps/guibase/views.py
+++ devilry/apps/guibase/views.py
@@ -9,18 +9,23 @@ class GuiBaseView(View):
     appname = None
     templatename = None
     with_css = False
+    include_old_exjsclasses = False
+    libs = []
 
     @classonlymethod
-    def as_appview(cls, appname, templatename='production', with_css=False):
-        return cls.as_view(appname=appname, templatename=templatename, with_css=with_css)
+    def as_appview(cls, appname, templatename, **kwargs):
+        return cls.as_view(appname=appname, templatename=templatename, **kwargs)
 
     def get(self, request):
         return render(request, 'guibase/{0}.django.html'.format(self.templatename),
                       {'appname': self.appname,
-                       'with_css': self.with_css})
+                       'with_css': self.with_css,
+                       'libs': self.libs,
+                       'include_old_exjsclasses': self.include_old_exjsclasses})
 
 
 
-def create_urls(appname, with_css=False):
-    return (url(r'^ui$', login_required(GuiBaseView.as_appview(appname=appname, with_css=with_css))),
-            url(r'^test$', GuiBaseView.as_appview(appname=appname, templatename='test', with_css=with_css)))
+def create_urls(appname, with_css=False, libs=[], include_old_exjsclasses=False):
+    kwargs = dict(with_css=with_css, libs=libs, include_old_exjsclasses=include_old_exjsclasses)
+    return (url(r'^ui$', login_required(GuiBaseView.as_appview(appname, 'production', **kwargs))),
+            url(r'^test$', GuiBaseView.as_appview(appname, 'test', **kwargs)))
--- devilry/apps/subjectadmin/urls.py
+++ devilry/apps/subjectadmin/urls.py
@@ -2,4 +2,7 @@ from django.conf.urls.defaults import patterns
 from devilry.apps.guibase.views import create_urls
 
 urlpatterns = patterns('devilry.apps.subjectadmin',
-                       *create_urls('subjectadmin', with_css=True))
+                       *create_urls('subjectadmin',
+                                    with_css=True,
+                                    include_old_exjsclasses=True,
+                                    libs=['guibase', 'themebase']))
