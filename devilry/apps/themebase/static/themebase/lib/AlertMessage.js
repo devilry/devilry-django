0c1abcaff79bfaca8c637223c38ea3ef450907e9
Espen A. Kristiansen <post@espenak.net>
2012-01-29 00:19:49 +0100

    apps.themebase...AlertMessage: General purpose alert message.

---
+++ devilry/apps/themebase/static/themebase/lib/AlertMessage.js
@@ -0,0 +1,55 @@
+/**
+One-line messages for highlighting the failure, possible failure, or success of
+an action. Particularly useful for forms.
+*/ 
+Ext.define('themebase.AlertMessage', {
+    extend: 'Ext.Component',
+    alias: 'widget.alertmessage',
+    
+    tpl: [
+        '<div class="alert-message {type}">',
+        '  <p>{message}</p>',
+        '</div>'
+    ],
+
+    data: {
+        title: dtranslate('themebase.routenotfound'),
+        gotodashboard: dtranslate('themebase.gotodashboard')
+    },
+
+    /**
+     * @cfg
+     * Type of message. Valid values: 'error', 'warning', 'info' or 'success'.
+     * Defaults to 'warning'.
+     */
+    type: 'warning',
+
+    /**
+     * @cfg
+     * The one line of message to display. This is not required, as it may be
+     * useful to initialize an hidden alert message and use update() to change
+     * the message when there is a message to show.
+     */
+    message: '',
+
+    initComponent: function() {
+        this.data.type = this.type;
+        this.data.message = this.message;
+        this.callParent(arguments);
+    },
+
+    /**
+     * Update the message and optionally the type. If the type is not
+     * specified, the type will not be changed.
+     * */
+    update: function(message, type) {
+        if(type) {
+            this.type = type;
+        }
+        this.message = message;
+        this.callParent({
+            type: this.type,
+            message: this.message
+        });
+    }
+});
