f270d195670b3c2603b64adba90b5f90eb60990f
Espen A. Kristiansen <post@espenak.net>
2012-01-28 22:24:49 +0100

    apps.guibase|themebase: Docs.

---
+++ devilry/apps/guibase/__init__.py
@@ -0,0 +1,4 @@
+"""
+Defines a javascript application framework on top of *Django* and *ExtJS*. See
+the Devilry developer documentation for more information.
+"""
---
+++ devilry/apps/themebase/__init__.py
@@ -0,0 +1,4 @@
+"""
+The base for our theme. This package contains the theme (SASS/compass
+stylesheets) and selection of ExtJS widgets that make up the theme.
+"""
