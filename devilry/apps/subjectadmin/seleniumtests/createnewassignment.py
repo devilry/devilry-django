d9b12cdc4858965a6cc739b9dd355e0c7ea28a66
Espen A. Kristiansen <post@espenak.net>
2012-01-30 00:59:08 +0100

    apps.subjectadmin.seleniumtests.createnewassignment: test_form_render.

--- devilry/apps/subjectadmin/seleniumtests/createnewassignment.py
+++ devilry/apps/subjectadmin/seleniumtests/createnewassignment.py
@@ -11,7 +11,7 @@ class TestCreateNewAssignment(SeleniumTestCase):
         self.assertTrue('duck1100.2011h' in self.driver.page_source)
         self.assertTrue('subjectadmin.chooseperiod.title' in self.driver.page_source)
         self.assertTrue('subjectadmin.chooseperiod.sidebarhelp' in self.driver.page_source)
-        self.assertTrue('subjectadmin.next' in self.driver.page_source)
+        self.assertTrue('themebase.next' in self.driver.page_source)
         periodlist = self.driver.find_element_by_class_name('activeperiodslist')
         self.assertEquals(len(periodlist.find_elements_by_css_selector('tr.x-grid-row')), 3)
 
@@ -30,9 +30,23 @@ class TestCreateNewAssignment(SeleniumTestCase):
 
     def test_form_render(self):
         self.browseTo('/@@create-new-assignment/1')
-        self.waitForCssSelector('.createnewassignmentform ')
+        self.waitForCssSelector('.createnewassignmentform')
+
         self.assertTrue('subjectadmin.createnewassignment.title' in self.driver.page_source)
         self.assertTrue('subjectadmin.createnewassignment.sidebarhelp' in self.driver.page_source)
-        self.assertTrue('subjectadmin.next' in self.driver.page_source)
-        periodlist = self.driver.find_element_by_class_name('activeperiodslist')
-        self.assertEquals(len(periodlist.find_elements_by_css_selector('tr.x-grid-row')), 3)
+        self.assertTrue('themebase.create' in self.driver.page_source)
+
+        self.assertTrue('subjectadmin.assignment.long_name.help' in self.driver.page_source)
+        self.assertTrue('subjectadmin.assignment.long_name.label' in self.driver.page_source)
+
+        self.assertTrue('subjectadmin.assignment.delivery_types.help' in self.driver.page_source)
+        self.assertTrue('subjectadmin.assignment.delivery_types.label' in self.driver.page_source)
+
+        self.assertTrue('subjectadmin.assignment.short_name.help' in self.driver.page_source)
+        self.assertTrue('subjectadmin.assignment.short_name.label' in self.driver.page_source)
+
+        self.assertTrue('subjectadmin.assignment.publishing_time.help' in self.driver.page_source)
+        self.assertTrue('subjectadmin.assignment.publishing_time.label' in self.driver.page_source)
+
+        self.assertTrue('subjectadmin.assignment.anonymous.help' in self.driver.page_source)
+        self.assertTrue('subjectadmin.assignment.anonymous.label' in self.driver.page_source)
