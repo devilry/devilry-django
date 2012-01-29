1fe4e6cd513a5ddb9195cbdca61d294c0325235b
Espen A. Kristiansen <post@espenak.net>
2012-01-29 16:33:25 +0100

    devilry.utils.command: Added get_installed_apps.

--- devilry/utils/command.py
+++ devilry/utils/command.py
@@ -3,6 +3,10 @@ Utility functions for management commands.
 """
 
 import logging
+from os.path import exists, dirname, isdir
+
+from django.conf import settings
+from django.utils.importlib import import_module
 
 
 def setup_logging(verbosity):
@@ -19,3 +23,16 @@ def setup_logging(verbosity):
 
 def get_verbosity(options):
     return int(options.get('verbosity', '1'))
+
+
+def get_installed_apps():
+    installed_apps = []
+    checked = set()
+    for app in settings.INSTALLED_APPS:
+        if not app.startswith('django.') and not app in checked:
+            mod = import_module(app)
+            checked.add(app)
+            if exists(mod.__file__) and isdir(dirname(mod.__file__)):
+                moddir = dirname(mod.__file__)
+                installed_apps.append((moddir, mod, mod.__name__.split('.')[-1]))
+    return installed_apps
