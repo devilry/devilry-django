2c7e00b32c4b1144d207c87d31effd6f98b569af
Espen A. Kristiansen <post@espenak.net>
2012-01-30 21:37:17 +0100

    apps.i18n...i18n.dtranslate: Added fallback and docs.

--- devilry/apps/i18n/static/i18n/i18n.js
+++ devilry/apps/i18n/static/i18n/i18n.js
@@ -1,9 +1,21 @@
-function dtranslate(key) {
+/** Lookup a translation for the given ``key``.
+ *
+ * If the key is not found, the ``fallback`` is returned. As a last resort, the
+ * key is returned.
+ *
+ * **Usage:** Only use fallback when the UI will break (I.E. for date
+ * formatting strings etc..). In any other case it is easier to test and spot
+ * untranslated UI widgets when the key is displayed by default.
+ */
+function dtranslate(key, fallback) {
     if(window.document.i18n) {
         var translation = window.document.i18n[key];
         if(translation) {
             return translation;
-        }
+        } 
+    }
+    if(fallback !== undefined) {
+        return fallback;
     }
     return key;
 }
