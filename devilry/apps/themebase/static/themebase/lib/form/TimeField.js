ac80210cc7134fdf4968727a6090781bfe00ceda
Espen A. Kristiansen <post@espenak.net>
2012-01-30 22:27:46 +0100

    apps.themebase...TimeField: Set increment to 3 hours.

--- devilry/apps/themebase/static/themebase/lib/form/TimeField.js
+++ devilry/apps/themebase/static/themebase/lib/form/TimeField.js
@@ -1,5 +1,6 @@
 Ext.define('themebase.form.TimeField', {
     extend: 'Ext.form.field.Time',
     alias: 'widget.themebase-timefield',
-    format: dtranslate('themebase.timeformat', 'H:i')
+    format: dtranslate('themebase.timeformat', 'H:i'),
+    increment: 180
 });
