8dc6276c392b5b48cde59238f3799fec57ff8387
Espen A. Kristiansen <post@espenak.net>
2012-01-30 17:49:26 +0100

    apps.jsapp.views: Minor changes.

--- devilry/apps/jsapp/views.py
+++ devilry/apps/jsapp/views.py
@@ -5,27 +5,36 @@ from django.utils.decorators import classonlymethod
 from django.conf.urls.defaults import url
 
 
-class GuiBaseView(View):
+class JsAppView(View):
     appname = None
-    templatename = None
+    templatename = 'production'
     with_css = False
     include_old_exjsclasses = False
     libs = []
 
     @classonlymethod
-    def as_appview(cls, appname, templatename, **kwargs):
-        return cls.as_view(appname=appname, templatename=templatename, **kwargs)
+    def as_appview(cls, appname, **kwargs):
+        return cls.as_view(appname=appname, **kwargs)
+
+    def get_base(self, request, **extra):
+        kw = {'appname': self.appname,
+              'with_css': self.with_css,
+              'libs': self.libs,
+              'include_old_exjsclasses': self.include_old_exjsclasses}
+        kw.update(extra)
+        return render(request, 'jsapp/{0}.django.html'.format(self.templatename), kw)
 
     def get(self, request):
-        return render(request, 'jsapp/{0}.django.html'.format(self.templatename),
-                      {'appname': self.appname,
-                       'with_css': self.with_css,
-                       'libs': self.libs,
-                       'include_old_exjsclasses': self.include_old_exjsclasses})
+        return self.get_base(request)
 
+
+class JsAppTestView(JsAppView):
+    templatename = 'test'
+    def get(self, request):
+        return self.get_base(request)
 
 
 def create_urls(appname, with_css=False, libs=[], include_old_exjsclasses=False):
     kwargs = dict(with_css=with_css, libs=libs, include_old_exjsclasses=include_old_exjsclasses)
-    return (url(r'^ui$', login_required(GuiBaseView.as_appview(appname, 'production', **kwargs))),
-            url(r'^test$', GuiBaseView.as_appview(appname, 'test', **kwargs)))
+    return (url(r'^ui$', login_required(JsAppView.as_appview(appname, **kwargs))),
+            url(r'^test$', JsAppView.as_appview(appname, **kwargs)))
