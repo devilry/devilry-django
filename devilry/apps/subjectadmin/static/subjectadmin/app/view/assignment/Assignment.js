322f71f7a630cd38e41986bda7db6459d5aab5b4
Espen A. Kristiansen <post@espenak.net>
2012-02-01 23:52:26 +0100

    apps.subjectadmin...Assignment: Center layout.

--- devilry/apps/subjectadmin/static/subjectadmin/app/view/assignment/Assignment.js
+++ devilry/apps/subjectadmin/static/subjectadmin/app/view/assignment/Assignment.js
@@ -29,8 +29,6 @@ Ext.define('subjectadmin.view.assignment.Assignment' ,{
             layout: 'rightsidebar',
             frame: false,
 
-            //var currenturl = Ext.String.format('#{0}/{1}/{2}'
-
             items: [{
                 xtype: 'container',
                 cls: 'centercolumn',
@@ -45,18 +43,51 @@ Ext.define('subjectadmin.view.assignment.Assignment' ,{
                         xtype: 'actionlist',
                         cls: 'centerbody',
                         links: [{
-                            url: '#/@@create-new-assignment/@@chooseperiod',
+                            url: '#',
                             text: dtranslate('themebase.edit-something')
                         }, {
                             url: '#',
                             text: dtranslate('subjectadmin.assignment.browse-and-manage-students')
                         }, {
-                            url: '#/@@register-for-final-exams',
+                            url: '#',
                             text: dtranslate('subjectadmin.assignment.manage-deadlines')
                         }, {
-                            url: '#/@@global-statistics',
+                            url: '#',
                             buttonType: 'danger',
                             text: dtranslate('themebase.delete-something')
+                        }]
+                    }]
+                }, {
+                    xtype: 'container',
+                    cls: 'twocol-centerbox',
+                    layout: 'column',
+                    items: [{
+                        xtype: 'container',
+                        cls: 'centerbox',
+                        columnWidth: .5,
+                        items: [{
+                            xtype: 'box',
+                            html: Ext.String.format('<h2 class="centertitle">{0}</h2>',
+                                Ext.String.ellipsis(dtranslate('subjectadmin.assignment.waitingforfeedback'), 25)
+                            ),
+                        }, {
+                            xtype: 'box',
+                            cls: 'centerbody',
+                            html: 'TODO'
+                        }]
+                    }, {
+                        xtype: 'container',
+                        columnWidth: .5,
+                        cls: 'centerbox',
+                        items: [{
+                            xtype: 'box',
+                            html: Ext.String.format('<h2 class="centertitle">{0}</h2>',
+                                Ext.String.ellipsis(dtranslate('subjectadmin.assignment.upcoming-deadlines'), 25)
+                            ),
+                        }, {
+                            xtype: 'box',
+                            cls: 'centerbody',
+                            html: 'TODO'
                         }]
                     }]
                 }]
