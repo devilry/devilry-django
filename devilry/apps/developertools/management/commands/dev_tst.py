eb1c1c35d6ee5c55c5c68e92434635d5abf0247f
Espen A. Kristiansen <post@espenak.net>
2012-02-04 08:43:44 +0100

    apps.developertools...dev_tst: GroupDao example.

--- devilry/apps/developertools/management/commands/dev_tst.py
+++ devilry/apps/developertools/management/commands/dev_tst.py
@@ -1,22 +1,20 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
-from devilry.apps.administrator.simplified import *
 from django.db import connection
+from devilry.apps.subjectadmin.rest.group import GroupDao
+
 
 class Command(BaseCommand):
     help = 'tst'
 
     def handle(self, *args, **options):
         grandma = User.objects.get(username='grandma')
-        #[x for x in SimplifiedRelatedStudent.search(grandma)]
-        #[x for x in SimplifiedAssignmentGroup.search(grandma,
-                                                     #filters=[{"field":"parentnode","comp":"exact","value":1}],
-                                                     #result_fieldgroups=["feedback"])]
-        [x for x in SimplifiedCandidate.search(grandma,
-                                               filters=[{"field":"assignment_group__parentnode__parentnode","comp":"exact","value":1}])]
+        dao = GroupDao()
+        dao.read(grandma, 1)
+        print
         for qry in connection.queries:
             #print '{time:<10}: {sql}'.format(**qry)
-            print '{sql}'.format(**qry)
+            print '{sql} {time}'.format(**qry)
             print
         print
         print len(connection.queries)
