894d1beced1a6a7af73136b0034bdb3707b4e279
Espen A. Kristiansen <post@espenak.net>
2012-02-06 14:24:01 +0100

    apps.themebase...CancelButton: Reusable cancel button.

---
+++ devilry/apps/themebase/static/themebase/lib/CancelButton.js
@@ -0,0 +1,9 @@
+/**
+Cancel button
+*/ 
+Ext.define('themebase.CancelButton', {
+    extend: 'Ext.button.Button',
+    alias: 'widget.cancelbutton',
+    scale: 'medium',
+    text: dtranslate('themebase.cancel')
+});
