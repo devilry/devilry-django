e1c0b5730aea44b7836afb2cc757a3816bd4bc26
Espen A. Kristiansen <post@espenak.net>
2012-02-13 18:32:05 +0100

    apps.core.tests.candidate: Test email change on User change.

--- devilry/apps/core/tests/candidate.py
+++ devilry/apps/core/tests/candidate.py
@@ -61,10 +61,13 @@ class TestCandidate(TestCase, TestHelper):
     def test_sync_candidate_with_user_on_change(self):
         candidate = self.inf1100_autumn_assignment1_g1.candidates.all()[0]
         self.assertEquals(candidate.identifier, 'student1')
+        self.assertEquals(candidate.email, 'student1@example.com')
         candidate.student.username = 'changed'
+        candidate.student.email = 'somethingchanged@example.com'
         candidate.student.save()
         candidate_db = Candidate.objects.get(id=candidate.id) # Must be re-fetched to avoid reading student from cache
         self.assertEquals(candidate_db.identifier, 'changed')
+        self.assertEquals(candidate_db.email, 'somethingchanged@example.com')
 
     def test_sync_candidate_with_userprofile_on_change(self):
         candidate = self.inf1100_autumn_assignment1_g1.candidates.all()[0]
