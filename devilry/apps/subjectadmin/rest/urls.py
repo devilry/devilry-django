16658378dc9e5caf1c1487e6281761cb9e2c3012
Espen A. Kristiansen <post@espenak.net>
2012-02-26 03:33:30 +0100

    apps.subjectadmin.rest.urls: Added RestCreateNewAssignment.

--- devilry/apps/subjectadmin/rest/urls.py
+++ devilry/apps/subjectadmin/rest/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls.defaults import patterns
 
 from group import RestGroup
-from relateduser import RestRelatedStudent, RestRelatedExaminer
+from relateduser import RestRelatedStudent
+from relateduser import RestRelatedExaminer
+from createnewassignment import RestCreateNewAssignment
 
 urlpatterns = patterns('devilry.apps.subjectadmin.rest',
                        RestRelatedStudent.create_url("relatedstudent", "restrelatedstudent-api", "1.0"),
                        RestRelatedExaminer.create_url("relatedexaminer", "restrelatedexaminer-api", "1.0"),
+                       RestCreateNewAssignment.create_url("createnewassignment", "restgroup-api", "1.0"),
                        RestGroup.create_url("group", "restgroup-api", "1.0"))
