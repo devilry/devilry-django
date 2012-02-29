62da2920e8c0baeeb445f8815a8dfd6bf7eb7c3d
Espen A. Kristiansen <post@espenak.net>
2012-02-29 07:09:18 +0100

    apps.themebase...RestApiProxyErrorHandler: Add support for i18n messages.

--- devilry/apps/themebase/static/themebase/lib/RestApiProxyErrorHandler.js
+++ devilry/apps/themebase/static/themebase/lib/RestApiProxyErrorHandler.js
@@ -12,6 +12,17 @@ Ext.define('themebase.RestApiProxyErrorHandler', {
         }
     },
 
+    _translate: function(i18ndata) {
+        var i18nkey = i18ndata[0];
+        var i18nparameters = i18ndata[1];
+        var message = dtranslate(i18nkey);
+        console.log(message, i18nparameters);
+        if(i18nparameters) {
+            message = Ext.create('Ext.XTemplate', message).apply(i18nparameters);
+        }
+        return message;
+    },
+
     /**
      * Add errors from an extjs response object (such as the one given to proxy
      * exception events) on the format produced by the python function
@@ -22,6 +33,16 @@ Ext.define('themebase.RestApiProxyErrorHandler', {
         if(responseData) {
             this.errormessages = responseData.errormessages;
             this.fielderrors = responseData.fielderrors;
+            Ext.Array.each(responseData.i18nErrormessages, function(i18ndata) {
+                this.errormessages.push(this._translate(i18ndata));
+            }, this);
+            Ext.Object.each(responseData.i18nFielderrors, function(fieldname, i18ndatalist) {
+                var messages =  [];
+                Ext.Array.each(i18ndatalist, function(i18ndata) {
+                    messages.push(this._translate(i18ndata));
+                }, this);
+                this.fielderrors[fieldname] = messages;
+            }, this);
             return true;
         } else {
             return false;
