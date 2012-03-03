a4708fcfaac26b62bdce70b62f9c5e2deb18cb65
Espen A. Kristiansen <post@espenak.net>
2012-03-03 18:07:37 +0100

    apps.subjectadmin.tests.shortcuts: Updated tests for long_name.

--- devilry/apps/subjectadmin/tests/shortcuts.py
+++ devilry/apps/subjectadmin/tests/shortcuts.py
@@ -7,7 +7,7 @@ class TestShortcuts(SeleniumTestCase):
     def test_shortcutlist(self):
         self.browseToTest('/@@dashboard/shortcutlist')
         self.waitForCssSelector('.shortcutlist')
-        self.assertTrue('>2012h.assignment1<' in self.driver.page_source)
         self.assertTrue('>duck1100<' in self.driver.page_source)
-        self.assertTrue('>week1<' in self.driver.page_source)
+        self.assertTrue('>Week one<' in self.driver.page_source)
+        self.assertTrue('>2012h.Assignment one<' in self.driver.page_source)
         self.assertTrue('href="#/duck-mek2030/2012h/assignment1"' in self.driver.page_source)
