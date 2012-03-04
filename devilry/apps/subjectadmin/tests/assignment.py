dbfc8d82e6ad946e6c4dee11bc92e0c8306418c9
Espen A. Kristiansen <post@espenak.net>
2012-03-04 12:25:17 +0100

    apps.subjectadmin.tests.assignment: TestEditAnonymous and fix TestEditPublishingTime.

--- devilry/apps/subjectadmin/tests/assignment.py
+++ devilry/apps/subjectadmin/tests/assignment.py
@@ -51,16 +51,16 @@ class TestEditPublishingTime(SeleniumTestCase):
         self.timefield.send_keys(time)
 
     def test_editpublishingtime(self):
-        self.assertTrue('subjectadmin.assignment.editpublishingtime.title' in self.driver.page_source)
-        self.assertTrue('subjectadmin.assignment.editpublishingtime.help' in self.driver.page_source)
+        self.assertTrue('subjectadmin.assignment.publishing_time.label' in self.driver.page_source)
+        self.assertTrue('subjectadmin.assignment.publishing_time.edithelp' in self.driver.page_source)
         self._set_datetime('2012-01-10', '12:00')
         self.savebutton.click()
         self.waitForText('2012-01-10T12:00') # If this times out, the proxy has not been updated
         self.assertTrue('2012-01-10 12:00 is published' in self.driver.page_source)
 
     def test_editpublishingtime_notpublished(self):
-        self.assertTrue('subjectadmin.assignment.editpublishingtime.title' in self.driver.page_source)
-        self.assertTrue('subjectadmin.assignment.editpublishingtime.help' in self.driver.page_source)
+        self.assertTrue('subjectadmin.assignment.publishing_time.label' in self.driver.page_source)
+        self.assertTrue('subjectadmin.assignment.publishing_time.edithelp' in self.driver.page_source)
         self._set_datetime('3012-01-10', '12:00') # If Devilry is still in use in 3012, we can afford to fix this:)
         self.savebutton.click()
         self.waitForText('3012-01-10T12:00') # If this times out, the proxy has not been updated
@@ -70,3 +70,31 @@ class TestEditPublishingTime(SeleniumTestCase):
         self._set_datetime('2012-02-01', '12:00') # subjectadmin.model.AssignmentTestMock defines day 01 to raise an error (for _this_ testcase)
         self.savebutton.click()
         self.waitForText('It is triggered since the day of month is "1" (for testing only).') # If this times out, the error was not added to the body
+
+
+class TestEditAnonymous(SeleniumTestCase):
+    appname = 'subjectadmin'
+
+    def afterSetUp(self):
+        self.browseToTest('/duck1100/2012h/week1')
+        self.waitForCssSelector('.editanonymous-widget button')
+        button = self.driver.find_element_by_css_selector('.editanonymous-widget button')
+        button.click()
+        self.waitForCssSelector('.editanonymous')
+
+        editanonymous_window = self.driver.find_element_by_css_selector('.editanonymous')
+        self.anonymouscheckbox = editanonymous_window.find_element_by_css_selector('input[role="checkbox"]')
+        self.savebutton = editanonymous_window.find_element_by_css_selector('.savebutton button')
+
+    def test_editanonymous(self):
+        self.assertTrue('subjectadmin.assignment.anonymous.label' in self.driver.page_source)
+        self.assertTrue('subjectadmin.assignment.anonymous.help' in self.driver.page_source)
+        self.anonymouscheckbox.click()
+        self.savebutton.click()
+        self.waitForText('subjectadmin.assignment.is_anonymous.body') # If this times out, the proxy has not been updated
+        self.waitForText('subjectadmin.assignment.is_anonymous.title') # If this times out, the proxy has not been updated
+
+    def test_editanonymous_nochange(self):
+        self.savebutton.click()
+        self.waitForText('subjectadmin.assignment.not_anonymous.body') # If this times out, the proxy has not been updated
+        self.waitForText('subjectadmin.assignment.not_anonymous.title') # If this times out, the proxy has not been updated
