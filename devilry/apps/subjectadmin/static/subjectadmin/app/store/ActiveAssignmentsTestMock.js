c2b719df569acf5a7dda9342bce3240206bb02e4
Espen A. Kristiansen <post@espenak.net>
2012-03-19 02:10:52 +0100

    apps.subjectadmin...ActiveAssignments: Filter out nonactive.

--- devilry/apps/subjectadmin/static/subjectadmin/app/store/ActiveAssignmentsTestMock.js
+++ devilry/apps/subjectadmin/static/subjectadmin/app/store/ActiveAssignmentsTestMock.js
@@ -1,4 +1,8 @@
 Ext.define('subjectadmin.store.ActiveAssignmentsTestMock', {
     extend: 'Ext.data.Store',
-    model: 'subjectadmin.model.ActiveAssignmentTestMock'
+    model: 'subjectadmin.model.ActiveAssignmentTestMock',
+
+    loadActiveAssignments: function(config) {
+        this.load(config);
+    }
 });
