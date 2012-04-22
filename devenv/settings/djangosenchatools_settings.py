de85540b07df66f4531bdae9be084557e642e9a3
Espen A. Kristiansen <post@espenak.net>
2012-04-22 22:15:57 +0200

    devenv: Settings files for djangosenchatools.

---
+++ devenv/settings/djangosenchatools_settings.py
@@ -0,0 +1,18 @@
+# Use this settings module with runserver when running senchatoolsbuild:
+#
+#    $ bin/django_dev.py runserver --settings settings.djangosenchatools_settings
+#    and in another terminal:
+#    $ bin/django_dev.py senchatoolsbuild --buildall
+#
+#
+# See the djangosenchatools docs (https://github.com/espenak/djangosenchatools)
+# for details.
+
+from settings import *
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+EXTJS4_DEBUG = True
+MIDDLEWARE_CLASSES += ['djangosenchatools.auth.SettingUserMiddleware']
+AUTHENTICATION_BACKENDS = ('djangosenchatools.auth.SettingUserBackend',)
+SENCHATOOLS_USER = 'grandma'
