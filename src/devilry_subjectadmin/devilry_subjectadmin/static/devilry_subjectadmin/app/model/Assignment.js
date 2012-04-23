cc5aec446b9790e495fbea3f300d2c2c5614f63e
Espen A. Kristiansen <post@espenak.net>
2012-04-23 02:11:13 +0200

    devilry_subjectadmin...model.Assignment: Specify X_DEVILRY_USE_EXTJS header.

--- src/devilry_subjectadmin/devilry_subjectadmin/static/devilry_subjectadmin/app/model/Assignment.js
+++ src/devilry_subjectadmin/devilry_subjectadmin/static/devilry_subjectadmin/app/model/Assignment.js
@@ -4,6 +4,7 @@ Ext.define('devilry_subjectadmin.model.Assignment', {
 
     proxy: Ext.create('devilry.extjshelpers.RestProxy', {
         url: DevilrySettings.DEVILRY_URLPATH_PREFIX + '/administrator/restfulsimplifiedassignment/',
+        headers: {X_DEVILRY_USE_EXTJS: true},
         orderby: ['-publishing_time'],
         result_fieldgroups: ["subject", "period"]
     }),
