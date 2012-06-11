11edf32aa5e619e6b99ea03c03a9b1c19034b00a
Espen A. Kristiansen <post@espenak.net>
2012-06-12 01:41:15 +0200

    devilry.rest.testutils: Useful error message on json load error.

--- src/devilry/devilry/rest/testutils.py
+++ src/devilry/devilry/rest/testutils.py
@@ -16,40 +16,46 @@ def isoformat_datetime(datetimeobj):
     return datetimeobj.strftime('%Y-%m-%dT%H:%M:%S')
 
 class RestClient(Client):
+    def _load_json(self, content):
+        try:
+            return json.loads(content)
+        except ValueError, e:
+            raise ValueError('{0}: {1}'.format(e, content))
+
     def rest_create(self, url, **params):
         response = self.post(url,
                              data=json.dumps(params),
                              content_type="application/json",
                              HTTP_ACCEPT="application/json")
-        return json.loads(response.content), response
+        return self._load_json(response.content), response
 
     def rest_update(self, url, id, **params):
         response = self.put(url + str(id),
                             data=json.dumps(params),
                             content_type="application/json",
                             HTTP_ACCEPT="application/json")
-        return json.loads(response.content), response
+        return self._load_json(response.content), response
 
     def rest_read(self, url, id, **params):
         response = self.get(url + str(id),
                             data=params,
                             HTTP_ACCEPT="application/json")
-        return json.loads(response.content), response
+        return self._load_json(response.content), response
 
     def rest_delete(self, url, id):
         response = self.delete(url + str(id),
                                HTTP_ACCEPT="application/json")
-        return json.loads(response.content), response
+        return self._load_json(response.content), response
 
     def rest_batch(self, url, **params):
         response = self.put(url,
                             data=json.dumps(params),
                             content_type="application/json",
                             HTTP_ACCEPT="application/json")
-        return json.loads(response.content), response
+        return self._load_json(response.content), response
 
     def rest_list(self, url, **params):
         response = self.get(url,
                             data=params,
                             HTTP_ACCEPT="application/json")
-        return json.loads(response.content), response
+        return self._load_json(response.content), response
