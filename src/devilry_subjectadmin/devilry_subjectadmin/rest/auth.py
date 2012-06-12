a3920e5a307d2731ba336a18f376c78d3c405b4a
Espen A. Kristiansen <post@espenak.net>
2012-06-12 02:27:07 +0200

    devilry_subjectadmin...rest.auth: djangorestframework checkers.

--- src/devilry_subjectadmin/devilry_subjectadmin/rest/auth.py
+++ src/devilry_subjectadmin/devilry_subjectadmin/rest/auth.py
@@ -49,6 +49,32 @@ def _assignmentadmin_required(user, assignmentid):
     _admin_required(Assignment, user, _('Permission denied'), assignmentid)
 
 
+class IsSubjectAdmin(BasePermission):
+    """
+    Djangorestframework permission checker that checks if the requesting user
+    has admin-permissions on the subject given as the first argument to the
+    view.
+    """
+    def check_permission(self, user):
+        if len(self.view.args) != 1:
+            raise PermissionDeniedError('The IsSubjectAdmin permission checker requires an assignmentid.')
+        subjectid = self.view.args[0]
+        _subjectadmin_required(user, subjectid)
+
+
+class IsPeriodAdmin(BasePermission):
+    """
+    Djangorestframework permission checker that checks if the requesting user
+    has admin-permissions on the period given as the first argument to the
+    view.
+    """
+    def check_permission(self, user):
+        if len(self.view.args) != 1:
+            raise PermissionDeniedError('The IsPeriodAdmin permission checker requires an assignmentid.')
+        periodid = self.view.args[0]
+        periodadmin_required(user, periodid)
+
+
 class IsAssignmentAdmin(BasePermission):
     """
     Djangorestframework permission checker that checks if the requesting user
