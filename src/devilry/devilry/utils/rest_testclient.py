d509d63dca3e207be25b78b3e55d3392e41d6606
Espen A. Kristiansen <post@espenak.net>
2012-06-12 02:05:31 +0200

    devilry.utils.rest_testclient: Useful error message on json load error.

--- src/devilry/devilry/utils/rest_testclient.py
+++ src/devilry/devilry/utils/rest_testclient.py
@@ -7,27 +7,34 @@ class RestClient(Client):
     Extends the ``django.test.Client`` with methods for the REST verbs and
     application/json.
     """
+
+    def _load_json(self, content):
+        try:
+            return json.loads(content)
+        except ValueError, e:
+            raise ValueError('{0}: {1}'.format(e, content))
+
     def rest_post(self, url, data):
         response = self.post(url,
                              data=json.dumps(data),
                              content_type="application/json",
                              HTTP_ACCEPT="application/json")
-        return json.loads(response.content), response
+        return self._load_json(response.content), response
 
     def rest_put(self, url, data):
         response = self.put(url,
                             data=json.dumps(data),
                             content_type="application/json",
                             HTTP_ACCEPT="application/json")
-        return json.loads(response.content), response
+        return self._load_json(response.content), response
 
     def rest_get(self, url, **data):
         response = self.get(url,
                             data=data,
                             HTTP_ACCEPT="application/json")
-        return json.loads(response.content), response
+        return self._load_json(response.content), response
 
     def rest_delete(self, url):
         response = self.delete(url,
                                HTTP_ACCEPT="application/json")
-        return json.loads(response.content), response
+        return self._load_json(response.content), response
