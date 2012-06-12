621f09b8ab7c911c1e0edf3d809f510d68e4168a
Espen A. Kristiansen <post@espenak.net>
2012-06-12 16:39:17 +0200

    devilry_subjectadmin...tests.createnewassignment: test_create_with_related.

--- src/devilry_subjectadmin/devilry_subjectadmin/tests/createnewassignment.py
+++ src/devilry_subjectadmin/devilry_subjectadmin/tests/createnewassignment.py
@@ -1,5 +1,6 @@
 from datetime import date, timedelta
 from devilry.apps.core.testhelper import TestHelper
+from devilry.apps.core.models import Assignment, AssignmentGroup
 
 from .base import SubjectAdminSeleniumTestCase
 
@@ -11,6 +12,7 @@ class TestCreateNewAssignment(SubjectAdminSeleniumTestCase):
         self.testhelper = TestHelper()
         self.testhelper.create_superuser('grandma')
         self.login('grandma')
+        self.tomorrow = date.today() + timedelta(days=1)
 
         self.testhelper.add(nodes='uni',
                             subjects=['duck1100'],
@@ -60,8 +62,9 @@ class TestCreateNewAssignment(SubjectAdminSeleniumTestCase):
         field = self.selenium.find_element_by_css_selector('input[name={0}]'.format(fieldname))
         field.send_keys(value)
 
-    def _set_datetime_value(self, fieldclass, value):
-        field = self.selenium.find_element_by_css_selector('.{fieldclass} .datetimefield_date input[type=text]'.format(fieldclass=fieldclass))
+    def _set_datetime_value(self, fieldclass, field, value):
+        field = self.selenium.find_element_by_css_selector('.{fieldclass} .datetimefield_{field} input[type=text]'.format(fieldclass=fieldclass,
+                                                                                                                          field=field))
         field.send_keys(value)
 
     def test_form_createbutton(self):
@@ -78,15 +81,22 @@ class TestCreateNewAssignment(SubjectAdminSeleniumTestCase):
         self._set_value('short_name', 'test')
         self.waitForEnabled(createbutton)
 
+
+    #def test_duplicate(self):
+        #self.testhelper.add_to_path('uni;duck1100.2012h.a1')
+        #self.browseTo('/@@create-new-assignment/{0}'.format(self.period_id))
+        #self.waitForCssSelector('.createnewassignmentform')
+        #self._set_value('long_name', 'A1')
+        #self._set_value('short_name', 'a1')
+
     def test_success(self):
         self.browseTo('/@@create-new-assignment/{0}'.format(self.period_id))
         self.waitForCssSelector('.createnewassignmentform')
 
         self._set_value('long_name', 'Test')
         self._set_value('short_name', 'sometest')
-        tomorrow = date.today() + timedelta(days=1)
-        self.selenium.find_element_by_css_selector('.first_deadline .datetimefield_date input[type=text]').send_keys(tomorrow.isoformat())
-        self.selenium.find_element_by_css_selector('.first_deadline .datetimefield_time input[type=text]').send_keys('15:00')
+        self._set_datetime_value('first_deadline', 'date', self.tomorrow.isoformat())
+        self._set_datetime_value('first_deadline', 'time', '15:00')
 
         createbutton = self.selenium.find_element_by_css_selector('.createbutton button')
         self.waitForEnabled(createbutton)
@@ -97,6 +107,43 @@ class TestCreateNewAssignment(SubjectAdminSeleniumTestCase):
         self.assertEquals(len(links), 2)
         self.assertEquals(links[0].get_attribute('href'), 'http://localhost:8081/devilry_subjectadmin/#/duck1100/2012h/sometest/')
         self.assertEquals(links[1].get_attribute('href'), u'http://localhost:8081/devilry_subjectadmin/#/@@create-new-assignment/1')
+
+        created = Assignment.objects.get(parentnode__id=self.period_id, short_name='sometest')
+        self.assertEquals(created.long_name, 'Test')
+        return created
+
+
+    def _create_related_student(self, username, candidate_id=None, tags=None):
+        user = self.testhelper.create_user(username)
+        relatedstudent = self.testhelper.duck1100_2012h.relatedstudent_set.create(user=user,
+                                                                          candidate_id=candidate_id)
+        if tags:
+            relatedstudent.tags = tags
+            relatedstudent.save()
+        return relatedstudent
+
+    def _create_related_examiner(self, username, tags=None):
+        user = self.testhelper.create_user(username)
+        relatedexaminer = self.testhelper.duck1100_2012h.relatedexaminer_set.create(user=user)
+        if tags:
+            relatedexaminer.tags = tags
+            relatedexaminer.save()
+        return relatedexaminer
+
+    def test_create_with_related(self):
+        self._create_related_student('student0', tags=['group1'])
+        self._create_related_student('student1', tags=['group1'])
+        self._create_related_student('student2', tags=['group1'])
+        self._create_related_student('student3', tags=['group2'])
+        self._create_related_examiner('examiner0', tags=['group1'])
+        created = self.test_success()
+        self.assertEquals(created.assignmentgroups.all().count(), 4)
+        student0group = AssignmentGroup.where_is_candidate(self.testhelper.student0).get(parentnode=created.id)
+        self.assertEquals(student0group.examiners.all()[0].user, self.testhelper.examiner0)
+        self.assertEquals(student0group.deadlines.count(), 1)
+        self.assertEquals(student0group.deadlines.all()[0].deadline.date(), self.tomorrow)
+        self.assertEquals(student0group.deadlines.all()[0].deadline.time().hour, 15)
+        self.assertEquals(student0group.deadlines.all()[0].deadline.time().minute, 0)
 
     def test_success_direct(self):
         self.browseTo('/@@create-new-assignment/@@success')
