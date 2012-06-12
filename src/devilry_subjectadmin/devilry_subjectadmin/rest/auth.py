329b92b21e097251793f4fbe6b5988c7e0cd5c43
Espen A. Kristiansen <post@espenak.net>
2012-06-12 15:12:36 +0200

    devilry_subjectadmin...auth.IsPeriodAdmin: Generalized and docs.

--- src/devilry_subjectadmin/devilry_subjectadmin/rest/auth.py
+++ src/devilry_subjectadmin/devilry_subjectadmin/rest/auth.py
@@ -65,13 +65,15 @@ class IsSubjectAdmin(BasePermission):
 class IsPeriodAdmin(BasePermission):
     """
     Djangorestframework permission checker that checks if the requesting user
-    has admin-permissions on the period given as the first argument to the
-    view.
+    has admin-permissions on the period given in ``CONTENT['period_id']``.
     """
+    #: The CONTENT-key containing the period-id
+    PERIODID_KEY = 'period_id'
+
     def check_permission(self, user):
-        if not 'period_id' in self.view.CONTENT:
+        if not self.PERIODID_KEY in self.view.CONTENT:
             raise PermissionDeniedError('The IsPeriodAdmin permission checker requires period_id as part of the request data.')
-        period_id = self.view.CONTENT['period_id']
+        period_id = self.view.CONTENT[self.PERIODID_KEY]
         periodadmin_required(user, period_id)
 
 
