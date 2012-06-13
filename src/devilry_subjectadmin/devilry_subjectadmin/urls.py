d45fb517c95647953dff09aff2cae93101c10de0
Espen A. Kristiansen <post@espenak.net>
2012-06-13 22:38:02 +0200

    devilry_subjectadmin.urls: Remove old jsapp urls.

--- src/devilry_subjectadmin/devilry_subjectadmin/urls.py
+++ src/devilry_subjectadmin/devilry_subjectadmin/urls.py
@@ -3,8 +3,6 @@ from django.contrib.auth.decorators import login_required
 from django.views.i18n import javascript_catalog
 from django.views.decorators.csrf import csrf_protect, ensure_csrf_cookie
 
-
-from devilry.apps.jsapp.views import create_app_urls
 from views import AppView
 
 i18n_packages = ('devilry_subjectadmin',)
@@ -13,8 +11,4 @@ urlpatterns = patterns('devilry_subjectadmin',
                        url('^rest/', include('devilry_subjectadmin.rest.urls')),
                        url('^$', login_required(csrf_protect(ensure_csrf_cookie(AppView.as_view())))),
                        url('^i18n.js$', javascript_catalog, kwargs={'packages': i18n_packages},
-                           name='devilry_subjectadmin_i18n'),
-                       *create_app_urls(appname='devilry_subjectadmin',
-                                        with_css=True,
-                                        include_old_exjsclasses=True,
-                                        libs=['jsapp', 'themebase']))
+                           name='devilry_subjectadmin_i18n'))
