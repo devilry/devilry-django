bf1adda71313cf377c4d3535ec8de375ff85304d
Espen A. Kristiansen <post@espenak.net>
2012-06-14 16:39:55 +0200

    devilry_subjectadmin: Deleted old Application files.

--- src/devilry_subjectadmin/devilry_subjectadmin/static/devilry_subjectadmin/app/JasmineTestApplication.js
+++
@@ -1,8 +0,0 @@
-Ext.define('devilry_subjectadmin.JasmineTestApplication', {
-    extend: 'devilry_subjectadmin.TestApplication',
-    requires: ['jsapp.JasmineTest'],
-
-    launch: function() {
-        jsapp.JasmineTest.execute();
-    }
-});
