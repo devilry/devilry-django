2f864e1ad7dd5973458aff64f529db70a28ca2bf
Espen A. Kristiansen <post@espenak.net>
2012-06-16 06:50:48 +0200

    developertools...dev_tst: Modified.

--- src/devilry/devilry/apps/developertools/management/commands/dev_tst.py
+++ src/devilry/devilry/apps/developertools/management/commands/dev_tst.py
@@ -8,13 +8,14 @@ class Command(BaseCommand):
     help = 'tst'
 
     def handle(self, *args, **options):
-        items = Subject.objects.all().prefetch_related('admins__devilryuserprofile')
+        items = Subject.objects.all()#.prefetch_related('admins__devilryuserprofile')
         for item in items:
             print item.short_name
-            for admin in item.admins.all():
+            for admin in item.admins.all().prefetch_related('devilryuserprofile'):
                 print admin.devilryuserprofile.full_name
         print
         for qry in connection.queries:
+            print qry
             print '{sql} {time}'.format(**qry)
             print
         print
