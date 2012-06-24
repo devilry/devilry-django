09f7cb41b6ef68c32f5d3519f3a5f8f09a87280f
Espen A. Kristiansen <post@espenak.net>
2012-06-24 15:53:41 +0200

    developertools...dev_tst: Some changes.

--- src/devilry/devilry/apps/developertools/management/commands/dev_tst.py
+++ src/devilry/devilry/apps/developertools/management/commands/dev_tst.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
-from devilry.apps.core.models import Subject
+from devilry.apps.core.models import Subject, Period, Assignment
 from django.db import connection
 
 
@@ -8,14 +8,19 @@ class Command(BaseCommand):
     help = 'tst'
 
     def handle(self, *args, **options):
-        items = Subject.objects.all()#.prefetch_related('admins__devilryuserprofile')
-        for item in items:
-            print item.short_name
-            for admin in item.admins.all().prefetch_related('devilryuserprofile'):
-                print admin.devilryuserprofile.full_name
-        print
+        per = Assignment.objects.select_related('parentnode',
+                                                'parentnode__parentnode',
+                                                'parentnode__parentnode',
+                                                'parentnode__parentnode__parentnode',
+                                                'parentnode__parentnode__parentnode__parentnode',
+                                                'parentnode__parentnode__parentnode__parentnode__parentnode').get(id=1)
+        print per.short_name
+        print per.parentnode.short_name
+        print per.parentnode.parentnode.short_name
+        print per.parentnode.parentnode.parentnode.short_name
+        print per.parentnode.parentnode.parentnode.parentnode.short_name
         for qry in connection.queries:
-            print qry
+            #print qry
             print '{sql} {time}'.format(**qry)
             print
         print
