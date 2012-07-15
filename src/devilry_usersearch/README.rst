3c927bcab8b1b6973c823e0c968e8cd04317c4de
Espen A. Kristiansen <post@espenak.net>
2012-07-15 04:14:36 +0200

    devilry_usersearch: Skeleton.

---
+++ src/devilry_usersearch/README.rst
@@ -0,0 +1,6 @@
+###################
+devilry_usersearch
+###################
+
+REST APIs that makes it possible to autocomplete usernames and get their
+internal ID in response.
---
+++ src/devilry_usersearch/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+setup(name = 'devilry_extjsextras',
+      description = 'General purpose REST APIs for user searching (typically for autocomplete).',
+      version = '1.0',
+      license='BSD',
+      author = 'Espen Angell Kristiansen',
+      packages=find_packages(exclude=['ez_setup']),
+      install_requires = ['setuptools'],
+      include_package_data=True,
+      long_description = open('README.rst').read(),
+      zip_safe=False,
+      classifiers=[
+                   'Development Status :: 4 - Beta',
+                   'Environment :: Web Environment',
+                   'Framework :: Django',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: JavaScript',
+                   'Programming Language :: Python'
+                  ]
+)
