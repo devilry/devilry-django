36699421291311289eac17efa967eedaae2b6ba8
Espen A. Kristiansen <post@espenak.net>
2012-07-15 04:16:55 +0200

    devilry_usersearch: Cleanup and add to development-base.cfg.

--- src/devilry_usersearch/setup.py
+++ src/devilry_usersearch/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 
-setup(name = 'devilry_extjsextras',
+setup(name = 'devilry_usersearch',
       description = 'General purpose REST APIs for user searching (typically for autocomplete).',
       version = '1.0',
       license='BSD',
