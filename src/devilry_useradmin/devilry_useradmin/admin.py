e2f6814e46b3cbba95345ca4aa5530fb43309a58
Espen A. Kristiansen <post@espenak.net>
2012-07-15 19:39:37 +0200

    devilry_useradmin: Required confirmation when changing is_staff and is_superuser.

--- src/devilry_useradmin/devilry_useradmin/admin.py
+++ src/devilry_useradmin/devilry_useradmin/admin.py
@@ -7,6 +7,10 @@ from django.utils.decorators import method_decorator
 from django.utils.translation import ugettext_lazy as _
 from django.views.decorators.csrf import csrf_protect
 from django.views.decorators.debug import sensitive_post_parameters
+from django.contrib.admin.util import unquote
+from django.core.exceptions import ValidationError
+from django.utils.encoding import force_unicode
+from django.shortcuts import render
 
 from devilry.apps.core.models import DevilryUserProfile
 
@@ -83,15 +87,66 @@ class DevilryUserAdmin(UserAdmin):
         self.inlines = []
         return super(DevilryUserAdmin, self).add_view(*args, **kwargs)
 
+    def _parse_change_form_for_dangerous_attributes(self, request, object_id):
+        obj = self.get_object(request, unquote(object_id))
+        saved_data = {'is_staff': obj.is_staff,
+                      'is_superuser': obj.is_superuser}
+        ModelForm = self.get_form(request, obj)
+        formsets = []
+        inline_instances = self.get_inline_instances(request)
+        form = ModelForm(request.POST, request.FILES, instance=obj)
+        if form.is_valid():
+            return obj, saved_data, form.cleaned_data
+        else:
+            raise ValidationError()
+
+    def _check_for_changes_to_dangerous_attributes(self, request, object_id):
+        obj, saved_data, cleaned_data = self._parse_change_form_for_dangerous_attributes(request, object_id)
+        changed_dangerous_attributes = []
+        for attrname in ('is_staff', 'is_superuser'):
+            if cleaned_data[attrname] != saved_data[attrname] and cleaned_data[attrname] == True:
+                changed_dangerous_attributes.append(attrname)
+        return obj, changed_dangerous_attributes
+
+    def _show_confirm_view(self, request, obj, changed_dangerous_attributes):
+        warnings = {'is_staff': _('Enable the user to log in to this admin site, which is normally only for administrators.'),
+                    'is_superuser': _('Grant the user permission to view, edit and delete EVERYTHING.')}
+        warningmessages = [warnings[attrname] for attrname in changed_dangerous_attributes]
+        request.session['before_confirm_request_data'] = request.POST
+
+        context = {"title": _('Confirm change {username}').format(username=obj.username),
+                   "object_name": force_unicode(self.model._meta.verbose_name),
+                   "object": obj,
+                   "opts": self.model._meta,
+                   "app_label": self.model._meta.app_label,
+                   "warningmessages": warningmessages}
+        return render(request,
+                      'devilry_useradmin/confirm_change_view.django.html',
+                      context, current_app=self.admin_site.name)
+
     @sensitive_post_parameters()
     @csrf_protect_m
     @transaction.commit_on_success
-    def change_view(self, *args, **kwargs):
+    def change_view(self, request, object_id, *args, **kwargs):
         """
         Add InlineDevilryUserProfile to change form.
         """
         self.inlines = [InlineDevilryUserProfile]
-        return super(UserAdmin, self).change_view(*args, **kwargs)
+        if request.method == 'POST':
+            if request.POST.get('dangerous_changes_confirmed'):
+                request.POST = request.session['before_confirm_request_data']
+                del request.session['before_confirm_request_data']
+            else:
+                # Check if any dangerous attributes have been changed, and show confirm page if so
+                try:
+                    obj, changed_dangerous_attributes = self._check_for_changes_to_dangerous_attributes(request, object_id)
+                    if changed_dangerous_attributes:
+                        return self._show_confirm_view(request, obj, changed_dangerous_attributes)
+                except ValidationError:
+                    pass # Ignore errors, since they are handled in super.change_view()
+        return super(UserAdmin, self).change_view(request, object_id, *args, **kwargs)
+
+
 
 
 admin.site.unregister(User)
