e44e92c0ba5a4d0da0ee03113f0cb2ec66a76a5e
Espen A. Kristiansen <post@espenak.net>
2012-07-16 02:04:44 +0200

    devilry_usersearch...UserSearchStore: Usable.

--- src/devilry_usersearch/devilry_usersearch/static/devilry_usersearch/UserSearchStore.js
+++ src/devilry_usersearch/devilry_usersearch/static/devilry_usersearch/UserSearchStore.js
@@ -1,12 +1,4 @@
 Ext.define('devilry_usersearch.UserSearchStore', {
     extend: 'Ext.data.Store',
-    model: 'devilry_usersearch.UserSearchModel',
-
-    search: function(query, callbackFn, callbackScope) {
-        this.proxy.extraParams.query = query;
-        this.load({
-            scope: callbackScope,
-            callback: callbackFn
-        });
-    }
+    model: 'devilry_usersearch.UserSearchModel'
 });
