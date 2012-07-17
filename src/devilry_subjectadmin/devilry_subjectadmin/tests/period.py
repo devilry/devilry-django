f8539d4bd58efc0cd32d1924ff8ae3629e493645
Espen A. Kristiansen <post@espenak.net>
2012-07-17 05:01:22 +0200

    devilry_subjectadmin.tests.period: Common period test utils in separate class.

--- src/devilry_subjectadmin/devilry_subjectadmin/tests/period.py
+++ src/devilry_subjectadmin/devilry_subjectadmin/tests/period.py
@@ -6,8 +6,12 @@ from .base import RenameBasenodeTestMixin
 from .base import DeleteBasenodeTestMixin
 
 
+class PeriodTestCommonMixin(object):
+    def browseToPeriod(self, id):
+        self.browseTo('/period/{id}/'.format(id=id))
 
-class TestPeriodOverview(SubjectAdminSeleniumTestCase, RenameBasenodeTestMixin, DeleteBasenodeTestMixin):
+class TestPeriodOverview(SubjectAdminSeleniumTestCase, PeriodTestCommonMixin,
+                         RenameBasenodeTestMixin, DeleteBasenodeTestMixin):
     renamebutton_id = 'periodRenameButton'
     deletebutton_id = 'periodDeleteButton'
 
@@ -17,25 +21,22 @@ class TestPeriodOverview(SubjectAdminSeleniumTestCase, RenameBasenodeTestMixin, 
                             subjects=['duck9000'],
                             periods=['period1:admin(period1admin):ln(Period One)'])
 
-    def _browseToPeriod(self, id):
-        self.browseTo('/period/{id}/'.format(id=id))
-
     def test_doesnotexists(self):
         self.login('uniadmin')
-        self._browseToPeriod(100000)
+        self.browseToPeriod(100000)
         self.waitForCssSelector('.devilry_extjsextras_alertmessagelist')
         self.assertTrue('403: FORBIDDEN' in self.selenium.page_source)
 
     def test_doesnotexists_superadmin(self):
         self.testhelper.create_superuser('grandma')
         self.login('grandma')
-        self._browseToPeriod(100000)
+        self.browseToPeriod(100000)
         self.waitForCssSelector('.devilry_extjsextras_alertmessagelist')
         self.assertTrue('404: NOT FOUND' in self.selenium.page_source)
 
     def test_breadcrumb(self):
         self.login('period1admin')
-        self._browseToPeriod(self.testhelper.duck9000_period1.id)
+        self.browseToPeriod(self.testhelper.duck9000_period1.id)
         breadcrumbtext = self.get_breadcrumbstring('duck9000')
         self.assertEquals(breadcrumbtext, ['All subjects', 'duck9000', 'period1'])
 
@@ -48,7 +49,7 @@ class TestPeriodOverview(SubjectAdminSeleniumTestCase, RenameBasenodeTestMixin, 
         self.testhelper.add_to_path('uni;duck9000.period2.first:ln(First Assignment)')
 
         self.login('period1admin')
-        self._browseToPeriod(self.testhelper.duck9000_period1.id)
+        self.browseToPeriod(self.testhelper.duck9000_period1.id)
         self.waitForText('Period One')
         self.waitForCssSelector('li.devilry_assignment')
 
@@ -63,7 +64,7 @@ class TestPeriodOverview(SubjectAdminSeleniumTestCase, RenameBasenodeTestMixin, 
 
     def test_dangerous_panel(self):
         self.login('period1admin')
-        self._browseToPeriod(self.testhelper.duck9000_period1.id)
+        self.browseToPeriod(self.testhelper.duck9000_period1.id)
         self.waitForCssSelector('.devilry_subjectadmin_periodoverview')
         self.waitForText('Delete duck9000.period1')
         self.waitForText('Rename duck9000.period1')
@@ -72,13 +73,13 @@ class TestPeriodOverview(SubjectAdminSeleniumTestCase, RenameBasenodeTestMixin, 
 
     def test_rename(self):
         self.login('period1admin')
-        self._browseToPeriod(self.testhelper.duck9000_period1.id)
+        self.browseToPeriod(self.testhelper.duck9000_period1.id)
         self.waitForCssSelector('.devilry_subjectadmin_periodoverview')
         self.rename_test_helper(self.testhelper.duck9000_period1)
 
     def test_rename_failure(self):
         self.login('period1admin')
-        self._browseToPeriod(self.testhelper.duck9000_period1.id)
+        self.browseToPeriod(self.testhelper.duck9000_period1.id)
         self.waitForCssSelector('.devilry_subjectadmin_periodoverview')
         self.rename_test_failure_helper()
 
@@ -87,7 +88,7 @@ class TestPeriodOverview(SubjectAdminSeleniumTestCase, RenameBasenodeTestMixin, 
                             subjects=['sub'],
                             periods=['willbedeleted'])
         self.login('uniadmin')
-        self._browseToPeriod(self.testhelper.sub_willbedeleted.id)
+        self.browseToPeriod(self.testhelper.sub_willbedeleted.id)
         self.waitForCssSelector('.devilry_subjectadmin_periodoverview')
         periodurl = self.selenium.current_url
         self.perform_delete()
@@ -99,7 +100,7 @@ class TestPeriodOverview(SubjectAdminSeleniumTestCase, RenameBasenodeTestMixin, 
                             subjects=['sub'],
                             periods=['willbedeleted:admin(willbedeletedadm)'])
         self.login('willbedeletedadm')
-        self._browseToPeriod(self.testhelper.sub_willbedeleted.id)
+        self.browseToPeriod(self.testhelper.sub_willbedeleted.id)
         self.waitForCssSelector('.devilry_subjectadmin_periodoverview')
         self.click_delete_button()
         self.waitForText('Only superusers can delete non-empty items') # Will time out and fail unless the dialog is shown
@@ -107,7 +108,7 @@ class TestPeriodOverview(SubjectAdminSeleniumTestCase, RenameBasenodeTestMixin, 
     def test_delete_not_empty(self):
         self.testhelper.add_to_path('uni;duck9000.period1.a1:ln(Assignment One)')
         self.login('uniadmin')
-        self._browseToPeriod(self.testhelper.duck9000_period1.id)
+        self.browseToPeriod(self.testhelper.duck9000_period1.id)
         self.waitForCssSelector('.devilry_subjectadmin_periodoverview')
         self.click_delete_button()
         self.waitForText('Only superusers can delete non-empty items') # Will time out and fail unless the dialog is shown
