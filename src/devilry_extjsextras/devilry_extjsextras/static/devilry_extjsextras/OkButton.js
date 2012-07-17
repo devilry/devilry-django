f7ae723945e3e31cbb22a6a995ea435caafb385a
Espen A. Kristiansen <post@espenak.net>
2012-07-17 07:28:49 +0200

    devilry_extjsextras...OkButton: A OK primarybutton.

---
+++ src/devilry_extjsextras/devilry_extjsextras/static/devilry_extjsextras/OkButton.js
@@ -0,0 +1,7 @@
+Ext.define('devilry_extjsextras.OkButton', {
+    extend: 'devilry_extjsextras.PrimaryButton',
+    alias: 'widget.okbutton',
+    cls: 'devilry_extjsextras_okbutton',
+    text: pgettext('uibutton', 'Ok'),
+    width: 70
+});
