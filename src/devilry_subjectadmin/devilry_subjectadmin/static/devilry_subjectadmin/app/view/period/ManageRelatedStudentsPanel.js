3a2d322ff0a22f2c71fe5a0e2c6b2b8ea756d415
Espen A. Kristiansen <post@espenak.net>
2012-07-17 13:17:57 +0200

    devilry_subjectadmin...ManageRelatedStudentsPanel: Search for candidate_id.

--- src/devilry_subjectadmin/devilry_subjectadmin/static/devilry_subjectadmin/app/view/period/ManageRelatedStudentsPanel.js
+++ src/devilry_subjectadmin/devilry_subjectadmin/static/devilry_subjectadmin/app/view/period/ManageRelatedStudentsPanel.js
@@ -7,5 +7,10 @@ Ext.define('devilry_subjectadmin.view.period.ManageRelatedStudentsPanel' ,{
             header: 'Candidate ID',  dataIndex: 'candidate_id', flex: 1
         }
         this.callParent(arguments);
+    },
+
+    searchMatchesRecord: function(query, record) {
+        var match = this.callParent(arguments);
+        return match || this.caseIgnoreContains(record.get('candidate_id'), query);
     }
 });
