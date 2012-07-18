451b29744b2e8500fb9953be6e58890535c3b572
Espen A. Kristiansen <post@espenak.net>
2012-07-18 22:18:56 +0200

    devilry_subjectadmin.rest.group: TestListGroupRest.

--- src/devilry_subjectadmin/devilry_subjectadmin/tests/rest/group.py
+++ src/devilry_subjectadmin/devilry_subjectadmin/tests/rest/group.py
@@ -12,59 +12,109 @@ from devilry.utils.rest_testclient import RestClient
 
 from devilry_subjectadmin.rest.errors import PermissionDeniedError
 from devilry_subjectadmin.rest.group import GroupDao
-from devilry_subjectadmin.rest.group import GroupListingAggregator
 
 
+class TestListGroupRest(TestCase):
+    def setUp(self):
+        self.testhelper = TestHelper()
+        self.client = RestClient()
 
-class TestGroupListingAggator(TestCase):
-    def create_testassignments(self):
-        testhelper = TestHelper()
-        testhelper.add(nodes='uni',
+    def _geturl(self, assignment_id):
+        assignment_id = assignment_id or self.testhelper.sub_p1_a1.id
+        return '/devilry_subjectadmin/rest/group/{0}/'.format(assignment_id)
+
+    def _getas(self, username, assignment_id, **data):
+        self.client.login(username=username, password='test')
+        return self.client.rest_get(self._geturl(assignment_id), **data)
+
+    def _create_testassignments(self):
+        self.testhelper.add(nodes='uni',
                        subjects=['duck1010'],
                        periods=['firstsem'],
-                       assignments=['a1', 'a2'])
-        testhelper.create_superuser("superuser")
-        return testhelper
+                       assignments=['a1:admin(a1admin)', 'a2'])
 
-    def test_tull(self):
-        testhelper = self.create_testdata()
-        assignment1 = testhelper.duck1010_firstsem_a1
-        for assignment in GroupListingAggregator().tull(assignment1.id):
-            print assignment.deadlines.all()
-
-    def create_testdata(self):
-        testhelper = self.create_testassignments()
-
+    def _create_testdata(self):
+        self._create_testassignments()
         for num in xrange(3):
-            path = 'uni;duck1010.firstsem.a1.g{num}:candidate(student{num}):examiner(examiner1).d1'
-            testhelper.add_to_path(path.format(**vars()))
-            group = getattr(testhelper, 'duck1010_firstsem_a1_g{0}'.format(num))
+            path = 'uni;duck1010.firstsem.a1.g{num}:candidate(student{num},extrastudent):examiner(examiner1,extraexaminer).d1'
+            self.testhelper.add_to_path(path.format(**vars()))
+            group = getattr(self.testhelper, 'duck1010_firstsem_a1_g{0}'.format(num))
             group.tags.create(tag="stuff")
             group.tags.create(tag="lownumber")
-            delivery = testhelper.add_delivery(group)
-            testhelper.add_feedback(delivery,
-                                    verdict=dict(grade='A', points=100, is_passing_grade=True))
-        return testhelper
+            delivery = self.testhelper.add_delivery(group)
+            self.testhelper.add_feedback(delivery,
+                                         verdict=dict(grade='A', points=100,
+                                                      is_passing_grade=True))
 
-    def test_read(self):
-        testhelper = self.create_testdata()
-        assignment1 = testhelper.duck1010_firstsem_a1
-        groups = GroupListingAggregator().list(assignment1.id)
-        # We only check a few values here. The most important thing is that the
-        # database queries are sane, since the other stuff is tested in
-        # smaller units
-        self.assertEquals(len(groups), 3)
-        self.assertTrue('examiners' in groups[0].keys())
-        self.assertTrue('students' in groups[0].keys())
-        self.assertTrue('deadlines' in groups[0].keys())
-        fields = set(['id', 'name', 'is_open', 'feedback__grade', 'feedback__points',
-                      'feedback__is_passing_grade', 'feedback__save_timestamp',
-                      'examiners', 'students', 'tags', 'deadlines', 'num_deliveries'])
-        self.assertEquals(set(groups[0].keys()), fields)
-        self.assertEquals(AssignmentGroup.objects.get(id=groups[0]['id']).parentnode_id,
-                          assignment1.id)
+    def test_list_permissiondenied(self):
+        self.testhelper.create_user('nobody')
+        self._create_testassignments()
+        a1 = self.testhelper.duck1010_firstsem_a1
+        content, response = self._getas('nobody', a1.id)
+        self.assertEquals(response.status_code, 403)
 
 
+    def _test_list_as(self, username):
+        self._create_testdata()
+        a1 = self.testhelper.duck1010_firstsem_a1
+        content, response = self._getas(username, a1.id)
+        #from pprint import pprint
+        #pprint(content)
+
+        self.assertEquals(len(content), 3)
+        self.assertEquals(set(content[0].keys()),
+                          set(['name', 'feedback', 'deadlines', 'id', 'etag',
+                               'is_open', 'parentnode', 'candidates',
+                               'examiners', 'num_deliveries']))
+
+        # Properties directly from group
+        self.assertEquals(AssignmentGroup.objects.get(id=content[0]['id']).parentnode_id,
+                          a1.id)
+        self.assertEquals(content[0]['is_open'], False)
+        self.assertEquals(content[0]['name'], 'g0')
+        self.assertEquals(content[0]['num_deliveries'], 1)
+        self.assertEquals(content[0]['parentnode'], a1.id)
+
+        # Feedback
+        feedback = content[0]['feedback']
+        self.assertEquals(set(feedback.keys()),
+                          set(['id', 'grade', 'is_passing_grade', 'points', 'save_timestamp']))
+        self.assertEquals(feedback['grade'], 'A')
+        self.assertEquals(feedback['is_passing_grade'], True)
+        self.assertEquals(feedback['points'], 100)
+
+        # Canididates
+        def get_usernames(users):
+            return [user['user']['username'] for user in users]
+        self.assertEquals(len(content[0]['candidates']), 2)
+        self.assertEquals(set(get_usernames(content[0]['candidates'])),
+                          set(['student0', 'extrastudent']))
+        self.assertEquals(set(content[0]['candidates'][0].keys()),
+                          set(['id', 'candidate_id', 'user']))
+        self.assertEquals(set(content[0]['candidates'][0]['user'].keys()),
+                          set(['email', 'full_name', 'id', 'username']))
+
+        # Examiners
+        self.assertEquals(len(content[0]['examiners']), 2)
+        self.assertEquals(set(get_usernames(content[0]['examiners'])),
+                          set(['examiner1', 'extraexaminer']))
+        self.assertEquals(set(content[0]['examiners'][0].keys()),
+                          set(['id', 'user']))
+        self.assertEquals(set(content[0]['examiners'][0]['user'].keys()),
+                          set(['email', 'full_name', 'id', 'username']))
+
+        # Deadlines
+        self.assertEquals(len(content[0]['deadlines']), 1)
+        deadline = content[0]['deadlines'][0]
+        self.assertEquals(set(deadline.keys()),
+                          set(['id', 'deadline']))
+
+    def test_list(self):
+        self._test_list_as('a1admin')
+
+    def test_list_as_superuser(self):
+        self.testhelper.create_superuser('superuser')
+        self._test_list_as('superuser')
 
 
 class TestCreateStuff(TestCase):
