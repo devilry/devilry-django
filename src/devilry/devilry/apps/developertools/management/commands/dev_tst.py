4686a87ef4fbd0bda9ad951e374b9fd6b09ab4c9
Espen A. Kristiansen <post@espenak.net>
2012-07-18 21:52:27 +0200

    developertools...dev_tst: Changes.

--- src/devilry/devilry/apps/developertools/management/commands/dev_tst.py
+++ src/devilry/devilry/apps/developertools/management/commands/dev_tst.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
-from devilry.apps.core.models import Subject, Period, Assignment
+from devilry.apps.core.models import Subject, Period, Assignment, AssignmentGroup
 from django.db import connection
 
 
@@ -8,17 +8,10 @@ class Command(BaseCommand):
     help = 'tst'
 
     def handle(self, *args, **options):
-        per = Assignment.objects.select_related('parentnode',
-                                                'parentnode__parentnode',
-                                                'parentnode__parentnode',
-                                                'parentnode__parentnode__parentnode',
-                                                'parentnode__parentnode__parentnode__parentnode',
-                                                'parentnode__parentnode__parentnode__parentnode__parentnode').get(id=1)
-        print per.short_name
-        print per.parentnode.short_name
-        print per.parentnode.parentnode.short_name
-        print per.parentnode.parentnode.parentnode.short_name
-        print per.parentnode.parentnode.parentnode.parentnode.short_name
+        groups = AssignmentGroup.objects.prefetch_related('deadlines').all()[:3]
+        for g in groups:
+            print g.name
+            print g.deadlines.all()
         for qry in connection.queries:
             #print qry
             print '{sql} {time}'.format(**qry)
