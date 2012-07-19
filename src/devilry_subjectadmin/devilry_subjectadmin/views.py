9553cbc21b0acd62837150d4197add874192d953
Espen A. Kristiansen <post@espenak.net>
2012-07-19 10:36:19 +0200

    devilry_subjectadmin.views: Minor changes.

--- src/devilry_subjectadmin/devilry_subjectadmin/views.py
+++ src/devilry_subjectadmin/devilry_subjectadmin/views.py
@@ -1,5 +1,3 @@
-from django.views.generic import View
-from django.shortcuts import render
 from django.utils.translation import ugettext as _
 from extjs4.views import Extjs4AppView
 
@@ -9,4 +7,5 @@ class AppView(Extjs4AppView):
     template_name = "devilry_subjectadmin/app.django.html"
     appname = 'devilry_subjectadmin'
     css_staticpath = 'devilry_theme/resources/stylesheets/devilry.css'
+    #css_staticpath = 'extjs4/resources/css/ext-all-gray.css'
     title = _('Devilry - Subject administrator')
