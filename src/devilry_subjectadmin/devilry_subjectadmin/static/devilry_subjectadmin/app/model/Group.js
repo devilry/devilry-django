2d5527eabb2ea531021ed49fd807b92382110824
Espen A. Kristiansen <post@espenak.net>
2012-07-20 03:37:23 +0200

    devilry_subjectadmin...model.Group: Debug format method.

--- src/devilry_subjectadmin/devilry_subjectadmin/static/devilry_subjectadmin/app/model/Group.js
+++ src/devilry_subjectadmin/devilry_subjectadmin/static/devilry_subjectadmin/app/model/Group.js
@@ -26,5 +26,32 @@ Ext.define('devilry_subjectadmin.model.Group', {
         reader: {
             type: 'json'
         }
+    },
+
+    _debugFormatArray: function(array, tpl) {
+        var itemtpl = Ext.create('Ext.XTemplate', tpl);
+        var items = [];
+        Ext.Array.each(array, function(item) {
+            items.push(itemtpl.apply(item));
+        }, this);
+        return '{' + items.join(', ') + '}';
+    },
+
+    debugFormat: function() {
+        var usertpl = [
+            'id:{id}, ',
+            '<tpl if="candidate_id">candidate_id:{candidate_id}, </tpl>',
+            'user.id:{user.id}, username:{user.username}, full_name:{user.full_name}, email:{user.email}'
+        ];
+        var tagtpl = ['id:{id}, tag:{tag}'];
+        return Ext.String.format('id={0}, num_deliveries={1}, name={2}, is_open={3},\n- tags=[{4}],\n- candidates=[{5}],\n- examiners=[{6}]',
+            this.get('id'),
+            this.get('num_deliveries'),
+            this.get('name'),
+            this.get('is_open'),
+            this._debugFormatArray(this.get('tags'), tagtpl),
+            this._debugFormatArray(this.get('candidates'), usertpl),
+            this._debugFormatArray(this.get('examiners'), usertpl)
+        );
     }
 });
