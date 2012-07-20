49c522ed5dec21844d0fbb3887b587b773a4a8ab
Espen A. Kristiansen <post@espenak.net>
2012-07-20 09:27:43 +0200

    devilry_subjectadmin...AutocompleteRelatedUserWidget: Include tags.

--- src/devilry_subjectadmin/devilry_subjectadmin/static/devilry_subjectadmin/app/view/managestudents/AutocompleteRelatedUserWidget.js
+++ src/devilry_subjectadmin/devilry_subjectadmin/static/devilry_subjectadmin/app/view/managestudents/AutocompleteRelatedUserWidget.js
@@ -9,15 +9,22 @@ Ext.define('devilry_subjectadmin.view.managestudents.AutocompleteRelatedUserWidg
     displayField: 'id',
     minChars: 1,
 
-    emptyText: gettext('Select a user from the list, or search for username, full name or email...'),
+    emptyText: gettext('Click the arrow to select a user, or search for username, full name, tags or email...'),
 
     listGetInnerTpl: function() {
         return[
             '<div class="matchlistitem matchlistitem_{user.username}">',
                 '<h3>{user.full_name}</h3>',
-                '<small class="username">{user.username}</small>',
-                '<tpl if="user.email">',
-                    ' <small class="user.email">&lt;{user.email}&gt;</small>',
+                '<div class="userinfo">',
+                    '<small class="username">{user.username}</small>',
+                    '<tpl if="user.email">',
+                        ' <small class="user.email">&lt;{user.email}&gt;</small>',
+                    '</tpl>',
+                '</div>',
+                '<tpl if="tags">',
+                    '<div class="tags">',
+                        '<small>Tags: {tags}</small>',
+                    '</div>',
                 '</tpl>',
             '</div>'
         ].join('');
