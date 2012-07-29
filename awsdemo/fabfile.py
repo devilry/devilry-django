c1dc40b4a90b6908c81f0f5c71fb8fef9b431904
Espen A. Kristiansen <post@espenak.net>
2012-07-30 00:22:39 +0200

    awsdemo.fabfile: Close to usable set of awsfabrictasks tasks.

--- awsdemo/fabfile.py
+++ awsdemo/fabfile.py
@@ -1,14 +1,55 @@
 from fabric.api import task, run, sudo
+from fabric.contrib.files import exists
+from fabric.context_managers import cd
 
 
+repo = 'git://github.com/devilry/devilry-django.git'
+branch = 'master'
+checkout_dir = '/home/ubuntu/devilry-django'
+prodenv_dir = checkout_dir + '/example-productionenv'
+
+
+def _install(*packages):
+    sudo('apt-get install --yes {0}'.format(' '.join(packages)))
 
 @task
 def install_requirements():
     """
-    Run ``uname -a``
+    Install requirements for the demo.
     """
-    sudo('uname -a')
+    _install('python-virtualenv', 'fabric', 'nginx', 'git')
 
+@task
+def checkout_devilry():
+    """
+    Checkout or pull.
+    """
+    if not exists(checkout_dir):
+        run('git clone {repo}'.format(repo=repo))
+        with cd(checkout_dir):
+            run('git checkout {branch}'.format(branch=branch))
+    else:
+        with cd(checkout_dir):
+            run('git checkout {branch}'.format(branch=branch))
+            run('git pull origin {branch}'.format(branch=branch))
+
+
+@task
+def refresh():
+    with cd(prodenv_dir):
+        run('fab refresh')
+
+
+@task
+def run_gunicorn_fg():
+    with cd(prodenv_dir):
+        run('bin/django_production.py run_gunicorn -w 4 127.0.0.1:9000')
+
+
+@task
+def run_nginx_fg():
+    with cd(prodenv_dir):
+        run('nginx -c {prodenv_dir}/server-conf/nginx.conf -g "daemon off;"'.format(prodenv_dir=prodenv_dir))
 
 
 #####################
