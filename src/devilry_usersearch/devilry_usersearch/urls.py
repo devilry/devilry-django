6995488f7445d534bc9287cfd60277d7ad8eed60
Espen A. Kristiansen <post@espenak.net>
2012-07-31 20:56:04 +0200

    devilry_usersearch...urls: Add end to regex.

--- src/devilry_usersearch/devilry_usersearch/urls.py
+++ src/devilry_usersearch/devilry_usersearch/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls.defaults import patterns, url
 from .rest import SearchForUsers
 
 urlpatterns = patterns('devilry_usersearch',
-                       url(r'^search', SearchForUsers.as_view()),
+                       url(r'^search$', SearchForUsers.as_view()),
                       )
 
