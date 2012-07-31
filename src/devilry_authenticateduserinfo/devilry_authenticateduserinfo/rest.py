b6c68ea0711a628c73661641566079d3237c88e7
Espen A. Kristiansen <post@espenak.net>
2012-07-31 13:18:27 +0200

    devilry_authenticateduserinfo...UserInfo: Completed and tested.

--- src/devilry_authenticateduserinfo/devilry_authenticateduserinfo/rest.py
+++ src/devilry_authenticateduserinfo/devilry_authenticateduserinfo/rest.py
@@ -5,6 +5,8 @@ from devilry.apps.core.models.devilryuserprofile import user_is_nodeadmin
 from devilry.apps.core.models.devilryuserprofile import user_is_subjectadmin
 from devilry.apps.core.models.devilryuserprofile import user_is_periodadmin
 from devilry.apps.core.models.devilryuserprofile import user_is_assignmentadmin
+from devilry.apps.core.models.devilryuserprofile import user_is_student
+from devilry.apps.core.models.devilryuserprofile import user_is_examiner
 
 
 
@@ -16,18 +18,18 @@ class UserInfo(View):
     # GET
     An object with the following attributes:
 
-    - ``id`` (internal Devilry ID)
-    - ``username``
-    - ``full_name``
-    - ``email``
-    - ``languagecode`` (preferred language)
-    - ``is_superuser``
-    - ``is_nodeadmin``
-    - ``is_subjectadmin``
-    - ``is_periodadmin``
-    - ``is_assignmentadmin``
-    - ``is_student``
-    - ``is_examiner``
+    - ``id`` (int): Internal Devilry ID. Is never ``null``, and unlike the username, this is never changed.
+    - ``username`` (string): The unique Devilry username for this user. Is never ``null``, however it may be changed by a superuser.
+    - ``full_name`` (string|null): The full name of the user.
+    - ``email`` (string|null): The email address of the user.
+    - ``languagecode`` (string|null): The languagecode of the preferred language.
+    - ``is_superuser`` (boolean): Is the user a superuser?
+    - ``is_nodeadmin`` (boolean): Is the user admin directly on any Node?
+    - ``is_subjectadmin`` (boolean): Is the user admin directly on any Subject?
+    - ``is_periodadmin`` (boolean): Is the user admin directly on any Period?
+    - ``is_assignmentadmin`` (boolean): Is the user admin directly on any Assignment?
+    - ``is_student`` (boolean): Is the user candidate on any AssignmentGroup?
+    - ``is_examiner`` (boolean): Is the user examiner on any AssignmentGroup?
     """
     permissions = (IsAuthenticated,)
 
@@ -44,5 +46,7 @@ class UserInfo(View):
                 'is_nodeadmin': user_is_nodeadmin(user),
                 'is_subjectadmin': user_is_subjectadmin(user),
                 'is_periodadmin': user_is_periodadmin(user),
-                'is_assignmentadmin': user_is_assignmentadmin(user)
+                'is_assignmentadmin': user_is_assignmentadmin(user),
+                'is_student': user_is_student(user),
+                'is_examiner': user_is_examiner(user)
                }
