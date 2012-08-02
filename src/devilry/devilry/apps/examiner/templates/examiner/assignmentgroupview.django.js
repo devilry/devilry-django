0e0f73e42cbbb656af8f1a517c41428e0b57369f
Espen A. Kristiansen <post@espenak.net>
2012-08-02 18:26:09 +0200

    apps.examiner...assignmentgroupview: Breadcrumbs.

--- src/devilry/devilry/apps/examiner/templates/examiner/assignmentgroupview.django.js
+++ src/devilry/devilry/apps/examiner/templates/examiner/assignmentgroupview.django.js
@@ -30,7 +30,31 @@
             assignmentgroupid: {{ objectid }},
             isAdministrator: false,
             canExamine: true,
-            padding: '0 20 0 20'
+            padding: '0 20 0 20',
+            listeners: {
+                assignmentGroupLoaded: function(groupRecord) {
+                    var path = [
+                        groupRecord.get('parentnode__parentnode__parentnode__short_name'),
+                        groupRecord.get('parentnode__parentnode__short_name'),
+                        groupRecord.get('parentnode__short_name')].join('.');
+                    var groupmemebers = [];
+                    Ext.Array.each(groupRecord.get('candidates'), function(candidate) {
+                        groupmemebers.push(candidate.full_name || candidate.identifier);
+                    }, this);
+                    var groupIdent = groupmemebers.join(', ');
+                    if(groupRecord.get('name')) {
+                        groupIdent = Ext.String.format('{0}: {1}', groupRecord.get('name'), groupIdent);
+                    }
+                    if(Ext.String.trim(groupIdent) == '') {
+                        groupIdent = groupRecord.get('id');
+                    }
+                    devilry_header.Breadcrumbs.getInBody().set([{
+                        text: path,
+                        url: Ext.String.format('../assignment/{0}', groupRecord.get('parentnode'))
+                    }], groupIdent);
+                    window.document.title = Ext.String.format('{0} - Devilry', groupIdent);
+                }
+            }
         }]
     });
 {% endblock %}
