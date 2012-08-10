7e77b8a8ff4bbfa7976dfa0adff91a0b179b0687
Espen A. Kristiansen <post@espenak.net>
2012-08-11 00:13:49 +0200

    apps.students.urls: Use the new i18n helper.

--- src/devilry/devilry/apps/student/urls.py
+++ src/devilry/devilry/apps/student/urls.py
@@ -3,13 +3,14 @@ from django.contrib.auth.decorators import login_required
 from django.views.i18n import javascript_catalog
 
 from devilry.restful.forbidden import forbidden_if_not_authenticated
+from devilry_settings.i18n import get_javascript_catalog_packages
 from restful import student_restful
 from views import (MainView, AddDeliveryView,
                    FileUploadView, AssignmentGroupView,
                    FileDownloadView, ShowDeliveryView,
                    CompressedFileDownloadView)
 
-i18n_packages = ('devilry.apps.core',)
+i18n_packages = get_javascript_catalog_packages('devilry.apps.core')
 
 urlpatterns = patterns('devilry.apps.student',
                        url(r'^$', login_required(MainView.as_view()), name='student'),
