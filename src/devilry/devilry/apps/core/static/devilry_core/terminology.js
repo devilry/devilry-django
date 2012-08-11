224038a88d30ab446f84a14887f6c8dd7abda570
Espen A. Kristiansen <post@espenak.net>
2012-08-11 07:02:43 +0200

    apps.core: lowecase terminology and some extra words.

--- src/devilry/devilry/apps/core/static/devilry_core/terminology.js
+++ src/devilry/devilry/apps/core/static/devilry_core/terminology.js
@@ -5,11 +5,35 @@ function gettext_noop(key) {
 
 var terms = [
     gettext_noop('Node'), gettext_noop('Nodes'),
+    gettext_noop('node'), gettext_noop('nodes'),
+
     gettext_noop('Subject'), gettext_noop('Subjects'),
+    gettext_noop('subject'), gettext_noop('subjects'),
+
     gettext_noop('Period'), gettext_noop('Periods'),
+    gettext_noop('period'), gettext_noop('periods'),
+
     gettext_noop('Assignment'), gettext_noop('Assignments'),
+    gettext_noop('assignment'), gettext_noop('assignments'),
+
     gettext_noop('Group'), gettext_noop('Groups'),
+    gettext_noop('group'), gettext_noop('groups'),
+
     gettext_noop('Deadline'), gettext_noop('Deadlines'),
+    gettext_noop('deadline'), gettext_noop('deadlines'),
+
     gettext_noop('Delivery'), gettext_noop('Deliveries'),
-    gettext_noop('Examiner'), gettext_noop('Examiners')
+    gettext_noop('delivery'), gettext_noop('deliveries'),
+
+    gettext_noop('Examiner'), gettext_noop('Examiners'),
+    gettext_noop('examiner'), gettext_noop('examiners'),
+
+    gettext_noop('Student'), gettext_noop('Students'),
+    gettext_noop('student'), gettext_noop('students'),
+
+    gettext_noop('Yes'),
+    gettext_noop('No'),
+    gettext_noop('Ok'),
+    gettext_noop('Cancel'),
+    gettext_noop('Error')
 ]
