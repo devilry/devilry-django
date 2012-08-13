f1db6570fbc5e2ad35469ecca6f077db43488d93
Espen A. Kristiansen <post@espenak.net>
2012-08-13 18:37:56 +0200

    apps.examiner: i18n view.

--- src/devilry/devilry/apps/examiner/templates/examiner/base.django.js
+++ src/devilry/devilry/apps/examiner/templates/examiner/base.django.js
@@ -1,6 +1,10 @@
 {% extends "theme/common.django.html" %}
 {% load extjs %}
 
+{% block i18nimport %}
+    <script type="text/javascript" src="{% url devilry_examiner_i18n %}"></script>
+{% endblock %}
+
 {% block imports %}
     {{ block.super }}
     Ext.require('devilry.extjshelpers.searchwidget.SearchWidget');
--- src/devilry/devilry/apps/examiner/urls.py
+++ src/devilry/devilry/apps/examiner/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls.defaults import patterns, url
 from django.contrib.auth.decorators import login_required
+from django.views.i18n import javascript_catalog
 
+from devilry_settings.i18n import get_javascript_catalog_packages
 from restful import examiner_restful
-from views import (MainView, AssignmentGroupView, 
+from views import (MainView, AssignmentGroupView,
                     AssignmentView, CompressedFileDownloadView)
+
+i18n_packages = get_javascript_catalog_packages('devilry.apps.examiner', 'devilry.apps.core')
 
 urlpatterns = patterns('devilry.apps.examiner',
                        url(r'^$', login_required(MainView.as_view()), name='examiner'),
@@ -12,7 +16,10 @@ urlpatterns = patterns('devilry.apps.examiner',
                        url(r'^assignment/(?P<assignmentid>\d+)$',
                            login_required(AssignmentView.as_view()), name='examiner-assignment-view'),
                        url(r'^assignment/compressedfiledownload/(?P<assignmentid>\d+)$',
-                           login_required(CompressedFileDownloadView.as_view()))
+                           login_required(CompressedFileDownloadView.as_view())),
+                       url('^i18n.js$', javascript_catalog,
+                           kwargs={'packages': i18n_packages},
+                           name='devilry_examiner_i18n')
                       )
 
 urlpatterns += examiner_restful
