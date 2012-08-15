005ad811c78cfe3e403a0483073932b809bc5cb5
Espen A. Kristiansen <post@espenak.net>
2012-08-15 04:10:02 +0200

    devilry_student...browsehistory: Link to the new group view.

--- src/devilry_student/devilry_student/static/devilry_student/app/view/browsehistory/Overview.js
+++ src/devilry_student/devilry_student/static/devilry_student/app/view/browsehistory/Overview.js
@@ -17,8 +17,9 @@ Ext.define('devilry_student.view.browsehistory.Overview' ,{
     items: [{
         xtype: 'student-browseperiods',
         urlCreateFn: function(groupRecord) {
-            return Ext.String.format('{0}/student/assignmentgroup/{1}',
-                DevilrySettings.DEVILRY_URLPATH_PREFIX, groupRecord.get('id'));
+            //return Ext.String.format('{0}/student/assignmentgroup/{1}',
+                //DevilrySettings.DEVILRY_URLPATH_PREFIX, groupRecord.get('id'));
+            return Ext.String.format('#/group/{0}/', groupRecord.get('id'));
 
         }
     }]
