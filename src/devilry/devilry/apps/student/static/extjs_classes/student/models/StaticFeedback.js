60974794b7e1a7f1233ebe84fabefed316804e79
Espen A. Kristiansen <post@espenak.net>
2012-08-19 20:46:23 +0200

    apps.student: Removed the old extjs_classes.

--- src/devilry/devilry/apps/student/static/extjs_classes/student/models/StaticFeedback.js
+++
@@ -1,7 +0,0 @@
-Ext.define('devilry.student.models.StaticFeedback', {
-    extend: 'devilry.extjshelpers.models.StaticFeedback',
-    belongsTo: 'devilry.student.models.Delivery',
-    proxy: Ext.create('devilry.extjshelpers.RestProxy', {
-        url: '/student/restfulsimplifiedstaticfeedback/'
-    })
-});
