a6720c512191c71ed4d8f78c56d8a7924d5faa00
Espen A. Kristiansen <post@espenak.net>
2012-08-21 19:11:46 +0200

    devilry_subjectadmin.rest.deadlinesbulk: Working put and get.

--- src/devilry_subjectadmin/devilry_subjectadmin/rest/deadlinesbulk.py
+++ src/devilry_subjectadmin/devilry_subjectadmin/rest/deadlinesbulk.py
@@ -1,20 +1,64 @@
 from datetime import datetime
+from django import forms
+from django.db import transaction
+from django.core.urlresolvers import reverse
+from django.core.exceptions import ValidationError
 from djangorestframework.views import View
+from djangorestframework.resources import FormResource, ModelResource
 from djangorestframework.permissions import IsAuthenticated
 from .auth import IsAssignmentAdmin
+import hashlib
 
 from devilry.apps.core.models import Deadline
+from devilry.apps.core.models import AssignmentGroup
 from devilry.utils.restformat import format_datetime
 from devilry.utils.restformat import format_timedelta
-from .log import logger
+from .group import GroupSerializer
+from .errors import NotFoundError
+from .errors import ValidationErrorResponse
+#from .log import logger
 
+
+ID_DATETIME_FORMAT = '%Y-%m-%dT%H_%M_%S'
+
+
+def sha1hash(text):
+    m = hashlib.sha1()
+    m.update(text.encode('utf-8'))
+    return m.hexdigest()
+
+def encode_bulkdeadline_id(deadline):
+    formatted_datetime = deadline.deadline.strftime(ID_DATETIME_FORMAT)
+    if deadline.text:
+        texthash = sha1hash(deadline.text)
+    else:
+        texthash = ''
+    return '{0}--{1}'.format(formatted_datetime, texthash)
+
+def decode_bulkdeadline_id(bulkdeadline_id):
+    formatted_datetime, texthash = bulkdeadline_id.split('--')
+    deadline = datetime.strptime(formatted_datetime, ID_DATETIME_FORMAT)
+    return deadline, texthash
+
+
+def texthashmatch(texthash, text):
+    if text == None:
+        return bool(texthash) == False
+    else:
+        return texthash == sha1hash(text)
 
 
 class DeadlinesBulkRest(View):
     """
+    Handle deadlines on an assignment in bulk.
+
+    # GET
     List all deadlines on an assignment with newest deadline firsts. Deadlines
     with exactly the same ``deadline`` and ``text`` are collapsed into a single
     entry in the list, with the number of groups listed.
+
+    # POST
+    Create a new deadline.
     """
     permissions = (IsAuthenticated, IsAssignmentAdmin)
 
@@ -22,22 +66,27 @@ class DeadlinesBulkRest(View):
         """
         Serialize ``Deadline``-object as plain python.
         """
-        return {'deadline': format_datetime(deadline.deadline),
+        bulkdeadline_id = encode_bulkdeadline_id(deadline)
+        return {'bulkdeadline_id': bulkdeadline_id,
+                'deadline': format_datetime(deadline.deadline),
                 'in_the_future': deadline.deadline > self.now,
                 'offset_from_now': format_timedelta(self.now - deadline.deadline),
+                'url': reverse('devilry_subjectadmin_rest_deadlinesbulkinstance',
+                               kwargs={'id': self.assignment_id,
+                                       'bulkdeadline_id': bulkdeadline_id}),
                 'text': deadline.text}
 
     def _get_distinct_deadlines(self, deadlines):
         distinct_deadlines = {}
-        keyformat = '{deadline}:{text}'
+        idformat = '{deadline}:{text_firstchar}'
         for deadline in deadlines:
-            key = keyformat.format(deadline=deadline.deadline, text=deadline.text)
-            if key in distinct_deadlines:
-                distinct_deadlines[key]['groupcount'] += 1
+            bulkid = encode_bulkdeadline_id(deadline)
+            if bulkid in distinct_deadlines:
+                distinct_deadlines[bulkid]['groupcount'] += 1
             else:
                 serialized_deadline = self._serialize_deadline(deadline)
                 serialized_deadline['groupcount'] = 1
-                distinct_deadlines[key] = serialized_deadline
+                distinct_deadlines[bulkid] = serialized_deadline
         return distinct_deadlines.values()
 
     def _deadline_cmp(self, a, b):
@@ -58,4 +107,127 @@ class DeadlinesBulkRest(View):
         self.assignment_id = id
         self.now = datetime.now()
         distinct_deadlines = self._aggregate_deadlines()
-        return {'deadlines': distinct_deadlines}
+        return distinct_deadlines
+
+
+
+
+class UpdateDeadlinesBulkRestForm(forms.Form):
+    deadline = forms.DateTimeField(required=True)
+    text = forms.CharField(required=False)
+
+
+class UpdateDeadlinesBulkRestResource(FormResource):
+    form = UpdateDeadlinesBulkRestForm
+
+    def deadline(self, dct):
+        return format_datetime(dct['deadline'])
+
+
+class GroupsListResource(ModelResource):
+    model = AssignmentGroup
+    fields = ('id', 'name', 'etag', 'is_open', 'num_deliveries',
+              'parentnode', 'feedback', 'candidates')
+
+    def parentnode(self, instance):
+        return int(instance.parentnode_id)
+
+    def feedback(self, instance):
+        return GroupSerializer(instance).serialize_feedback()
+
+    def deadlines(self, instance):
+        return GroupSerializer(instance).serialize_deadlines()
+
+    #def tags(self, instance):
+        #return GroupSerializer(instance).serialize_tags()
+
+    #def examiners(self, instance):
+        #return GroupSerializer(instance).serialize_examiners()
+
+    def candidates(self, instance):
+        return GroupSerializer(instance).serialize_candidates()
+
+
+
+class UpdateDeadlinesBulkRest(View):
+    """
+    # About bulkdeadline_id
+    The ``bulkdeadline_id`` is a generated string on this format:
+
+        <datetime>--<texthash>
+
+    - ``<datetime>`` is the datetime of the deadaline formatted as
+      ``YYYY-MM-DDThh_mm_ss``.
+    - ``<texthash>`` is the first character of
+      the deadline text. If the deadline text is ``null`` or empty string,
+      ``<texthash>`` will be an empty string.
+
+
+    # PUT
+    Update all deadlines matching the ``bulkdeadline_id`` given as the last
+    item in the url path.
+
+    # GET
+    List groups in this deadline.
+    """
+    permissions = (IsAuthenticated, IsAssignmentAdmin)
+    resource = UpdateDeadlinesBulkRestResource
+
+    def _deadlineqry(self):
+        assignment_id = self.kwargs['id']
+        bulkdeadline_id = self.kwargs['bulkdeadline_id']
+        deadline_datetime, texthash = decode_bulkdeadline_id(bulkdeadline_id)
+        qry = Deadline.objects.filter(assignment_group__parentnode=assignment_id,
+                                      deadline=deadline_datetime)
+        qry = qry.select_related('assignment_group', 'assignment_group__feedback')
+        qry = qry.prefetch_related('assignment_group__examiners',
+                                   'assignment_group__examiners__user',
+                                   'assignment_group__examiners__user__devilryuserprofile',
+                                   'assignment_group__candidates',
+                                   'assignment_group__candidates__student',
+                                   'assignment_group__candidates__student__devilryuserprofile')
+        # Filter out deadlines that do not match the texthash
+        def hashmatch(deadline):
+            match = texthashmatch(texthash, deadline.text)
+            return match
+        deadlines = filter(hashmatch, qry)
+        if len(deadlines) == 0:
+            raise NotFoundError('No deadline matching: {0}'.format(bulkdeadline_id))
+        return deadlines
+
+    def _as_groupobjects(self, deadlines):
+        return map(lambda deadline: deadline.assignment_group, deadlines)
+
+    def _create_response(self, deadline, groups):
+        return {'bulkdeadline_id': encode_bulkdeadline_id(deadline),
+                'deadline': deadline.deadline,
+                'text': deadline.text,
+                'groups': GroupsListResource().serialize(groups)}
+
+    def _update_deadlines(self, deadlines):
+        new_deadline = self.CONTENT['deadline']
+        text = self.CONTENT['text']
+        with transaction.commit_manually():
+            try:
+                for deadline in deadlines:
+                    deadline.deadline = new_deadline
+                    deadline.text = text
+                    deadline.full_clean()
+                    deadline.save()
+            except ValidationError as e:
+                raise ValidationErrorResponse(e)
+            else:
+                transaction.commit()
+        return deadlines
+
+    def get(self, request, id, bulkdeadline_id):
+        deadlines = self._deadlineqry()
+        groups = self._as_groupobjects(deadlines)
+        return self._create_response(deadlines[0], groups)
+
+    def put(self, request, id, bulkdeadline_id):
+        bulkdeadline_id = self.kwargs['bulkdeadline_id']
+        deadlines = self._deadlineqry()
+        deadlines = self._update_deadlines(deadlines)
+        groups = self._as_groupobjects(deadlines)
+        return self._create_response(deadlines[0], groups)
--- src/devilry_subjectadmin/devilry_subjectadmin/tests/rest/deadlinesbulk.py
+++ src/devilry_subjectadmin/devilry_subjectadmin/tests/rest/deadlinesbulk.py
@@ -1,7 +1,14 @@
+from datetime import datetime, timedelta
 from django.test import TestCase
 
 from devilry.apps.core.testhelper import TestHelper
 from devilry.utils.rest_testclient import RestClient
+from devilry.apps.core.models import Deadline
+from devilry.utils.restformat import format_datetime
+
+from devilry_subjectadmin.rest.deadlinesbulk import encode_bulkdeadline_id
+from devilry_subjectadmin.rest.deadlinesbulk import decode_bulkdeadline_id
+from devilry_subjectadmin.rest.deadlinesbulk import sha1hash
 
 
 class TestDeadlinesBulkRest(TestCase):
@@ -12,7 +19,7 @@ class TestDeadlinesBulkRest(TestCase):
                             periods=['p1:begins(-2)'], # 2 months ago
                             assignments=['a1:admin(adm):pub(0)']) # 0 days after period begins
         self.client = RestClient()
-        self.url = '/devilry_subjectadmin/rest/deadlinesbulk/{0}'.format(self.testhelper.sub_p1_a1.id)
+        self.url = '/devilry_subjectadmin/rest/deadlinesbulk/{0}/'.format(self.testhelper.sub_p1_a1.id)
 
     def _listas(self, username, **data):
         self.client.login(username=username, password='test')
@@ -21,7 +28,7 @@ class TestDeadlinesBulkRest(TestCase):
     def test_get_empty(self):
         content, response = self._listas('adm')
         self.assertEquals(response.status_code, 200)
-        self.assertEquals(len(content['deadlines']), 0)
+        self.assertEquals(len(content), 0)
 
     def test_get_simple(self):
         for groupnum in xrange(3):
@@ -29,11 +36,11 @@ class TestDeadlinesBulkRest(TestCase):
             self.testhelper.add_to_path('uni;sub.p1.a1.g{0}.d1:ends(5)'.format(groupnum))
         content, response = self._listas('adm')
         self.assertEquals(response.status_code, 200)
-        self.assertEquals(len(content['deadlines']), 1)
-        d1 = content['deadlines'][0]
+        self.assertEquals(len(content), 1)
+        d1 = content[0]
         self.assertEquals(set(d1.keys()),
                           set(['deadline', 'text', 'groupcount', 'offset_from_now',
-                               'in_the_future']))
+                               'in_the_future', 'bulkdeadline_id']))
         self.assertEquals(d1['groupcount'], 3)
         self.assertEquals(d1['in_the_future'], False)
         self.assertEquals(d1['text'], None)
@@ -47,9 +54,9 @@ class TestDeadlinesBulkRest(TestCase):
         self.testhelper.sub_p1_a1_g1_d1.save()
         content, response = self._listas('adm')
         self.assertEquals(response.status_code, 200)
-        self.assertEquals(len(content['deadlines']), 2)
-        g2_g3_d1 = content['deadlines'][0]
-        g1_d1 = content['deadlines'][1]
+        self.assertEquals(len(content), 2)
+        g2_g3_d1 = content[0]
+        g1_d1 = content[1]
         self.assertEquals(g1_d1['text'], 'Test')
         self.assertEquals(g1_d1['groupcount'], 1)
         self.assertEquals(g2_g3_d1['text'], None)
@@ -64,9 +71,9 @@ class TestDeadlinesBulkRest(TestCase):
             self.testhelper.add_to_path('uni;sub.p1.a1.g{0}.d2:ends(70)'.format(groupnum))
         content, response = self._listas('adm')
         self.assertEquals(response.status_code, 200)
-        self.assertEquals(len(content['deadlines']), 2)
-        d1 = content['deadlines'][1]
-        d2 = content['deadlines'][0]
+        self.assertEquals(len(content), 2)
+        d1 = content[1]
+        d2 = content[0]
         self.assertEquals(d1['groupcount'], 3)
         self.assertEquals(d1['in_the_future'], False)
         self.assertEquals(d2['groupcount'], 2)
@@ -75,4 +82,115 @@ class TestDeadlinesBulkRest(TestCase):
     def test_get_nobody(self):
         self.testhelper.create_user('nobody')
         content, response = self._listas('nobody')
+        self.assertEquals(response.status_code, 403)
+
+
+class TestUpdateDeadlinesBulkRest(TestCase):
+    def setUp(self):
+        self.testhelper = TestHelper()
+        self.testhelper.add(nodes='uni',
+                            subjects=['sub'],
+                            periods=['p1:begins(-2)']) # 2 months ago 
+        self.testhelper.sub_p1.start_time = datetime(2000, 1, 1, 22, 30, 49)
+        self.testhelper.sub_p1.save()
+        self.testhelper.add_to_path('uni;sub.p1.a1:admin(adm):pub(0)') # 0 days after period begins + 2 sec
+        self.client = RestClient()
+
+        for groupnum in xrange(3):
+            # deadline 5 days after assignment starts
+            self.testhelper.add_to_path('uni;sub.p1.a1.g{0}.d1:ends(5)'.format(groupnum))
+
+
+    def _geturl(self):
+        bulkdeadline_id = encode_bulkdeadline_id(self.testhelper.sub_p1_a1_g1_d1)
+        return '/devilry_subjectadmin/rest/deadlinesbulk/{0}/{1}'.format(self.testhelper.sub_p1_a1.id,
+                                                                         bulkdeadline_id)
+
+    def test_encode_unique_bulkdeadline_id(self):
+        d = Deadline(deadline=datetime(2000, 12, 24, 22, 30, 49))
+        self.assertEquals(encode_bulkdeadline_id(d),
+                          '2000-12-24T22_30_49--')
+        d.text = 'Hello world'
+        self.assertEquals(encode_bulkdeadline_id(d),
+                          '2000-12-24T22_30_49--{0}'.format(sha1hash('Hello world')))
+        # Ensure unicode works
+        d.text = u'\u00e5ello world'
+        self.assertEquals(encode_bulkdeadline_id(d),
+                          '2000-12-24T22_30_49--{0}'.format(sha1hash(u'\u00e5ello world')))
+
+
+    def test_decode_unique_bulkdeadline_id(self):
+        self.assertEquals(decode_bulkdeadline_id('2000-12-24T22_30_49--'),
+                          (datetime(2000, 12, 24, 22, 30, 49), ''))
+        self.assertEquals(decode_bulkdeadline_id('2000-12-24T22_30_49--{0}'.format(sha1hash('Hello world'))),
+                          (datetime(2000, 12, 24, 22, 30, 49), sha1hash('Hello world')))
+
+
+    def _putas(self, username, data):
+        self.client.login(username=username, password='test')
+        return self.client.rest_put(self._geturl(), data)
+
+    def test_put(self):
+        self.assertEquals(self.testhelper.sub_p1_a1.publishing_time, datetime(2000, 1, 1, 22, 30, 51))
+        self.assertEquals(self.testhelper.sub_p1_a1_g1_d1.deadline,
+                          datetime(2000, 1, 1, 22, 30, 51) + timedelta(days=5))
+
+        new_deadline = datetime(2004, 12, 24, 20, 30, 40)
+        content, response = self._putas('adm', {'deadline': format_datetime(new_deadline),
+                                                'text': 'Hello'})
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(decode_bulkdeadline_id(content['bulkdeadline_id'])[0],
+                          new_deadline)
+        self.assertEquals(len(content['groups']), 3)
+        for groupnum in xrange(3):
+            deadline_id = getattr(self.testhelper, 'sub_p1_a1_g{0}_d1'.format(groupnum)).id
+            deadline = Deadline.objects.get(id=deadline_id)
+            self.assertEquals(deadline.deadline, new_deadline)
+            self.assertEquals(deadline.text, 'Hello')
+
+    def test_put_nonetext(self):
+        self.assertEquals(self.testhelper.sub_p1_a1.publishing_time, datetime(2000, 1, 1, 22, 30, 51))
+        self.assertEquals(self.testhelper.sub_p1_a1_g1_d1.deadline,
+                          datetime(2000, 1, 1, 22, 30, 51) + timedelta(days=5))
+
+        new_deadline = datetime(2004, 12, 24, 20, 30, 40)
+        content, response = self._putas('adm', {'deadline': format_datetime(new_deadline),
+                                                'text': None})
+        self.assertEquals(response.status_code, 200)
+        for groupnum in xrange(3):
+            deadline_id = getattr(self.testhelper, 'sub_p1_a1_g{0}_d1'.format(groupnum)).id
+            deadline = Deadline.objects.get(id=deadline_id)
+            self.assertEquals(deadline.deadline, new_deadline)
+            self.assertEquals(deadline.text, '')
+
+    def test_put_before_publishingtime(self):
+        new_deadline = datetime(1990, 1, 1, 20, 30, 40)
+        content, response = self._putas('adm', {'deadline': format_datetime(new_deadline),
+                                                'text': None})
+        self.assertEquals(response.status_code, 400)
+        self.assertEquals(content['errors'], ['Deadline cannot be before publishing time.'])
+
+    def test_put_nobody(self):
+        self.testhelper.create_user('nobody')
+        content, response = self._putas('nobody', {})
+        self.assertEquals(response.status_code, 403)
+
+
+    def _getas(self, username, **data):
+        self.client.login(username=username, password='test')
+        return self.client.rest_get(self._geturl(), **data)
+
+    def test_get(self):
+        content, response = self._getas('adm')
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(decode_bulkdeadline_id(content['bulkdeadline_id'])[0],
+                          self.testhelper.sub_p1_a1_g1_d1.deadline)
+        self.assertEquals(content['deadline'],
+                          format_datetime(self.testhelper.sub_p1_a1_g1_d1.deadline))
+        self.assertEquals(content['text'], None)
+        self.assertEquals(len(content['groups']), 3)
+
+    def test_get_nobody(self):
+        self.testhelper.create_user('nobody')
+        content, response = self._getas('nobody')
         self.assertEquals(response.status_code, 403)
