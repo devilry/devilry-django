83c7dafe02abf20dc4746f6008652b5cf82ea4c2
Espen A. Kristiansen <post@espenak.net>
2012-08-24 08:34:16 +0200

    Revert "utils.restformat: Handle negative deltas correctly."
    This reverts commit 6f0a79e9396e37da3cfc709145cd0365902743ff.

--- src/devilry/devilry/utils/restformat.py
+++ src/devilry/devilry/utils/restformat.py
@@ -4,11 +4,7 @@ def format_datetime(datetime):
 def format_timedelta(timedelta_obj):
     hours, remainder = divmod(timedelta_obj.seconds, 3600)
     minutes, seconds = divmod(remainder, 60)
-    if timedelta_obj.days >= 0:
-        days = timedelta_obj.days
-    else:
-        days = abs(timedelta_obj.days) - 1
-    return {'days': days,
+    return {'days': abs(timedelta_obj.days),
             'hours': hours,
             'minutes': minutes,
             'seconds': seconds}
