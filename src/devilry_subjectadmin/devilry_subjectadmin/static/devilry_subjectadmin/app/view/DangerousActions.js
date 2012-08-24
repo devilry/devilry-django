6f8c00b749af7c44b5c74e15d7149088c5ad32a2
Espen A. Kristiansen <post@espenak.net>
2012-08-24 09:39:21 +0200

    devilry_subjectadmin...DangerousActions: New widget.

---
+++ src/devilry_subjectadmin/devilry_subjectadmin/static/devilry_subjectadmin/app/view/DangerousActions.js
@@ -0,0 +1,35 @@
+Ext.define('devilry_subjectadmin.view.DangerousActions', {
+    extend: 'Ext.container.Container',
+    alias: 'widget.dangerousactions',
+    cls: 'devilry_subjectadmin_dangerousactions',
+
+    requires: [
+    ],
+
+    title: gettext('Dangerous actions'),
+
+    initComponent: function() {
+        Ext.apply(this, {
+            items: [{
+                xtype: 'box',
+                cls: 'bootstrap',
+                //margin: '0 0 2 0',
+                itemId: 'header',
+                tpl: '<h2>{heading}</h2>',
+                data: {
+                    heading: this.title
+                }
+            }, {
+                xtype: 'panel',
+                bodyPadding: 10,
+                layout: 'anchor',
+                defaults: {
+                    anchor: '100%',
+                    margin: '10 0 0 0'
+                },
+                items: this.items
+            }]
+        });
+        this.callParent(arguments);
+    }
+});
