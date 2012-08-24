467f2cf16922aaee8b44c4c0c26e31e6e319077b
Espen A. Kristiansen <post@espenak.net>
2012-08-24 02:23:32 +0200

    devilry_subjectadmin: fabfile for i18n.

---
+++ src/devilry_subjectadmin/devilry_subjectadmin/fabfile.py
@@ -0,0 +1,13 @@
+from fabric.api import local, task
+
+@task
+def makemessages(langcode):
+    local(('../../../devenv/bin/django_dev.py makemessages -d djangojs -l {0} '
+           '-i "static/devilry_student/app-all.js" '
+           '-i "static/devilry_student/all-classes.js"'
+          ).format(langcode))
+
+
+@task
+def compilemessages():
+    local('../../../devenv/bin/django_dev.py compilemessages')
