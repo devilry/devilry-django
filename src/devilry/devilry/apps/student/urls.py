dd231cda7ec31f3db45878c0f6cc0e9025336882
Espen A. Kristiansen <post@espenak.net>
2012-08-25 07:38:48 +0200

    apps.student.urls: Removed all old views and imports.

--- src/devilry/devilry/apps/student/urls.py
+++ src/devilry/devilry/apps/student/urls.py
@@ -1,24 +1,13 @@
 from django.conf.urls.defaults import patterns, url
 from django.contrib.auth.decorators import login_required
 
-from devilry.restful.forbidden import forbidden_if_not_authenticated
-from restful import student_restful
-from views import (MainView, AddDeliveryView,
-                   FileUploadView, AssignmentGroupView,
-                   FileDownloadView, ShowDeliveryView,
-                   CompressedFileDownloadView)
+from .restful import student_restful
+from .views import FileDownloadView
+from .views import CompressedFileDownloadView
+
+
 
 urlpatterns = patterns('devilry.apps.student',
-                       url(r'^$', login_required(MainView.as_view())),
-                       #url(r'^add-delivery/(?P<assignmentgroupid>\d+)$', 
-                           #login_required(AddDeliveryView.as_view()), 
-                           #name='devilry-add-delivery'),
-                       #url(r'^add-delivery/fileupload/(?P<assignmentgroupid>\d+)$',
-                           #forbidden_if_not_authenticated(FileUploadView.as_view()),
-                           #name='file-upload'),
-                       #url(r'^assignmentgroup/(?P<assignmentgroupid>\d+)$',
-                           #login_required(AssignmentGroupView.as_view()),
-                           #name='student-show-assignmentgroup'),
                        #url(r'^show-delivery/(?P<deliveryid>\d+)$',
                            #login_required(ShowDeliveryView.as_view()),
                            #name='show-delivery'),
@@ -27,10 +16,7 @@ urlpatterns = patterns('devilry.apps.student',
                            name='devilry-delivery-file-download'),
                        url(r'^show-delivery/compressedfiledownload/(?P<deliveryid>\d+)$',
                            login_required(CompressedFileDownloadView.as_view()),
-                           name='devilry-delivery-download-all-zip'),
-                       #url(r'^show-delivery/tarfiledownload/(?P<deliveryid>\d+)$',
-                           #login_required(TarFileDownloadView.as_view()),
-                           #name='tar-file-download')
+                           name='devilry-delivery-download-all-zip')
                        )
 
 urlpatterns += student_restful
