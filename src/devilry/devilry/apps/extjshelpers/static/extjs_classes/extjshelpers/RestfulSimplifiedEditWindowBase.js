82500407da2cc83d52ae636fa11a8a3960c73d4a
Espen A. Kristiansen <post@espenak.net>
2012-08-27 07:03:19 +0200

    apps.extjshelpers...RestfulSimplifiedEditWindowBase: Autoresize when to small.

--- src/devilry/devilry/apps/extjshelpers/static/extjs_classes/extjshelpers/RestfulSimplifiedEditWindowBase.js
+++ src/devilry/devilry/apps/extjshelpers/static/extjs_classes/extjshelpers/RestfulSimplifiedEditWindowBase.js
@@ -7,18 +7,11 @@ Ext.define('devilry.extjshelpers.RestfulSimplifiedEditWindowBase', {
     maximizable: true,
     modal: true,
 
-    config: {
-        /**
-         * @cfg
-         * The {@link devilry.extjshelpers.RestfulSimplifiedEditPanel} to use for editing.
-         */
-        editpanel: undefined
-    },
-    
-    constructor: function(config) {
-        this.callParent([config]);
-        this.initConfig(config);
-    },
+    /**
+     * @cfg
+     * The {@link devilry.extjshelpers.RestfulSimplifiedEditPanel} to use for editing.
+     */
+    editpanel: undefined,
     
     initComponent: function() {
         var me = this;
@@ -28,6 +21,8 @@ Ext.define('devilry.extjshelpers.RestfulSimplifiedEditWindowBase', {
             this.width = form.suggested_windowsize.width,
             this.height = form.suggested_windowsize.height
         }
+        this._preferredWidth = this.width || 700;
+        this._preferredHeight = this.height || 500;
 
         this.editpanel.addListener('saveSucess', function(record) {
             me.onSaveSuccess(record);
@@ -36,10 +31,40 @@ Ext.define('devilry.extjshelpers.RestfulSimplifiedEditWindowBase', {
         Ext.apply(this, {
             items: this.editpanel
         });
+        this._setupAutosizing();
         this.callParent(arguments);
     },
 
     onSaveSuccess: function(record) {
         throw "Must implement onSaveSuccess()"
+    },
+
+
+    _setupAutosizing: function() {
+        Ext.fly(window).on('resize', this._onWindowResize, this);
+        this.on('show', this._onShowWindow, this);
+    },
+    _onShowWindow: function() {
+        this._setSizeAndPosition();
+    },
+    _onWindowResize: function() {
+        if(this.isVisible() && this.isFloating()) {
+            this._setSizeAndPosition();
+        }
+    },
+    _setSizeAndPosition: function() {
+        if(this.isFloating()) {
+            var padding = 20;
+            var bodysize = Ext.getBody().getViewSize();
+            var bodyWidth = bodysize.width - padding;
+            var bodyHeight = bodysize.height - padding;
+            var height = bodyHeight < this._preferredHeight? bodyHeight: this._preferredHeight;
+            var width = bodyWidth < this._preferredWidth? bodyWidth: this._preferredWidth;
+            this.setSize({
+                width: width,
+                height: height,
+            });
+            this.center();
+        }
     }
 });
