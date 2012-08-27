db44059d81ca1143a61e806084589bd5725c443a
Espen A. Kristiansen <post@espenak.net>
2012-08-27 10:49:28 +0200

    devilry.utils.restformat: Made negative work in format_timedelta.

--- src/devilry/devilry/utils/restformat.py
+++ src/devilry/devilry/utils/restformat.py
@@ -2,9 +2,11 @@ def format_datetime(datetime):
     return datetime.strftime('%Y-%m-%d %H:%M:%S')
 
 def format_timedelta(timedelta_obj):
-    hours, remainder = divmod(timedelta_obj.seconds, 3600)
+    total_seconds = abs(timedelta_obj.total_seconds())
+    days, remainder = divmod(total_seconds, 86400)
+    hours, remainder = divmod(remainder, 3600)
     minutes, seconds = divmod(remainder, 60)
-    return {'days': abs(timedelta_obj.days),
-            'hours': hours,
-            'minutes': minutes,
-            'seconds': seconds}
+    return {'days': int(days),
+            'hours': int(hours),
+            'minutes': int(minutes),
+            'seconds': int(seconds)}
--- src/devilry/devilry/utils/tests/__init__.py
+++ src/devilry/devilry/utils/tests/__init__.py
@@ -1,3 +1,4 @@
-from streamable_archive_tests import * 
-from delivery_collection_tests import * 
-from importutils import *
+from .streamable_archive_tests import * 
+from .delivery_collection_tests import * 
+from .importutils import *
+from .restformat import *
