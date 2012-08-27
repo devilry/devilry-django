f2fac226c3f94c016efdb7c6dd8b361d4a90a0e6
Espen A. Kristiansen <post@espenak.net>
2012-08-27 09:14:18 +0200

    apps.core.testhelper: Only save deadline after setting all attrs.

--- src/devilry/devilry/apps/core/testhelper.py
+++ src/devilry/devilry/apps/core/testhelper.py
@@ -547,8 +547,6 @@ class TestHelper(object):
 #######
     def _create_or_add_deadline(self, deadline_name, parentnode, extras):
         deadline = Deadline(assignment_group=parentnode, deadline=parentnode.parentnode.publishing_time + timedelta(days=10))
-        deadline.full_clean()
-        deadline.save()
 
         if extras['ends']:
             deadline.deadline = parentnode.parentnode.publishing_time + timedelta(int(extras['ends'][0]))
