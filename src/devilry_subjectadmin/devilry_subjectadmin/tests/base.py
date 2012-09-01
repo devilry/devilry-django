69cfc8bd16f4950eab5f303074cba3d29574f141
Espen A. Kristiansen <post@espenak.net>
2012-09-01 14:23:44 +0200

    devilry_subjectadmin.tests.base: Wait for username input on login.

--- src/devilry_subjectadmin/devilry_subjectadmin/tests/base.py
+++ src/devilry_subjectadmin/devilry_subjectadmin/tests/base.py
@@ -16,6 +16,7 @@ class SubjectAdminSeleniumTestCase(SeleniumTestCase):
         self.selenium.get('{0}{1}?next={2}'.format(self.live_server_url,
                                                    settings.LOGIN_URL,
                                                    loadurl))
+        self.waitForCssSelector('#id_username')
         username_input = self.selenium.find_element_by_name("username")
         username_input.send_keys(username)
         password_input = self.selenium.find_element_by_name("password")
