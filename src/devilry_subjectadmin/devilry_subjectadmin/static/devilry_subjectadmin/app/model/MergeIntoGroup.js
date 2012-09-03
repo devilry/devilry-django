381e9106cd7308e77190f9ac990d3f32cb729e1c
Espen A. Kristiansen <post@espenak.net>
2012-09-03 17:01:51 +0200

    devilry_subjectadmin...model.MergeIntoGroup: Made a model for the mergeintogroup API.

---
+++ src/devilry_subjectadmin/devilry_subjectadmin/static/devilry_subjectadmin/app/model/MergeIntoGroup.js
@@ -0,0 +1,25 @@
+Ext.define('devilry_subjectadmin.model.MergeIntoGroup', {
+    extend: 'Ext.data.Model',
+    idProperty: 'source_group_id',
+    fields: [
+        {name: 'source_group_id', type: 'int'},
+        {name: 'target_group_id', type: 'int'},
+        {name: 'success', type: 'bool', persist: false}
+    ],
+
+    proxy: {
+        type: 'rest',
+        urlpatt: DevilrySettings.DEVILRY_URLPATH_PREFIX + '/devilry_subjectadmin/rest/mergeintogroup/{0}',
+        url: null, // We use urlpatt to dynamically generate the url
+        appendId: false, 
+        extraParams: {
+            format: 'json'
+        },
+        reader: {
+            type: 'json'
+        },
+        setUrl: function(assignment_id) {
+            this.url = Ext.String.format(this.urlpatt, assignment_id);
+        }
+    }
+});
