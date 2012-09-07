6ff320a804491c9b549a00e424522fb914f239d6
Espen A. Kristiansen <post@espenak.net>
2012-09-07 20:19:10 +0200

    devilry_subjectadmin.views: Comment out css.

--- src/devilry_subjectadmin/devilry_subjectadmin/views.py
+++ src/devilry_subjectadmin/devilry_subjectadmin/views.py
@@ -6,6 +6,6 @@ from extjs4.views import Extjs4AppView
 class AppView(Extjs4AppView):
     template_name = "devilry_subjectadmin/app.django.html"
     appname = 'devilry_subjectadmin'
-    css_staticpath = 'devilry_theme/resources/stylesheets/devilry.css'
+    #css_staticpath = 'devilry_theme/resources/stylesheets/devilry.css'
     #css_staticpath = 'extjs4/resources/css/ext-all-gray.css'
     title = _('Devilry - Subject administrator')
