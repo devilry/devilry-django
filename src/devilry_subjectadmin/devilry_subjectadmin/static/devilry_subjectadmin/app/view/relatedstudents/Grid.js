4e741c3439d91a14d02239f9e42d88b67579d198
Espen A. Kristiansen <post@espenak.net>
2012-09-10 14:44:45 +0200

    devilry_subjectadmin...relatedexaminers: Working.

--- src/devilry_subjectadmin/devilry_subjectadmin/static/devilry_subjectadmin/app/view/relatedstudents/Grid.js
+++ src/devilry_subjectadmin/devilry_subjectadmin/static/devilry_subjectadmin/app/view/relatedstudents/Grid.js
@@ -1,87 +1,27 @@
 Ext.define('devilry_subjectadmin.view.relatedstudents.Grid', {
-    extend: 'Ext.grid.Panel',
+    extend: 'devilry_subjectadmin.view.relatedexaminers.Grid',
     alias: 'widget.relatedstudentsgrid',
     cls: 'devilry_subjectadmin_relatedusergrid devilry_subjectadmin_relatedstudentsgrid bootstrap',
     requires: [
-        'Ext.XTemplate',
-        'devilry_extjsextras.GridMultiSelectModel',
-        'Ext.selection.CheckboxModel',
-        'Ext.grid.plugin.CellEditing',
-        'Ext.grid.plugin.RowEditing'
+        'Ext.grid.plugin.CellEditing'
     ],
 
     store: 'RelatedStudents',
-    border: 1,
-    frame: false,
-    hideHeaders: false,
-    multiSelect: true,
 
-    col1Tpl: [
-        '<div class="meta_cell relateduserid_{record.id} relateduser_username_{record.user.username}">',
-            '<div class="full_name">',
-                '<tpl if="record.user.full_name">',
-                    '<strong>{record.user.full_name}</strong>',
-                '<tpl else>',
-                    '<em class="nofullname">', gettext('Full name missing'), '</em>',
-                '</tpl>',
-            '</div>',
-            '<div class="username">',
-                '<small>{record.user.username}</small>',
-            '</div>',
-        '</div>'
-    ],
-
-    col2Tpl: [
-        '<div class="tags_cell" style="white-space:normal !important;">',
-            '{record.tags}',
-        '</div>'
-    ],
-
-    initComponent: function() {
-        this.col1TplCompiled = Ext.create('Ext.XTemplate', this.col1Tpl);
-        this.col2TplCompiled = Ext.create('Ext.XTemplate', this.col2Tpl);
-
+    initExtra: function() {
         var editingPlugin = Ext.create('Ext.grid.plugin.CellEditing', {
             clicksToEdit: 2
         });
-        Ext.apply(this, {
-            selModel: Ext.create('devilry_extjsextras.GridMultiSelectModel'),
-            columns: [{
-                dataIndex: 'id',
-                flex: 5,
-                header: gettext('Student'),
-                menuDisabled: true,
-                renderer: this.renderCol1,
-                sortable: false
-            }, {
-                dataIndex: 'tags',
-                flex: 5,
-                header: gettext('Tags'),
-                menuDisabled: true,
-                sortable: false,
-                renderer: this.renderCol2
-            }, {
-                dataIndex: 'candidate_id',
-                itemId: 'candidateColumn',
-                flex: 2,
-                header: gettext('Candidate ID'),
-                editor: {
-                    xtype: 'textfield'
-                }
-            }],
-            plugins: [editingPlugin]
+        this.columns.push({
+            dataIndex: 'candidate_id',
+            itemId: 'candidateColumn',
+            flex: 2,
+            header: gettext('Candidate ID'),
+            editor: {
+                xtype: 'textfield'
+            }
         });
+        this.plugins = [editingPlugin];
         this.callParent(arguments);
-    },
-
-    renderCol1: function(unused, unused2, record) {
-        return this.col1TplCompiled.apply({
-            record: record.data
-        });
-    },
-    renderCol2: function(unused, unused2, record) {
-        return this.col2TplCompiled.apply({
-            record: record.data
-        });
     }
 });
