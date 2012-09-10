63f2c491cec1ca6a6aa485e145f1a3ba68a7c663
Espen A. Kristiansen <post@espenak.net>
2012-09-10 18:19:17 +0200

    devilry_subjectadmin...ListOfAssignments: Include publishing time.

--- src/devilry_subjectadmin/devilry_subjectadmin/static/devilry_subjectadmin/app/view/period/ListOfAssignments.js
+++ src/devilry_subjectadmin/devilry_subjectadmin/static/devilry_subjectadmin/app/view/period/ListOfAssignments.js
@@ -7,14 +7,37 @@ Ext.define('devilry_subjectadmin.view.period.ListOfAssignments', {
     cls: 'devilry_listofassignments bootstrap',
     store: 'Assignments',
 
+    requires: [
+        'devilry_extjsextras.DatetimeHelpers'
+    ],
+
     tpl: [
-        '<ul>',
+        '<ul class="unstyled">',
             '<tpl for=".">',
                 '<li class="devilry_assignment"><p>',
-                    '<a href="#/assignment/{id}/">{long_name}</a>',
+                    '<div class="name">',
+                        '<a href="#/assignment/{id}/">{long_name}</a> ',
+                    '</div>',
+                    '<div class="assignmentmeta">',
+                        '<small class="muted">',
+                            '<em>',
+                                gettext('Publishing time'),
+                            ': </em>',
+                            '{[this.formatRelativeDatetime(values.publishing_time_offset_from_now, values.is_published)]} ',
+                            '({[this.formatDatetime(values.publishing_time)]})',
+                        '</small>',
+                    '<div>',
                 '</p></li>',
             '</tpl>',
-        '<ul>'
+        '<ul>', {
+            formatDatetime: function(datetime) {
+                return Ext.Date.format(datetime, 'Y-m-d h:i');
+            },
+            formatRelativeDatetime: function(publishing_time_offset_from_now, is_published) {
+                return devilry_extjsextras.DatetimeHelpers.formatTimedeltaRelative(
+                    publishing_time_offset_from_now, !is_published);
+            }
+        }
     ],
     itemSelector: 'li.devilry_assignment'
 });
