edd6121c039ce70e83fc10df18da8c010670b39c
Espen A. Kristiansen <post@espenak.net>
2012-09-16 14:50:23 +0200

    devilry_extjsextras...DateTimeField: Cleanup and ``change`` event.
    Partly solves #297.

--- src/devilry_extjsextras/devilry_extjsextras/static/devilry_extjsextras/form/DateTimeField.js
+++ src/devilry_extjsextras/devilry_extjsextras/static/devilry_extjsextras/form/DateTimeField.js
@@ -2,7 +2,7 @@
  * Based on http://www.sencha.com/forum/showthread.php?134345-Ext.ux.form.field.DateTime
  */
 Ext.define('devilry_extjsextras.form.DateTimeField', {
-    extend:'Ext.form.FieldContainer',
+    extend: 'Ext.form.FieldContainer',
     mixins:{    
         field:'Ext.form.field.Field'
     },
@@ -12,7 +12,6 @@ Ext.define('devilry_extjsextras.form.DateTimeField', {
     
     combineErrors: true,
     msgTarget: 'under',    
-    layout: 'hbox',
     readOnly: false,
 
     /**
@@ -37,79 +36,82 @@ Ext.define('devilry_extjsextras.form.DateTimeField', {
      */
     timeFieldEmptyText: pgettext('extjs time emptytext', 'hh:mm'),
     
-    // properties
-    
-    dateValue: null, // Holds the actual date
-    /**
-     * @property dateField
-     * @type Ext.form.field.Date
-     */
-    dateField: null,
-    /**
-     * @property timeField
-     * @type Ext.form.field.Time
-     */
-    timeField: null,
 
     initComponent: function(){
-        var me = this
-            ,i = 0
-            ,key
-            ,tab;
-            
-        me.items = me.items || [];
-        
         this.childrenRendered = 0;
-        me.dateField = Ext.create('devilry_extjsextras.form.DateField', Ext.apply({
-            flex:1,
-            isFormField:false, //exclude from field query's
-            emptyText: this.dateFieldEmptyText,
-            submitValue:false,
-            listeners: {
-                scope: this,
-                render: this._onChildRender
-            }
-        }, me.dateConfig));
-        me.items.push(me.dateField);
+        this.currentValue = null;
+        Ext.apply(this, {
+            layout: 'column',
+            items: [Ext.Object.merge({
+                xtype: 'devilry_extjsextras_datefield',
+                columnWidth: 0.5,
+                isFormField: false, //exclude from field query's
+                emptyText: this.dateFieldEmptyText,
+                submitValue:false,
+                listeners: {
+                    scope: this,
+                    render: this._onChildRender,
+                    blur: this._onItemBlur,
+                    change: this._onFieldChange,
+                    focus: this._onItemFocus
+                }
+            }, this.dateConfig), Ext.Object.merge({
+                xtype: 'devilry_extjsextras_timefield',
+                columnWidth: 0.5,
+                isFormField:false, //exclude from field query's
+                emptyText: this.timeFieldEmptyText,
+                submitValue:false,
+                listeners: {
+                    scope: this,
+                    render: this._onChildRender,
+                    blur: this._onItemBlur,
+                    change: this._onFieldChange,
+                    focus: this._onItemFocus
+                }
+            }, this.timeConfig)]
+        });
+
         
-        me.timeField = Ext.create('devilry_extjsextras.form.TimeField', Ext.apply({
-            flex:1,
-            isFormField:false, //exclude from field query's
-            emptyText: this.timeFieldEmptyText,
-            submitValue:false,
-            listeners: {
-                scope: this,
-                render: this._onChildRender
-            }
-        }, me.timeConfig));
-        me.items.push(me.timeField);
-        
-        for (; i < me.items.length; i++) {
-            me.items[i].on('focus', Ext.bind(me.onItemFocus, me));
-            me.items[i].on('blur', Ext.bind(me.onItemBlur, me));
-            //me.items[i].on('specialkey', function(field, event){
-                //key = event.getKey();
-                //tab = key == event.TAB;
+        //for (var i=0; i < this.items.length; i++) {
+            //this.items[i].on('specialkey', function(field, event){
+                //var key = event.getKey();
+                //var tab = key == event.TAB;
                 
-                //if (tab && me.focussedItem == me.dateField) {
+                //if (tab && this.focussedItem == this.getDateField()) {
                     //event.stopEvent();
-                    //me.timeField.focus();
+                    //this.getTimeField().focus();
                     //return;
                 //}
                 
-                //me.fireEvent('specialkey', field, event);
+                //this.fireEvent('specialkey', field, event);
             //});
-        }
+        //}
 
-        me.callParent();
+        this.callParent(arguments);
         
         // this dummy is necessary because Ext.Editor will not check whether an inputEl is present or not
         this.inputEl = {
             dom:{},
             swallowEvent:function(){}
         };
-        
-        me.initField();
+
+        this.initField();
+    },
+
+    getDateField: function() {
+        return this.down('devilry_extjsextras_datefield');
+    },
+    getTimeField: function() {
+        return this.down('devilry_extjsextras_timefield');
+    },
+
+    _onFieldChange: function(timefield, newValue, oldValue) {
+        var oldFullValue = this.currentValue;
+        var newFullValue = this.getValue();
+        if(this.newFullValue !== null) {
+            this.currentValue = newFullValue;
+            this.fireEvent('change', this, newFullValue, oldFullValue);
+        }
     },
 
     _onChildRender: function() {
@@ -121,76 +123,71 @@ Ext.define('devilry_extjsextras.form.DateTimeField', {
     
     focus:function(){
         this.callParent();
-        this.dateField.focus();
+        this.getDateField().focus();
     },
 
-    onItemFocus:function(item){
+    _onItemFocus: function(item){
         if (this.blurTask){
             this.blurTask.cancel();
         }
         this.focussedItem = item;
     },
     
-    onItemBlur:function(item){
-        var me = this;
-        if (item != me.focussedItem){ return; }
+    _onItemBlur: function(item){
+        if (item != this.focussedItem) {
+            return;
+        }
         // 100ms to focus a new item that belongs to us, otherwise we will assume the user left the field
-        me.blurTask = new Ext.util.DelayedTask(function(){
-            me.fireEvent('blur', me);
-        });
-        me.blurTask.delay(100);
+        this.blurTask = new Ext.util.DelayedTask(function(){
+            this.fireEvent('blur', this);
+        }, this);
+        this.blurTask.delay(100);
     },
     
     getValue: function(){
-        var value = null
-            ,date = this.dateField.getSubmitValue()
-            ,time = this.timeField.getSubmitValue()
-            ,format;
-
+        var value = null;
+        var date = this.getDateField().getSubmitValue()
         if (date){
+            var time = this.getTimeField().getSubmitValue()
             if (time){
-                format = this.getFormat();
+                var format = this.getFormat();
                 value = Ext.Date.parse(date + ' ' + time, format);
             } else {   
-                value = this.dateField.getValue();
+                value = this.getDateField().getValue();
             }
         }
         return value;
     },
     
     getSubmitValue: function(){   
-        var me = this
-            ,format = me.getFormat()
-            ,value = me.getValue();
-            
+        var format = this.getFormat()
+        var value = this.getValue();
         return value ? Ext.Date.format(value, format) : null;        
     },
  
     setValue: function(value){    
         if (Ext.isString(value)){
-            value = Ext.Date.parse(value, this.getFormat()); //this.dateTimeFormat
+            value = Ext.Date.parse(value, this.getFormat());
         }
-        this.dateField.setValue(value);
-        this.timeField.setValue(value);
+        this.getDateField().setValue(value);
+        this.getTimeField().setValue(value);
     },
     
     getFormat: function(){
-        return (this.dateField.submitFormat || this.dateField.format) + " " + (this.timeField.submitFormat || this.timeField.format);
+        return (this.getDateField().submitFormat || this.getDateField().format) + " " + (this.getTimeField().submitFormat || this.getTimeField().format);
     },
     
     // Bug? A field-mixin submits the data from getValue, not getSubmitValue
     getSubmitData: function(){
-        var me = this
-            ,data = null;
-            
-        if (!me.disabled && me.submitValue && !me.isFileUpload()) {
+        var data = null;
+        if (!this.disabled && this.submitValue && !this.isFileUpload()) {
             data = {};
-            data[me.getName()] = '' + me.getSubmitValue();
+            data[this.getName()] = '' + this.getSubmitValue();
         }
         return data;
     },
 
     isValid: function() {
-        return this.dateField.isValid() && this.timeField.isValid();
+        return this.getDateField().isValid() && this.getTimeField().isValid();
     }
 });
