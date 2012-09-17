028958646e6e996154c9de5fed9aed0f909b3519
Espen A. Kristiansen <post@espenak.net>
2012-09-17 16:06:17 -0700

    devilry_extjsextras...EvenRandomSelection: Ported from apps.extjshelpers.

---
+++ src/devilry_extjsextras/devilry_extjsextras/static/devilry_extjsextras/EvenRandomSelection.js
@@ -0,0 +1,32 @@
+Ext.define('devilry_extjsextras.EvenRandomSelection', {
+    config: {
+        /**
+        * @cfg {Object[]} [selection]
+        * The items in the selection pool.
+        */
+        selection: []
+    },
+
+    constructor: function(config) {
+        this.pool = []; // Currently available subset of the selection. When this is empty, it is re-cloned from selection
+        this.initConfig(config);
+    },
+
+    /** Get random item from the selection pool.
+     *
+     * The pool is filled with ``selection``, re-filled each time the pool is
+     * empty. This method picks a random item from the pool. This means that
+     * we always pick every item in the pool before we re-start picking items. */
+    getRandomItem: function() {
+        if(this.pool.length === 0) {
+            this.pool = Ext.Array.clone(this.selection);
+        }
+        console.log(this.pool);
+        var randomIndex = Math.floor(Math.random() * (this.pool.length));
+        console.log(randomIndex);
+        var next = this.pool[randomIndex];
+        Ext.Array.remove(this.pool, next);
+        console.log(next);
+        return next;
+    }
+});
