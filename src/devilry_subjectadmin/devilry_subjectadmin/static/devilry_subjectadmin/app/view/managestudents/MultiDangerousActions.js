6da42bfc02d7551823a9e228c8c6453c25c05c73
Espen A. Kristiansen <post@espenak.net>
2012-09-18 11:59:13 -0700

    devilry_subjectadmin...MultiDangerousActions: Added forgotten file.

---
+++ src/devilry_subjectadmin/devilry_subjectadmin/static/devilry_subjectadmin/app/view/managestudents/MultiDangerousActions.js
@@ -0,0 +1,26 @@
+Ext.define('devilry_subjectadmin.view.managestudents.MultiDangerousActions', {
+    extend: 'devilry_subjectadmin.view.DangerousActions',
+    alias: 'widget.multigroupdangerous',
+    cls: 'devilry_subjectadmin_multigroupdangerous',
+    requires: [
+        'devilry_extjsextras.SingleActionBox'
+    ],
+
+    initComponent: function() {
+        Ext.apply(this, {
+            titleTpl: '<h3>{heading}</h3>',
+            items: [{
+                xtype: 'singleactionbox',
+                itemId: 'multiDeleteButton',
+                id: 'multi_group_delete_button',
+                titleText: gettext('Delete'),
+                bodyHtml: interpolate(gettext('Once you delete a %(group_term)s, there is no going back. Only superusers can delete a %(group_term)s with deliveries.'), {
+                    group_term: gettext('group')
+                }, true),
+                buttonText: gettext('Delete') + ' ...',
+                buttonUi: 'danger'
+            }]
+        });
+        this.callParent(arguments);
+    }
+});
