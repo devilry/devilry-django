58cca8c0b2a225cc22ff1bff2364ecfefc335e3a
Espen A. Kristiansen <post@espenak.net>
2012-10-10 09:02:57 -0700

    devilry_subjectadmin...AdminsBox: Made it far more compact.
    Administrators and inherited administrators are under the "Administrator
    rights" heading, and the users have to select "details" to see them.
    Also added information about what it means to be administrator.
    
    Should cover #344, however we need to update the tests before it can be
    closed.

--- src/devilry_subjectadmin/devilry_subjectadmin/static/devilry_subjectadmin/app/view/AdminsBox.js
+++ src/devilry_subjectadmin/devilry_subjectadmin/static/devilry_subjectadmin/app/view/AdminsBox.js
@@ -9,54 +9,85 @@ Ext.define('devilry_subjectadmin.view.AdminsBox', {
         'devilry_subjectadmin.utils.UrlLookup',
         'devilry_subjectadmin.view.ManageAdminsPanel',
         'devilry_extjsextras.EditableSidebarBox',
-        'devilry_extjsextras.AutoSizedWindow'
+        'devilry_extjsextras.AutoSizedWindow',
+        'devilry_extjsextras.MarkupMoreInfoBox'
     ],
 
     adminsTpl: [
-        '<tpl if="admins.length &gt; 0">',
-            '<ul class="devilry_subjectadmin_administratorlist">',
-                '<tpl for="admins">',
-                    '<li class="administratorlistitem administratorlistitem_{username}">',
-                        '<a href="mailto:{email}">',
-                            '<tpl if="full_name != null">',
-                                '{full_name}',
-                            '</tpl>',
-                            '<tpl if="full_name == null">',
-                                '{username}',
-                            '</tpl>',
-                        '</a>',
-                    '</li>',
-                '</tpl>',
-            '</ul>',
-        '</tpl>',
-        '<tpl if="admins.length == 0">',
-            '<p><small class="muted">', gettext('No administrators') ,'</small></p>',
-        '</tpl>'
-    ],
-    inheritedAdminsTpl: [
-        '<tpl if="inherited_admins.length &gt; 0">',
-            //'<p><small class="muted">(',
-                //gettext('The links in parenthesis is to the location where the administrator is inherited from'),
-            //')</small></p>',
-            '<ul class="devilry_subjectadmin_inherited_administratorlist">',
-                '<tpl for="inherited_admins">',
-                    '<li class="inherited_administratorlistitem inherited_administratorlistitem_{user.username}">',
-                        '<a href="mailto:{user.email}">',
-                            '<tpl if="user.full_name != null">',
-                                '{user.full_name}',
-                            '</tpl>',
-                            '<tpl if="user.full_name == null">',
-                                '{user.username}',
-                            '</tpl>',
-                        '</a>',
-                        ' <small>(<a href="{url}">{basenode.path}</a>)</small>',
-                    '</li>',
-                '</tpl>',
-            '</ul>',
-        '</tpl>',
-        '<tpl if="inherited_admins.length == 0">',
-            '<p><small class="muted">', gettext('No administrators') ,'</small></p>',
-        '</tpl>'
+        '<h4>', gettext('Administrator rights'), '</h4>',
+        '<p class="muted">',
+            '<small>',
+                gettext('{admincount} users have administrator rights on this item.'),
+                ' {MORE_BUTTON}',
+            '</small>',
+        '</p>',
+        '<div {MORE_ATTRS}>',
+            '<h5>', gettext('How administrator rights work'), '</h5>',
+            '<p>',
+                gettext('Devilry has a very simple permission system for administrators. You are eighter administrator on an item, or you have no access to the administrator views for an item.'),
+            '</p>',
+            '<p>',
+                gettext('Administrators can be set anywhere in the hierarchy, from nodes on the top, to assignments at the bottom, and permissions propagate downwards in the hierarchy.'),
+            '</p>',
+
+            '<h5>', gettext('Administrators on this item') ,'</h5>',
+            '<tpl if="admins.length &gt; 0">',
+                '<p><small class="muted">',
+                    gettext('These are the administrators on this item.'),
+                '</small></p>',
+                '<ul class="devilry_subjectadmin_administratorlist">',
+                    '<tpl for="admins">',
+                        '<li class="administratorlistitem administratorlistitem_{username}">',
+                            '<a href="mailto:{email}">',
+                                '<tpl if="full_name != null">',
+                                    '{full_name}',
+                                '</tpl>',
+                                '<tpl if="full_name == null">',
+                                    '{username}',
+                                '</tpl>',
+                            '</a>',
+                        '</li>',
+                    '</tpl>',
+                '</ul>',
+                '<p>',
+                    ' <a href="#" class="btn editadmins_link">',
+                        gettext('Add or edit administrators'),
+                    '</a>',
+                '</p>',
+            '</tpl>',
+            '<tpl if="admins.length == 0">',
+                '<p>',
+                    '<small class="muted">', gettext('No administrators') ,'</small>',
+                '</p>',
+                '<p>',
+                    ' <a href="#" class="btn editadmins_link">',
+                        gettext('Add administrators'),
+                    '</a>',
+                '</p>',
+            '</tpl>',
+
+            '<tpl if="inherited_admins.length &gt; 0">',
+                '<h5>', gettext('Other administrators'), '</h5>',
+                '<p><small class="muted">',
+                    gettext('These administrators are from items higher up in the hierarchy. The links in parenthesis is to the location where the administrator propagates from.'),
+                '</small></p>',
+                '<ul class="devilry_subjectadmin_inherited_administratorlist">',
+                    '<tpl for="inherited_admins">',
+                        '<li class="inherited_administratorlistitem inherited_administratorlistitem_{user.username}">',
+                            '<a href="mailto:{user.email}">',
+                                '<tpl if="user.full_name != null">',
+                                    '{user.full_name}',
+                                '</tpl>',
+                                '<tpl if="user.full_name == null">',
+                                    '{user.username}',
+                                '</tpl>',
+                            '</a>',
+                            ' <small>(<a href="{url}">{basenode.path}</a>)</small>',
+                        '</li>',
+                    '</tpl>',
+                '</ul>',
+            '</tpl>',
+        '<div>'
     ],
 
 
@@ -95,35 +126,27 @@ Ext.define('devilry_subjectadmin.view.AdminsBox', {
             return aKey.localeCompare(bKey);
         });
         
+        var inherited_admins = this._get_inherited_with_urls(this.basenodeRecord);
         this.add([{
-            xtype: 'editablesidebarbox',
-            title: gettext('Administrators'),
-            bodyTpl: this.adminsTpl,
-            bodyData: {
-                admins: sorted_admins
+            xtype: 'markupmoreinfobox',
+            moreCls: '',
+            moretext: gettext('Details') + ' ...',
+            lesstext: gettext('Hide details') + ' ...',
+            tpl: this.adminsTpl,
+            data: {
+                admins: sorted_admins,
+                inherited_admins: inherited_admins,
+                admincount: sorted_admins.length + inherited_admins.length
             },
             listeners: {
                 scope: this,
-                edit: this._onEdit
+                element: 'el',
+                delegate: 'a.editadmins_link',
+                click: function(e) {
+                    e.preventDefault();
+                    this._onEdit();
+                }
             }
-        }, {
-            xtype: 'container',
-            margin: '10 0 0 0',
-            cls: 'bootstrap',
-            items: [{
-                xtype: 'box',
-                itemId: 'title',
-                tpl: '<h4>{title}</h4>',
-                data: {
-                    title: gettext('Inherited administrators'),
-                }
-            }, {
-                xtype: 'box',
-                tpl: this.inheritedAdminsTpl,
-                data: {
-                    inherited_admins: this._get_inherited_with_urls(this.basenodeRecord)
-                }
-            }]
         }]);
     },
 
