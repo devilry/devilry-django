3da9c17807fedbaf187d630498452200956bef8f
Espen A. Kristiansen <post@espenak.net>
2012-10-31 06:12:56 -0700

    utils.management: Encoding fallbacks.
    Tries in this order sys.std*, sys.getdefaultencoding, "UTF-8".

--- src/devilry/devilry/utils/management.py
+++ src/devilry/devilry/utils/management.py
@@ -1,12 +1,14 @@
 import sys
 from optparse import make_option
 
+DEFAULT_ENCODING = 'utf-8'
+
 def get_input_encoding():
     """ Get the input encoding used for input to management commands.
 
     :return: ``sys.stdin.encoding``
     """
-    return sys.stdin.encoding
+    return sys.stdin.encoding or sys.getdefaultencoding() or DEFAULT_ENCODING
 
 def make_input_encoding_option():
     """
@@ -18,15 +20,16 @@ def make_input_encoding_option():
     return make_option('--input-encoding',
                        dest='inputencoding',
                        default=get_input_encoding(),
-                       help=('Input encoding. Defaults to ``sys.stdin.encoding``, '
-                             'which currently is set to: {0}').format(get_input_encoding()))
+                       help=('Input encoding. Defaults to ``sys.stdin.encoding``, falling back '
+                             'to ``sys.getdefaultencoding()`` and back to utf-8 if both are undefined. '
+                             'It is currently is set to: {0}').format(get_input_encoding()))
 
 def get_output_encoding():
     """ Get the output encoding used for output to management commands.
 
     :return: ``sys.stdout.encoding``
     """
-    return sys.stdout.encoding
+    return sys.stdin.encoding or sys.getdefaultencoding() or DEFAULT_ENCODING
 
 def make_output_encoding_option():
     """
@@ -38,5 +41,6 @@ def make_output_encoding_option():
     return make_option('--output-encoding',
                        dest='outputencoding',
                        default=get_output_encoding(),
-                       help=('Input encoding. Defaults to ``sys.stdout.encoding``, '
-                             'which currently is set to: {0}').format(get_output_encoding()))
+                       help=('Output encoding. Defaults to ``sys.stdout.encoding``, falling back '
+                             'to ``sys.getdefaultencoding()`` and back to utf-8 if both are undefined. '
+                             'It is currently is set to: {0}').format(get_output_encoding()))
