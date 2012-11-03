be299cbd360fa5f6248d0686afbf500b68b5001b
Espen A. Kristiansen <post@espenak.net>
2012-11-02 22:26:15 -0700

    devilry_subjectadmin...subject: Same layout as assignment.

--- src/devilry_subjectadmin/devilry_subjectadmin/static/devilry_subjectadmin/app/view/subject/Overview.js
+++ src/devilry_subjectadmin/devilry_subjectadmin/static/devilry_subjectadmin/app/view/subject/Overview.js
@@ -2,14 +2,15 @@
  * Subject overview (overview of an subject).
  */
 Ext.define('devilry_subjectadmin.view.subject.Overview' ,{
-    extend: 'Ext.panel.Panel',
+    extend: 'Ext.container.Container',
     alias: 'widget.subjectoverview',
     cls: 'devilry_subjectoverview',
     requires: [
         'Ext.layout.container.Column',
         'devilry_extjsextras.AlertMessageList',
         'devilry_subjectadmin.view.AdminsBox',
-        'devilry_extjsextras.SingleActionBox'
+        'devilry_extjsextras.SingleActionBox',
+        'devilry_subjectadmin.view.EditSidebarContainer'
     ],
 
 
@@ -20,82 +21,74 @@ Ext.define('devilry_subjectadmin.view.subject.Overview' ,{
 
     initComponent: function() {
         Ext.apply(this, {
-            frame: false,
-            border: 0,
-            bodyPadding: '20 40 20 40',
+            padding: '20 20 20 20',
             autoScroll: true,
+            layout: 'column',
 
             items: [{
-                xtype: 'alertmessagelist'
-            }, {
-                xtype: 'panel',
-                frame: false,
+                xtype: 'editsidebarcontainer',
+                layout: 'anchor',
                 border: false,
-                layout: 'column',
+                width: 250,
+                margin: '6 40 0 0',
+                padding: '0 10 0 10',
+                defaults: {
+                    margin: '10 0 0 0'
+                },
                 items: [{
-                    xtype: 'container',
-                    columnWidth: 1,
-                    layout: 'anchor',
-                    defaults: {
-                        anchor: '100%'
-                    },
-                    items: [{
-                        xtype: 'box',
-                        cls: 'bootstrap',
-                        margin: '0 0 20 0',
-                        itemId: 'header',
-                        tpl: '<h1>{heading}</h1>',
-                        data: {
-                            heading: gettext('Loading') + ' ...'
-                        }
-                    }, {
-                        xtype: 'box',
-                        cls: 'bootstrap devilry_subjectadmin_navigation',
-                        tpl: [
-                            '<p><strong><a href="{url}">{text}</a></strong></p>'
-                        ],
-                        data: {
-                            url: devilry_subjectadmin.utils.UrlLookup.createNewPeriod(this.subject_id),
-                            text: interpolate(gettext('Create new %(period_term)s'), {
-                                period_term: gettext('period')
-                            }, true)
-                        }
-                    }, {
-                        xtype: 'listofperiods'
-                    }, {
-                        xtype: 'dangerousactions',
-                        margin: '20 0 0 0',
-                        items: [{
-                            xtype: 'singleactionbox',
-                            margin: '0 0 0 0',
-                            itemId: 'renameButton',
-                            id: 'subjectRenameButton',
-                            titleText: gettext('Loading ...'),
-                            bodyHtml: gettext('Renaming a subject should not done without a certain amount of consideration. The name of a subject, especially the short name, is often used as an identifier when integrating other systems with Devilry.'),
-                            buttonText: gettext('Rename') + ' ...'
-                        }, {
-                            xtype: 'singleactionbox',
-                            itemId: 'deleteButton',
-                            id: 'subjectDeleteButton',
-                            titleText: gettext('Loading ...'),
-                            bodyHtml: gettext('Once you delete a subject, there is no going back. Only superusers can delete a non-empty subject.'),
-                            buttonText: gettext('Delete') + ' ...',
-                            buttonUi: 'danger'
-                        }]
-                    }]
+                    xtype: 'adminsbox',
+                    anchor: '100%',
+                    margin: '0 0 0 0'
+                }]
+            }, {
+                xtype: 'container',
+                columnWidth: 1,
+                layout: 'anchor',
+                defaults: {
+                    anchor: '100%'
+                },
+                items: [{
+                    xtype: 'box',
+                    cls: 'bootstrap',
+                    margin: '0 0 20 0',
+                    itemId: 'header',
+                    tpl: '<h1>{heading}</h1>',
+                    data: {
+                        heading: gettext('Loading') + ' ...'
+                    }
                 }, {
-                    xtype: 'container',
-                    layout: 'anchor',
-                    border: false,
-                    width: 250,
-                    margin: '6 0 0 40',
-                    defaults: {
-                        margin: '20 0 0 0'
-                    },
+                    xtype: 'box',
+                    cls: 'bootstrap devilry_subjectadmin_navigation',
+                    tpl: [
+                        '<p><strong><a href="{url}">{text}</a></strong></p>'
+                    ],
+                    data: {
+                        url: devilry_subjectadmin.utils.UrlLookup.createNewPeriod(this.subject_id),
+                        text: interpolate(gettext('Create new %(period_term)s'), {
+                            period_term: gettext('period')
+                        }, true)
+                    }
+                }, {
+                    xtype: 'listofperiods'
+                }, {
+                    xtype: 'dangerousactions',
+                    margin: '45 0 0 0',
                     items: [{
-                        xtype: 'adminsbox',
-                        anchor: '100%',
-                        margin: '0 0 0 0'
+                        xtype: 'singleactionbox',
+                        margin: '0 0 0 0',
+                        itemId: 'renameButton',
+                        id: 'subjectRenameButton',
+                        titleText: gettext('Loading ...'),
+                        bodyHtml: gettext('Renaming a subject should not done without a certain amount of consideration. The name of a subject, especially the short name, is often used as an identifier when integrating other systems with Devilry.'),
+                        buttonText: gettext('Rename') + ' ...'
+                    }, {
+                        xtype: 'singleactionbox',
+                        itemId: 'deleteButton',
+                        id: 'subjectDeleteButton',
+                        titleText: gettext('Loading ...'),
+                        bodyHtml: gettext('Once you delete a subject, there is no going back. Only superusers can delete a non-empty subject.'),
+                        buttonText: gettext('Delete') + ' ...',
+                        buttonUi: 'danger'
                     }]
                 }]
             }]
