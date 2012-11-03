b245622c6b4117cb73abcefac798e2138af58926
Espen A. Kristiansen <post@espenak.net>
2012-11-03 14:15:37 -0700

    devilry_subjectadmin...NoGroupSelectedView: Fixed #358.
    Only show important help on the managestudents frontpage.

--- src/devilry_subjectadmin/devilry_subjectadmin/static/devilry_subjectadmin/app/view/managestudents/NoGroupSelectedView.js
+++ src/devilry_subjectadmin/devilry_subjectadmin/static/devilry_subjectadmin/app/view/managestudents/NoGroupSelectedView.js
@@ -4,10 +4,7 @@
 Ext.define('devilry_subjectadmin.view.managestudents.NoGroupSelectedView' ,{
     extend: 'Ext.container.Container',
     alias: 'widget.nogroupselectedview',
-    cls: 'nogroupselectedview bootstrap',
-    requires: [
-        'devilry_subjectadmin.view.managestudents.HelpPanel'
-    ],
+    cls: 'nogroupselectedview',
 
     initComponent: function() {
         Ext.apply(this, {
@@ -18,12 +15,40 @@ Ext.define('devilry_subjectadmin.view.managestudents.NoGroupSelectedView' ,{
                 anchor: '100%'
             },
             items: [{
-                xtype: 'alertmessage',
-                type: 'info',
-                message: gettext('Select groups from the list on your left hand side. The help below is always available by clicking on the help-column on your right hand side.')
-            }, {
-                xtype: 'managestudents_help',
-                border: false
+                xtype: 'box',
+                cls: 'bootstrap',
+                padding: '60 20 0 20',
+                tpl: [
+                    '<div>',
+                        '<div class="pull-left">',
+                            '<i class="icon-arrow-left"></i>',
+                        '</div>',
+                        '<p style="margin-left:20px;">',
+                            '<strong style="font-size:160%;">',
+                                gettext('Select GROUPS from the list to your left'),
+                            '</strong>',
+                            '<br/>',
+                            '<span class="muted">',
+                                gettext('Students are always in a (project) group even when they work alone.'),
+                            '</span>',
+                        '</p>',
+                    '</div>',
+                    '<div style="margin-top: 60px;">',
+                        '<div class="pull-right">',
+                            '<i class="icon-arrow-right"></i>',
+                        '</div>',
+                        '<p style="text-align:right; margin-right:20px;">',
+                            '<strong style="font-size:160%;">',
+                                gettext('Click the help-column to your right if you need help'),
+                            '</strong>',
+                            '<br/>',
+                            '<span class="muted">',
+                                gettext('First time visitors should read the help to avoid common misconceptions.'),
+                            '</span>',
+                        '</p>',
+                    '</div>'
+                ],
+                data: {}
             }]
         });
         this.callParent(arguments);
