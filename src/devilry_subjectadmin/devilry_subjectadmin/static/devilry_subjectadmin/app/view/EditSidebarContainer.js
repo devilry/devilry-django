173a1c4c262ec858469e5076369f98b8b18441bb
Espen A. Kristiansen <post@espenak.net>
2012-11-03 14:55:36 -0700

    devilry_subjectadmin...EditSidebarContainer: Less opaque.

--- src/devilry_subjectadmin/devilry_subjectadmin/static/devilry_subjectadmin/app/view/EditSidebarContainer.js
+++ src/devilry_subjectadmin/devilry_subjectadmin/static/devilry_subjectadmin/app/view/EditSidebarContainer.js
@@ -1,5 +1,8 @@
 Ext.define('devilry_subjectadmin.view.EditSidebarContainer', {
     extend: 'devilry_extjsextras.UnfocusedContainer',
     alias: 'widget.editsidebarcontainer',
-    cls: 'devilry_subjectadmin_editsidebarcontainer'
+    cls: 'devilry_subjectadmin_editsidebarcontainer',
+
+    defaultOpacity: 0.7,
+    hoverOpacity: 1,
 });
