651db5fb815c7cd36436a16d61d2d8e7dae06d64
Espen A. Kristiansen <post@espenak.net>
2012-11-21 17:48:24 +0100

    devilry_demo: Skeleton.

---
+++ src/devilry_demo/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+
+setup(name = 'devilry_demo',
+      description = 'Makes it easy to setup demos of Devilry.',
+      version = '1.0',
+      license='BSD',
+      author = 'Espen Angell Kristiansen',
+      packages=find_packages(exclude=['ez_setup']),
+      install_requires = ['setuptools', 'Django',
+                          'devilry_theme'],
+      include_package_data=True,
+      long_description = open('README.rst').read(),
+      zip_safe=False,
+      classifiers=[
+                   'Development Status :: 4 - Beta',
+                   'Environment :: Web Environment',
+                   'Framework :: Django',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: JavaScript',
+                   'Programming Language :: Python'
+                  ]
+)
