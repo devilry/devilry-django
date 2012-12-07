07241a0f951ee1901396ec78b6c24b6e4c31ac32
Espen A. Kristiansen <post@espenak.net>
2012-12-07 09:22:35 +0100

    docs|devenv|devilry_developer: Use sphinxbuilder.

---
+++ src/devilry_developer/devilry_developer/settings/docs.py
@@ -0,0 +1,7 @@
+# These settings are used when we generate our documentation
+
+from .base import *
+
+MEDIA_ROOT = "filestore"
+DATABASES['default']['NAME'] = ':memory:'
+DEVILRY_DELIVERY_STORE_BACKEND = 'devilry.apps.core.deliverystore.MemoryDeliveryStore'
