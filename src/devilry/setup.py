309cbba83d106149c8fd8f2ccc45a7ef5b6913a0
Espen A. Kristiansen <post@espenak.net>
2012-12-16 10:27:04 +0100

    devilry.setup: Removed flup dependency.

--- src/devilry/setup.py
+++ src/devilry/setup.py
@@ -10,7 +10,7 @@ setup(
     packages = find_packages(),
     install_requires = ['setuptools', 'Django', 'Markdown', 'django_errortemplates',
                         'djangorestframework', 'Pygments',
-                        'flup',
+                        #'flup',
                         #'PyYAML',
                         'gunicorn']
 )
