a391b178923b193d8697391238dcdf05ae673867
Espen Angell Kristiansen <post@espenak.net>
2012-12-16 09:07:45 +0100

    subjectadmin...AllWhereIsAdminList: Use the focus style.

--- src/devilry_subjectadmin/devilry_subjectadmin/static/devilry_subjectadmin/app/view/allwhereisadmin/AllWhereIsAdminPanel.js
+++ src/devilry_subjectadmin/devilry_subjectadmin/static/devilry_subjectadmin/app/view/allwhereisadmin/AllWhereIsAdminPanel.js
@@ -1,5 +1,5 @@
 Ext.define('devilry_subjectadmin.view.allwhereisadmin.AllWhereIsAdminPanel' ,{
-    extend: 'Ext.panel.Panel',
+    extend: 'Ext.container.Container',
     alias: 'widget.allwhereisadminpanel', // Define the widget xtype as allwhereisadminpanel
     cls: 'dashboard',
 
@@ -7,13 +7,40 @@ Ext.define('devilry_subjectadmin.view.allwhereisadmin.AllWhereIsAdminPanel' ,{
         'devilry_subjectadmin.view.allwhereisadmin.AllWhereIsAdminList'
     ],
 
-    layout: 'fit',
+    layout: 'anchor',
     frame: false,
     border: 0,
-    bodyPadding: '20 40 20 40',
+    padding: '20 40 20 40',
     autoScroll: true,
 
-    items: {
-        xtype: 'allwhereisadminlist'
-    }
+    items: [{
+        xtype: 'box',
+        cls: 'bootstrap',
+        anchor: '100%',
+        html: [
+            '<h1 style="margin-top: 0;">',
+                interpolate(gettext('All %(subjects_term)s'), {
+                    subjects_term: gettext('subjects')
+                }, true),
+            '</h1>',
+            '<p class="muted">',
+                interpolate(gettext('These are all %(subjects_term)s, %(periods_term)s and assignments where you have administrator rights. Assignments are listed if you have administrator-rights on the assignments, and not on the %(period_term)s or %(subject_term)s containing the assignment. The %(subject_term)s name is a link on if you have administrator rights on the subject.'), {
+                    subjects_term: gettext('subjects'),
+                    periods_term: gettext('periods'),
+                    subject_term: gettext('subject'),
+                    period_term: gettext('period')
+                }, true),
+            '</p>'
+        ].join('')
+    }, {
+        xtype: 'container',
+        border: 1,
+        padding: '10 20 10 20',
+        anchor: '100%',
+        layout: 'fit',
+        cls: 'devilry_focuscontainer',
+        items: {
+            xtype: 'allwhereisadminlist'
+        }
+    }]
 });
