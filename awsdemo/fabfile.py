c4a43f76f7b2d0990dfc9cdd16ab7e223c4b9ea5
Espen A. Kristiansen <post@espenak.net>
2012-12-16 09:21:10 +0100

    awsfab.fabfile: Changed branch to master.

--- awsdemo/fabfile.py
+++ awsdemo/fabfile.py
@@ -6,7 +6,7 @@ from fabric.context_managers import cd
 
 
 repo = 'git://github.com/devilry/devilry-django.git'
-branch = 'admin'
+branch = 'master'
 checkout_dir = '/home/ubuntu/devilry-django'
 prodenv_dir = checkout_dir + '/example-productionenv' # Must not end with ``/``
 gunicorn_pidfile = '/tmp/gunicorn.pid'
