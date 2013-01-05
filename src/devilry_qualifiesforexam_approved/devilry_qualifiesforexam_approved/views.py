9af11b64b0176e84f398e21448efe457aeee1769
Espen Angell Kristiansen <post@espenak.net>
2013-01-05 22:37:04 +0100

    devilry_qualfiesforexam_approved: Started using the new grouper.

--- src/devilry_qualifiesforexam_approved/devilry_qualifiesforexam_approved/views.py
+++ src/devilry_qualifiesforexam_approved/devilry_qualifiesforexam_approved/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404
 from django.views.generic import RedirectView
 from django.core.urlresolvers import reverse
+from devilry.utils.groups_groupedby_relatedstudent_and_assignment import GroupsGroupedByRelatedStudentAndAssignment
 
 from devilry.apps.core.models import Period
 
@@ -30,8 +31,19 @@ class AllApprovedView(RedirectView, QualifiesForExamViewMixin):
     permanent = False
     query_string = True
 
+    def _passed_all_assignments(self, aggregated_relstudentinfo):
+        for grouplist in aggregated_relstudentinfo.iter_groups_by_assignment():
+            feedback = grouplist.get_feedback_with_most_points()
+            if not (feedback and feedback.is_passing_grade):
+                return False
+        return True
+
     def _get_passing_students(self):
         passing_relatedstudents = []
+        grouper = GroupsGroupedByRelatedStudentAndAssignment(self.period)
+        for aggregated_relstudentinfo in grouper.iter_relatedstudents_with_results():
+            if self._passed_all_assignments(aggregated_relstudentinfo):
+                passing_relatedstudents.append(aggregated_relstudentinfo.user)
         return passing_relatedstudents
 
     def get(self, request):
