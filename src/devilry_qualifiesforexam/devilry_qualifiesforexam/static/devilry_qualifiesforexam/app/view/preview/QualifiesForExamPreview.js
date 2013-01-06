b03411d89acfe04947b8a3d7af24f93554d8cef9
Espen Angell Kristiansen <post@espenak.net>
2013-01-06 23:54:50 +0100

    devilry_qualfiesforexam...preview: Usable view.

--- src/devilry_qualifiesforexam/devilry_qualifiesforexam/static/devilry_qualifiesforexam/app/view/preview/QualifiesForExamPreview.js
+++ src/devilry_qualifiesforexam/devilry_qualifiesforexam/static/devilry_qualifiesforexam/app/view/preview/QualifiesForExamPreview.js
@@ -12,16 +12,51 @@ Ext.define('devilry_qualifiesforexam.view.preview.QualifiesForExamPreview' ,{
      */
 
     requires: [
-        'devilry_qualifiesforexam.view.preview.PreviewGrid'
+        'devilry_qualifiesforexam.view.preview.PreviewGrid',
+        'devilry_extjsextras.PrimaryButton'
     ],
 
-    layout: 'border',
+
+    summaryTpl: [
+        '<h1 style="margin: 0 0 10px 0;">',
+            gettext('Preview and confirm'),
+        '</h1>',
+        '<p>',
+            gettext('Please click the save-button below when you are ready proceed. The table below shows the result of your choices.'),
+        '</p>'
+    ],
 
     initComponent: function() {
         Ext.apply(this, {
+            layout: 'anchor',
+            padding: '20',
             items: [{
+                xtype: 'container',
+                cls: 'devilry_focuscontainer bootstrap',
+                padding: 20,
+                height: 195,
+                margin: '0 0 20 0',
+                layout: 'fit',
+                items: [{
+                    xtype: 'panel',
+                    itemId: 'summary',
+                    tpl: this.summaryTpl,
+                    autoScroll: true,
+                    border: false,
+                    data: {},
+                    fbar: [{
+                        xtype: 'button',
+                        itemId: 'backButton',
+                        text: gettext('Back')
+                    }, {
+                        xtype: 'primarybutton',
+                        itemId: 'saveButton',
+                        text: gettext('Save')
+                    }]
+                }]
+            }, {
                 xtype: 'previewgrid',
-                region: 'center'
+                anchor: '0 -215'
             }]
         });
         this.callParent(arguments);
