9e309ab4205695d5432eb637edff24d85360d787
ilyakh <ilyakh@ulrik.uio.no>
2013-01-09 19:56:20 +0100

    nodeadmin: Cleanup class names.

--- src/devilry_nodeadmin/devilry_nodeadmin/static/devilry_nodeadmin/model/Node.js
+++
@@ -1,9 +0,0 @@
-Ext.define( 'devilry_nodeadmin.model.Node', {
-    extend: 'Ext.data.Model',
-    idProperty: 'id',
-    fields: [
-        {name: 'id',  type: 'int'},
-	{short_name: 'short_name', type: 'string'},
-	{long_name: 'long_name', type: 'string'}
-    ]
-});
--- src/devilry_nodeadmin/devilry_nodeadmin/static/devilry_nodeadmin/store/Nodes.js
+++
@@ -1,4 +0,0 @@
-Ext.define( 'devilry_nodeadmin.store.Nodes', {
-    extend: 'Ext.data.Store',
-    model: 'devilry_nodeadmin.model.Node'
-});
