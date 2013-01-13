3e3dff242876276fd580221f83b9fb10e89b84ab
ilyakh <ilyakh@ulrik.uio.no>
2013-01-13 06:13:53 +0100

    nodeadmin: reverses language strings and translates

--- src/devilry_nodeadmin/devilry_nodeadmin/static/devilry_nodeadmin/app/view/dashboard/DefaultNodeList.js
+++ src/devilry_nodeadmin/devilry_nodeadmin/static/devilry_nodeadmin/app/view/dashboard/DefaultNodeList.js
@@ -3,7 +3,7 @@ Ext.define('devilry_nodeadmin.view.dashboard.DefaultNodeList', {
     alias: 'widget.defaultnodelist',
     cls: 'devilry_nodeadmin_defaultnodelist bootstrap',
     tpl: [
-        '<h1>', gettext("Navigate"),
+        '<h1>', gettext( 'Navigate' ),
         '<a class="to-frontpage" href="/devilry_frontpage/">',
             '<i class="icon-chevron-left"></i>',
             gettext( 'Or return (to the front-page)' ),
@@ -14,16 +14,14 @@ Ext.define('devilry_nodeadmin.view.dashboard.DefaultNodeList', {
             '<a class="node" href="/devilry_nodeadmin/#/node/{ id }">{ long_name }</a>',
         '</tpl>',
         /*
-        '<div class="footer">Denne listen viser kun de nodene du administrerer. Klikk på et element for å se ',
-        'de underliggende nivåene, emnene og periodene. Du kan gå ett nivå tilbake ved hjelp av knappen ',
-        'i det øvre høyre hjørne.</div>'
+        '<div class="footer"></div>'
         */
         '<div class="footer">',
             gettext( 'These are only the nodes you control. Select an element to see the child levels, its subjects and periods. The menu in the upper right corner of the page will always let you navigate back and to the top of the structure.' ),
         '</div>'
     ],
 
-    itemSelector: 'div.node',
+    itemSelector: 'a.node',
 
     initComponent: function() {
         this.store = Ext.create( 'devilry_nodeadmin.store.RelatedNodes' );
--- src/devilry_nodeadmin/devilry_nodeadmin/static/devilry_nodeadmin/app/view/nodebrowser/NodeBrowserOverview.js
+++ src/devilry_nodeadmin/devilry_nodeadmin/static/devilry_nodeadmin/app/view/nodebrowser/NodeBrowserOverview.js
@@ -16,13 +16,6 @@ Ext.define('devilry_nodeadmin.view.nodebrowser.NodeBrowserOverview', {
         cls: 'devilry_nodeadmin_primary',
         columnWidth: 0.5,
         padding: '30 20 20 30'
-//        items: [{
-//            xtype: 'nodeparentlink',
-//            node_pk: this.node_pk
-//        }, {
-//            xtype: 'nodechildrenlist',
-//            node_pk: this.node_pk
-//        }]
     }, {
         xtype: 'container',
         itemId: 'secondary',
