4a7a0eda9f2aecd969c1145ba334b6441d5d2ec8
Espen A. Kristiansen <post@espenak.net>
2013-01-18 05:18:41 +0100

    nodeadmin.fabfile: Correct ignores.

--- src/devilry_nodeadmin/devilry_nodeadmin/fabfile.py
+++ src/devilry_nodeadmin/devilry_nodeadmin/fabfile.py
@@ -3,8 +3,8 @@ from fabric.api import local, task
 @task
 def makemessages(langcode):
     local(('../../../devenv/bin/django_dev.py makemessages -d djangojs -l {0} '
-           '-i "static/devilry_subjectadmin/app-all.js" '
-           '-i "static/devilry_subjectadmin/all-classes.js"'
+           '-i "static/devilry_nodeadmin/app-all.js" '
+           '-i "static/devilry_nodeadmin/all-classes.js"'
           ).format(langcode))
     #local(('../../../devenv/bin/django_dev.py makemessages -l {0}').format(langcode))
 
