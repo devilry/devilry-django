f11e63c80f92b9d180d3bb6e52bcff38f29a48e0
Espen Angell Kristiansen <post@espenak.net>
2013-01-18 12:33:18 +0100

    subjectadmin...examinerstats: Usable details for each examiner (#302).

--- src/devilry_subjectadmin/devilry_subjectadmin/static/devilry_subjectadmin/app/view/examinerstats/SingleExaminerStatBox.js
+++ src/devilry_subjectadmin/devilry_subjectadmin/static/devilry_subjectadmin/app/view/examinerstats/SingleExaminerStatBox.js
@@ -44,6 +44,13 @@ Ext.define('devilry_subjectadmin.view.examinerstats.SingleExaminerStatBox' ,{
 
     initComponent: function() {
         this._createChartStore();
+        var colormap = {
+            passed: '#468847',
+            failed: '#f89406',
+            waitingforfeedback: '#aaaaaa',
+            waitingfordeliveries: '#eeeeee',
+            closedwithoutfeedback: '#00ffff'
+        };
         Ext.apply(this, {
             layout: 'column',
             items: [{
@@ -72,19 +79,26 @@ Ext.define('devilry_subjectadmin.view.examinerstats.SingleExaminerStatBox' ,{
                     field: 'groups_percent',
                     showInLegend: true,
                     donut: false,
+                    colorSet: [
+                        '#468847', // passed
+                        '#b94a48', // failed
+                        '#999999', // waitingforfeedback
+                        '#eeeeee', // waitingfordeliveries
+                        '#ff0000'  // closedwithoutfeedback
+                    ],
                     tips: {
-                      trackMouse: true,
-                      width: 140,
+                        trackMouse: true,
+                        width: 140,
 //                      height: ,
-                      renderer: function(record, item) {
-                        this.setTitle(record.get('title') + ': ' +
-                            Math.round(record.get('groups_percent')) + '%');
-                      }
+                        renderer: function(record, item) {
+                            this.setTitle(record.get('title') + ': ' +
+                                Math.round(record.get('groups_percent')) + '%');
+                        }
                     },
                     highlight: {
-                      segment: {
-                        margin: 20
-                      }
+                        segment: {
+                            margin: 20
+                        }
                     },
                     label: {
                         field: 'title',
@@ -92,6 +106,13 @@ Ext.define('devilry_subjectadmin.view.examinerstats.SingleExaminerStatBox' ,{
                         contrast: true,
                         font: '12px Ubuntu, sans-serif'
                     }
+//                    renderer: function(sprite, record, attr, index, store) {
+//                        var color = colormap[record.get('name')];
+//                        Ext.apply(attr, {
+//                            fill: color
+//                        });
+//                        return attr;
+//                    }
                 }]
             }]
         });
@@ -100,23 +121,29 @@ Ext.define('devilry_subjectadmin.view.examinerstats.SingleExaminerStatBox' ,{
 
     _createChartStore:function () {
         var data = [{
+            name: 'passed',
             title: gettext('Passed groups'),
             groups_percent: this.examinerstat.get('passed_percent')
         }, {
+            name: 'failed',
             title: gettext('Failed groups'),
             groups_percent: this.examinerstat.get('failed_percent')
         }, {
+            name: 'waitingforfeedback',
             title: gettext('Waiting for feedback'),
             groups_percent: this.examinerstat.get('waitingforfeedback_percent')
         }, {
+            name: 'waitingfordeliveries',
             title: gettext('Waiting for deadline to expire'),
             groups_percent: this.examinerstat.get('waitingfordeliveries_percent')
         }, {
+            name: 'closedwithoutfeedback',
             title: gettext('Closed without feedback'),
             groups_percent: this.examinerstat.get('closedwithoutfeedback_percent')
         }];
         this.chartStore = Ext.create('Ext.data.Store', {
             fields: [
+                {name: 'name', type: 'string'},
                 {name: 'title', type: 'string'},
                 {name: 'groups_percent', type: 'float'}
             ],
