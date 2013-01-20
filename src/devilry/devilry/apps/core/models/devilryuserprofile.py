651e779aa1c6dba145843d6f2b48a7b87df969f2
Espen Angell Kristiansen <post@espenak.net>
2013-01-20 10:57:51 +0100

    apps.core.models.devilryuserprofile: Added get_displayname.

--- src/devilry/devilry/apps/core/models/devilryuserprofile.py
+++ src/devilry/devilry/apps/core/models/devilryuserprofile.py
@@ -117,6 +117,13 @@ class DevilryUserProfile(models.Model):
     class Meta:
         app_label = 'core'
 
+    def get_displayname(self):
+        """
+        Get a name for this user, preferrably the full name, but falls back to username of
+        that is unavailable.
+        """
+        return self.full_name or self.username
+
 
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
