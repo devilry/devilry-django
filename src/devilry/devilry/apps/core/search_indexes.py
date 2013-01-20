d4de3ce0a82f78dcefcb5aa1f8a5e18f5698d7fc
Espen Angell Kristiansen <post@espenak.net>
2013-01-20 11:46:13 +0100

    apps.core.search_indexes: Optimize index queries.

--- src/devilry/devilry/apps/core/search_indexes.py
+++ src/devilry/devilry/apps/core/search_indexes.py
@@ -15,13 +15,19 @@ class AdminsSearchIndex(BaseIndex):
 
 
 class NodeIndex(AdminsSearchIndex):
-    pass
+    def index_queryset(self):
+        qry = super(NodeIndex, self).index_queryset()
+        qry = qry.prefetch_related('admins')
+        return qry
 
 site.register(Node, NodeIndex)
 
 
 class SubjectIndex(AdminsSearchIndex):
-    pass
+    def index_queryset(self):
+        qry = super(SubjectIndex, self).index_queryset()
+        qry = qry.prefetch_related('admins')
+        return qry
 
 site.register(Subject, SubjectIndex)
 
@@ -30,11 +36,30 @@ class PeriodIndex(AdminsSearchIndex):
     start_time = indexes.DateTimeField(model_attr='start_time')
     end_time = indexes.DateTimeField(model_attr='end_time')
 
+    def index_queryset(self):
+        qry = super(PeriodIndex, self).index_queryset()
+        qry = qry.select_related('parentnode')
+        qry = qry.prefetch_related(
+            'admins',
+            'parentnode__admins')
+        return qry
+
 site.register(Period, PeriodIndex)
 
 
 class AssignmentIndex(AdminsSearchIndex):
     publishing_time = indexes.DateTimeField(model_attr='publishing_time')
+
+    def index_queryset(self):
+        qry = super(AssignmentIndex, self).index_queryset()
+        qry = qry.select_related(
+            'parentnode'
+            'parentnode__parentnode')
+        qry = qry.prefetch_related(
+            'admins',
+            'parentnode__admins',
+            'parentnode__parentnode__admins')
+        return qry
 
 site.register(Assignment, AssignmentIndex)
 
@@ -50,6 +75,21 @@ class AssignmentGroupIndex(AdminsSearchIndex):
 
     def prepare_student_ids(self, obj):
         return [candidate.student.id for candidate in obj.candidates.all()]
+
+    def index_queryset(self):
+        qry = super(AssignmentGroupIndex, self).index_queryset()
+        qry = qry.select_related(
+            'parentnode',  'parentnode__parentnode',
+            'parentnode__parentnode__parentnode')
+        qry = qry.prefetch_related(
+            'tags',
+            'parentnode__admins',
+            'parentnode__parentnode__admins',
+            'parentnode__parentnode__parentnode__admins',
+            'examiners', 'examiners__user', 'examiners__user__devilryuserprofile',
+            'candidates', 'candidates__student', 'candidates__student__devilryuserprofile')
+        return qry
+
 
 
 site.register(AssignmentGroup, AssignmentGroupIndex)
