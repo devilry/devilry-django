c041e08849dfdd1f702646ff812055077fc43d16
Espen Angell Kristiansen <post@espenak.net>
2013-01-22 08:50:29 +0100

    devilry_developer.settings.docs: Disable haystack registrations.

--- src/devilry_developer/devilry_developer/settings/docs.py
+++ src/devilry_developer/devilry_developer/settings/docs.py
@@ -5,3 +5,4 @@ from .base import *
 MEDIA_ROOT = "filestore"
 DATABASES['default']['NAME'] = ':memory:'
 DEVILRY_DELIVERY_STORE_BACKEND = 'devilry.apps.core.deliverystore.MemoryDeliveryStore'
+HAYSTACK_ENABLE_REGISTRATIONS = False
