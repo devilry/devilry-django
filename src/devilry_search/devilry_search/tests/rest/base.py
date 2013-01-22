a90a33d761d131912a528e8316e69b4406767599
Espen Angell Kristiansen <post@espenak.net>
2013-01-22 15:30:23 +0100

    devilry_search.tests.rest.base.HaystackTestSettings: Simplify and add warning.
    Our approach did not force the use of a temporary search backend.

--- src/devilry_search/devilry_search/tests/rest/base.py
+++ src/devilry_search/devilry_search/tests/rest/base.py
@@ -5,34 +5,25 @@ from django.core import management
 
 
 
-class HaystackTestSettings(override_settings):
+class HaystackTestSettings(object):
     """
-    Wraps the override_settings context to:
+    .. warning:: Rebuilds the search index! Se why below.
+
+    We would like to extends ``override_settings`` context to:
+
     - set haystack engine to whoosh
     - create the whoosh search index path
     - rebuild the search index on enter
     - remove the search index directory on exit
 
-    ..note::
-        We use a directory in CWD for the temporary index because tempfile.mkdtemp does not work.
-        Using ``HAYSTACK_WHOOSH_STORAGE='ram' does not work for some reason.
+    But that is not easy, so for now we use the index configured in settings.py directly. See
+    the history of this file for our simple tries at solving this problem.
     """
-    def __init__(self):
-        self.whoosh_path = 'devilry_search_tests_tmp_whooshpath'
-        mkdir(self.whoosh_path)
-        super(HaystackTestSettings, self).__init__(
-            HAYSTACK_SEARCH_ENGINE = 'whoosh',
-            HAYSTACK_WHOOSH_PATH = self.whoosh_path
-        )
-
     def __enter__(self):
-        super(HaystackTestSettings, self).__enter__()
         management.call_command('rebuild_index', verbosity=0, interactive=False)
 
     def __exit__(self, *args, **kwargs):
-        super(HaystackTestSettings, self).__exit__(*args, **kwargs)
-        rmtree(self.whoosh_path)
-
+        pass
 
 
 class AssertSearchResultMixin(object):
