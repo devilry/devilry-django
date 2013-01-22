363528c5380ac3586282625f8a6e1b1cae00fae2
Espen Angell Kristiansen <post@espenak.net>
2013-01-22 08:48:50 +0100

    apps.core.models: Relative imports with ``.``

--- src/devilry/devilry/apps/core/models/__init__.py
+++ src/devilry/devilry/apps/core/models/__init__.py
@@ -1,20 +1,20 @@
-from abstract_is_admin import AbstractIsAdmin
-from abstract_is_examiner import AbstractIsExaminer
-from abstract_is_candidate import AbstractIsCandidate
-from basenode import BaseNode
-from node import Node
-from subject import Subject
-from period import Period, PeriodApplicationKeyValue
-from relateduser import RelatedExaminer, RelatedStudent, RelatedStudentKeyValue
-from assignment import Assignment
-from assignment_group import AssignmentGroup, AssignmentGroupTag
-from delivery import Delivery
-from deadline import Deadline
-from candidate import Candidate
-from static_feedback import StaticFeedback
-from filemeta import FileMeta
-from devilryuserprofile import DevilryUserProfile
-from examiner import Examiner
+from .abstract_is_admin import AbstractIsAdmin
+from .abstract_is_examiner import AbstractIsExaminer
+from .abstract_is_candidate import AbstractIsCandidate
+from .basenode import BaseNode
+from .node import Node
+from .subject import Subject
+from .period import Period, PeriodApplicationKeyValue
+from .relateduser import RelatedExaminer, RelatedStudent, RelatedStudentKeyValue
+from .assignment import Assignment
+from .assignment_group import AssignmentGroup, AssignmentGroupTag
+from .delivery import Delivery
+from .deadline import Deadline
+from .candidate import Candidate
+from .static_feedback import StaticFeedback
+from .filemeta import FileMeta
+from .devilryuserprofile import DevilryUserProfile
+from .examiner import Examiner
 
 __all__ = ("AbstractIsAdmin", "AbstractIsExaminer", "AbstractIsCandidate",
            "BaseNode", "Node", "Subject", "Period", 'RelatedExaminer', 'RelatedStudent',
