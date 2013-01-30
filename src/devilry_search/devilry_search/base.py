1bb6715ddfdc6dbf08cc5abbe250e342ea0b3412
Espen Angell Kristiansen <post@espenak.net>
2013-01-30 20:56:48 +0100

    devilry_search.base: Suggestions field.

--- src/devilry_search/devilry_search/base.py
+++ src/devilry_search/devilry_search/base.py
@@ -9,3 +9,9 @@ class BaseIndex(indexes.SearchIndex):
     See: http://unfoldthat.com/2011/05/04/you-should-use-django-haystack-like-this.html
     """
     text = indexes.CharField(document=True, use_template=True)
+    suggestions = indexes.CharField()
+
+    def prepare(self, obj):
+        prepared_data = super(BaseIndex, self).prepare(obj)
+        prepared_data['suggestions'] = prepared_data['text']
+        return prepared_data
