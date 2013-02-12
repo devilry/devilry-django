7ec946bc6ba3ab653fd2d42c16e840f439b4d305
Espen A. Kristiansen <post@espenak.net>
2013-02-12 10:20:06 +0100

    Set version to 1.2.1.4

--- src/devilry/devilry/version.py
+++ src/devilry/devilry/version.py
@@ -1,1 +1,1 @@
-devilry_version = '1.2.1.3'
+devilry_version = '1.2.1.4'
--- src/devilry/setup.py
+++ src/devilry/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "devilry",
-    version = "1.2.1.3",
+    version = "1.2.1.4",
     url = 'http://devilry.org',
     license = 'BSD',
     description = "A system for handling electronic deliveries.",
