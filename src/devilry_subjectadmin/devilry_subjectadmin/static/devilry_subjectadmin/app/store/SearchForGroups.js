50aa821e7abdada0832084a03ba4700805cfc1dc
Espen A. Kristiansen <post@espenak.net>
2013-03-03 23:32:04 +0100

    devilry_subjectadmin...store.SearchForGroups: Use correct URL.
    Append ``/`` to the url to avoid a redirect. I belive this redirect is
    the cause of #408, and if it is not, this is still a bug.

--- src/devilry_subjectadmin/devilry_subjectadmin/static/devilry_subjectadmin/app/store/SearchForGroups.js
+++ src/devilry_subjectadmin/devilry_subjectadmin/static/devilry_subjectadmin/app/store/SearchForGroups.js
@@ -4,7 +4,7 @@ Ext.define('devilry_subjectadmin.store.SearchForGroups', {
     extend: 'devilry_subjectadmin.store.Groups',
     proxy: {
         type: 'rest',
-        urlpatt: DevilrySettings.DEVILRY_URLPATH_PREFIX + '/devilry_subjectadmin/rest/group/{0}',
+        urlpatt: DevilrySettings.DEVILRY_URLPATH_PREFIX + '/devilry_subjectadmin/rest/group/{0}/',
         url: null, // We use urlpatt to dynamically generate the url
         extraParams: {
             format: 'json'
