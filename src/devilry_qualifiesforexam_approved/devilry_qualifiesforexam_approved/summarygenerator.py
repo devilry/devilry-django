9870bb14b01bd1fe2a89484c85d443c88f6cd597
Espen A. Kristiansen <post@espenak.net>
2013-03-16 19:44:49 +0100

    devilry_qualifiesforexam_approved.summarygenerator: Removed print.

--- src/devilry_qualifiesforexam_approved/devilry_qualifiesforexam_approved/summarygenerator.py
+++ src/devilry_qualifiesforexam_approved/devilry_qualifiesforexam_approved/summarygenerator.py
@@ -2,7 +2,6 @@ from django.utils.translation import ugettext as _
 
 
 def make_settings_summary_subset(status):
-    print dir(status)
     settings = status.devilry_qualifiesforexam_approved_subsetpluginsetting
     out = [_(u'Selected assignments'), u': ']
     longnames = [selected.assignment.long_name for selected in settings.selectedassignment_set.all()]
