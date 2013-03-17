23582e9b1d72bb8b6ee12b1d143369eaccedcbd9
Espen A. Kristiansen <post@espenak.net>
2013-03-17 21:08:10 +0100

    devilry_qualifiesforexam...PreviewGrid: Show total points.

--- src/devilry_qualifiesforexam/devilry_qualifiesforexam/static/devilry_qualifiesforexam/app/view/preview/PreviewGrid.js
+++ src/devilry_qualifiesforexam/devilry_qualifiesforexam/static/devilry_qualifiesforexam/app/view/preview/PreviewGrid.js
@@ -4,7 +4,7 @@ Ext.define('devilry_qualifiesforexam.view.preview.PreviewGrid', {
     cls: 'devilry_qualifiesforexam_previewgrid bootstrap',
     mixins: ['devilry_extjsextras.AutoHeightComponentMixin'],
 
-    firstAssignmentColumnIndex: 2,
+    firstAssignmentColumnIndex: 3,
 
     /**
      * @property {int[]} [passing_relatedstudentids_map]
@@ -44,6 +44,31 @@ Ext.define('devilry_qualifiesforexam.view.preview.PreviewGrid', {
             sortable: false,
             renderer: Ext.bind(this._renderQualifiesColumn, this)
         });
+
+        this.columns.push({
+            text: gettext('Total points'),
+            dataIndex: 'id',
+            flex: 1,
+            minWidth: 100,
+            menuDisabled: true,
+            sortable: false,
+            renderer: Ext.bind(this._renderTotalPointsColumn, this)
+        });
+    },
+
+    _renderTotalPointsColumn: function(value, meta, record, rowIndex, colIndex) {
+        var groups_by_assignment = record.get('groups_by_assignment');
+        var total = 0;
+        for(var index=0; index<groups_by_assignment.length; index++)  {
+            var assignment = groups_by_assignment[index];
+            var group = assignment.grouplist[0];
+            var points = 0;
+            if(!Ext.isEmpty(group) && group.status === 'corrected') {
+                points = group.feedback.points;
+            }
+            total += points;
+        }
+        return total.toString();
     },
 
     _qualifiesForFinalExam: function(record) {
