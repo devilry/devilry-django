6a4b15bfe6346ba0538a4f232659e8873dfa2988
Espen A. Kristiansen <post@espenak.net>
2013-04-23 19:22:03 +0200

    devilry_nodeadmin: Cleaner dashboard.
    Removed unneded information.

--- src/devilry_nodeadmin/devilry_nodeadmin/static/devilry_nodeadmin/app/view/dashboard/DefaultNodeList.js
+++ src/devilry_nodeadmin/devilry_nodeadmin/static/devilry_nodeadmin/app/view/dashboard/DefaultNodeList.js
@@ -6,13 +6,6 @@ Ext.define('devilry_nodeadmin.view.dashboard.DefaultNodeList', {
     store: 'RelatedNodes',
 
     tpl: [
-        '<h1>',
-        gettext( 'Navigate' ),
-        '<a class="to-frontpage" href="', window.DevilrySettings.DEVILRY_URLPATH_PREFIX, '/">',
-            '<i class="icon-chevron-left"></i>',
-            gettext( 'Or return (to the front-page)' ),
-        '</a>',
-        '</h1>',
         '<ul class="unstyled devilry_nodeadmin_massive_navlist">',
             '<tpl for=".">',
                 '<li>',
@@ -20,14 +13,12 @@ Ext.define('devilry_nodeadmin.view.dashboard.DefaultNodeList', {
                 '</li>',
             '</tpl>',
         '</ul>',
-        '<div class="footer">',
-            '<div>',
-                interpolate(gettext( 'These are only the nodes you control. Select an element to see the child levels, its %(subjects_term)s and %(periods_term)s. The menu in the upper right corner of the page will always let you navigate back and to the top of the structure.' ), {
-                    subjects_term: gettext('subjects'),
-                    periods_term: gettext('periods')
-                 }, true),
-            '</div>',
-        '</div>'
+        '<p class="muted">',
+            interpolate(gettext( 'These are only the nodes you control. Select an element to see the child levels, its %(subjects_term)s and %(periods_term)s.' ), {
+                subjects_term: gettext('subjects'),
+                periods_term: gettext('periods')
+             }, true),
+        '</p>'
     ],
 
     itemSelector: 'a.node'
