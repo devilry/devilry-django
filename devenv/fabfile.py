72513a449c55098ba0354926074b4cb036706e37
Espen A. Kristiansen <post@espenak.net>
2013-05-01 14:35:42 +0200

    devenv: Added --no-site-packages.

--- devenv/fabfile.py
+++ devenv/fabfile.py
@@ -9,9 +9,9 @@ from fabric.api import local, task
 @task
 def bootstrap():
     """
-    Runs ``virtualenv --no-site-packages . && bin/easy_install zc.buildout && bin/buildout && bin/fab postbootstrap``.
+    Runs ``virtualenv --no-site-packages venv && venv/bin/python ../bootstrap.py && bin/buildout && bin/fab postbootstrap``.
     """
-    local('virtualenv venv')
+    local('virtualenv --no-site-packages venv')
     local('venv/bin/python ../bootstrap.py')
     local('bin/buildout')
     local('bin/fab postbootstrap')
