6f4ee659ac3dd9c9646e71d605efddb5cae90d50
Espen A. Kristiansen <post@espenak.net>
2013-05-22 21:15:16 +0200

    devilry_qualifiesforexam.views: Removed extra comma.

--- src/devilry_qualifiesforexam/devilry_qualifiesforexam/views.py
+++ src/devilry_qualifiesforexam/devilry_qualifiesforexam/views.py
@@ -34,7 +34,7 @@ class StatusPrintViewForm(forms.Form):
         self.helper.form_id = 'sortform'
         self.helper.form_method = 'get'
         self.helper.layout = Layout(
-                Field('sortby', id='sortby-field'),
+                Field('sortby', id='sortby-field')
         )
 
 
