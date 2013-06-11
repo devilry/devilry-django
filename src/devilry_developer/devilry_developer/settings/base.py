68cb8e684086460c981e72fb534337e7cef3706f
Espen A. Kristiansen <post@espenak.net>
2013-06-11 17:17:56 +0200

    devilry_examiner: Skeleton.

--- src/devilry_developer/devilry_developer/settings/base.py
+++ src/devilry_developer/devilry_developer/settings/base.py
@@ -35,6 +35,8 @@ INSTALLED_APPS += [
     'django_extensions',
     'devserver',
 
+    'devilry_examiner',
+
     # Not apps, but here for the Django test system to discover them:
     'devilry.utils',
     'devilry.restful',
@@ -47,7 +49,7 @@ TEMPLATE_DEBUG = DEBUG
 EXTJS4_DEBUG = True
 STATIC_ROOT = 'static'
 
-DEVILRY_ENABLE_MATHJAX = False
+DEVILRY_ENABLE_MATHJAX = True
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '+g$%**q(w78xqa_2)(_+%v8d)he-b_^@d*pqhq!#2p*a7*9e9h'
