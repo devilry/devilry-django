1c400e8fe688db4aa0402e17430f72744cdbc4a8
Espen A. Kristiansen <post@espenak.net>
2013-06-12 13:50:42 +0200

    devilry|devilry_rest: Moved rest stuff from devilry.utils to devilry_rest.
    Kept the modules in devilry.utils, with deprecation warnings.

---
+++ src/devilry_rest/devilry_rest/serializehelpers.py
@@ -0,0 +1,19 @@
+def format_datetime(datetime):
+    return datetime.strftime('%Y-%m-%d %H:%M:%S')
+
+def format_timedelta(timedelta_obj):
+    total_seconds = abs(timedelta_obj.total_seconds())
+    days, remainder = divmod(total_seconds, 86400)
+    hours, remainder = divmod(remainder, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    return {'days': int(days),
+            'hours': int(hours),
+            'minutes': int(minutes),
+            'seconds': int(seconds)}
+
+def serialize_user(user):
+    return {'id': user.id,
+            'username': user.username,
+            'email': user.email,
+            'displayname': user.devilryuserprofile.full_name or user.username,
+            'full_name': user.devilryuserprofile.full_name}
