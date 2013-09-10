32c3f15fd5a74f3ea355cc4f15ac043bd1afc897
Espen A. Kristiansen <post@espenak.net>
2013-09-10 14:17:26 +0200

    devilry_nodeadmin.rest.aggregatedstudentinfo: Some testing.

--- src/devilry_nodeadmin/devilry_nodeadmin/rest/aggregatedstudentinfo.py
+++ src/devilry_nodeadmin/devilry_nodeadmin/rest/aggregatedstudentinfo.py
@@ -1,13 +1,27 @@
 from simple_rest import Resource
 from simple_rest.response import RESTfulResponse
 from devilry_rest.auth import authentication_required
+from django.db.models import Q
 
+from devilry.apps.core.models import Node
+from devilry.apps.core.models import Candidate
+from devilry.apps.core.models import RelatedStudent
 
 
 
 @authentication_required
 class AggregatedStudentInfo(Resource):
     @RESTfulResponse()
-    def get(self, request, **kwargs):
+    def get(self, request, user_id, **kwargs):
         data = ['Hello', 'world']
+        user_obj = self.request.user
+        nodepks_where_is_admin = Node._get_nodepks_where_isadmin(user_obj)
+        qry = Candidate.objects.filter(student_id=user_id)
+        qry = qry.filter(
+                Q(assignment_group__parentnode__admins=user_obj) | \
+                Q(assignment_group__parentnode__parentnode__admins=user_obj) | \
+                Q(assignment_group__parentnode__parentnode__parentnode__admins=user_obj) | \
+                Q(assignment_group__parentnode__parentnode__parentnode__parentnode__pk__in=nodepks_where_is_admin))
+        for candidate in qry:
+            print candidate
         return data
--- src/devilry_nodeadmin/devilry_nodeadmin/tests/rest/test_aggregatedstudentinfo.py
+++ src/devilry_nodeadmin/devilry_nodeadmin/tests/rest/test_aggregatedstudentinfo.py
@@ -16,12 +16,11 @@ class TestRestAggregatedStudentInfo(TestCase):
                             periods=['p1:begins(-1)'],
                             assignments=['a1'])
         self.testhelper.add_to_path('uni;sub.p1.a1.g1:candidate(student1,student2):examiner(examiner1).d1:ends(5)')
-        self.testhelper.add_to_path('uni;sub.p1.a1.g1.d2:ends(10)')
         self.group = self.testhelper.sub_p1_a1_g1
 
 
-    def _get(self, id):
-        return self.client.rest_get(reverse('devilry_nodeadmin-aggregatedstudentinfo', kwargs={'id': id}))
+    def _get(self, user_id):
+        return self.client.rest_get(reverse('devilry_nodeadmin-aggregatedstudentinfo', kwargs={'user_id': user_id}))
 
     def _getas(self, username, id):
         self.client.login(username=username, password='test')
