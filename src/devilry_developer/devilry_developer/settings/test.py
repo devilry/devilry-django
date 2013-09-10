2c4d859378012f04bbae22876e9aaf246cb2a56d
Espen A. Kristiansen <post@espenak.net>
2013-09-10 13:53:29 +0200

    devilry_developer.settings.test: Skip South migrations.

--- src/devilry_developer/devilry_developer/settings/test.py
+++ src/devilry_developer/devilry_developer/settings/test.py
@@ -3,3 +3,6 @@ from .base import *
 # Disable haystack (speeds up tests a lot)
 HAYSTACK_SEARCH_ENGINE = 'dummy' # http://django-haystack.readthedocs.org/en/v1.2.7/tutorial.html#modify-your-settings-py
 HAYSTACK_SITECONF = 'devilry_developer.dummy_haystack_search_sites'
+
+SOUTH_TESTS_MIGRATE = False # To disable migrations and use syncdb instead
+SKIP_SOUTH_TESTS = True # To disable South's own unit tests
