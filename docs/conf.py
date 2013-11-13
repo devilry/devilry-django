64bfdc540ec66a4611b4ee5c951c59ea769510c2
Espen A. Kristiansen <post@espenak.net>
2013-11-14 00:01:35 +0100

    docs.conf: Set theme to default to get the new readthedocs theme.

--- docs/conf.py
+++ docs/conf.py
@@ -136,7 +136,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'armstrong'
+#html_theme = 'armstrong'
 #html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
