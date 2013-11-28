47cca9bf00588037393cc20d6b15299203127182
Espen A. Kristiansen <post@espenak.net>
2013-11-28 19:56:59 +0100

    devilry_examiner.views.dashboard: Very simple plain ul listing.

--- src/devilry_examiner/devilry_examiner/views/dashboard.py
+++ src/devilry_examiner/devilry_examiner/views/dashboard.py
@@ -1,6 +1,13 @@
 from django.views.generic import TemplateView
 
+from devilry.apps.core.models import Assignment
+
 
 
 class DashboardView(TemplateView):
     template_name = "devilry_examiner/dashboard.django.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(DashboardView, self).get_context_data(**kwargs)
+        context['active_assignments'] = Assignment.examiner_objects.active(self.request.user)
+        return context
