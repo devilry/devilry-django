66ff76c92773c837d5e96c7a2419678e5edb765e
Espen A. Kristiansen <post@espenak.net>
2013-12-06 22:40:07 +0100

    devilry_examiner.views.singlegroupoverview: Use the new corebuilder.
    For tests.

--- src/devilry_examiner/devilry_examiner/tests/views/test_singlegroupoverview.py
+++ src/devilry_examiner/devilry_examiner/tests/views/test_singlegroupoverview.py
@@ -2,6 +2,8 @@ from django.test import TestCase
 from django.core.urlresolvers import reverse
 
 from devilry.apps.core.testhelper import TestHelper
+from devilry_developer.testhelpers.corebuilder import NodeBuilder
+from devilry_developer.testhelpers.corebuilder import UserBuilder
 from devilry_developer.testhelpers.soupselect import cssFind
 from devilry_developer.testhelpers.soupselect import cssGet
 
@@ -9,6 +11,12 @@ from devilry_developer.testhelpers.soupselect import cssGet
 class TestSingleGroupOverview(TestCase):
     def setUp(self):
         self.testhelper = TestHelper()
+        self.examiner1 = UserBuilder('examiner1').user
+        self.student1 = UserBuilder('student1').user
+        self.week1builder = NodeBuilder('ducku')\
+                .add_subject('duck1010')\
+                .add_6month_active_period('current')\
+                .add_assignment('week1')
 
     def _getas(self, username, groupid, *args, **kwargs):
         self.client.login(username=username, password='test')
@@ -16,14 +24,11 @@ class TestSingleGroupOverview(TestCase):
                 *args, **kwargs)
 
     def test_header(self):
-        self.testhelper.add(nodes='uni',
-                subjects=['sub'],
-                periods=['period1:begins(-2)'], # 2 months ago
-                assignments=['week2:pub(1)'], # 2 months + 1day ago
-                assignmentgroups=['g1:candidate(student1):examiner(examiner1)'])
-        response = self._getas('examiner1', self.testhelper.sub_period1_week2_g1.id)
+        groupbuilder = self.week1builder.add_group(students=[self.student1], examiners=[self.examiner1])
+        response = self._getas('examiner1', groupbuilder.group.id)
         self.assertEquals(response.status_code, 200)
         html = response.content
+        print html
         #pageheader = cssGet(html, '.assignmentheader h1').strip()
         #self.assertEquals(len(listitems), 1)
         #self.assertEquals(listitems[0].text.strip(), 'sub.period1.week2')
