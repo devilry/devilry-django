48f9a9fb896243387bf69f515440138ac0c481e5
Espen Angell Kristiansen <post@espenak.net>
2013-12-30 21:12:40 +0100

    devilry_examiner...devilry_examiner_tags: Marked translations.

--- src/devilry_examiner/devilry_examiner/templatetags/devilry_examiner_tags.py
+++ src/devilry_examiner/devilry_examiner/templatetags/devilry_examiner_tags.py
@@ -1,32 +1,33 @@
 from django import template
+from django.utils.translation import ugettext_lazy as _
 
 register = template.Library()
 
 @register.filter(name='formatted_status')
 def formatted_status(value):
     if value == 'waiting-for-feedback':
-        return "Waiting for feedback"
+        return _("Waiting for feedback")
     elif value == 'waiting-for-deliveries':
-        return "Waiting for deliveries"
+        return _("Waiting for deliveries")
     elif value == 'no-deadlines':
-        return "No deadlines"
+        return _("No deadlines")
     elif value == 'corrected':
-        return "Corrected"
+        return _("Corrected")
     elif value == 'closed-without-feedback':
-        return "Closed without feedback"
+        return _("Closed without feedback")
     return value
 
 
 @register.filter(name='status_to_buttontext')
 def status_to_buttontext(value):
     if value == 'waiting-for-feedback':
-        return "Write feedback"
+        return _("Write feedback")
     elif value == 'waiting-for-deliveries':
-        return "Waiting for deliveries"
+        return _("Waiting for deliveries")
     elif value == 'no-deadlines':
-        return "No deadlines"
+        return _("No deadlines")
     elif value == 'corrected':
-        return "Show feedback"
+        return _("Show feedback")
     elif value == 'closed-without-feedback':
-        return "Closed without feedback"
+        return _("Closed without feedback")
     return value
