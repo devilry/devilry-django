a98a6480a2fbb7c3886f3893a6cf86f7364e3c5b
Espen Angell Kristiansen <post@espenak.net>
2014-01-05 13:07:25 +0100

    devilry_examiner: Prepare for use of decoupled feedback text editor.

--- src/devilry_examiner/devilry_examiner/static/devilry_examiner/coffee/edit_feedback.coffee
+++ src/devilry_examiner/devilry_examiner/static/devilry_examiner/coffee/edit_feedback.coffee
@@ -1,1 +1,8 @@
 
+class EditFeedback
+  constructor: (@js_editor_api_name) ->
+    
+
+window.devilry_examiner_edit_feedback = {
+  EditFeedback: EditFeedback
+}
--- src/devilry_examiner/devilry_examiner/static/devilry_examiner/js/edit_feedback.js
+++ src/devilry_examiner/devilry_examiner/static/devilry_examiner/js/edit_feedback.js
@@ -1,4 +1,17 @@
 (function() {
+  var EditFeedback;
 
+  EditFeedback = (function() {
+    function EditFeedback(js_editor_api_name) {
+      this.js_editor_api_name = js_editor_api_name;
+    }
+
+    return EditFeedback;
+
+  })();
+
+  window.devilry_examiner_edit_feedback = {
+    EditFeedback: EditFeedback
+  };
 
 }).call(this);
