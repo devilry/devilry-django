b8f18efcbd0fc6e3506dc96a1df7e19ccb2f35c5
Espen Angell Kristiansen <post@espenak.net>
2014-01-06 06:53:25 +0100

    devilry_examiner: More work on the decoupling of feedback editor.

--- src/devilry_examiner/devilry_examiner/static/devilry_examiner/coffee/edit_feedback.coffee
+++ src/devilry_examiner/devilry_examiner/static/devilry_examiner/coffee/edit_feedback.coffee
@@ -1,7 +1,20 @@
 
 class EditFeedback
-  constructor: (@js_editor_api_name) ->
-    
+  ###
+  ###
+  constructor: (unique_name) ->
+    @editor_api = window[unique_name]
+    $footer = $("#wrapper-footer-#{unique_name }")
+    $footer.find('.btn.publish-feedback-without-preview').click(@_onClickPublishWithoutPreview)
+    $footer.find('.btn.preview-feedback').click(@_onClickPreview)
+
+  _onClickPublishWithoutPreview: (e) =>
+    e.preventDefault()
+    console.log '_onClickPublishWithoutPreview'
+
+  _onClickPreview: (e) =>
+    e.preventDefault()
+    console.log '_onClickPreview'
 
 window.devilry_examiner_edit_feedback = {
   EditFeedback: EditFeedback
--- src/devilry_examiner/devilry_examiner/static/devilry_examiner/js/edit_feedback.js
+++ src/devilry_examiner/devilry_examiner/static/devilry_examiner/js/edit_feedback.js
@@ -1,10 +1,30 @@
 (function() {
-  var EditFeedback;
+  var EditFeedback,
+    __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
 
   EditFeedback = (function() {
-    function EditFeedback(js_editor_api_name) {
-      this.js_editor_api_name = js_editor_api_name;
+    /*
+    */
+
+    function EditFeedback(unique_name) {
+      this._onClickPreview = __bind(this._onClickPreview, this);
+      this._onClickPublishWithoutPreview = __bind(this._onClickPublishWithoutPreview, this);
+      var $footer;
+      this.editor_api = window[unique_name];
+      $footer = $("#wrapper-footer-" + unique_name);
+      $footer.find('.btn.publish-feedback-without-preview').click(this._onClickPublishWithoutPreview);
+      $footer.find('.btn.preview-feedback').click(this._onClickPreview);
     }
+
+    EditFeedback.prototype._onClickPublishWithoutPreview = function(e) {
+      e.preventDefault();
+      return console.log('_onClickPublishWithoutPreview');
+    };
+
+    EditFeedback.prototype._onClickPreview = function(e) {
+      e.preventDefault();
+      return console.log('_onClickPreview');
+    };
 
     return EditFeedback;
 
