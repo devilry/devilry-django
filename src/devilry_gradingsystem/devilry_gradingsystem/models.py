02c606542c0c2123926df2ee1e42fffde35ab7a5
Espen Angell Kristiansen <post@espenak.net>
2014-01-08 15:37:48 +0100

    devilry_gradingsystem.models: Turned the skeleton into a usable model.

--- src/devilry_gradingsystem/devilry_gradingsystem/models.py
+++ src/devilry_gradingsystem/devilry_gradingsystem/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.db import models
 from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
@@ -12,11 +13,12 @@ class FeedbackDraft(models.Model):
     automatically created when a FeedbackDraft is published.
     """
     delivery = models.ForeignKey(Delivery, related_name='devilry_gradingsystem_feedbackdraft_set')
-    feedbacktext_raw = models.TextField()
-    feedbacktext_html = models.TextField()
-    save_timestamp = models.DateTimeField(
-        auto_now=True, blank=False, null=False,
-        help_text='Time when this feedback was saved. Since FeedbackDraft is immutable, this never changes.')
+    feedbacktext_raw = models.TextField(
+        blank=True, null=True)
+    feedbacktext_html = models.TextField(
+        blank=True, null=True)
+    points = models.PositiveIntegerField(
+        blank=False, null=False)
     saved_by = models.ForeignKey(User, related_name='devilry_gradingsystem_feedbackdraft_set')
     published = models.BooleanField(default=False,
         help_text='Has this draft been published as a StaticFeedback? Setting this to true on create automatically creates a StaticFeedback.')
@@ -24,9 +26,9 @@ class FeedbackDraft(models.Model):
         blank=True, null=True,
         related_name='devilry_gradingsystem_feedbackdraft_set',
         help_text='The StaticFeedback where this was published if this draft has been published.')
-
-    def _get_config(self):
-        return self.delivery.deadline.assignment_group.parentnode.devilry_gradingsystem_config
+    save_timestamp = models.DateTimeField(
+        blank=False, null=False,
+        help_text='Time when this feedback was saved. Since FeedbackDraft is immutable, this never changes.')
 
     def clean(self):
         if self.id == None: # If creating a new FeedbackDraft
@@ -36,30 +38,5 @@ class FeedbackDraft(models.Model):
             raise ValidationError('FeedbackDraft is immutable (it can not be changed).')
 
     def save(self, *args, **kwargs):
-        """
-        Save the draft and optionally a :class:`devilry.core.models.StaticFeedback`
-        in the database. The ``StaticFeedback`` is only saved if
-        ``self.publish`` is ``True``.
-
-        Uses :meth:`to_staticfeedback` to create the staticfeedback.
-        """
-        if self.published:
-            tmp_staticfeedback = self.to_staticfeedback()
-            tmp_staticfeedback.full_clean()
-            tmp_staticfeedback.save()
-            self.staticfeedback = tmp_staticfeedback # Note: We use tmp_staticfeedback because if we need a variable in which to store the staticfeedback while we save it. We can not just save self.staticfeedback() because that would just create create a copy without actually setting self.staticfeedback to the newly saved value.
+        self.save_timestamp = datetime.now()
         super(FeedbackDraft, self).save(*args, **kwargs)
-
-    def to_staticfeedback(self):
-        """ Return a staticfeedback generated from self. """
-        # gradeeditor = config._get_gradeeditor()
-        # kwargs = gradeeditor.draft_to_staticfeedback_kwargs(self.draft, config.config)
-
-        # Create StaticFeedback from kwargs. We copy by key instead of **kwargs to make sure we dont get anything extra.
-        # return StaticFeedback(is_passing_grade=kwargs['is_passing_grade'],
-        #                       grade=kwargs['grade'],
-        #                       points=kwargs['points'],
-        #                       rendered_view=kwargs['rendered_view'],
-        #                       delivery=self.delivery,
-        #                       save_timestamp=None,
-        #                       saved_by=self.saved_by)
