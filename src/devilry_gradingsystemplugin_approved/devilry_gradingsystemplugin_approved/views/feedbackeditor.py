a7f479b224841d38a8598588408d70b02a69e35e
Tor Ivar Johansen <toij@ifi.uio.no>
2014-01-12 17:56:37 +0100

    devilry_gradingsystemplugin_approved: Fixed point to bool conversion

--- src/devilry_gradingsystemplugin_approved/devilry_gradingsystemplugin_approved/views/feedbackeditor.py
+++ src/devilry_gradingsystemplugin_approved/devilry_gradingsystemplugin_approved/views/feedbackeditor.py
@@ -10,11 +10,13 @@ from devilry_gradingsystem.models import FeedbackDraft
 
 
 class ApprovedFeedbackEditorForm(FeedbackEditorFormBase):
-    points = forms.IntegerField(
-        label=_('Points'))
+    points = forms.BooleanField(
+        label=_('Approved'),
+        help_text=_('Check if delivery is Approved.'),
+        required=False)
 
     def __init__(self, *args, **kwargs):
-        super(PointsFeedbackEditorForm, self).__init__(*args, **kwargs)
+        super(ApprovedFeedbackEditorForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.layout = Layout(
             Field('points')
@@ -23,13 +25,15 @@ class ApprovedFeedbackEditorForm(FeedbackEditorFormBase):
 
 
 class ApprovedFeedbackEditorView(FeedbackEditorFormView):
-    template_name = 'devilry_gradingsystemplugin_points/feedbackeditor.django.html'
+    template_name = 'devilry_gradingsystemplugin_approved/feedbackeditor.django.html'
     form_class = ApprovedFeedbackEditorForm
 
     def get_initial_from_last_draft(self):
-        initial = super(PointsFeedbackEditorView, self).get_initial_from_last_draft()
-        initial['points'] = self.last_draft.points
+        initial = super(ApprovedFeedbackEditorView, self).get_initial_from_last_draft()
+        initial['points'] = bool(self.last_draft.points)
         return initial
 
     def get_points_from_form(self, form):
-        return form.cleaned_data['points']
+        if form.cleaned_data['points']:
+            return 1
+        return 0
