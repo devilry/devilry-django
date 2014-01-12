59806d734106ffd343e26d6c2e5b012e39876a2e
Tor Ivar Johansen <toij@ifi.uio.no>
2014-01-12 15:03:01 +0100

    devilry_gradingsystemplugin_approved: Registred as new plugin

---
+++ src/devilry_gradingsystemplugin_approved/devilry_gradingsystemplugin_approved/devilry_plugin.py
@@ -0,0 +1,17 @@
+from django.utils.translation import ugettext_lazy as _
+from django.core.urlresolvers import reverse
+
+from devilry_gradingsystem.pluginregistry import gradingsystempluginregistry
+from devilry_gradingsystem.pluginregistry import GradingSystemPluginInterface
+
+
+class PointsPluginApi(GradingSystemPluginInterface):
+    id = 'devilry_gradingsystemplugin_approved'
+    title = _('Specify approved or not approved delivery attempt')
+    description = _('Provide a single checkbox which conclude the attempt to be approved when checked')
+
+    def get_edit_feedback_url(self, deliveryid):
+        return reverse('devilry_gradingsystemplugin_approved_feedbackeditor', kwargs={'deliveryid': deliveryid})
+
+
+gradingsystempluginregistry.add(PointsPluginApi)
