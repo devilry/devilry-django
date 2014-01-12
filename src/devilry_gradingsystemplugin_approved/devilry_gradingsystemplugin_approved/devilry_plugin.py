90189cb667ed1f143fbb29fb8b925a3e5cd10341
Tor Ivar Johansen <toij@ifi.uio.no>
2014-01-12 20:20:53 +0100

    devilry_gradingsystemdevilry_gradingsystem: Fixed PluginAPI

--- src/devilry_gradingsystemplugin_approved/devilry_gradingsystemplugin_approved/devilry_plugin.py
+++ src/devilry_gradingsystemplugin_approved/devilry_gradingsystemplugin_approved/devilry_plugin.py
@@ -7,11 +7,19 @@ from devilry_gradingsystem.pluginregistry import GradingSystemPluginInterface
 
 class ApprovedPluginApi(GradingSystemPluginInterface):
     id = 'devilry_gradingsystemplugin_approved'
+    sets_passing_grade_min_points_automatically = True
+    sets_max_points_automatically = True
     title = _('Specify approved or not approved delivery attempt')
     description = _('Provide a single checkbox which conclude the attempt to be approved when checked')
 
     def get_edit_feedback_url(self, deliveryid):
         return reverse('devilry_gradingsystemplugin_approved_feedbackeditor', kwargs={'deliveryid': deliveryid})
+
+    def get_passing_grade_min_points(self):
+        return 1
+
+    def get_max_points(self):
+        return 1
 
 
 gradingsystempluginregistry.add(ApprovedPluginApi)
