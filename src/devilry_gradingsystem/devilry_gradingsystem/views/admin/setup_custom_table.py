18496c60170fcc05fddf0c7b95968c4de749d52b
Espen Angell Kristiansen <post@espenak.net>
2014-01-13 01:46:15 +0100

    devilry_gradingsystem...setup_custom_table: Usable.

--- src/devilry_gradingsystem/devilry_gradingsystem/views/admin/setup_custom_table.py
+++ src/devilry_gradingsystem/devilry_gradingsystem/views/admin/setup_custom_table.py
@@ -11,43 +11,40 @@ from crispy_forms.layout import Layout
 from crispy_forms.layout import Submit
 # from crispy_forms.layout import ButtonHolder
 
-# from devilry.apps.core.models import Assignment
-# from devilry.apps.core.models import PointToGradeMap
 from devilry.apps.core.models import PointRangeToGrade
+from devilry.apps.core.models.pointrange_to_grade import DuplicateGradeError
 from .base import AssignmentSingleObjectRequiresValidPluginMixin
 
 
 
 def point_to_range_grade_form_factory(max_points):
     class PointRangeToGradeForm(forms.Form):
+        grade = forms.CharField(max_length=12, required=False)
         minimum_points = forms.IntegerField(
             min_value=0,
             max_value=max_points,
-            required=True)
-        grade = forms.CharField(max_length=12, required=True)
+            required=False)
     return PointRangeToGradeForm
 
 
 class PointRangeToGradeFormSetHelper(FormHelper):
     def __init__(self, *args, **kwargs):
         super(PointRangeToGradeFormSetHelper, self).__init__(*args, **kwargs)
-        self.form_method = 'post'
+        self.form_tag = False
         self.layout = Layout(
-            'minimum_points',
             'grade',
+            'minimum_points',
         )
-        self.add_input(Submit("submit", _("Save")))
-        #self.render_required_fields = True
+        self.template = 'bootstrap/table_inline_formset.html'
 
 
 class SetupCustomTableView(AssignmentSingleObjectRequiresValidPluginMixin, DetailView):
     template_name = 'devilry_gradingsystem/admin/setup_custom_table.django.html'
 
-    def get_formset_class(self):
+    def get_formset_class(self, extra=0):
         PointRangeToGradeForm = point_to_range_grade_form_factory(self.assignment.max_points)
         PointRangeToGradeFormSet = formset_factory(PointRangeToGradeForm,
-            extra=6,
-            max_num=6)
+            extra=extra)
         return PointRangeToGradeFormSet
 
     def get_initial(self):
@@ -62,30 +59,46 @@ class SetupCustomTableView(AssignmentSingleObjectRequiresValidPluginMixin, Detai
     def get(self, *args, **kwargs):
         self.object = self.get_object()
         self.assignment = self.object
-        PointRangeToGradeFormSet = self.get_formset_class()
-        self.formset = PointRangeToGradeFormSet(initial=self.get_initial())
+        initial = self.get_initial()
+        if initial:
+            extra = 0
+        else:
+            extra = 6
+        PointRangeToGradeFormSet = self.get_formset_class(extra=extra)
+        self.formset = PointRangeToGradeFormSet(initial=initial)
         return super(SetupCustomTableView, self).get(*args, **kwargs)
 
     def post(self, *args, **kwargs):
         self.object = self.get_object()
         self.assignment = self.object
+        add_rows = 'submit_add_rows' in self.request.POST
+        postdata = self.request.POST
+        if add_rows:
+            postdata = postdata.copy()
+            postdata['form-TOTAL_FORMS'] = int(postdata['form-TOTAL_FORMS']) + 3
         PointRangeToGradeFormSet = self.get_formset_class()
-        self.formset = PointRangeToGradeFormSet(self.request.POST)
-        if self.formset.is_valid():
-            minimum_points_to_grade_list = []
-            for valuedict in self.formset.cleaned_data:
-                if valuedict: # We get empty valuedicts even when the form validates, so we have to skip those
-                    minimum_points_to_grade_list.append(
-                        (valuedict['minimum_points'], valuedict['grade']))
-            minimum_points_to_grade_list.sort(lambda a, b: cmp(a[0], b[0]))
-            point_to_grade_map = self.assignment.get_point_to_grade_map()
-            point_to_grade_map.recreate_map(*minimum_points_to_grade_list)
-            try:
-                point_to_grade_map.clean()
-            except ValidationError as e:
-                self.validationerrors = e.messages
-            else:
-               pass
+        self.formset = PointRangeToGradeFormSet(postdata)
+
+        if not add_rows:
+            if self.formset.is_valid():
+                minimum_points_to_grade_list = []
+                for valuedict in self.formset.cleaned_data:
+                    if valuedict and len(valuedict) == 2:
+                        minimum_points_to_grade_list.append(
+                            (valuedict['minimum_points'], valuedict['grade']))
+                minimum_points_to_grade_list.sort(lambda a, b: cmp(a[0], b[0]))
+                point_to_grade_map = self.assignment.get_point_to_grade_map()
+                try:
+                    point_to_grade_map.recreate_map(*minimum_points_to_grade_list)
+                except DuplicateGradeError as e:
+                    self.validationerrors = [e.message]
+                else:
+                    try:
+                        point_to_grade_map.clean()
+                    except ValidationError as e:
+                        self.validationerrors = e.messages
+                    else:
+                       return redirect(self.get_success_url())
         return super(SetupCustomTableView, self).get(*args, **kwargs)
 
     def get_context_data(self, **kwargs):
@@ -96,8 +109,10 @@ class SetupCustomTableView(AssignmentSingleObjectRequiresValidPluginMixin, Detai
         return context
 
     def get_success_url(self):
-        # TODO: Check if passing_grade_min_points is needed
-        return reverse('devilry_gradingsystem_admin_summary', kwargs={
+        if self.assignment.get_gradingsystem_plugin_api().sets_passing_grade_min_points_automatically:
+            viewname = 'devilry_gradingsystem_admin_summary'
+        else:
+            viewname = 'devilry_gradingsystem_admin_setpassing_grade_min_points'
+        return reverse(viewname, kwargs={
             'assignmentid': self.object.id
         })
-
