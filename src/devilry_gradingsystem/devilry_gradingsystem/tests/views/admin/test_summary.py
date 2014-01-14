347c27c8a25eae82ac915f21b5c03c84593e2ecb
Espen Angell Kristiansen <post@espenak.net>
2014-01-14 22:08:46 +0100

    devilry_gradingsystem...summary: Tests for the messages.

--- src/devilry_gradingsystem/devilry_gradingsystem/tests/views/admin/test_summary.py
+++ src/devilry_gradingsystem/devilry_gradingsystem/tests/views/admin/test_summary.py
@@ -8,6 +8,7 @@ from devilry_developer.testhelpers.soupselect import cssGet
 from devilry_developer.testhelpers.soupselect import cssFind
 from devilry_gradingsystem.pluginregistry import GradingSystemPluginRegistry
 
+from devilry_gradingsystem.models import FeedbackDraft
 from .base import AdminViewTestMixin
 # from .base import MockApprovedPluginApi
 from .base import MockPointsPluginApi
@@ -35,3 +36,68 @@ class TestSummaryView(TestCase, AdminViewTestMixin):
             html = response.content
             self.assertEquals(cssGet(html, '.page-header h1').text.strip(),
                 'Grading system')
+
+    def test_no_grading_system_configured(self):
+        myregistry = GradingSystemPluginRegistry()
+        self.assignmentbuilder.update(
+            grading_system_plugin_id=None
+        )
+        with patch('devilry.apps.core.models.assignment.gradingsystempluginregistry', myregistry):
+            response = self.get_as(self.admin1)
+            self.assertEquals(response.status_code, 200)
+            html = response.content
+            self.assertIn('No grading system configured.', html)
+
+    def test_invalid_grading_setup(self):
+        myregistry = GradingSystemPluginRegistry()
+        myregistry.add(MockPointsPluginApi)
+        with patch('devilry.apps.core.models.assignment.gradingsystempluginregistry', myregistry):
+            self.assignmentbuilder.update(
+                grading_system_plugin_id=MockPointsPluginApi.id,
+                max_points=None
+            )
+            response = self.get_as(self.admin1)
+            self.assertEquals(response.status_code, 200)
+            html = response.content
+            self.assertIn('The grading system is not configured correctly.', html)
+
+    def test_no_drafts_or_feedbacks_message(self):
+        myregistry = GradingSystemPluginRegistry()
+        with patch('devilry.apps.core.models.assignment.gradingsystempluginregistry', myregistry):
+            response = self.get_as(self.admin1)
+            self.assertEquals(response.status_code, 200)
+            html = response.content
+            self.assertIn('You can safely reconfigure the grading system for this assignment.', html)
+
+    def test_has_feedbackdrafts_message(self):
+        delivery = self.assignmentbuilder\
+            .add_group()\
+            .add_deadline_in_x_weeks(weeks=1)\
+            .add_delivery_x_hours_before_deadline(hours=1).delivery
+        examiner1 = UserBuilder('examiner1').user
+        FeedbackDraft.objects.create(
+            points=40,
+            delivery=delivery,
+            saved_by=examiner1,
+            feedbacktext_html='This is a test.'
+        )
+        myregistry = GradingSystemPluginRegistry()
+        with patch('devilry.apps.core.models.assignment.gradingsystempluginregistry', myregistry):
+            response = self.get_as(self.admin1)
+            self.assertEquals(response.status_code, 200)
+            html = response.content
+            self.assertIn('You can reconfigure the grading system for this assignment, but be careful, at least one examiner has saved a feedback draft.', html)
+
+    def test_has_staticfeedbacks_warning(self):
+        examiner1 = UserBuilder('examiner1').user
+        delivery = self.assignmentbuilder\
+            .add_group()\
+            .add_deadline_in_x_weeks(weeks=1)\
+            .add_delivery_x_hours_before_deadline(hours=1)\
+            .add_passed_feedback(saved_by=examiner1)
+        myregistry = GradingSystemPluginRegistry()
+        with patch('devilry.apps.core.models.assignment.gradingsystempluginregistry', myregistry):
+            response = self.get_as(self.admin1)
+            self.assertEquals(response.status_code, 200)
+            html = response.content
+            self.assertIn('You SHOULD NOT reconfigure the grading system for this assignment.', html)
