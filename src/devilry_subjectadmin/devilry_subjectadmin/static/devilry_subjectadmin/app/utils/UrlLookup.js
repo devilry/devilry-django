8fdb283b3c66a3f16b8e034f197cbf532e02f1be
Espen Angell Kristiansen <post@espenak.net>
2014-01-14 17:12:57 +0100

    devilry_subjectadmin...UrlLookup: Updated links to examiner UI.

--- src/devilry_subjectadmin/devilry_subjectadmin/static/devilry_subjectadmin/app/utils/UrlLookup.js
+++ src/devilry_subjectadmin/devilry_subjectadmin/static/devilry_subjectadmin/app/utils/UrlLookup.js
@@ -123,18 +123,16 @@ Ext.define('devilry_subjectadmin.utils.UrlLookup', {
     //
     //
     examinerGroupOverview: function(group_id) {
-        return Ext.String.format('{0}/examiner/assignmentgroup/{1}',
+        return Ext.String.format('{0}/devilry_examiner/singlegroupoverview/{1}',
             window.DevilrySettings.DEVILRY_URLPATH_PREFIX,
             group_id);
     },
     examinerGroupsOverviewTodo: function(assignment_id) {
-        return Ext.String.format('{0}/examiner/assignment/{1}',
+        return Ext.String.format('{0}/devilry_examiner/allgroupsoverview/{1}',
             window.DevilrySettings.DEVILRY_URLPATH_PREFIX,
             assignment_id);
     },
     examinerGroupsOverviewStudents: function(assignment_id) {
-        return Ext.String.format('{0}/examiner/assignment/{1}#students',
-            window.DevilrySettings.DEVILRY_URLPATH_PREFIX,
-            assignment_id);
+        return this.examinerGroupsOverviewTodo(assignment_id);
     }
 });
