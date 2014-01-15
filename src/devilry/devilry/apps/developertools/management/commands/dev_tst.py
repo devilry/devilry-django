2470484b9fb83846134c4d942a3efd1db0bc1250
Espen Angell Kristiansen <post@espenak.net>
2014-01-15 23:13:36 +0100

    apps.developertools...dev_tst: smart_create deadline testing.

--- src/devilry/devilry/apps/developertools/management/commands/dev_tst.py
+++ src/devilry/devilry/apps/developertools/management/commands/dev_tst.py
@@ -1,20 +1,16 @@
 from django.core.management.base import BaseCommand
-from haystack.query import SearchQuerySet
 from django.contrib.auth.models import User
-from devilry.apps.core.models import AssignmentGroup
+from datetime import datetime
 
 
 class Command(BaseCommand):
     help = 'tst'
 
     def handle(self, *args, **options):
-        thor = User.objects.get(username='thor')
-#        print thor.id
-        results = SearchQuerySet().filter(admin_ids=thor.id).models(AssignmentGroup).auto_query('donald')
-        for result in results:
-#            admin_ids = map(int, result.admins)
-#            admins = User.objects.filter(id__in=admin_ids)
-#            print dir(result)
-            print result.object, result.student_ids, result.model_name
-            print result.text
-#            print thor in admins, result.object.get_path()
+        from devilry.apps.core.models import Deadline
+        from devilry.apps.core.models import Assignment
+        assignment = Assignment.objects.get(id=1)
+        Deadline.objects.smart_create(
+            assignment.assignmentgroups.all(), #.filter(id=55),
+            deadline_datetime=datetime.now(),
+            text='Hello world')
