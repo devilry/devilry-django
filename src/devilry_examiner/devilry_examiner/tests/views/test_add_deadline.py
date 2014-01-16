c52a0a99d1fb1bb6d26b354cf641ed074eeef8e5
Espen Angell Kristiansen <post@espenak.net>
2014-01-16 17:30:25 +0100

    devilry_examiner...add_deadline: Usable.
    The UI makes it hard not to include a message as explained in #435.

--- src/devilry_examiner/devilry_examiner/tests/views/test_add_deadline.py
+++ src/devilry_examiner/devilry_examiner/tests/views/test_add_deadline.py
@@ -1,11 +1,15 @@
 from django.test import TestCase
 from django.core.urlresolvers import reverse
 
+from devilry.apps.core.models import Deadline
 from devilry_developer.testhelpers.corebuilder import PeriodBuilder
 from devilry_developer.testhelpers.corebuilder import UserBuilder
+from devilry_developer.testhelpers.datebuilder import DateTimeBuilder
 from devilry_developer.testhelpers.soupselect import cssFind
 from devilry_developer.testhelpers.soupselect import cssGet
 from devilry_developer.testhelpers.soupselect import cssExists
+from devilry_examiner.tests.utils import isoformat_datetime
+
 
 
 class TestAddDeadlineView(TestCase):
@@ -13,20 +17,63 @@ class TestAddDeadlineView(TestCase):
         self.examiner1 = UserBuilder('examiner1').user
         self.assignment1builder = PeriodBuilder.quickadd_ducku_duck1010_active()\
             .add_assignment('assignment1', long_name='Assignment One')
+        self.url = reverse('devilry_examiner_add_deadline', kwargs={
+            'assignmentid': self.assignment1builder.assignment.id})
 
-    def _getas(self, username, *args, **kwargs):
-        self.client.login(username=username, password='test')
-        return self.client.get(reverse('devilry_examiner_add_deadline', kwargs={
-            'assignmentid': self.assignment1builder.assignment.id}), *args, **kwargs)
+    def _getas(self, user, *args, **kwargs):
+        self.client.login(username=user.username, password='test')
+        return self.client.get(self.url, *args, **kwargs)
 
-    def test_404_when_not_examiner(self):
-        response = self._getas('examiner1')
+    def _postas(self, user, *args, **kwargs):
+        self.client.login(username=user.username, password='test')
+        return self.client.post(self.url, *args, **kwargs)
+
+
+    ##############################
+    # Security checks
+    ##############################
+
+    def test_post_200_when_examiner(self):
+        group1 = self.assignment1builder\
+            .add_group(examiners=[self.examiner1]).group
+        response = self._postas(self.examiner1, {
+            'group_ids': [group1.id]
+        })
+        self.assertEquals(response.status_code, 200)
+
+    def test_post_404_when_not_examiner(self):
+        group1 = self.assignment1builder\
+            .add_group().group # Not adding any examiners.
+        response = self._postas(self.examiner1, {
+            'group_ids': [group1.id]
+        })
         self.assertEquals(response.status_code, 404)
 
-    def test_header(self):
-        group1 = deliverybuilder = self.assignment1builder\
+    def test_get_200_when_examiner(self):
+        group1 = self.assignment1builder\
             .add_group(examiners=[self.examiner1]).group
-        response = self._getas('examiner1', {
+        response = self._getas(self.examiner1, {
+            'group_ids': [group1.id]
+        })
+        self.assertEquals(response.status_code, 200)
+
+    def test_get_404_when_not_examiner(self):
+        group1 = self.assignment1builder\
+            .add_group().group # Not adding any examiners.
+        response = self._getas(self.examiner1, {
+            'group_ids': [group1.id]
+        })
+        self.assertEquals(response.status_code, 404)
+
+
+    #######################################
+    # Initial rendering
+    #######################################
+
+    def test_header(self):
+        group1 = self.assignment1builder\
+            .add_group(examiners=[self.examiner1]).group
+        response = self._postas(self.examiner1, {
             'group_ids': [group1.id]
         })
         self.assertEquals(response.status_code, 200)
@@ -35,3 +82,77 @@ class TestAddDeadlineView(TestCase):
             'Add deadline')
         self.assertEquals(cssGet(html, '.page-header .subheader').text.strip(),
             "Assignment One &mdash; duck1010, active")
+
+
+    ##########################################
+    # Form handling
+    ##########################################
+
+    def test_post_single(self):
+        group1builder = self.assignment1builder\
+            .add_group(examiners=[self.examiner1])
+        deadline_datetime = Deadline.reduce_datetime_precision(DateTimeBuilder.now().plus(days=10))
+        response = self._postas(self.examiner1, {
+            'group_ids': [group1builder.group.id],
+            'deadline': isoformat_datetime(deadline_datetime),
+            'text': 'Hello world',
+            'submit_primary': 'i18nlabel'
+        })
+        self.assertEquals(response.status_code, 302)
+        group1builder.reload_from_db()
+        self.assertEquals(group1builder.group.last_deadline.deadline, deadline_datetime)
+        self.assertEquals(group1builder.group.last_deadline.text, 'Hello world')
+
+
+    def test_post_multiple(self):
+        group1builder = self.assignment1builder\
+            .add_group(examiners=[self.examiner1])
+        group2builder = self.assignment1builder\
+            .add_group(examiners=[self.examiner1])
+        deadline_datetime = Deadline.reduce_datetime_precision(DateTimeBuilder.now().plus(days=10))
+        response = self._postas(self.examiner1, {
+            'group_ids': [group1builder.group.id, group2builder.group.id],
+            'deadline': isoformat_datetime(deadline_datetime),
+            'text': 'Hello world',
+            'submit_primary': 'i18nlabel'
+        })
+        self.assertEquals(response.status_code, 302)
+
+        group1builder.reload_from_db()
+        self.assertEquals(group1builder.group.last_deadline.deadline, deadline_datetime)
+        self.assertEquals(group1builder.group.last_deadline.text, 'Hello world')
+
+        group2builder.reload_from_db()
+        self.assertEquals(group2builder.group.last_deadline.deadline, deadline_datetime)
+        self.assertEquals(group2builder.group.last_deadline.text, 'Hello world')
+
+    def test_post_no_text(self):
+        group1builder = self.assignment1builder\
+            .add_group(examiners=[self.examiner1])
+        deadline_datetime = Deadline.reduce_datetime_precision(DateTimeBuilder.now().plus(days=10))
+        response = self._postas(self.examiner1, {
+            'group_ids': [group1builder.group.id],
+            'deadline': isoformat_datetime(deadline_datetime),
+            'submit_primary': 'i18nlabel',
+            'no_text': 'on'
+        })
+        self.assertEquals(response.status_code, 302)
+        group1builder.reload_from_db()
+        self.assertEquals(group1builder.group.last_deadline.deadline, deadline_datetime)
+        self.assertEquals(group1builder.group.last_deadline.text, '')
+
+
+    def test_post_no_text_checkbox_required(self):
+        group1builder = self.assignment1builder\
+            .add_group(examiners=[self.examiner1])
+        deadline_datetime = Deadline.reduce_datetime_precision(DateTimeBuilder.now().plus(days=10))
+        response = self._postas(self.examiner1, {
+            'group_ids': [group1builder.group.id],
+            'deadline': isoformat_datetime(deadline_datetime),
+            'submit_primary': 'i18nlabel'
+        })
+        self.assertEquals(response.status_code, 200)
+        group1builder.reload_from_db()
+        self.assertEquals(group1builder.group.last_deadline, None)
+        self.assertIn('You must specify a deadline text, or select that you do not want to specify any text.',
+            response.content)
--- src/devilry_examiner/devilry_examiner/views/add_deadline.py
+++ src/devilry_examiner/devilry_examiner/views/add_deadline.py
@@ -8,17 +8,45 @@ from crispy_forms.layout import Layout
 from crispy_forms.layout import Field
 from crispy_forms.layout import Submit
 from crispy_forms.layout import Hidden
+from crispy_forms.layout import HTML
 from crispy_forms.layout import ButtonHolder
 
+from devilry.apps.core.models import Deadline
 from devilry.apps.core.models import Assignment
 from devilry_examiner.forms import GroupIdsForm
 from .crispylayout import DefaultSubmit
 from .bulkviewbase import BulkViewBase
 
 
+
+class DevilryDatetimeFormField(forms.DateTimeField):
+    def __init__(self, **kwargs):
+        input_formats = kwargs.pop('input_formats', ['%Y-%m-%d %H:%M'])
+        error_messages = kwargs.pop('error_messages', {
+            'invalid': _('Enter a valid date/time. Example "2014-12-24 18:00" to specify Dec 24 2014 6pm.')
+        })
+        kwargs.update({
+            'input_formats': input_formats,
+            'error_messages': error_messages
+        })
+        super(DevilryDatetimeFormField, self).__init__(**kwargs)
+
+
+
 class AddDeadlineForm(GroupIdsForm):
-    deadline = forms.DateTimeField()
-    text = forms.CharField(required=False)
+    deadline = DevilryDatetimeFormField(
+        label=_('Deadline'),
+        help_text=_('Format YYYY-MM-DD hh:mm'),
+    )
+    text = forms.CharField(
+        label=_('Text'),
+        help_text=_('Why are you adding this deadline? Make this informative, and remember that this information may be important for course administrators if they need to move the deadline.'),
+        widget=forms.Textarea(attrs={'rows': 7}),
+        required=False)
+    no_text = forms.BooleanField(
+        label=_('I do not want to provide a deadline text.'),
+        help_text=_('You should normally tell your students why they get a new deadline. If some rare cases this makes no sense, so you can select this option to avoid specifying a text.'),
+        required=False, initial=False)
 
     def clean(self):
         cleaned_data = super(AddDeadlineForm, self).clean()
@@ -26,18 +54,32 @@ class AddDeadlineForm(GroupIdsForm):
         if deadline and hasattr(self, 'cleaned_groups'):
             if self.cleaned_groups.filter(last_deadline__deadline__gt=deadline).exists():
                 raise forms.ValidationError('The last deadline of one or more of the selected groups is after the selected deadline.')
+
+        text = cleaned_data.get('text', None)
+        no_text = cleaned_data.get('no_text', False)
+        if not text and not no_text:
+            error_message = _('You must specify a deadline text, or select that you do not want to specify any text.')
+            self._errors["text"] = self.error_class([error_message])
+            self._errors["no_text"] = self.error_class([error_message])
+            if 'text' in cleaned_data:
+                del cleaned_data['text']
+            if 'no_text' in cleaned_data:
+                del cleaned_data['no_text']
+
         return cleaned_data
 
     def __init__(self, *args, **kwargs):
         super(AddDeadlineForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.layout = Layout(
-            'deadline',
+            Field('deadline', placeholder='YYYY-MM-DD hh:mm'),
             'text',
+            'no_text',
             'group_ids',
+            HTML('<hr>'),
             ButtonHolder(
-                DefaultSubmit('submit_go_back', _('Back')),
-                Submit('submit_add_deadline', _('Add deadline'))
+                Submit('submit_primary', _('Add deadline'), css_class='pull-right'),
+                DefaultSubmit('submit_cancel', _('Cancel')),
             )
         )
 
@@ -47,16 +89,12 @@ class AddDeadlineView(BulkViewBase):
     form_class = AddDeadlineForm
 
     def form_valid(self, form):
-        print
-        print 'Create deadline on:'
-        for group in form.cleaned_groups:
-            print group
-        print
-        return super(AddDeadlineView, self).form_valid()
-
+        Deadline.objects.smart_create(
+            form.cleaned_groups,
+            deadline_datetime=form.cleaned_data['deadline'],
+            text=form.cleaned_data['text'])
+        return super(AddDeadlineView, self).form_valid(form)
 
     def get_context_data(self, **kwargs):
         context = super(AddDeadlineView, self).get_context_data(**kwargs)
-        print context['form']
-        print context['group_ids_form']
         return context
