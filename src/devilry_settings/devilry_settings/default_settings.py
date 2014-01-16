0e3d42477d8255008c1d59aef2d58b4e3cf80753
Espen Angell Kristiansen <post@espenak.net>
2014-01-16 21:10:10 +0100

    devilry_settings.default_settings: Added django.core.context_processors.request.

--- src/devilry_settings/devilry_settings/default_settings.py
+++ src/devilry_settings/devilry_settings/default_settings.py
@@ -96,6 +96,7 @@ TEMPLATE_CONTEXT_PROCESSORS = ("django.contrib.auth.context_processors.auth",
                                "django.core.context_processors.i18n",
                                "django.core.context_processors.media",
                                "django.core.context_processors.debug",
+                               "django.core.context_processors.request",
                                'django.contrib.messages.context_processors.messages',
                                'extjs4.context_processors.extjs4',
                                'devilry.apps.theme.templatecontext.template_variables')
