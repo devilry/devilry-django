e3b6b417bd66edb862d4da512ebc5f84ac15184b
Tor Ivar Johansen <toij@ifi.uio.no>
2014-01-19 16:56:56 +0100

    devilry_search: Simple test for About Me

--- src/devilry_search/devilry_search/tests/__init__.py
+++ src/devilry_search/devilry_search/tests/__init__.py
@@ -3,3 +3,4 @@ from .rest.studentcontent import TestRestSearchStudentContent
 from .rest.examinercontent import TestRestSearchExaminerContent
 from .test_views_search import *
 from .test_search_helper import *
+from .test_aboutme import TestAboutMeView
