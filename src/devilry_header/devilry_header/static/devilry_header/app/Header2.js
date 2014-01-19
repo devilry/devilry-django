bfc1621737609292ce6c2144df40b425598c176c
Espen Angell Kristiansen <post@espenak.net>
2014-01-19 20:34:36 +0100

    devilry_header: Version 2 working with ExtJS.

--- src/devilry_header/devilry_header/static/devilry_header/app/Header2.js
+++ src/devilry_header/devilry_header/static/devilry_header/app/Header2.js
@@ -1,13 +1,18 @@
 /**
 Devilry header version2 compatibility layer for the ExtJS apps. Renders as an empty
-block of height 40px - this makes room for the header. The header is included in the 
+block of height 70px - this makes room for the header. The header is included in the 
 header-block of the apptemplate, and is not removed when the viewport renders.
 */
 Ext.define('devilry_header.Header2', {
     extend: 'Ext.container.Container',
     alias: 'widget.devilryheader2',
     margin: '0 0 0 0',
-    height: 40, // NOTE: Make sure to adjust to the height of the actual devilry_header2
+
+    // NOTE: Make sure to adjust to the height of the height of
+    // the .devilry_header2_extjs css class, plus room for the shadow.
+    height: 42,
+    heightWithoutBreadcrumb: 42,
+    heightWithBreadcrumb: 72,
 
     requires: [
         'devilry_header.Breadcrumbs'
@@ -25,32 +30,51 @@ Ext.define('devilry_header.Header2', {
      */
 
     constructor: function(config) {
-        config.cls = 'devilry_header2_extjsplaceholder';
-        this.breadcrumbWrapper = Ext.widget('container', {
-            cls: 'devilry_header2_breadcrumbwrapper'
-        });
+        config.cls = 'devilry_header2_extjsplaceholder';        
+        this.headerel = Ext.get('devilry_header2');
         this.callParent(arguments);
         this.setNavClass(config.navclass);
     },
 
     initComponent: function() {
+        this.callParent(arguments);
+
         var breadcrumbareaItem;
         if(this.breadcrumbs) {
             breadcrumbareaItem = this.breadcrumbs;
         } else {
             breadcrumbareaItem = Ext.widget('breadcrumbs');
         }
-        this.callParent(arguments);
+        this._setBreadcrumbComponent(breadcrumbareaItem);
     },
 
     setNavClass: function(navclass) {
         this.navclass = navclass;
-        headerel = Ext.get('devilry_header2');
-        headerel.addCls(Ext.String.format('devilry_header2_role_{0}', navclass));
+        this.headerel.addCls(Ext.String.format('devilry_header2_role_{0}', navclass));
     },
 
-    setBreadcrumbComponent: function(config) {
-        this.breadcrumbWrapper.removeAll();
-        this.breadcrumbWrapper.add(config);
+    _setBreadcrumbComponent: function(breadcrumbareaItem) {
+        var breadcrumbWrapper = Ext.widget('container', {
+            cls: 'devilry_header2_breadcrumbwrapper'
+        });
+        breadcrumbWrapper.add(breadcrumbareaItem);
+        var breadcrumbElement = Ext.get('devilry_header2_extjsbreadcrumb');
+        breadcrumbWrapper.render(breadcrumbElement);
     },
+
+    /*
+    Used by devilry_header.Breadcrumbs, not for external use!
+    */
+    resizeHeaderForBreadcrumbs: function() {
+        this.setHeight(this.heightWithBreadcrumb);
+        this.headerel.addCls('devilry_header2_extjs_with_breadcrumb');
+    },
+
+    /*
+    Used by devilry_header.Breadcrumbs, not for external use!
+    */
+    resizeHeaderForNoBreadcrumbs: function() {
+        this.setHeight(this.heightWithoutBreadcrumb);
+        this.headerel.removeCls('devilry_header2_extjs_with_breadcrumb');
+    }
 });
