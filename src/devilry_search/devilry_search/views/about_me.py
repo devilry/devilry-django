fb81b364f8e83bcb32a66511b8d15c8dc6f6f7a1
Tor Ivar Johansen <toij@ifi.uio.no>
2014-01-19 18:00:13 +0100

    devilry_search: Updated with wrong info link

--- src/devilry_search/devilry_search/views/about_me.py
+++ src/devilry_search/devilry_search/views/about_me.py
@@ -1,7 +1,7 @@
 from django.views.generic import DetailView
 from django.contrib.auth.models import User
 from django.conf import settings
-
+from devilry_settings.views import urlsetting_or_unsetview
 
 class AboutMeView(DetailView):
     template_name = "devilry_search/about_me.django.html"
@@ -13,4 +13,5 @@ class AboutMeView(DetailView):
     def get_context_data(self, **kwargs):
         context = super(AboutMeView, self).get_context_data(**kwargs)
         context['logout_url'] = settings.DEVILRY_LOGOUT_URL
+        context['wrong_info_url'] = urlsetting_or_unsetview('DEVILRY_WRONG_USERINFO_URL')
         return context
