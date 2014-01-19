aebd08f5a613777511ba98ac56fe56ff7ae5836a
Espen Angell Kristiansen <post@espenak.net>
2014-01-19 15:01:29 +0100

    devilry_subjectadmin: Redirect views for subject, period, assignment and group.
    Needed for search.

--- src/devilry_subjectadmin/devilry_subjectadmin/urls.py
+++ src/devilry_subjectadmin/devilry_subjectadmin/urls.py
@@ -5,6 +5,8 @@ from django.views.decorators.csrf import csrf_protect, ensure_csrf_cookie
 
 from devilry_settings.i18n import get_javascript_catalog_packages
 from .views import AppView
+from .views import RedirectToSubjectAdminAppView
+from .views import RedirectToGroupAdminAppView
 from .exportdetailedperiodoverview import ExportDetailedPeriodOverview
 
 
@@ -14,15 +16,40 @@ def emptyview(request):
     return HttpResponse('Logged in')
 
 
-i18n_packages = get_javascript_catalog_packages('devilry_subjectadmin', 'devilry_extjsextras', 'devilry.apps.core')
+i18n_packages = get_javascript_catalog_packages(
+    'devilry_subjectadmin', 'devilry_extjsextras', 'devilry.apps.core')
 
-urlpatterns = patterns('devilry_subjectadmin',
-                       url('^rest/', include('devilry_subjectadmin.rest.urls')),
-                       url('^$', login_required(csrf_protect(ensure_csrf_cookie(AppView.as_view()))),
-                          name='devilry_subjectadmin'),
-                       url('^emptytestview', emptyview), # NOTE: Only used for testing
-                       url('^export/periodoverview/(?P<id>[^/]+)$',
-                            login_required(ExportDetailedPeriodOverview.as_view()),
-                            name='devilry_subjectadmin_export_period_details'),
-                       url('^i18n.js$', javascript_catalog, kwargs={'packages': i18n_packages},
-                           name='devilry_subjectadmin_i18n'))
+urlpatterns = patterns(
+    'devilry_subjectadmin',
+    url('^rest/',
+        include('devilry_subjectadmin.rest.urls')),
+    url('^$', login_required(csrf_protect(ensure_csrf_cookie(AppView.as_view()))),
+        name='devilry_subjectadmin'),
+    # NOTE: Only used for testing
+    url('^emptytestview', emptyview),
+    url('^export/periodoverview/(?P<id>[^/]+)$',
+        login_required(
+            ExportDetailedPeriodOverview.as_view()),
+        name='devilry_subjectadmin_export_period_details'),
+    url('^i18n.js$', javascript_catalog, kwargs={'packages': i18n_packages},
+        name='devilry_subjectadmin_i18n'),
+
+    # Views that just redirect to ``/devilry_subjectadmin/#<some-path>``
+    url('^subject/(?P<id>[^/]+)',
+        login_required(RedirectToSubjectAdminAppView.as_view(pathformat='/subject/{id}/')),
+        name='devilry_subjectadmin_subject'),
+
+    # Views that just redirect to ``/devilry_subjectadmin/#<some-path>``
+    url('^subject/(?P<id>\d+)',
+        login_required(RedirectToSubjectAdminAppView.as_view(pathformat='/subject/{id}/')),
+        name='devilry_subjectadmin_subject'),
+    url('^period/(?P<id>\d+)',
+        login_required(RedirectToSubjectAdminAppView.as_view(pathformat='/period/{id}/')),
+        name='devilry_subjectadmin_period'),
+    url('^assignment/(?P<id>\d+)',
+        login_required(RedirectToSubjectAdminAppView.as_view(pathformat='/assignment/{id}/')),
+        name='devilry_subjectadmin_assignment'),
+    url('^group/(?P<id>\d+)',
+        login_required(RedirectToGroupAdminAppView.as_view()),
+        name='devilry_subjectadmin_group'),
+)
--- src/devilry_subjectadmin/devilry_subjectadmin/views.py
+++ src/devilry_subjectadmin/devilry_subjectadmin/views.py
@@ -1,14 +1,14 @@
 from django.utils.translation import ugettext as _
 from django.views.generic import View
 from django.http import HttpResponse
-from django.http import HttpResponseBadRequest
-from django.http import HttpResponseForbidden
-from extjs4.views import Extjs4AppView
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from django.views.generic import View
+from django.shortcuts import get_object_or_404
 from cStringIO import StringIO
+from extjs4.views import Extjs4AppView
 
-from devilry.apps.core.models import Period
-from devilry.utils.groups_groupedby_relatedstudent_and_assignment import GroupsGroupedByRelatedStudentAndAssignment
-
+from devilry.apps.core.models import AssignmentGroup
 
 
 class AppView(Extjs4AppView):
@@ -17,3 +17,23 @@ class AppView(Extjs4AppView):
     #css_staticpath = 'devilry_theme/resources/stylesheets/devilry.css'
     #css_staticpath = 'extjs4/resources/css/ext-all-gray.css'
     title = _('Devilry - Subject administrator')
+
+
+
+
+class RedirectToSubjectAdminAppView(View):
+    pathformat = None
+
+    def get(self, request, **kwargs):
+        path = self.pathformat.format(**kwargs)
+        url = '{}?routeTo={}'.format(reverse('devilry_subjectadmin'), path)
+        return HttpResponseRedirect(url)
+
+
+class RedirectToGroupAdminAppView(View):
+    def get(self, request, id):
+        group = get_object_or_404(AssignmentGroup, id=id)
+        path = '/assignment/{assignment.id}/@@manage-students/@@select/{group.id}'.format(
+            assignment=group.assignment, group=group)
+        url = '{}?routeTo={}'.format(reverse('devilry_subjectadmin'), path)
+        return HttpResponseRedirect(url)
