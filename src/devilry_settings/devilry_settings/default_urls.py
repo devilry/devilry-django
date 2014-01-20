a92ab54e37bbac186f36170d00453721dca08586
Espen Angell Kristiansen <post@espenak.net>
2014-01-20 02:33:48 +0100

    devilry_settings: Imported development URLs for new examiner UI.

--- src/devilry_settings/devilry_settings/default_urls.py
+++ src/devilry_settings/devilry_settings/default_urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import include
+from django.conf.urls.defaults import url
 from django.contrib import admin
 from devilry_frontpage.views import frontpage
 
@@ -30,5 +31,9 @@ devilry_urls = ((r'^student/', include('devilry.apps.student.urls')),
                 (r'^devilry_qualifiesforexam_approved/', include('devilry_qualifiesforexam_approved.urls')),
                 (r'^devilry_qualifiesforexam_points/', include('devilry_qualifiesforexam_points.urls')),
                 (r'^devilry_qualifiesforexam_select/', include('devilry_qualifiesforexam_select.urls')),
+                url(r'^devilry_examiner/', include('devilry_examiner.urls')),
+                url(r'^devilry_gradingsystem/', include('devilry_gradingsystem.urls')),
+                url(r'^devilry_gradingsystemplugin_points/', include('devilry_gradingsystemplugin_points.urls')),
+                url(r'^devilry_gradingsystemplugin_approved/', include('devilry_gradingsystemplugin_approved.urls')),
                 (r'^$', frontpage),
-               )
+)
