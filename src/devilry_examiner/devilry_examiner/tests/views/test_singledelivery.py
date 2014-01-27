72c54264a65cbae8ec9777309e5f20b4eb6d1deb
Espen Angell Kristiansen <post@espenak.net>
2014-01-27 01:45:10 +0100

    apps.core.models.delivery: Really fix is_last_delivery.
    Fixes #521.

--- src/devilry_examiner/devilry_examiner/tests/views/test_singledelivery.py
+++ src/devilry_examiner/devilry_examiner/tests/views/test_singledelivery.py
@@ -94,12 +94,14 @@ class TestSingleDeliveryView(TestCase):
         self.assertFalse(cssExists(html, '.not_last_delivery_message'))
 
     def test_not_last_delivery_message(self):
-        deadlinebuilder = self.week1builder\
-            .add_group(examiners=[self.examiner1])\
-            .add_deadline_x_weeks_ago(weeks=1)
+        groupbuilder = self.week1builder.add_group(examiners=[self.examiner1])
+        deadlinebuilder = groupbuilder.add_deadline_x_weeks_ago(weeks=1)
         delivery1 = deadlinebuilder.add_delivery_x_hours_before_deadline(hours=1).delivery
         delivery2 = deadlinebuilder.add_delivery_x_hours_after_deadline(hours=1).delivery
         response = self._getas('examiner1', delivery1.id)
+
+        groupbuilder.reload_from_db()
+        self.assertEquals(groupbuilder.group.last_delivery, delivery2)
         html = response.content
         self.assertEquals(
             cssGet(html, '.not_last_delivery_message').text.strip(),
