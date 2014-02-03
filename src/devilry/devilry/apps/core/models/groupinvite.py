d71b6d0a46d2dce8c69efc40cf99ed214c54ec37
Espen Angell Kristiansen <post@espenak.net>
2014-02-03 20:00:27 +0100

    apps.core.models.groupinvite: Validation.
    See #526.

--- src/devilry/devilry/apps/core/models/groupinvite.py
+++ src/devilry/devilry/apps/core/models/groupinvite.py
@@ -1,13 +1,21 @@
 from datetime import datetime
 from django.db import models
+from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
 from .assignment_group import AssignmentGroup
 
 
 
 class GroupInviteQuerySet(models.query.QuerySet):
     def filter_accepted(self):
-        return self.filter(accepted_datetime__isnull=False)
+        return self.filter(accepted=True)
+
+    def filter_rejected(self):
+        return self.filter(accepted=False)
+
+    def filter_no_response(self):
+        return self.filter(accepted=None)
 
 
 class GroupInviteManager(models.Manager):
@@ -26,15 +34,18 @@ class GroupInviteManager(models.Manager):
     def filter_rejected(self):
         return self.get_queryset().filter_rejected()
 
+    def filter_no_response(self):
+        return self.get_queryset().filter_no_response()
+
 
 class GroupInvite(models.Model):
     group = models.ForeignKey(AssignmentGroup)
     sent_datetime = models.DateTimeField(default=datetime.now)
     sent_by = models.ForeignKey(User, related_name='groupinvite_sent_by_set')
     sent_to = models.ForeignKey(User, related_name='groupinvite_sent_to_set')
-    accepted_datetime = models.DateTimeField(
-        default=None, blank=True, null=True)
-    rejected_datetime = models.DateTimeField(
+
+    accepted = models.NullBooleanField(default=None)
+    responded_datetime = models.DateTimeField(
         default=None, blank=True, null=True)
 
     objects = GroupInviteManager()
@@ -42,10 +53,17 @@ class GroupInvite(models.Model):
     class Meta:
         app_label = 'core'
 
-    @property
-    def accepted(self):
-        return self.accepted_datetime != None
+    def clean(self):
+        if self.accepted and not self.responded_datetime:
+            self.responded_datetime = datetime.now()
+        if self.sent_by and not self.group.candidates.filter(student=self.sent_by).exists():
+            raise ValidationError('The user sending an invite must be a Candiate on the group.')
+        if self.sent_to and self.group.candidates.filter(student=self.sent_to).exists():
+            raise ValidationError(_(u'The student is already a member of the group.'))
 
-    @property
-    def rejected(self):
-        return self.rejected_datetime != None
+        assignment = self.group.assignment
+        if assignment.students_can_create_groups:
+            if assignment.students_can_not_create_groups_after and assignment.students_can_not_create_groups_after < datetime.now():
+                raise ValidationError(_('Creating project groups without administrator approval is not allowed anymore. Please contact you course administrator if you think this is wrong.'))
+        else:
+            raise ValidationError(_('This assignment does not allow project groups.'))
--- src/devilry/devilry/apps/core/tests/groupinvite.py
+++ src/devilry/devilry/apps/core/tests/groupinvite.py
@@ -1,11 +1,14 @@
 from datetime import datetime
+from datetime import timedelta
 
 from django.test import TestCase
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 # from django.core.exceptions import ValidationError
 
 from devilry_developer.testhelpers.corebuilder import UserBuilder
 from devilry_developer.testhelpers.corebuilder import PeriodBuilder
+from devilry_developer.testhelpers.datebuilder import DateTimeBuilder
 # from devilry.apps.core.models import Assignment
 from devilry.apps.core.models import GroupInvite
 
@@ -14,16 +17,74 @@ class TestGroupInvite(TestCase):
     def setUp(self):
         self.testuser1 = UserBuilder('testuser1').user
         self.testuser2 = UserBuilder('testuser2').user
+        self.testuser3 = UserBuilder('testuser3').user
 
     def test_create_sanity(self):
         group = PeriodBuilder.quickadd_ducku_duck1010_active()\
             .add_assignment('assignment1',
                 passing_grade_min_points=1,
                 students_can_create_groups=True)\
-            .add_group().group
+            .add_group(students=[self.testuser1]).group
         before = datetime.now()
-        invite = group.groupinvite_set.create(
+        invite = GroupInvite(
+            group=group,
             sent_by=self.testuser1,
             sent_to=self.testuser2)
+        invite.full_clean()
+        invite.save()
         after = datetime.now()
         self.assertTrue(invite.sent_datetime >= before and invite.sent_datetime <= after)
+
+    def test_only_groupmember_can_invite(self):
+        group = PeriodBuilder.quickadd_ducku_duck1010_active()\
+            .add_assignment('assignment1',
+                passing_grade_min_points=1,
+                students_can_create_groups=True)\
+            .add_group(students=[self.testuser1]).group
+        with self.assertRaises(ValidationError):
+            GroupInvite(group=group, sent_by=self.testuser2, sent_to=self.testuser3).clean()
+
+    def test_can_not_invite_groupmember(self):
+        group = PeriodBuilder.quickadd_ducku_duck1010_active()\
+            .add_assignment('assignment1',
+                passing_grade_min_points=1,
+                students_can_create_groups=True)\
+            .add_group(students=[self.testuser1, self.testuser2]).group
+        with self.assertRaises(ValidationError):
+            GroupInvite(group=group, sent_by=self.testuser1, sent_to=self.testuser2).clean()
+
+    def test_can_not_invite_self(self):
+        group = PeriodBuilder.quickadd_ducku_duck1010_active()\
+            .add_assignment('assignment1',
+                passing_grade_min_points=1,
+                students_can_create_groups=True)\
+            .add_group(students=[self.testuser1]).group
+        with self.assertRaises(ValidationError):
+            GroupInvite(group=group, sent_by=self.testuser1, sent_to=self.testuser1).clean()
+
+    def test_only_when_allowed_on_assignment(self):
+        group = PeriodBuilder.quickadd_ducku_duck1010_active()\
+            .add_assignment('assignment1',
+                passing_grade_min_points=1,
+                students_can_create_groups=False)\
+            .add_group(students=[self.testuser1]).group
+        invite = GroupInvite(
+            group=group,
+            sent_by=self.testuser1,
+            sent_to=self.testuser2)
+        with self.assertRaises(ValidationError):
+            invite.full_clean()
+
+    def test_students_can_not_create_groups_after(self):
+        group = PeriodBuilder.quickadd_ducku_duck1010_active()\
+            .add_assignment('assignment1',
+                passing_grade_min_points=1,
+                students_can_not_create_groups_after=DateTimeBuilder.now().minus(days=1),
+                students_can_create_groups=True)\
+            .add_group(students=[self.testuser1]).group
+        invite = GroupInvite(
+            group=group,
+            sent_by=self.testuser1,
+            sent_to=self.testuser2)
+        with self.assertRaises(ValidationError):
+            invite.full_clean()
