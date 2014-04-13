c0d112743dbbf414242072c9f8d54b74c414e4c0
Espen Angell Kristiansen <post@espenak.net>
2014-04-13 16:54:24 +0200

    core.models.examiner: Improved docs, and added bulkremove_examiners_from_groups().

--- src/devilry/devilry/apps/core/models/examiner.py
+++ src/devilry/devilry/apps/core/models/examiner.py
@@ -5,8 +5,10 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.db.models import Q
 
-from abstract_is_admin import AbstractIsAdmin
-from node import Node
+from .abstract_is_admin import AbstractIsAdmin
+from .node import Node
+from .relateduser import RelatedExaminer
+from .relateduser import RelatedStudent
 
 
 
@@ -92,8 +94,32 @@ class ExaminerManager(models.Manager):
         :param groups:
             Iterable of :class:`devilry.apps.core.models.AssignmentGroup`
             objects to remove all current examiners from.
+
+        .. seealso:: :meth:`.bulkremove_examiners_from_groups`.
         """
         Examiner.objects.filter(assignmentgroup__in=groups).delete()
+
+
+    def bulkremove_examiners_from_groups(self, examinerusers, groups):
+        """
+        Remove the given examiners from the given groups.
+
+        Examples::
+
+            Examiner.objects.bulkremove_examiners_from_groups([user1, user2], [group1, group2])
+            Examiner.objects.bulkremove_examiners_from_groups(
+                [user1, user2],
+                AssignmentGroup.objects.filter(parentnode=myassignment))
+
+        :param examinerusers: An iterable of user objects.
+        :param groups:
+            Iterable of :class:`devilry.apps.core.models.AssignmentGroup`
+            objects to remove all current examiners from.
+
+        .. seealso:: :meth:`.bulkclear_examiners_from_groups`.
+        """
+        Examiner.objects.filter(user__in=examinerusers, assignmentgroup__in=groups).delete()
+
 
 
     def randomdistribute_examiners(self, examinerusers, groups):
@@ -117,6 +143,15 @@ class ExaminerManager(models.Manager):
                 examinerusers=[user1, user2],
                 groups=AssignmentGroup.objects.filter(parentnode=myassignment))
 
+
+        :param examinerusers: An iterable of user objects.
+        :param groups:
+            Iterable of :class:`devilry.apps.core.models.AssignmentGroup`
+            objects to remove all current examiners from.
+        :return: ``None``. We do not perform a query to get the created examiners because
+            we assume the most common scenario is to not do anything more with the examiners
+            right after they have been created.
+
         :raises django.db.IntegrityError
             If any of the ``examinerusers`` is already examiner on any of the groups,
             the method will fail with IntegrityError, if we happen
@@ -138,6 +173,59 @@ class ExaminerManager(models.Manager):
         self.bulk_create(examiners_to_create)
 
 
+    def setup_examiners_by_relatedusertags(self, period, examinerusers, groups):
+        """
+        TODO:
+            Use RelatedExaminer tags, and match them with the current tags of the groups.
+            This way, admins can change the tags of the groups and then apply examiners by tag.
+
+        :param examinerusers: An iterable of user objects.
+        :param groups:
+            Iterable of :class:`devilry.apps.core.models.AssignmentGroup`
+            objects to remove all current examiners from.
+        :return: ``None``. We do not perform a query to get the created examiners because
+            we assume the most common scenario is to not do anything more with the examiners
+            right after they have been created.
+
+        .. note::
+
+            You will usually want to call::
+
+                Examiner.objects.bulkremove_examiners_from_groups(examinerusers, groups)
+
+            before calling this method unless you KNOW that the ``examinerusers`` is
+            not examiner on any of the ``groups``.
+
+        :raises django.db.IntegrityError
+            If any of the ``examinerusers`` is already examiner on any of the
+            groups they are assigned by tag.
+        """
+
+        relatedexaminersbytag = period.relatedexaminers_by_tag()
+        relatatedstudentsbytag = period.relatedstudents_by_tag()
+        self.bulkclear_examiners_from_groups(groups)
+
+        # Group AssignmentGroups by user to make direct lookup by user possible
+        groupsbyuser = {}
+        candidates = Candidate.objects.filter(assignmentgroup__in=groups)\
+            .select_related('assignmentgroup', 'student')
+        for candidate in candidates:
+            if not candidate.student in groupsbyuser:
+                groupsbyuser[candidate.student] = []
+            groupsbyuser[candidate.student].append(candidate.group)
+
+        examiners_to_create = []
+        # Loop through relatedexaminers grouped by tag
+        for tag, relatedexaminers in relatatedstudentsbytag.iteritems():
+            # Loop through all relatedstudents matching the same tag as the current relatedexaminer
+            for relatedstudent in relatatedstudentsbytag.get(tag, []):
+                # Loop through all groups where the relatedstudent in candidate (usually 0 or 1)
+                for group in groupsbyuser.get(relatedstudent.user, []):
+                    # Add all relatedexaminers with the current tag to the matched group
+                    for relatedexaminer in relatedexaminers:
+                        examiner = Examiner(user=relatedexaminer.user, assignmentgroup=group)  
+                        examiners_to_create.append(examiner)
+        self.bulk_create(examiners_to_create)
 
 
 class Examiner(models.Model, AbstractIsAdmin):
--- src/devilry/devilry/apps/core/tests/examiner.py
+++ src/devilry/devilry/apps/core/tests/examiner.py
@@ -57,6 +57,33 @@ class TestExaminer(TestCase):
         self.assertEquals(Examiner.objects.count(), 1)
         self.assertEquals(Examiner.objects.all()[0].assignmentgroup, ignoredgroup)
 
+    def test_bulkremove_examiners_from_groups(self):
+        assignmentbuilder = PeriodBuilder.quickadd_ducku_duck1010_active()\
+            .add_assignment('assignment1')
+        examineruser1 = UserBuilder('examineruser1').user
+        examineruser2 = UserBuilder('examineruser2').user
+        examineruser3 = UserBuilder('examineruser3').user
+        group1builder = assignmentbuilder.add_group(
+            examiners=[examineruser1])
+        group2builder = assignmentbuilder.add_group(
+            examiners=[examineruser1, examineruser2])
+        group3builder = assignmentbuilder.add_group()
+        group4builder = assignmentbuilder.add_group(
+            examiners=[examineruser3])
+        ignoredgroupbuilder = assignmentbuilder.add_group(examiners=[examineruser1])
+
+        self.assertEquals(Examiner.objects.count(), 5)
+        Examiner.objects.bulkremove_examiners_from_groups(
+            [examineruser1, examineruser3],
+            [group1builder.group, group2builder.group, group3builder.group, group4builder.group])
+        self.assertEquals(Examiner.objects.count(), 2)
+        self.assertEquals(
+            set([examiner.assignmentgroup for examiner in Examiner.objects.all()]),
+            set([ignoredgroupbuilder.group, group2builder.group]))
+        group2builder.reload_from_db()
+        self.assertEquals(group2builder.group.examiners.count(), 1)
+        self.assertEquals(group2builder.group.examiners.all()[0].user, examineruser2)
+
 
     def test_randomdistribute_examiners_even(self):
         assignmentbuilder = PeriodBuilder.quickadd_ducku_duck1010_active()\
@@ -88,3 +115,38 @@ class TestExaminer(TestCase):
         self.assertEquals(Examiner.objects.count(), 3)
         self.assertIn(Examiner.objects.filter(user=examineruser1).count(), [1, 2])
         self.assertIn(Examiner.objects.filter(user=examineruser2).count(), [1, 2])
+
+
+
+
+    # def test_setup_examiners_by_relatedusertags(self):
+    #     studentuser1 = UserBuilder('studentuser1').user
+
+    #     periodbuilder = PeriodBuilder.quickadd_ducku_duck1010_active()
+    #     period = periodbuilder.period
+    #     relatedexaminer1 = period.relatedexaminer_set.create(
+    #         user=,
+    #         tags='group1,special')
+    #     relatedexaminer2 = period.relatedexaminer_set.create(
+    #         user=UserBuilder('studentuser2').user,
+    #         tags='group1')
+    #     relatedexaminer3 = period.relatedexaminer_set.create(
+    #         user=UserBuilder('studentuser3').user,
+    #         tags='group2,special')
+
+    #     assignmentbuilder = periodbuilder.add_assignment('assignment1')
+    #     group1 = assignmentbuilder.add_group().group
+    #     group2 = assignmentbuilder.add_group().group
+    #     examineruser1 = UserBuilder('examineruser1').user
+    #     examineruser2 = UserBuilder('examineruser2').user
+
+    #     self.assertEquals(Examiner.objects.count(), 0)
+    #     Examiner.objects.bulkadd_examiners_to_groups(
+    #         examinerusers=[examineruser1, examineruser2],
+    #         groups=[group1, group2])
+    #     self.assertEquals(Examiner.objects.count(), 4)
+    #     self.assertEquals(group1.examiners.count(), 2)
+    #     self.assertEquals(group2.examiners.count(), 2)
+    #     self.assertEquals(
+    #         set([e.user for e in group1.examiners.all()]),
+    #         set([examineruser1, examineruser2]))
