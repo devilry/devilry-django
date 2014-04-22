3cc73949eec8c9fcf23bbd88aced5cdff9438e60
Espen Angell Kristiansen <post@espenak.net>
2014-04-22 17:11:06 +0200

    devilry_header: Removed the ExtJS app.

--- src/devilry_header/devilry_header/static/devilry_header/app/store/BaseSearchResults.js
+++
@@ -1,8 +0,0 @@
-Ext.define('devilry_header.store.BaseSearchResults', {
-    extend: 'Ext.data.Store',
-
-    search: function (params, loadConfig) {
-        Ext.apply(this.proxy.extraParams, params);
-        this.load(loadConfig);
-    }
-});
--- src/devilry_header/devilry_header/static/devilry_header/app/store/ExaminerSearchResults.js
+++
@@ -1,4 +0,0 @@
-Ext.define('devilry_header.store.ExaminerSearchResults', {
-    extend: 'devilry_header.store.BaseSearchResults',
-    model: 'devilry_header.model.ExaminerSearchResult'
-});
