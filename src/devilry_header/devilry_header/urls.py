092e6fc9f17fc28994e3e3020e91e88f59dd7e00
Espen Angell Kristiansen <post@espenak.net>
2014-04-22 18:56:53 +0200

    devilry_header: Reverted:
    	commit c035b277de33156d3d249109ba68f292143e4a4d
    	Author: Espen Angell Kristiansen <post@espenak.net>
    	Date:   Tue Apr 22 17:06:13 2014 +0200
    
    	    devilry_header: Removed the old header view.

--- src/devilry_header/devilry_header/urls.py
+++ src/devilry_header/devilry_header/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, url
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_protect, ensure_csrf_cookie
 
+from .views.appview import AppView
 from .views.about_me import AboutMeView
 from .views.change_language import ChangeLanguageView
 
@@ -11,5 +13,7 @@ urlpatterns = patterns('devilry_header',
         name='devilry_header_aboutme'),
     url('^change_language$',
         login_required(ChangeLanguageView.as_view()),
-        name='devilry_change_language')
+        name='devilry_change_language'),
+
+    url('^$', login_required(csrf_protect(ensure_csrf_cookie(AppView.as_view())))),
 )
