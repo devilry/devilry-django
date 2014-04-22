22f8bbfdbd058612f688102e6733408c2e7a320d
Espen Angell Kristiansen <post@espenak.net>
2014-04-22 14:57:17 +0200

    devilry_frontpage.views.FrontpageView: Skeleton.

--- src/devilry_frontpage/devilry_frontpage/views.py
+++ src/devilry_frontpage/devilry_frontpage/views.py
@@ -1,3 +1,4 @@
+from django.views.generic import TemplateView
 from django.views.decorators.csrf import csrf_protect, ensure_csrf_cookie
 from django.contrib.auth.decorators import login_required
 from django.utils.translation import ugettext as _
@@ -12,3 +13,14 @@ class AppView(Extjs4AppView):
 
 
 frontpage = login_required(csrf_protect(ensure_csrf_cookie(AppView.as_view())))
+
+
+
+
+class FrontpageView(TemplateView):
+    def get_context_data(self, **kwargs):
+        context = super(FrontpageView, self).get_context_data(**kwargs)
+        context['user_is_student'] = user_is_student()
+        context['user_is_examiner'] = user_is_examiner()
+        context['user_is_admin_or_superadmin'] = user_is_admin_or_superadmin()
+        return context
