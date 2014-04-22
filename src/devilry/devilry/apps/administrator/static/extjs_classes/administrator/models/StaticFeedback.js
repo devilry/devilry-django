a4cd8482abefcafab9683ca6ab55fb797f8ae0b5
Espen Angell Kristiansen <post@espenak.net>
2014-04-22 14:40:59 +0200

    apps.administrator: Deleted all old views - only keeping REST apis.

--- src/devilry/devilry/apps/administrator/static/extjs_classes/administrator/models/StaticFeedback.js
+++
@@ -1,7 +0,0 @@
-Ext.define('devilry.administrator.models.StaticFeedback', {
-    extend: 'devilry.extjshelpers.models.StaticFeedback',
-    belongsTo: 'devilry.administrator.models.Delivery',
-    proxy: Ext.create('devilry.extjshelpers.RestProxy', {
-        url: '/administrator/restfulsimplifiedstaticfeedback/'
-    })
-});
