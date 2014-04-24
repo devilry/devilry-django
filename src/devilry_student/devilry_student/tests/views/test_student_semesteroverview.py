178cf452f4f506121acf1a10d7092b2459cf86ed
Tor Ivar Johansen <toij@ifi.uio.no>
2014-04-24 23:32:39 +0200

    devilry_student: updated tests

--- src/devilry_student/devilry_student/tests/views/test_student_semesteroverview.py
+++ src/devilry_student/devilry_student/tests/views/test_student_semesteroverview.py
@@ -98,25 +98,42 @@ class TestSemesterOverview(TestCase):
         self.assertEquals(response.status_code, 200)
 
         self.assertEquals(len(cssFind(html, '.qualifies-for-exams')), 1)
+
+    def test_get_qualifiesforexam_noe(self):
+        period = PeriodBuilder.quickadd_ducku_duck1010_active()
         
-    # def test_get_qualifiesforexam_none(self):
-    #     self.testhelper.add_to_path('uni;sub.p1.a1.g1:candidate(student1).d1')
-    #     content, response = self._getas('student1')
-    #     subjects = content
-    #     self.assertEquals(response.status_code, 200)
-    #     period = subjects[0]['periods'][0]
-    #     self.assertIsNone(period['qualifiesforexams'])
+        groupbuilder = period.add_assignment('assignment1')\
+        .add_group(students=[self.testuser])
+        period.add_relatedstudents(self.testuser)
 
-    # def test_get_qualifiesforexam_notready(self):
-    #     self.testhelper.add_to_path('uni;sub.p1.a1.g1:candidate(student1).d1')
-    #     status = Status.objects.create(period=self.testhelper.sub_p1,
-    #             user=self.testhelper.testuser,
-    #             status=Status.NOTREADY)
-    #     relstudent = self.testhelper.sub_p1.relatedstudent_set.create(user=self.testhelper.student1)
-    #     status.students.create(relatedstudent=relstudent, qualifies=False)
+        deadline1builder = groupbuilder.add_deadline_x_weeks_ago(weeks=4)
+        delivery1 = deadline1builder.add_delivery_x_hours_before_deadline(hours=10).delivery
 
-    #     content, response = self._getas('student1')
-    #     subjects = content
-    #     self.assertEquals(response.status_code, 200)
-    #     period = subjects[0]['periods'][0]
-    #     self.assertIsNone(period['qualifiesforexams'])
+        response = self._getas(self.testuser, id=period.period.id)
+        html = response.content
+        self.assertEquals(response.status_code, 200)
+
+        self.assertEquals(len(cssFind(html, '.qualifies-for-exams')), 0)  
+
+    def test_get_qualifiesforexam_notready(self):
+        period = PeriodBuilder.quickadd_ducku_duck1010_active()
+        
+        groupbuilder = period.add_assignment('assignment1')\
+        .add_group(students=[self.testuser])
+        period.add_relatedstudents(self.testuser)
+
+        deadline1builder = groupbuilder.add_deadline_x_weeks_ago(weeks=4)
+        delivery1 = deadline1builder.add_delivery_x_hours_before_deadline(hours=10).delivery
+
+        relatedstudent = period.period.relatedstudent_set.get(user=self.testuser)
+
+        status = Status.objects.create(period=period.period,
+                user=self.testuser,
+                status=Status.NOTREADY)
+        status.students.create(relatedstudent=relatedstudent, qualifies=False)
+
+        response = self._getas(self.testuser, id=period.period.id)
+        html = response.content
+        self.assertEquals(response.status_code, 200)
+
+        self.assertEquals(len(cssFind(html, '.qualifies-for-exams')), 0)
