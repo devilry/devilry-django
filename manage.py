a9cbacebe9d7a1a9b9578aed560523c20f164e16
Espen Angell Kristiansen <post@espenak.net>
2014-05-13 14:16:51 +0200

    Update manage.py to enable us to select settings file with DJANGOENV.

--- manage.py
+++ manage.py
@@ -3,7 +3,15 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "devilry_developer.settings.base")
+
+    DJANGOENV = os.environ.get('DJANGOENV', 'develop')
+
+    if DJANGOENV == 'develop': # Used for local development
+        from devilry_developer.settings.develop import *
+    elif DJANGOENV == 'test': # Used when running the Django tests
+        from devilry_developer.settings.test import *
+    else:
+        raise ValueError('Invalid value for the DJANGOENV environment variable: {}'.format(DJANGOENV))
 
     from django.core.management import execute_from_command_line
 
