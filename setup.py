7d1386dd4b5351b784031cf80d928c0ba972803b
Espen Angell Kristiansen <post@espenak.net>
2014-08-08 21:50:12 +0200

    Made the changes required to requirements/ for the new src/ hierarchy.
    Also added a setup.py to the root of the repo.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="devilry",
+    version="1.4.8",
+    url='http://devilry.org',
+    license='BSD',
+    description="A system for handling electronic deliveries.",
+    author='The Devilry developers',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=['setuptools']
+)
