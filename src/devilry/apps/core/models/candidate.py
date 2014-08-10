c2113b782e7fff83418b39f460ee9a4ef09f613b
Espen Angell Kristiansen <post@espenak.net>
2014-08-10 03:27:20 +0200

    core.models.candidate: Simplified, removing ALL cache fields.
    Also removed the AbstractIsAdmin, the post_save handlers used to update
    the cache fields and the etag field.

--- src/devilry/apps/core/models/candidate.py
+++ src/devilry/apps/core/models/candidate.py
@@ -1,16 +1,8 @@
-from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.db import models
-from django.db.models import Q
-
-from model_utils import Etag
-from abstract_is_admin import AbstractIsAdmin
-from node import Node
-from devilryuserprofile import DevilryUserProfile
 
 
-
-class Candidate(models.Model, Etag, AbstractIsAdmin):
+class Candidate(models.Model):
     """
     .. attribute:: assignment_group
 
@@ -26,86 +18,22 @@ class Candidate(models.Model, Etag, AbstractIsAdmin):
         more than 30 characters. When the assignment is anonymous, this is
         the "name" shown to examiners instead of the username of the
         student.
-
-    .. attribute:: identifier
-        The candidate_id if this is a candidate on an anonymous assignment, and username if not.
     """
 
     class Meta:
         app_label = 'core'
 
     student = models.ForeignKey(User)
-    assignment_group = models.ForeignKey('AssignmentGroup',
-                                         related_name='candidates')
-
-    candidate_id = models.CharField(max_length=30, blank=True, null=True, help_text='An optinal candidate id. This can be anything as long as it is less than 30 characters.')
-    identifier = models.CharField(max_length=30,
-                                  help_text='The candidate_id if this is a candidate on an anonymous assignment, and username if not.')
-    full_name = models.CharField(max_length=300, blank=True, null=True,
-                                 help_text='None if this is a candidate on an anonymous assignment, and full name if not.')
-    email = models.CharField(max_length=300, blank=True, null=True,
-                                 help_text='None if this is a candidate on an anonymous assignment, and email address if not.')
-    etag = models.DateTimeField(auto_now_add=True)
-
-    @classmethod
-    def q_is_admin(cls, user_obj):
-        return Q(assignment_group__parentnode__admins=user_obj) | \
-            Q(assignment_group__parentnode__parentnode__admins=user_obj) | \
-            Q(assignment_group__parentnode__parentnode__parentnode__admins=user_obj) | \
-            Q(assignment_group__parentnode__parentnode__parentnode__parentnode__pk__in=Node._get_nodepks_where_isadmin(user_obj))
+    assignment_group = models.ForeignKey(
+        'AssignmentGroup',
+        related_name='candidates')
+    candidate_id = models.CharField(
+        max_length=30, blank=True, null=True,
+        help_text='An optional candidate id. This can be anything as long as it '
+                  'is less than 30 characters. Used to show the user on anonymous assignmens.')
 
     def __unicode__(self):
-        return 'id={id} identifier={identifier}'.format(id=self.id, identifier=self.identifier)
-
-    def update_identifier(self, anonymous):
-        if anonymous:
-            self.full_name = None
-            self.email = None
-            if not self.candidate_id:
-                self.identifier = "candidate-id missing"
-            else:
-                self.identifier = self.candidate_id
-        else:
-            self.email = self.student.email
-            self.full_name = self.student.devilryuserprofile.full_name
-            self.identifier = self.student.username
-
-    def save(self, *args, **kwargs):
-        anonymous = kwargs.pop('anonymous', self.assignment_group.parentnode.anonymous)
-        self.update_identifier(anonymous)
-        super(Candidate, self).save(*args, **kwargs)
-
-    @property
-    def displayname(self):
-        """
-        Return a name for the candidate, preferrably the full name, but use the
-        ``identifier`` if that is not available.
-        """
-        if self.full_name:
-            return self.full_name
-        else:
-            return self.identifier
-
-
-def sync_candidate_with_user_on_change(sender, **kwargs):
-    """
-    Signal handler which is invoked when a User is saved.
-    """
-    user = kwargs['instance']
-    for candidate in Candidate.objects.filter(student=user):
-        candidate.save()
-
-post_save.connect(sync_candidate_with_user_on_change,
-                  sender=User)
-
-
-def sync_candidate_with_userprofile_on_change(sender, **kwargs):
-    """
-    Signal handler which is invoked when a DevilryUserProfile is saved.
-    """
-    userprofile = kwargs['instance']
-    for candidate in Candidate.objects.filter(student=userprofile.user):
-        candidate.save()
-
-post_save.connect(sync_candidate_with_userprofile_on_change,
-                  sender=DevilryUserProfile)
+        return 'Candiate id={id}, student={student}, group={group}'.format(
+            id=self.id,
+            student=self.student,
+            group=self.assignment_group)
