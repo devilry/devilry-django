a385ca96998e33bb7cc8cefd4973fe3ce0bdf112
Espen A. Kristiansen <post@espenak.net>
2014-10-22 13:21:34 +0200

    devilry_detektor: Fixed tests.
    See #595.

--- src/devilry_detektor/devilry_detektor/tests/test_tasks.py
+++ src/devilry_detektor/devilry_detektor/tests/test_tasks.py
@@ -18,18 +18,6 @@ class TestRunDetektorOnAssignment(TestCase):
             run_detektor_on_assignment.delay(
                 assignment_id=200001).wait()
 
-    def test_already_running(self):
-        processing_started_datetime = datetime.now()
-        DetektorAssignment.objects.create(
-            assignment_id=self.assignmentbuilder.assignment.id,
-            processing_started_by=self.testuser,
-            processing_started_datetime=processing_started_datetime)
-
-        run_detektor_on_assignment.delay(
-            assignment_id=self.assignmentbuilder.assignment.id).wait()
-        detektorassignment = DetektorAssignment.objects.all()[0]
-        self.assertEqual(detektorassignment.processing_started_datetime, processing_started_datetime)
-
     def test_processing_detektorassignment_doesnotexist(self):
         with self.assertRaises(DetektorAssignment.DoesNotExist):
             run_detektor_on_assignment.delay(
