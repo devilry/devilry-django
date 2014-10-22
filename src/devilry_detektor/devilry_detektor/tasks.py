4dd3d6b9ad6d75eed8f8d09f878f05eabb7b01ef
Espen A. Kristiansen <post@espenak.net>
2014-10-22 13:20:46 +0200

    devilry_detektor: Simpified the task logic.
    See #595.

--- src/devilry_detektor/devilry_detektor/tasks.py
+++ src/devilry_detektor/devilry_detektor/tasks.py
@@ -15,14 +15,10 @@ def run_detektor_on_assignment(assignment_id):
     logger.info('run_detektor_on_assignment on assignment: id=%s (%s)',
                 assignment_id, detektorassignment.assignment)
 
-    if detektorassignment.processing_started_datetime is None:
 
-        # TODO: Process all deliveries within the assignment
-        logger.info('run_detektor_on_assignment processing all deliveries on %s requested by %s',
-                    detektorassignment.assignment, detektorassignment.processing_started_by)
+    # TODO: Process all deliveries within the assignment
+    logger.info('run_detektor_on_assignment processing all deliveries on %s requested by %s',
+                detektorassignment.assignment, detektorassignment.processing_started_by)
 
-        detektorassignment.processing_started_datetime = None
-        detektorassignment.save()
-    else:
-        logger.warn('run_detektor_on_assignment for assignment %s aborted because it is already running',
-                    detektorassignment.assignment)
+    detektorassignment.processing_started_datetime = None
+    detektorassignment.save()
