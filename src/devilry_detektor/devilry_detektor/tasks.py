0efaa75a3419d194136fb1eddbfe41ef941aba0e
Espen A. Kristiansen <post@espenak.net>
2014-10-23 14:18:21 +0200

    devilry_detektor: Everything ready to just add the code to run detektor.
    See #595.

--- src/devilry_detektor/devilry_detektor/tasks.py
+++ src/devilry_detektor/devilry_detektor/tasks.py
@@ -1,24 +1,102 @@
+from cStringIO import StringIO
 from celery import task
 from celery.utils.log import get_task_logger
+import os
 
+from devilry.apps.core.models import Delivery
 from devilry_detektor.models import DetektorAssignment
 
 
 logger = get_task_logger(__name__)
 
 
+class RunDetektorOnDelivery(object):
+    SUPPORTED_FILE_TYPES = {
+        'java': {'.java'},
+        'python': {'.py'},
+        # 'c/cpp': {'.c', '.cxx', '.cpp', '.h', '.hxx', '.hpp'},
+        # '.pl',
+        # '.rb',
+        # '.js',
+        # '.c',
+        # '.cpp',
+        # '.cxx'
+    }
+
+    #: We do not send files larger than this to detektor
+    MAX_FILESIZE = (2**20) * 10  # 10 MB
+
+    def __init__(self, delivery):
+        self.delivery = delivery
+
+    def _get_filetype_from_filename(self, filename):
+        name, extension = os.path.splitext(filename)
+        for filetype, extensions in self.SUPPORTED_FILE_TYPES.iteritems():
+            if extension in extensions:
+                return filetype
+        return None
+
+    def _group_filemetas_by_extension(self):
+        filemetasinfo_by_filetype = {}
+        for filemeta in self.delivery.filemetas.order_by('filename'):
+            if filemeta.size > self.MAX_FILESIZE:
+                logger.warning('Skipping filemeta with ID=%s due to size limit.', filemeta.id)
+                continue
+            filetype = self._get_filetype_from_filename(filemeta.filename)
+            if filetype:
+                if not filetype in filemetasinfo_by_filetype:
+                    filemetasinfo_by_filetype[filetype] = {'filemetas': [], 'size': 0}
+                filemetasinfo_by_filetype[filetype]['filemetas'].append(filemeta)
+                filemetasinfo_by_filetype[filetype]['size'] += filemeta.size
+        return filemetasinfo_by_filetype
+
+    def _find_most_prominent_filetype_filemetas(self, filemetasinfo_by_filetype):
+        """
+        Order by total size of files descending order, and return the first.
+        """
+        filemetasinfolist = filemetasinfo_by_filetype.values()
+        filemetasinfolist.sort(lambda a, b: cmp(b['size'], a['size']))
+        return filemetasinfolist[0]['filemetas']
+
+    def _merge_filemetas_into_single_fileobject(self, filemetas):
+        merged_file = StringIO()
+        for filemeta in filemetas:
+            merged_file.write(filemeta.get_all_data_as_string())
+        merged_file.seek(0)
+        return merged_file
+
+    def run_detektor(self):
+        filemetasinfo_by_filetype = self._group_filemetas_by_filetype()
+        if not filemetasinfo_by_filetype:
+            return
+        filemetas = self._find_most_prominent_filetype_filemetas(filemetasinfo_by_filetype)
+        fileobject = self._merge_filemetas_into_single_fileobject(filemetas)
+        # Run detektor
+
+
+class RunDetektorOnAssignment(object):
+    def __init__(self, assignment_id):
+        self.detektorassignment = DetektorAssignment.objects\
+            .select_related('assignment')\
+            .get(assignment_id=assignment_id)
+        logger.info('run_detektor_on_assignment on assignment: id=%s (%s)',
+                    assignment_id, self.detektorassignment.assignment)
+        self._process_deliveries()
+        self.detektorassignment.processing_started_datetime = None
+        self.detektorassignment.save()
+
+    def _process_deliveries(self):
+        unprocessed_deliveries = Delivery.objects\
+            .filter(deadline__assignment_group__parentnode=self.detektorassignment.assignment)\
+            .exclude(delivery_id__in=self.detektorassignment.detektordelivery_set.values_list('delivery_id'))\
+            .prefetch_related('filemetas')
+        for delivery in unprocessed_deliveries:
+            self._process_delivery(delivery)
+
+    def _process_delivery(self, delivery):
+        process_delivery_runner = RunDetektorOnDelivery(delivery)
+        process_delivery_runner.run_detektor()
+
 @task()
 def run_detektor_on_assignment(assignment_id):
-    detektorassignment = DetektorAssignment.objects\
-        .select_related('assignment')\
-        .get(assignment_id=assignment_id)
-    logger.info('run_detektor_on_assignment on assignment: id=%s (%s)',
-                assignment_id, detektorassignment.assignment)
-
-
-    # TODO: Process all deliveries within the assignment
-    logger.info('run_detektor_on_assignment processing all deliveries on %s requested by %s',
-                detektorassignment.assignment, detektorassignment.processing_started_by)
-
-    detektorassignment.processing_started_datetime = None
-    detektorassignment.save()
+    RunDetektorOnAssignment(assignment_id)
--- src/devilry_detektor/devilry_detektor/tests/test_tasks.py
+++ src/devilry_detektor/devilry_detektor/tests/test_tasks.py
@@ -1,8 +1,10 @@
 from datetime import datetime
+from pprint import pprint
 from django.test import TestCase
 
 from devilry_detektor.models import DetektorAssignment
 from devilry_detektor.tasks import run_detektor_on_assignment
+from devilry_detektor.tasks import RunDetektorOnDelivery
 from devilry_developer.testhelpers.corebuilder import UserBuilder
 from devilry_developer.testhelpers.corebuilder import PeriodBuilder
 
@@ -35,3 +37,77 @@ class TestRunDetektorOnAssignment(TestCase):
 
         detektorassignment = DetektorAssignment.objects.all()[0]
         self.assertEqual(detektorassignment.processing_started_datetime, None)
+
+
+class TestRunDetektorOnDelivery(TestCase):
+    def setUp(self):
+        self.testuser = UserBuilder('testuser').user
+        self.deliverybuilder = PeriodBuilder.quickadd_ducku_duck1010_active()\
+            .add_assignment('testassignment')\
+            .add_group(students=[self.testuser])\
+            .add_deadline_in_x_weeks(weeks=1)\
+            .add_delivery()
+
+    def test_get_filetype_from_filename(self):
+        process_delivery_runner = RunDetektorOnDelivery(self.deliverybuilder.delivery)
+        self.assertEquals(process_delivery_runner._get_filetype_from_filename('test.java'), 'java')
+        self.assertEquals(process_delivery_runner._get_filetype_from_filename('test.py'), 'python')
+
+    def test_group_by_extension_no_filemetas(self):
+        filemetas_by_filetype = RunDetektorOnDelivery(self.deliverybuilder.delivery)._group_filemetas_by_extension()
+        self.assertEquals(filemetas_by_filetype, {})
+
+    def test_group_by_extension_no_supported_files(self):
+        self.deliverybuilder.add_filemeta(filename='helloworld.txt', data='Hello world')
+        filemetas_by_filetype = RunDetektorOnDelivery(self.deliverybuilder.delivery)._group_filemetas_by_extension()
+        self.assertEquals(filemetas_by_filetype, {})
+
+    def test_group_by_extension_has_supported_files(self):
+        self.deliverybuilder.add_filemeta(filename='helloworld.txt', data='Hello world')
+        helloworldpy_filemeta = self.deliverybuilder\
+            .add_filemeta(filename='helloworld.py', data='def abc()').filemeta
+        helloworldjava_filemeta = self.deliverybuilder\
+            .add_filemeta(filename='HelloWorld.java', data='// test').filemeta
+        helloworldjava2_filemeta = self.deliverybuilder\
+            .add_filemeta(filename='HelloWorld2.java', data='// test2').filemeta
+
+        filemetas_by_filetype = RunDetektorOnDelivery(self.deliverybuilder.delivery)._group_filemetas_by_extension()
+        self.assertEquals(
+            set(filemetas_by_filetype.keys()),
+            {'java', 'python'})
+        self.assertEquals(
+            filemetas_by_filetype['java']['size'],
+            len('// test') + len('// test2'))
+        self.assertEquals(
+            filemetas_by_filetype['python']['size'],
+            len('def abs()'))
+        self.assertEquals(
+            filemetas_by_filetype['java']['filemetas'],
+            [helloworldjava_filemeta, helloworldjava2_filemeta])
+        self.assertEquals(
+            filemetas_by_filetype['python']['filemetas'],
+            [helloworldpy_filemeta])
+
+    def test_find_most_prominent_filetype_filemetas(self):
+        filemetas = RunDetektorOnDelivery(self.deliverybuilder.delivery)._find_most_prominent_filetype_filemetas({
+            'java': {
+                'size': 20,
+                'filemetas': 'Mocked java filemetas'
+            },
+            'python': {
+                'size': 200,
+                'filemetas': 'Mocked python filemetas'
+            }
+        })
+        self.assertEqual(filemetas, 'Mocked python filemetas')
+
+    def test_merge_filemetas_into_single_fileobject(self):
+        self.deliverybuilder.add_filemeta(filename='helloworld.txt', data='Hello world')
+        filemeta1 = self.deliverybuilder\
+            .add_filemeta(filename='HelloWorld.java', data='test\n').filemeta
+        filemeta2 = self.deliverybuilder\
+            .add_filemeta(filename='HelloWorld2.java', data='test2').filemeta
+
+        merged_file = RunDetektorOnDelivery(self.deliverybuilder.delivery)\
+            ._merge_filemetas_into_single_fileobject([filemeta1, filemeta2])
+        self.assertEquals(merged_file.read(), 'test\ntest2')
