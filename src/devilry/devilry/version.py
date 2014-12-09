e3a456335769f6d76770047eb331e4d7a5837b88
Espen Angell Kristiansen <post@espenak.net>
2014-12-09 13:06:10 +0100

    Released 1.4.14.

--- src/devilry/devilry/version.py
+++ src/devilry/devilry/version.py
@@ -1,1 +1,1 @@
-devilry_version = '1.4.13'
+devilry_version = '1.4.14'
--- src/devilry/setup.py
+++ src/devilry/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "devilry",
-    version = "1.4.13",
+    version = "1.4.14",
     url = 'http://devilry.org',
     license = 'BSD',
     description = "A system for handling electronic deliveries.",
