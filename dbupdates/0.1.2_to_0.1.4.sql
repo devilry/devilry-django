2cac0445eb6b780b1e971d0dc4f948143677929d
Espen A. Kristiansen <post@espenak.net>
2014-12-12 15:36:31 +0100

    Removed unused files and directories in the repo root.

--- dbupdates/0.1.2_to_0.1.4.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE core_deadline ADD COLUMN "is_head" bool NOT NULL DEFAULT(0);
-ALTER TABLE core_delivery ADD COLUMN "deadline_tag_id" integer REFERENCES "core_deadline" ("id");
-ALTER TABLE core_deadline ADD COLUMN "deliveries_available_before_deadline" bool NOT NULL
-CREATE INDEX "core_delivery_3105f29b" ON "core_delivery" ("deadline_tag_id");
-
-
--- dbupdates/0.1.5_to_0.1.6.sql
+++
@@ -1,2 +0,0 @@
-alter table grade_rstschema_rstschemadefinition add column "grade_to_points_mapping" text DEFAULT NULL;
-alter table core_assignment add column "students_can_see_points" boolean NOT NULL DEFAULT true;
