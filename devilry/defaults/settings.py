be4de729e98d8decf397f820701eb563f7522a2e
Espen A. Kristiansen <post@espenak.net>
2014-12-14 03:55:41 +0100

    devilry.defaults: Removed settings.
    No longer in use. Part of #630.

--- devilry/defaults/settings.py
+++
@@ -1,4 +0,0 @@
-from devilry_settings import *
-import warnings
-warnings.warn('Importing default Devilry settings from ``devilry.defaults.settings`` is deprecated. Use ``devilry_settings.default_settings`` instead.', stacklevel=2)
-from devilry_settings.default_settings import *
