4044e9a7cef391f837eeb8abbe888c052ae2e534
Espen A. Kristiansen <post@espenak.net>
2014-12-14 19:06:23 +0100

    Moved devilry.devilry_settings.default_urls to devilry.project.common.default_urls.
    See #629.

--- devilry/devilry_settings/default_root_urlconf.py
+++ devilry/devilry_settings/default_root_urlconf.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from devilry.apps.core import pluginloader
-from devilry.devilry_settings.default_urls import devilry_urls
+from devilry.project.common.default_urls import devilry_urls
 
 
 urlpatterns = patterns('',
