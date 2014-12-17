cf8f606221b739e55912b33bc85b4a9f4bf30945
Espen A. Kristiansen <post@espenak.net>
2014-12-17 19:49:00 +0100

    apps.statistics: Deleted. Not in use.
    See #630.

--- devilry/apps/statistics/static/extjs_classes/statistics/OverviewOfSingleStudentRecord.js
+++
@@ -1,5 +0,0 @@
-Ext.define('devilry.statistics.OverviewOfSingleStudentRecord', {
-    extend: 'Ext.data.Model',
-    idProperty: 'id',
-    fields: ['id', 'assignment__long_name', 'assignmentid', 'assignment__short_name', 'is_open', 'feedback__points', 'feedback']
-});
--- devilry/apps/statistics/static/extjs_classes/statistics/activeperiods/AggregatedPeriodModel.js
+++
@@ -1,5 +0,0 @@
-Ext.define('devilry.statistics.activeperiods.AggregatedPeriodModel', {
-    extend: 'Ext.data.Model',
-    fields: ['period_id', 'subject_long_name', 'period_long_name', 'qualifies_for_exam_ready_for_export'],
-    idProperty: 'period_id'
-});
