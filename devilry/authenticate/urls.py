997d6a0ee354598a9d5358745402e62117cb8446
Espen A. Kristiansen <post@espenak.net>
2014-12-18 16:45:55 +0100

    Moved devilry.apps.authenticate to devilry.authenticate.
    See #629.

--- devilry/authenticate/urls.py
+++ devilry/authenticate/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, url
 
-urlpatterns = patterns('devilry.apps.authenticate',
+urlpatterns = patterns('devilry.authenticate',
                        url(r'^logout$', 'views.logout', name='logout'),
                        url(r'^login$', 'views.login', name='login'))
