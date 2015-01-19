895a162f67532c32170b809c7112d515baed70a5
Espen A. Kristiansen <post@espenak.net>
2015-01-19 17:34:07 +0100

    subjectadmin.tests.test_dashboard: Rewrote all tests.
    See #651.

--- devilry/devilry_subjectadmin/tests/test_dashboard.py
+++ devilry/devilry_subjectadmin/tests/test_dashboard.py
@@ -1,36 +1,39 @@
-from devilry.apps.core.testhelper import TestHelper
-
 from devilry.devilry_subjectadmin.tests.base import SubjectAdminSeleniumTestCase
+from devilry.project.develop.testhelpers.corebuilder import UserBuilder, PeriodBuilder, SubjectBuilder, NodeBuilder
 
 
 class TestDashboard(SubjectAdminSeleniumTestCase):
     def setUp(self):
-        self.testhelper = TestHelper()
-        self.testhelper.create_superuser('grandma')
-        self.login('grandma')
+        self.testuser = UserBuilder('testuser').user
 
-        self.testhelper.add(nodes='uni',
-                            subjects=['duck1100', 'duck1010:ln(DUCK 1010 - Programming)'],
-                            periods=['2012h:begins(-1)'],
-                            assignments=['week1:ln(Week One)', 'week2', 'week3', 'week4'])
-        self.testhelper.add(nodes='uni',
-                            subjects=['duck1010'],
-                            periods=['oldperiod:begins(-100)'],
-                            assignments=['oldassignment'])
+    def test_dashboard_list_active(self):
+        NodeBuilder('ducku')\
+            .add_subject(short_name='duck1010', long_name='DUCK 1010 Programming')\
+            .add_admins(self.testuser)\
+            .add_6month_active_period()
 
-    def test_dashboard(self):
-        self.browseTo('')
-        self.waitForCssSelector('.shortcutlist')
-        self.assertTrue('Active subjects' in self.selenium.page_source)
-        self.assertFalse('oldperiod' in self.selenium.page_source)
-        self.assertFalse('oldassignment' in self.selenium.page_source)
-        self.assertEquals(self.selenium.page_source.count('Week One'), 2)
-        self.assertEquals(self.selenium.page_source.count('Add assignment'), 2)
-        self.assertTrue('duck1010' in self.selenium.page_source)
-        self.assertEquals(len(self.selenium.find_elements_by_link_text('Add assignment')), 2)
-        self.assertEquals(len(self.selenium.find_elements_by_link_text('Week One')), 2)
+        self.loginTo('testuser', path='')
+        self.waitForCssSelector('.devilry_subjectadmin_allactivewhereisadminlist')
+        self.assertIn('Active subjects', self.selenium.page_source)
+        self.assertIn('DUCK 1010 Programming', self.selenium.page_source)
 
-        browse_all_buttons = self.selenium.find_elements_by_link_text('Browse all your subjects (including old/archived)')
-        self.assertEquals(len(browse_all_buttons), 1)
-        self.assertEquals(browse_all_buttons[0].get_attribute('href'),
+        browse_all_button = self.selenium.find_element_by_link_text(
+            'Browse everything where you are administrator')
+        self.assertEquals(browse_all_button.get_attribute('href'),
+                          self.get_absolute_url('/'))
+
+    def test_dashboard_list_exclude_inactive(self):
+        NodeBuilder('ducku')\
+            .add_subject(short_name='duck1010', long_name='DUCK 1010 Programming')\
+            .add_admins(self.testuser)\
+            .add_6month_lastyear_period()
+
+        self.loginTo('testuser', path='')
+        self.waitForCssSelector('.devilry_subjectadmin_allactivewhereisadminlist')
+        self.assertIn('Active subjects', self.selenium.page_source)
+        self.assertNotIn('DUCK 1010 Programming', self.selenium.page_source)
+
+        browse_all_button = self.selenium.find_element_by_link_text(
+            'Browse everything where you are administrator')
+        self.assertEquals(browse_all_button.get_attribute('href'),
                           self.get_absolute_url('/'))
