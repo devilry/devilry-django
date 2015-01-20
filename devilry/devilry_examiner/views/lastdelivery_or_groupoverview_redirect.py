8d41a9bee6c8576542daa96af6ec490394dfc850
Espen A. Kristiansen <post@espenak.net>
2015-01-20 23:16:28 +0100

    examiner...lastdelivery_or_groupoverview_redirect: Handle removal of last_delivery.
    See #648.

--- devilry/devilry_examiner/views/lastdelivery_or_groupoverview_redirect.py
+++ devilry/devilry_examiner/views/lastdelivery_or_groupoverview_redirect.py
@@ -9,9 +9,10 @@ from devilry.apps.core.models import AssignmentGroup
 
 class LastDeliveryOrGroupOverviewRedirectView(View):
     def get(self, request, groupid):
-        group = get_object_or_404(AssignmentGroup, id=groupid)
-        if group.last_delivery:
-            url = reverse('devilry_examiner_singledeliveryview', args=[group.last_delivery.id])
+        queryset = AssignmentGroup.objects.annotate_with_last_delivery_id()
+        group = get_object_or_404(queryset, id=groupid)
+        if group.last_delivery_id is not None:
+            url = reverse('devilry_examiner_singledeliveryview', args=[group.last_delivery_id])
             edit_feedback = self.request.GET.get('edit_feedback', False) == 'true'
             if edit_feedback:
                 url = '{}?edit_feedback=true'.format(url)
