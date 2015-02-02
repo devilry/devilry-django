076ecd646424f55e0e5f55f9046535fac671d096
Espen Angell Kristiansen <post@espenak.net>
2015-02-03 00:54:06 +0100

    student...recentdeliveriesapp: Moved the LastDeadlineColumn into the app.
    It is the only place it is used.

--- devilry/devilry_student/cradmin_student/waitingfordeliveriesapp.py
+++ devilry/devilry_student/cradmin_student/waitingfordeliveriesapp.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from django.template.loader import render_to_string
 from django_cradmin.viewhelpers import objecttable
 from django_cradmin import crapp
 from django_cradmin import crinstance
@@ -5,7 +7,6 @@ from django.utils.translation import ugettext_lazy as _
 
 from devilry.apps.core.models import AssignmentGroup
 from devilry.devilry_student.cradminextensions import studentobjecttable
-from devilry.devilry_student.cradminextensions.columntypes import LastDeadlineColumn
 
 
 class AssignmentInfoColumn(objecttable.SingleActionColumn):
@@ -69,6 +70,28 @@ class PeriodInfoXs(objecttable.PlainTextColumn):
 
     def is_sortable(self):
         return False
+
+
+class LastDeadlineColumn(objecttable.PlainTextColumn):
+    orderingfield = 'last_deadline_datetime'
+    modelfield = 'last_deadline_datetime'
+
+    def get_header(self):
+        return _('Deadline')
+
+    def render_value(self, obj):
+        deadline_datetime = super(LastDeadlineColumn, self).render_value(obj)
+        if deadline_datetime:
+            return render_to_string(
+                'devilry_student/cradmin_student/waitingfordeliveriesapp/last-deadline.django.html', {
+                'deadline_datetime': deadline_datetime,
+                'in_the_future': deadline_datetime > datetime.now()
+            })
+        else:
+            return deadline_datetime
+
+    def get_default_order_is_ascending(self):
+        return True
 
 
 class WaitingForDeliveriesListView(studentobjecttable.StudentObjectTableView):
--- devilry/devilry_student/cradminextensions/columntypes.py
+++ devilry/devilry_student/cradminextensions/columntypes.py
@@ -1,26 +1,6 @@
-from datetime import datetime
-from django.template.loader import render_to_string
 from django_cradmin import crinstance
 from django_cradmin.viewhelpers import objecttable
 from django.utils.translation import ugettext_lazy as _
-
-
-class LastDeadlineColumn(objecttable.PlainTextColumn):
-    orderingfield = 'last_deadline_datetime'
-    modelfield = 'last_deadline_datetime'
-
-    def get_header(self):
-        return _('Deadline')
-
-    def render_value(self, obj):
-        deadline_datetime = super(LastDeadlineColumn, self).render_value(obj)
-        if deadline_datetime:
-            return render_to_string('devilry_student/cradminextensions/columntypes/last-deadline.django.html', {
-                'deadline_datetime': deadline_datetime,
-                'in_the_future': deadline_datetime > datetime.now()
-            })
-        else:
-            return deadline_datetime
 
 
 class DeliverySummaryColumn(objecttable.SingleActionColumn):
