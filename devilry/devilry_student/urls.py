fa3bdd13402621814fe15ee565952cef72ecceb3
Espen A. Kristiansen <post@espenak.net>
2015-02-03 08:55:36 +0100

    student: Removed lots of old code.

--- devilry/devilry_student/urls.py
+++ devilry/devilry_student/urls.py
@@ -14,9 +14,6 @@ from .views.frontpage import FrontpageView
 from .views.projectgroup_overview import ProjectGroupOverviewView
 from .views.groupinvite_respond import GroupInviteRespondView
 from .views.groupinvite_delete import GroupInviteDeleteView
-from .views.browseview import BrowseView
-from .views.groupdetails import GroupDetailsView
-from .views.semesteroverview import SemesterOverview
 from .views.download_deliveryfiles import CompressedFileDownloadView
 from .views.download_deliveryfiles import FileDownloadView
 
@@ -47,9 +44,8 @@ urlpatterns = patterns(
     url('^rest/', include('devilry.devilry_student.rest.urls')),
     url('^emptytestview', emptyview), # NOTE: Only used for testing
     url('^i18n.js$', javascript_catalog, kwargs={'packages': i18n_packages},
-       name='devilry_student_i18n'),
-    url(r'^show_delivery/(?P<delivery_id>\d+)$', 'views.show_delivery.show_delivery',
-       name='devilry_student_show_delivery'),
+        name='devilry_student_i18n'),
+
     url(r'^groupinvite/overview/(?P<group_id>\d+)$',
         login_required(ProjectGroupOverviewView.as_view()),
         name='devilry_student_projectgroup_overview'),
@@ -59,17 +55,8 @@ urlpatterns = patterns(
     url(r'^groupinvite/remove/(?P<invite_id>\d+)$',
         login_required(GroupInviteDeleteView.as_view()),
         name='devilry_student_groupinvite_delete'),
-    url(r'^browse/$',
-        login_required(BrowseView.as_view()),
-        name='devilry_student_browse'),
-    url(r'^browse/period/(?P<pk>\d+)$',
-        login_required(SemesterOverview.as_view()),
-        name='devilry_student_browseperiod'),
-    url(r'^groupdetails/(?P<id>\d+)$',
-        login_required(GroupDetailsView.as_view()),
-        name='devilry_student_groupdetails'),
+    #url(r'^groupinvite/leave/(?P<group_id>\d+)$')
 
-    # TODO: Rename the views
     url(r'^show-delivery/filedownload/(?P<filemetaid>\d+)$',
         login_required(FileDownloadView.as_view()),
         name='devilry-delivery-file-download'),
@@ -77,7 +64,9 @@ urlpatterns = patterns(
         login_required(CompressedFileDownloadView.as_view()),
         name='devilry-delivery-download-all-zip'),
 
-    #url(r'^groupinvite/leave/(?P<group_id>\d+)$')
+    url(r'^show_delivery/(?P<delivery_id>\d+)$', 'views.show_delivery.show_delivery',
+        name='devilry_student_show_delivery'),
+
 
     url(r'^period/', include(cradmin_period.CrAdminInstance.urls())),
     url(r'^group/', include(cradmin_group.CrAdminInstance.urls())),
