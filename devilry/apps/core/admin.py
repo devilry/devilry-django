7c701a8a2f3313137f6253272d663acf46f2cbc9
Espen Angell Kristiansen <post@espenak.net>
2015-02-06 09:04:53 +0100

    core: Added an admin UI for nodes.
    Fixes #645.

--- devilry/apps/core/admin.py
+++ devilry/apps/core/admin.py
@@ -1,5 +1,53 @@
 from django.contrib import admin
-from devilry.apps.core.models import AssignmentGroup
+from devilry.apps.core.models import AssignmentGroup, Node
+from django.utils.translation import ugettext_lazy as _
+
+
+class BaseNodeAdmin(admin.ModelAdmin):
+    filter_horizontal = ['admins']
+    raw_id_fields = [
+        'parentnode'
+    ]
+    list_display = (
+        'id',
+        'short_name',
+        'long_name',
+        'admins_as_string'
+    )
+    search_fields = [
+        'id',
+        'short_name',
+        'long_name',
+        'admins__username',
+        'admins__email',
+        'admins__devilryuserprofile__full_name',
+    ]
+
+    def admins_as_string(self, obj):
+        return u', '.join([user.username for user in obj.admins.all()])
+    admins_as_string.short_description = _("Admins")
+
+    def admins_as_string(self, obj):
+        return obj.get_path()
+    admins_as_string.short_description = _("Path")
+
+    def get_queryset(self, request):
+        return super(BaseNodeAdmin, self).get_queryset(request)\
+            .prefetch_related(
+                'admins',
+                'admins__devilryuserprofile')
+
+
+class NodeAdmin(BaseNodeAdmin):
+    def get_queryset(self, request):
+        return super(NodeAdmin, self).get_queryset(request)\
+            .select_related(
+                'parentnode',
+                'parentnode__parentnode',
+                'parentnode__parentnode__parentnode')\
+
+
+admin.site.register(Node, NodeAdmin)
 
 
 class AssignmentGroupAdmin(admin.ModelAdmin):
