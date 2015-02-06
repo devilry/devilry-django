ed9a8b3a7e0a27dccb257309c255be2d07798b57
Espen Angell Kristiansen <post@espenak.net>
2015-02-06 07:43:42 +0100

    devilry_header: Update aboutme tests to include language selector tests.

--- devilry/devilry_header/tests/test_aboutme.py
+++ devilry/devilry_header/tests/test_aboutme.py
@@ -5,11 +5,12 @@ from devilry.project.develop.testhelpers.corebuilder import UserBuilder
 # from develop.testhelpers.soupselect import cssFind
 # from develop.testhelpers.soupselect import cssGet
 # from develop.testhelpers.soupselect import cssExists
+from devilry.project.develop.testhelpers.soupselect import cssExists, cssGet
 
 
 class TestAboutMeView(TestCase):
     def setUp(self):
-        self.testuser = UserBuilder('testuser').user
+        self.testuserbuilder = UserBuilder('testuserbuilder')
         self.url = reverse('devilry_header_aboutme')
 
     def _getas(self, user, *args, **kwargs):
@@ -21,6 +22,29 @@ class TestAboutMeView(TestCase):
         self.assertEquals(response.status_code, 302)
 
     def test_render_noinput(self):
-        response = self._getas(self.testuser)
+        response = self._getas(self.testuserbuilder.user)
         self.assertEquals(response.status_code, 200)
         # html = response.content
+
+    def test_languageselect(self):
+        self.testuserbuilder.update_profile(
+            languagecode='en'
+        )
+        with self.settings(LANGUAGES=[('en', 'English'), ('nb', 'Norwegian')]):
+            html = self._getas(self.testuserbuilder.user).content
+            self.assertTrue(cssExists(html,
+                '#devilry_frontpage_languageselect #devilry_change_language_form'))
+            self.assertEquals(
+                cssGet(html, '#devilry_change_language_form option[value="en"]')['selected'],
+                'selected')
+
+    def test_languageselect_no_current_language(self):
+        with self.settings(
+                LANGUAGES=[('en', 'English'), ('nb', 'Norwegian')],
+                LANGUAGE_CODE='nb'):
+            html = self._getas(self.testuserbuilder.user).content
+            self.assertTrue(cssExists(html,
+                '#devilry_frontpage_languageselect #devilry_change_language_form'))
+            self.assertEquals(
+                cssGet(html, '#devilry_change_language_form option[value="nb"]')['selected'],
+                'selected')
