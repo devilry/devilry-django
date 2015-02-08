063d1965bc23c3a954ce0c9937cd3107390d818a
Espen Angell Kristiansen <post@espenak.net>
2015-02-08 20:09:16 +0100

    qualifiesforexam: Removed all terms from translations.

--- devilry/devilry_qualifiesforexam/static/devilry_qualifiesforexam/app/view/summaryview/SummaryView.js
+++ devilry/devilry_qualifiesforexam/static/devilry_qualifiesforexam/app/view/summaryview/SummaryView.js
@@ -27,19 +27,19 @@ Ext.define('devilry_qualifiesforexam.view.summaryview.SummaryView' ,{
                     xtype: 'box',
                     tpl: [
                         '<h1>', gettext('Qualified for final exams summary'), '</h1>',
-                        '<p>', gettext('Each {subject_term} in the list below has one of the following statuses:'), '</p>',
+                        '<p>', gettext('Each course in the list below has one of the following statuses:'), '</p>',
                         '<dl>',
                             '<dt><span class="text-success">', gettext('Ready for export'), '</span></dt>',
                             '<dd>', gettext('The list of qualified students are ready for export. Use the view- or print-button to browse the list of qualified students.'), '</dd>',
                         '<dl>',
                         '<dl>',
                             '<dt><span class="muted">', gettext('None'), '</span></dt>',
-                            '<dd>', gettext('The {subject} administrator has not selected students that qualify for final exams yet.'), '</dd>',
+                            '<dd>', gettext('The {subject} manager has not selected students that qualify for final exams yet.'), '</dd>',
                         '<dl>',
                         '<dl>',
                             '<dt><span class="text-warning">', gettext('Not ready for export (retracted)'), '</span></dt>',
                             '<dd>',
-                                gettext('The subject administrator has retracted a previously "Ready for export" status. This may be bad if you have already exported the status.'),
+                                gettext('The course manager has retracted a previously "Ready for export" status. This may be bad if you have already exported the status.'),
                             '</dd>',
                         '<dl>',
 
@@ -50,9 +50,7 @@ Ext.define('devilry_qualifiesforexam.view.summaryview.SummaryView' ,{
                             gettext('Devilry does NOT notify you when a status is changed. This could lead to problems when someone retracts or updates an exported status. Please contact the Devilry project if you want to help us create a good workflow for handling status changes.'),
                         '</p>'
                     ],
-                    data: {
-                        subject_term: gettext('subject')
-                    }
+                    data: {}
                 }]
             }, {
                 xtype: 'summaryviewgrid',
