dadbe7f3fe69ea853e2e92181e942d22270f0ab9
Espen A. Kristiansen <post@espenak.net>
2015-02-09 09:58:18 +0100

    Set Whoosh as default development search backend.

--- devilry/project/develop/settings/develop.py
+++ devilry/project/develop/settings/develop.py
@@ -33,20 +33,20 @@ if profiler_middleware:
 ##################################################################################
 # Haystack (search)
 ##################################################################################
-# HAYSTACK_CONNECTIONS = {  # Whoosh
-#     'default': {
-#         'ENGINE': 'haystack.backends.whoosh_backend.WhooshEngine',
-#         'PATH': join(developfilesdir, 'devilry_whoosh_index'),
-#     },
-# }
-
-HAYSTACK_CONNECTIONS = {  # Elastisearch
+HAYSTACK_CONNECTIONS = {  # Whoosh
     'default': {
-        'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
-        'URL': 'http://127.0.0.1:9200/',
-        'INDEX_NAME': 'haystack',
+        'ENGINE': 'haystack.backends.whoosh_backend.WhooshEngine',
+        'PATH': join(developfilesdir, 'devilry_whoosh_index'),
     },
 }
+
+# HAYSTACK_CONNECTIONS = {  # Elastisearch
+#     'default': {
+#         'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
+#         'URL': 'http://127.0.0.1:9200/',
+#         'INDEX_NAME': 'haystack',
+#     },
+# }
 
 
 
