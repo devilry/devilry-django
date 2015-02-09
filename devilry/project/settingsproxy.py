c5d794265058e7f5ffa8f0e777f13df1e5f943bf
Espen A. Kristiansen <post@espenak.net>
2015-02-09 07:19:56 +0100

    project: Fixed settingsproxy after an error when adding postres DJANGOENVs.
    See #691.

--- devilry/project/settingsproxy.py
+++ devilry/project/settingsproxy.py
@@ -16,7 +16,7 @@ DJANGOENV = os.environ.get('DJANGOENV', 'develop')
 
 if DJANGOENV == 'develop':  # Used for local development
     from devilry.project.develop.settings.develop import *
-if DJANGOENV == 'postgres_develop':  # Used for local development with Postgres
+elif DJANGOENV == 'postgres_develop':  # Used for local development with Postgres
     from devilry.project.develop.settings.postgres_develop import *
 elif DJANGOENV == 'test':  # Used when running the Django tests
     from devilry.project.develop.settings.test import *
@@ -27,4 +27,4 @@ elif DJANGOENV == 'extjsbuild':  # Used when building extjs apps
 elif DJANGOENV == 'noextjsdebug':  # Used to develop/try the UI with built ExtJS apps
     from devilry.project.develop.settings.noextjsdebug import *
 else:
-    raise ValueError('Invalid value for the DJANGOENV environment variable: {}'.format(DJANGOENV))
+    raise ValueError('Invalid value for the DJANGOENV environment variable: {!r}'.format(DJANGOENV))
