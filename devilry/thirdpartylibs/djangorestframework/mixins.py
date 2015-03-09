58aa48eae83f971b3c6bd77683932004ded7736f
Espen A. Kristiansen <post@espenak.net>
2015-03-09 04:41:18 +0100

    thirdpartylibs.djangorestframework: Refactor mimetype to content_type.
    Mimetype is no longer a supported argument of HttpResponse.
    
    See #730.

--- devilry/thirdpartylibs/djangorestframework/mixins.py
+++ devilry/thirdpartylibs/djangorestframework/mixins.py
@@ -254,7 +254,7 @@ class ResponseMixin(object):
             content = renderer.render()
 
         # Build the HTTP Response
-        resp = HttpResponse(content, mimetype=response.media_type, status=response.status)
+        resp = HttpResponse(content, content_type=response.media_type, status=response.status)
         for (key, val) in response.headers.items():
             resp[key] = val
 
