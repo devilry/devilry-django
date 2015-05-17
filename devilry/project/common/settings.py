9dbb0137a39176acfa1c517d6bcfcee050813d0b
Vegard Angell <v.angell123@gmail.com>
2015-05-17 05:27:29 +0200

    Managementscript for migrating all candidates from one user to another.
    closes #748

--- devilry/project/common/settings.py
+++ devilry/project/common/settings.py
@@ -87,7 +87,7 @@ INSTALLED_APPS = [
     'devilry.devilry_gradingsystemplugin_approved',
     'devilry.devilry_rest',
     'devilry.devilry_detektor',
-
+    'devilry.project.common'
 ]
 
 TEMPLATE_CONTEXT_PROCESSORS = ("django.contrib.auth.context_processors.auth",
