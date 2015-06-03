f2accf83b4057d58b7bc1abf369fc4619cd66a22
Tor Ivar Johansen <tor@torjohansen.no>
2015-06-03 16:42:10 +0200

    Fixed bug in username regex in devilry_send_email_to_students url

--- devilry/devilry_send_email_to_students/urls.py
+++ devilry/devilry_send_email_to_students/urls.py
@@ -5,7 +5,7 @@ from devilry.devilry_send_email_to_students.views import EmailSendingDebug
 
 
 urlpatterns = patterns('devilry.devilry_send_email_to_students',
-                       url(r'^email_sending_debug/(?P<username>\w+)$',
+                       url(r'^email_sending_debug/(?P<username>.*\w+.*)$',
                            login_required(EmailSendingDebug.as_view()),
                            name='send_email_to_students_email_sending_debug')
                       )
