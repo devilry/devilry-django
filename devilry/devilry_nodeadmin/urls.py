de063c373a537f5ebb5c340ae1dd84b50d7b2b3b
Espen A. Kristiansen <post@espenak.net>
2015-07-01 00:27:10 +0200

    nodeadmin.urls: Fixed.

--- devilry/devilry_nodeadmin/urls.py
+++ devilry/devilry_nodeadmin/urls.py
@@ -8,10 +8,6 @@ from devilry.devilry_nodeadmin.views import AppView, RedirectToNodeAdminAppView
 
 from devilry.project.common.i18n import get_javascript_catalog_packages
 from devilry.devilry_nodeadmin import cradmin
-from devilry.devilry_nodeadmin.crapps import listpermissionnodes
-from django.shortcuts import redirect
-from django_cradmin import crinstance
-from devilry.apps.core.models import node
 
 i18n_packages = get_javascript_catalog_packages(
     'devilry_nodeadmin',
@@ -20,25 +16,18 @@ i18n_packages = get_javascript_catalog_packages(
     'devilry.apps.core'
 )
 
-# @login_required
-# def redirect_to_nodeadmin_frontpage_view(request):
-#     return redirect(crinstance.reverse_cradmin_url(
-#         instanceid='devilry_nodeadmin',
-#         appname='listnodes_index',
-#         roleid=1))
-
 urlpatterns = patterns(
     'devilry.devilry_nodeadmin',
-    # url('^$', redirect_to_nodeadmin_frontpage_view, name='devilry_nodeadmin'),
-    url(r'^', include(cradmin.NodeListingCrAdminInstance.urls())),
-    # url('^$',
-    #     login_required(csrf_protect(ensure_csrf_cookie(AppView.as_view()))),
-    #         name='devilry_nodeadmin'),
-    # url('^i18n.js$', javascript_catalog,
-    #     kwargs={'packages': i18n_packages},
-    #     name='devilry_nodeadmin_i18n'),
-    # url('^rest/', include('devilry.devilry_nodeadmin.rest.urls')),
-    # url('^node/(?P<id>\d+)',
-    #     login_required(RedirectToNodeAdminAppView.as_view(pathformat='/node/{id}')),
-    #     name='devilry_nodeadmin_node'),
+
+    url('^$',
+        login_required(csrf_protect(ensure_csrf_cookie(AppView.as_view()))),
+        name='devilry_nodeadmin'),
+    url('^i18n.js$', javascript_catalog,
+        kwargs={'packages': i18n_packages},
+        name='devilry_nodeadmin_i18n'),
+    url('^rest/', include('devilry.devilry_nodeadmin.rest.urls')),
+    url('^node/(?P<id>\d+)',
+        login_required(RedirectToNodeAdminAppView.as_view(pathformat='/node/{id}')),
+        name='devilry_nodeadmin_node'),
+    url(r'^new/', include(cradmin.NodeListingCrAdminInstance.urls())),
 )
