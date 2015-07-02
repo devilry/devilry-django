a549cb0308b47421dc231078e6b94fd8d99da857
StianJul <stianjul@gmail.com>
2015-07-02 18:01:42 +0200

    Added Assignment-DocType and -RegistryItem

--- devilry/devilry_elasticsearch_cache/elasticsearch_doctypes.py
+++ devilry/devilry_elasticsearch_cache/elasticsearch_doctypes.py
@@ -1,42 +1,87 @@
 from elasticsearch_dsl import DocType, String
 
+from django.contrib.auth import models
+
 from devilry.apps.core import models as coremodels
 from devilry.devilry_elasticsearch_cache import elasticsearch_registry
 
 
 class AbstractBaseNodeRegistryItem(elasticsearch_registry.RegistryItem):
     """
-    TODO: Document
+    TODO: document
     """
 
-    #: TODO - document
+    #: The model it should reflect. E.g: :class:`core.models.node.Node`
     modelclass = None
 
-    #: TODO - document
+    #: The DocType(elasticsearch _doc_type) class it should reflect. Ex: :class:`.Node`
     doctype_class = None
 
-    def get_search_text(self, node):
+    def get_search_text(self, modelobject):
         """
-        TODO: document
+        Get searchable text consisting of short_name and long_name from this object
+        and a unspecified number of parents.
 
         Examples:
 
-            Typically something like this::
+            Typically something like this for a Period left to right where period is Spring 2001::
 
-                dsajkdasj lkdjas dsakjlk oda
+                "Spring 2001 spring2001 DUCK1010 - Objectoriented programming duck1010 Ifi ifi"
+
+        This must be implemented in all subclasses.
         """
         raise NotImplementedError()
 
+    def get_tags(self, modelobject):
+        """
+        Get list of keywords/tags consisting of short_name and long_name from this object
+        and a unspecified number of parents.
+
+        Examples:
+
+            Typically something like this for a Period left to right where period is Spring 2001::
+
+                ['Spring 2001', 'spring2001', 'DUCK1010 - Objectoriented programming', 'duck1010', 'Ifi', 'ifi']
+
+        This must be implemented in all subclasses.
+        """
+        raise NotImplementedError()
+
+    def __get_parentnode_id(self, modelobject):
+        """
+        Returns parentnode id if modelobject has parent,
+        else -1 is returned (topnode)
+        """
+        parent = modelobject.parentnode
+        if parent is not None:
+            return parent.id
+        return -1
+
     def get_doctype_object_kwargs(self, modelobject):
         """
-        TODO: document
+        Get the description fields for a DocType object where data for each field
+        is aquired from the modelobject argument, where modelobject is e.g: :class:`core.models.node.Node`
+
+        Example:
+
+            Something like this::
+
+                return {
+                    '_id': modelobject.id,
+                    'short_name': modelobject.short_name,
+                    'long_name': modelobject.long_name,
+                    'path': modelobject.get_path(),
+                    'search_text': self.get_search_text(modelobject),
+                }
         """
         return {
             '_id': modelobject.id,
+            'parentnode_id': self.__get_parentnode_id(modelobject),
             'short_name': modelobject.short_name,
             'long_name': modelobject.long_name,
             'path': modelobject.get_path(),
-            'search_text': self.get_search_text(modelobject)
+            'search_text': self.get_search_text(modelobject),
+            'tags': self.get_tags(modelobject)
         }
 
     def to_doctype_object(self, modelobject):
@@ -48,7 +93,7 @@ class AbstractBaseNodeRegistryItem(elasticsearch_registry.RegistryItem):
 
 class Node(DocType):
     """
-    TODO: Document
+    Class to represent a Node from :class:`devilry.apps.core.models.node.Node`
     """
     # short_name = String(index='not_analyzed')
     # long_name = String(index='not_analyzed')
@@ -59,7 +104,9 @@ class Node(DocType):
 
 class NodeRegistryItem(AbstractBaseNodeRegistryItem):
     """
-    TODO: Document
+    A RegistryItem class of :class:`core.models.node.Node` that defines the properties of a :class:`.Node` as DocType.
+
+    Is added to the :file:`.elasticsearch_registry.registry` singleton.
     """
     modelclass = coremodels.Node
     doctype_class = Node
@@ -74,6 +121,11 @@ class NodeRegistryItem(AbstractBaseNodeRegistryItem):
                                           self.get_search_text(node.parentnode))
         return search_text
 
+    def get_tags(self, modelobject):
+        node = modelobject
+        tags = ['tag test']
+        return tags
+
     def get_all_modelobjects(self):
         return coremodels.Node.objects.select_related('parentnode').all()
 
@@ -82,7 +134,7 @@ elasticsearch_registry.registry.add(NodeRegistryItem())
 
 class Subject(DocType):
     """
-    TODO: Document
+    Class to represent a Subject from :class:`devilry.apps.core.models.subject.Subject`
     """
     class Meta:
         index = 'devilry'
@@ -90,7 +142,9 @@ class Subject(DocType):
 
 class SubjectRegistryItem(AbstractBaseNodeRegistryItem):
     """
-    TODO: Document
+    A RegistryItem class of :class:`core.models.subject.Subject` that defines the properties of a :class:`.Subject` as DocType.
+
+    Is added to the :file:`.elasticsearch_registry.registry` singleton.
     """
     modelclass = coremodels.Subject
     doctype_class = Subject
@@ -106,6 +160,11 @@ class SubjectRegistryItem(AbstractBaseNodeRegistryItem):
                 parentnode_short_name=subject.parentnode.short_name)
         return search_text
 
+    def get_tags(self, modelobject):
+        subject = modelobject
+        tags = ['tag test']
+        return tags
+
     def get_all_modelobjects(self):
         return coremodels.Subject.objects.select_related('parentnode').all()
 
@@ -114,7 +173,7 @@ elasticsearch_registry.registry.add(SubjectRegistryItem())
 
 class Period(DocType):
     """
-    TODO: Document
+    Class to represent a Period from :class:`devilry.apps.core.models.period.Period`
     """
     class Meta:
         index = 'devilry'
@@ -122,7 +181,9 @@ class Period(DocType):
 
 class PeriodRegistryItem(AbstractBaseNodeRegistryItem):
     """
-    TODO: Document
+    A RegistryItem class of :class:`core.models.period.Period` that defines the properties of a :class:`.Period` as DocType.
+
+    Is added to the :file:`.elasticsearch_registry.registry` singleton.
     """
     modelclass = coremodels.Period
     doctype_class = Period
@@ -141,6 +202,11 @@ class PeriodRegistryItem(AbstractBaseNodeRegistryItem):
                 parentnode_short_name=period.subject.parentnode.short_name)
         return search_text
 
+    def get_tags(self, modelobject):
+        period = modelobject
+        tags = ['tag test']
+        return tags
+
     def get_doctype_object_kwargs(self, modelobject):
         kwargs = super(PeriodRegistryItem, self).get_doctype_object_kwargs(modelobject=modelobject)
         period = modelobject
@@ -157,3 +223,82 @@ class PeriodRegistryItem(AbstractBaseNodeRegistryItem):
         ).all()
 
 elasticsearch_registry.registry.add(PeriodRegistryItem())
+
+
+class Assignment(DocType):
+    class Meta:
+        index = 'devilry'
+
+
+class AssignmentRegistryItem(AbstractBaseNodeRegistryItem):
+    """
+    A RegistryItem class of :class:`coremodels.assignment.Assignment` that defines the properties of a :class:`.Assignment` as DocType.
+
+    Is added to the :file:`.elasticsearch_registry.registry` singleton.
+    """
+    modelclass = coremodels.Assignment
+    doctype_class = Assignment
+
+    def get_search_text(self, modelobject):
+        assignment = modelobject
+        search_text = \
+            u'{long_name} {short_name} ' \
+            u'{subject_long_name} {subject_short_name} ' \
+            u'{parentnode_long_name} {parentnode_short_name} ' \
+            u'{parentnode_parentnode_long_name} {parentnode_parentnode_short_name}'.format(
+                long_name=assignment.long_name,
+                short_name=assignment.short_name,
+                subject_long_name=assignment.period.long_name,
+                subject_short_name=assignment.period.short_name,
+                parentnode_long_name=assignment.period.subject.long_name,
+                parentnode_short_name=assignment.period.subject.short_name,
+                parentnode_parentnode_long_name=assignment.period.subject.parentnode.long_name,
+                parentnode_parentnode_short_name=assignment.period.subject.parentnode.short_name)
+        return search_text
+
+    def get_tags(self, modelobject):
+        assignment = modelobject
+        tags = ['tag test']
+        return tags
+
+    def __get_admins_for_assignment(self, modelobject):
+        assignment = modelobject
+        admins = []
+        node = assignment
+        while node is not None:
+            for admin in node.admins.all():
+                admins.append(admin.username)
+            node = node.parentnode
+
+        return admins
+
+    def get_doctype_object_kwargs(self, modelobject):
+        kwargs = super(AssignmentRegistryItem, self).get_doctype_object_kwargs(modelobject=modelobject)
+        assignment = modelobject
+        kwargs.update({
+            'publishing_time': assignment.publishing_time,
+            'anonymous': assignment.anonymous,
+            'students_can_see_points': assignment.students_can_see_points,
+            'admins': self.__get_admins_for_assignment(assignment),
+            'delivery_types': assignment.delivery_types,
+            'deadline_handling': assignment.deadline_handling,
+            'scale_points_percent': assignment.scale_points_percent,
+            'first_deadline': assignment.first_deadline,
+            'max_points': assignment.max_points,
+            'passing_grade_min_points': assignment.passing_grade_min_points,
+            'points_to_grade_mapper': assignment.points_to_grade_mapper,
+            'grading_system_plugin_id': assignment.grading_system_plugin_id,
+            'students_can_create_groups': assignment.students_can_create_groups,
+            'students_can_not_create_groups_after': assignment.students_can_not_create_groups_after,
+            'feedback_workflow': assignment.feedback_workflow,
+        })
+        return kwargs
+
+    def get_all_modelobjects(self):
+        return coremodels.Assignment.objects.select_related(
+            'parentnode', # Period
+            'parentnode_parentnode', # parentnode of subject
+            'parentnode_parentnode_parentnode', # parentnode of subjects parentnode
+        ).all()
+
+elasticsearch_registry.registry.add(AssignmentRegistryItem())
