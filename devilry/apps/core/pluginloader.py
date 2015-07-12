e4201721e27351b2f61c79ffdce41e7115ba082a
Espen A. Kristiansen <post@espenak.net>
2015-07-12 18:00:41 +0200

    core.pluginloader: Updated to work with AppConfig.

--- devilry/apps/core/pluginloader.py
+++ devilry/apps/core/pluginloader.py
@@ -1,8 +1,10 @@
+from django.apps import apps
 from django.utils.importlib import import_module
 
 # A flag to tell us if autodiscover is running.  autodiscover will set this to
 # True while running, and False when it finishes.
 LOADING = False
+
 
 def autodiscover():
     """
@@ -17,33 +19,12 @@ def autodiscover():
     import imp
     from django.conf import settings
 
-    for app in settings.INSTALLED_APPS:
-        # For each app, we need to look for an admin.py inside that app's
-        # package. We can't use os.path here -- recall that modules may be
-        # imported different ways (think zip files) -- so we need to get
-        # the app's __path__ and look for admin.py on that path.
-
-        # Step 1: find out the app's __path__ Import errors here will (and
-        # should) bubble up, but a missing __path__ (which is legal, but weird)
-        # fails silently -- apps that do weird things with __path__ might
-        # need to roll their own admin registration.
-        mod = import_module(app)
+    for app in apps.get_app_configs():
         try:
-            app_path = mod.__path__
-        except AttributeError:
-            continue
-
-        # Step 2: use imp.find_module to find the app's admin.py. For some
-        # reason imp.find_module raises ImportError if the app can't be found
-        # but doesn't actually try to import the module. So skip this app if
-        # its admin.py doesn't exist
-        try:
-            imp.find_module('devilry_plugin', app_path)
+            imp.find_module('devilry_plugin', app.module.__name__)
         except ImportError:
             continue
 
-        # Step 3: import the app's admin file. If this has errors we want them
-        # to bubble up.
-        import_module("%s.devilry_plugin" % app)
+        import_module("%s.devilry_plugin" % app.module.__name)
     # autodiscover was successful, reset loading flag.
     LOADING = False
