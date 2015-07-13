6b8b2ae6eeab34ef22dccb4263b1441bdb49e8ee
StianJul <stianjul@gmail.com>
2015-07-13 17:36:21 +0200

    Added appconfig to installed apps and fixed signal handler

--- devilry/devilry_elasticsearch_cache/core_signal_handlers.py
+++ devilry/devilry_elasticsearch_cache/core_signal_handlers.py
@@ -9,26 +9,26 @@ def index_node_post_save(sender, instance, **kwargs):
         long_name=node.long_name)
     es_node.save()
 
-# def index_subject_post_save(sender, instance, **kwargs):
-#     subject = instance
-#     es_subject = elasticsearch_doctypes.Subject(
-#         _id=subject.id,
-#         short_name=subject.short_name,
-#         long_name=subject.long_name)
-#     es_subject.save()
-#
-# def index_period_post_save(sender, instance, **kwargs):
-#     period = instance
-#     es_period = elasticsearch_doctypes.Period(
-#         _id=period.id,
-#         short_name=period.short_name,
-#         long_name=period.long_name)
-#     es_period.save()
-#
-# def index_assignment_post_save(sender, instance, **kwargs):
-#     assignment = instance
-#     es_assignment = elasticsearch_doctypes.Assignment(
-#         _id=assignment.id,
-#         short_name=assignment.short_name,
-#         long_name=assignment.long_name)
-#     es_assignment.save()
+def index_subject_post_save(sender, instance, **kwargs):
+    subject = instance
+    es_subject = elasticsearch_doctypes.Subject(
+        _id=subject.id,
+        short_name=subject.short_name,
+        long_name=subject.long_name)
+    es_subject.save()
+
+def index_period_post_save(sender, instance, **kwargs):
+    period = instance
+    es_period = elasticsearch_doctypes.Period(
+        _id=period.id,
+        short_name=period.short_name,
+        long_name=period.long_name)
+    es_period.save()
+
+def index_assignment_post_save(sender, instance, **kwargs):
+    assignment = instance
+    es_assignment = elasticsearch_doctypes.Assignment(
+        _id=assignment.id,
+        short_name=assignment.short_name,
+        long_name=assignment.long_name)
+    es_assignment.save()
