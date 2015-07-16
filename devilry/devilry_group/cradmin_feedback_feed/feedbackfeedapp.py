528064aa67a69f8ea2778a7528aba3c44528ab89
Vegard Angell <v.angell123@gmail.com>
2015-07-16 11:24:45 +0200

    added get_context_data in FeedbackFeedView

--- devilry/devilry_group/cradmin_feedback_feed/feedbackfeedapp.py
+++ devilry/devilry_group/cradmin_feedback_feed/feedbackfeedapp.py
@@ -3,7 +3,12 @@ from django.views.generic import base
 
 class FeedbackFeedView(base.TemplateView):
     template_name = "devilry_group/cradmin_feedback_feed/feedbackfeed.django.html"
-    context_object_name = 'feedback'
+
+    def get_context_data(self, **kwargs):
+        context = super(FeedbackFeedView, self).get_context_data(**kwargs)
+
+        context['header_field_text'] = "Hello, I was set in the FeedbackFeedView's get_context_data function!"
+        return context
 
 class App(crapp.App):
     appurls = [
