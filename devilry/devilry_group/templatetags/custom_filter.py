ca1fc7b9cb259652f9fa58a260e3647cb9e01107
StianJul <stianjul@gmail.com>
2015-07-20 21:39:44 +0200

    Modified custom filter for file extensions and tests for the filter.

--- devilry/devilry_group/templatetags/custom_filter.py
+++ devilry/devilry_group/templatetags/custom_filter.py
@@ -4,12 +4,29 @@ register = template.Library()
 
 @register.filter("devilry_truncatefileextension")
 def devilry_truncatefileextension(value, max_length):
-    # Filter that takes a filename-string and
-    # and gets the n first characters from the string
-    # and combines it with the extension of the file.
-    # Example: if max_length is 3, then "SomeFile.pdf" becomes "Som...pdf".
-    if len(value) > max_length:
-        extension = os.path.splitext(value)[1]
-        truncated_value = value[:max_length] + '..' + extension
-        return truncated_value
+    # max_length is the number of characters the truncated
+    # string consists of including the extension
+    # Example:  if max_length is 10, the string "Delivery.pdf" will be
+    #           truncated to "Deli...pdf"
+    # Three letters from the filename will be showed either way.
+    if len(value) == 0:
+        return 'NO_NAME'
+    else:
+        offset = 3              # show at least three letters from filename
+        min_value_length = 8    # entire value name is same length or less than ex. Del.html
+
+        if len(value) <= min_value_length:
+            return value
+        elif len(value) > max_length:
+            splitted = os.path.splitext(value)
+            value_extension = splitted[1].strip('.')   # get extension
+
+            if (max_length - len(value_extension) - 3) < offset:
+                # show at least three letters from valuename
+                # to ensure no files are called "...<fileextension>"
+                truncated_value = value[:offset] + '...' + value_extension
+            else:
+                truncated_value = value[:max_length - len(value_extension) - 3] + '...' + value_extension
+            return truncated_value
+
     return value
--- devilry/devilry_group/tests/test_templatetags.py
+++ devilry/devilry_group/tests/test_templatetags.py
@@ -4,12 +4,41 @@ from devilry.devilry_group.templatetags.custom_filter import devilry_truncatefil
 
 class TestDevilryGroupTemplateTags(TestCase):
     def setUp(self):
-        self.file = 'DesignDocumentForDUCK1100_DeweyDuck.pdf'
+        self.filename = 'Delivery.pdf'
+        self.filename_length = len('Delivery.pdf')
 
     def test_truncatefileextension_truncated(self):
-        truncated_filename = devilry_truncatefileextension(self.file, 10)
-        self.assertEqual('DesignDocu...pdf', truncated_filename)
+        truncated_filename = devilry_truncatefileextension(self.filename, 10)
+        self.assertEqual('Deli...pdf', truncated_filename)
 
     def test_truncatefileextension_not_truncated(self):
-        truncated_filename = devilry_truncatefileextension(self.file, 100)
-        self.assertEqual('DesignDocumentForDUCK1100_DeweyDuck.pdf', truncated_filename)
+        truncated_filename = devilry_truncatefileextension(self.filename, 20)
+        self.assertEqual('Delivery.pdf', truncated_filename)
+
+    def test_truncatefileextension_same_as_max(self):
+        truncated_filename = devilry_truncatefileextension(self.filename, self.filename_length)
+        self.assertEqual('Delivery.pdf', truncated_filename)
+
+    def test_truncatefileextension_one_shorter_than_max(self):
+        truncated_filename = devilry_truncatefileextension(self.filename, self.filename_length+1)
+        self.assertEqual('Delivery.pdf', truncated_filename)
+
+    def test_truncatefileextension_one_longer_than_max(self):
+        truncated_filename = devilry_truncatefileextension(self.filename, self.filename_length-1)
+        self.assertEqual('Deliv...pdf', truncated_filename)
+
+    def test_truncatefileextension_shorter_than_min_length(self):
+        truncated_filename = devilry_truncatefileextension('De.html', 8)
+        self.assertEqual('De.html', truncated_filename)
+
+    def test_truncatefileextension_three_letters_from_value(self):
+        truncated_filename = devilry_truncatefileextension('Deliv.pdf', 8)
+        self.assertEqual('Del...pdf', truncated_filename)
+
+    def test_truncatefileextension_no_extension(self):
+        truncated_filename = devilry_truncatefileextension('DeliveryFile', 8)
+        self.assertEqual('Deliv...', truncated_filename)
+
+    def test_min_truncation(self):
+        truncated_filename = devilry_truncatefileextension('DesignDocument.pdf', 0)
+        self.assertEqual('Des...pdf', truncated_filename)
