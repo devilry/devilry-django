5c62a1dcbae95c13de8771087c07f54ed721be14
StianJul <stianjul@gmail.com>
2015-07-21 15:00:31 +0200

    Added connection check for ElasticSearch unittest server. Should be able to run unittests without having to run the ElasticSearch unittest server

--- devilry/devilry_elasticsearch_cache/apps.py
+++ devilry/devilry_elasticsearch_cache/apps.py
@@ -1,7 +1,12 @@
+import httplib
 from django.apps import AppConfig
 from django.conf import settings
+from django.contrib.sites import requests
 from django.db.models.signals import post_save
+import elasticsearch
 from elasticsearch_dsl.connections import connections
+from socket import error as socket_error
+from urllib3 import exceptions as urllib3_exceptions
 
 from devilry.devilry_elasticsearch_cache.core_signal_handlers import index_node_post_save
 
@@ -21,7 +26,16 @@ class ElasticsearchCacheAppConfig(AppConfig):
         from devilry.apps.core.models import Assignment
 
         connections.create_connection(hosts=settings.DEVILRY_ELASTICSEARCH_HOSTS)
-        post_save.connect(index_node_post_save, sender=Node)
-        post_save.connect(index_node_post_save, sender=Subject)
-        post_save.connect(index_node_post_save, sender=Period)
-        post_save.connect(index_node_post_save, sender=Assignment)
+
+        # should be able to run unittests without having the ElasticSearch unittest server
+        try:
+            addr = '{}:{}'.format(settings.DEVILRY_ELASTICSEARCH_HOSTS[0]['host'], settings.DEVILRY_ELASTICSEARCH_HOSTS[0]['port'])
+            httpServer = httplib.HTTPConnection("localhost", 9492).connect()
+        except socket_error:
+            print 'Connection error to ElasticSearch hosts'
+            print 'Hint: If you want the model objects to be saved in ES, start the ElasticSearch unittestserver!\nElse ignore this error'
+        else:
+            post_save.connect(index_node_post_save, sender=Node)
+            post_save.connect(index_node_post_save, sender=Subject)
+            post_save.connect(index_node_post_save, sender=Period)
+            post_save.connect(index_node_post_save, sender=Assignment)
