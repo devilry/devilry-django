6a39e20c1d007448a27d41bb768ab7306581c1b2
Espen A. Kristiansen <post@espenak.net>
2015-07-31 13:20:11 +0200

    devilry_admin: Created the app, and added a skeleton for the period overview.

---
+++ devilry/devilry_admin/cradmin_instances/crinstance_period.py
@@ -0,0 +1,40 @@
+from django_cradmin import crmenu
+
+from django_cradmin import crinstance
+
+from devilry.apps.core.models import Period
+from devilry.devilry_admin.views.period import overview
+
+
+class Menu(crmenu.Menu):
+    def build_menu(self):
+        group = self.request.cradmin_role
+        self.add_headeritem(
+            label=group.subject.long_name,
+            url=self.appindex_url('overview'),
+            icon="th-list")
+
+
+class CrAdminInstance(crinstance.BaseCrAdminInstance):
+    menuclass = Menu
+    roleclass = Period
+    apps = [
+        ('overview', overview.App)
+    ]
+    id = 'devilry_admin_periodadmin'
+    rolefrontpage_appname = 'overview'
+
+    def get_rolequeryset(self):
+        return Period.where_is_admin_or_superadmin(self.request.user)
+
+    def get_titletext_for_role(self, role):
+        """
+        Get a short title briefly describing the given ``role``.
+        Remember that the role is an Period.
+        """
+        period = role
+        return period
+
+    @classmethod
+    def matches_urlpath(cls, urlpath):
+        return urlpath.startswith('/devilry_admin/period')
