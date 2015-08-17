70640e618ca4e03e6819282149fc2c5676aca42c
StianJul <stianjul@gmail.com>
2015-08-17 17:19:19 +0200

    Added single file download. Started on downloading files to zipped folder.

--- devilry/devilry_group/urls.py
+++ devilry/devilry_group/urls.py
@@ -4,6 +4,7 @@ from devilry.devilry_group.cradmin_instances import crinstance_student
 from devilry.devilry_group.cradmin_instances import crinstance_examiner
 from devilry.devilry_group.cradmin_instances import crinstance_subjectadmin
 from devilry.devilry_group.cradmin_instances import crinstance_nodeadmin
+from devilry.devilry_group.views import feedbackfeed_download_files
 
 urlpatterns = patterns(
     'devilry.devilry_group',
@@ -11,4 +12,12 @@ urlpatterns = patterns(
     url(r'^examiner/', include(crinstance_examiner.ExaminerCrInstance.urls())),
     url(r'^nodeadmin/', include(crinstance_nodeadmin.NodeAdminCrInstance.urls())),
     url(r'^subjectadmin/', include(crinstance_subjectadmin.SubjectAdminCrInstance.urls())),
+
+    url(r'devilry-feedbackfeed-file-download/(?P<feedbackset_id>[0-9]+)/(?P<commentfile_id>[0-9]+)',
+        feedbackfeed_download_files.FileDownloadFeedbackfeedView.as_view(),
+        name='devilry-feedbackfeed-file-download'),
+
+    url(r'devilry-feedbackfeed-compressed-file-download/(?P<feedbackset_id>[0-9]+)',
+        feedbackfeed_download_files.CompressedFileDownloadView.as_view(),
+        name='devilry-feedbackfeed-compressed-file-download'),
 )
---
+++ devilry/devilry_group/views/feedbackfeed_download_files.py
@@ -0,0 +1,72 @@
+import posixpath
+from tempfile import NamedTemporaryFile
+import os
+import zipfile
+from django import http
+from django.shortcuts import get_object_or_404
+from django.views import generic
+from devilry.devilry_group import models as group_models
+from devilry.devilry_comment import models as comment_models
+from devilry.apps.core import models as core_models
+
+
+class FileDownloadFeedbackfeedView(generic.View):
+    """
+
+    """
+
+    def get(self, request, feedbackset_id, commentfile_id):
+        feedbackset = get_object_or_404(group_models.FeedbackSet, id=feedbackset_id)
+        comment_file = get_object_or_404(comment_models.CommentFile, id=commentfile_id)
+
+        if not (feedbackset.group.is_candidate(request.user) \
+                or feedbackset.group.is_examiner(request.user) \
+                or feedbackset.group.is_admin(request.user)):
+            return http.HttpResponseForbidden('Forbidden')
+
+        response = http.HttpResponse(comment_file.file, content_type=comment_file.mimetype)
+        response['content-disposition'] = 'attachement; filename=%s' % \
+            comment_file.filename.encode('ascii', 'replace')
+        response['content-length'] = comment_file.filesize
+
+        return response
+
+class CompressedFileDownloadView(generic.View):
+
+    def get(self, request, feedbackset_id):
+        feedbackset = get_object_or_404(group_models.FeedbackSet, id=feedbackset_id)
+        assignmentgroup = get_object_or_404(core_models.AssignmentGroup, id=feedbackset.group.id)
+
+        if not (assignmentgroup.is_candidate(request.user) \
+                or assignmentgroup.is_examiner(request.user) \
+                or assignmentgroup.is_admin(request.user)):
+            return http.HttpResponseForbidden('Forbidden')
+
+        dirname = u'{}-{}-delivery{}'.format(
+            assignmentgroup.parentnode.get_path(),
+            assignmentgroup.short_displayname,
+            assignmentgroup.id
+        )
+
+        zip_file_name = u'{}.zip'.format(dirname.encode('ascii', 'ignore'))
+
+        tempfile = NamedTemporaryFile()
+        zip_file = zipfile.ZipFile(tempfile, 'w')
+
+        for feedbackset in assignmentgroup.feedbackset_set.all():
+            for group_comment in feedbackset.groupcomment_set.all():
+                if not (group_comment.commentfile_set is None):
+                    for comment_file in group_comment.commentfile_set.all():
+                        file_content = comment_file
+                        zip_file.write(file_content.filename, posixpath.join(dirname, file_content.filename))
+
+        zip_file.close()
+
+        tempfile.seek(0)
+        response = http.HttpResponse(tempfile, content_type=comment_file.mimetype)
+        response['content-disposition'] = 'attachement; filename=%' % \
+            zip_file_name.encode('ascii', 'replace')
+        response['content-length'] = os.stat(tempfile.name).st_size
+
+        return response
+
