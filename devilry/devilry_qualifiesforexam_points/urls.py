6b99299edfed7b09e33680265c49f3271bf8deb8
Espen A. Kristiansen <post@espenak.net>
2015-08-24 01:07:20 +0200

    devilry_qualifiesforexam_*: Commented out tests and urls.

--- devilry/devilry_qualifiesforexam_points/urls.py
+++ devilry/devilry_qualifiesforexam_points/urls.py
@@ -1,19 +1,19 @@
-from django.conf.urls import patterns, url
-from django.contrib.auth.decorators import login_required
-from django.views.i18n import javascript_catalog
-
-from devilry.project.common.i18n import get_javascript_catalog_packages
-from .views import QualifiesBasedOnPointsView
-
-
-i18n_packages = get_javascript_catalog_packages('devilry_header', 'devilry.apps.core')
-
-
-
-urlpatterns = patterns('devilry.devilry_qualifiesforexam_points',
-    url('^all/$', login_required(QualifiesBasedOnPointsView.as_view()),
-        name='devilry_qualifiesforexam_points'),
-    url('^i18n.js$', javascript_catalog, kwargs={'packages': i18n_packages},
-        name='devilry_qualifiesforexam_points_i18n')
-)
-
+# from django.conf.urls import patterns, url
+# from django.contrib.auth.decorators import login_required
+# from django.views.i18n import javascript_catalog
+#
+# from devilry.project.common.i18n import get_javascript_catalog_packages
+# from .views import QualifiesBasedOnPointsView
+#
+#
+# i18n_packages = get_javascript_catalog_packages('devilry_header', 'devilry.apps.core')
+#
+#
+#
+# urlpatterns = patterns('devilry.devilry_qualifiesforexam_points',
+#     url('^all/$', login_required(QualifiesBasedOnPointsView.as_view()),
+#         name='devilry_qualifiesforexam_points'),
+#     url('^i18n.js$', javascript_catalog, kwargs={'packages': i18n_packages},
+#         name='devilry_qualifiesforexam_points_i18n')
+# )
+#
--- devilry/devilry_qualifiesforexam_select/urls.py
+++ devilry/devilry_qualifiesforexam_select/urls.py
@@ -1,23 +1,23 @@
-from django.conf.urls import patterns, url
-from django.contrib.auth.decorators import login_required
-from django.views.i18n import javascript_catalog
-from django.views.decorators.csrf import csrf_protect, ensure_csrf_cookie
-
-from devilry.project.common.i18n import get_javascript_catalog_packages
-from .views import QualifiesBasedOnManualSelectView
-from .views import BuildExtjsAppView
-
-
-i18n_packages = get_javascript_catalog_packages('devilry_extjsextras', 'devilry_header', 'devilry.apps.core', 'devilry_subjectadmin', 'devilry_qualifiesforexam', 'devilry_qualifiesforexam_select')
-
-
-
-
-urlpatterns = patterns('devilry.devilry_qualifiesforexam_select',
-    url('^$', login_required(csrf_protect(ensure_csrf_cookie(BuildExtjsAppView.as_view())))),
-    url('^select$', login_required(csrf_protect(ensure_csrf_cookie(QualifiesBasedOnManualSelectView.as_view()))),
-        name='devilry_qualifiesforexam_select'),
-    url('^i18n.js$', javascript_catalog, kwargs={'packages': i18n_packages},
-        name='devilry_qualifiesforexam_select_i18n')
-)
-
+# from django.conf.urls import patterns, url
+# from django.contrib.auth.decorators import login_required
+# from django.views.i18n import javascript_catalog
+# from django.views.decorators.csrf import csrf_protect, ensure_csrf_cookie
+#
+# from devilry.project.common.i18n import get_javascript_catalog_packages
+# from .views import QualifiesBasedOnManualSelectView
+# from .views import BuildExtjsAppView
+#
+#
+# i18n_packages = get_javascript_catalog_packages('devilry_extjsextras', 'devilry_header', 'devilry.apps.core', 'devilry_subjectadmin', 'devilry_qualifiesforexam', 'devilry_qualifiesforexam_select')
+#
+#
+#
+#
+# urlpatterns = patterns('devilry.devilry_qualifiesforexam_select',
+#     url('^$', login_required(csrf_protect(ensure_csrf_cookie(BuildExtjsAppView.as_view())))),
+#     url('^select$', login_required(csrf_protect(ensure_csrf_cookie(QualifiesBasedOnManualSelectView.as_view()))),
+#         name='devilry_qualifiesforexam_select'),
+#     url('^i18n.js$', javascript_catalog, kwargs={'packages': i18n_packages},
+#         name='devilry_qualifiesforexam_select_i18n')
+# )
+#
