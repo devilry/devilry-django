7cf99987c9328497ad95e98b84ab51e052da6739
Espen A. Kristiansen <post@espenak.net>
2015-08-24 00:56:48 +0200

    devilry_helplinks: Removed the directory, but not from settings.

--- devilry/devilry_helplinks/README.rst
+++
@@ -1,5 +0,0 @@
-###################
-devilry_helplinks
-###################
-
-Provides superadmins the ability to edit the help links available to users.
--- devilry/devilry_helplinks/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, url
-
-from .rest import ListHelpLinks
-
-urlpatterns = patterns('devilry.devilry_usersearch',
-                       url(r'^helplinks/$', ListHelpLinks.as_view()),
-                      )
-
