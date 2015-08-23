dccd51c40cba11218836c948eeb73bbb55eb5d78
Espen A. Kristiansen <post@espenak.net>
2015-08-23 18:27:35 +0200

    devilry_account: Added authentication backends for authenticating with email or username.

---
+++ devilry/devilry_account/authbackend/default.py
@@ -0,0 +1,84 @@
+from django.contrib import auth
+from django.contrib.auth import models as auth_models, get_user_model
+
+
+class AbstractBaseAuthBackend(object):
+    def get_user(self, user_id):
+        """
+        locate and return a :class:`User` based on the ``primary key`` of the current :class:`User` model.
+
+        NOTE: this function is defined by Django as required for an Auth-backend
+
+        :param user_id: the ``id`` of a :class:`User`
+        :return: the ``User`` matching the given ``user_id`` if it exists, ``None`` if not.
+        """
+        try:
+            return auth.get_user_model().objects.get(pk=user_id)
+        except auth.get_user_model().DoesNotExist:
+            return None
+
+
+class EmailAuthBackend(AbstractBaseAuthBackend):
+    """
+    An authentication backend that authenticates a user by email.
+    """
+
+    def authenticate(self, password, email):
+        """
+        Find the `User` corresponding to ``email``, verify ``password`` and return user.
+
+        NOTE: this function is defined by Django as required for an Auth-backend
+
+        Parameters:
+            password (str): ``password`` for the user to authenticate
+            email (str): ``email`` for the user to authenticate
+
+        Returns:
+            User: The authenticated user if authentication was successful, or ``None`` if not.
+        """
+        try:
+            user = self.lookup_user(email=email)
+        except get_user_model().DoesNotExist:
+            return None
+        else:
+            password_valid = auth_models.check_password(password, user.password)
+            if not password_valid:
+                return None
+            return user
+
+    def lookup_user(self, email):
+        user = get_user_model().objects.get_by_email(email=email)
+        return user
+
+
+class UsernameAuthBackend(AbstractBaseAuthBackend):
+    """
+    An authentication backend that authenticates a user by username.
+    """
+
+    def authenticate(self, password, username):
+        """
+        Find the `User` corresponding to ``username``, verify ``password`` and return user.
+
+        NOTE: this function is defined by Django as required for an Auth-backend
+
+        Parameters:
+            password (str): ``password`` for the user to authenticate
+            username (str): ``username`` for the user to authenticate
+
+        Returns:
+            User: The authenticated user if authentication was successful, or ``None`` if not.
+        """
+        try:
+            user = self.lookup_user(username=username)
+        except get_user_model().DoesNotExist:
+            return None
+        else:
+            password_valid = auth_models.check_password(password, user.password)
+            if not password_valid:
+                return None
+            return user
+
+    def lookup_user(self, username):
+        user = get_user_model().objects.get_by_username(username=username)
+        return user
--- devilry/devilry_account/tests/authbackend/test_default.py
+++ devilry/devilry_account/tests/authbackend/test_default.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from devilry.devilry_account.authbackend.default import EmailAuthBackend
+from devilry.devilry_account.authbackend.default import EmailAuthBackend, UsernameAuthBackend
 from devilry.devilry_account.models import User
 
 
@@ -11,7 +11,7 @@ class TestEmailAuthBackend(TestCase):
         self.authbackend = EmailAuthBackend()
 
     def test_lookup_user(self):
-        user = self.authbackend.lookup_user(email=self.testuser.email)
+        user = self.authbackend.lookup_user(email='testuser@example.com')
         self.assertEquals(user.pk, self.testuser.pk)
 
     def test_lookup_user_invalid_email(self):
@@ -27,17 +27,57 @@ class TestEmailAuthBackend(TestCase):
         self.assertIsNone(user)
 
     def test_authenticate(self):
-        user = self.authbackend.authenticate(email=self.testuser.email,
+        user = self.authbackend.authenticate(email='testuser@example.com',
                                              password='test')
         self.assertIsNotNone(user)
         self.assertEquals(self.testuser.pk, user.pk)
 
     def test_authenticate_invalid_password_returns_none(self):
-        user = self.authbackend.authenticate(email=self.testuser.email,
+        user = self.authbackend.authenticate(email='testuser@example.com',
                                              password='notcorrectpassword')
         self.assertIsNone(user)
 
     def test_authenticate_invalid_email_returns_none(self):
         user = self.authbackend.authenticate(email='doesnotexist@example.com',
+                                             password='test')
+        self.assertIsNone(user)
+
+
+class TestUsernameAuthBackend(TestCase):
+    def setUp(self):
+        self.testuser = User.objects.create_user(
+            username='testuser',
+            password='test')
+        self.authbackend = UsernameAuthBackend()
+
+    def test_lookup_user(self):
+        user = self.authbackend.lookup_user(username='testuser')
+        self.assertEquals(user.pk, self.testuser.pk)
+
+    def test_lookup_user_invalid_email(self):
+        with self.assertRaises(User.DoesNotExist):
+            self.authbackend.lookup_user(username='invalid')
+
+    def test_get_user(self):
+        user = self.authbackend.get_user(self.testuser.pk)
+        self.assertEquals(user.pk, self.testuser.pk)
+
+    def test_get_user_returns_none(self):
+        user = self.authbackend.get_user(42)
+        self.assertIsNone(user)
+
+    def test_authenticate(self):
+        user = self.authbackend.authenticate(username='testuser',
+                                             password='test')
+        self.assertIsNotNone(user)
+        self.assertEquals(self.testuser.pk, user.pk)
+
+    def test_authenticate_invalid_password_returns_none(self):
+        user = self.authbackend.authenticate(username='testuser',
+                                             password='notcorrectpassword')
+        self.assertIsNone(user)
+
+    def test_authenticate_invalid_email_returns_none(self):
+        user = self.authbackend.authenticate(username='doesnotexist',
                                              password='test')
         self.assertIsNone(user)
