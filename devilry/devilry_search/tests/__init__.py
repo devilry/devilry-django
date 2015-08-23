aa81c5591f024bc6056f268f791493b32139ebc4
Espen A. Kristiansen <post@espenak.net>
2015-08-24 00:55:30 +0200

    devilry_search: Removed all the old REST API stuff, but not the search view.

--- devilry/devilry_search/tests/__init__.py
+++
@@ -1,6 +0,0 @@
-from .rest.admincontent import TestRestSearchAdminContent
-from .rest.studentcontent import TestRestSearchStudentContent
-from .rest.examinercontent import TestRestSearchExaminerContent
-from .test_views_search import *
-from .test_search_helper import *
-# from .test_aboutme import TestAboutMeView
--- devilry/devilry_search/urls.py
+++ devilry/devilry_search/urls.py
@@ -4,5 +4,4 @@ from django.contrib.auth.decorators import login_required
 from .views.search import SearchView
 
 urlpatterns = patterns('devilry.devilry_search',
-                       url('^rest/', include('devilry.devilry_search.rest.urls')),
                        url('^$', login_required(SearchView.as_view()), name='devilry_search'))
