1b2ba3b63735cccd68bf9dfe530c7a746eaeabbd
Espen A. Kristiansen <post@espenak.net>
2015-08-24 16:27:37 +0200

    devilry_admin: Added all instances to urls.

--- devilry/devilry_admin/urls.py
+++ devilry/devilry_admin/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import patterns, url, include
 
+from devilry.devilry_admin.cradmin_instances import crinstance_node
 from devilry.devilry_admin.cradmin_instances import crinstance_subject
 from devilry.devilry_admin.cradmin_instances import crinstance_period
+from devilry.devilry_admin.cradmin_instances import crinstance_assignment
 
 urlpatterns = patterns(
     '',
+    url(r'^node/', include(crinstance_node.CrAdminInstance.urls())),
     url(r'^subject/', include(crinstance_subject.CrAdminInstance.urls())),
     url(r'^period/', include(crinstance_period.CrAdminInstance.urls())),
+    url(r'^assignment/', include(crinstance_assignment.CrAdminInstance.urls())),
 )
