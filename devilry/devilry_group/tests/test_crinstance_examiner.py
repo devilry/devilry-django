2817e501ad1d0f469432bceed29637dd4448ff67
StianJul <stianjul@gmail.com>
2015-08-26 19:51:40 +0200

    devilry_group: Created testfiles for crinstances in devilry group

--- devilry/devilry_group/tests/test_crinstance_examiner.py
+++ devilry/devilry_group/tests/test_crinstance_examiner.py
@@ -1,7 +1,7 @@
 from django import test
 
 from devilry.apps.core import testhelper
-from devilry.devilry_student.cradmin_group.cradmin_group import CrAdminInstance
+from devilry.devilry_group.cradmin_instances.crinstance_examiner import ExaminerCrInstance
 from devilry.project.develop.testhelpers.corebuilder import UserBuilder, NodeBuilder, \
     FeedbackSetBuilder
 from devilry.project.develop.testhelpers.datebuilder import DateTimeBuilder
