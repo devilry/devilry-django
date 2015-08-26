318b09670d0ac59a7b919d632529f0d02cd38095
StianJul <stianjul@gmail.com>
2015-08-26 19:25:23 +0200

    Added tests for cradmin instances

---
+++ devilry/devilry_group/tests/test_crinstance_examiner.py
@@ -0,0 +1,11 @@
+from django import test
+
+from devilry.apps.core import testhelper
+from devilry.devilry_student.cradmin_group.cradmin_group import CrAdminInstance
+from devilry.project.develop.testhelpers.corebuilder import UserBuilder, NodeBuilder, \
+    FeedbackSetBuilder
+from devilry.project.develop.testhelpers.datebuilder import DateTimeBuilder
+
+
+class TestCrinstanceExaminer(test.TestCase):
+    pass
