becd9a9a4d8b632669c062ff83cd89df99ac073b
StianJul <stianjul@gmail.com>
2015-08-27 18:59:30 +0200

    devilry_group: Added testfiles for crinstances and feedbackviews

---
+++ devilry/devilry_group/tests/test_feedbackfeed_admin.py
@@ -0,0 +1,7 @@
+from django.test import TestCase
+from devilry.devilry_group.tests import test_feedbackfeed_common
+from devilry.devilry_group.views import feedbackfeed_admin
+
+
+class TestFeedbackfeedAdmin(TestCase, test_feedbackfeed_common.TestFeedbackFeedMixin):
+    viewclass = feedbackfeed_admin.AdminFeedbackFeedView
