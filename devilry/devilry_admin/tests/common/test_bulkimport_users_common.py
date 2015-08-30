fb4f5f1d9f292749c1469efcd1a1cde4cffe1a34
Espen A. Kristiansen <post@espenak.net>
2015-08-30 14:38:35 +0200

    admin...bulkimport_users_common: Usable abstract view for bulk importing users.

---
+++ devilry/devilry_admin/tests/common/test_bulkimport_users_common.py
@@ -0,0 +1,129 @@
+from django.test import TestCase
+from django_cradmin import cradmin_testhelpers
+
+from devilry.devilry_admin.views.common.bulkimport_users_common import AbstractTypeInUsersView
+
+
+class AbstractTypeInUsersViewTestMixin(cradmin_testhelpers.TestCaseMixin):
+    def test_get_render_form(self):
+        mockresponse = self.mock_http200_getrequest_htmls()
+        self.assertTrue(mockresponse.selector.exists('textarea#id_users_blob'))
+
+    def test_get_render_form_help_text_email_backend(self):
+        with self.settings(DJANGO_CRADMIN_USE_EMAIL_AUTH_BACKEND=True):
+            mockresponse = self.mock_http200_getrequest_htmls()
+            self.assertEqual(mockresponse.selector.one('#hint_id_users_blob').alltext_normalized,
+                             'Type or paste in email addresses separated '
+                             'by comma (","), space or one user on each line.')
+
+    def test_get_render_form_help_text_username_backend(self):
+        with self.settings(DJANGO_CRADMIN_USE_EMAIL_AUTH_BACKEND=False):
+            mockresponse = self.mock_http200_getrequest_htmls()
+            self.assertEqual(mockresponse.selector.one('#hint_id_users_blob').alltext_normalized,
+                             'Type or paste in usernames separated '
+                             'by comma (","), space or one user on each line.')
+
+    def test_get_render_form_placeholder_email_backend(self):
+        with self.settings(DJANGO_CRADMIN_USE_EMAIL_AUTH_BACKEND=True):
+            mockresponse = self.mock_http200_getrequest_htmls()
+            self.assertEqual(mockresponse.selector.one('#id_users_blob')['placeholder'],
+                             'jane@example.com\njohn@example.com')
+
+    def test_get_render_form_placeholder_username_backend(self):
+        with self.settings(DJANGO_CRADMIN_USE_EMAIL_AUTH_BACKEND=False):
+            mockresponse = self.mock_http200_getrequest_htmls()
+            self.assertEqual(mockresponse.selector.one('#id_users_blob')['placeholder'],
+                             'jane\njohn')
+
+    def test_post_blank(self):
+        mockresponse = self.mock_http200_postrequest_htmls()
+        self.assertEqual('This field is required.',
+                         mockresponse.selector.one('#error_1_id_users_blob').alltext_normalized)
+
+    def test_post_invalid_emails_simple(self):
+        with self.settings(DJANGO_CRADMIN_USE_EMAIL_AUTH_BACKEND=True):
+            mockresponse = self.mock_http200_postrequest_htmls(requestkwargs=dict(data={
+                'users_blob': 'test'
+            }))
+            self.assertEqual('Invalid email addresses: test',
+                             mockresponse.selector.one('#error_1_id_users_blob').alltext_normalized)
+
+    def test_post_invalid_emails_multiple(self):
+        with self.settings(DJANGO_CRADMIN_USE_EMAIL_AUTH_BACKEND=True):
+            mockresponse = self.mock_http200_postrequest_htmls(requestkwargs=dict(data={
+                'users_blob': 'test,test2@example.com,test3@@example.com'
+            }))
+            self.assertEqual('Invalid email addresses: test, test3@@example.com',
+                             mockresponse.selector.one('#error_1_id_users_blob').alltext_normalized)
+
+    def test_post_invalid_usernames_simple(self):
+        with self.settings(DJANGO_CRADMIN_USE_EMAIL_AUTH_BACKEND=False):
+            mockresponse = self.mock_http200_postrequest_htmls(requestkwargs=dict(data={
+                'users_blob': 'Test'
+            }))
+            self.assertEqual('Invalid usernames: Test',
+                             mockresponse.selector.one('#error_1_id_users_blob').alltext_normalized)
+
+    def test_post_invalid_usernames_multiple(self):
+        with self.settings(DJANGO_CRADMIN_USE_EMAIL_AUTH_BACKEND=False):
+            mockresponse = self.mock_http200_postrequest_htmls(requestkwargs=dict(data={
+                'users_blob': 'test,test2@example.com,test3.ing,Test4,test5'
+            }))
+            self.assertEqual('Invalid usernames: Test4, test2@example.com, test3.ing',
+                             mockresponse.selector.one('#error_1_id_users_blob').alltext_normalized)
+
+
+class TestAbstractTypeInUsersView(TestCase, AbstractTypeInUsersViewTestMixin):
+    viewclass = AbstractTypeInUsersView
+
+    def test_split_users_blob_empty(self):
+        self.assertEqual(
+            set(),
+            AbstractTypeInUsersView.split_users_blob(''))
+
+    def test_split_users_blob_single(self):
+        self.assertEqual(
+            {'test@example.com'},
+            AbstractTypeInUsersView.split_users_blob('test@example.com'))
+
+    def test_split_users_blob_multiple(self):
+        self.assertEqual(
+            {'test@example.com', 'test2@example.com', 'test3@example.com'},
+            AbstractTypeInUsersView.split_users_blob('test@example.com test2@example.com test3@example.com'))
+
+    def test_split_users_blob_newlines(self):
+        self.assertEqual(
+            {'test@example.com', 'test2@example.com', 'test3@example.com'},
+            AbstractTypeInUsersView.split_users_blob('test@example.com\ntest2@example.com\ntest3@example.com'))
+
+    def test_split_users_blob_comma(self):
+        self.assertEqual(
+            {'test@example.com', 'test2@example.com', 'test3@example.com'},
+            AbstractTypeInUsersView.split_users_blob('test@example.com,test2@example.com,test3@example.com'))
+
+    def test_split_users_blob_semicolon(self):
+        self.assertEqual(
+            {'test@example.com', 'test2@example.com', 'test3@example.com'},
+            AbstractTypeInUsersView.split_users_blob('test@example.com;test2@example.com;test3@example.com'))
+
+    def test_split_users_blob_messy_middle(self):
+        self.assertEqual(
+            {'test@example.com', 'test2@example.com', 'test3@example.com'},
+            AbstractTypeInUsersView.split_users_blob('test@example.com, \ntest2@example.com'
+                                                     '\n,\n,  test3@example.com'))
+
+    def test_split_users_blob_messy_suffix(self):
+        self.assertEqual(
+            {'test@example.com'},
+            AbstractTypeInUsersView.split_users_blob('test@example.com, \n;\n,'))
+
+    def test_split_users_blob_messy_prefix(self):
+        self.assertEqual(
+            {'test@example.com'},
+            AbstractTypeInUsersView.split_users_blob(', ;\n\n,\ntest@example.com'))
+
+    def test_split_users_blob_messy_everywhere(self):
+        self.assertEqual(
+            {'test@example.com', 'test2@example.com', 'test3@example.com'},
+            AbstractTypeInUsersView.split_users_blob('  \n,,; \n, test@example.com, ,;,\ntest2@example.com'
+                                                     '\n,\n,  test3@example.com,,,,\n,  \n,  '))
---
+++ devilry/devilry_admin/views/common/bulkimport_users_common.py
@@ -0,0 +1,111 @@
+import re
+
+from crispy_forms import layout
+from django import forms
+from django.conf import settings
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
+from django_cradmin.crispylayouts import PrimarySubmit
+from django_cradmin.viewhelpers import formbase
+from django.utils.translation import ugettext_lazy as _
+
+
+class AbstractTypeInUsersView(formbase.FormView):
+    users_blob_split_pattern = re.compile(r'[,;\s]+')
+    create_button_label = _('Save')
+
+    @classmethod
+    def split_users_blob(cls, users_blob):
+        """
+        Split the given string of users by ``,`` and whitespace.
+
+        Returns a set.
+        """
+        users_blob_split = cls.users_blob_split_pattern.split(users_blob)
+        if len(users_blob_split) == 0:
+            return []
+        if users_blob_split[0] == '':
+            del users_blob_split[0]
+        if len(users_blob_split) > 0 and users_blob_split[-1] == '':
+            del users_blob_split[-1]
+        return set(users_blob_split)
+
+    def __get_users_blob_help_text(self):
+        if settings.DJANGO_CRADMIN_USE_EMAIL_AUTH_BACKEND:
+            return _('Type or paste in email addresses separated '
+                     'by comma (","), space or one user on each line.')
+        else:
+            return _('Type or paste in usernames separated '
+                     'by comma (","), space or one user on each line.')
+
+    def __get_users_blob_placeholder(self):
+        if settings.DJANGO_CRADMIN_USE_EMAIL_AUTH_BACKEND:
+            return _('jane@example.com\njohn@example.com')
+        else:
+            return _('jane\njohn')
+
+    def get_form_class(self):
+        users_blob_help_text = self.__get_users_blob_help_text()
+
+        class UserImportForm(forms.Form):
+            users_blob = forms.CharField(
+                widget=forms.Textarea,
+                required=True,
+                help_text=users_blob_help_text
+            )
+
+            def __validate_users_blob_emails(self, emails):
+                invalid_emails = []
+                for email in emails:
+                    try:
+                        validate_email(email)
+                    except ValidationError:
+                        invalid_emails.append(email)
+                if invalid_emails:
+                    self.add_error(
+                        'users_blob',
+                        _('Invalid email addresses: %(emails)s') % {
+                            'emails': ', '.join(sorted(invalid_emails))
+                        }
+                    )
+
+            def __validate_users_blob_usernames(self, usernames):
+                valid_username_pattern = re.compile(
+                    getattr(settings, 'DEVILRY_VALID_USERNAME_PATTERN', r'^[a-z0-9]+$'))
+                invalid_usernames = []
+                for username in usernames:
+                    if not valid_username_pattern.match(username):
+                        invalid_usernames.append(username)
+                self.add_error(
+                    'users_blob',
+                    _('Invalid usernames: %(usernames)s') % {
+                        'usernames': ', '.join(sorted(invalid_usernames))
+                    }
+                )
+
+            def clean(self):
+                cleaned_data = super(UserImportForm, self).clean()
+                users_blob = cleaned_data.get('users_blob', None)
+                if users_blob:
+                    users = AbstractTypeInUsersView.split_users_blob(users_blob)
+                    if settings.DJANGO_CRADMIN_USE_EMAIL_AUTH_BACKEND:
+                        self.__validate_users_blob_emails(emails=users)
+                    else:
+                        self.__validate_users_blob_usernames(usernames=users)
+
+        return UserImportForm
+
+    def get_field_layout(self):
+        return [
+            layout.Div(
+                layout.Field('users_blob', placeholder=self.__get_users_blob_placeholder()),
+                css_class='cradmin-globalfields')
+        ]
+
+    def get_buttons(self):
+        return [
+            PrimarySubmit('save', self.create_button_label),
+        ]
+
+    # def form_valid(self, form):
+    #     return HttpResponseRedirect('/some/view')
