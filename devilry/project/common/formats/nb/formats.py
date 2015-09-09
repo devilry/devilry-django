4845304d3edb69700707866b30502291a919b864
Espen A. Kristiansen <post@espenak.net>
2015-09-09 16:28:41 +0200

    project.common.formats: Include dayname in DATETIME_FORMAT.

--- devilry/project/common/formats/nb/formats.py
+++ devilry/project/common/formats/nb/formats.py
@@ -1,5 +1,5 @@
 # Locale formats Norwegian Bokmaal
 DATE_FORMAT = 'j F'
 SHORT_DATE_FORMAT = 'j N'
-DATETIME_FORMAT = 'j F, Y, H:i'
+DATETIME_FORMAT = 'l j F, Y, H:i'
 SHORT_DATETIME_FORMAT = 'j b Y, H:i'
