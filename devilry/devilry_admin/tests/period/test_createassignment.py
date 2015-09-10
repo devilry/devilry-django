9f7aed047c15aeb142d7ebc9a05ecc5cdeed5630
Espen A. Kristiansen <post@espenak.net>
2015-09-10 12:36:44 +0200

    admin...createassignment: Handle name suggestions with edge cases.

--- devilry/devilry_admin/tests/period/test_createassignment.py
+++ devilry/devilry_admin/tests/period/test_createassignment.py
@@ -3,19 +3,52 @@ from django.template import defaultfilters
 from django.test import TestCase
 from django.utils import timezone
 from django_cradmin import cradmin_testhelpers
+from devilry.apps.core.models import Assignment
 from devilry.devilry_admin.views.period import createassignment
 from devilry.project.develop.testhelpers import corebuilder
+from devilry.utils import datetimeutils
 
 
-class TestCreateSuggestedName(TestCase):
-    def test_no_number(self):
-        self.assertEqual(createassignment.create_suggested_name('test'), '')
+class TestNameSuggestion(TestCase):
+    def test_number_no_number(self):
+        self.assertEqual(createassignment.NameSuggestion(long_name='Test', short_name='test').number, None)
 
-    def test_number_in_the_middle(self):
-        self.assertEqual(createassignment.create_suggested_name('Assignment 10 test'), '')
+    def test_number_not_the_same_number(self):
+        self.assertEqual(createassignment.NameSuggestion(long_name='Test1', short_name='test2').number, None)
 
-    def test_suffixed_with_number(self):
-        self.assertEqual(createassignment.create_suggested_name('test10'), 'test11')
+    def test_number_number_in_the_middle(self):
+        self.assertEqual(
+            createassignment.NameSuggestion(long_name='Test 1 assignment', short_name='test1assignment').number,
+            None)
+
+    def test_number_suffixed_with_number(self):
+        self.assertEqual(createassignment.NameSuggestion(long_name='Test1', short_name='test1').number, 1)
+
+    def test_number_suffixed_with_number_zero(self):
+        # Ensure we do not have any ``if not number``, which would
+        # evaluate to no match for ``0``.
+        self.assertEqual(createassignment.NameSuggestion(long_name='Test0', short_name='test0').number, 0)
+
+    def test_number_suffixed_multicharacter_number(self):
+        self.assertEqual(createassignment.NameSuggestion(long_name='Test2001', short_name='test2001').number, 2001)
+
+    def test_create_names_from_number_no_number(self):
+        namesuggestion = createassignment.NameSuggestion(long_name='Test', short_name='test')
+        self.assertEqual(namesuggestion.suggested_long_name, '')
+        self.assertEqual(namesuggestion.suggested_short_name, '')
+
+    def test_create_names_from_number(self):
+        namesuggestion = createassignment.NameSuggestion(long_name='Test 1', short_name='test1')
+        self.assertEqual(namesuggestion.suggested_long_name, 'Test 2')
+        self.assertEqual(namesuggestion.suggested_short_name, 'test2')
+
+    def test_has_suggestion_false(self):
+        namesuggestion = createassignment.NameSuggestion(long_name='Test', short_name='test')
+        self.assertFalse(namesuggestion.has_suggestion())
+
+    def test_has_suggestion_true(self):
+        namesuggestion = createassignment.NameSuggestion(long_name='Test 1', short_name='test1')
+        self.assertTrue(namesuggestion.has_suggestion())
 
 
 class TestCreateView(TestCase, cradmin_testhelpers.TestCaseMixin):
@@ -29,43 +62,56 @@ class TestCreateView(TestCase, cradmin_testhelpers.TestCaseMixin):
         self.assertTrue(mockresponse.selector.exists('input[name=short_name]'))
         self.assertTrue(mockresponse.selector.exists('input[name=first_deadline]'))
 
-    def test_get_suggested_assignment_long_name_first_assignment(self):
+    def test_get_suggested_name_first_assignment(self):
         testperiod = corebuilder.PeriodBuilder.make().period
         mockresponse = self.mock_http200_getrequest_htmls(
             cradmin_role=testperiod)
         self.assertEqual(mockresponse.selector.one('input[name=long_name]').get('value', ''), '')
+        self.assertEqual(mockresponse.selector.one('input[name=short_name]').get('value', ''), '')
 
-    def test_get_suggested_assignment_long_name_previous_assignment_not_suffixed_with_number(self):
-        testperiod = corebuilder.AssignmentBuilder.make(long_name='Test').assignment.period
+    def test_get_suggested_name_previous_assignment_not_suffixed_with_number(self):
+        testperiod = corebuilder.AssignmentBuilder\
+            .make(long_name='Test', short_name='test').assignment.period
         mockresponse = self.mock_http200_getrequest_htmls(
             cradmin_role=testperiod)
         self.assertEqual(mockresponse.selector.one('input[name=long_name]').get('value', ''), '')
+        self.assertEqual(mockresponse.selector.one('input[name=short_name]').get('value', ''), '')
 
-    def test_get_suggested_assignment_long_name_previous_assignment_suffixed_with_number(self):
-        testperiod = corebuilder.AssignmentBuilder.make(
-            long_name='Test1', first_deadline=timezone.now()).assignment.period
+    def test_get_suggested_name_previous_assignment_suffixed_with_number(self):
+        testperiod = corebuilder.AssignmentBuilder\
+            .make(long_name='Test1', short_name='test1',
+                  first_deadline=timezone.now())\
+            .assignment.period
         mockresponse = self.mock_http200_getrequest_htmls(
             cradmin_role=testperiod)
         self.assertEqual(mockresponse.selector.one('input[name=long_name]').get('value', ''), 'Test2')
-
-    def test_get_suggested_assignment_short_name_first_assignment(self):
-        testperiod = corebuilder.PeriodBuilder.make().period
-        mockresponse = self.mock_http200_getrequest_htmls(
-            cradmin_role=testperiod)
-        self.assertEqual(mockresponse.selector.one('input[name=short_name]').get('value', ''), '')
-
-    def test_get_suggested_assignment_short_name_previous_assignment_not_suffixed_with_number(self):
-        testperiod = corebuilder.AssignmentBuilder.make(short_name='Test').assignment.period
-        mockresponse = self.mock_http200_getrequest_htmls(
-            cradmin_role=testperiod)
-        self.assertEqual(mockresponse.selector.one('input[name=short_name]').get('value', ''), '')
-
-    def test_get_suggested_assignment_short_name_previous_assignment_suffixed_with_number(self):
-        testperiod = corebuilder.AssignmentBuilder.make(
-            short_name='test1', first_deadline=timezone.now()).assignment.period
-        mockresponse = self.mock_http200_getrequest_htmls(
-            cradmin_role=testperiod)
         self.assertEqual(mockresponse.selector.one('input[name=short_name]').get('value', ''), 'test2')
+
+    def test_get_suggested_name_previous_assignment_suffixed_with_number_namecollision_no_first_deadline(self):
+        testperiod = corebuilder.PeriodBuilder.make().period
+        corebuilder.AssignmentBuilder\
+            .make(long_name='Test1', short_name='test1',
+                  first_deadline=timezone.now(),
+                  period=testperiod)
+        corebuilder.AssignmentBuilder\
+            .make(long_name='Test2', short_name='test2',
+                  first_deadline=None,
+                  period=testperiod)
+        mockresponse = self.mock_http200_getrequest_htmls(
+            cradmin_role=testperiod)
+        self.assertEqual(mockresponse.selector.one('input[name=long_name]').get('value', ''), '')
+        self.assertEqual(mockresponse.selector.one('input[name=short_name]').get('value', ''), '')
+
+    def test_get_suggested_name_previous_assignment_suffixed_with_number_namecollision_strange_order(self):
+        periodbuilder = corebuilder.PeriodBuilder.make()
+        periodbuilder.add_assignment(long_name='Test1', short_name='test1',
+                                     first_deadline=timezone.now())
+        periodbuilder.add_assignment(long_name='Test2', short_name='test2',
+                                     first_deadline=timezone.now() - timedelta(days=30))
+        mockresponse = self.mock_http200_getrequest_htmls(
+            cradmin_role=periodbuilder.period)
+        self.assertEqual(mockresponse.selector.one('input[name=long_name]').get('value', ''), '')
+        self.assertEqual(mockresponse.selector.one('input[name=short_name]').get('value', ''), '')
 
     def test_get_suggested_deadlines_first_assignment(self):
         testperiod = corebuilder.PeriodBuilder.make().period
@@ -146,3 +192,42 @@ class TestCreateView(TestCase, cradmin_testhelpers.TestCaseMixin):
             defaultfilters.date(datetime(2015, 3, 1, 12, 30) + timedelta(days=28),
                                 'DATETIME_FORMAT'),
         ])
+
+    def __get_valid_first_deadline_isoformatted(self):
+        return datetimeutils.isoformat_noseconds(
+            timezone.now() + timedelta(days=10))
+
+    def test_post_ok(self):
+        periodbuilder = corebuilder.PeriodBuilder.make()
+        self.assertEqual(Assignment.objects.count(), 0)
+        first_deadline_isoformat = self.__get_valid_first_deadline_isoformatted()
+        self.mock_http302_postrequest(
+            cradmin_role=periodbuilder.period,
+            requestkwargs={
+                'data': {
+                    'long_name': 'Test assignment',
+                    'short_name': 'testassignment',
+                    'first_deadline': first_deadline_isoformat,
+                }
+            })
+        self.assertEqual(Assignment.objects.count(), 1)
+        created_assignment = Assignment.objects.first()
+        self.assertEqual(created_assignment.long_name, 'Test assignment')
+        self.assertEqual(created_assignment.short_name, 'testassignment')
+        self.assertEqual(
+            first_deadline_isoformat,
+            datetimeutils.isoformat_noseconds(created_assignment.first_deadline))
+
+    # def test_post_first_deadline_before_preferred_publishing_time(self):
+    #     periodbuilder = corebuilder.PeriodBuilder.make()
+    #     self.assertEqual(Assignment.objects.count(), 0)
+    #     first_deadline_isoformat = datetimeutils.isoformat_noseconds(timezone.now())
+    #     mockresponse = self.mock_http200_postrequest_htmls(
+    #         cradmin_role=periodbuilder.period,
+    #         requestkwargs={
+    #             'data': {
+    #                 'long_name': 'Test assignment',
+    #                 'short_name': 'testassignment',
+    #                 'first_deadline': first_deadline_isoformat,
+    #             }
+    #         })
--- devilry/devilry_admin/views/period/createassignment.py
+++ devilry/devilry_admin/views/period/createassignment.py
@@ -1,25 +1,68 @@
 from datetime import timedelta
 import re
+
 from crispy_forms import layout
 from django import forms
+from django.conf import settings
+from django.db import models
 from django.template.loader import render_to_string
+from django.utils import timezone
 from django_cradmin import crapp
 from django_cradmin.viewhelpers import create
 from django_cradmin.viewhelpers import crudbase
 from django.utils.translation import ugettext_lazy as _
 from django_cradmin.widgets.datetimepicker import DateTimePickerWidget
+
 from devilry.apps.core.models import Assignment
 
 
-def create_suggested_name(name):
-    match = re.match(r'^.+?(?P<suffixnumber>\d+)$', name)
-    if match:
-        suffixnumber = int(match.group(1))
+class NameSuggestion(object):
+    pattern = re.compile(r'^.+?(?P<suffixnumber>\d+)$')
+
+    def __init__(self, long_name, short_name):
+        self.long_name = long_name
+        self.short_name = short_name
+        self.number = self.__find_common_number()
+        self.suggested_short_name = ''
+        self.suggested_long_name = ''
+        if self.number is not None:
+            self.__suggest_names_from_number()
+
+    def has_suggestion(self):
+        return self.number is not None
+
+    def __extract_number(self, name):
+        match = self.pattern.match(name)
+        if match:
+            return int(match.group(1))
+        else:
+            return None
+
+    def __find_common_number(self):
+        """
+        If both short and long name is suffixed with the same number,
+        return the number as an int. If not, return ``None``.
+        """
+        short_name_number = self.__extract_number(self.short_name)
+        if short_name_number is None:
+            return None
+        long_name_number = self.__extract_number(self.long_name)
+        if long_name_number is None:
+            return None
+        if short_name_number == long_name_number:
+            return short_name_number
+        else:
+            return None
+
+    def __suggest_name_from_number(self, name):
+        match = self.pattern.match(name)
         prefix = name[:match.start(1)]
-        suggested_name = u'{}{}'.format(prefix, suffixnumber + 1)
+        suggested_name = u'{}{}'.format(prefix, self.number + 1)
         return suggested_name
-    else:
-        return ''
+
+    def __suggest_names_from_number(self):
+        self.suggested_long_name = self.__suggest_name_from_number(self.long_name)
+        self.suggested_short_name = self.__suggest_name_from_number(self.short_name)
 
 
 class CreateForm(forms.ModelForm):
@@ -29,10 +72,12 @@ class CreateForm(forms.ModelForm):
             'long_name',
             'short_name',
             'first_deadline',
+            'publishing_time',
+            'parentnode'
         ]
 
     def __init__(self, *args, **kwargs):
-        # self.period = kwargs.pop('period')
+        self.period = kwargs.pop('period')
         super(CreateForm, self).__init__(*args, **kwargs)
         self.fields['long_name'].help_text = _(
             'Type the name of your assignment.')
@@ -47,6 +92,26 @@ class CreateForm(forms.ModelForm):
             'students on the assignment.'
         )
 
+        # We ignore this, we just need to include it to be able to
+        # change the value in clean()
+        self.fields['publishing_time'].widget = forms.HiddenInput()
+        self.fields['publishing_time'].required = False
+
+        # We ignore this, we just need to include it to be able to
+        # change the value in clean()
+        self.fields['parentnode'].widget = forms.HiddenInput()
+        self.fields['parentnode'].required = False
+
+    def clean(self):
+        cleaned_data = super(CreateForm, self).clean()
+        first_deadline = cleaned_data.get('first_deadline', None)
+        cleaned_data['parentnode'] = self.period
+        if first_deadline:
+            publishing_time = timezone.now() + timedelta(
+                minutes=settings.DEVILRY_ASSIGNMENT_PUBLISHING_TIME_DELAY_MINUTES)
+            cleaned_data['publishing_time'] = publishing_time
+        return cleaned_data
+
 
 class CreateView(crudbase.OnlySaveButtonMixin, create.CreateView):
     form_class = CreateForm
@@ -55,24 +120,36 @@ class CreateView(crudbase.OnlySaveButtonMixin, create.CreateView):
     helpbox_template_name = 'devilry_admin/period/createassignment/helpbox.django.html'
 
     def dispatch(self, *args, **kwargs):
-        period = self.request.cradmin_role
-        self.previous_assignment = period.assignments\
-            .exclude(first_deadline=None)\
-            .order_by('first_deadline')\
+        self.period = self.request.cradmin_role
+        self.previous_assignment = self.period.assignments \
+            .exclude(first_deadline=None) \
+            .order_by('first_deadline') \
             .last()
         return super(CreateView, self).dispatch(*args, **kwargs)
+
+    def get_form_kwargs(self):
+        kwargs = super(CreateView, self).get_form_kwargs()
+        kwargs['period'] = self.request.cradmin_role
+        return kwargs
 
     def get_initial(self):
         initial = super(CreateView, self).get_initial()
         if self.previous_assignment:
-            initial['long_name'] = create_suggested_name(self.previous_assignment.long_name)
-            initial['short_name'] = create_suggested_name(self.previous_assignment.short_name)
+            namesuggestion = NameSuggestion(long_name=self.previous_assignment.long_name,
+                                            short_name=self.previous_assignment.short_name)
+            if namesuggestion.has_suggestion():
+                namecollision_queryset = self.period.assignments.filter(
+                    models.Q(long_name=namesuggestion.suggested_long_name) |
+                    models.Q(short_name=namesuggestion.suggested_short_name))
+                if not namecollision_queryset.exists():
+                    initial['long_name'] = namesuggestion.suggested_long_name
+                    initial['short_name'] = namesuggestion.suggested_short_name
         return initial
 
     def __get_suggested_deadlines(self):
         suggested_deadlines = []
         if self.previous_assignment:
-            for days_forward in range(7, (7*4)+1, 7):
+            for days_forward in range(7, (7 * 4) + 1, 7):
                 suggested_deadline = self.previous_assignment.first_deadline + timedelta(days=days_forward)
                 suggested_deadlines.append(suggested_deadline)
         return suggested_deadlines
@@ -112,9 +189,6 @@ class CreateView(crudbase.OnlySaveButtonMixin, create.CreateView):
                 css_class='cradmin-globalfields'
             )
         ]
-
-    def get_form_helper(self):
-        return
 
 
 class App(crapp.App):
