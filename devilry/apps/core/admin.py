c2e9edad02bb6576fc19b75ad17439b4e98aa93a
Espen A. Kristiansen <post@espenak.net>
2015-09-10 11:24:06 +0200

    core...admin: Added admin UI for Assignment.

--- devilry/apps/core/admin.py
+++ devilry/apps/core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from devilry.apps.core.models import AssignmentGroup, Node, Subject, Period
+from devilry.apps.core.models import AssignmentGroup, Node, Subject, Period, Assignment
 from django.utils.translation import ugettext_lazy as _
 
 
@@ -12,35 +12,37 @@ class BaseNodeAdmin(admin.ModelAdmin):
         'id',
         'short_name',
         'long_name',
+        'path_as_string',
         'admins_as_string'
     )
     search_fields = [
         'id',
         'short_name',
         'long_name',
-        'admins__username',
-        'admins__email',
+        'admins__shortname',
         'admins__fullname',
     ]
 
     def admins_as_string(self, obj):
         return u', '.join([user.username for user in obj.admins.all()])
+
     admins_as_string.short_description = _("Admins")
 
-    def admins_as_string(self, obj):
+    def path_as_string(self, obj):
         return obj.get_path()
-    admins_as_string.short_description = _("Path")
+
+    path_as_string.short_description = _("Path")
 
     def get_queryset(self, request):
-        return super(BaseNodeAdmin, self).get_queryset(request)\
+        return super(BaseNodeAdmin, self).get_queryset(request) \
             .prefetch_related(
-                'admins')
+            'admins')
 
 
 class NodeAdmin(BaseNodeAdmin):
     def get_queryset(self, request):
-        return super(NodeAdmin, self).get_queryset(request)\
-            .select_related('parentnode')\
+        return super(NodeAdmin, self).get_queryset(request) \
+            .select_related('parentnode')
 
 
 admin.site.register(Node, NodeAdmin)
@@ -49,12 +51,28 @@ admin.site.register(Node, NodeAdmin)
 class SubjectAdmin(BaseNodeAdmin):
     pass
 
+
 admin.site.register(Subject, SubjectAdmin)
+
 
 class PeriodAdmin(BaseNodeAdmin):
     pass
 
+
 admin.site.register(Period, PeriodAdmin)
+
+
+class AssignmentAdmin(BaseNodeAdmin):
+    search_fields = BaseNodeAdmin.search_fields + [
+        'parentnode__long_name',
+        'parentnode__short_name',
+        'parentnode__parentnode__long_name',
+        'parentnode__parentnode__short_name',
+    ]
+
+
+admin.site.register(Assignment, AssignmentAdmin)
+
 
 class AssignmentGroupAdmin(admin.ModelAdmin):
     list_display = ('id', 'assignment', 'long_displayname')
@@ -68,13 +86,14 @@ class AssignmentGroupAdmin(admin.ModelAdmin):
     ]
 
     def get_queryset(self, request):
-        return super(AssignmentGroupAdmin, self).get_queryset(request)\
+        return super(AssignmentGroupAdmin, self).get_queryset(request) \
             .select_related(
-                'parentnode', 'parentnode__parentnode',
-                'parentnode__parentnode__parentnode')
+            'parentnode', 'parentnode__parentnode',
+            'parentnode__parentnode__parentnode')
 
-    # def admins_as_string(self, obj):
-    #     return ', '.join([user.username for user in obj.admins.all()])
-    # admins_as_string.short_description = "Admins"
+        # def admins_as_string(self, obj):
+        #     return ', '.join([user.username for user in obj.admins.all()])
+        # admins_as_string.short_description = "Admins"
+
 
 admin.site.register(AssignmentGroup, AssignmentGroupAdmin)
