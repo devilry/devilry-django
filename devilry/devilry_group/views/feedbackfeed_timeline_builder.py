06f068d738e6e6373dbfcf874009e24d803eddc9
StianJul <stianjul@gmail.com>
2015-09-13 19:58:45 +0200

    devilry_group.views..feedbackfeed_timeline_builder.py: moved timeline building logic to class FeedbackfeedTimelineBuilder.

---
+++ devilry/devilry_group/views/feedbackfeed_timeline_builder.py
@@ -0,0 +1,136 @@
+import collections
+import datetime
+
+from devilry.devilry_group import models as group_models
+
+
+class FeedbackFeedTimelineBuilder():
+
+    def __init__(self, view):
+        self.viewclass=view
+
+    def get_feedbacksets_for_group(self, group):
+        """
+        TODO: document
+
+        :param group:
+        :return:
+        """
+        return group_models.FeedbackSet.objects.filter(group=group)
+
+    def add_comments_to_timeline(self, group, timeline):
+        comments = self.viewclass._get_comments_for_group(group)
+        for comment in comments:
+            if comment.published_datetime not in timeline.keys():
+                timeline[comment.published_datetime] = []
+            timeline[comment.published_datetime].append({
+                "type": "comment",
+                "obj": comment
+            })
+        return timeline
+
+    def add_announcements_to_timeline(self, group, feedbacksets, timeline):
+        """
+        TODO: document
+
+        :param group:
+        :param feedbacksets:
+        :param timeline:
+        :return:
+        """
+        if len(feedbacksets) == 0:
+            return group.assignment.first_deadline, timeline
+
+        first_feedbackset = feedbacksets[0]
+        last_deadline = first_feedbackset.deadline_datetime
+
+        for index, feedbackset in enumerate(feedbacksets):
+            if index == 0:
+                if group.assignment.first_deadline is not None:
+                    deadline_datetime = group.assignment.first_deadline
+                else:
+                    deadline_datetime = feedbackset.deadline_datetime
+                # deadline_datetime = group.assignment.first_deadline
+            else:
+                deadline_datetime = feedbackset.deadline_datetime
+            if deadline_datetime not in timeline.keys():
+                timeline[deadline_datetime] = []
+            timeline[deadline_datetime].append({
+                "type": "deadline_expired"
+            })
+            if feedbackset.created_datetime not in timeline.keys():
+                timeline[feedbackset.created_datetime] = []
+
+            # Add available first_deadline, either assignment.first_deadline(if index is 0)
+            # or
+            # feedbackset.deadline_datetime
+            if group.assignment.first_deadline is not None and index == 0:
+                if deadline_datetime <= group.assignment.first_deadline:
+                    timeline[feedbackset.created_datetime].append({
+                        "type": "deadline_created",
+                        "obj": group.assignment.first_deadline,
+                        "user": first_feedbackset.created_by
+                    })
+                    first_feedbackset = feedbackset
+            elif feedbackset.deadline_datetime is not None:
+                if deadline_datetime <= feedbackset.deadline_datetime:
+                    timeline[feedbackset.created_datetime].append({
+                        "type": "deadline_created",
+                        "obj": feedbackset.deadline_datetime,
+                        "user": feedbackset.created_by
+                    })
+                    first_feedbackset = feedbackset
+            elif feedbackset is not feedbacksets[0]:
+                timeline[feedbackset.created_datetime].append({
+                    "type": "deadline_created",
+                    "obj": deadline_datetime,
+                    "user": feedbackset.created_by
+                })
+
+            if deadline_datetime is None or last_deadline is None:
+                pass
+            elif deadline_datetime > last_deadline:
+                last_deadline = deadline_datetime
+
+            if feedbackset.published_datetime is not None:
+                if feedbackset.published_datetime not in timeline.keys():
+                    timeline[feedbackset.published_datetime] = []
+                timeline[feedbackset.published_datetime].append({
+                    "type": "grade",
+                    "obj": feedbackset.points
+                })
+        return last_deadline, timeline
+
+    def sort_timeline(self, timeline):
+        """
+        TODO: document
+
+        :param timeline:
+        :return:
+        """
+        def compare_timeline_items(a, b):
+            datetime_a = a[0]
+            datetime_b = b[0]
+            if datetime_a is None:
+                datetime_a = datetime.datetime(1970, 1, 1)
+            if datetime_b is None:
+                datetime_b = datetime.datetime(1970, 1, 1)
+            return cmp(datetime_a, datetime_b)
+
+        sorted_timeline = collections.OrderedDict(sorted(timeline.items(), compare_timeline_items))
+        return sorted_timeline
+
+    def build_timeline(self, group, feedbacksets):
+        """
+        TODO: document
+
+        :param group:
+        :param feedbacksets:
+        :return:
+        """
+        timeline = {}
+        timeline = self.add_comments_to_timeline(group, timeline)
+        last_deadline, timeline = self.add_announcements_to_timeline(group, feedbacksets, timeline)
+        timeline = self.sort_timeline(timeline)
+
+        return last_deadline, timeline
