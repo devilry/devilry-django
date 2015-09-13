6b7a5b744d699fa3707be3345a72787686b3a176
Espen A. Kristiansen <post@espenak.net>
2015-09-13 16:27:00 +0200

    core.download_staticfeedbackfileattachment: Fixed pep8 errors.

--- devilry/apps/core/views/download_staticfeedbackfileattachment.py
+++ devilry/apps/core/views/download_staticfeedbackfileattachment.py
@@ -1,22 +1,21 @@
 from mimetypes import guess_type
-from django.db.models import Q
 
-from django.http import HttpResponse, HttpResponseForbidden
+from django.db.models import Q
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django.views.generic import View
+
 from devilry.apps.core.models import StaticFeedbackFileAttachment
 
 
 class DownloadStaticFeedbackFileAttachmentView(View):
-
     def get(self, request, pk, asciifilename=None):
         if self.request.user.is_superuser:
             queryset = StaticFeedbackFileAttachment.objects.all()
         else:
-            queryset = StaticFeedbackFileAttachment.objects\
-                .filter(
-                    Q(staticfeedback__delivery__deadline__assignment_group__candidates__student=self.request.user) |
-                    Q(staticfeedback__delivery__deadline__assignment_group__examiners__user=self.request.user))\
+            queryset = StaticFeedbackFileAttachment.objects \
+                .filter(Q(staticfeedback__delivery__deadline__assignment_group__candidates__student=self.request.user) |
+                        Q(staticfeedback__delivery__deadline__assignment_group__examiners__user=self.request.user)) \
                 .distinct()
         fileattachment = get_object_or_404(queryset, pk=pk)
 
