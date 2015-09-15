920bbd6a90552e9f771e8ec9f1047c0b75752dbb
Espen A. Kristiansen <post@espenak.net>
2015-09-15 11:47:25 +0200

    project...fabrictasks: Cleanup.

--- devilry/project/develop/fabrictasks.py
+++ devilry/project/develop/fabrictasks.py
@@ -1,13 +1,10 @@
 import os
 from os.path import exists, join, relpath
 from os import remove, getcwd
-
-# from shutil import rmtree, make_archive
-# from zipfile import ZipFile
 import shutil
+
 from fabric.api import local, abort, task
 from fabric.context_managers import shell_env, lcd
-
 
 DB_FILE = join('devilry_developfiles', 'db.sqlite3')
 LANGUAGES = ['en', 'nb']
@@ -31,14 +28,14 @@ def remove_db(djangoenv='develop'):
         _managepy('dbdev_destroy', djangoenv=djangoenv)
 
 
-
 @task
 def syncmigrate(djangoenv='develop'):
     """
     Run ``bin/django_dev.py syncmigrate -v0 --noinput``
     """
     # _managepy('syncdb -v0 --noinput', djangoenv=djangoenv)
-    _managepy('migrate -v0 --noinput', djangoenv=djangoenv)
+    _managepy('migrate --noinput', djangoenv=djangoenv)
+
 
 @task
 def reset_db(djangoenv='develop'):
@@ -80,18 +77,18 @@ def demodb(djangoenv='develop'):
     """
     reset_db(djangoenv=djangoenv)
     _managepy('devilry_developer_demodb',
-        djangoenv=djangoenv,
-        environment={
-            'DEVILRY_EMAIL_BACKEND': 'django.core.mail.backends.dummy.EmailBackend'
-        })
+              djangoenv=djangoenv,
+              environment={
+                  'DEVILRY_EMAIL_BACKEND': 'django.core.mail.backends.dummy.EmailBackend'
+              })
 
 
 def _demodb_managepy(command, djangoenv):
     _managepy(command,
-        djangoenv=djangoenv,
-        environment={
-            'DEVILRY_EMAIL_BACKEND': 'django.core.mail.backends.dummy.EmailBackend'
-        })
+              djangoenv=djangoenv,
+              environment={
+                  'DEVILRY_EMAIL_BACKEND': 'django.core.mail.backends.dummy.EmailBackend'
+              })
 
 
 @task
@@ -103,120 +100,6 @@ def new_demodb(djangoenv='develop'):
     """
     reset_db(djangoenv=djangoenv)
     _demodb_managepy('devilry_developer_demodb_createusers', djangoenv=djangoenv)
-    # _demodb_managepy('devilry_developer_demodb_createnode duckburgh', djangoenv=djangoenv)
-    # _demodb_managepy('devilry_developer_demodb_createsubject --node=duckburgh duck1010', djangoenv=djangoenv)
-    # _demodb_managepy(
-    #     'devilry_developer_demodb_createperiod --subject=duck1010 '
-    #     '--starts-in-months -4 --duration-months 6 spring2015',
-    #     djangoenv=djangoenv)
-    # _demodb_managepy(
-    #     'devilry_developer_demodb_create_pointassignment --period=duck1010.spring2015 '
-    #     '--publishing-time-in-days -14 week1',
-    #     djangoenv=djangoenv)
-    # _demodb_managepy(
-    #     'devilry_developer_demodb_create_pointassignment --period=duck1010.spring2015 '
-    #     '--publishing-time-in-days 10 week2',
-    #     djangoenv=djangoenv)
-
-
-
-# def _gzip_file(infile):
-#     import gzip
-#     f_in = open(infile, 'rb')
-#     gzipped_outfile = '{0}.gz'.format(infile)
-#     f_out = gzip.open(gzipped_outfile, 'wb')
-#     f_out.writelines(f_in)
-#     f_out.close()
-#     f_in.close()
-#     remove(infile)
-
-
-# def _get_stashdir(home):
-#     from os.path import expanduser
-#     from os import getcwd
-#     if home == 'yes':
-#         return join(expanduser('~'), '.devilry_db_and_deliveries_stash')
-#     else:
-#         return join(getcwd(), 'db_and_deliveries_stash')
-
-# @task
-# def stash_db_and_deliveries(home=False):
-#     """
-#     Dump the database and deliveries into the
-#     ``db_and_deliveries_stash/``-directory.
-
-#     :param home:
-#         Use ``home=yes`` to stash to ``~/.devilry_db_and_deliveries_stash``
-#         instead of ``<this dir>/db_and_deliveries_stash/``
-#     """
-#     stashdir = _get_stashdir(home)
-#     if exists(stashdir):
-#         rmtree(stashdir)
-#     mkdir(stashdir)
-
-#     # DB
-#     dbdumpfile = join(stashdir, 'dbdump.sql')
-#     backup_db(dbdumpfile)
-#     _gzip_file(dbdumpfile)
-
-#     # Delivery files
-#     import logging
-#     logging.basicConfig(level=logging.INFO)
-#     log = logging.getLogger('files.zip')
-#     make_archive(join(stashdir, 'files'), 'zip', logger=log, base_dir="deliverystorehier")
-
-
-# def _gunzip_file(gzipped_infile):
-#     import gzip
-#     unzipped = gzip.open(gzipped_infile, 'rb').read()
-#     outfile = gzipped_infile.replace('.gz', '')
-#     open(outfile, 'wb').write(unzipped)
-#     return outfile
-
-
-
-# @task
-# def unstash_db_and_deliveries(home=False):
-#     """
-#     Undo ``stash_db_and_deliveries``.
-
-#     :param home:
-#         Use ``home=yes`` to unstash from ``~/.devilry_db_and_deliveries_stash``
-#         instead of ``<this dir>/db_and_deliveries_stash/``
-#     """
-#     # DB
-#     stashdir = _get_stashdir(home)
-#     dbfile = _gunzip_file(join(stashdir, 'dbdump.sql.gz'))
-#     restore_db(dbfile)
-#     remove(dbfile) # We remove the unzipped dbdump, but keep the .gz
-
-#     # Delivery files
-#     if exists('deliverystorehier'):
-#         rmtree('deliverystorehier')
-#     zipfile = ZipFile(join(stashdir, 'files.zip'))
-#     zipfile.extractall()
-
-
-# @task
-# def backup_db(sqldumpfile):
-#     """
-#     Dumps a backup of ``db.sqlite3`` to the given ``sqldumpfile``.
-
-#     :param sqldumpfile: The SQL file to write the dump to.
-#     """
-#     local('sqlite3 db.sqlite3 .dump > {sqldumpfile}'.format(**vars()))
-
-# @task
-# def restore_db(sqldumpfile):
-#     """
-#     Restore ``db.sqlite3`` from the given ``sqldumpfile``.
-
-#     :param sqldumpfile: The SQL file to restore the database from.
-#     """
-#     from os.path import exists
-#     if exists(DB_FILE):
-#         remove(DB_FILE)
-#     local('sqlite3 db.sqlite3 < {sqldumpfile}'.format(**vars()))
 
 
 @task
@@ -254,7 +137,7 @@ def jsbuild(appname, nocompress=False, watch=False, no_jsbcreate=False, no_build
         if no_jsbcreate == 'next':
             if not watch:
                 abort('no_jsbcreate="next" only makes sense with --watch')
-            jsbuild(appname, nocompress, watch=False) # build one with no_jsbcreate=False
+            jsbuild(appname, nocompress, watch=False)  # build one with no_jsbcreate=False
         extra_args.append('--no-jsbcreate')
     extra_args = ' '.join(extra_args)
     _managepy(
@@ -286,6 +169,7 @@ def makemessages_javascript():
             '--ignore "bower_components" '
             '-l {}'.format(languagecode), working_directory='devilry')
 
+
 @task
 def compilemessages():
     _managepy('compilemessages', working_directory='devilry')
@@ -298,6 +182,7 @@ def run_elasticsearch_develop_server():
     """
     command = 'elasticsearch --config=not_for_deploy/elasticsearch.develop.yml'
     return local(command)
+
 
 @task
 def run_elasticsearch_unittest_server():
