e481e24c5eb668d5882866d1c1d68242ed647f13
Espen A. Kristiansen <post@espenak.net>
2015-09-16 00:29:00 +0200

    admin...subject.overview: Basic subject overview.

--- devilry/devilry_admin/tests/subject/test_overview.py
+++ devilry/devilry_admin/tests/subject/test_overview.py
@@ -1,39 +1,107 @@
-from django.test import TestCase, RequestFactory
-import htmls
-import mock
+from django.test import TestCase
+from django_cradmin import cradmin_testhelpers
+from django_cradmin import crinstance
+from model_mommy import mommy
 
+from devilry.apps.core.mommy_recipes import ACTIVE_PERIOD_START, ACTIVE_PERIOD_END
 from devilry.devilry_admin.views.subject import overview
-from devilry.project.develop.testhelpers.corebuilder import UserBuilder, SubjectBuilder
+from devilry.utils import datetimeutils
 
 
-class TestOverviewApp(TestCase):
-    def setUp(self):
-        self.testuser = UserBuilder('testuser').user
-        self.factory = RequestFactory()
-        self.subjectbuilder = SubjectBuilder.quickadd_ducku_duck1010()
-
-    def __mock_get_request(self, subject):
-        request = self.factory.get('/')
-        request.user = self.testuser
-        request.cradmin_role = subject
-        request.cradmin_app = mock.MagicMock()
-        request.cradmin_instance = mock.MagicMock()
-        request.session = mock.MagicMock()
-        response = overview.Overview.as_view()(request)
-        return response
-
-    def __mock_http200_getrequest_htmls(self, subject):
-        response = self.__mock_get_request(subject=subject)
-        self.assertEqual(response.status_code, 200)
-        response.render()
-        selector = htmls.S(response.content)
-        return selector
+class TestOverviewApp(TestCase, cradmin_testhelpers.TestCaseMixin):
+    viewclass = overview.Overview
 
     def test_title(self):
-        selector = self.__mock_http200_getrequest_htmls(subject=self.subjectbuilder.subject)
-        self.assertEquals(selector.one('title').alltext_normalized, 'duck1010')
+        testsubject = mommy.make('core.Subject',
+                                 short_name='testsubject')
+        mockresponse = self.mock_http200_getrequest_htmls(cradmin_role=testsubject)
+        self.assertEqual('testsubject',
+                         mockresponse.selector.one('title').alltext_normalized)
 
     def test_h1(self):
-        self.subjectbuilder.update(long_name='DUCK 1010')
-        selector = self.__mock_http200_getrequest_htmls(subject=self.subjectbuilder.subject)
-        self.assertEquals(selector.one('h1').alltext_normalized, 'DUCK 1010')
+        testsubject = mommy.make('core.Subject',
+                                 long_name='Test Subject')
+        mockresponse = self.mock_http200_getrequest_htmls(cradmin_role=testsubject)
+        self.assertEqual(u'Test Subject',
+                         mockresponse.selector.one('h1').alltext_normalized)
+
+    # def test_createperiod_link_text(self):
+    #     testsubject = mommy.make('core.Subject')
+    #     mockresponse = self.mock_http200_getrequest_htmls(cradmin_role=testsubject)
+    #     self.assertEqual('Create new period',
+    #                      mockresponse.selector.one(
+    #                          '#devilry_admin_period_createperiod_link').alltext_normalized)
+    #
+    # def test_createperiod_link_url(self):
+    #     testsubject = mommy.make('core.Subject')
+    #     mockresponse = self.mock_http200_getrequest_htmls(cradmin_role=testsubject)
+    #     mockresponse.request.cradmin_instance.reverse_url.assert_called_once_with(
+    #         appname='createperiod',
+    #         viewname='INDEX',
+    #         args=(), kwargs={})
+
+    def test_periodlist_no_periods(self):
+        testsubject = mommy.make('core.Subject')
+        mockresponse = self.mock_http200_getrequest_htmls(cradmin_role=testsubject)
+        self.assertFalse(mockresponse.selector.exists('#devilry_admin_period_overview_periodlist'))
+
+    def test_periodlist_itemrendering_name(self):
+        testsubject = mommy.make('core.Subject')
+        mommy.make_recipe('devilry.apps.core.period_active',
+                          parentnode=testsubject,
+                          long_name='Test Period')
+        mockresponse = self.mock_http200_getrequest_htmls(cradmin_role=testsubject)
+        self.assertEqual('Test Period',
+                         mockresponse.selector.one(
+                             '.devilry-admin-period-overview-period-link').alltext_normalized)
+
+    def test_periodlist_itemrendering_url(self):
+        testsubject = mommy.make('core.Subject')
+        testperiod = mommy.make_recipe('devilry.apps.core.period_active',
+                                       parentnode=testsubject,
+                                       long_name='Test Period')
+        mockresponse = self.mock_http200_getrequest_htmls(cradmin_role=testsubject)
+        self.assertEqual(crinstance.reverse_cradmin_url(instanceid='devilry_admin_periodadmin',
+                                                        appname='overview',
+                                                        roleid=testperiod.id),
+                         mockresponse.selector.one(
+                             '.devilry-admin-period-overview-period-link')['href'])
+
+    def test_periodlist_itemrendering_start_time(self):
+        testsubject = mommy.make('core.Subject')
+        mommy.make_recipe('devilry.apps.core.period_active', parentnode=testsubject)
+        with self.settings(DATETIME_FORMAT=datetimeutils.ISODATETIME_DJANGOFORMAT, USE_L10N=False):
+            mockresponse = self.mock_http200_getrequest_htmls(cradmin_role=testsubject)
+        self.assertEqual(datetimeutils.isoformat_noseconds(ACTIVE_PERIOD_START),
+                         mockresponse.selector.one(
+                             '.devilry-admin-period-overview-period-start_time-value').alltext_normalized)
+
+    def test_periodlist_itemrendering_end_time(self):
+        testsubject = mommy.make('core.Subject')
+        mommy.make_recipe('devilry.apps.core.period_active',
+                          parentnode=testsubject)
+        with self.settings(DATETIME_FORMAT=datetimeutils.ISODATETIME_DJANGOFORMAT, USE_L10N=False):
+            mockresponse = self.mock_http200_getrequest_htmls(cradmin_role=testsubject)
+        self.assertEqual(datetimeutils.isoformat_noseconds(ACTIVE_PERIOD_END),
+                         mockresponse.selector.one(
+                             '.devilry-admin-period-overview-period-end_time-value').alltext_normalized)
+
+    def test_periodlist_ordering(self):
+        testsubject = mommy.make('core.Subject')
+        mommy.make_recipe('devilry.apps.core.period_active',
+                          parentnode=testsubject,
+                          long_name='Period 2')
+        mommy.make_recipe('devilry.apps.core.period_old',
+                          parentnode=testsubject,
+                          long_name='Period 1')
+        mommy.make_recipe('devilry.apps.core.period_future',
+                          parentnode=testsubject,
+                          long_name='Period 3')
+        mockresponse = self.mock_http200_getrequest_htmls(cradmin_role=testsubject)
+        periodnames = [element.alltext_normalized
+                       for element in mockresponse.selector.list('.devilry-admin-period-overview-period-link')]
+        self.assertEqual([
+            'Period 3',
+            'Period 2',
+            'Period 1',
+        ], periodnames)
--- devilry/devilry_admin/views/subject/overview.py
+++ devilry/devilry_admin/views/subject/overview.py
@@ -2,24 +2,15 @@ from __future__ import unicode_literals
 from django.views.generic import TemplateView
 
 from django_cradmin import crapp
-from django_cradmin.viewhelpers.objecttable import ObjectTableView
-from devilry.apps.core.models import Period
 
 
-class Overview(ObjectTableView):
+class Overview(TemplateView):
     template_name = 'devilry_admin/subject/overview.django.html'
-    model = Period
-
-    def get_queryset_for_role(self, role):
-        """
-        Get a queryset with all objects of :obj:`.model`  that
-        the current role can access.
-        """
-        return self.get_model_class().objects.filter(parentnode=role)
 
     def get_context_data(self, **kwargs):
         context = super(Overview, self).get_context_data(**kwargs)
         subject = self.request.cradmin_role
+        context['subject'] = subject
         context['periods'] = list(subject.periods.order_by('-start_time'))
         return context
 
