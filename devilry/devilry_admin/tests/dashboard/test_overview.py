f9def7df05dd72dfb548cf1c4d1e060a06a2f1f5
Espen A. Kristiansen <post@espenak.net>
2015-09-19 14:29:18 +0200

    admin...dashboard.overview: Basic handling of active semesters and your courses.

--- devilry/devilry_admin/tests/dashboard/test_overview.py
+++ devilry/devilry_admin/tests/dashboard/test_overview.py
@@ -1,7 +1,8 @@
 from django.conf import settings
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
 from django_cradmin import cradmin_testhelpers
 from model_mommy import mommy
+from devilry.devilry_account.models import PermissionGroup
 from devilry.devilry_admin.views.dashboard import overview
 
 
@@ -19,3 +20,28 @@ class TestOverviewApp(TestCase, cradmin_testhelpers.TestCaseMixin):
         mockresponse = self.mock_http200_getrequest_htmls(cradmin_role=testuser)
         self.assertEqual(u'Administrator dashboard',
                          mockresponse.selector.one('h1').alltext_normalized)
+
+    def __minimal_mockrequest_with_user(self, user):
+        mockrequest = RequestFactory().get('')
+        mockrequest.user = user
+        view = overview.Overview()
+        view.request = mockrequest
+        return view
+
+    def test__get_all_subjects_where_user_is_subjectadmin_none(self):
+        testuser = mommy.make(settings.AUTH_USER_MODEL)
+        view = self.__minimal_mockrequest_with_user(user=testuser)
+        self.assertEqual(
+            [],
+            view._Overview__get_all_subjects_where_user_is_subjectadmin())
+
+    def test__get_all_subjects_where_user_is_subjectadmin_not_subjectadmin(self):
+        testuser = mommy.make(settings.AUTH_USER_MODEL)
+        permissiongroup = mommy.make('devilry_account.PermissionGroup',
+                                     grouptype=PermissionGroup.GROUPTYPE_SUBJECTADMIN)
+        mommy.make('devilry_account.SubjectPermissionGroup',
+                   permissiongroup=permissiongroup)
+        view = self.__minimal_mockrequest_with_user(user=testuser)
+        self.assertEqual(
+            [],
+            view._Overview__get_all_subjects_where_user_is_subjectadmin())
--- devilry/devilry_admin/views/dashboard/overview.py
+++ devilry/devilry_admin/views/dashboard/overview.py
@@ -1,11 +1,53 @@
-from __future__ import unicode_literals
-from django.views.generic import TemplateView
+from django.db import models
+from django.utils import timezone
 
+from django.views.generic import TemplateView
 from django_cradmin import crapp
+
+from devilry.devilry_account.models import SubjectPermissionGroup, PeriodPermissionGroup
 
 
 class Overview(TemplateView):
     template_name = 'devilry_admin/dashboard/overview.django.html'
+
+    def __get_all_subjects_where_user_is_subjectadmin(self):
+        if not hasattr(self, '_subjects_where_user_is_subjectadmin'):
+            subjects = []
+            for subjectpermissiongroup in SubjectPermissionGroup.objects\
+                    .filter(permissiongroup__users=self.request.user)\
+                    .select_related('subject')\
+                    .order_by('subject__long_name')\
+                    .distinct():
+                subjects.append(subjectpermissiongroup.subject)
+            self._subjects_where_user_is_subjectadmin = subjects
+        return self._subjects_where_user_is_subjectadmin
+
+    def __get_all_periods_where_user_is_subjectadmin_or_periodadmin(self):
+        periods = []
+        subjects_where_user_is_subjectadmin = self.__get_all_subjects_where_user_is_subjectadmin()
+        now = timezone.now()
+        for periodpermissiongroup in PeriodPermissionGroup.objects\
+                .filter(
+                    period__start_time__lt=now,
+                    period__end_time__gt=now,
+                )\
+                .filter(
+                    models.Q(permissiongroup__users=self.request.user) |
+                    models.Q(period__parentnode__in=subjects_where_user_is_subjectadmin)
+                )\
+                .select_related('period', 'period__parentnode')\
+                .order_by('period__parentnode__long_name', 'period__start_time')\
+                .distinct():
+            periods.append(periodpermissiongroup.period)
+        return periods
+
+    def get_context_data(self, **kwargs):
+        context = super(Overview, self).get_context_data(**kwargs)
+        context['subjects_where_user_is_subjectadmin'] = \
+            self.__get_all_subjects_where_user_is_subjectadmin()
+        context['periods_where_user_is_subjectadmin_or_periodadmin'] = \
+            self.__get_all_periods_where_user_is_subjectadmin_or_periodadmin()
+        return context
 
 
 class App(crapp.App):
