e4cd5c0b479b31d2ff00631eca647621e179e6ab
Espen A. Kristiansen <post@espenak.net>
2015-11-22 20:14:38 +0100

    admin...assignment.students: Created a students overview page skeleton.

---
+++ devilry/devilry_admin/views/assignment/students/overview.py
@@ -0,0 +1,23 @@
+from __future__ import unicode_literals
+
+from django_cradmin import crapp
+from django_cradmin.viewhelpers.detail import DetailRoleView
+
+from devilry.apps.core import models as coremodels
+
+
+class Overview(DetailRoleView):
+    model = coremodels.Assignment
+    context_object_name = "assignment"
+    template_name = 'devilry_admin/assignment/students/overview.django.html'
+
+    def get_context_data(self, **kwargs):
+        # assignment = self.get_object()
+        context = super(Overview, self).get_context_data(**kwargs)
+        return context
+
+
+class App(crapp.App):
+    appurls = [
+        crapp.Url(r'^$', Overview.as_view(), name=crapp.INDEXVIEW_NAME)
+    ]
