5d18772eb5b437d07ec6613be55aa9a7f4b28a32
StianJul <stianjul@gmail.com>
2015-11-23 16:32:40 +0100

    devilry_gradeform/views/editable_gradeform.py: created class for advanced editable gradeform

--- devilry/devilry_gradeform/views/editable_grade_form.py
+++ devilry/devilry_gradeform/views/editable_grade_form.py
@@ -7,21 +7,21 @@ class AbstractEditableRenderer(renderable.AbstractRenderable):
     """
 
 
-class EditableGradeForm(AbstractEditableRenderer):
-    """
+# class EditableGradeForm(AbstractEditableRenderer):
+#     """
+#
+#     """
 
-    """
+class AdvancedEditableGradeForm(AbstractEditableRenderer):
 
-class AdvancedEditableGradeForm(EditableGradeForm):
-
-    template_name = 'devilry_gradeform/advanced.gradeform.django.html'
+    template_name = "devilry_gradeform/advanced.gradeform.django.html"
 
     def __init__(self, assignment, feedbackset):
         self.assignment = assignment
         self.feedbackset = feedbackset
 
     def get_template_context_object(self, request=None):
-
         context = super(AdvancedEditableGradeForm, self).get_context_data()
+        context['test'] = 'Hello, I\'m template'
 
         return context
