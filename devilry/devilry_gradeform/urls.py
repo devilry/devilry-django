8b67c7787c8b572d90deae494952b70782328385
StianJul <stianjul@gmail.com>
2015-11-24 18:49:24 +0100

    devilry_gradeform/urls.py: not using this at the moment

--- devilry/devilry_gradeform/urls.py
+++ devilry/devilry_gradeform/urls.py
@@ -1,14 +1,12 @@
 from django.conf.urls import patterns
 from django.conf.urls import url
-from devilry.apps.core.models import Assignment
 from devilry.devilry_gradeform.views.grade_form import AbstractGradeForm, AdvancedGradeForm
-from devilry.devilry_group.models import FeedbackSet
 
 from views import grade_form
 
-urlpatterns = patterns(
-    'devilry.devilry_gradeform',
-    url(r'^advanced_editable',
-        grade_form.AdvancedGradeForm.render_editable(AdvancedGradeForm(), None, None),
-        name='devilry-gradeform-advanced-editable'),
-)
+# urlpatterns = patterns(
+#     'devilry.devilry_gradeform',
+#     url(r'^advanced_editable',
+#         grade_form.AdvancedGradeForm.render_editable(AdvancedGradeForm(), None, None),
+#         name='devilry-gradeform-advanced-editable'),
+# )
