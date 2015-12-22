985ba0b99c0c1287fdd289ea7c1d65af9da8ab13
Espen A. Kristiansen <post@espenak.net>
2015-12-22 16:00:45 +0100

    theme3|theme2: Moved devilry_theme2_tags into devilry_theme3, and renamed it devilry_theme3_tags.

--- devilry/devilry_theme3/templatetags/devilry_theme3_tags.py
+++ devilry/devilry_theme3/templatetags/devilry_theme3_tags.py
@@ -3,7 +3,7 @@ from django import template
 register = template.Library()
 
 
-@register.inclusion_tag('devilry_theme2/templatetags/user-verbose-inline.django.html')
+@register.inclusion_tag('devilry_theme3/templatetags/user-verbose-inline.django.html')
 def devilry_user_verbose_inline(user):
     """
     Returns the user wrapped in HTML formatting tags perfect for showing
@@ -13,7 +13,8 @@ def devilry_user_verbose_inline(user):
         'user': user
     }
 
-@register.inclusion_tag('devilry_theme2/templatetags/user-verbose-inline.django.html')
+
+@register.inclusion_tag('devilry_theme3/templatetags/user-verbose-inline.django.html')
 def devilry_status_verbose_inline(status):
     """
     Returns the status wrapped in HTML formatting tags perfect for showing
