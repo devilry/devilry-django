4c00752bbf3ff91ead337dd8e2b70c48437b9a5e
Espen A. Kristiansen <post@espenak.net>
2015-12-28 13:15:24 +0100

    admin...students.overview: Tests for the choices.

--- devilry/devilry_admin/tests/assignment/students/test_overview.py
+++ devilry/devilry_admin/tests/assignment/students/test_overview.py
@@ -1,3 +1,4 @@
+import mock
 from django.test import TestCase
 from django_cradmin import cradmin_testhelpers
 from model_mommy import mommy
@@ -21,6 +22,94 @@ class TestOverview(TestCase, cradmin_testhelpers.TestCaseMixin):
         self.assertEqual(
             'Students on Test Assignment',
             mockresponse.selector.one('h1').alltext_normalized)
+
+    def test_choices_sanity(self):
+        testassignment = mommy.make('core.Assignment')
+        mockresponse = self.mock_http200_getrequest_htmls(cradmin_role=testassignment)
+        self.assertEqual(
+            3,
+            mockresponse.selector.count(
+                    '#devilry_admin_assignment_students_overview_choices li'))
+
+    def test_choice_add_groups_link(self):
+        testassignment = mommy.make('core.Assignment')
+        mock_cradmin_instance = mock.MagicMock()
+
+        def mock_reverse_url(appname, viewname, **kwargs):
+            return '/{}/{}'.format(appname, viewname)
+
+        mock_cradmin_instance.reverse_url = mock_reverse_url
+        mockresponse = self.mock_http200_getrequest_htmls(cradmin_role=testassignment,
+                                                          cradmin_instance=mock_cradmin_instance)
+        self.assertEqual(
+            '/create_assignmentgroups/INDEX',
+            mockresponse.selector
+            .one('#devilry_admin_assignment_students_overview_choice_add_groups a')['href'])
+
+    def test_choice_add_groups_text(self):
+        testassignment = mommy.make('core.Assignment',
+                                    parentnode__short_name='testperiod',
+                                    parentnode__parentnode__short_name='testsubject')
+        mockresponse = self.mock_http200_getrequest_htmls(cradmin_role=testassignment)
+        self.assertEqual(
+            'Add students',
+            mockresponse.selector
+            .one('#devilry_admin_assignment_students_overview_choice_add_groups a')
+            .alltext_normalized)
+
+    def test_choice_merge_groups_link(self):
+        testassignment = mommy.make('core.Assignment')
+        mock_cradmin_instance = mock.MagicMock()
+
+        def mock_reverse_url(appname, viewname, **kwargs):
+            return '/{}/{}'.format(appname, viewname)
+
+        mock_cradmin_instance.reverse_url = mock_reverse_url
+        mockresponse = self.mock_http200_getrequest_htmls(cradmin_role=testassignment,
+                                                          cradmin_instance=mock_cradmin_instance)
+        self.assertEqual(
+            # '/merge_groups/INDEX',
+            '#',
+            mockresponse.selector
+            .one('#devilry_admin_assignment_students_overview_choice_merge_groups a')['href'])
+
+    def test_choice_merge_groups_text(self):
+        testassignment = mommy.make('core.Assignment',
+                                    parentnode__short_name='testperiod',
+                                    parentnode__parentnode__short_name='testsubject')
+        mockresponse = self.mock_http200_getrequest_htmls(cradmin_role=testassignment)
+        self.assertEqual(
+            'Organize students in project groups',
+            mockresponse.selector
+            .one('#devilry_admin_assignment_students_overview_choice_merge_groups a')
+            .alltext_normalized)
+
+    def test_choice_delete_groups_link(self):
+        testassignment = mommy.make('core.Assignment')
+        mock_cradmin_instance = mock.MagicMock()
+
+        def mock_reverse_url(appname, viewname, **kwargs):
+            return '/{}/{}'.format(appname, viewname)
+
+        mock_cradmin_instance.reverse_url = mock_reverse_url
+        mockresponse = self.mock_http200_getrequest_htmls(cradmin_role=testassignment,
+                                                          cradmin_instance=mock_cradmin_instance)
+        self.assertEqual(
+            # '/delete_groups/INDEX',
+            '#',
+            mockresponse.selector
+            .one('#devilry_admin_assignment_students_overview_choice_delete_groups a')['href'])
+
+    def test_choice_delete_groups_text(self):
+        testassignment = mommy.make('core.Assignment',
+                                    parentnode__short_name='testperiod',
+                                    parentnode__parentnode__short_name='testsubject')
+        mockresponse = self.mock_http200_getrequest_htmls(cradmin_role=testassignment)
+        self.assertEqual(
+            'Delete students or project groups',
+            mockresponse.selector
+            .one('#devilry_admin_assignment_students_overview_choice_delete_groups a')
+            .alltext_normalized)
 
     def test_grouplist_no_groups(self):
         testassignment = mommy.make('core.Assignment')
