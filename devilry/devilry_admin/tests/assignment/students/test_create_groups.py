ef185afa87eaa499264a5744d50b37ddfe3c1417
Espen A. Kristiansen <post@espenak.net>
2016-01-02 23:49:41 +0100

    admin...create_groups: All the code for rendering the ChooseMethod view including tests.

--- devilry/devilry_admin/tests/assignment/students/test_create_groups.py
+++ devilry/devilry_admin/tests/assignment/students/test_create_groups.py
@@ -1,12 +1,14 @@
+import datetime
 from django.test import TestCase
 from django_cradmin import cradmin_testhelpers
 from model_mommy import mommy
 
+from devilry.apps.core.mommy_recipes import ACTIVE_PERIOD_START
 from devilry.devilry_admin.views.assignment.students import create_groups
 
 
-class TestOverview(TestCase, cradmin_testhelpers.TestCaseMixin):
-    viewclass = create_groups.Overview
+class TestChooseMethod(TestCase, cradmin_testhelpers.TestCaseMixin):
+    viewclass = create_groups.ChooseMethod
 
     def test_title(self):
         testassignment = mommy.make('core.Assignment',
@@ -25,53 +27,93 @@ class TestOverview(TestCase, cradmin_testhelpers.TestCaseMixin):
             'Add students',
             mockresponse.selector.one('h1').alltext_normalized)
 
-    def test_header_lead_text(self):
-        testassignment = mommy.make('core.Assignment')
-        mockresponse = self.mock_http200_getrequest_htmls(cradmin_role=testassignment)
-        self.assertEqual(
-            'How would you like to add students?',
-            mockresponse.selector.one('.django-cradmin-page-header-inner .lead').alltext_normalized)
-
     def test_choices_sanity(self):
         testassignment = mommy.make('core.Assignment')
         mockresponse = self.mock_http200_getrequest_htmls(cradmin_role=testassignment)
         self.assertEqual(
             2,
-            mockresponse.selector.count(
-                    '#devilry_admin_assignment_students_create_groups_overview_choices li'))
+            mockresponse.selector.count('select#id_method option'))
 
-    def test_choice_all_link(self):
+    def test_choice_all_value(self):
         testassignment = mommy.make('core.Assignment')
         mockresponse = self.mock_http200_getrequest_htmls(cradmin_role=testassignment)
         self.assertEqual(
-            '#',
-            mockresponse.selector
-            .one('#devilry_admin_assignment_students_create_groups_overview_choice_all a')['href'])
+            'all-from-period',
+            mockresponse.selector.one('select#id_method option:first-child')['value'])
 
-    def test_choice_all_text(self):
+    def test_choice_all_label(self):
         testassignment = mommy.make('core.Assignment',
-                                    parentnode__short_name='testperiod',
-                                    parentnode__parentnode__short_name='testsubject')
+                                    parentnode__parentnode__short_name='testsubject',
+                                    parentnode__short_name='testperiod')
         mockresponse = self.mock_http200_getrequest_htmls(cradmin_role=testassignment)
         self.assertEqual(
-            'Add all students registered on testsubject.testperiod',
-            mockresponse.selector
-            .one('#devilry_admin_assignment_students_create_groups_overview_choice_all a')
-            .alltext_normalized)
+            'All students registered on testsubject.testperiod',
+            mockresponse.selector.one('select#id_method option:first-child').alltext_normalized)
 
-    def test_choice_select_link(self):
+    def test_choice_select_manually_value(self):
         testassignment = mommy.make('core.Assignment')
         mockresponse = self.mock_http200_getrequest_htmls(cradmin_role=testassignment)
         self.assertEqual(
-            '#',
-            mockresponse.selector
-            .one('#devilry_admin_assignment_students_create_groups_overview_choice_select a')['href'])
+            'select-manually',
+            mockresponse.selector.one('select#id_method option:last-child')['value'])
 
-    def test_choice_select_text(self):
+    def test_choice_select_manually_label(self):
         testassignment = mommy.make('core.Assignment')
         mockresponse = self.mock_http200_getrequest_htmls(cradmin_role=testassignment)
         self.assertEqual(
-            'Select students manually',
-            mockresponse.selector
-            .one('#devilry_admin_assignment_students_create_groups_overview_choice_select a')
-            .alltext_normalized)
+            'Select manually',
+            mockresponse.selector.one('select#id_method option:last-child').alltext_normalized)
+
+    def test_choices_does_not_include_current_assignment(self):
+        testperiod = mommy.make('core.Period')
+        mommy.make('core.Assignment', parentnode=testperiod)
+        testassignment = mommy.make('core.Assignment', parentnode=testperiod)
+        mockresponse = self.mock_http200_getrequest_htmls(cradmin_role=testassignment)
+        self.assertEqual(
+            4,
+            mockresponse.selector.count('select#id_method option'))
+        self.assertFalse(
+            mockresponse.selector.exists('select#id_method option[value="copy-passing-from-assignment-{}"]'.format(
+                    testassignment.pk)))
+        self.assertFalse(
+            mockresponse.selector.exists('select#id_method option[value="copy-all-from-assignment-{}"]'.format(
+                    testassignment.pk)))
+
+    def test_other_assignment_rending(self):
+        testperiod = mommy.make('core.Period')
+        otherassignment = mommy.make('core.Assignment', parentnode=testperiod,
+                                     long_name='Other Assignment')
+        testassignment = mommy.make('core.Assignment', parentnode=testperiod)
+        mockresponse = self.mock_http200_getrequest_htmls(cradmin_role=testassignment)
+        self.assertEqual(
+            'Students with passing grade on Other Assignment',
+            mockresponse.selector.one('select#id_method option[value="copy-passing-from-assignment-{}"]'.format(
+                    otherassignment.pk)).alltext_normalized)
+        self.assertEqual(
+            'All students registered on Other Assignment',
+            mockresponse.selector.one('select#id_method option[value="copy-all-from-assignment-{}"]'.format(
+                    otherassignment.pk)).alltext_normalized)
+
+    def test_other_assignments_ordering(self):
+        testperiod = mommy.make_recipe('devilry.apps.core.period_active')
+        mommy.make('core.Assignment', parentnode=testperiod,
+                   long_name='Assignment 1',
+                   publishing_time=ACTIVE_PERIOD_START + datetime.timedelta(days=1))
+        mommy.make('core.Assignment', parentnode=testperiod,
+                   long_name='Assignment 2',
+                   publishing_time=ACTIVE_PERIOD_START + datetime.timedelta(days=2))
+        mommy.make('core.Assignment', parentnode=testperiod,
+                   long_name='Assignment 3',
+                   publishing_time=ACTIVE_PERIOD_START + datetime.timedelta(days=3))
+        assignment4 = mommy.make('core.Assignment', parentnode=testperiod)
+        mockresponse = self.mock_http200_getrequest_htmls(cradmin_role=assignment4)
+        optgroup_labels = [optgroup['label']
+                           for optgroup in mockresponse.selector.list('select#id_method optgroup')]
+        self.assertEqual(
+            [
+                'Copy from Assignment 3',
+                'Copy from Assignment 2',
+                'Copy from Assignment 1',
+            ],
+            optgroup_labels
+        )
--- devilry/devilry_admin/views/assignment/students/create_groups.py
+++ devilry/devilry_admin/views/assignment/students/create_groups.py
@@ -1,12 +1,92 @@
-from django.views.generic import TemplateView
+from crispy_forms import layout
+from django import forms
+from django.http import HttpResponseRedirect
+from django.utils.translation import pgettext_lazy
 from django_cradmin import crapp
+from django_cradmin.crispylayouts import PrimarySubmit
+from django_cradmin.viewhelpers import formbase
 
 
-class Overview(TemplateView):
-    template_name = 'devilry_admin/assignment/students/create_groups/overview.django.html'
+class ChooseMethod(formbase.FormView):
+    template_name = 'devilry_admin/assignment/students/create_groups/choose-method.django.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        self.assignment = self.request.cradmin_role
+        self.period = self.assignment.parentnode
+        return super(ChooseMethod, self).dispatch(request, *args, **kwargs)
+
+    def __make_copy_from_assignment_choice(self, assignment):
+        copy_passing_value = 'copy-passing-from-assignment-{}'.format(assignment.id)
+        copy_passing_label = pgettext_lazy('admin create_groups',
+                                           'Students with passing grade on %(assignment)s') % {
+            'assignment': assignment.long_name
+        }
+        copy_all_value = 'copy-all-from-assignment-{}'.format(assignment.id)
+        copy_all_label = pgettext_lazy('admin create_groups',
+                                       'All students registered on %(assignment)s') % {
+            'assignment': assignment.long_name
+        }
+        return (
+            pgettext_lazy('admin create_groups', 'Copy from %(assignment)s') % {
+                'assignment': assignment.long_name
+            },
+            (
+                (copy_passing_value, copy_passing_label),
+                (copy_all_value, copy_all_label),
+            )
+        )
+
+    def get_other_assignments_in_period_method_choices(self):
+        assignments = self.period.assignments\
+            .order_by('-publishing_time')\
+            .exclude(pk=self.assignment.pk)
+        return [self.__make_copy_from_assignment_choice(assignment)
+                for assignment in assignments]
+
+    def get_method_choices(self):
+        choices = [
+            ('all-from-period', pgettext_lazy(
+                    'admin create_groups',
+                    'All students registered on %(semester)s') % {'semester': self.period.get_path()}),
+            ('select-manually', pgettext_lazy(
+                    'admin create_groups',
+                    'Select manually'))
+        ]
+        choices.extend(self.get_other_assignments_in_period_method_choices())
+        return choices
+
+    def get_form_class(self):
+        method_choices = self.get_method_choices()
+
+        class SelectMethodForm(forms.Form):
+            method = forms.ChoiceField(
+                required=True,
+                label=pgettext_lazy('devilry_admin create_groups', 'How would you like to add students?'),
+                initial=method_choices[0][0],
+                choices=method_choices
+            )
+
+        return SelectMethodForm
+
+    def get_field_layout(self):
+        return [
+            layout.Div(
+                'method',
+                css_class='cradmin-globalfields')
+        ]
+
+    def get_buttons(self):
+        return [
+            PrimarySubmit('add-students',
+                          pgettext_lazy('admin create_groups', 'Add students')),
+        ]
+
+    def form_valid(self, form):
+        # ... do something with the form ...
+        return HttpResponseRedirect('/some/view')
 
 
 class App(crapp.App):
     appurls = [
-        crapp.Url(r'^$', Overview.as_view(), name=crapp.INDEXVIEW_NAME)
+        crapp.Url(r'^$', ChooseMethod.as_view(), name=crapp.INDEXVIEW_NAME)
     ]
