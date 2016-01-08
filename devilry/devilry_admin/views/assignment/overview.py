1450ed2d9b0217bd574ffd6a651919ec82656b8f
Magne Westlie <magne@appresso.no>
2016-01-08 16:26:46 +0100

    Some first changes to assignment overview.

--- devilry/devilry_admin/views/assignment/overview.py
+++ devilry/devilry_admin/views/assignment/overview.py
@@ -11,13 +11,32 @@ class Overview(DetailRoleView):
     context_object_name = "assignment"
     template_name = 'devilry_admin/assignment/overview.django.html'
 
+    def get_administrators_count(self):
+        # len(self.assignment.get_all_admins()) is deprecated
+        return 10
+
+    def get_candidates_count(self):
+        return coremodels.Candidate.objects.filter(assignment_group__parentnode=self.assignment).count()
+
+    def get_examiners_count(self):
+        examiners_count = 0
+        for group in self.assignment.assignmentgroups.all():
+            examiners_count += group.examiners.count()
+        return examiners_count
+    
+    def get_assignmentgroups_count(self):
+        return self.assignment.assignmentgroups.count()
+
+    def dispatch(self, request, *args, **kwargs):
+        self.assignment = self.get_object()
+        return super(Overview, self).dispatch(request, *args, **kwargs)
+
     def get_context_data(self, **kwargs):
-        assignment = self.get_object()
         context = super(Overview, self).get_context_data(**kwargs)
-        context['assignmentgroups_count'] = assignment.assignmentgroups.count()
-        context['candidates_count'] = coremodels.Candidate.objects.filter(
-            assignment_group__parentnode=assignment).count()
-        context['administrator_count'] = len(assignment.get_all_admins())
+        context['assignmentgroups_count'] = self.get_assignmentgroups_count()
+        context['candidates_count'] = self.get_assignmentgroups_count()
+        context['administrator_count'] = self.get_administrators_count()
+        context['examiners_count'] = self.get_examiners_count()
         return context
 
 
