930baa3d844ad461328c966c44fa31cfac795bab
Espen A. Kristiansen <post@espenak.net>
2016-01-12 11:00:33 +0100

    core...examiner|candidate: Removed automatic_anonymous_id - migration.
    See #847.

---
+++ devilry/apps/core/migrations/0014_auto_20160112_1052.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0013_auto_20160111_2021'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='candidate',
+            name='automatic_anonymous_id',
+        ),
+        migrations.RemoveField(
+            model_name='examiner',
+            name='automatic_anonymous_id',
+        ),
+    ]
