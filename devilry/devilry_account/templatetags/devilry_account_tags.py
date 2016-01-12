b19c68da1f6f1a0568ae8578e9d65158976f5961
Espen A. Kristiansen <post@espenak.net>
2016-01-12 15:37:39 +0100

    theme3|account: Moved the user-related template tags from theme3 into devilry_account_tags.

---
+++ devilry/devilry_account/templatetags/devilry_account_tags.py
@@ -0,0 +1,27 @@
+from django import template
+
+register = template.Library()
+
+
+@register.inclusion_tag('devilry_account/templatetags/user-verbose-inline.django.html')
+def devilry_user_verbose_inline(user):
+    """
+    Returns the user wrapped in HTML formatting tags perfect for showing
+    the user inline.
+    """
+    return {
+        'user': user
+    }
+
+
+@register.inclusion_tag('devilry_account/templatetags/users-verbose-inline.django.html')
+def devilry_users_verbose_inline(users):
+    """
+    Returns the provided iterable of user objects HTML formatted.
+
+    Perfect for formatting lists of users inline, such as when showing examiners
+    or candidates on a group.
+    """
+    return {
+        'users': users
+    }
--- devilry/devilry_theme3/templatetags/devilry_theme3_tags.py
+++ devilry/devilry_theme3/templatetags/devilry_theme3_tags.py
@@ -4,30 +4,6 @@ register = template.Library()
 
 
 @register.inclusion_tag('devilry_theme3/templatetags/user-verbose-inline.django.html')
-def devilry_user_verbose_inline(user):
-    """
-    Returns the user wrapped in HTML formatting tags perfect for showing
-    the user inline.
-    """
-    return {
-        'user': user
-    }
-
-
-@register.inclusion_tag('devilry_theme3/templatetags/users-verbose-inline.django.html')
-def devilry_users_verbose_inline(users):
-    """
-    Returns the provided iterable of user objects HTML formatted.
-
-    Perfect for formatting lists of users inline, such as when showing examiners
-    or candidates on a group.
-    """
-    return {
-        'users': users
-    }
-
-
-@register.inclusion_tag('devilry_theme3/templatetags/user-verbose-inline.django.html')
 def devilry_status_verbose_inline(status):
     """
     Returns the status wrapped in HTML formatting tags perfect for showing
