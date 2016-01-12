8e229a687e9c2250bdd1692368e6e63cbe929964
Espen A. Kristiansen <post@espenak.net>
2016-01-12 19:25:24 +0100

    examiner: Updated for new anonymization methods in core.

--- devilry/devilry_examiner/tests/test_cradminextensions/test_devilry_crmenu_examiner.py
+++ devilry/devilry_examiner/tests/test_cradminextensions/test_devilry_crmenu_examiner.py
@@ -2,31 +2,12 @@ import mock
 from django import test
 from model_mommy import mommy
 
+from devilry.apps.core.models import Assignment
 from devilry.devilry_examiner.cradminextensions import devilry_crmenu_examiner
 
 
 class TestMenu(test.TestCase):
-    def test_get_group_label_no_candidates(self):
-        testgroup = mommy.make('core.AssignmentGroup')
-        mock_cradmin_instance = mock.MagicMock()
-        menu = devilry_crmenu_examiner.Menu(cradmin_instance=mock_cradmin_instance)
-        self.assertEqual(
-            '#{}'.format(testgroup.id),
-            menu.get_group_label(group=testgroup))
-
-    def test_get_group_label_nonanonymous_fullname(self):
-        testgroup = mommy.make('core.AssignmentGroup')
-        mommy.make('core.Candidate',
-                   assignment_group=testgroup,
-                   relatedstudent__user__shortname='testuser',
-                   relatedstudent__user__fullname='Test User')
-        mock_cradmin_instance = mock.MagicMock()
-        menu = devilry_crmenu_examiner.Menu(cradmin_instance=mock_cradmin_instance)
-        self.assertEqual(
-            'Test User',
-            menu.get_group_label(group=testgroup))
-
-    def test_get_group_label_nonanonymous_shortname(self):
+    def test_get_group_label_nonanonymous(self):
         testgroup = mommy.make('core.AssignmentGroup')
         mommy.make('core.Candidate',
                    assignment_group=testgroup,
@@ -38,30 +19,15 @@ class TestMenu(test.TestCase):
             'testuser',
             menu.get_group_label(group=testgroup))
 
-    def test_get_group_label_nonanonymous_multiple_candidates(self):
-        testgroup = mommy.make('core.AssignmentGroup')
+    def test_get_group_label_anonymous(self):
+        testgroup = mommy.make('core.AssignmentGroup',
+                               parentnode__anonymizationmode=Assignment.ANONYMIZATIONMODE_SEMI_ANONYMOUS)
         mommy.make('core.Candidate',
                    assignment_group=testgroup,
-                   relatedstudent__user__shortname='testusera',
-                   relatedstudent__user__fullname='A Test User')
-        mommy.make('core.Candidate',
-                   assignment_group=testgroup,
-                   relatedstudent__user__shortname='testuserb',
-                   relatedstudent__user__fullname='')
+                   relatedstudent__user__shortname='testuser',
+                   relatedstudent__automatic_anonymous_id='secret')
         mock_cradmin_instance = mock.MagicMock()
         menu = devilry_crmenu_examiner.Menu(cradmin_instance=mock_cradmin_instance)
         self.assertEqual(
-            {'A Test User', 'testuserb'},
-            set(menu.get_group_label(group=testgroup).split(', ')))
-
-    # def test_get_group_label_anonymous(self):
-    #     testgroup = mommy.make('core.AssignmentGroup')
-    #     mommy.make('core.Candidate',
-    #                assignment_group=testgroup,
-    #                relatedstudent__user__shortname='testuser',
-    #                relatedstudent__user__fullname='Test User')
-    #     mock_cradmin_instance = mock.MagicMock()
-    #     menu = devilry_crmenu_examiner.Menu(cradmin_instance=mock_cradmin_instance)
-    #     self.assertEqual(
-    #         'Test User',
-    #         menu.get_group_label(group=testgroup))
+            'secret',
+            menu.get_group_label(group=testgroup))
