fd5e5a48072f303cffe3b50a7c72e4fd5e083a92
Magne Westlie <magne@appresso.no>
2016-01-14 15:23:06 +0100

    core: Added migration that renamed the `user` field on class `Examiner` (this field should not be used any more).

--- devilry/apps/core/models/examiner.py
+++ devilry/apps/core/models/examiner.py
@@ -26,7 +26,7 @@ class Examiner(models.Model, AbstractIsAdmin):
         db_table = 'core_assignmentgroup_examiners'
 
     #: Will be removed in 3.0 - see https://github.com/devilry/devilry-django/issues/812
-    user = models.ForeignKey(User)
+    old_reference_not_in_use_user = models.ForeignKey(User)
 
     #: The :class:`devilry.apps.core.models.assignment_group.AssignmentGroup`
     #: where this examiner belongs.
