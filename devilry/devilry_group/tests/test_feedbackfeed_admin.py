259bcc5cf626329f8e14ff63ea2a03c68ef974e9
StianJul <stianjul@gmail.com>
2016-01-14 10:45:41 +0100

    devilry_group/tests/test_crinstance_admin.py: tests for admin crinstance.

--- devilry/devilry_group/tests/test_feedbackfeed_admin.py
+++ devilry/devilry_group/tests/test_feedbackfeed_admin.py
@@ -3,5 +3,8 @@ from devilry.devilry_group.tests import test_feedbackfeed_common
 from devilry.devilry_group.views import feedbackfeed_admin
 
 
-# class TestFeedbackfeedAdmin(TestCase, test_feedbackfeed_common.TestFeedbackFeedMixin):
-#     viewclass = feedbackfeed_admin.AdminFeedbackFeedView
+class TestFeedbackfeedAdmin(TestCase, test_feedbackfeed_common.TestFeedbackFeedMixin):
+    viewclass = feedbackfeed_admin.AdminFeedbackFeedView
+
+    def test_get(self):
+        pass
