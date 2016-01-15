617a9c69c38d9682ecd0c7270786a4c88a89aef8
StianJul <stianjul@gmail.com>
2016-01-15 13:51:14 +0100

    devilry_group/tests/feedbackfeed/test_feedbackfeed_examiner: testing that examiner can see buttons for adding feedback and discuss.

--- devilry/devilry_group/tests/feedbackfeed/test_feedbackfeed_examiner.py
+++ devilry/devilry_group/tests/feedbackfeed/test_feedbackfeed_examiner.py
@@ -18,6 +18,17 @@ class TestFeedbackfeedExaminer(TestCase, test_feedbackfeed_common.TestFeedbackFe
         self.assertEquals(mockresponse.selector.one('title').alltext_normalized,
                           examiner.assignmentgroup.assignment.get_path())
 
+    def test_get_feedbackfeed_examiner_can_see_feedback_and_discuss_in_header(self):
+        assignment = mommy.make_recipe('devilry.apps.core.assignment_activeperiod_start')
+        group = mommy.make('core.AssignmentGroup', parentnode=assignment)
+        examiner = mommy.make('core.Examiner',
+                               assignmentgroup=group,
+                               relatedexaminer=mommy.make('core.RelatedExaminer'))
+        mockresponse = self.mock_http200_getrequest_htmls(cradmin_role=examiner.assignmentgroup,
+                                                          requestuser=examiner.relatedexaminer.user)
+        self.assertTrue(mockresponse.selector.exists('.devilry-group-feedbackfeed-feedback-button'))
+        self.assertTrue(mockresponse.selector.exists('.devilry-group-feedbackfeed-discuss-button'))
+
     def test_get_feedbackfeed_examiner_wysiwyg_get_comment_choise_add_comment_for_examiners_and_admins_button(self):
         examiner = mommy.make('core.Examiner')
         feedbackset = mommy.make('devilry_group.FeedbackSet', group=examiner.assignmentgroup)
