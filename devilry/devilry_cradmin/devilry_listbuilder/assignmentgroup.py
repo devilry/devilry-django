6dd4f48687940773646534085d5c6b1ac6275209
Espen A. Kristiansen <post@espenak.net>
2016-01-17 23:55:23 +0100

    devilry_cradmin...listbuilder.assignmentgroup: Handle rendering of examiners.

--- devilry/devilry_cradmin/devilry_listbuilder/assignmentgroup.py
+++ devilry/devilry_cradmin/devilry_listbuilder/assignmentgroup.py
@@ -3,15 +3,47 @@ from django_cradmin.viewhelpers import listbuilder
 from devilry.apps.core.models import Assignment
 
 
-class ItemValue(listbuilder.itemvalue.TitleDescription):
+class FullyAnonymousSubjectAdminItemValue(listbuilder.itemvalue.TitleDescription):
+    """
+    This item value renderer is for fully anonymous assignments
+    with the "subjectadmin" devilryrole. It does not include anything
+    that can link the student to an anonymized student in the examiner
+    UI (in case the admin is also examiner). It basically only
+    renders the name of the students in each group.
+
+    See :devilryissue:`846` for more information.
+    """
+    valuealias = 'group'
+    template_name = 'devilry_cradmin/devilry_listbuilder/assignmentgroup/' \
+                    'fully-anonymous-subjectadmin-group-item-value.django.html'
+
+    def __init__(self, *args, **kwargs):
+        super(FullyAnonymousSubjectAdminItemValue, self).__init__(*args, **kwargs)
+        if self.group.assignment.anonymizationmode != Assignment.ANONYMIZATIONMODE_FULLY_ANONYMOUS:
+            raise ValueError('Can only use FullyAnonymousSubjectAdminItemValue for fully anonymous assignments. '
+                             'Use SubjectAdminItemValue istead.')
+
+    def get_all_candidate_users(self):
+        return [candidate.relatedstudent.user
+                for candidate in self.group.candidates.all()]
+
+
+class AbstractItemValue(listbuilder.itemvalue.TitleDescription):
     valuealias = 'group'
     template_name = 'devilry_cradmin/devilry_listbuilder/assignmentgroup/group-item-value.django.html'
+
+    def __init__(self, *args, **kwargs):
+        super(AbstractItemValue, self).__init__(*args, **kwargs)
+        self._examiners = list(self.group.examiners.all())
 
     def get_devilryrole(self):
         raise NotImplementedError()
 
-    def get_candidate_users(self):
-        return [candidate.relatedstudent.user for candidate in self.group.candidates.all()]
+    def should_include_examiners(self):
+        return True
+
+    def get_examiners(self):
+        return self._examiners
 
     # def get_extra_css_classes_list(self):
     #     css_classes = ['devilry-examiner-listbuilder-assignmentlist-assignmentitemvalue']
@@ -22,19 +54,22 @@ class ItemValue(listbuilder.itemvalue.TitleDescription):
     #     return css_classes
 
 
-class StudentItemValue(ItemValue):
+class StudentItemValue(AbstractItemValue):
     def get_devilryrole(self):
         return 'student'
 
 
-class ExaminerItemValue(ItemValue):
+class ExaminerItemValue(AbstractItemValue):
     def get_devilryrole(self):
         return 'examiner'
 
+    def should_include_examiners(self):
+        return self.kwargs.get('include_examiners', False)
 
-class AbstractAdminItemValue(ItemValue):
+
+class AbstractAdminItemValue(AbstractItemValue):
     """
-    Base class for the admin roles ItemValue classes.
+    Base class for the admin roles AbstractItemValue classes.
     """
 
 
@@ -63,30 +98,3 @@ class SubjectAdminItemValue(AbstractAdminItemValue):
 class DepartmentAdminItemValue(AbstractAdminItemValue):
     def get_devilryrole(self):
         return 'departmentadmin'
-
-
-class FullyAnonymousSubjectAdminItemValue(AbstractAdminItemValue):
-    """
-    This item value renderer is for fully anonymous assignments
-    with the "subjectadmin" devilryrole. It does not include anything
-    that can link the student to an anonymized student in the examiner
-    UI (in case the admin is also examiner). It basically only
-    renders the name of the students in each group.
-
-    See :devilryissue:`846` for more information.
-    """
-    template_name = 'devilry_cradmin/devilry_listbuilder/assignmentgroup/' \
-                    'fully-anonymous-subjectadmin-group-item-value.django.html'
-
-    def __init__(self, *args, **kwargs):
-        super(FullyAnonymousSubjectAdminItemValue, self).__init__(*args, **kwargs)
-        if self.group.assignment.anonymizationmode != Assignment.ANONYMIZATIONMODE_FULLY_ANONYMOUS:
-            raise ValueError('Can only use FullyAnonymousSubjectAdminItemValue for fully anonymous assignments. '
-                             'Use SubjectAdminItemValue istead.')
-
-    def get_devilryrole(self):
-        return 'subjectadmin'
-
-    def get_all_candidate_users(self):
-        return [candidate.relatedstudent.user
-                for candidate in self.group.candidates.all()]
--- devilry/devilry_cradmin/tests/test_devilry_listbuilder/test_assignmentgroup.py
+++ devilry/devilry_cradmin/tests/test_devilry_listbuilder/test_assignmentgroup.py
@@ -6,8 +6,38 @@ from devilry.apps.core.models import Assignment
 from devilry.devilry_cradmin import devilry_listbuilder
 
 
+class TestFullyAnonymousSubjectAdminItemValue(test.TestCase):
+    def test_non_anonymous_not_allowed(self):
+        testgroup = mommy.make('core.AssignmentGroup')
+        with self.assertRaisesMessage(ValueError,
+                                      'Can only use FullyAnonymousSubjectAdminItemValue for fully '
+                                      'anonymous assignments. Use SubjectAdminItemValue istead.'):
+            devilry_listbuilder.assignmentgroup.FullyAnonymousSubjectAdminItemValue(value=testgroup)
+
+    def test_semi_anonymous_is_not_allowed(self):
+        testgroup = mommy.make('core.AssignmentGroup',
+                               parentnode__anonymizationmode=Assignment.ANONYMIZATIONMODE_SEMI_ANONYMOUS)
+        with self.assertRaisesMessage(ValueError,
+                                      'Can only use FullyAnonymousSubjectAdminItemValue for fully '
+                                      'anonymous assignments. Use SubjectAdminItemValue istead.'):
+            devilry_listbuilder.assignmentgroup.FullyAnonymousSubjectAdminItemValue(value=testgroup)
+
+    def test_name_fully_anonymous_is_not_anoymized(self):
+        testgroup = mommy.make('core.AssignmentGroup',
+                               parentnode__anonymizationmode=Assignment.ANONYMIZATIONMODE_FULLY_ANONYMOUS)
+        mommy.make('core.Candidate',
+                   assignment_group=testgroup,
+                   relatedstudent__user__fullname='Test User',
+                   relatedstudent__user__shortname='testuser@example.com')
+        selector = htmls.S(devilry_listbuilder.assignmentgroup.FullyAnonymousSubjectAdminItemValue(
+            value=testgroup).render())
+        self.assertEqual(
+            'Test User(testuser@example.com)',
+            selector.one('.django-cradmin-listbuilder-itemvalue-titledescription-title').alltext_normalized)
+
+
 class TestStudentItemValue(test.TestCase):
-    def test_title(self):
+    def test_name(self):
         testgroup = mommy.make('core.AssignmentGroup')
         mommy.make('core.Candidate',
                    assignment_group=testgroup,
@@ -18,7 +48,7 @@ class TestStudentItemValue(test.TestCase):
             'Test User(testuser@example.com)',
             selector.one('.django-cradmin-listbuilder-itemvalue-titledescription-title').alltext_normalized)
 
-    def test_title_semi_anonymous_is_not_anoymized(self):
+    def test_name_semi_anonymous_is_not_anoymized(self):
         testgroup = mommy.make('core.AssignmentGroup',
                                parentnode__anonymizationmode=Assignment.ANONYMIZATIONMODE_SEMI_ANONYMOUS)
         mommy.make('core.Candidate',
@@ -30,7 +60,7 @@ class TestStudentItemValue(test.TestCase):
             'Test User(testuser@example.com)',
             selector.one('.django-cradmin-listbuilder-itemvalue-titledescription-title').alltext_normalized)
 
-    def test_title_fully_anonymous_is_not_anoymized(self):
+    def test_name_fully_anonymous_is_not_anoymized(self):
         testgroup = mommy.make('core.AssignmentGroup',
                                parentnode__anonymizationmode=Assignment.ANONYMIZATIONMODE_FULLY_ANONYMOUS)
         mommy.make('core.Candidate',
@@ -44,7 +74,7 @@ class TestStudentItemValue(test.TestCase):
 
 
 class TestExaminerItemValue(test.TestCase):
-    def test_title(self):
+    def test_name(self):
         testgroup = mommy.make('core.AssignmentGroup')
         mommy.make('core.Candidate',
                    assignment_group=testgroup,
@@ -55,7 +85,7 @@ class TestExaminerItemValue(test.TestCase):
             'Test User(testuser@example.com)',
             selector.one('.django-cradmin-listbuilder-itemvalue-titledescription-title').alltext_normalized)
 
-    def test_title_semi_anonymous_is_anonymized(self):
+    def test_name_semi_anonymous_is_anonymized(self):
         testgroup = mommy.make('core.AssignmentGroup',
                                parentnode__anonymizationmode=Assignment.ANONYMIZATIONMODE_SEMI_ANONYMOUS)
         mommy.make('core.Candidate',
@@ -66,7 +96,7 @@ class TestExaminerItemValue(test.TestCase):
             'MyAnonymousID',
             selector.one('.django-cradmin-listbuilder-itemvalue-titledescription-title').alltext_normalized)
 
-    def test_title_fully_anonymous_is_anonymized(self):
+    def test_name_fully_anonymous_is_anonymized(self):
         testgroup = mommy.make('core.AssignmentGroup',
                                parentnode__anonymizationmode=Assignment.ANONYMIZATIONMODE_FULLY_ANONYMOUS)
         mommy.make('core.Candidate',
@@ -77,9 +107,20 @@ class TestExaminerItemValue(test.TestCase):
             'MyAnonymousID',
             selector.one('.django-cradmin-listbuilder-itemvalue-titledescription-title').alltext_normalized)
 
+    def test_examiners_include_examiners_false(self):
+        testgroup = mommy.make('core.AssignmentGroup')
+        mommy.make('core.Examiner',
+                   assignmentgroup=testgroup,
+                   relatedexaminer__user__fullname='Test User',
+                   relatedexaminer__user__shortname='testuser@example.com')
+        selector = htmls.S(devilry_listbuilder.assignmentgroup.ExaminerItemValue(
+            value=testgroup, include_examiners=False).render())
+        self.assertFalse(selector.exists('.devilry-cradmin-groupitemvalue-examiners'))
+        self.assertFalse(selector.exists('.devilry-cradmin-groupitemvalue-examiners-wrapper'))
+
 
 class TestPeriodAdminItemValue(test.TestCase):
-    def test_title(self):
+    def test_name(self):
         testgroup = mommy.make('core.AssignmentGroup')
         mommy.make('core.Candidate',
                    assignment_group=testgroup,
@@ -100,7 +141,7 @@ class TestPeriodAdminItemValue(test.TestCase):
 
 
 class TestSubjectAdminItemValue(test.TestCase):
-    def test_title(self):
+    def test_name(self):
         testgroup = mommy.make('core.AssignmentGroup')
         mommy.make('core.Candidate',
                    assignment_group=testgroup,
@@ -111,7 +152,7 @@ class TestSubjectAdminItemValue(test.TestCase):
             'Test User(testuser@example.com)',
             selector.one('.django-cradmin-listbuilder-itemvalue-titledescription-title').alltext_normalized)
 
-    def test_title_semi_anonymous_is_not_anonymized(self):
+    def test_name_semi_anonymous_is_not_anonymized(self):
         testgroup = mommy.make('core.AssignmentGroup',
                                parentnode__anonymizationmode=Assignment.ANONYMIZATIONMODE_SEMI_ANONYMOUS)
         mommy.make('core.Candidate',
@@ -133,7 +174,7 @@ class TestSubjectAdminItemValue(test.TestCase):
 
 
 class TestDepartmentAdminItemValue(test.TestCase):
-    def test_title(self):
+    def test_name(self):
         testgroup = mommy.make('core.AssignmentGroup')
         mommy.make('core.Candidate',
                    assignment_group=testgroup,
@@ -144,7 +185,7 @@ class TestDepartmentAdminItemValue(test.TestCase):
             'Test User(testuser@example.com)',
             selector.one('.django-cradmin-listbuilder-itemvalue-titledescription-title').alltext_normalized)
 
-    def test_title_semi_anonymous_is_not_anoymized(self):
+    def test_name_semi_anonymous_is_not_anoymized(self):
         testgroup = mommy.make('core.AssignmentGroup',
                                parentnode__anonymizationmode=Assignment.ANONYMIZATIONMODE_SEMI_ANONYMOUS)
         mommy.make('core.Candidate',
@@ -156,7 +197,7 @@ class TestDepartmentAdminItemValue(test.TestCase):
             'Test User(testuser@example.com)',
             selector.one('.django-cradmin-listbuilder-itemvalue-titledescription-title').alltext_normalized)
 
-    def test_title_fully_anonymous_is_not_anoymized(self):
+    def test_name_fully_anonymous_is_not_anoymized(self):
         testgroup = mommy.make('core.AssignmentGroup',
                                parentnode__anonymizationmode=Assignment.ANONYMIZATIONMODE_FULLY_ANONYMOUS)
         mommy.make('core.Candidate',
@@ -169,31 +210,41 @@ class TestDepartmentAdminItemValue(test.TestCase):
             selector.one('.django-cradmin-listbuilder-itemvalue-titledescription-title').alltext_normalized)
 
 
-class TestFullyAnonymousSubjectAdminItemValue(test.TestCase):
-    def test_non_anonymous_not_allowed(self):
+class MockItemValue(devilry_listbuilder.assignmentgroup.AbstractItemValue):
+    def get_devilryrole(self):
+        return 'student'  # Should not affect any of the tests that uses this class
+
+
+class TestItemValue(test.TestCase):
+    def test_examiners(self):
         testgroup = mommy.make('core.AssignmentGroup')
-        with self.assertRaisesMessage(ValueError,
-                                      'Can only use FullyAnonymousSubjectAdminItemValue for fully '
-                                      'anonymous assignments. Use SubjectAdminItemValue istead.'):
-            devilry_listbuilder.assignmentgroup.FullyAnonymousSubjectAdminItemValue(value=testgroup)
-
-    def test_semi_anonymous_is_not_allowed(self):
-        testgroup = mommy.make('core.AssignmentGroup',
-                               parentnode__anonymizationmode=Assignment.ANONYMIZATIONMODE_SEMI_ANONYMOUS)
-        with self.assertRaisesMessage(ValueError,
-                                      'Can only use FullyAnonymousSubjectAdminItemValue for fully '
-                                      'anonymous assignments. Use SubjectAdminItemValue istead.'):
-            devilry_listbuilder.assignmentgroup.FullyAnonymousSubjectAdminItemValue(value=testgroup)
-
-    def test_title_fully_anonymous_is_not_anoymized(self):
-        testgroup = mommy.make('core.AssignmentGroup',
-                               parentnode__anonymizationmode=Assignment.ANONYMIZATIONMODE_FULLY_ANONYMOUS)
-        mommy.make('core.Candidate',
-                   assignment_group=testgroup,
-                   relatedstudent__user__fullname='Test User',
-                   relatedstudent__user__shortname='testuser@example.com')
-        selector = htmls.S(devilry_listbuilder.assignmentgroup.FullyAnonymousSubjectAdminItemValue(
-            value=testgroup).render())
+        mommy.make('core.Examiner',
+                   assignmentgroup=testgroup,
+                   relatedexaminer__user__fullname='Test User',
+                   relatedexaminer__user__shortname='testuser@example.com')
+        selector = htmls.S(MockItemValue(value=testgroup).render())
         self.assertEqual(
             'Test User(testuser@example.com)',
-            selector.one('.django-cradmin-listbuilder-itemvalue-titledescription-title').alltext_normalized)
+            selector.one('.devilry-cradmin-groupitemvalue-examiners-names').alltext_normalized)
+
+    def test_examiners_semi_anonymous(self):
+        testgroup = mommy.make('core.AssignmentGroup',
+                               parentnode__anonymizationmode=Assignment.ANONYMIZATIONMODE_SEMI_ANONYMOUS)
+        mommy.make('core.Examiner',
+                   assignmentgroup=testgroup,
+                   relatedexaminer__automatic_anonymous_id='MyAnonymousID')
+        selector = htmls.S(MockItemValue(value=testgroup).render())
+        self.assertEqual(
+            'MyAnonymousID',
+            selector.one('.devilry-cradmin-groupitemvalue-examiners-names').alltext_normalized)
+
+    def test_examiners_fully_anonymous(self):
+        testgroup = mommy.make('core.AssignmentGroup',
+                               parentnode__anonymizationmode=Assignment.ANONYMIZATIONMODE_FULLY_ANONYMOUS)
+        mommy.make('core.Examiner',
+                   assignmentgroup=testgroup,
+                   relatedexaminer__automatic_anonymous_id='MyAnonymousID')
+        selector = htmls.S(MockItemValue(value=testgroup).render())
+        self.assertEqual(
+            'MyAnonymousID',
+            selector.one('.devilry-cradmin-groupitemvalue-examiners-names').alltext_normalized)
