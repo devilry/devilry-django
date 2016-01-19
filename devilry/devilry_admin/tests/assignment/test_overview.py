5263fc656e2ac3ed7100916462acb2af1f8d89bc
Tor Ivar Johansen <tor@torjohansen.no>
2016-01-19 23:19:45 +0100

    devilry_admin: Optimized imports

--- devilry/devilry_admin/tests/assignment/test_overview.py
+++ devilry/devilry_admin/tests/assignment/test_overview.py
@@ -1,13 +1,11 @@
-from datetime import datetime, timedelta
+from datetime import datetime
+
 from django.test import TestCase
 from django_cradmin import cradmin_testhelpers
 from model_mommy import mommy
 
 from devilry.apps.core.models import Assignment
-from devilry.apps.core.mommy_recipes import assignment_activeperiod_end
-from devilry.apps.core import models as coremodels
 from devilry.devilry_admin.views.assignment import overview
-from devilry.project.develop.testhelpers.corebuilder import AssignmentBuilder, UserBuilder, PeriodBuilder
 
 
 class TestOverviewApp(TestCase, cradmin_testhelpers.TestCaseMixin):
