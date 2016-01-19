b816fbeea5a8d36f56f7e9217ec3d0e5ad600efd
Espen A. Kristiansen <post@espenak.net>
2016-01-19 05:52:46 +0100

    devilry_comment...CommentFile: Added created_datetime field and some docs.

---
+++ devilry/devilry_comment/migrations/0004_commentfile_created_datetime.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.utils.timezone
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('devilry_comment', '0003_auto_20160109_1239'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='commentfile',
+            name='created_datetime',
+            field=models.DateTimeField(default=django.utils.timezone.now),
+        ),
+    ]
