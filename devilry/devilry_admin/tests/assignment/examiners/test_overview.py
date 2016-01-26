8ee461518b294b8e97255808b6bec22f1617813e
Espen A. Kristiansen <post@espenak.net>
2016-01-26 10:31:35 +0100

    admin...assignment.examiners.overview: Fixed the links to RelatedExaminer overview.

--- devilry/devilry_admin/tests/assignment/examiners/test_overview.py
+++ devilry/devilry_admin/tests/assignment/examiners/test_overview.py
@@ -3,6 +3,7 @@ from django import test
 from django.conf import settings
 from django.http import Http404
 from django_cradmin import cradmin_testhelpers
+from django_cradmin.crinstance import reverse_cradmin_url
 from model_mommy import mommy
 
 from devilry.apps.core.models import Assignment
@@ -107,7 +108,7 @@ class TestOverview(test.TestCase, cradmin_testhelpers.TestCaseMixin):
             .one('#devilry_admin_assignment_examiners_overview_button_random_organize_examiners')
             .alltext_normalized)
 
-    def test_examinerlist_no_relatedexaminers(self):
+    def test_examinerlist_no_relatedexaminers_sanity(self):
         testuser = mommy.make(settings.AUTH_USER_MODEL)
         testassignment = mommy.make_recipe('devilry.apps.core.assignment_activeperiod_start')
         mockresponse = self.mock_http200_getrequest_htmls(
@@ -118,7 +119,42 @@ class TestOverview(test.TestCase, cradmin_testhelpers.TestCaseMixin):
             '#devilry_admin_assignment_examiners_overview_no_relatedexaminers'))
         self.assertFalse(mockresponse.selector.exists('#django_cradmin_listbuilderview_listwrapper'))
 
-    def test_has_relatedexaminers(self):
+    def test_examinerlist_no_relatedexaminers_text(self):
+        testuser = mommy.make(settings.AUTH_USER_MODEL)
+        testassignment = mommy.make_recipe('devilry.apps.core.assignment_activeperiod_start',
+                                           parentnode__parentnode__short_name='testsubject',
+                                           parentnode__short_name='testperiod')
+        mockresponse = self.mock_http200_getrequest_htmls(
+            cradmin_role=testassignment,
+            cradmin_instance=self.__mockinstance_with_devilryrole('departmentadmin'),
+            requestuser=testuser)
+        self.assertEqual(
+            'You have no users registered as examiner for testsubject.testperiod. You need to '
+            'add users as examiners on the semester page for the course before you can use '
+            'them as examiners for assignments.',
+            mockresponse.selector.one(
+                '#devilry_admin_assignment_examiners_overview_no_relatedexaminers p').alltext_normalized)
+        self.assertEqual(
+            'Add examiners',
+            mockresponse.selector.one(
+                '#devilry_admin_assignment_examiners_overview_no_relatedexaminers a').alltext_normalized)
+
+    def test_examinerlist_no_relatedexaminers_url(self):
+        testuser = mommy.make(settings.AUTH_USER_MODEL)
+        testassignment = mommy.make_recipe('devilry.apps.core.assignment_activeperiod_start')
+        mockresponse = self.mock_http200_getrequest_htmls(
+            cradmin_role=testassignment,
+            cradmin_instance=self.__mockinstance_with_devilryrole('departmentadmin'),
+            requestuser=testuser)
+        self.assertEqual(
+            reverse_cradmin_url(
+                instanceid='devilry_admin_periodadmin',
+                appname='examiners',
+                roleid=testassignment.period.id),
+            mockresponse.selector.one(
+                '#devilry_admin_assignment_examiners_overview_no_relatedexaminers a')['href'])
+
+    def test_has_relatedexaminers_sanity(self):
         testuser = mommy.make(settings.AUTH_USER_MODEL)
         testassignment = mommy.make_recipe('devilry.apps.core.assignment_activeperiod_start')
         mommy.make('core.RelatedExaminer', period=testassignment.period, _quantity=5)
@@ -130,6 +166,38 @@ class TestOverview(test.TestCase, cradmin_testhelpers.TestCaseMixin):
         self.assertEqual(5, mockresponse.selector.count('.django-cradmin-listbuilder-itemvalue'))
         self.assertFalse(mockresponse.selector.exists(
             '#devilry_admin_assignment_examiners_overview_no_relatedexaminers'))
+
+    def test_listbuilderlist_footer_text(self):
+        testuser = mommy.make(settings.AUTH_USER_MODEL)
+        testassignment = mommy.make_recipe('devilry.apps.core.assignment_activeperiod_start',
+                                           parentnode__parentnode__short_name='testsubject',
+                                           parentnode__short_name='testperiod')
+        mommy.make('core.RelatedExaminer', period=testassignment.period)
+        mockresponse = self.mock_http200_getrequest_htmls(
+            cradmin_role=testassignment,
+            cradmin_instance=self.__mockinstance_with_devilryrole('departmentadmin'),
+            requestuser=testuser)
+        self.assertEqual(
+            'Only users registered as examiner for testsubject.testperiod is available '
+            'as examiners for assignments. Add more examiners.',
+            mockresponse.selector.one(
+                '.devilry-listbuilderlist-footer').alltext_normalized)
+
+    def test_listbuilderlist_footer_url(self):
+        testuser = mommy.make(settings.AUTH_USER_MODEL)
+        testassignment = mommy.make_recipe('devilry.apps.core.assignment_activeperiod_start')
+        mommy.make('core.RelatedExaminer', period=testassignment.period)
+        mockresponse = self.mock_http200_getrequest_htmls(
+            cradmin_role=testassignment,
+            cradmin_instance=self.__mockinstance_with_devilryrole('departmentadmin'),
+            requestuser=testuser)
+        self.assertEqual(
+            reverse_cradmin_url(
+                instanceid='devilry_admin_periodadmin',
+                appname='examiners',
+                roleid=testassignment.period.id),
+            mockresponse.selector.one(
+                '.devilry-listbuilderlist-footer a')['href'])
 
     #
     #
