85126b331c528bc8b85eabaffb6a29eb843ab4cf
Espen A. Kristiansen <post@espenak.net>
2016-01-26 23:35:20 +0100

    admin...assignment.examiners.add_groups_to_examiner: Generalized the common examiner detail view stuff in a superclass.

--- devilry/devilry_admin/views/assignment/examiners/add_groups_to_examiner.py
+++ devilry/devilry_admin/views/assignment/examiners/add_groups_to_examiner.py
@@ -1,12 +1,12 @@
 from __future__ import unicode_literals
 
 from django.contrib import messages
-from django.http import Http404
 from django.shortcuts import redirect
 from django.utils.translation import ugettext_lazy, ungettext_lazy
 from django_cradmin import crapp
 
-from devilry.apps.core.models import RelatedExaminer, Examiner
+from devilry.apps.core.models import Examiner
+from devilry.devilry_admin.views.assignment.examiners import base_single_examinerview
 from devilry.devilry_admin.views.assignment.students import groupview_base
 from devilry.devilry_cradmin import devilry_listbuilder
 
@@ -16,49 +16,26 @@ class TargetRenderer(devilry_listbuilder.assignmentgroup.GroupTargetRenderer):
         return ugettext_lazy('Add students')
 
 
-class AddGroupsToExaminerView(groupview_base.BaseMultiselectView):
+class AddGroupsToExaminerView(groupview_base.BaseMultiselectView,
+                              base_single_examinerview.SingleExaminerViewMixin):
     template_name = 'devilry_admin/assignment/examiners/add_groups_to_examiner.django.html'
-
-    def dispatch(self, request, *args, **kwargs):
-        self.relatedexaminer = self.__get_relatedexaminer()
-        return super(AddGroupsToExaminerView, self).dispatch(request, *args, **kwargs)
-
-    def __get_relatedexaminer_id(self):
-        return self.kwargs['relatedexaminer_id']
-
-    def __get_relatedexaminer_queryset(self):
-        assignment = self.request.cradmin_role
-        period = assignment.period
-        queryset = RelatedExaminer.objects\
-            .filter(period=period)\
-            .select_related('user')\
-            .annotate_with_number_of_groups_on_assignment(assignment=assignment)\
-            .extra_annotate_with_number_of_candidates_on_assignment(assignment=assignment)\
-            .exclude(active=False)
-        return queryset
-
-    def __get_relatedexaminer(self):
-        try:
-            return self.__get_relatedexaminer_queryset().get(id=self.__get_relatedexaminer_id())
-        except RelatedExaminer.DoesNotExist:
-            raise Http404()
 
     def get_target_renderer_class(self):
         return TargetRenderer
 
     def get_filterlist_url(self, filters_string):
         return self.request.cradmin_app.reverse_appurl(
-            'organize-manually',
+            'add-groups-to-examiner',
             kwargs={'filters_string': filters_string,
-                    'relatedexaminer_id': self.__get_relatedexaminer_id()})
+                    'relatedexaminer_id': self.get_relatedexaminer_id()})
 
     def get_unfiltered_queryset_for_role(self, role):
         return super(AddGroupsToExaminerView, self).get_unfiltered_queryset_for_role(role=role)\
-            .exclude(examiners__relatedexaminer=self.relatedexaminer)
+            .exclude(examiners__relatedexaminer=self.get_relatedexaminer())
 
     def get_context_data(self, **kwargs):
         context = super(AddGroupsToExaminerView, self).get_context_data(**kwargs)
-        context['relatedexaminer'] = self.relatedexaminer
+        context['relatedexaminer'] = self.get_relatedexaminer()
         return context
 
     def get_success_message(self, groupcount, candidatecount):
@@ -82,7 +59,7 @@ class AddGroupsToExaminerView(groupview_base.BaseMultiselectView):
         candidatecount = 0
         for group in groupqueryset:
             examiner = Examiner(assignmentgroup=group,
-                                relatedexaminer=self.relatedexaminer)
+                                relatedexaminer=self.get_relatedexaminer())
             examiners.append(examiner)
             groupcount += 1
             candidatecount += len(group.candidates.all())
@@ -101,5 +78,5 @@ class App(crapp.App):
     appurls = [
         crapp.Url(r'^(?P<relatedexaminer_id>\d+)/(?P<filters_string>.+)?$',
                   AddGroupsToExaminerView.as_view(),
-                  name='organize-manually'),
+                  name='add-groups-to-examiner'),
     ]
