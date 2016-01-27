a9b8a0e0a512c37bd159c6c4151487c632b98fce
Espen A. Kristiansen <post@espenak.net>
2016-01-27 02:15:12 +0100

    admin...assignment.examiners.bulk_organize.random: Working POST and most of GET logic.

--- devilry/devilry_admin/tests/assignment/examiners/test_bulk_organize.py
+++ devilry/devilry_admin/tests/assignment/examiners/test_bulk_organize.py
@@ -3,6 +3,7 @@ from django import test
 from django_cradmin import cradmin_testhelpers
 from model_mommy import mommy
 
+from devilry.apps.core.models import Examiner
 from devilry.devilry_admin.views.assignment.examiners import bulk_organize
 
 
@@ -35,18 +36,17 @@ class TestSelectMethodView(test.TestCase, cradmin_testhelpers.TestCaseMixin):
     def test_buttonbar_organize_examiners_link(self):
         testassignment = mommy.make('core.Assignment')
         mommy.make('core.RelatedExaminer', period=testassignment.period)
-        mock_cradmin_instance = mock.MagicMock()
+        mock_cradmin_app = mock.MagicMock()
 
-        def mock_reverse_url(appname, viewname, **kwargs):
-            return '/{}/{}'.format(appname, viewname)
+        def mock_reverse_url(viewname, *args, **kwargs):
+            return '/{}'.format(viewname)
 
-        mock_cradmin_instance.reverse_url = mock_reverse_url
+        mock_cradmin_app.reverse_appurl = mock_reverse_url
         mockresponse = self.mock_http200_getrequest_htmls(
             cradmin_role=testassignment,
-            cradmin_instance=mock_cradmin_instance)
+            cradmin_app=mock_cradmin_app)
         self.assertEqual(
-            # '/bulk_organize_examiners/INDEX',
-            '#',
+            '/random',
             mockresponse.selector
             .one('#devilry_admin_assignment_examiners_bulk_organize_button_random')['href'])
 
@@ -60,3 +60,119 @@ class TestSelectMethodView(test.TestCase, cradmin_testhelpers.TestCaseMixin):
             mockresponse.selector
             .one('#devilry_admin_assignment_examiners_bulk_organize_button_random')
             .alltext_normalized)
+
+
+class TestRandomView(test.TestCase, cradmin_testhelpers.TestCaseMixin):
+    """
+    NOTE: Much of the functionality for this view is tested in
+    test_groupview_base.test_groupviewmixin.TestGroupViewMixin
+    and test_basemultiselectview.TestBaseMultiselectView.
+    """
+    viewclass = bulk_organize.RandomView
+
+    def __mockinstance_with_devilryrole(self, devilryrole):
+        mockinstance = mock.MagicMock()
+        mockinstance.get_devilryrole_for_requestuser.return_value = devilryrole
+        return mockinstance
+
+    def test_title(self):
+        testassignment = mommy.make('core.Assignment')
+        mockresponse = self.mock_http200_getrequest_htmls(
+            cradmin_role=testassignment,
+            cradmin_instance=self.__mockinstance_with_devilryrole('departmentadmin'))
+        self.assertIn(
+            'Organize examiners randomly',
+            mockresponse.selector.one('title').alltext_normalized)
+
+    def test_h1(self):
+        testassignment = mommy.make('core.Assignment')
+        mockresponse = self.mock_http200_getrequest_htmls(
+            cradmin_role=testassignment,
+            cradmin_instance=self.__mockinstance_with_devilryrole('departmentadmin'))
+        self.assertEqual(
+            'Organize examiners randomly',
+            mockresponse.selector.one('h1').alltext_normalized)
+
+    def test_groups_sanity(self):
+        testassignment = mommy.make_recipe('devilry.apps.core.assignment_activeperiod_start')
+        mommy.make('core.AssignmentGroup', parentnode=testassignment, _quantity=3)
+        mockresponse = self.mock_http200_getrequest_htmls(
+            cradmin_role=testassignment,
+            cradmin_instance=self.__mockinstance_with_devilryrole('departmentadmin'))
+        self.assertEqual(
+            3,
+            mockresponse.selector.count('.django-cradmin-listbuilder-itemvalue'))
+
+    def test_submit_button_text(self):
+        testassignment = mommy.make_recipe('devilry.apps.core.assignment_activeperiod_start')
+        mockresponse = self.mock_http200_getrequest_htmls(
+            cradmin_role=testassignment,
+            cradmin_instance=self.__mockinstance_with_devilryrole('departmentadmin'))
+        self.assertEqual(
+            'Randomly assign selected students to selected examiners',
+            mockresponse.selector.one('.django-cradmin-multiselect2-target-footer').alltext_normalized)
+
+    def test_target_with_selected_items_title(self):
+        testassignment = mommy.make_recipe('devilry.apps.core.assignment_activeperiod_start')
+        mockresponse = self.mock_http200_getrequest_htmls(
+            cradmin_role=testassignment,
+            cradmin_instance=self.__mockinstance_with_devilryrole('departmentadmin'))
+        self.assertEqual(
+            'Selected students:',
+            mockresponse.selector.one('.django-cradmin-multiselect2-target-title').alltext_normalized)
+
+    def test_post_ok(self):
+        testassignment = mommy.make_recipe('devilry.apps.core.assignment_activeperiod_start')
+        relatedexaminer1 = mommy.make('core.RelatedExaminer', period=testassignment.period)
+        relatedexaminer2 = mommy.make('core.RelatedExaminer', period=testassignment.period)
+        testgroup1 = mommy.make('core.AssignmentGroup', parentnode=testassignment)
+        testgroup2 = mommy.make('core.AssignmentGroup', parentnode=testassignment)
+        self.assertEqual(0, Examiner.objects.count())
+        self.mock_http302_postrequest(
+            cradmin_role=testassignment,
+            cradmin_instance=self.__mockinstance_with_devilryrole('departmentadmin'),
+            requestkwargs={
+                'data': {
+                    'selected_items': [str(testgroup1.id), str(testgroup2.id)],
+                    'selected_relatedexaminers': [str(relatedexaminer1.id), str(relatedexaminer2.id)],
+                }
+            })
+        self.assertEqual(2, Examiner.objects.count())
+
+    def test_post_evenly_distributed(self):
+        testassignment = mommy.make_recipe('devilry.apps.core.assignment_activeperiod_start')
+        relatedexaminer1 = mommy.make('core.RelatedExaminer', period=testassignment.period)
+        relatedexaminer2 = mommy.make('core.RelatedExaminer', period=testassignment.period)
+        relatedexaminer3 = mommy.make('core.RelatedExaminer', period=testassignment.period)
+        testgroup1 = mommy.make('core.AssignmentGroup', parentnode=testassignment)
+        testgroup2 = mommy.make('core.AssignmentGroup', parentnode=testassignment)
+        testgroup3 = mommy.make('core.AssignmentGroup', parentnode=testassignment)
+        testgroup4 = mommy.make('core.AssignmentGroup', parentnode=testassignment)
+        testgroup5 = mommy.make('core.AssignmentGroup', parentnode=testassignment)
+        testgroup6 = mommy.make('core.AssignmentGroup', parentnode=testassignment)
+        testgroup7 = mommy.make('core.AssignmentGroup', parentnode=testassignment)
+        testgroup8 = mommy.make('core.AssignmentGroup', parentnode=testassignment)
+        for index in range(30):
+            self.mock_http302_postrequest(
+                cradmin_role=testassignment,
+                cradmin_instance=self.__mockinstance_with_devilryrole('departmentadmin'),
+                requestkwargs={
+                    'data': {
+                        'selected_items': [str(testgroup1.id), str(testgroup2.id),
+                                           str(testgroup3.id), str(testgroup4.id),
+                                           str(testgroup5.id), str(testgroup5.id),
+                                           str(testgroup6.id), str(testgroup7.id),
+                                           str(testgroup8.id)],
+                        'selected_relatedexaminers': [str(relatedexaminer1.id),
+                                                      str(relatedexaminer2.id),
+                                                      str(relatedexaminer3.id)],
+                    }
+                })
+            count_examiner1 = relatedexaminer1.examiner_set.count()
+            count_examiner2 = relatedexaminer2.examiner_set.count()
+            count_examiner3 = relatedexaminer3.examiner_set.count()
+            self.assertTrue(
+                (count_examiner1 == 2 and count_examiner2 == 3 and count_examiner3 == 3) or
+                (count_examiner1 == 3 and count_examiner2 == 2 and count_examiner3 == 3) or
+                (count_examiner1 == 3 and count_examiner2 == 3 and count_examiner3 == 2)
+            )
--- devilry/devilry_admin/views/assignment/examiners/bulk_organize.py
+++ devilry/devilry_admin/views/assignment/examiners/bulk_organize.py
@@ -1,11 +1,101 @@
 from __future__ import unicode_literals
 
+import math
+import random
+
+from django import forms
+from django.shortcuts import redirect
+from django.utils.translation import ugettext_lazy
 from django.views.generic import TemplateView
 from django_cradmin import crapp
 
+from devilry.apps.core.models import Examiner, RelatedExaminer
+from devilry.devilry_admin.views.assignment.students import groupview_base
+from devilry.devilry_cradmin import devilry_listbuilder
+
 
 class SelectMethodView(TemplateView):
-    template_name = 'devilry_admin/assignment/examiners/bulk_organize.django.html'
+    template_name = 'devilry_admin/assignment/examiners/bulk_organize/select_method.django.html'
+
+
+class RandomOrganizeForm(groupview_base.SelectedGroupsForm):
+    selected_relatedexaminers = forms.ModelMultipleChoiceField(
+        queryset=RelatedExaminer.objects.none()
+    )
+
+    def __init__(self, *args, **kwargs):
+        selectable_relatedexaminers_queryset = kwargs.pop('selectable_relatedexaminers_queryset')
+        super(RandomOrganizeForm, self).__init__(*args, **kwargs)
+        self.fields['selected_relatedexaminers'].queryset = selectable_relatedexaminers_queryset
+
+
+class RandomOrganizeTargetRenderer(devilry_listbuilder.assignmentgroup.GroupTargetRenderer):
+    def get_submit_button_text(self):
+        return ugettext_lazy('Randomly assign selected students to selected examiners')
+
+
+class RandomView(groupview_base.BaseMultiselectView):
+    filterview_name = 'random'
+    template_name = 'devilry_admin/assignment/examiners/bulk_organize/random.django.html'
+
+    def get_target_renderer_class(self):
+        return RandomOrganizeTargetRenderer
+
+    def get_form_class(self):
+        return RandomOrganizeForm
+
+    def __get_relatedexaminerqueryset(self):
+        assignment = self.request.cradmin_role
+        period = assignment.period
+        queryset = RelatedExaminer.objects\
+            .filter(period=period)\
+            .select_related('user')\
+            .exclude(active=False)
+        return queryset
+
+    def get_form_kwargs(self):
+        kwargs = super(RandomView, self).get_form_kwargs()
+        kwargs['selectable_relatedexaminers_queryset'] = self.__get_relatedexaminerqueryset()
+        return kwargs
+
+    # def get_success_message(self, candidatecount):
+    #     return ugettext_lazy('Removed %(count)s students from this assignment.') % {
+    #         'count': candidatecount
+    #     }
+
+    def get_success_url(self):
+        return self.request.cradmin_instance.reverse_url(
+            appname='examineroverview',
+            viewname=crapp.INDEXVIEW_NAME)
+
+    def __clear_examiners(self, groupqueryset):
+        Examiner.objects.filter(assignmentgroup__in=groupqueryset).delete()
+
+    def __random_organize_examiners(self, groupqueryset, relatedexaminerqueryset):
+        relatedexaminers = list(relatedexaminerqueryset)
+        groups = list(groupqueryset)
+        max_per_examiner = int(math.ceil(len(groups) / len(relatedexaminers)))
+        relatedexaminer_to_count_map = {}
+        examiners_to_create = []
+        for group in groupqueryset:
+            relatedexaminer = random.choice(relatedexaminers)
+            if relatedexaminer.id not in relatedexaminer_to_count_map:
+                relatedexaminer_to_count_map[relatedexaminer.id] = 0
+            relatedexaminer_to_count_map[relatedexaminer.id] += 1
+            if relatedexaminer_to_count_map[relatedexaminer.id] > max_per_examiner:
+                relatedexaminers.remove(relatedexaminer)
+            examiner_to_create = Examiner(relatedexaminer=relatedexaminer, assignmentgroup=group)
+            examiners_to_create.append(examiner_to_create)
+        Examiner.objects.bulk_create(examiners_to_create)
+
+    def form_valid(self, form):
+        groupqueryset = form.cleaned_data['selected_items']
+        relatedexaminerqueryset = form.cleaned_data['selected_relatedexaminers']
+        self.__clear_examiners(groupqueryset=groupqueryset)
+        self.__random_organize_examiners(groupqueryset=groupqueryset,
+                                         relatedexaminerqueryset=relatedexaminerqueryset)
+        # messages.success(self.request, self.get_success_message(candidatecount=candidatecount))
+        return redirect(self.get_success_url())
 
 
 class App(crapp.App):
@@ -13,4 +103,7 @@ class App(crapp.App):
         crapp.Url(r'^$',
                   SelectMethodView.as_view(),
                   name=crapp.INDEXVIEW_NAME),
+        crapp.Url(r'^random/(?P<filters_string>.+)?$',
+                  RandomView.as_view(),
+                  name='random'),
     ]
