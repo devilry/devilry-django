be566127575eefab0649449cd3b0e2940af9886d
Espen A. Kristiansen <post@espenak.net>
2016-01-30 22:07:01 +0100

    admin...subject.overview: Use cradmin listbuilder view with search filter.

--- devilry/devilry_admin/tests/subject/test_overview.py
+++ devilry/devilry_admin/tests/subject/test_overview.py
@@ -8,7 +8,7 @@ from devilry.devilry_admin.views.subject import overview
 from devilry.utils import datetimeutils
 
 
-class TestOverviewApp(TestCase, cradmin_testhelpers.TestCaseMixin):
+class TestOverview(TestCase, cradmin_testhelpers.TestCaseMixin):
     viewclass = overview.Overview
 
     def test_title(self):
@@ -53,7 +53,7 @@ class TestOverviewApp(TestCase, cradmin_testhelpers.TestCaseMixin):
         mockresponse = self.mock_http200_getrequest_htmls(cradmin_role=testsubject)
         self.assertEqual('Test Period',
                          mockresponse.selector.one(
-                             '.devilry-admin-period-overview-period-link').alltext_normalized)
+                             '.django-cradmin-listbuilder-itemvalue-titledescription-title').alltext_normalized)
 
     def test_periodlist_itemrendering_url(self):
         testsubject = mommy.make('core.Subject')
@@ -65,7 +65,7 @@ class TestOverviewApp(TestCase, cradmin_testhelpers.TestCaseMixin):
                                                         appname='overview',
                                                         roleid=testperiod.id),
                          mockresponse.selector.one(
-                             '.devilry-admin-period-overview-period-link')['href'])
+                             '.devilry-admin-period-overview-perioditemframe')['href'])
 
     def test_periodlist_itemrendering_start_time(self):
         testsubject = mommy.make('core.Subject')
@@ -74,7 +74,7 @@ class TestOverviewApp(TestCase, cradmin_testhelpers.TestCaseMixin):
             mockresponse = self.mock_http200_getrequest_htmls(cradmin_role=testsubject)
         self.assertEqual(datetimeutils.isoformat_noseconds(ACTIVE_PERIOD_START),
                          mockresponse.selector.one(
-                             '.devilry-admin-period-overview-period-start_time-value').alltext_normalized)
+                             '.devilry-admin-period-overview-period-start-time-value').alltext_normalized)
 
     def test_periodlist_itemrendering_end_time(self):
         testsubject = mommy.make('core.Subject')
@@ -84,7 +84,7 @@ class TestOverviewApp(TestCase, cradmin_testhelpers.TestCaseMixin):
             mockresponse = self.mock_http200_getrequest_htmls(cradmin_role=testsubject)
         self.assertEqual(datetimeutils.isoformat_noseconds(ACTIVE_PERIOD_END),
                          mockresponse.selector.one(
-                             '.devilry-admin-period-overview-period-end_time-value').alltext_normalized)
+                             '.devilry-admin-period-overview-period-end-time-value').alltext_normalized)
 
     def test_periodlist_ordering(self):
         testsubject = mommy.make('core.Subject')
@@ -98,8 +98,10 @@ class TestOverviewApp(TestCase, cradmin_testhelpers.TestCaseMixin):
                           parentnode=testsubject,
                           long_name='Period 3')
         mockresponse = self.mock_http200_getrequest_htmls(cradmin_role=testsubject)
-        periodnames = [element.alltext_normalized
-                       for element in mockresponse.selector.list('.devilry-admin-period-overview-period-link')]
+        periodnames = [
+            element.alltext_normalized
+            for element in mockresponse.selector.list(
+                    '.django-cradmin-listbuilder-itemvalue-titledescription-title')]
         self.assertEqual([
             'Period 3',
             'Period 2',
--- devilry/devilry_admin/views/subject/overview.py
+++ devilry/devilry_admin/views/subject/overview.py
@@ -1,21 +1,61 @@
 from __future__ import unicode_literals
-from django.views.generic import TemplateView
 
 from django_cradmin import crapp
+from django_cradmin.crinstance import reverse_cradmin_url
+from django_cradmin.viewhelpers import listbuilder
+from django_cradmin.viewhelpers import listbuilderview
+from django_cradmin.viewhelpers import listfilter
+
+from devilry.apps.core.models import Period
+from devilry.devilry_cradmin import devilry_listbuilder
 
 
-class Overview(TemplateView):
+class PeriodItemFrame(listbuilder.itemframe.Link):
+    valuealias = 'period'
+
+    def get_url(self):
+        return reverse_cradmin_url(
+            instanceid='devilry_admin_periodadmin',
+            appname='overview',
+            roleid=self.period.id,
+            viewname=crapp.INDEXVIEW_NAME
+        )
+
+    def get_extra_css_classes_list(self):
+        return ['devilry-admin-period-overview-perioditemframe']
+
+
+class Overview(listbuilderview.FilterListMixin, listbuilderview.View):
     template_name = 'devilry_admin/subject/overview.django.html'
+    model = Period
+    frame_renderer_class = PeriodItemFrame
+    value_renderer_class = devilry_listbuilder.period.ItemValue
 
-    def get_context_data(self, **kwargs):
-        context = super(Overview, self).get_context_data(**kwargs)
+    def add_filterlist_items(self, filterlist):
+        filterlist.append(listfilter.django.single.textinput.Search(
+            slug='search',
+            modelfields=[
+                'long_name',
+                'short_name',
+            ],
+            label_is_screenreader_only=True
+        ))
+
+    def get_filterlist_url(self, filters_string):
+        return self.request.cradmin_app.reverse_appurl(
+            'filter',
+            kwargs={'filters_string': filters_string})
+
+    def get_unfiltered_queryset_for_role(self, role):
         subject = self.request.cradmin_role
-        context['subject'] = subject
-        context['periods'] = list(subject.periods.order_by('-start_time'))
-        return context
+        return Period.objects.filter(parentnode=subject)\
+            .order_by('-start_time')
 
 
 class App(crapp.App):
     appurls = [
-        crapp.Url(r'^$', Overview.as_view(), name=crapp.INDEXVIEW_NAME)
+        crapp.Url(r'^$', Overview.as_view(), name=crapp.INDEXVIEW_NAME),
+        crapp.Url(r'^filter/(?P<filters_string>.+)?$',
+                  Overview.as_view(),
+                  name='filter'),
     ]
