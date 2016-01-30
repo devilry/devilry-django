3ad3a846977a62ea353e22ff36796836159d2dd8
Espen A. Kristiansen <post@espenak.net>
2016-01-30 20:47:38 +0100

    admin.cradminextension...listfilter_relatedstudent: No "Order by:" prefix for the filter.

--- devilry/devilry_admin/cradminextensions/listfilter/listfilter_relatedstudent.py
+++ devilry/devilry_admin/cradminextensions/listfilter/listfilter_relatedstudent.py
@@ -7,29 +7,29 @@ from django_cradmin.viewhelpers import listfilter
 class OrderRelatedStudentsFilter(listfilter.django.single.select.AbstractOrderBy):
     def get_ordering_options(self):
         if settings.DJANGO_CRADMIN_USE_EMAIL_AUTH_BACKEND:
-            shortname_ascending_label = ugettext_lazy('Order by: Email')
-            shortname_descending_label = ugettext_lazy('Order by: Email descending')
+            shortname_ascending_label = ugettext_lazy('Email')
+            shortname_descending_label = ugettext_lazy('Email descending')
         else:
-            shortname_ascending_label = ugettext_lazy('Order by: Username')
-            shortname_descending_label = ugettext_lazy('Order by: Username descending')
+            shortname_ascending_label = ugettext_lazy('Username')
+            shortname_descending_label = ugettext_lazy('Username descending')
 
         # NOTE: We use Concat below to get sorting that works even when the user
         #       does not have a fullname, and we use Lower to sort ignoring case.
         return [
             ('', {
-                'label': ugettext_lazy('Order by: Name'),
+                'label': ugettext_lazy('Name'),
                 'order_by': [Lower(Concat('user__fullname', 'user__shortname'))],
             }),
             ('name_descending', {
-                'label': ugettext_lazy('Order by: Name descending'),
+                'label': ugettext_lazy('Name descending'),
                 'order_by': [Lower(Concat('user__fullname', 'user__shortname')).desc()],
             }),
             ('lastname_ascending', {
-                'label': ugettext_lazy('Order by: Last name'),
+                'label': ugettext_lazy('Last name'),
                 'order_by': [Lower('user__lastname')],
             }),
             ('lastname_descending', {
-                'label': ugettext_lazy('Order by: Last name descending'),
+                'label': ugettext_lazy('Last name descending'),
                 'order_by': [Lower('user__lastname').desc()],
             }),
             ('shortname_ascending', {
