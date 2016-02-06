08d8e2e6f42a746b37de9edfa400157971909e88
Espen A. Kristiansen <post@espenak.net>
2016-02-06 13:33:12 +0100

    student: Removed old downnload_deliveryfiles views.
    We have a new view for this.

--- devilry/devilry_student/urls.py
+++ devilry/devilry_student/urls.py
@@ -7,9 +7,6 @@ from devilry.devilry_student.cradmin_group.projectgroupapp import GroupInviteRes
 from devilry.devilry_student.cradmin_student import cradmin_student
 from devilry.devilry_student.cradmin_period import cradmin_period
 from devilry.devilry_student.cradmin_group import cradmin_group
-from .views.download_deliveryfiles import CompressedFileDownloadView
-from .views.download_deliveryfiles import FileDownloadView
-
 
 @login_required
 def redirect_to_student_frontpage_view(request):
@@ -26,13 +23,6 @@ urlpatterns = patterns(
     url(r'^groupinvite/respond/(?P<invite_id>\d+)$',
         login_required(GroupInviteRespondView.as_view()),
         name='devilry_student_groupinvite_respond'),
-
-    url(r'^show-delivery/filedownload/(?P<filemetaid>\d+)$',
-        login_required(FileDownloadView.as_view()),
-        name='devilry-delivery-file-download'),
-    url(r'^show-delivery/compressedfiledownload/(?P<deliveryid>\d+)$',
-        login_required(CompressedFileDownloadView.as_view()),
-        name='devilry-delivery-download-all-zip'),
 
     url(r'^show_delivery/(?P<delivery_id>\d+)$', 'views.show_delivery.show_delivery',
         name='devilry_student_show_delivery'),
