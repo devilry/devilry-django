058417b16152b0c742a6954a507e87495bde6b1e
Espen A. Kristiansen <post@espenak.net>
2016-02-07 12:47:25 +0100

    devilry_cradmin.listbuilder.period.StudentItemValue: Added assignment count to description.
    Needed by #878.

--- devilry/devilry_cradmin/devilry_listbuilder/period.py
+++ devilry/devilry_cradmin/devilry_listbuilder/period.py
@@ -1,7 +1,11 @@
+from __future__ import unicode_literals
 from django_cradmin.viewhelpers import listbuilder
 
 
 class AdminItemValueMixin(object):
+    """
+    Item value for a Period in the admin UI.
+    """
     valuealias = 'period'
     template_name = 'devilry_cradmin/devilry_listbuilder/period/admin-itemvalue.django.html'
 
@@ -16,9 +20,21 @@ class AdminItemValue(AdminItemValueMixin, listbuilder.itemvalue.TitleDescription
     """
     ItemValue renderer for a single period for admins.
     """
+    def get_base_css_classes_list(self):
+        cssclasses = super(AdminItemValue, self).get_base_css_classes_list()
+        cssclasses.append('devilry-cradmin-perioditemvalue-admin')
+        return cssclasses
 
 
 class StudentItemValueMixin(object):
+    """
+    Item value for a Period in the student UI.
+
+    Requires the Period queryset to be annotated with:
+
+    - :meth:`devilry.apps.core.models.PeriodQuerySet.extra_annotate_with_assignmentcount_for_studentuser`.
+    - :meth:`devilry.apps.core.models.PeriodQuerySet.extra_annotate_with_user_qualifies_for_final_exam`.
+    """
     valuealias = 'period'
     template_name = 'devilry_cradmin/devilry_listbuilder/period/student-itemvalue.django.html'
 
@@ -33,3 +49,7 @@ class StudentItemValue(StudentItemValueMixin, listbuilder.itemvalue.TitleDescrip
     """
     ItemValue renderer for a single period for students.
     """
+    def get_base_css_classes_list(self):
+        cssclasses = super(StudentItemValue, self).get_base_css_classes_list()
+        cssclasses.append('devilry-cradmin-perioditemvalue-student')
+        return cssclasses
--- devilry/devilry_cradmin/tests/test_devilry_listbuilder/test_period.py
+++ devilry/devilry_cradmin/tests/test_devilry_listbuilder/test_period.py
@@ -3,13 +3,20 @@ from __future__ import unicode_literals
 
 import htmls
 from django import test
+from django.conf import settings
 from django_cradmin import datetimeutils
 from model_mommy import mommy
 
+from devilry.apps.core.models import Period
 from devilry.devilry_cradmin import devilry_listbuilder
 
 
 class TestAdminItemValue(test.TestCase):
+    def test_custom_cssclass(self):
+        testperiod = mommy.make('core.Period')
+        selector = htmls.S(devilry_listbuilder.period.AdminItemValue(value=testperiod).render())
+        self.assertTrue(selector.exists('.devilry-cradmin-perioditemvalue-admin'))
+
     def test_title(self):
         testperiod = mommy.make('core.Period', long_name='Test Period')
         selector = htmls.S(devilry_listbuilder.period.AdminItemValue(value=testperiod).render())
@@ -28,6 +35,11 @@ class TestAdminItemValue(test.TestCase):
 
 
 class TestStudentItemValue(test.TestCase):
+    def test_custom_cssclass(self):
+        testperiod = mommy.make('core.Period')
+        selector = htmls.S(devilry_listbuilder.period.StudentItemValue(value=testperiod).render())
+        self.assertTrue(selector.exists('.devilry-cradmin-perioditemvalue-student'))
+
     def test_title(self):
         testperiod = mommy.make('core.Period',
                                 parentnode__long_name='Test Subject',
@@ -37,11 +49,49 @@ class TestStudentItemValue(test.TestCase):
                 'Test Subject - Test Period',
                 selector.one('.django-cradmin-listbuilder-itemvalue-titledescription-title').alltext_normalized)
 
-    # def test_description(self):
-    #     testperiod = mommy.make('core.Period',
-    #                             start_time=datetimeutils.default_timezone_datetime(2015, 1, 15),
-    #                             end_time=datetimeutils.default_timezone_datetime(2015, 12, 24))
-    #     selector = htmls.S(devilry_listbuilder.period.StudentItemValue(value=testperiod).render())
-    #     self.assertEqual(
-    #             'Thursday January 15, 2015, 00:00 \u2014 Thursday December 24, 2015, 00:00',
-    #             selector.one('.django-cradmin-listbuilder-itemvalue-titledescription-description').alltext_normalized)
+    def test_description_no_assignments(self):
+        testuser = mommy.make(settings.AUTH_USER_MODEL)
+        testperiod = mommy.make('core.Period')
+        testperiod_annotated = Period.objects\
+            .extra_annotate_with_assignmentcount_for_studentuser(user=testuser)\
+            .get(id=testperiod.id)
+        selector = htmls.S(devilry_listbuilder.period.StudentItemValue(value=testperiod_annotated).render())
+        self.assertEqual(
+                '0 assignments',
+                selector.one('.django-cradmin-listbuilder-itemvalue-titledescription-description').alltext_normalized)
+
+    def test_description_single_assignment(self):
+        testuser = mommy.make(settings.AUTH_USER_MODEL)
+        testperiod = mommy.make('core.Period')
+        relatedstudent = mommy.make('core.RelatedStudent', user=testuser, period=testperiod)
+        testassignment = mommy.make('core.Assignment', parentnode=testperiod)
+        mommy.make('core.Candidate',
+                   assignment_group__parentnode=testassignment,
+                   relatedstudent=relatedstudent)
+        testperiod_annotated = Period.objects\
+            .extra_annotate_with_assignmentcount_for_studentuser(user=testuser)\
+            .get(id=testperiod.id)
+        selector = htmls.S(devilry_listbuilder.period.StudentItemValue(value=testperiod_annotated).render())
+        self.assertEqual(
+                '1 assignment',
+                selector.one('.django-cradmin-listbuilder-itemvalue-titledescription-description').alltext_normalized)
+
+    def test_description_multiple_assignments_assignment(self):
+        testuser = mommy.make(settings.AUTH_USER_MODEL)
+        testperiod = mommy.make('core.Period')
+        relatedstudent = mommy.make('core.RelatedStudent', user=testuser, period=testperiod)
+        testassignment1 = mommy.make('core.Assignment', parentnode=testperiod)
+        mommy.make('core.Candidate',
+                   assignment_group__parentnode=testassignment1,
+                   relatedstudent=relatedstudent)
+        testassignment2 = mommy.make('core.Assignment', parentnode=testperiod)
+        mommy.make('core.Candidate',
+                   assignment_group__parentnode=testassignment2,
+                   relatedstudent=relatedstudent)
+        testperiod_annotated = Period.objects\
+            .extra_annotate_with_assignmentcount_for_studentuser(user=testuser)\
+            .get(id=testperiod.id)
+        selector = htmls.S(devilry_listbuilder.period.StudentItemValue(value=testperiod_annotated).render())
+        self.assertEqual(
+                '2 assignments',
+                selector.one('.django-cradmin-listbuilder-itemvalue-titledescription-description').alltext_normalized)
