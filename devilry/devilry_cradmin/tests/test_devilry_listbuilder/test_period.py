fa456958cd4c121f45774d0ce4e97c1ff8fe8edc
Espen A. Kristiansen <post@espenak.net>
2016-02-07 13:22:13 +0100

    devilry_cradmin.listbuilder.period.StudentItemValue: Added qualified for final exams message.
    Needed by #878.

--- devilry/devilry_cradmin/tests/test_devilry_listbuilder/test_period.py
+++ devilry/devilry_cradmin/tests/test_devilry_listbuilder/test_period.py
@@ -9,6 +9,7 @@ from model_mommy import mommy
 
 from devilry.apps.core.models import Period
 from devilry.devilry_cradmin import devilry_listbuilder
+from devilry.devilry_qualifiesforexam.models import Status
 
 
 class TestAdminItemValue(test.TestCase):
@@ -95,3 +96,52 @@ class TestStudentItemValue(test.TestCase):
         self.assertEqual(
                 '2 assignments',
                 selector.one('.django-cradmin-listbuilder-itemvalue-titledescription-description').alltext_normalized)
+
+    def test_no_qualified_for_final_exam_status(self):
+        testuser = mommy.make(settings.AUTH_USER_MODEL)
+        testperiod = mommy.make('core.Period')
+        testperiod_annotated = Period.objects\
+            .extra_annotate_with_user_qualifies_for_final_exam(user=testuser)\
+            .get(id=testperiod.id)
+        selector = htmls.S(devilry_listbuilder.period.StudentItemValue(value=testperiod_annotated).render())
+        self.assertFalse(selector.exists('.devilry-cradmin-perioditemvalue-student-qualifedforexam'))
+
+    def test_qualified_for_final_exam(self):
+        testuser = mommy.make(settings.AUTH_USER_MODEL)
+        testperiod = mommy.make('core.Period')
+        relatedstudent = mommy.make('core.RelatedStudent', period=testperiod, user=testuser)
+        status = mommy.make('devilry_qualifiesforexam.Status', period=testperiod,
+                            status=Status.ALMOSTREADY)
+        mommy.make('devilry_qualifiesforexam.QualifiesForFinalExam',
+                   relatedstudent=relatedstudent,
+                   status=status,
+                   qualifies=True)
+        testperiod_annotated = Period.objects\
+            .extra_annotate_with_user_qualifies_for_final_exam(user=testuser)\
+            .get(id=testperiod.id)
+        selector = htmls.S(devilry_listbuilder.period.StudentItemValue(value=testperiod_annotated).render())
+        self.assertTrue(selector.exists('.devilry-cradmin-perioditemvalue-student-qualifedforexam-yes'))
+        self.assertFalse(selector.exists('.devilry-cradmin-perioditemvalue-student-qualifedforexam-no'))
+        self.assertEqual(
+                'Qualified for final exam',
+                selector.one('.devilry-cradmin-perioditemvalue-student-qualifedforexam').alltext_normalized)
+
+    def test_not_qualified_for_final_exam(self):
+        testuser = mommy.make(settings.AUTH_USER_MODEL)
+        testperiod = mommy.make('core.Period')
+        relatedstudent = mommy.make('core.RelatedStudent', period=testperiod, user=testuser)
+        status = mommy.make('devilry_qualifiesforexam.Status', period=testperiod,
+                            status=Status.ALMOSTREADY)
+        mommy.make('devilry_qualifiesforexam.QualifiesForFinalExam',
+                   relatedstudent=relatedstudent,
+                   status=status,
+                   qualifies=False)
+        testperiod_annotated = Period.objects\
+            .extra_annotate_with_user_qualifies_for_final_exam(user=testuser)\
+            .get(id=testperiod.id)
+        selector = htmls.S(devilry_listbuilder.period.StudentItemValue(value=testperiod_annotated).render())
+        self.assertFalse(selector.exists('.devilry-cradmin-perioditemvalue-student-qualifedforexam-yes'))
+        self.assertTrue(selector.exists('.devilry-cradmin-perioditemvalue-student-qualifedforexam-no'))
+        self.assertEqual(
+                'NOT qualified for final exam',
+                selector.one('.devilry-cradmin-perioditemvalue-student-qualifedforexam').alltext_normalized)
