e11801997d59eafe9241489616a2d1ffee6de0c5
Espen A. Kristiansen <post@espenak.net>
2016-04-17 03:45:03 +0200

    dbcache: Bulk insert that returns the IDs of the created objects - better tests.
    See #886.

--- devilry/devilry_dbcache/tests/test_bulk_create_queryset_mixin.py
+++ devilry/devilry_dbcache/tests/test_bulk_create_queryset_mixin.py
@@ -6,7 +6,7 @@ from devilry.devilry_dbcache.devilry_dbcache_testapp.models import Person
 
 
 class TestBulkCreateQuerySetMixin(test.TestCase):
-    def test_posgres_bulk_create_and_return_ids_sanity(self):
+    def test_sanity(self):
         bulk_operation = Person.objects.posgres_bulk_create([
             Person(name='test1'),
             Person(name='test2'),
@@ -19,7 +19,15 @@ class TestBulkCreateQuerySetMixin(test.TestCase):
         self.assertTrue(Person.objects.filter(name='test2').exists())
         self.assertTrue(Person.objects.filter(name='test3').exists())
 
-    def test_posgres_bulk_create_and_return_ids_batch_size(self):
+    def test_works_with_defaults(self):
+        bulk_operation = Person.objects.posgres_bulk_create([
+            Person(name='test1'),
+        ])
+        bulk_operation.execute()
+        created_person = Person.objects.get(name='test1')
+        self.assertEqual(20, created_person.age)
+
+    def test_batch_size(self):
         bulk_operation = Person.objects.posgres_bulk_create([
             Person(name='test1'),
             Person(name='test2'),
@@ -27,7 +35,6 @@ class TestBulkCreateQuerySetMixin(test.TestCase):
             Person(name='test4'),
             Person(name='test5'),
         ], batch_size=2)
-        # print bulk_operation.explain(compact=False)
         ids = bulk_operation.execute()
         self.assertEqual(len(ids), 5)
         self.assertTrue(Person.objects.filter(name='test1').exists())
@@ -36,7 +43,7 @@ class TestBulkCreateQuerySetMixin(test.TestCase):
         self.assertTrue(Person.objects.filter(name='test4').exists())
         self.assertTrue(Person.objects.filter(name='test5').exists())
 
-    def test_postgres_bulk_create_and_return_objects(self):
+    def test_return_objects(self):
         bulk_operation = Person.objects.posgres_bulk_create([
             Person(name='test1'),
             Person(name='test2'),
@@ -47,3 +54,9 @@ class TestBulkCreateQuerySetMixin(test.TestCase):
         self.assertEqual(people[0], Person.objects.get(name='test1'))
         self.assertEqual(people[1], Person.objects.get(name='test2'))
         self.assertEqual(people[2], Person.objects.get(name='test3'))
+
+    # def test_performance(self):
+    #     people = [Person(name='test{}'.format(x)) for x in range(10000)]
+    #     bulk_operation = Person.objects.posgres_bulk_create(people)
+    #     bulk_operation.execute()
+    #     self.assertEqual(10000, Person.objects.count())
