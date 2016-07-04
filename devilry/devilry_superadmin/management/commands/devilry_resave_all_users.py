0626a2b163f078c64d9a92fc9f354e9abbcae188
Espen A. Kristiansen <post@espenak.net>
2016-07-04 16:53:50 +0200

    Replaced NoArgsCommand with BaseCommand - required for Django 1.10.

--- devilry/devilry_superadmin/management/commands/devilry_resave_all_users.py
+++ devilry/devilry_superadmin/management/commands/devilry_resave_all_users.py
@@ -1,14 +1,14 @@
 from django.contrib.auth import get_user_model
 from django.db import transaction
-from django.core.management.base import NoArgsCommand
+from django.core.management.base import BaseCommand
 
 
-class Command(NoArgsCommand):
+class Command(BaseCommand):
     help = "Re-save all users. This is useful if you have any apps that listens for " \
            "post_save signals on User, such as 'devilry.apps.autoset_empty_email_by_username'."
 
-    def handle_noargs(self, **options):
-        verbosity = int(options.get('verbosity', '1'))
+    def handle(self, *args, **kwargs):
+        verbosity = int(kwargs.get('verbosity', '1'))
 
         updates = 0
         with transaction.commit_manually():
--- devilry/project/develop/management/commands/devilry_developer_set_all_passwords_to_test.py
+++ devilry/project/develop/management/commands/devilry_developer_set_all_passwords_to_test.py
@@ -1,11 +1,11 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.hashers import make_password
-from django.core.management.base import NoArgsCommand
+from django.core.management.base import BaseCommand
 
 
-class Command(NoArgsCommand):
+class Command(BaseCommand):
     help = 'Set all passwords to "test".'
 
-    def handle_noargs(self, **options):
+    def handle(self, *args, **kwargs):
         user_model = get_user_model()
         user_model.objects.all().update(password=make_password('test'))
