c3ff81f45c78db0e294976c3d08d0c8b83049f72
Eska <eskil.nordland@gmail.com>
2016-07-13 12:52:50 +0200

    devilry_api: fixed typo in authenticate_credentials

--- devilry/devilry_api/auth/authentication.py
+++ devilry/devilry_api/auth/authentication.py
@@ -1,6 +1,7 @@
 # -​*- coding: utf-8 -*​-
 from __future__ import unicode_literals
 
+from django.utils.translation import ugettext_lazy
 from rest_framework import authentication
 
 from devilry.devilry_api.models import APIKey
@@ -8,3 +9,22 @@ from devilry.devilry_api.models import APIKey
 
 class TokenAuthentication(authentication.TokenAuthentication):
     model = APIKey
+
+    def authenticate_credentials(self, key):
+        model = self.get_model()
+
+        try:
+            token = model.objects.select_related('user').get(key=key)
+        except model.DoesNotExist:
+            raise authentication.exceptions.AuthenticationFailed(ugettext_lazy('Invalid token.'))
+
+        if not token.user.is_active:
+            raise authentication.exceptions.AuthenticationFailed(ugettext_lazy('User inactive or deleted.'))
+
+        if token.has_expired:
+            raise authentication.exceptions.AuthenticationFailed(ugettext_lazy('Api key has expired.'))
+
+        if not token.is_active:
+            raise authentication.exceptions.AuthenticationFailed(ugettext_lazy('Api key is not active.'))
+
+        return (token.user, token)
