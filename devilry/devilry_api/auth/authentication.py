c69028ee5588c7b89eab5fc57cf2356d14f145dc
Eska <eskil.nordland@gmail.com>
2016-07-13 14:05:52 +0200

    devilry_api: authentication - if api key is not provided raise AuthenticationFailed

--- devilry/devilry_api/auth/authentication.py
+++ devilry/devilry_api/auth/authentication.py
@@ -10,6 +10,27 @@ from devilry.devilry_api.models import APIKey
 class TokenAuthentication(authentication.TokenAuthentication):
     model = APIKey
 
+    def authenticate(self, request):
+        auth = authentication.get_authorization_header(request).split()
+        if not auth or auth[0].lower() != b'token':
+            msg = ugettext_lazy('Token not provided.')
+            raise authentication.exceptions.AuthenticationFailed(msg)
+
+        if len(auth) == 1:
+            msg = ugettext_lazy('Invalid token header. No credentials provided.')
+            raise authentication.exceptions.AuthenticationFailed(msg)
+        elif len(auth) > 2:
+            msg = ugettext_lazy('Invalid token header. Token string should not contain spaces.')
+            raise authentication.exceptions.AuthenticationFailed(msg)
+
+        try:
+            token = auth[1].decode()
+        except UnicodeError:
+            msg = ugettext_lazy('Invalid token header. Token string should not contain invalid characters.')
+            raise authentication.exceptions.AuthenticationFailed(msg)
+
+        return self.authenticate_credentials(token)
+
     def authenticate_credentials(self, key):
         model = self.get_model()
 
