b1ac70a8cde4e2366a0439c39133a176b0b5d75a
Eska <eskil.nordland@gmail.com>
2016-07-13 19:48:27 +0200

    devilry_api: auth.

---
+++ devilry/devilry_api/permission/role_permission.py
@@ -0,0 +1,2 @@
+from rest_framework.permissions import BasePermission
+
--- devilry/devilry_api/student/views/assignment_views.py
+++ devilry/devilry_api/student/views/assignment_views.py
@@ -1,17 +1,19 @@
 # -​*- coding: utf-8 -*​-
 from __future__ import unicode_literals
 
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from devilry.apps.core.models.assignment_group import AssignmentGroup
-from devilry.devilry_api.auth.authentication import TokenAuthentication
+from devilry.devilry_api.auth.authentication import TokenAuthentication, SessionAuthentication
 from devilry.devilry_api.student.serializers.assignment_serializers import AssignmentGroupModelSerializer
 
 
 class AssignmentGroupListView(APIView):
     serializer_class = AssignmentGroupModelSerializer
-    authentication_classes = (TokenAuthentication, )
+    permission_classes = (IsAuthenticated, )
+    authentication_classes = (TokenAuthentication, SessionAuthentication, )
 
     def get(self, request, *args, **kwargs):
         """
@@ -21,4 +23,3 @@ class AssignmentGroupListView(APIView):
         assignment_groups = AssignmentGroup.objects.filter_user_is_candidate(self.request.user)
         serializer = self.serializer_class(assignment_groups, many=True)
         return Response(serializer.data)
-
