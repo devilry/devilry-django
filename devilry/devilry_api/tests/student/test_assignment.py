279ef6bf49f3d1302976c678fb7156bf81d61dfc
Eska <eskil.nordland@gmail.com>
2016-07-13 21:16:24 +0200

    devilry_api: refactor

--- devilry/devilry_api/tests/student/test_assignment.py
+++ devilry/devilry_api/tests/student/test_assignment.py
@@ -6,9 +6,9 @@ from model_mommy import mommy
 
 from devilry.apps.core import mommy_recipes
 from devilry.devilry_api.student.views import assignment_views
-from devilry.devilry_api.tests.mixins import auth_test_common
+from devilry.devilry_api.tests.mixins import test_auth_common
 
 
-class TestAssignmentGroupListView(auth_test_common.TestAuthAPIKeyMixin, test.TestCase):
+class TestAssignmentGroupListView(test_auth_common.TestAuthAPIKeyMixin, test.TestCase):
     viewclass = assignment_views.AssignmentGroupListView
     route = '/student/assignment-list/'
