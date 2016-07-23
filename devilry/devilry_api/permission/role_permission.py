4e73c1dfbbeb5fb5b7567c1c33fbd21061e99890
Eska <eskil.nordland@gmail.com>
2016-07-23 05:02:09 +0200

    devilry_api/permission: permission class for student and examiner added

--- devilry/devilry_api/permission/role_permission.py
+++ devilry/devilry_api/permission/role_permission.py
@@ -1,2 +1,23 @@
-from rest_framework.permissions import BasePermission
+from rest_framework.permissions import IsAuthenticated
 
+
+class IsStudent(IsAuthenticated):
+    """
+    Permission class for student.
+
+    Ensures that the request.user is authenticated and is student.
+    """
+
+    def has_permission(self, request, view):
+        return super(IsStudent, self).has_permission(request, view) and request.user.user_is_student()
+
+
+class IsExaminer(IsAuthenticated):
+    """
+    Permission class for examiner.
+
+    Ensures that the request.user is authenticated and is examiner.
+    """
+
+    def has_permission(self, request, view):
+        return super(IsExaminer, self).has_permission(request, view) and request.user.user_is_examiner()
