3db81fa71e64a53271be39b0411badb507b83a24
Eska <eskil.nordland@gmail.com>
2016-07-23 05:22:38 +0200

    devilry_api/assignment: updated urls for assignment.

--- devilry/devilry_api/assignment/urls.py
+++ devilry/devilry_api/assignment/urls.py
@@ -4,8 +4,10 @@ from __future__ import unicode_literals
 from django.conf.urls import url
 
 from devilry.devilry_api.assignment.views.assignment_student import (
-    AssignmentListView)
+    AssignmentListView,
+    AssignmentView)
 
 urlpatterns = [
     url('^student/list/$', AssignmentListView.as_view(), name='assigment-list'),
+    url('^student/(?P<subject>.+)/(?P<semester>.+)/(?P<assignment>.+)/', AssignmentView.as_view(), name='assignment'),
 ]
