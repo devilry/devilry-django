47d46bbe58703f03cb6d4958f41ea5333a68a52e
Eska <eskil.nordland@gmail.com>
2016-07-23 05:21:29 +0200

    devilry_api/assignment: commented out old code in serializer

--- devilry/devilry_api/assignment/serializers.py
+++ devilry/devilry_api/assignment/serializers.py
@@ -3,24 +3,24 @@ from __future__ import unicode_literals
 
 from rest_framework import serializers
 
-from devilry.apps.core.models.assignment_group import AssignmentGroup, Assignment
+from devilry.apps.core.models.assignment_group import Assignment
 
 
-class AssignmentGroupModelSerializer(serializers.ModelSerializer):
-    assignment_name = serializers.SerializerMethodField()
-    assignment_gradeform_setup_json = serializers.SerializerMethodField()
-
-    class Meta:
-        model = AssignmentGroup
-        fields = ['assignment_name', 'assignment_gradeform_setup_json',
-                  'created_datetime', 'last_deadline', 'delivery_status']
-
-    def get_assignment_name(self, instance):
-        return instance.parentnode.short_name
-
-    def get_assignment_gradeform_setup_json(self, instance):
-        return instance.parentnode.gradeform_setup_json
-
+# class AssignmentGroupModelSerializer(serializers.ModelSerializer):
+#     assignment_name = serializers.SerializerMethodField()
+#     assignment_gradeform_setup_json = serializers.SerializerMethodField()
+#
+#     class Meta:
+#         model = AssignmentGroup
+#         fields = ['assignment_name', 'assignment_gradeform_setup_json',
+#                   'created_datetime', 'last_deadline', 'delivery_status']
+#
+#     def get_assignment_name(self, instance):
+#         return instance.parentnode.short_name
+#
+#     def get_assignment_gradeform_setup_json(self, instance):
+#         return instance.parentnode.gradeform_setup_json
+#
 
 class AssignmentModelSerializer(serializers.ModelSerializer):
     semester = serializers.SerializerMethodField()
