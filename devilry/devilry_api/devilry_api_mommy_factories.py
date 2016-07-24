dd77c242f35c047052cdad1a3d52943b01438b34
Eska <eskil.nordland@gmail.com>
2016-07-24 14:33:55 +0200

    devilry_api: mommy factories

--- devilry/devilry_api/devilry_api_mommy_factories.py
+++ devilry/devilry_api/devilry_api_mommy_factories.py
@@ -3,80 +3,74 @@ from devilry.apps.core import mommy_recipes
 from devilry.devilry_api.models import APIKey
 
 
-def api_key_short_lifetime(user, **kwargs):
+def api_key_short_lifetime(**kwargs):
     """
-    Creates a api key for a specific user with short lifetime.
+    Creates a api key with short lifetime.
     Args:
-        user: :obj:`~devilry_account.User` owner of the key
         **kwargs:
 
     Returns:
-        :obj:`~devilry_api.APIKey` api key for user
+        :obj:`~devilry_api.APIKey` api key
     """
-    return mommy.make('devilry_api.APIKey', user=user, lifetime=APIKey.LIFETIME_SHORT, **kwargs)
+    return mommy.make('devilry_api.APIKey', lifetime=APIKey.LIFETIME_SHORT, **kwargs)
 
 
-def api_key_long_lifetime(user, **kwargs):
+def api_key_long_lifetime(**kwargs):
     """
-    Creates a api key for a specific user with long lifetime.
+    Creates a api key with long lifetime.
     Args:
-        user: :obj:`~devilry_account.User` owner of the key
         **kwargs:
 
     Returns:
-        :obj:`~devilry_api.APIKey` api key for user
+        :obj:`~devilry_api.APIKey` api key
     """
-    return mommy.make('devilry_api.APIKey', user=user, lifetime=APIKey.LIFETIME_LONG, **kwargs)
+    return mommy.make('devilry_api.APIKey', lifetime=APIKey.LIFETIME_LONG, **kwargs)
 
 
-def api_key_expired(user, **kwargs):
+def api_key_expired(**kwargs):
     """
-    Creates a api key for a specific user that has expired.
+    Creates a api key that has expired.
     Args:
-        user: :obj:`~devilry_account.User` owner of the key
         **kwargs:
 
     Returns:
-        :obj:`~devilry_api.APIKey` api key for user
+        :obj:`~devilry_api.APIKey` api key
     """
-    return mommy.make('devilry_api.APIKey', user=user, created_datetime=mommy_recipes.OLD_PERIOD_START, **kwargs)
+    return mommy.make('devilry_api.APIKey', created_datetime=mommy_recipes.OLD_PERIOD_START, **kwargs)
 
 
-def api_key_student_permission_read(user, **kwargs):
+def api_key_student_permission_read(**kwargs):
     """
-    Creates a api key for a specific user that has student read persmission.
+    Creates a api key that has student read persmission.
     Args:
-        user: :obj:`~devilry_account.User` owner of the key
         **kwargs:
 
     Returns:
-        :obj:`~devilry_api.APIKey` api key for user
+        :obj:`~devilry_api.APIKey` api key
     """
-    return mommy.make('devilry_api.APIKey', user=user, student_permission=APIKey.STUDENT_PERMISSION_READ, **kwargs)
+    return mommy.make('devilry_api.APIKey', student_permission=APIKey.STUDENT_PERMISSION_READ, **kwargs)
 
 
-def api_key_examiner_permission_read(user, **kwargs):
+def api_key_examiner_permission_read(**kwargs):
     """
-    Creates a api key for a specific user that has examiner read persmission.
+    Creates a api key that has examiner read persmission.
     Args:
-        user: :obj:`~devilry_account.User` owner of the key
         **kwargs:
 
     Returns:
-        :obj:`~devilry_api.APIKey` api key for user
+        :obj:`~devilry_api.APIKey` api key
     """
-    return mommy.make('devilry_api.APIKey', user=user, examiner_permission=APIKey.EXAMINER_PERMISSION_READ, **kwargs)
+    return mommy.make('devilry_api.APIKey', examiner_permission=APIKey.EXAMINER_PERMISSION_READ, **kwargs)
 
 
-def api_key_admin_permission_read(user, **kwargs):
+def api_key_admin_permission_read(**kwargs):
     """
-    Creates a api key for a specific user that has admin read persmission.
+    Creates a api key that has admin read persmission.
     Args:
-        user: :obj:`~devilry_account.User` owner of the key
         **kwargs:
 
     Returns:
-        :obj:`~devilry_api.APIKey` api key for user
+        :obj:`~devilry_api.APIKey` api key
     """
-    return mommy.make('devilry_api.APIKey', user=user, admin_permission=APIKey.ADMIN_PERMISSION_READ, **kwargs)
+    return mommy.make('devilry_api.APIKey', admin_permission=APIKey.ADMIN_PERMISSION_READ, **kwargs)
 
