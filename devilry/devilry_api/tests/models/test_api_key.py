89f90a0eeddef4425fa76bd629e023b4f28b0a87
Eska <eskil.nordland@gmail.com>
2016-07-24 14:11:45 +0200

    devilry_api: test for APIKey model

--- devilry/devilry_api/tests/models/test_api_key.py
+++ devilry/devilry_api/tests/models/test_api_key.py
@@ -13,38 +13,10 @@ class TestAPIKey(testcases.TestCase):
         testkey = mommy.make('devilry_api.APIKey')
         self.assertEqual(40, len(testkey.key))
 
-    def test_key_expired(self):
-        testkey = mommy.make('devilry_api.APIKey', expiration_date=mommy_recipes.OLD_PERIOD_START)
+    def test_key_has_expired(self):
+        testkey = mommy.make('devilry_api.APIKey', created_datetime=mommy_recipes.OLD_PERIOD_START)
         self.assertTrue(testkey.has_expired)
 
-    def test_key_not_expired(self):
-        testkey = mommy.make('devilry_api.APIKey', expiration_date=mommy_recipes.ACTIVE_PERIOD_END)
+    def test_key_has_not_expired(self):
+        testkey = mommy.make('devilry_api.APIKey', created_datetime=mommy_recipes.FUTURE_PERIOD_START)
         self.assertFalse(testkey.has_expired)
-
-    def test_key_not_expired_expiration_none(self):
-        testkey = mommy.make('devilry_api.APIKey', expiration_date=None)
-        self.assertFalse(testkey.has_expired)
-
-    def test_key_is_active_expiration_none(self):
-        testkey = mommy.make('devilry_api.APIKey',
-                             start_datetime=mommy_recipes.ACTIVE_PERIOD_START,
-                             expiration_date=None)
-        self.assertTrue(testkey.is_active)
-
-    def test_key_is_active_expiration_is_set(self):
-        testkey = mommy.make('devilry_api.APIKey',
-                             start_datetime=mommy_recipes.ACTIVE_PERIOD_START,
-                             expiration_date=mommy_recipes.ACTIVE_PERIOD_END)
-        self.assertTrue(testkey.is_active)
-
-    def test_key_is_not_active_expiration_none(self):
-        testkey = mommy.make('devilry_api.APIKey',
-                             start_datetime=mommy_recipes.FUTURE_PERIOD_START,
-                             expiration_date=None)
-        self.assertFalse(testkey.is_active)
-
-    def test_key_is_not_active(self):
-        testkey = mommy.make('devilry_api.APIKey',
-                             start_datetime=mommy_recipes.FUTURE_PERIOD_START,
-                             expiration_date=mommy_recipes.FUTURE_PERIOD_END)
-        self.assertFalse(testkey.is_active)
