563669167e27b58d35e6ba63bb918a16d6a44b6b
Eska <eskil.nordland@gmail.com>
2016-07-24 11:09:39 +0200

    devilry_api: added mommy factories for devilry_api, infinite lifetime key

--- devilry/devilry_api/devilry_api_mommy_factories.py
+++ devilry/devilry_api/devilry_api_mommy_factories.py
@@ -1,4 +1,5 @@
 from model_mommy import mommy
+from devilry.apps.core import mommy_recipes
 
 
 def api_key_for_user(user, **kwargs):
@@ -9,8 +10,20 @@ def api_key_for_user(user, **kwargs):
         **kwargs:
 
     Returns:
-        :obj:`~devilry_api.APIKey`
+        :obj:`~devilry_api.APIKey` api key for user
 
     """
     return mommy.make('devilry_api.APIKey', user=user, **kwargs)
 
+
+def api_key_infinite_lifetime(user, **kwargs):
+    """
+    Creates a new api key for a specific user with infinite lifetime
+    Args:
+        user: :obj:`~devilry_account.User` owner of the key
+        **kwargs:
+
+    Returns:
+        :obj:`~devilry_api.APIKey` api key for user
+    """
+    return mommy.make('devilry_api.APIKey', user=user, expiration_date=mommy_recipes.FUTURE_PERIOD_END, **kwargs)
