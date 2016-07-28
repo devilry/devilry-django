174ae1c9345bb86cbbdf125c5f907d66844374b9
Eska <eskil.nordland@gmail.com>
2016-07-28 20:59:36 +0200

    devilry_api/permission:

--- devilry/devilry_api/permission/base_permission.py
+++ devilry/devilry_api/permission/base_permission.py
@@ -6,21 +6,21 @@ from devilry.devilry_api.models import APIKey
 
 class BaseIsAuthenticatedAPIKey(IsAuthenticated):
 
-    def get_apikey(self, request):
-        """
-        This returns the api key object for the request
-
-        Returns:
-            :obj: `~devilry_api.APIKey`
-        """
-        auth = get_authorization_header(request).split()
-        if not auth or auth[0].lower() != b'token':
-            return None
-        key = auth[1].decode()
-        return APIKey.objects.get(key=key)
+    # def get_apikey(self, request):
+    #     """
+    #     This returns the api key object for the request
+    #
+    #     Returns:
+    #         :obj: `~devilry_api.APIKey`
+    #     """
+    #     auth = get_authorization_header(request).split()
+    #     if not auth or auth[0].lower() != b'token':
+    #         return None
+    #     key = auth[1].decode()
+    #     return APIKey.objects.get(key=key)
 
     def has_permission(self, request, view):
-        self.apikey = self.get_apikey(request)
+        self.apikey = request.apikey_token
         return super(BaseIsAuthenticatedAPIKey, self).has_permission(request, view) and self.apikey
 
 # class IsStudent(BaseIsAuthenticatedAPIKey):
--- devilry/devilry_api/permission/student_permission.py
+++ devilry/devilry_api/permission/student_permission.py
@@ -2,8 +2,23 @@ from devilry.devilry_api.models import APIKey
 
 from devilry.devilry_api.permission.base_permission import BaseIsAuthenticatedAPIKey
 
+READ_HTTP_METHODS = ['GET', 'HEAD', 'OPTIONS']
+WRITE_HTTP_METHODS = ['GET', 'HEAD', 'OPTIONS', 'POST', 'PUT', 'DELETE', 'PATCH']
 
-class StudentReadOnlyAPIKey(BaseIsAuthenticatedAPIKey):
+
+class BaseStudentPermissionAPIKey(BaseIsAuthenticatedAPIKey):
+    required_student_permissions = []
+    http_allowed_methods = []
+
+    def has_permission(self, request, view):
+        return (
+            super(BaseStudentPermissionAPIKey, self).has_permission(request, view) and
+            self.apikey.student_permission in self.required_student_permissions and
+            request.method in self.http_allowed_methods
+        )
+
+
+class StudentReadOnlyAPIKey(BaseStudentPermissionAPIKey):
     """
     Permission for student read only
 
@@ -12,11 +27,10 @@ class StudentReadOnlyAPIKey(BaseIsAuthenticatedAPIKey):
     method is read only
     """
 
-    read_only_methods = ('GET', 'HEAD', 'OPTIONS')
+    http_allowed_methods = READ_HTTP_METHODS
+    required_student_permissions = [APIKey.STUDENT_PERMISSION_READ, APIKey.STUDENT_PERMISSION_WRITE]
 
-    def has_permission(self, request, view):
-        return (
-            super(StudentReadOnlyAPIKey, self).has_permission(request, view) and
-            self.apikey.student_permission == APIKey.STUDENT_PERMISSION_READ and
-            request.method in self.read_only_methods
-        )
+
+class StudentWriteAPIKey(BaseStudentPermissionAPIKey):
+    http_allowed_methods = WRITE_HTTP_METHODS
+    required_student_permissions = [APIKey.STUDENT_PERMISSION_WRITE]
