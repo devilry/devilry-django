b1bc003432a9fd45e5d4e6d43fe2dbc9fbf9a239
StianJul <stianjul@gmail.com>
2016-08-02 13:33:20 +0200

    devilry_ziputil: Add mockbackend to registry (for basic testing of the ziputil)

--- devilry/devilry_ziputil/apps.py
+++ devilry/devilry_ziputil/apps.py
@@ -1,10 +1,12 @@
 from django.apps import AppConfig
 
+from devilry.devilry_ziputil import backend_registry
+
 
 class DevilryZipAppConfig(AppConfig):
     name = 'devilry.devilry_ziputil'
     verbose_name = "Devilry zip utility"
 
     def ready(self):
-        # TODO: Add singleton backends to registry.
-        pass
+        from devilry.devilry_group.views.download_files import backends as group_backends
+        backend_registry.Registry.get_instance().add(group_backends.DevilryGroupZipBackend)
