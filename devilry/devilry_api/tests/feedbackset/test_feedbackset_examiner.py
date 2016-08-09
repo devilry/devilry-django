94eda1b8cf705d9543e3abcc13a220657a969c6f
Eska <eskil.nordland@gmail.com>
2016-08-09 02:16:36 +0200

    devilry_api/tests/feedbackset: sanity tests for Examiner

--- devilry/devilry_api/tests/feedbackset/test_feedbackset_examiner.py
+++ devilry/devilry_api/tests/feedbackset/test_feedbackset_examiner.py
@@ -9,6 +9,7 @@ from devilry.apps.core import devilry_core_mommy_factories
 from devilry.devilry_api import devilry_api_mommy_factories
 from devilry.devilry_api.feedbackset.views import feedbackset_examiner
 from devilry.devilry_api.tests.mixins import test_examiner_mixins, api_test_helper, test_common_mixins
+from devilry.devilry_group.models import FeedbackSet
 
 
 class TestFeedbacksetSanity(test_common_mixins.TestReadOnlyPermissionMixin,
@@ -22,9 +23,99 @@ class TestFeedbacksetSanity(test_common_mixins.TestReadOnlyPermissionMixin,
         self.assertEqual(401, response.status_code)
 
     def test_sanity(self):
-        set = mommy.make('devilry_group.Feedbackset')
+        group = mommy.make('core.AssignmentGroup',
+                           parentnode=mommy.make_recipe('devilry.apps.core.assignment_activeperiod_start'))
+        set = mommy.make('devilry_group.Feedbackset', group=group)
         examiner = devilry_core_mommy_factories.examiner(set.group)
         apikey = devilry_api_mommy_factories.api_key_examiner_permission_read(user=examiner.relatedexaminer.user)
         response = self.mock_get_request(apikey=apikey.key)
         self.assertEqual(200, response.status_code)
 
+    def test_id(self):
+        group = mommy.make('core.AssignmentGroup',
+                           parentnode=mommy.make_recipe('devilry.apps.core.assignment_activeperiod_start'))
+        feedbackset = mommy.make('devilry_group.Feedbackset', id=10, group=group)
+        examiner = devilry_core_mommy_factories.examiner(feedbackset.group)
+        apikey = devilry_api_mommy_factories.api_key_examiner_permission_read(user=examiner.relatedexaminer.user)
+        response = self.mock_get_request(apikey=apikey.key)
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(response.data[0]['id'], 10)
+
+    def test_group(self):
+        group = mommy.make('core.AssignmentGroup')
+        group = mommy.make('core.AssignmentGroup',
+                           parentnode=mommy.make_recipe('devilry.apps.core.assignment_activeperiod_start'))
+        feedbackset = mommy.make('devilry_group.Feedbackset', group=group)
+        examiner = devilry_core_mommy_factories.examiner(feedbackset.group)
+        apikey = devilry_api_mommy_factories.api_key_examiner_permission_read(user=examiner.relatedexaminer.user)
+        response = self.mock_get_request(apikey=apikey.key)
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(response.data[0]['group'], group.id)
+
+    def test_created_datetime(self):
+        group = mommy.make('core.AssignmentGroup',
+                           parentnode=mommy.make_recipe('devilry.apps.core.assignment_activeperiod_start'))
+        feedbackset = mommy.make('devilry_group.Feedbackset', group=group)
+        examiner = devilry_core_mommy_factories.examiner(feedbackset.group)
+        apikey = devilry_api_mommy_factories.api_key_examiner_permission_read(user=examiner.relatedexaminer.user)
+        response = self.mock_get_request(apikey=apikey.key)
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(response.data[0]['created_datetime'], feedbackset.created_datetime.isoformat())
+
+    def test_feedbackset_type(self):
+        group = mommy.make('core.AssignmentGroup',
+                           parentnode=mommy.make_recipe('devilry.apps.core.assignment_activeperiod_start'))
+        feedbackset = mommy.make('devilry_group.Feedbackset',
+                                 group=group,
+                                 feedbackset_type=FeedbackSet.FEEDBACKSET_TYPE_FIRST_ATTEMPT)
+        examiner = devilry_core_mommy_factories.examiner(feedbackset.group)
+        apikey = devilry_api_mommy_factories.api_key_examiner_permission_read(user=examiner.relatedexaminer.user)
+        response = self.mock_get_request(apikey=apikey.key)
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(response.data[0]['feedbackset_type'], feedbackset.feedbackset_type)
+
+    def test_is_last_in_group(self):
+        group = mommy.make('core.AssignmentGroup',
+                           parentnode=mommy.make_recipe('devilry.apps.core.assignment_activeperiod_start'))
+        feedbackset = mommy.make('devilry_group.Feedbackset',
+                                 group=group,
+                                 is_last_in_group=True)
+        examiner = devilry_core_mommy_factories.examiner(feedbackset.group)
+        apikey = devilry_api_mommy_factories.api_key_examiner_permission_read(user=examiner.relatedexaminer.user)
+        response = self.mock_get_request(apikey=apikey.key)
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(response.data[0]['is_last_in_group'], feedbackset.is_last_in_group)
+
+    def test_deadline_datetime_first_attempt(self):
+        group = mommy.make('core.AssignmentGroup',
+                           parentnode=mommy.make_recipe('devilry.apps.core.assignment_activeperiod_start'))
+        feedbackset = mommy.make('devilry_group.Feedbackset',
+                                 group=group,
+                                 feedbackset_type=FeedbackSet.FEEDBACKSET_TYPE_FIRST_ATTEMPT)
+        examiner = devilry_core_mommy_factories.examiner(feedbackset.group)
+        apikey = devilry_api_mommy_factories.api_key_examiner_permission_read(user=examiner.relatedexaminer.user)
+        response = self.mock_get_request(apikey=apikey.key)
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(response.data[0]['deadline_datetime'], group.parentnode.first_deadline)
+
+    def test_deadline_datetime_new_attempt(self):
+        group = mommy.make('core.AssignmentGroup',
+                           parentnode=mommy.make_recipe('devilry.apps.core.assignment_activeperiod_start'))
+        feedbackset = mommy.make('devilry_group.Feedbackset',
+                                 group=group,
+                                 feedbackset_type=FeedbackSet.FEEDBACKSET_TYPE_NEW_ATTEMPT)
+        examiner = devilry_core_mommy_factories.examiner(feedbackset.group)
+        apikey = devilry_api_mommy_factories.api_key_examiner_permission_read(user=examiner.relatedexaminer.user)
+        response = self.mock_get_request(apikey=apikey.key)
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(response.data[0]['deadline_datetime'], feedbackset.deadline_datetime)
+
+    def test_created_by_fullname(self):
+        group = mommy.make('core.AssignmentGroup',
+                           parentnode=mommy.make_recipe('devilry.apps.core.assignment_activeperiod_start'))
+        examiner = devilry_core_mommy_factories.examiner(group, fullname='Thor')
+        mommy.make('devilry_group.Feedbackset', group=group, created_by=examiner.relatedexaminer.user)
+        apikey = devilry_api_mommy_factories.api_key_examiner_permission_read(user=examiner.relatedexaminer.user)
+        response = self.mock_get_request(apikey=apikey.key)
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(response.data[0]['created_by_fullname'], 'Thor')
