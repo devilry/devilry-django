1906ac7475ea03dd7ab42d5b5836859eb90c0824
StianJul <stianjul@gmail.com>
2016-08-17 18:57:32 +0200

    devilry_ziputil/models.py: Added manager for CompressedArchiveMeta.
    Renamed from CompressedFileMeta to CompressedArchiveMeta.

--- devilry/devilry_ziputil/models.py
+++ devilry/devilry_ziputil/models.py
@@ -1,9 +1,13 @@
+# Django imports
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
 
 class GenericMeta(models.Model):
+    """
+    Abstract class that implements usage of GenericForeignKey.
+    """
     #: Foreignkey to Djangos ContentType.
     content_type = models.ForeignKey(ContentType, related_name='content_type_compressed_file_meta')
 
@@ -21,22 +25,48 @@ class GenericMeta(models.Model):
         unique_together = ('content_type', 'content_object_id')
 
 
-class CompressedFileMeta(GenericMeta):
+class CompressedArchiveMetaManager(models.Manager):
+    def create_meta(self, instance, archive_path, zipfile_backend):
+        """
+        Manager provides a way to create a meta entry for a archive.
+        See :class:`~devilry.devilry_ziputil.models.CompressedArchiveMeta`.
+
+        Args:
+            instance: Instance the archive is for.
+            archive_path: Path to achive at storage location.
+            zipfile_backend: Backend used to zip the file.
+        """
+        zipfile_backend.readmode = True
+        archive_meta = CompressedArchiveMeta(content_object=instance,
+                                             archive_path=archive_path,
+                                             archive_location=zipfile_backend.get_storage_location(),
+                                             archive_size=zipfile_backend.archive_size())
+        archive_meta.clean()
+        archive_meta.save()
+        return archive_meta
+
+
+class CompressedArchiveMeta(GenericMeta):
     """
     Contains metadata about a compressed archive. Name of the archive, path to it and it's size.
     """
 
+    objects = CompressedArchiveMetaManager()
+
+    #: When the archive was created.
+    created_datetime = models.DateTimeField(auto_now_add=True)
+
     #: Name of the compressed archive.
-    archive_name = models.CharField(max_length=50, null=False, blank=False)
+    archive_path = models.CharField(max_length=100, null=False, blank=False)
 
     #: Path to the compressed archive(without archive name).
-    archive_path = models.CharField(max_length=200, null=False, blank=False)
+    archive_location = models.CharField(max_length=200, null=False, blank=False)
 
     #: Size of the archive in bytes.
-    archive_size = models.PositiveIntegerField(null=False)
+    archive_size = models.PositiveIntegerField(null=False, blank=False)
 
     def get_full_path(self):
-        return '{}/{}'.format(self.archive_path, self.archive_name)
+        return '{}{}'.format(self.archive_location, self.archive_path)
 
     def __unicode__(self):
         return self.get_full_path()
