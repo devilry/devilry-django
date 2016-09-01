d10c1cfe041077cc22baeae5fe062a55bfcf2616
StianJul <stianjul@gmail.com>
2016-09-01 20:34:58 +0200

    devilry_ziputil/backends: ZipFile tweaks and support bz2 and gzip.

--- devilry/devilry_ziputil/backends/backend_mock.py
+++ devilry/devilry_ziputil/backends/backend_mock.py
@@ -1,8 +1,5 @@
 from devilry.devilry_ziputil.backends.backends_base import PythonZipFileBackend
-
-from django.conf import settings
-
-import os
+from devilry.devilry_ziputil.backends.backends_base import PythonTarFileBackend
 
 
 class MockDevilryZipBackend(PythonZipFileBackend):
@@ -11,24 +8,18 @@ class MockDevilryZipBackend(PythonZipFileBackend):
     """
     backend_id = 'default'
 
-    def __init__(self, dry_run=False, **kwargs):
+    def __init__(self, **kwargs):
         super(MockDevilryZipBackend, self).__init__(**kwargs)
-        if not dry_run:
-            self.__create_path_if_not_exists()
 
-    def __create_path_if_not_exists(self):
-        """
-        Create path if given path does not exist.
-        """
-        archivedirname = os.path.dirname(self.archive_path)
-        if not os.path.exists(archivedirname):
-            os.makedirs(archivedirname)
 
-    def get_path(self):
-        """
-        Get the archive path. Used for testing.
-        """
-        return self.archive_path
+class MockDevilryTarBackend(PythonTarFileBackend):
+    """
+    Used for testing.
+    """
+    backend_id = 'tar'
+
+    def __init__(self, **kwargs):
+        super(MockDevilryTarBackend, self).__init__(**kwargs)
 
 
 class MockDevilryZipBackendS3(MockDevilryZipBackend):
@@ -36,10 +27,7 @@ class MockDevilryZipBackendS3(MockDevilryZipBackend):
 
     """
     backend_id = 's3'
-
-    @classmethod
-    def get_storage_location(cls):
-        return 'url/to/s3/filestorage'
+    storage_location = 'url/to/s3/filestorage'
 
     def __init__(self, **kwargs):
         super(MockDevilryZipBackendS3, self).__init__(**kwargs)
@@ -50,10 +38,7 @@ class MockDevilryZipBackendHeroku(MockDevilryZipBackend):
 
     """
     backend_id = 'heroku'
-
-    @classmethod
-    def get_storage_location(cls):
-        return 'url/to/heroku/filestorage'
+    storage_location = 'url/to/heroku/filestorage'
 
     def __init__(self, **kwargs):
         super(MockDevilryZipBackendHeroku, self).__init__(**kwargs)
--- devilry/devilry_ziputil/backends/backends_base.py
+++ devilry/devilry_ziputil/backends/backends_base.py
@@ -1,5 +1,7 @@
 import os
 import zipfile
+import tarfile
+import shutil
 
 from django.conf import settings
 
@@ -8,55 +10,76 @@ class BaseArchiveBackend(object):
     """
     Specifies the interface for a backend compression-subclass.
 
-    All backends must implement this class
+    All backends must implement this class.
     """
     class Meta:
         abstract = True
 
     def __init__(self, archive_path, archive_name='', readmode=True):
+        """
+
+        Args:
+            archive_path: Full path to archive including the archive name.
+            archive_name: Only the archive name.
+            readmode: Can be read from, defaults to ``True``.
+
+        """
+        self.archive = None
         self.archive_path = archive_path
         self.archive_name = archive_name
         self.readmode = readmode
         self.__closed = False
 
+    def _create_path_if_not_exists(self):
+        """
+        Create path if given path does not exist.
+        """
+        archivedirname = os.path.dirname(self.archive_path)
+        if not os.path.exists(archivedirname):
+            os.makedirs(archivedirname)
+
     def add_file(self, path, filelike_obj):
         """
         Add files to archive.
         """
+        # self._create_path_if_not_exists()
         raise NotImplementedError()
 
-    def close_archive(self):
+    def close(self):
         """
-        Close archive.
+        Close zipfile when done with adding files to it.
         """
-        raise NotImplementedError()
+        if not self.archive:
+            raise ValueError('Archive does not exist at {}'.format(self.archive_path))
+        self.__closed = True
+        self.archive.close()
+
+    def open_archive(self):
+        if not self.readmode:
+                raise ValueError('Must be in readmode')
+        if not self.archive:
+            raise ValueError('Archive does not exist at {}'.format(self.archive_path))
+        return self.archive.open(self.archive_path, 'r')
 
     def read_archive(self):
-        """
-        Open archive in readmode.
-
-        Returns:
-            File object: Python fileobject.
-        """
-        raise NotImplementedError()
+        if not self.readmode:
+                raise ValueError('Must be in readmode')
+        if not self.archive:
+            raise ValueError('Archive does not exist at {}'.format(self.archive_path))
+        return open(self.archive_path, mode='rb')
 
     def get_archive(self):
         """
-        Get the Zip archive
-
-        Returns:
-            ZipFile: Zip-archive created.
+        Get the archive for compressed files.
         """
         raise NotImplementedError()
 
     def archive_size(self):
-        """
-        Get the size of the compressed Zip-archive.
-
-        Return:
-            int: Size of the Zip-archive.
-        """
-        raise NotImplementedError()
+        if not self.readmode:
+            raise ValueError('Must be in readmode')
+        if not self.archive:
+            raise ValueError('Archive does not exist at {}'.format(self.archive_path))
+        return os.stat(self.archive_path).st_size
 
 
 class PythonZipFileBackend(BaseArchiveBackend):
@@ -70,15 +93,12 @@ class PythonZipFileBackend(BaseArchiveBackend):
     #: A unique string ID for the subclasses to use that describes what kind of backend that
     #: is used and is the identifier for a registry-class. Example IDs ``s3``, ``heroku`` etc.
     backend_id = None
-
-    @classmethod
-    def get_storage_location(cls):
-        return settings.DEVILRY_COMPRESSED_ARCHIVES_DIRECTORY
+    storage_location = settings.DEVILRY_COMPRESSED_ARCHIVES_DIRECTORY
 
     def __init__(self, **kwargs):
         super(PythonZipFileBackend, self).__init__(**kwargs)
-        self.__archive = None
         self.__add_path_extension()
+        self._create_path_if_not_exists()
 
     def __add_path_extension(self):
         """
@@ -87,9 +107,9 @@ class PythonZipFileBackend(BaseArchiveBackend):
         """
         if not self.archive_path.endswith('.zip'):
             self.archive_name += '.zip'
-            self.archive_path = PythonZipFileBackend.get_storage_location() + self.archive_path + '.zip'
+            self.archive_path = PythonZipFileBackend.storage_location + self.archive_path + '.zip'
         else:
-            self.archive_path = PythonZipFileBackend.get_storage_location() + self.archive_path
+            self.archive_path = PythonZipFileBackend.storage_location + self.archive_path
 
     def add_file(self, path, filelike_obj):
         """
@@ -101,10 +121,10 @@ class PythonZipFileBackend(BaseArchiveBackend):
         """
         if self.readmode is True:
             raise ValueError('readmode must be False to add files.')
-        if self.__archive is None or self.__closed:
+        if self.archive is None or self.__closed:
             self.__closed = False
-            self.__archive = zipfile.ZipFile(self.archive_path, 'a', zipfile.ZIP_DEFLATED, allowZip64=True)
-        self.__archive.writestr(path, filelike_obj.read())
+            self.archive = zipfile.ZipFile(self.archive_path, 'a', zipfile.ZIP_DEFLATED, allowZip64=True)
+        self.archive.writestr(path, filelike_obj.read())
 
     def read_archive(self):
         """
@@ -120,18 +140,9 @@ class PythonZipFileBackend(BaseArchiveBackend):
         """
         if not self.readmode:
             raise ValueError('Must be in readmode')
-        if not self.__archive:
+        if not self.archive:
             raise ValueError('Archive does not exist at {}'.format(self.archive_path))
         return open(self.archive_path, mode='rb')
-
-    def close_archive(self):
-        """
-        Close zipfile when done with adding files to it.
-        """
-        if not self.__archive:
-            raise ValueError('Archive does not exist at {}'.format(self.archive_path))
-        self.__closed = True
-        self.__archive.close()
 
     def get_archive(self):
         """
@@ -151,6 +162,142 @@ class PythonZipFileBackend(BaseArchiveBackend):
         """
         if not self.readmode:
             raise ValueError('Must be in readmode')
-        if not self.__archive:
+        if not self.archive:
             raise ValueError('Archive does not exist at {}'.format(self.archive_path))
         return os.stat(self.archive_path).st_size
+
+
+class PythonTarFileBackend(BaseArchiveBackend):
+    """
+    A baseclass backend using :class:`~TarFile` for archiving files to at tarball.
+    Supports no compression, ``gzip`` and``bzip2`` compression through the :class:'~TarFile' class.
+    """
+
+    #: Compression formats supported.
+    compression_formats = ['', 'gz', 'bz2']
+    backend_id = None
+    storage_location = settings.DEVILRY_COMPRESSED_ARCHIVES_DIRECTORY
+
+    def __init__(self, stream=False, compression='', **kwargs):
+        """
+
+        Args:
+            stream: If it should be handled as a stream or not.
+            compression: The compression mode used, defaults to ``uncompressed``, but
+                modes ``gz``, ``bz2`` and ``xz`` can also be used.
+
+        """
+        super(PythonTarFileBackend, self).__init__(**kwargs)
+        self.__temp_path = os.path.join(
+                '{}{}{}'.format(PythonTarFileBackend.storage_location, 'tempdir', self.archive_name), ''
+        )
+        os.makedirs(self.__temp_path)
+        self.__stream = stream
+        self.__compression = compression
+        self.__add_path_extension()
+        self._create_path_if_not_exists()
+
+    def __add_path_extension(self):
+        """
+        Sets :obj:`~.PythonZipFileBackend.archive_path` to full path by prepending the backend storage location
+        to the archive_path. Also adds .zip extension
+        """
+        if self.__compression == '':
+            self.archive_path = PythonTarFileBackend.storage_location + self.archive_path + '.tar'
+        else:
+            self.archive_path = PythonTarFileBackend.storage_location + self.archive_path + '.tar.' + self.__compression
+
+    def __check_compression_support(self):
+        """
+        Check that the ``compression`` specified is supported.
+
+        Raises:
+            ValueError: If compression format is not supported.
+        """
+        if self.__compression not in PythonTarFileBackend.compression_formats:
+            raise ValueError('Unsupported compression format: {}'.format(self.__compression))
+
+    def add_file(self, path, filelike_obj):
+        """
+        Writes a file to the archive on the given ``path``.
+
+        In fact, this creates a temporary folder hierarchy which is zipped when ``closed()`` is invoked on a
+        instance of this class.
+
+        Args:
+            path: Path to file inside the archive.
+            filelike_obj: An object that behaves like a File(read, write..).
+
+        """
+        self.__check_compression_support()
+
+        # Create path inside __tempdir
+        sub_path = os.path.join(self.__temp_path, path, '')
+        if not os.path.exists(sub_path):
+            os.makedirs(sub_path)
+
+        # Write file to temp directory.
+        new_temp_file = open('{}{}'.format(sub_path, filelike_obj.name), 'a+')
+        new_temp_file.write(filelike_obj.read())
+        new_temp_file.close()
+
+    def read_archive(self):
+        """
+        Get TarFile in readmode.
+
+        Returns:
+            TarFile: The compressed tar archive.
+        """
+        if not self.readmode:
+            raise ValueError('Must be in readmode')
+        if not self.archive:
+            raise ValueError('Archive does not exist at {}'.format(self.archive_path))
+        return tarfile.open(self.archive_path, mode='r')
+
+    def __get_mode(self):
+        """
+        Get the mode the tarfile should be opened in.
+
+        Here's an example of the writing modes without streaming::
+
+            ``':'``    # writing without compression
+            ``':gz'``  # writing with gzip compression
+            ``':bz2'`` # writing with bz2 compression
+
+        Here's an example of the writing modes with streaming::
+
+            ``'|'``      # writing without compression
+            ``'|gz'``    # writing with gzip compression
+            ``'|bz2'``   # writing with bz2 compression
+
+        Returns:
+            mode (str): mode based on params passed to ``__init__``.
+        """
+        if self.__stream:
+            return '|'+self.__compression
+        return ':'+self.__compression
+
+    def close(self):
+        """
+        Must be invoked in order to finalize the archive by compressing the 'temp' directory
+        to whichever compression level specified.
+
+        The temporary directory is then removed, and the tar archive is closed.
+        """
+        self.archive = tarfile.open(self.archive_path, mode='w' + self.__get_mode())
+        self.archive.add(self.__temp_path, arcname=self.archive_name)
+        self.archive.close()
+
+        # Remove temp directory.
+        shutil.rmtree(self.__temp_path, ignore_errors=False)
+        self.archive.close()
+
+    def get_archive(self):
+        """
+        Get TarFile(archive) in read-mode.
+
+        Returns:
+            TarFile: Opened in read mode.
+
+        """
+        return tarfile.open(self.archive_path, 'r')
