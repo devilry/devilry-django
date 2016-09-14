b8240fff844f03d123b7356ec3fdb9c50c55cc48
StianJul <stianjul@gmail.com>
2016-09-14 20:06:22 +0200

    devilry_qualifiesforexam/tests.py: commented out querycount test for now.

--- devilry/devilry_qualifiesforexam/tests/test_crinstance.py
+++ devilry/devilry_qualifiesforexam/tests/test_crinstance.py
@@ -58,37 +58,35 @@ class TestCrInstance(test.TestCase):
 
         self.assertEquals(0, len(rolequeryset))
 
-    def test_get_rolequeryset_num_queries(self):
-        testsubject = mommy.make('core.Subject', short_name='Duck1010')
-        testperiod1 = mommy.make_recipe('devilry.apps.core.period_active', parentnode=testsubject)
-        testassignment1 = mommy.make('core.Assignment', parentnode=testperiod1, short_name='Assignment1')
-        testassignment2 = mommy.make('core.Assignment', parentnode=testperiod1, short_name='Assignment2')
-        testassignment3 = mommy.make('core.Assignment', parentnode=testperiod1, short_name='Assignment3')
-        mommy.make('core.Candidate',
-                   assignment_group__parentnode=testassignment1,
-                   relatedstudent__period=testperiod1,
-                   _quantity=20)
-        mommy.make('core.Candidate',
-                   assignment_group__parentnode=testassignment2,
-                   relatedstudent__period=testperiod1,
-                   _quantity=20)
-        mommy.make('core.Candidate',
-                   assignment_group__parentnode=testassignment3,
-                   relatedstudent__period=testperiod1,
-                   _quantity=20)
-
-        mockrequest = mock.MagicMock()
-        mockrequest.cradmin_role = testperiod1
-        testcrinstance = crinstance.CrInstance(request=mockrequest)
-
-        print len(testcrinstance.get_rolequeryset())
-
-        # with self.assertNumQueries(6):
-        #     rolequeryset = testcrinstance.get_rolequeryset()
-        #
-        #     for period in rolequeryset.all():
-        #         for assignment in period.assignments.all():
-        #             for assignmentgroup in assignment.assignmentgroups.all():
-        #                 # candidates = assignmentgroup.candidates.all()
-        #                 for candidate in assignmentgroup.candidates.all():
-        #                     print candidate
+    # def test_get_rolequeryset_num_queries(self):
+    #     testsubject = mommy.make('core.Subject', short_name='Duck1010')
+    #     testperiod1 = mommy.make_recipe('devilry.apps.core.period_active', parentnode=testsubject)
+    #     testassignment1 = mommy.make('core.Assignment', parentnode=testperiod1, short_name='Assignment1')
+    #     testassignment2 = mommy.make('core.Assignment', parentnode=testperiod1, short_name='Assignment2')
+    #     testassignment3 = mommy.make('core.Assignment', parentnode=testperiod1, short_name='Assignment3')
+    #     mommy.make('core.Candidate',
+    #                assignment_group__parentnode=testassignment1,
+    #                relatedstudent__period=testperiod1,
+    #                _quantity=20)
+    #     mommy.make('core.Candidate',
+    #                assignment_group__parentnode=testassignment2,
+    #                relatedstudent__period=testperiod1,
+    #                _quantity=20)
+    #     mommy.make('core.Candidate',
+    #                assignment_group__parentnode=testassignment3,
+    #                relatedstudent__period=testperiod1,
+    #                _quantity=20)
+    #
+    #     mockrequest = mock.MagicMock()
+    #     mockrequest.cradmin_role = testperiod1
+    #     testcrinstance = crinstance.CrInstance(request=mockrequest)
+    #
+    #     with self.assertNumQueries(6):
+    #         rolequeryset = testcrinstance.get_rolequeryset()
+    #
+    #         for period in rolequeryset.all():
+    #             for assignment in period.assignments.all():
+    #                 for assignmentgroup in assignment.assignmentgroups.all():
+    #                     # candidates = assignmentgroup.candidates.all()
+    #                     for candidate in assignmentgroup.candidates.all():
+    #                         print candidate
