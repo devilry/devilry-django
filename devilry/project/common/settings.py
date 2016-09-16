550923b9a12c336647ad984653cc5afa0b9f09df
StianJul <stianjul@gmail.com>
2016-09-16 17:53:42 +0200

    project/common/settings.py

--- devilry/project/common/settings.py
+++ devilry/project/common/settings.py
@@ -82,6 +82,7 @@ INSTALLED_APPS = [
     'devilry.devilry_settings',
     # 'devilry.devilry_search',
     'devilry.devilry_qualifiesforexam',
+    'devilry.devilry_qualifiesforexam_plugin_approved',
     # 'devilry.devilry_qualifiesforexam_approved',
     # 'devilry.devilry_qualifiesforexam_points',
     # 'devilry.devilry_qualifiesforexam_select',
