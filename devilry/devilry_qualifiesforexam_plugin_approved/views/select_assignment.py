047ffcbceb91e115a424a49cb1212cb4274dcba8
StianJul <stianjul@gmail.com>
2016-11-16 15:08:46 +0100

    devilry_qualifiesforexam_plugin_approved: Added approved plugin.

--- devilry/devilry_qualifiesforexam_plugin_approved/views/select_assignment.py
+++ devilry/devilry_qualifiesforexam_plugin_approved/views/select_assignment.py
@@ -5,7 +5,6 @@ from __future__ import unicode_literals
 from django.http import HttpResponseRedirect
 
 # Devilry imports
-from devilry.devilry_qualifiesforexam.views import plugin_mixin
 from devilry.devilry_qualifiesforexam.views.plugin_base_views import base_multiselect_view
 from devilry.apps.core import models as core_models
 from devilry.devilry_qualifiesforexam_plugin_approved import resultscollector
@@ -21,7 +20,7 @@ class PluginForm(base_multiselect_view.SelectedQualificationForm):
         self.fields['selected_items'].queryset = selectable_qualification_items_queryset
 
 
-class PluginSelectAssignmentsView(base_multiselect_view.QualificationItemListView, plugin_mixin.PluginMixin):
+class PluginSelectAssignmentsView(base_multiselect_view.QualificationItemListView):
     model = core_models.Assignment
     plugintypeid = 'devilry_qualifiesforexam_plugin_approved.plugin_select_assignments'
 
