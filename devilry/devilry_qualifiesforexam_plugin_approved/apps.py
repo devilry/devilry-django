66f923affe78aa49351ec01a1e79617b886c1dce
StianJul <stianjul@gmail.com>
2016-11-17 15:16:39 +0100

    devilry_qualifiesforexam_plugin_approved/apps.py: removed load print statement.

--- devilry/devilry_qualifiesforexam_plugin_approved/apps.py
+++ devilry/devilry_qualifiesforexam_plugin_approved/apps.py
@@ -10,7 +10,6 @@ class DevilryQualifiesForExamApprovedAppConfig(AppConfig):
     verbose_name = 'Devilry qualifies for exam plugin approved'
 
     def ready(self):
-        print 'Loading approved plugin'
         from devilry.devilry_qualifiesforexam import plugintyperegistry
         from . import plugin
         plugintyperegistry.Registry.get_instance().add(plugin.SelectAssignmentsPlugin)
