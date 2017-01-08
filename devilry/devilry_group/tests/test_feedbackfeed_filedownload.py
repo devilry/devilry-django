f47b64039844fe50a29e32ebdbf8bb532b829bcf
Espen A. Kristiansen <post@espenak.net>
2017-01-08 17:37:34 +0100

    group...test_feedbackfeed_filedownload: Update for dbcache.

--- devilry/devilry_group/tests/test_feedbackfeed_filedownload.py
+++ devilry/devilry_group/tests/test_feedbackfeed_filedownload.py
@@ -18,9 +18,11 @@ from ievv_opensource.ievv_batchframework import batchregistry
 from django_cradmin.cradmin_testhelpers import TestCaseMixin
 
 # Devilry imports
+from devilry.devilry_dbcache.customsql import AssignmentGroupDbCacheCustomSql
 from devilry.devilry_group.views.download_files import batch_download_files
 from devilry.devilry_group import models as group_models
 from devilry.devilry_group import tasks
+from devilry.devilry_group import devilry_group_mommy_factories
 
 
 class AbstractTestCase(TestCase):
@@ -40,12 +42,15 @@ class TestFileDownloadFeedbackfeedView(TestCase, TestCaseMixin):
     """
     viewclass = batch_download_files.FileDownloadFeedbackfeedView
 
+    def setUp(self):
+        AssignmentGroupDbCacheCustomSql().initialize()
+
     def test_single_file_download(self):
         # Test download of single file
         testgroup = mommy.make('core.AssignmentGroup')
         testuser = mommy.make(settings.AUTH_USER_MODEL, shortname='dewey@example.com', fullname='Dewey Duck')
         testcomment = mommy.make('devilry_group.GroupComment',
-                                 feedback_set__group=testgroup,
+                                 feedback_set=testgroup.feedbackset_set.first(),
                                  user=testuser,
                                  user_role='student')
         commentfile = mommy.make('devilry_comment.CommentFile', comment=testcomment, filename='testfile.txt')
@@ -66,7 +71,7 @@ class TestFileDownloadFeedbackfeedView(TestCase, TestCaseMixin):
         candidate1 = mommy.make('core.Candidate', assignment_group=testgroup, relatedstudent__user=testuser1)
         candidate2 = mommy.make('core.Candidate', assignment_group=testgroup, relatedstudent__user=testuser2)
         testcomment = mommy.make('devilry_group.GroupComment',
-                                 feedback_set__group=testgroup,
+                                 feedback_set=testgroup.feedbackset_set.first(),
                                  user=testuser1,
                                  user_role='student')
         commentfile = mommy.make('devilry_comment.CommentFile', comment=testcomment, filename='testfile.txt')
@@ -90,7 +95,9 @@ class TestFileDownloadFeedbackfeedView(TestCase, TestCaseMixin):
         # Test user can't download if not part of AssignmentGroup
         testgroup = mommy.make('core.AssignmentGroup')
         testuser = mommy.make(settings.AUTH_USER_MODEL, shortname='dewey@example.com', fullname='Dewey Duck')
-        testcomment = mommy.make('devilry_group.GroupComment', user_role='examiner')
+        testcomment = mommy.make('devilry_group.GroupComment',
+                                 user_role='examiner',
+                                 feedback_set=devilry_group_mommy_factories.make_first_feedbackset_in_group())
         commentfile = mommy.make('devilry_comment.CommentFile', comment=testcomment, filename='testfile.txt')
         commentfile.file.save('testfile.txt', ContentFile('testcontent'))
 
@@ -108,7 +115,7 @@ class TestFileDownloadFeedbackfeedView(TestCase, TestCaseMixin):
         testuser = mommy.make(settings.AUTH_USER_MODEL, shortname='dewey@example.com', fullname='Dewey Duck')
         testcomment = mommy.make('devilry_group.GroupComment',
                                  user_role='examiner',
-                                 feedback_set__group=testgroup,
+                                 feedback_set=testgroup.feedbackset_set.first(),
                                  visibility=group_models.GroupComment.VISIBILITY_PRIVATE)
         commentfile = mommy.make('devilry_comment.CommentFile', comment=testcomment, filename='testfile.txt')
         commentfile.file.save('testfile.txt', ContentFile('testcontent'))
