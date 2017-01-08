ed5e304db618d9b57c36861c83e4fc52202eddb4
Espen A. Kristiansen <post@espenak.net>
2017-01-08 18:46:29 +0100

    project.develop.settings.codeship_test: Use postgres 9.5.

--- devilry/project/develop/settings/codeship_test.py
+++ devilry/project/develop/settings/codeship_test.py
@@ -7,6 +7,8 @@ DATABASES = {
         'NAME': 'test',
         'USER': os.environ.get('PG_USER'),
         'PASSWORD': os.environ.get('PG_PASSWORD'),
+        'PORT': '5435',  # Postgres 9.5,
+        # 'PORT': '5436',  # Postgres 9.6,
         'HOST': '127.0.0.1',
     }
 }
