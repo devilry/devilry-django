850c56a491ea7caf8d51e10b42394f76514f94cd
Eskil Nordland <eskil@appresso.no>
2017-01-23 11:29:47 +0100

    assignment group merge guide, explanation of different approaches we tried and our current implementation.

--- devilry/devilry_admin/views/assignment/students/merge_groups.py
+++ devilry/devilry_admin/views/assignment/students/merge_groups.py
@@ -31,8 +31,6 @@ class MergeGroupsView(groupview_base.BaseMultiselectView):
         return MergeGroupsTargetRenderer
 
     def form_valid(self, form):
-        print(self.request.post)
-        print(form.clean_data)
         messages.warning(
             self.request,
             'Merge groups is not finished')
