285a9b4173a4d0b38a1fe54816708f07555db638
Stian Julseth <stianjul@gmail.com>
2017-01-31 13:11:30 +0100

    devilry_admin/view/period: Added backlink for all results overview and test.

--- devilry/devilry_admin/tests/period/test_overview_all_results.py
+++ devilry/devilry_admin/tests/period/test_overview_all_results.py
@@ -28,6 +28,19 @@ class TestOverviewAllResults(test.TestCase, cradmin_testhelpers.TestCaseMixin):
         )
         self.assertEquals('All students results', mockresponse.selector.one('title').alltext_normalized)
 
+    def test_backlink_exists(self):
+        testperiod = mommy.make('core.Period')
+        testuser = mommy.make(settings.AUTH_USER_MODEL)
+        mockresponse = self.mock_http200_getrequest_htmls(
+            cradmin_role=testperiod,
+            requestuser=testuser
+        )
+        self.assertEqual(1, len(mockresponse.request.cradmin_instance.reverse_url.call_args_list))
+        self.assertEquals(
+            mock.call(appname='overview', args=(), viewname='INDEX', kwargs={}),
+            mockresponse.request.cradmin_instance.reverse_url.call_args_list[0]
+        )
+
     def test_table_class(self):
         testperiod = mommy.make('core.Period')
         testuser = mommy.make(settings.AUTH_USER_MODEL)
