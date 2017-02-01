e751a9ad843514f67c2d88b11aefe0eaefa4a3ad
Stian Julseth <stianjul@gmail.com>
2017-02-01 14:33:58 +0100

    devilry_group: Cleanup. Removed batch-tasks for GroupComment.

--- devilry/devilry_group/tasks.py
+++ devilry/devilry_group/tasks.py
@@ -1,153 +1,111 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-# ievv imports
 import os
 
 from ievv_opensource.ievv_batchframework import batchregistry
 
 
-# class GroupCommentCompressAction(batchregistry.Action):
-#     """
-#     Compress all files that belong to a :obj:`~devilry_group.models.GroupComment`.
-#
-#     A task than will be run by `ievv_opensource`s batchframework in celery.
-#     """
-#     def execute(self):
-#         groupcomment = self.kwargs.get('context_object')
-#
-#         commentfiles = groupcomment.commentfile_set.all()
-#         archivename = '{}-comment.zip'.format(
-#             groupcomment.user_role
-#         )
-#
-#         # Get backend and path
-#         from devilry.devilry_compressionutil import backend_registry
-#         zipfile_backend_class = backend_registry.Registry.get_instance().get('devilry_group_local')
-#         zipfile_path = os.path.join('%d' % groupcomment.feedback_set.group.id,
-#                                     '%d' % groupcomment.feedback_set.id,
-#                                     '%d' % groupcomment.id,
-#                                     archivename)
-#
-#         # Get backend instance and set the path
-#         # to the archive and the archivename.
-#         zipfile_backend = zipfile_backend_class(
-#                 archive_path=zipfile_path,
-#                 archive_name=archivename,
-#                 readmode=False
-#         )
-#
-#         # Write files to archive
-#         for commentfile in commentfiles:
-#             zipfile_backend.add_file('{}'.format(commentfile.filename), commentfile.file.file)
-#         zipfile_backend.close()
-#
-#         # create archive meta entry
-#         from devilry.devilry_compressionutil.models import CompressedArchiveMeta
-#         CompressedArchiveMeta.objects.create_meta(
-#                 instance=groupcomment,
-#                 zipfile_backend=zipfile_backend
-#         )
-
-
-class FeedbackSetCompressAction(batchregistry.Action):
+class AbstractBaseBatchAction(batchregistry.Action):
     """
-    Compress all files that belong to a :obj:`~devilry_group.models.FeedbackSet`.
 
-    A task than will be run by `ievv_opensource`s batchframework in celery.
     """
-    def _get_archive_name(self, feedbackset):
+    #: Backend id to get from registry in function `get_backend()`.
+    #: Must be set in subclass.
+    backend_id = ''
+
+    def get_backend(self, zipfile_path, archive_name):
         """
+        Get and instance of the backend to use.
 
         Args:
-            feedbackset:
+            zipfile_path: Path to the archive.
+            archive_name: Name of the archive.
 
         Returns:
 
         """
-        from django.utils import timezone
-        return 'feedbackset-{}-{}-delivery.zip'.format(
-            feedbackset.id,
-            timezone.now().strftime('%Y-%m-%d_%H-%M-%S.%f')
-        )
-
-    def _get_archive_path(self, feedbackset, archivename):
-        """
-
-        Args:
-            feedbackset:
-            archivename:
-
-        Returns:
-
-        """
-        return os.path.join(
-            str(feedbackset.group.parentnode.parentnode_id),
-            str(feedbackset.group.parentnode.id),
-            str(feedbackset.group.id),
-            archivename)
-
-    def _zipfile_add(self, zipfile_backend, comment_file, feedback_set):
-        """
-
-        Args:
-            zipfile_backend:
-            comment_file:
-            feedback_set:
-
-        Returns:
-
-        """
-        # Where should the file recide in the zipped archive.
-        # The folder the file should be added under is set to 'delivery' by default.
-        sub_folder_type = 'delivery'
-
-        if comment_file.comment.user_role == 'student':
-            current_deadline = feedback_set.current_deadline()
-            if current_deadline and comment_file.comment.published_datetime > current_deadline:
-                sub_folder_type = 'uploaded_after_deadline'
-        elif comment_file.comment.user_role == 'examiner':
-            sub_folder_type = 'uploaded_by_examiner'
-
-        # Write file to backend on the path defined by os.path.join
-        zipfile_backend.add_file(
-            os.path.join(sub_folder_type, comment_file.filename),
-            comment_file.file.file)
-
-    def execute(self):
-        """
-        The task performed by BatchOperation.
-
-        Returns:
-
-        """
-        feedback_set = self.kwargs.get('context_object')
-
-        # Create the name for the actual archive.
-        archive_name = self._get_archive_name(feedbackset=feedback_set)
-
-        # Get backend and path
         from devilry.devilry_compressionutil import backend_registry
-        zipfile_backend_class = backend_registry.Registry.get_instance().get('devilry_group_local')
-
-        zipfile_path = self._get_archive_path(feedbackset=feedback_set, archivename=archive_name)
-
-        # Get backend instance
-        zipfile_backend = zipfile_backend_class(
+        zipfile_backend_class = backend_registry.Registry.get_instance().get(self.backend_id)
+        return zipfile_backend_class(
             archive_path=zipfile_path,
             archive_name=archive_name,
             readmode=False
         )
 
-        # Write files to archive
+    def add_file(self, zipfile_backend, sub_path, comment_file, current_deadline):
+        """
+        Args:
+            zipfile_backend:
+            sub_path:
+            comment_file:
+            current_deadline:
+        """
+        sub_folder_type = 'delivery'
+        if comment_file.comment.user_role == 'student':
+            if current_deadline and comment_file.comment.published_datetime > current_deadline:
+                sub_folder_type = 'uploaded_after_deadline'
+        elif comment_file.comment.user_role == 'examiner' \
+                or comment_file.comment.user_role == 'admin':
+            sub_folder_type = 'uploaded_by_examiners_and_admins'
+
+        # Write file to backend on the path defined by os.path.join
+        zipfile_backend.add_file(
+            os.path.join(sub_path, sub_folder_type, comment_file.filename),
+            comment_file.file.file)
+
+    def execute(self):
+        raise NotImplementedError()
+
+
+class FeedbackSetBatchMixin(object):
+    """
+    Mixin for adding FeedbackSet files to zipfile.
+
+    Must be included in class together with :class:`~.AbstractBaseBatchAction`.
+    """
+    def zipfile_add_feedbackset(self, zipfile_backend, feedback_set, sub_path=''):
         from devilry.devilry_group import models as group_models
         for group_comment in feedback_set.groupcomment_set.all():
             # Don't add files from comments that are not visible to everyone.
             if group_comment.visibility == group_models.GroupComment.VISIBILITY_VISIBLE_TO_EVERYONE:
                 for comment_file in group_comment.commentfile_set.all():
-                    self._zipfile_add(zipfile_backend=zipfile_backend,
-                                      comment_file=comment_file,
-                                      feedback_set=feedback_set)
+                    self.add_file(zipfile_backend=zipfile_backend,
+                                  sub_path=sub_path,
+                                  comment_file=comment_file,
+                                  current_deadline=feedback_set.current_deadline())
+
+
+class FeedbackSetCompressAction(AbstractBaseBatchAction, FeedbackSetBatchMixin):
+    """
+    Compress all files that belong to a :obj:`~devilry_group.models.FeedbackSet`.
+
+    A task than will be run by `ievv_opensource`s batchframework in celery.
+    """
+    backend_id = 'devilry_group_local'
+
+    def execute(self):
+        feedback_set = self.kwargs.get('context_object')
+
+        # Create the name for the actual archive.
+        from django.utils import timezone
+        archive_name = 'feedbackset-{}-{}-delivery.zip'.format(
+            feedback_set.id,
+            timezone.now().strftime('%Y-%m-%d_%H-%M-%S.%f'))
+
+        # create the path for the archive
+        zipfile_path = os.path.join(
+            str(feedback_set.group.parentnode.parentnode_id),
+            str(feedback_set.group.parentnode.id),
+            str(feedback_set.group.id),
+            archive_name)
+
+        # Get backend
+        zipfile_backend = self.get_backend(zipfile_path=zipfile_path, archive_name=archive_name)
+
+        # Add FeedbackSet files to archive.
+        self.zipfile_add_feedbackset(zipfile_backend=zipfile_backend, feedback_set=feedback_set)
+
         zipfile_backend.close()
 
         # create archive meta entry
@@ -155,4 +113,57 @@ class FeedbackSetCompressAction(batchregistry.Action):
         CompressedArchiveMeta.objects.create_meta(
                 instance=feedback_set,
                 zipfile_backend=zipfile_backend
+        )
+
+
+class AssignmentBatchMixin(object):
+    """
+    Mixin for adding FeedbackSet files to zipfile for all AssignmentGroups in the Assignment.
+
+    Must be included in class together with :class:`~.FeedbackSetBatchMixin`.
+    """
+    def add_assignment_groups(self, user, zipfile_backend, assignment):
+        for group in assignment.assignmentgroups.filter_examiner_has_access(user=user):
+            group_path = 'group-{}'.format(group)
+            for feedback_set in group.feedbackset_set.all():
+                feedback_set_path = 'deadline{}'.format(feedback_set.current_deadline())
+                self.zipfile_add_feedbackset(
+                    zipfile_backend=zipfile_backend,
+                    feedback_set=feedback_set,
+                    sub_path=os.path.join(group_path, feedback_set_path)
+                )
+
+
+class AssignmentCompressAction(AbstractBaseBatchAction, AssignmentBatchMixin, FeedbackSetBatchMixin):
+    backend_id = 'devilry_group_local'
+
+    def execute(self):
+        assignment = self.kwargs.get('context_object')
+        started_by_user = self.kwargs.get('started_by')
+
+        # Create name for the actual archive
+        from django.utils import timezone
+        archive_name = '{}-{}.zip'.format(
+            assignment.short_name,
+            timezone.now().strftime('%Y-%m-%d_%H-%M-%S.%f'))
+
+        # create path for the archive.
+        zipfile_path = os.path.join(
+            str(assignment.parentnode.id),
+            str(assignment.id),
+            archive_name
+        )
+
+        # get backend
+        zipfile_backend = self.get_backend(zipfile_path=zipfile_path, archive_name=archive_name)
+
+        self.add_assignment_groups(user=started_by_user, zipfile_backend=zipfile_backend, assignment=assignment)
+
+        zipfile_backend.close()
+
+        # create archive meta entry
+        from devilry.devilry_compressionutil.models import CompressedArchiveMeta
+        CompressedArchiveMeta.objects.create_meta(
+            instance=assignment,
+            zipfile_backend=zipfile_backend
         )
