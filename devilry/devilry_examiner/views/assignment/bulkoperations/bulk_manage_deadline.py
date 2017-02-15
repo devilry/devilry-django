dc52823a81d8c0a5b48c24bff87ab74d8039bb64
Stian Julseth <stianjul@gmail.com>
2017-02-15 13:03:41 +0100

    devilry_deadlinemanagement: New app handling deadline management.

---
+++ devilry/devilry_examiner/views/assignment/bulkoperations/bulk_manage_deadline.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django_cradmin import crapp
+from django_cradmin.crinstance import reverse_cradmin_url
+
+from devilry.devilry_deadlinemanagement.cradmin_app import ExaminerDeadlineManagementApp
+from devilry.devilry_deadlinemanagement.views import deadline_listview
+
+
+class ExaminerManageDeadlineView(deadline_listview.DeadlineListView):
+    def get_startapp_backlink_url(self):
+        return reverse_cradmin_url(
+            instanceid='devilry_examiner_assignment',
+            appname='grouplist',
+            roleid=self.request.cradmin_role.id,
+            viewname=crapp.INDEXVIEW_NAME
+        )
+
+
+class App(ExaminerDeadlineManagementApp):
+    appurls = [
+        crapp.Url(r'^$',
+                  ExaminerManageDeadlineView.as_view(),
+                  name=crapp.INDEXVIEW_NAME)
+    ]
+
