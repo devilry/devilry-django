ba13c94aed45d1a6ab0b1a1f1a9c2de78cfc2bf5
Espen A. Kristiansen <post@espenak.net>
2017-02-19 18:31:00 +0100

    admin...gradingconfiguration: Update to handle changes in a _setState method.

--- devilry/devilry_theme3/staticsources/devilry_theme3/scripts/javascript/widgets/GradingConfigurationWidget.js
+++ devilry/devilry_theme3/staticsources/devilry_theme3/scripts/javascript/widgets/GradingConfigurationWidget.js
@@ -2,16 +2,40 @@ import AbstractWidget from "ievv_jsbase/widget/AbstractWidget";
 import HtmlParser from "ievv_jsbase/dom/HtmlParser";
 
 export default class GradingConfigurationWidget extends AbstractWidget {
+  _getCustomTableAtoFExampleConfig() {
+    return [
+      ['F', 0],
+      ['D', 25],
+      ['C', 50],
+      ['B', 75],
+      ['A', 90],
+    ];
+  }
+
+  getDefaultConfig() {
+    return {
+      // grading_system_plugin_id: 'devilry_gradingsystemplugin_approved',
+      // points_to_grade_mapper: 'passed-failed',
+      grading_system_plugin_id: 'devilry_gradingsystemplugin_points',
+      points_to_grade_mapper: 'custom-table',
+      custom_table_data: this._getCustomTableAtoFExampleConfig()
+    };
+  }
+
   constructor(element) {
     super(element);
     this.logger = new window.ievv_jsbase_core.LoggerSingleton().getLogger(
       'devilry.GradingConfigurationWidget');
     this._onPluginIdRadioChange = this._onPluginIdRadioChange.bind(this);
+    this._onPointsToGradeMapperRadioChange = this._onPointsToGradeMapperRadioChange.bind(this);
     this._onRemoveCustomTableRow = this._onRemoveCustomTableRow.bind(this);
     this._onAddCustomTableRow = this._onAddCustomTableRow.bind(this);
+    this._onCustomTablePointInputBlur = this._onCustomTablePointInputBlur.bind(this);
+    this._onCustomTableGradeInputChange = this._onCustomTableGradeInputChange.bind(this);
+    this._onCustomTablePointInputChange = this._onCustomTablePointInputChange.bind(this);
     this._onSetupCustomTableAtoFExample = this._onSetupCustomTableAtoFExample.bind(this);
-    this.pluginIdRadios = Array.from(this.element.querySelectorAll(
-      '#div_id_grading_system_plugin_id input[type="radio"]'));
+
+    this.pluginIdElements = this._getPluginIdElements();
     this.passingGradeMinPointsWrapperElement = document.getElementById(
       'div_id_passing_grade_min_points');
     this.maxPointsLabelElement = this.element.querySelector(
@@ -19,17 +43,77 @@ export default class GradingConfigurationWidget extends AbstractWidget {
     this.maxPointsHelpTextElement = document.getElementById(
       'hint_id_max_points');
     this.pointsToGradeMapperElements = this._getPointsToGradeMapperElements();
-    const initialPluginId = this.element.querySelector(
-      '#div_id_grading_system_plugin_id input[checked]').value;
-    // let customTableRowTemplateElement = document.getElementById('id_custom_table_row_template');
-    // this.customTableRowTemplateElement = customTableRowTemplateElement.parentNode.removeChild(
-    //   customTableRowTemplateElement);
+    this.customTableWrapperElement = document.getElementById(
+      'id_custom_table_wrapper');
+
+    // const initialPluginId = this.element.querySelector(
+    //   '#div_id_grading_system_plugin_id input[checked]').value;
     this.customTableBodyElement = document.getElementById('id_custom_table_body');
     this.customTableAddRowButton = document.getElementById('id_custom_table_add_row_button');
     this.customTableSetuAtoFExampleButton = document.getElementById('id_custom_table_setup_atof_example_button');
-    this._updateUiForPlugin(initialPluginId);
-    this._buildCustomTableAtoFExample();
+
+    this._state = {};
+    this._setState({
+      grading_system_plugin_id: this.config.grading_system_plugin_id,
+      points_to_grade_mapper: this.config.points_to_grade_mapper,
+      custom_table_data: this.config.custom_table_data
+    }, true);
     this._addEventListeners();
+  }
+
+  _setState(statePatch, initial=false) {
+    let oldState = Object.assign({}, this._state);
+    this._state = Object.assign({}, this._state, statePatch);
+    console.log('statePatch', statePatch);
+    console.log('oldState', oldState);
+    console.log('newState', this._state);
+
+    if(initial) {
+      this.pluginIdElements[this._state.grading_system_plugin_id].input.checked = true;
+      this.pointsToGradeMapperElements[this._state.points_to_grade_mapper].input.checked = true;
+    }
+    if(this._state.grading_system_plugin_id == 'devilry_gradingsystemplugin_approved' && this._state.points_to_grade_mapper == 'custom-points') {
+      this._state.points_to_grade_mapper = 'passed-failed';
+    }
+
+    if(this._state.grading_system_plugin_id != oldState.grading_system_plugin_id) {
+      this._updateUiForPlugin();
+    }
+    if(this._state.points_to_grade_mapper != oldState.points_to_grade_mapper) {
+      this._updateUiForPointsToGradeMapper();
+    }
+  }
+
+  _updateUiForPlugin() {
+    let pluginId = this._state.grading_system_plugin_id;
+    if(pluginId == 'devilry_gradingsystemplugin_approved') {
+      this._updateUiForApprovedPlugin();
+    } else if(pluginId == 'devilry_gradingsystemplugin_points') {
+      this._updateUiForPointsPlugin();
+    } else {
+      throw new Error(`Unsupported grading_system_plugin: "${pluginId}"`);
+    }
+  }
+
+  _updateUiForPointsToGradeMapper() {
+    let mapper = this._state.points_to_grade_mapper;
+    if(mapper == 'custom-table') {
+      this.customTableWrapperElement.style.display = 'block';
+    } else {
+      this.customTableWrapperElement.style.display = 'none';
+    }
+  }
+
+  _getPluginIdElements() {
+    const inputElements = Array.from(this.element.querySelectorAll(
+      '#div_id_grading_system_plugin_id input[type="radio"]'));
+    let pluginIdElements = {};
+    for(let inputElement of inputElements) {
+      pluginIdElements[inputElement.value] = {
+        input: inputElement
+      };
+    }
+    return pluginIdElements;
   }
 
   _getPointsToGradeMapperElements() {
@@ -46,20 +130,21 @@ export default class GradingConfigurationWidget extends AbstractWidget {
   }
 
   _addEventListeners() {
-    for(let pluginIdRadio of this.pluginIdRadios) {
-      pluginIdRadio.addEventListener(
+    for(let value of Object.keys(this.pluginIdElements)) {
+      let inputElement = this.pluginIdElements[value].input;
+      inputElement.addEventListener(
         'change', this._onPluginIdRadioChange);
+    }
+    for(let value of Object.keys(this.pointsToGradeMapperElements)) {
+      let inputElement = this.pointsToGradeMapperElements[value].input;
+      inputElement.addEventListener(
+        'change', this._onPointsToGradeMapperRadioChange);
     }
     this.customTableAddRowButton.addEventListener('click', this._onAddCustomTableRow);
     this.customTableSetuAtoFExampleButton.addEventListener('click', this._onSetupCustomTableAtoFExample);
   }
 
-  destroy() {
-    for(let gradin_system_pluginIdRadio of this.pluginIdRadios) {
-      gradin_system_plugin_id_radio.removeEventListener(
-        'change', this._onPluginIdRadioChange);
-    }
-  }
+  destroy() {}
 
   _hidePointsToGradeMapperCustomTableChoice() {
     this.pointsToGradeMapperElements['custom-table'].wrapper.style.display = 'none';
@@ -94,21 +179,6 @@ export default class GradingConfigurationWidget extends AbstractWidget {
     this._showPointsToGradeMapperCustomTableChoice();
     const pluginConfig = this.config['devilry_gradingsystemplugin_points'];
     this._updateUiLabels(pluginConfig);
-  }
-
-  _updateUiForPlugin(pluginId) {
-    if(pluginId == 'devilry_gradingsystemplugin_approved') {
-      this._updateUiForApprovedPlugin();
-    } else if(pluginId == 'devilry_gradingsystemplugin_points') {
-      this._updateUiForPointsPlugin();
-    } else {
-      throw new Error(`Unsupported grading_system_plugin: "${pluginId}"`);
-    }
-  }
-
-  _onPluginIdRadioChange(event) {
-    const pluginId = event.target.value;
-    this._updateUiForPlugin(pluginId);
   }
 
   _getRowChildElements(rowElement) {
@@ -161,6 +231,9 @@ export default class GradingConfigurationWidget extends AbstractWidget {
     this.customTableBodyElement.appendChild(rowElement);
     let childElements = this._getRowChildElements(rowElement);
     childElements.removeButton.addEventListener('click', this._onRemoveCustomTableRow);
+    childElements.minimumPointsInput.addEventListener('blur', this._onCustomTablePointInputBlur);
+    childElements.minimumPointsInput.addEventListener('change', this._onCustomTablePointInputChange);
+    childElements.gradeInput.addEventListener('change', this._onCustomTableGradeInputChange);
     this._reindexCustomTable();
     return rowElement;
   }
@@ -186,14 +259,32 @@ export default class GradingConfigurationWidget extends AbstractWidget {
     }
   }
 
-  _buildCustomTableAtoFExample() {
-    this._buildCustomTable([
-      ['F', 0],
-      ['D', 25],
-      ['C', 50],
-      ['B', 75],
-      ['A', 90],
-    ]);
+  _onPluginIdRadioChange(event) {
+    const pluginId = event.target.value;
+    this._setState({
+      grading_system_plugin_id: pluginId
+    });
+  }
+
+  _onPointsToGradeMapperRadioChange(event) {
+    const value = event.target.value;
+    this._setState({
+      points_to_grade_mapper: value
+    });
+  }
+
+  _onCustomTableGradeInputChange(event) {
+    const value = event.target.value;
+    console.log('Change', value);
+  }
+
+  _onCustomTablePointInputChange(event) {
+    const value = event.target.value;
+    console.log('Change', value);
+  }
+
+  _onCustomTablePointInputBlur(event) {
+    console.log('BLUR');
   }
 
   _onAddCustomTableRow(event) {
@@ -207,7 +298,7 @@ export default class GradingConfigurationWidget extends AbstractWidget {
     event.preventDefault();
     if (window.confirm(
         'Are you sure you want to setup the A-F example? Clears the table and inserts new rows.')) {
-      this._buildCustomTableAtoFExample();
+      this._buildCustomTable(this._getCustomTableAtoFExampleConfig());
     }
   }
 
