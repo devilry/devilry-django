92ffb28d94942277e79e2efac1996a6d0bf3c99b
Eskil Nordland <eskil@appresso.no>
2017-02-20 20:39:03 +0100

    project/common/formats: fixed date format

--- devilry/project/common/formats/en/formats.py
+++ devilry/project/common/formats/en/formats.py
@@ -1,5 +1,5 @@
 # Locale formats English
-DATE_FORMAT = 'F j'
-SHORT_DATE_FORMAT = 'N j'
+DATE_FORMAT = 'F j Y'
+SHORT_DATE_FORMAT = 'N j y'
 DATETIME_FORMAT = 'l F j, Y, H:i'
 SHORT_DATETIME_FORMAT = 'M j Y, H:i'
