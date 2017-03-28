3f87f2d3dc357f65bc67f29b71626ea25d5054b5
Eskil Opdahl Nordland <eskil.nordland@gmail.com>
2017-03-28 10:12:06 +0200

    devilry/devilry_api/apps.py: removed GroupCommentCompressAction

--- devilry/devilry_api/apps.py
+++ devilry/devilry_api/apps.py
@@ -21,13 +21,13 @@ class DevilryAPIAppConfig(AppConfig):
         backend_registry.Registry.get_instance().add(DevilryApiZipBackend)
 
         # add actiongroup for zipping groupcomment files to registry
-        batchregistry.Registry.get_instance().add_actiongroup(
-            batchregistry.ActionGroup(
-                name='batchframework_api_compress_groupcomment',
-                mode=batchregistry.ActionGroup.MODE_SYNCHRONOUS,
-                actions=[
-                    tasks.GroupCommentCompressAction
-                ]))
+        # batchregistry.Registry.get_instance().add_actiongroup(
+        #     batchregistry.ActionGroup(
+        #         name='batchframework_api_compress_groupcomment',
+        #         mode=batchregistry.ActionGroup.MODE_SYNCHRONOUS,
+        #         actions=[
+        #             tasks.FeedbackSetCompressAction
+        #         ]))
 
         # add actiongroup for zipping all files in a feedbackset to registry
         batchregistry.Registry.get_instance().add_actiongroup(
