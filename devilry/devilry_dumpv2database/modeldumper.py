e95a5ecef85cfc3e847063fab918e0416e4f790c
Stian Julseth <stianjul@gmail.com>
2017-05-25 14:40:38 +0200

    devilry_dumpv2database: Creates meta info file for each model class. Can be extended with data by subclass.

--- devilry/devilry_dumpv2database/modeldumper.py
+++ devilry/devilry_dumpv2database/modeldumper.py
@@ -2,6 +2,7 @@ import json
 import os
 
 from django.core import serializers
+from django.db import models
 
 
 class ModelDumper(object):
@@ -28,6 +29,56 @@ class ModelDumper(object):
             self.output_root,
             '{}.{}'.format(model_class._meta.app_label,
                            model_class.__name__.lower()))
+
+    def _get_max_id_for_model_class(self):
+        """
+        Get the highest sequence number of this models objects.
+
+        Aggregates the primary key and returns the highest.
+        """
+        return self.get_model_class().objects\
+            .aggregate(max_id=models.Max('pk'))\
+            .get('max_id')
+
+    def get_meta_file_data(self, **kwargs):
+        """
+        Get meta info for model class.
+
+        Override this to add more meta info about the model class and return with
+        a call to super.
+
+        Example::
+            def get_meta_file_data(self, **kwargs):
+                additional_data = {
+                    'entry_count': self.get_object_count()
+                    'some': 'more',
+                    'data': 'to add'
+                }
+                return super(SubclassOfModelDumper, self).get_meta_file_data(**additional_data)
+        """
+        model_class = self.get_model_class()
+        model_meta_info = {
+            'app_label': model_class._meta.app_label,
+            'model_class_name': model_class.__name__,
+            'max_id': self._get_max_id_for_model_class()
+        }
+        model_meta_info.update(**kwargs)
+        return model_meta_info
+
+    def create_meta_file_for_model_class(self):
+        """
+        Creates a meta file in root directory.
+        The meta file for a model is the app label of the model. E.g `auth.user.json`
+        """
+        model_class = self.get_model_class()
+        meta_file_name = '{}.{}.json'.format(
+            model_class._meta.app_label,
+            model_class.__name__.lower()
+        )
+        meta_file_path = os.path.join(self.output_root, meta_file_name)
+        with open(meta_file_path, 'wb') as outfile:
+            outfile.write(json.dumps(self.get_meta_file_data()))
+            outfile.close()
 
     def optimize_queryset(self, queryset):
         """
@@ -70,6 +121,12 @@ class ModelDumper(object):
                     self.output_directory
                 ))
             os.makedirs(self.output_directory)
+            self.create_meta_file_for_model_class()
+        else:
+            print
+            print '## model meta for', self.get_model_class().__name__
+            print self.get_meta_file_data()
+            print
         for obj in self.get_object_iterator():
             serialized = self.serialize_model_object(obj)
             filepath = self.get_output_filepath_for_model_object(obj)
