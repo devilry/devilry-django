f38ebc313fbee23a69c29e9c8e6eae0c7f63c79e
Stian Julseth <stianjul@gmail.com>
2017-05-26 15:25:09 +0200

    devilry_import_v2database/v2dump_directoryparser.py: Reads metafile and sets the sequence number for objects created with auto incremented sequence number.

--- devilry/devilry_import_v2database/v2dump_directoryparser.py
+++ devilry/devilry_import_v2database/v2dump_directoryparser.py
@@ -1,6 +1,8 @@
 import json
 import os
 
+from django.db import connection
+
 
 class V2DumpDirectoryParser(object):
     def __init__(self, input_root):
@@ -32,6 +34,34 @@ class V2DumpDirectoryParser(object):
             self.input_root,
             '{}.{}'.format(self.get_app_label(),
                            self.get_model_name_lowercase()))
+
+    @property
+    def meta_file_for_model(self):
+        """
+        """
+        return '{}.{}.json'.format(self.get_app_label(), self.get_model_name_lowercase())
+
+    def get_model_class_meta_dict(self):
+        """
+        """
+        file_path = os.path.join(self.input_root, self.meta_file_for_model)
+        if not os.path.isfile(file_path):
+            return None
+        with open(file_path, 'rb') as file_object:
+            file_content = file_object.read()
+            return json.loads(file_content.decode('utf-8'))
+
+    def set_max_id_for_models_with_auto_generated_sequence_numbers(self, model_class):
+        """
+        """
+        meta_dict = self.get_model_class_meta_dict()
+        if not meta_dict:
+            return
+        sql = """
+        SELECT setval(pg_get_serial_sequence('{db_table}', 'id'), {max_id});
+        """.format(db_table=model_class._meta.db_table, max_id=meta_dict['max_id'])
+        cursor = connection.cursor()
+        cursor.execute(sql)
 
     def get_object_dict_by_filename(self, filename):
         filepath = os.path.join(self.input_directory, filename)
