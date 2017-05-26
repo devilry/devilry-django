213be20aec187946ae89bfce31a1de791588d4f1
Stian Julseth <stianjul@gmail.com>
2017-05-26 15:27:33 +0200

    devilry_import_v2database/tests/test_modelimporters/importer_testcase_mixin.py: Model meta file support for tests.

--- devilry/devilry_import_v2database/tests/test_modelimporters/importer_testcase_mixin.py
+++ devilry/devilry_import_v2database/tests/test_modelimporters/importer_testcase_mixin.py
@@ -9,10 +9,15 @@ class ImporterTestCaseMixin(object):
     def tearDown(self):
         shutil.rmtree(self.temp_root_dir)
 
-    def create_v2dump(self, model_name, data):
+    def create_v2dump(self, model_name, data, model_meta=None):
         self.temp_root_dir = tempfile.mkdtemp()
         full_path_to_model_folder = os.path.join(self.temp_root_dir, model_name)
         os.makedirs(full_path_to_model_folder)
         # for key, value in data.iteritems():
+        if model_meta:
+            meta_out_file = open('{}/{}.json'.format(self.temp_root_dir, model_name), 'wb')
+            meta_out_file.write(json.dumps(model_meta))
+            meta_out_file.close()
         fp = open('{}/{}.json'.format(full_path_to_model_folder, data['pk']), 'w+')
         fp.write(json.dumps(data))
+        fp.close()
