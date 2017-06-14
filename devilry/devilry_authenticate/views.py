198d931f289826461b31158ace63ffc9e177b7f9
Tor Ivar Johansen <tor@torjohansen.no>
2017-06-14 13:22:51 +0200

    devilry_authenticate: working username form 3.0.0a6

--- devilry/devilry_authenticate/views.py
+++ devilry/devilry_authenticate/views.py
@@ -1,10 +1,13 @@
-from crispy_forms import layout
 from django.contrib.auth import get_user_model
+from django import forms
+from django.utils.translation import ugettext_lazy as _
 from django_cradmin.apps.cradmin_authenticate.views.login import LoginView, UsernameLoginForm
 
 
 class CustomUsernameLoginForm(UsernameLoginForm):
     username_field = 'shortname'
+    shortname = forms.CharField(
+        label=_('Username'))
 
 
 class CustomLoginView(LoginView):
@@ -13,15 +16,3 @@ class CustomLoginView(LoginView):
         if user_model.USERNAME_FIELD == 'shortname':
             return CustomUsernameLoginForm
         return super(CustomLoginView, self).get_form_class()
-
-    def get_field_layout(self):
-        form_class = self.get_form_class()
-        return [
-            layout.Field(form_class.username_field,
-                         placeholder=form_class.username_field_placeholder,
-                         css_class='input-lg',
-                         focusonme='focusonme'),
-            layout.Field('password',
-                         placeholder=form_class.password_field_placeholder,
-                         css_class='input-lg'),
-        ]
