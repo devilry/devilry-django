87f47feaf20806856e7f1706e7a0d8652cb02c22
Tor Ivar Johansen <tor@torjohansen.no>
2017-06-14 13:10:30 +0200

    devilry_authenticate: 3.0.0a5

--- devilry/devilry_authenticate/views.py
+++ devilry/devilry_authenticate/views.py
@@ -1,8 +1,11 @@
+from crispy_forms import layout
 from django.contrib.auth import get_user_model
 from django_cradmin.apps.cradmin_authenticate.views.login import LoginView, UsernameLoginForm
 
+
 class CustomUsernameLoginForm(UsernameLoginForm):
     username_field = 'shortname'
+
 
 class CustomLoginView(LoginView):
     def get_form_class(self):
@@ -10,3 +13,15 @@ class CustomLoginView(LoginView):
         if user_model.USERNAME_FIELD == 'shortname':
             return CustomUsernameLoginForm
         return super(CustomLoginView, self).get_form_class()
+
+    def get_field_layout(self):
+        form_class = self.get_form_class()
+        return [
+            layout.Field(form_class.username_field,
+                         placeholder=form_class.username_field_placeholder,
+                         css_class='input-lg',
+                         focusonme='focusonme'),
+            layout.Field('password',
+                         placeholder=form_class.password_field_placeholder,
+                         css_class='input-lg'),
+        ]
