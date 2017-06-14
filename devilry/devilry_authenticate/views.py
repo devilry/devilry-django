5dbaee21aef1db1de7577dc1e808f0fa3fc68b67
Tor Ivar Johansen <tor@torjohansen.no>
2017-06-14 11:58:53 +0200

    devilry_authenticate: Fix username_field in Form "3.0.0a4"

--- devilry/devilry_authenticate/views.py
+++ devilry/devilry_authenticate/views.py
@@ -1,10 +1,12 @@
 from django.contrib.auth import get_user_model
 from django_cradmin.apps.cradmin_authenticate.views.login import LoginView, UsernameLoginForm
 
+class CustomUsernameLoginForm(UsernameLoginForm):
+    username_field = 'shortname'
 
 class CustomLoginView(LoginView):
     def get_form_class(self):
         user_model = get_user_model()
         if user_model.USERNAME_FIELD == 'shortname':
-            return UsernameLoginForm
+            return CustomUsernameLoginForm
         return super(CustomLoginView, self).get_form_class()
