cc09671caada058cbcb54dd87e99f5b0388c70d4
Tor Ivar Johansen <tor@torjohansen.no>
2017-06-19 12:48:09 +0200

    3.0.0a9

--- devilry/devilry_authenticate/views.py
+++ devilry/devilry_authenticate/views.py
@@ -1,5 +1,5 @@
-from django.contrib.auth import get_user_model
 from django import forms
+from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
 from django_cradmin.apps.cradmin_authenticate.views.login import LoginView, UsernameLoginForm
 
@@ -9,29 +9,14 @@ class CustomUsernameLoginForm(UsernameLoginForm):
     shortname = forms.CharField(
         label=_('Username'))
 
-    def clean(self):
-        """
-        validate the form, and execute :func:`django.contrib.auth.authenticate` to login the user if form is valid.
-        """
-        username = self.cleaned_data.get(self.username_field)
-        password = self.cleaned_data.get('password')
-        if username and password:
-            authenticated_user = self.authenticate(**{
-                'username': username,
-                'password': password
-            })
-
-            if authenticated_user is None:
-                raise forms.ValidationError(self.error_message_invalid_login)
-            elif not authenticated_user.is_active:
-                raise forms.ValidationError(self.error_message_inactive)
-            self.__authenticated_user = authenticated_user
-        return self.cleaned_data
+    def authenticate(self, **kwargs):
+        username = kwargs.get(self.username_field)
+        password = kwargs.get('password')
+        return super(CustomUsernameLoginForm, self).authenticate(username=username, password=password)
 
 
 class CustomLoginView(LoginView):
     def get_form_class(self):
-        user_model = get_user_model()
-        if user_model.USERNAME_FIELD == 'shortname':
+        if not getattr(settings, 'DJANGO_CRADMIN_USE_EMAIL_AUTH_BACKEND', False):
             return CustomUsernameLoginForm
         return super(CustomLoginView, self).get_form_class()
