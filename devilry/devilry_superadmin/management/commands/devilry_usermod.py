60f9f249918a7be5c55fd9492c68678a0e774406
Espen A. Kristiansen <post@espenak.net>
2017-06-20 12:34:01 +0200

    superadmin...usermod: Cleanup and --email support.

--- devilry/devilry_superadmin/management/commands/devilry_usermod.py
+++ devilry/devilry_superadmin/management/commands/devilry_usermod.py
@@ -1,7 +1,9 @@
+from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
 from django.core.management.base import BaseCommand, CommandError
 
+from devilry.devilry_account.models import UserEmail
 from devilry.utils.management import add_input_encoding_argument
 
 
@@ -30,27 +32,30 @@ class Command(UserModCommand):
 
     def add_arguments(self, parser):
         parser.add_argument(
-            'shortname',
-            default='',
-            help='Shortname. The username if authenticating with username, '
-                 'and email if not.'),
+            'username_or_email',
+            help='The username if authenticating with username, '
+                 'and email if not.')
+        parser.add_argument(
+            '--email',
+            help='An email address that will be added if it is not registered '
+                 'for the user.')
         parser.add_argument(
             '--full_name',
             dest='full_name',
             default=None,
-            help='Full name'),
+            help='Full name')
         parser.add_argument(
             '--superuser',
             action='store_true',
             dest='superuser',
             default=False,
-            help='Make the user a superuser, with access to everything in the system.'),
+            help='Make the user a superuser, with access to everything in the system.')
         parser.add_argument(
             '--normaluser',
             action='store_true',
             dest='normaluser',
             default=False,
-            help='Make the user a normal user, with access to everything that they are given explicit access to.'),
+            help='Make the user a normal user, with access to everything that they are given explicit access to.')
         add_input_encoding_argument(parser)
 
     def handle(self, *args, **options):
@@ -63,9 +68,12 @@ class Command(UserModCommand):
             kw['is_superuser'] = False
 
         try:
-            user = get_user_model().objects.get(shortname=options['shortname'])
+            if settings.DJANGO_CRADMIN_USE_EMAIL_AUTH_BACKEND:
+                user = get_user_model().objects.get_by_email(email=options['username_or_email'])
+            else:
+                user = get_user_model().objects.get_by_username(username=options['username_or_email'])
         except get_user_model().DoesNotExist:
-            raise CommandError('User "{}" does not exist.'.format(options['shortname']))
+            raise CommandError('User "{}" does not exist.'.format(options['username_or_email']))
         else:
             for key, value in kw.iteritems():
                 setattr(user, key, value)
@@ -73,7 +81,18 @@ class Command(UserModCommand):
                 user.set_unusable_password()
             full_name = options.get('full_name')
             if full_name:
-                user.fullname = unicode(full_name, self.inputencoding)
+                try:
+                    user.fullname = unicode(full_name, self.inputencoding)
+                except TypeError:
+                    user.fullname = full_name
+            if options['email']:
+                if not user.useremail_set.filter(email=options['email']).exists():
+                    useremail = UserEmail(
+                        user=user,
+                        email=options['email']
+                    )
+                    useremail.full_clean()
+                    useremail.save()
             user.full_clean()
             user.save()
             if verbosity > 0:
