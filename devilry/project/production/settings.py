ec423759f33286e0711b18bd31fb1840250e4a5b
Stian Julseth <stianjul@gmail.com>
2017-09-27 11:38:25 +0200

    project: Uncomment, but keep default prod setting.

--- devilry/project/production/settings.py
+++ devilry/project/production/settings.py
@@ -16,25 +16,25 @@ ALLOWED_HOSTS = ['*']
 # Celery/Redis config
 #
 #####################################################
-redis_url_default = urlparse.urlparse(os.environ.get('REDIS_URL'))
-
-REDIS_URL_CONFIG = {
-    'port': redis_url_default.port,
-    'hostname': redis_url_default.hostname,
-    'username': redis_url_default.username,
-    'password': redis_url_default.password,
-}
-
-BROKER_URL = 'redis://{username}:{password}@{hostname}:{port}'.format(
-    username=REDIS_URL_CONFIG['username'],
-    password=REDIS_URL_CONFIG['password'],
-    hostname=REDIS_URL_CONFIG['hostname'],
-    port=REDIS_URL_CONFIG['port']
-)
-
-CELERY_RESULT_BACKEND = 'redis://{username}:{password}@{hostname}:{port}'.format(
-    username=REDIS_URL_CONFIG['username'],
-    password=REDIS_URL_CONFIG['password'],
-    hostname=REDIS_URL_CONFIG['hostname'],
-    port=REDIS_URL_CONFIG['port']
-)
+# redis_url_default = urlparse.urlparse(os.environ.get('REDIS_URL'))
+#
+# REDIS_URL_CONFIG = {
+#     'port': redis_url_default.port,
+#     'hostname': redis_url_default.hostname,
+#     'password': redis_url_default.password,
+#     'db_number': 0
+# }
+#
+# BROKER_URL = 'redis://:{password}@{hostname}:{port}/{db_number}'.format(
+#     password=REDIS_URL_CONFIG['password'],
+#     hostname=REDIS_URL_CONFIG['hostname'],
+#     port=REDIS_URL_CONFIG['port'],
+#     db_number=REDIS_URL_CONFIG['db_number']
+# )
+#
+# CELERY_RESULT_BACKEND = 'redis://:{password}@{hostname}:{port}/{db_number}'.format(
+#     password=REDIS_URL_CONFIG['password'],
+#     hostname=REDIS_URL_CONFIG['hostname'],
+#     port=REDIS_URL_CONFIG['port'],
+#     db_number=REDIS_URL_CONFIG['db_number']
+# )
