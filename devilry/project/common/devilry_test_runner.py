b1e4e6191895178520c9bd8b9d132b9ccbdbf8b4
Espen A. Kristiansen <t@espenak.net>
2018-01-16 10:49:07 +0100

    project.common: Custom test runner.

---
+++ devilry/project/common/devilry_test_runner.py
@@ -0,0 +1,13 @@
+import warnings
+
+from django.test.runner import DiscoverRunner
+from django.utils.deprecation import RemovedInDjango20Warning, RemovedInDjango110Warning
+
+
+class DevilryTestRunner(DiscoverRunner):
+    def setup_test_environment(self, **kwargs):
+        # warnings.filterwarnings('ignore', category=RemovedInDjango)
+        super(DevilryTestRunner, self).setup_test_environment(**kwargs)
+        warnings.filterwarnings('ignore', category=RemovedInDjango20Warning)
+        warnings.filterwarnings('ignore', category=DeprecationWarning)
+        warnings.filterwarnings('ignore', category=RemovedInDjango110Warning)
