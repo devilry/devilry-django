90c0738c39f7ace2bfc92376c291998f549a00e4
Espen A. Kristiansen <t@espenak.net>
2018-01-19 14:05:32 +0100

    admin...assignment.first_deadline: Back button in header.

--- devilry/devilry_admin/views/assignment/first_deadline.py
+++ devilry/devilry_admin/views/assignment/first_deadline.py
@@ -8,8 +8,17 @@ from devilry.apps.core import models as coremodels
 
 class AssignmentFirstDeadlineUpdateView(OnlySaveButtonMixin, UpdateView):
     model = coremodels.Assignment
+    template_name = 'devilry_cradmin/viewhelpers/devilry_updateview_with_backlink.django.html'
 
     fields = ['first_deadline']
 
     def get_queryset_for_role(self, role):
         return self.model.objects.filter(id=self.kwargs['pk'])
+
+    def get_backlink_url(self):
+        return self.request.cradmin_instance.rolefrontpage_url()
+
+    def get_context_data(self, **kwargs):
+        context = super(AssignmentFirstDeadlineUpdateView, self).get_context_data(**kwargs)
+        context['backlink_url'] = self.get_backlink_url()
+        return context
