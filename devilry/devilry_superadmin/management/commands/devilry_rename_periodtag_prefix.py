80a3807ede2ac3e2f161cec88ed0a8780adadc6d
Stian Julseth <stianjul@gmail.com>
2018-02-13 15:50:47 +0100

    #949: devilry_superadmin/management: Add command for renaming periodtag prefixes.

---
+++ devilry/devilry_superadmin/management/commands/devilry_rename_periodtag_prefix.py
@@ -0,0 +1,36 @@
+from django.core.management.base import BaseCommand
+
+from devilry.apps.core.models import PeriodTag
+
+
+class Command(BaseCommand):
+    args = 'Rename period tag prefixes'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--tag-prefix-old',
+            dest='tag_prefix_old',
+            default=None,
+            help='The prefix to replace.'
+        )
+        parser.add_argument(
+            '--tag-prefix-new',
+            dest='tag_prefix_new',
+            default=None,
+            help='The prefix to update period tags with.'
+        )
+
+    def handle(self, *args, **options):
+        tag_prefix_old = options.get('tag_prefix_old')
+        tag_prefix_new = options.get('tag_prefix_new')
+        if not tag_prefix_new or not tag_prefix_old:
+            raise ValueError('missing tag_prefix_old and tag_prefix_new params.')
+        periodtag_queryset = PeriodTag.objects.filter(prefix=tag_prefix_old)
+
+        self.stdout.write(
+            'Updating prefix from \'{}\' to \'{}\' for {} period tags'.format(
+                tag_prefix_old,
+                tag_prefix_new,
+                periodtag_queryset.count()))
+
+        periodtag_queryset.update(prefix=tag_prefix_new)
