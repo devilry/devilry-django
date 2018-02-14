4de68a143dd54e7cf6e16f0f8fcb8aefd70c4a60
Stian Julseth <stianjul@gmail.com>
2018-02-14 17:30:08 +0100

    #953: (work-in-progress) Added manually pass students from previous semester.

---
+++ devilry/devilry_admin/views/assignment/passed_previous_period/__init__.py
@@ -0,0 +1,34 @@
+from django_cradmin import crapp
+
+from devilry.devilry_admin.views.assignment.passed_previous_period import overview
+from devilry.devilry_admin.views.assignment.passed_previous_period import passed_previous_period
+from devilry.devilry_admin.views.assignment.passed_previous_period import passed_previous_semester_manual
+
+
+class App(crapp.App):
+    appurls = [
+        crapp.Url(
+            r'^$',
+            overview.Overview.as_view(),
+            name=crapp.INDEXVIEW_NAME
+        ),
+
+        # Auto pass students on selected period.
+        crapp.Url(
+            r'^select-period$',
+            passed_previous_period.SelectPeriodView.as_view(),
+            name='select_period'),
+        crapp.Url(
+            r'^assignment/(?P<period_id>\d+)$',
+            passed_previous_period.PassedPreviousAssignmentView.as_view(),
+            name='assignments'),
+        crapp.Url(
+            r'^confirm/(?P<period_id>\d+)$',
+            passed_previous_period.ApprovePreviousAssignments.as_view(),
+            name='confirm'),
+
+        # Manually select students to pass.
+        crapp.Url(r'^select-groups/(?P<filters_string>.+)?$',
+                  passed_previous_semester_manual.PassAssignmentGroupsView.as_view(),
+                  name='manually_select_groups'),
+    ]
