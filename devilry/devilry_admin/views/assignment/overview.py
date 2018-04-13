7ba1c104f5a66b959453b9de2e93b1d775d4df3c
Stian Julseth <stianjul@gmail.com>
2018-04-13 11:37:31 +0200

    #979: Possibility to switch deadline handling on assignment is ready but disabled pending related issue.

--- devilry/devilry_admin/views/assignment/overview.py
+++ devilry/devilry_admin/views/assignment/overview.py
@@ -8,7 +8,9 @@ from devilry.apps.core.models import Assignment
 from devilry.apps.core.models import Examiner
 from devilry.apps.core.models import RelatedExaminer
 from devilry.apps.core.models import RelatedStudent
+from devilry.devilry_account.models import SubjectPermissionGroup
 from devilry.devilry_admin.views.assignment.anonymizationmode import AssignmentAnonymizationmodeUpdateView
+from devilry.devilry_admin.views.assignment.deadline_handling import AssignmentDeadlineHandlingUpdateView
 from devilry.devilry_admin.views.assignment.gradingconfiguration import AssignmentGradingConfigurationUpdateView
 from devilry.devilry_admin.views.assignment.long_and_shortname import AssignmentLongAndShortNameUpdateView
 from devilry.devilry_admin.views.assignment.projectgroups import AssignmentProjectGroupUpdateView
@@ -62,6 +64,11 @@ class Overview(DetailRoleView):
             self._assignment = queryset.get()
         return self._assignment
 
+    def get_user_is_subjectadmin_or_higher(self):
+        return SubjectPermissionGroup.objects\
+            .get_devilryrole_for_user_on_subject(
+                user=self.request.user, subject=self.assignment.parentnode.parentnode) is not None
+
     def get_context_data(self, **kwargs):
         context = super(Overview, self).get_context_data(**kwargs)
         context['assignmentgroups_count'] = self.get_assignmentgroups_count()
@@ -70,6 +77,7 @@ class Overview(DetailRoleView):
         context['assignment'] = self.assignment
         context['relatedstudents_count'] = self.get_related_students_count()
         context['relatedexaminers_count'] = self.get_related_examiners_count()
+        context['user_is_subjectadmin_or_higher'] = self.get_user_is_subjectadmin_or_higher()
         context['show_info_box'] = self.show_info_box(
             context['assignment'],
             context['candidates_count'],
@@ -81,19 +89,39 @@ class Overview(DetailRoleView):
 
 class App(crapp.App):
     appurls = [
-        crapp.Url(r'^$', Overview.as_view(), name=crapp.INDEXVIEW_NAME),
-        crapp.Url(r'^update_assignment_short_and_long_name/(?P<pk>\d+)$', AssignmentLongAndShortNameUpdateView.as_view(),
+        crapp.Url(r'^$',
+                  Overview.as_view(),
+                  name=crapp.INDEXVIEW_NAME),
+
+        crapp.Url(r'^update_assignment_short_and_long_name/(?P<pk>\d+)$',
+                  AssignmentLongAndShortNameUpdateView.as_view(),
                   name="update_assignment_short_and_long_name"),
-        crapp.Url(r'^update_publishing_time/(?P<pk>\d+)$', AssignmentPublishingTimeUpdateView.as_view(),
+
+        crapp.Url(r'^update_publishing_time/(?P<pk>\d+)$',
+                  AssignmentPublishingTimeUpdateView.as_view(),
                   name="update_publishing_time"),
-        crapp.Url(r'^publish_assignment_now/(?P<pk>\d+)$', PublishNowRedirectView.as_view(),
+
+        crapp.Url(r'^publish_assignment_now/(?P<pk>\d+)$',
+                  PublishNowRedirectView.as_view(),
                   name="publish_assignment_now"),
-        crapp.Url(r'^update_first_deadline/(?P<pk>\d+)$', AssignmentFirstDeadlineUpdateView.as_view(),
+
+        crapp.Url(r'^update_first_deadline/(?P<pk>\d+)$',
+                  AssignmentFirstDeadlineUpdateView.as_view(),
                   name="update_first_deadline"),
-        crapp.Url(r'^update_gradingconfiguration/(?P<pk>\d+)$', AssignmentGradingConfigurationUpdateView.as_view(),
+
+        crapp.Url(r'^update_gradingconfiguration/(?P<pk>\d+)$',
+                  AssignmentGradingConfigurationUpdateView.as_view(),
                   name="update_gradingconfiguration"),
-        crapp.Url(r'^update_projectgroup_settings/(?P<pk>\d+)$', AssignmentProjectGroupUpdateView.as_view(),
+
+        crapp.Url(r'^update_projectgroup_settings/(?P<pk>\d+)$',
+                  AssignmentProjectGroupUpdateView.as_view(),
                   name="update_projectgroup_settings"),
-        crapp.Url(r'^update_anonymizationmode/(?P<pk>\d+)$', AssignmentAnonymizationmodeUpdateView.as_view(),
+
+        crapp.Url(r'^update_anonymizationmode/(?P<pk>\d+)$',
+                  AssignmentAnonymizationmodeUpdateView.as_view(),
                   name="update_anonymizationmode"),
+
+        # crapp.Url(r'^update_deadlinehandling/(?P<pk>\d+)',
+        #           AssignmentDeadlineHandlingUpdateView.as_view(),
+        #           name='update_deadline_handling')
     ]
