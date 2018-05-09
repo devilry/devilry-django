4cec5a385a0a1a8759bcfc681fe3e9b72de64a03
Stian Julseth <stianjul@gmail.com>
2018-05-09 15:31:09 +0200

    Remove auto_now_add on Comment.created_datetime and use null=False blank=False default=timezone.now

--- devilry/devilry_comment/admin.py
+++ devilry/devilry_comment/admin.py
@@ -5,9 +5,12 @@ from devilry.devilry_comment import models
 class CommentAdmin(admin.ModelAdmin):
     pass
 
+
 admin.site.register(models.Comment, CommentAdmin)
+
 
 class CommentFileadmin(admin.ModelAdmin):
     pass
+
 
 admin.site.register(models.CommentFile, CommentFileadmin)
