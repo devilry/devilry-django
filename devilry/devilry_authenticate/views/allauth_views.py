73bcb537cd687f3472176ae2d3036ee40b138986
Stian Julseth <stianjul@gmail.com>
2018-05-30 13:44:56 +0200

    Fix next url redirect not working

--- devilry/devilry_authenticate/views/allauth_views.py
+++ devilry/devilry_authenticate/views/allauth_views.py
@@ -22,7 +22,10 @@ class AllauthLoginView(LoginView):
                         request=self.request,
                         process='login'))
             else:
+                print reverse('cradmin-authenticate-login')
                 return HttpResponseRedirect(reverse('cradmin-authenticate-login'))
+        print 'LOL'
+        print self.request.GET
         return super(AllauthLoginView, self).get(*args, **kwargs)
 
 
