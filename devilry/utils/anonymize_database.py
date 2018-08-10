fb37349623dea6973e443e0a7f8f771271b4627b
Stian Julseth <stianjul@gmail.com>
2018-08-10 16:14:52 +0200

    utils: (work-in-progress) Add database anonymizer.

--- devilry/utils/anonymize_database.py
+++ devilry/utils/anonymize_database.py
@@ -1,25 +1,173 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import random
+
+from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.db import models
 from django.db.models import CharField
 from django.db.models.functions import Concat
 
 from devilry.devilry_account.models import UserEmail, UserName
+from devilry.devilry_comment.models import Comment, CommentEditHistory, CommentFile
+
+lorem_ipsum = """Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+Sed malesuada sagittis ipsum, quis malesuada sem placerat non. 
+Donec metus urna, consectetur a laoreet vitae, dapibus sit amet odio. 
+Morbi facilisis, nisl ut pellentesque consectetur, ipsum ante varius lectus, sed commodo felis metus 
+sagittis neque. Donec vitae tortor magna. 
+Nullam in massa quis sapien dignissim ullamcorper et quis urna. 
+Aenean facilisis quis mauris a porttitor. 
+Integer accumsan dolor sagittis sem sagittis sollicitudin quis bibendum diam. 
+Suspendisse malesuada, neque quis condimentum elementum, odio arcu pulvinar erat, 
+vitae imperdiet mauris turpis eu magna."""
 
 
-def anonymize_user(fast=True):
+class AnonymizeDatabaseException(Exception):
+    def __init__(self, message, *args, **kwargs):
+        self.message = message
+        super(AnonymizeDatabaseException, self).__init__(*args, **kwargs)
+
+
+class AnonymizeDatabase(object):
     """
-    Anonymize user and related UserEmail and UserName.
+    Anonymizes:
+        - User
+        - UserEmail
+        - UserNames
+        - Comments (comment text)
+        - CommentFiles (filename)
 
-    ``fast=True`` replaces:
-     - User.shortname with User.id
-     - UserEmail.email with <UserEmail.user_id@example.com>.
-     - UserName.username with UserName.user_id.
+
+
+    Generate a anonymized name based on the unanonymized name
+    and keeps the same length.
+
+    Replaces:
+        - Digits
+        - lower- and uppercase letters.
+
+    Does not replace:
+        - spaces
+        - hyphens and underscores
+        - special characters
+
+    Args:
+        unanonymized_string: The string to anonymize.
     """
-    if fast:
-        # Update user models
-        get_user_model().objects.update(
-            shortname=Concat(models.F('id'), models.Value(''), output_field=CharField()))
+    FALLBACK = 'empty'
+    LETTERS = 'abcdefghijklmnopqrstuvwxyz'
+    DIGITS = '0123456789'
+    NOOP_CHARACTERS = [' ', '_', '@', '-', '"']
+
+    def __init__(self, db_alias, fast=True):
+        if db_alias == 'default':
+            raise AnonymizeDatabaseException(message='Can not use database {}.'.format(db_alias))
+        self.db_alias = db_alias
+        self.fast = fast
+
+    def is_uppercase(self, character):
+        """
+        Check if character is uppercase.
+        """
+        return character.isupper()
+
+    def get_random_character(self, exclude_character):
+        """
+        Randomize the character.
+        """
+        if exclude_character.isdigit():
+            character_list = list(self.DIGITS)
+        else:
+            character_list = list(self.LETTERS)
+        character_list.remove(exclude_character.lower())
+        random_choice = random.choice(character_list)
+        if exclude_character.isupper():
+            return random_choice.upper()
+        return random_choice.lower()
+
+    def randomize_string(self, unanonymized_string):
+        """
+        Start randomizing the string.
+
+        Returns:
+            str: Randomized string.
+        """
+        if len(unanonymized_string) == 0 or unanonymized_string is None:
+            return self.FALLBACK
+        anonymized_string = ''
+        for character in list(unanonymized_string):
+            if character in self.NOOP_CHARACTERS:
+                anonymized_string += character
+            else:
+                anonymized_string += self.get_random_character(
+                    exclude_character=character)
+        return anonymized_string
+
+    def __anonymize_user_data_fast(self):
+        """
+        Simply sets usernames to the ID of the user.
+        """
+        if settings.DJANGO_CRADMIN_USE_EMAIL_AUTH_BACKEND:
+            get_user_model().objects.update(
+                fullname='Full Name',
+                lastname='Lastname',
+                shortname=Concat(models.F('id'), models.Value('@example.com'), output_field=CharField()))
+        else:
+            get_user_model().objects.update(
+                fullname='Full Name',
+                lastname='Lastname',
+                shortname=Concat(models.F('id'), models.Value(''), output_field=CharField()))
         UserEmail.objects.update(
-            email=Concat(models.F('user_id'), models.Value('@example.com'), output_field=CharField()))
+            email=Concat(models.F('user_id'), models.Value('_'), models.F('id'),
+                         models.Value('@example.com'), output_field=CharField()))
         UserName.objects.update(
-            username=Concat(models.F('user_id'), models.Value(''), output_field=CharField()))
+            username=Concat(models.F('user_id'), models.Value('_'),
+                            models.F('id'), output_field=CharField()))
+
+    def __anonymize_user_emails(self, user):
+        for user_email in UserEmail.objects.filter(user_id=user.id):
+            email_prefix = user_email.email.split('@')[0]
+            anonymized_email_prefix = self.randomize_string(unanonymized_string=email_prefix)
+            user_email.email = '{}@example.com'.format(anonymized_email_prefix)
+            user_email.save()
+
+    def __anonymize_user_names(self, user):
+        for user_name in UserName.objects.filter(user_id=user.id):
+            anonymized_user_name = self.randomize_string(unanonymized_string=user_name.username)
+            user_name.username = anonymized_user_name
+            user_name.save()
+
+    def __anonymize_user_data(self):
+        for user in get_user_model().objects.all():
+            shortname = user.shortname
+            if '@' in shortname:
+                shortname = shortname.split('@')[0]
+            anonymized_shortname = self.randomize_string(unanonymized_string=shortname)
+            if settings.DJANGO_CRADMIN_USE_EMAIL_AUTH_BACKEND:
+                anonymized_shortname += '@example.com'
+            user.shortname = anonymized_shortname
+            user.fullname = self.randomize_string(unanonymized_string=user.fullname)
+            user.lastname = self.randomize_string(unanonymized_string=user.lastname)
+            user.save()
+
+            self.__anonymize_user_emails(user=user)
+            self.__anonymize_user_names(user=user)
+
+    def anonymize_user(self):
+        if self.fast:
+            self.__anonymize_user_data_fast()
+        else:
+            self.__anonymize_user_data()
+
+    def __anonymize_comments_fast(self):
+        """
+        Anonymize Comment text and CommentFile filenames.
+        """
+        Comment.objects.update(text=lorem_ipsum)
+        CommentEditHistory.objects.update(post_edit_text=lorem_ipsum, pre_edit_text=lorem_ipsum)
+        CommentFile.objects.update(filename=Concat(models.F('id'), models.Value(''), output_field=CharField()))
+
+    def anonymize_comments(self):
+        self.__anonymize_comments_fast()
--- devilry/utils/tests/test_anonymize_db.py
+++ devilry/utils/tests/test_anonymize_db.py
@@ -1,35 +1,102 @@
 from django import test
 from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.test import override_settings
 from model_mommy import mommy
 
 from devilry.devilry_account.models import UserEmail, UserName
+from devilry.devilry_comment.models import Comment, CommentEditHistory, CommentFile
+from devilry.devilry_group.models import GroupComment, GroupCommentEditHistory
 from devilry.utils import anonymize_database
+
+
+class TestAnonymizeString(test.TestCase):
+    # from_db_alias = 'anonymize_db'
+    # multi_db = True
+
+    def test_simple_lowercase_string(self):
+        string_to_anonymize = 'test'
+        anonymized_string = anonymize_database.AnonymizeDatabase(db_alias='test').randomize_string(
+            unanonymized_string=string_to_anonymize)
+        self.assertNotEqual(anonymized_string, string_to_anonymize)
+        self.assertEqual(len(anonymized_string), len(string_to_anonymize))
+        self.assertTrue(anonymized_string.islower())
+
+    def test_simple_uppercase_string(self):
+        string_to_anonymize = 'TEST'
+        anonymized_string = anonymize_database.AnonymizeDatabase(db_alias='test').randomize_string(
+            unanonymized_string=string_to_anonymize)
+        self.assertNotEqual(anonymized_string, string_to_anonymize)
+        self.assertEqual(len(anonymized_string), len(string_to_anonymize))
+        self.assertTrue(anonymized_string.isupper())
+
+    def test_upper_and_lowercase(self):
+        string_to_anonymize = 'TeSt'
+        anonymized_string = anonymize_database.AnonymizeDatabase(db_alias='test').randomize_string(
+            unanonymized_string=string_to_anonymize)
+        self.assertNotEqual(anonymized_string, string_to_anonymize)
+        self.assertEqual(len(anonymized_string), len(string_to_anonymize))
+        self.assertTrue(anonymized_string[0].isupper())
+        self.assertTrue(anonymized_string[1].islower())
+        self.assertTrue(anonymized_string[2].isupper())
+        self.assertTrue(anonymized_string[3].islower())
+
+    def test_upper_and_lowercase_with_digits(self):
+        string_to_anonymize = 'Te2St6'
+        anonymized_string = anonymize_database.AnonymizeDatabase(db_alias='test').randomize_string(
+            unanonymized_string=string_to_anonymize)
+        self.assertNotEqual(anonymized_string, string_to_anonymize)
+        self.assertEqual(len(anonymized_string), len(string_to_anonymize))
+        self.assertTrue(anonymized_string[0].isupper())
+        self.assertTrue(anonymized_string[1].islower())
+        self.assertTrue(anonymized_string[2].isdigit())
+        self.assertTrue(anonymized_string[3].isupper())
+        self.assertTrue(anonymized_string[4].islower())
+        self.assertTrue(anonymized_string[5].isdigit())
+
+    def test_whitespace_character(self):
+        string_to_anonymize = 'Test Test'
+        anonymized_string = anonymize_database.AnonymizeDatabase(db_alias='test').randomize_string(
+            unanonymized_string=string_to_anonymize)
+        self.assertNotEqual(anonymized_string, string_to_anonymize)
+        self.assertEqual(len(anonymized_string), len(string_to_anonymize))
+        self.assertEqual(anonymized_string[4], ' ')
 
 
 class TestAnonymizeUserFast(test.TestCase):
     def test_anonymize_user_shortname(self):
         mommy.make(settings.AUTH_USER_MODEL, shortname='testuser')
         self.assertEqual(get_user_model().objects.first().shortname, 'testuser')
-        anonymize_database.anonymize_user()
+        anonymize_database.AnonymizeDatabase(db_alias='test').anonymize_user()
         self.assertEqual(get_user_model().objects.first().shortname,
                          str(get_user_model().objects.get().id))
+
+    @override_settings(DJANGO_CRADMIN_USE_EMAIL_AUTH_BACKEND=True)
+    def test_short_name_is_anonymized_with_email_if_django_cradmin_user_email_auth_backend_is_true(self):
+        mommy.make(settings.AUTH_USER_MODEL, shortname='testuser@test.com')
+        self.assertEqual(get_user_model().objects.first().shortname, 'testuser@test.com')
+        anonymize_database.AnonymizeDatabase(db_alias='test').anonymize_user()
+        self.assertEqual(get_user_model().objects.first().shortname,
+                         '{}@example.com'.format(get_user_model().objects.get().id))
 
     def test_anonymize_user_email(self):
         testuser = mommy.make(settings.AUTH_USER_MODEL, shortname='testuser')
         mommy.make('devilry_account.UserEmail', email='testuser@example.com', user=testuser)
         self.assertEqual(UserEmail.objects.get().email, 'testuser@example.com')
-        anonymize_database.anonymize_user()
+        anonymize_database.AnonymizeDatabase(db_alias='test').anonymize_user()
         self.assertEqual(UserEmail.objects.get().email,
-                         '{}{}'.format(get_user_model().objects.get().id, '@example.com'))
+                         '{}_{}{}'.format(
+                             get_user_model().objects.get().id,
+                             UserEmail.objects.get().id,
+                             '@example.com'))
 
     def test_anonymize_user_name(self):
         testuser = mommy.make(settings.AUTH_USER_MODEL, shortname='testuser')
-        mommy.make('devilry_account.UserName', username='testuser', user=testuser)
-        self.assertEqual(UserName.objects.get().username, 'testuser')
-        anonymize_database.anonymize_user()
+        testusername = mommy.make('devilry_account.UserName', username='testuser', user=testuser)
+        self.assertEqual(testusername.username, 'testuser')
+        anonymize_database.AnonymizeDatabase(db_alias='test').anonymize_user()
         self.assertEqual(UserName.objects.get().username,
-                         str(get_user_model().objects.get().id))
+                         '{}_{}'.format(get_user_model().objects.get().id, testusername.id))
 
     def test_anonymize_multiple_users(self):
         testuser1 = mommy.make(settings.AUTH_USER_MODEL, shortname='testuser1')
@@ -38,7 +105,7 @@ class TestAnonymizeUserFast(test.TestCase):
         self.assertEqual(get_user_model().objects.filter(shortname='testuser1').count(), 1)
         self.assertEqual(get_user_model().objects.filter(shortname='testuser2').count(), 1)
         self.assertEqual(get_user_model().objects.filter(shortname='testuser3').count(), 1)
-        anonymize_database.anonymize_user()
+        anonymize_database.AnonymizeDatabase(db_alias='test').anonymize_user()
         self.assertEqual(
             get_user_model().objects.get(id=testuser1.id).shortname,
             str(testuser1.id))
@@ -49,6 +116,16 @@ class TestAnonymizeUserFast(test.TestCase):
             get_user_model().objects.get(id=testuser3.id).shortname,
             str(testuser3.id))
 
+    def test_anonymize_multiple_useremails_for_same_user(self):
+        testuser = mommy.make(settings.AUTH_USER_MODEL, shortname='testuser')
+        testuseremail1 = mommy.make('devilry_account.UserEmail', email='testuser1@example.com', user=testuser)
+        testuseremail2 = mommy.make('devilry_account.UserEmail', email='testuser2@example.com', user=testuser)
+        self.assertTrue(UserEmail.objects.filter(email='testuser1@example.com').exists())
+        self.assertTrue(UserEmail.objects.filter(email='testuser2@example.com').exists())
+        anonymize_database.AnonymizeDatabase(db_alias='test').anonymize_user()
+        self.assertTrue(UserEmail.objects.filter(email='{}_{}@example.com'.format(testuser.id, testuseremail1.id)))
+        self.assertTrue(UserEmail.objects.filter(email='{}_{}@example.com'.format(testuser.id, testuseremail2.id)))
+
     def test_anonymize_multiple_user_emails(self):
         testemail1 = mommy.make('devilry_account.UserEmail', email='testemail1@example.com')
         testemail2 = mommy.make('devilry_account.UserEmail', email='testemail2@example.com')
@@ -56,16 +133,26 @@ class TestAnonymizeUserFast(test.TestCase):
         self.assertEqual(UserEmail.objects.filter(email='testemail1@example.com').count(), 1)
         self.assertEqual(UserEmail.objects.filter(email='testemail2@example.com').count(), 1)
         self.assertEqual(UserEmail.objects.filter(email='testemail3@example.com').count(), 1)
-        anonymize_database.anonymize_user()
+        anonymize_database.AnonymizeDatabase(db_alias='test').anonymize_user()
         self.assertEqual(
             UserEmail.objects.get(id=testemail1.id).email,
-            '{}@example.com'.format(testemail1.user.id))
+            '{}_{}@example.com'.format(testemail1.user.id, testemail1.id))
         self.assertEqual(
             UserEmail.objects.get(id=testemail2.id).email,
-            '{}@example.com'.format(testemail2.user.id))
+            '{}_{}@example.com'.format(testemail2.user.id, testemail2.id))
         self.assertEqual(
             UserEmail.objects.get(id=testemail3.id).email,
-            '{}@example.com'.format(testemail3.user.id))
+            '{}_{}@example.com'.format(testemail3.user.id, testemail3.id))
+
+    def test_anonymize_multiple_usernames_for_same_user(self):
+        testuser = mommy.make(settings.AUTH_USER_MODEL, shortname='testuser')
+        testusername1 = mommy.make('devilry_account.UserName', username='testuser1', user=testuser)
+        testusername2 = mommy.make('devilry_account.UserName', username='testuser2', user=testuser)
+        self.assertTrue(UserName.objects.filter(username='testuser1').exists())
+        self.assertTrue(UserName.objects.filter(username='testuser2').exists())
+        anonymize_database.AnonymizeDatabase(db_alias='test').anonymize_user()
+        self.assertTrue(UserName.objects.filter(username='{}_{}'.format(testuser.id, testusername1.id)))
+        self.assertTrue(UserName.objects.filter(username='{}_{}'.format(testuser.id, testusername2.id)))
 
     def test_anonymize_multiple_user_names(self):
         testusername1 = mommy.make('devilry_account.UserName', username='testusername1')
@@ -74,14 +161,105 @@ class TestAnonymizeUserFast(test.TestCase):
         self.assertEqual(UserName.objects.filter(username='testusername1').count(), 1)
         self.assertEqual(UserName.objects.filter(username='testusername2').count(), 1)
         self.assertEqual(UserName.objects.filter(username='testusername3').count(), 1)
-        anonymize_database.anonymize_user()
+        anonymize_database.AnonymizeDatabase(db_alias='test').anonymize_user()
         self.assertEqual(
             UserName.objects.get(id=testusername1.id).username,
-            str(testusername1.user.id))
+            '{}_{}'.format(testusername1.user.id, testusername1.id))
         self.assertEqual(
             UserName.objects.get(id=testusername2.id).username,
-            str(testusername2.user.id))
+            '{}_{}'.format(testusername2.user.id, testusername2.id))
         self.assertEqual(
             UserName.objects.get(id=testusername3.id).username,
-            str(testusername3.user.id))
+            '{}_{}'.format(testusername3.user.id, testusername3.id))
 
+
+class TestAnonymizeUser(test.TestCase):
+    def test_anonymize_user_shortname(self):
+        shortname_to_anonymize = 'testuser'
+        mommy.make(settings.AUTH_USER_MODEL, shortname=shortname_to_anonymize)
+        self.assertEqual(get_user_model().objects.first().shortname, shortname_to_anonymize)
+        anonymize_database.AnonymizeDatabase(db_alias='test', fast=False).anonymize_user()
+        anonymized_user = get_user_model().objects.get()
+        self.assertEqual(len(anonymized_user.shortname),
+                         len(shortname_to_anonymize))
+
+    def test_anonymize_user_fullname(self):
+        fullname_to_anonymize = 'Test User'
+        mommy.make(settings.AUTH_USER_MODEL, fullname=fullname_to_anonymize)
+        self.assertEqual(get_user_model().objects.first().fullname, fullname_to_anonymize)
+        anonymize_database.AnonymizeDatabase(db_alias='test', fast=False).anonymize_user()
+        anonymized_user = get_user_model().objects.get()
+        self.assertEqual(len(anonymized_user.fullname),
+                         len(fullname_to_anonymize))
+
+    def test_anonymize_user_lastname(self):
+        lastname_to_anonymize = 'Userlastname'
+        mommy.make(settings.AUTH_USER_MODEL, lastname=lastname_to_anonymize)
+        self.assertEqual(get_user_model().objects.first().lastname, lastname_to_anonymize)
+        anonymize_database.AnonymizeDatabase(db_alias='test', fast=False).anonymize_user()
+        anonymized_user = get_user_model().objects.get()
+        self.assertEqual(len(anonymized_user.lastname),
+                         len(lastname_to_anonymize))
+
+    def test_anonymize_useremail(self):
+        useremail_to_anonymize = 'testuser@test.com'
+        mommy.make('devilry_account.UserEmail', email=useremail_to_anonymize)
+        self.assertEqual(UserEmail.objects.get().email, useremail_to_anonymize)
+        anonymize_database.AnonymizeDatabase(db_alias='test', fast=False).anonymize_user()
+        anonymized_useremail = UserEmail.objects.get().email
+        self.assertEqual(len(anonymized_useremail.split('@')[0]), len('testuser'))
+        self.assertEqual(len(anonymized_useremail.split('@')[1]), len('example.com'))
+
+    def test_anonymize_username(self):
+        username_to_anonymize = 'testuser'
+        mommy.make('devilry_account.UserName', username=username_to_anonymize)
+        self.assertEqual(UserName.objects.get().username, username_to_anonymize)
+        anonymize_database.AnonymizeDatabase(db_alias='test', fast=False).anonymize_user()
+        anonymized_username = UserName.objects.get().username
+        self.assertNotEqual(username_to_anonymize, anonymized_username)
+        self.assertEqual(len(anonymized_username), len(username_to_anonymize))
+
+
+class TestAnonymizeCommentFast(test.TestCase):
+    def test_anonymize_comment_text(self):
+        comment_text = 'This is a comment.'
+        testcomment = mommy.make('devilry_comment.Comment', text=comment_text)
+        self.assertEqual(testcomment.text, 'This is a comment.')
+        anonymize_database.AnonymizeDatabase(db_alias='test').anonymize_comments()
+        self.assertFalse(Comment.objects.filter(text=comment_text).exists())
+        self.assertEqual(Comment.objects.get().text, anonymize_database.lorem_ipsum)
+
+    def test_anonymize_comment_edit_history(self):
+        testedithistory = mommy.make('devilry_comment.CommentEditHistory', post_edit_text='Test', pre_edit_text='Tst')
+        self.assertEqual(testedithistory.post_edit_text, 'Test')
+        self.assertEqual(testedithistory.pre_edit_text, 'Tst')
+        anonymize_database.AnonymizeDatabase(db_alias='test').anonymize_comments()
+        self.assertFalse(CommentEditHistory.objects.filter(post_edit_text='Test', pre_edit_text='Tst').exists())
+        self.assertEqual(CommentEditHistory.objects.get().post_edit_text, anonymize_database.lorem_ipsum)
+        self.assertEqual(CommentEditHistory.objects.get().pre_edit_text, anonymize_database.lorem_ipsum)
+
+    def test_anonymize_comment_file_name(self):
+        testcommentfile = mommy.make('devilry_comment.CommentFile', filename='Test.txt')
+        self.assertEqual(testcommentfile.filename, 'Test.txt')
+        anonymize_database.AnonymizeDatabase(db_alias='test').anonymize_comments()
+        self.assertFalse(CommentFile.objects.filter(filename='Test.txt').exists())
+        self.assertEqual(CommentFile.objects.get().filename, str(testcommentfile.id))
+
+    def test_anonymize_group_comment_text(self):
+        comment_text = 'This is a comment.'
+        testcomment = mommy.make('devilry_group.GroupComment', text=comment_text)
+        self.assertEqual(testcomment.text, 'This is a comment.')
+        anonymize_database.AnonymizeDatabase(db_alias='test').anonymize_comments()
+        self.assertFalse(Comment.objects.filter(text=comment_text).exists())
+        self.assertFalse(GroupComment.objects.filter(text=comment_text).exists())
+        self.assertEqual(GroupComment.objects.get().text, anonymize_database.lorem_ipsum)
+
+    def test_anonymize_group_comment_edit_history(self):
+        testedithistory = mommy.make('devilry_group.GroupCommentEditHistory', post_edit_text='Test', pre_edit_text='Tst')
+        self.assertEqual(testedithistory.post_edit_text, 'Test')
+        self.assertEqual(testedithistory.pre_edit_text, 'Tst')
+        anonymize_database.AnonymizeDatabase(db_alias='test').anonymize_comments()
+        self.assertFalse(CommentEditHistory.objects.filter(post_edit_text='Test', pre_edit_text='Tst').exists())
+        self.assertFalse(GroupCommentEditHistory.objects.filter(post_edit_text='Test', pre_edit_text='Tst').exists())
+        self.assertEqual(GroupCommentEditHistory.objects.get().post_edit_text, anonymize_database.lorem_ipsum)
+        self.assertEqual(GroupCommentEditHistory.objects.get().pre_edit_text, anonymize_database.lorem_ipsum)
