da0313d7ff1d6970805c497cb0a92acdf806e9fb
Stian Julseth <stianjul@gmail.com>
2021-04-23 11:01:03 +0200

    #1156 django-allauth 0.43 works

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
         'html2text==2018.1.*',
         'djangorestframework==3.12.*',
         # For django-allauth
-        'django-allauth>=0.42.*',
+        'django-allauth>=0.43.*',
         'certifi==2017.11.*',
         'chardet==3.0.*',
         'idna==2.6.*',
