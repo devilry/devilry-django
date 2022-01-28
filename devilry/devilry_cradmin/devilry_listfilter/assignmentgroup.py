cafbdd442cc6a44d230bebabce5de5cabc341963
Stian Julseth <stianjul@gmail.com>
2022-01-28 12:59:14 +0100

    #1208 Filter: Add empty option label for "Examiner" filter

--- devilry/devilry_cradmin/devilry_listfilter/assignmentgroup.py
+++ devilry/devilry_cradmin/devilry_listfilter/assignmentgroup.py
@@ -381,7 +381,7 @@ class ExaminerFilter(abstractselect.AbstractSelectFilter):
 
     def get_choices(self):
         choices = [
-            ('', '')
+            ('', pgettext_lazy('group examiner filter', 'Any'))
         ]
         choices.extend(self.__get_choices_cached())
         return choices
@@ -480,7 +480,7 @@ class AbstractCandidateExaminerCountFilter(abstractselect.AbstractSelectFilter):
 
     def get_choices(self):
         return [
-            ('', pgettext_lazy('exact candidate num', 'Any')),
+            ('', pgettext_lazy('examiner candidate count any', 'Any')),
             (pgettext_lazy('exact candidate num', 'Exactly'), self.get_exact_choices()),
             (pgettext_lazy('less than candidate num', 'Less than'), self.get_less_than_choices()),
             (pgettext_lazy('greater than candidate num', 'Greater than'), self.get_greater_than_choices())
