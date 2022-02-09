204c1a9581d5a8212d052c6dcdfd380aa1f9057e
Stian Julseth <stianjul@gmail.com>
2022-02-09 16:07:30 +0100

    comment editor: Implement simply markdown insertion in textarea

--- devilry/devilry_comment/editor_widget.py
+++ devilry/devilry_comment/editor_widget.py
@@ -5,8 +5,3 @@ from django import forms
 
 class DevilryMarkdownWidget(forms.widgets.Textarea):
     template_name = 'devilry_comment/devilry_markdown_editor.django.html'
-
-    def get_context(self, name, value, attrs):
-        context = super().get_context(name, value, attrs)
-        context['widget'].update({'json_attrs': json.dumps(context['widget']['attrs'])})
-        return context
