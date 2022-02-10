621a7cf14c66a13934e7550683d4a3bdf40681cd
Stian Julseth <stianjul@gmail.com>
2022-02-10 13:34:37 +0100

    comment-editor: Provide safe arguments from widget

--- devilry/devilry_comment/editor_widget.py
+++ devilry/devilry_comment/editor_widget.py
@@ -1,7 +1,67 @@
 import json
 
+from xml.sax.saxutils import quoteattr
+
+from django.utils.translation import pgettext
 from django import forms
 
 
 class DevilryMarkdownWidget(forms.widgets.Textarea):
     template_name = 'devilry_comment/devilry_markdown_editor.django.html'
+
+    def get_context(self, name, value, attrs):
+        context = super().get_context(name, value, attrs)
+        context['attributes'] = ' '.join(
+            f'{key}={quoteattr(value)}'
+            for key, value in {
+                'name': name,
+                'value': value,
+                'labelText': pgettext(
+                    'devilry markdown widget',
+                    'Comment'
+                ),
+                'helpText': pgettext(
+                    'devilry markdown widget',
+                    'Write a comment in markdown format. Use <strong>**text here**</strong> for bold and <em>*text here*</em> for italic.'
+                ),
+                'markdownGuideLinkText': pgettext(
+                    'devilry markdown widget',
+                    'Full guide for the markdown we support here'),
+                'markdownGuideLinkUrl': '/markdown-help',
+                'toolbarConfig': json.dumps({
+                    'heading': {
+                        'placeholderText': pgettext(
+                            'devilry markdown widget toolbar placeholder text',
+                            'Add text here'),
+                        'tooltip': pgettext(
+                            'devilry markdown widget toolbar tooltip text',
+                            'Heading')
+                    },
+                    'bold': {
+                        'placeholderText': pgettext(
+                            'devilry markdown widget toolbar placeholder text',
+                            'Add text here'),
+                        'tooltip': pgettext(
+                            'devilry markdown widget toolbar tooltip text',
+                            'Bold, <ctrl+b>')
+                    },
+                    'italic': {
+                        'placeholderText': pgettext(
+                            'devilry markdown widget toolbar placeholder text',
+                            'Add text here'),
+                        'tooltip': pgettext(
+                            'devilry markdown widget toolbar tooltip text',
+                            'Italic, <ctrl+i>')
+                    },
+                    'link': {
+                        'placeholderText': pgettext(
+                            'devilry markdown widget toolbar placeholder text',
+                            'Add text here'),
+                        'tooltip': pgettext(
+                            'devilry markdown widget toolbar tooltip text',
+                            'Link, <ctrl+k>')
+                    },
+                })
+            }.items()
+        )
+        return context
