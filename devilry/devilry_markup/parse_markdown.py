ae65f11052c090649cd2d5467b8b3e7bf2f74525
Espen Kristiansen <espen@appresso.no>
2022-05-27 10:59:03 +0000

    #1220 Working syntax hilighting for fenced code in markdown.

--- devilry/devilry_markup/parse_markdown.py
+++ devilry/devilry_markup/parse_markdown.py
@@ -1,4 +1,6 @@
 import markdown
+from markdown.extensions.codehilite import CodeHiliteExtension
+# from markdown.extensions.fenced_code import 
 
 from devilry.devilry_markup.markdown_extensions.code_diff import CodeDiffExtension
 from devilry.devilry_markup.markdown_extensions.latex_math import LatexMathExtension, LatexMathInlineExtension
@@ -12,7 +14,10 @@ def markdown_full(inputMarkdown):
             CodeDiffExtension(),
             LatexMathExtension(),
             LatexMathInlineExtension(),
-            'codehilite', # Syntax hilite code
+            CodeHiliteExtension(
+                guess_lang=False,
+                linenums=False
+            ), # Syntax hilite code
             'fenced_code', # Support github style code blocks
             'nl2br', # Support github style newline handling
             'sane_lists', # Break into new ul/ol tag when the next line starts with another class of list indicator
