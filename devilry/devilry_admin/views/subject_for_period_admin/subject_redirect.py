f3231c4bc0fc9ee955335f84b43e71ebb1ecb759
Espen Kristiansen <espen@appresso.no>
2022-09-20 09:02:29 +0000

    #1240 Fix SubjectRedirectView permission checks.

--- devilry/devilry_admin/views/subject_for_period_admin/subject_redirect.py
+++ devilry/devilry_admin/views/subject_for_period_admin/subject_redirect.py
@@ -13,13 +13,13 @@ class SubjectRedirectView(View):
     Redirects subject admin to full subject overview, and period admins to a more simpler view.
     """
     def dispatch(self, request, *args, **kwargs):
-        if Subject.objects.filter_user_is_admin(self.request.user):
+        if Subject.objects.filter_user_is_admin(self.request.user).filter(id=request.cradmin_role.id).exists():
             return HttpResponseRedirect(reverse_cradmin_url(
                 instanceid='devilry_admin_subjectadmin',
                 appname='overview',
                 roleid=request.cradmin_role.id
             ))
-        elif Subject.objects.filter_user_is_admin_for_any_periods_within_subject(self.request.user):
+        elif Subject.objects.filter_user_is_admin_for_any_periods_within_subject(self.request.user).filter(id=request.cradmin_role.id).exists():
             return HttpResponseRedirect(str(self.request.cradmin_instance.rolefrontpage_url(roleid=request.cradmin_role.id)))
         raise Http404()
 
