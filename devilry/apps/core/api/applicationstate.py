a74d99cef922892ca5eac9fc4988e32f89cea074
Eskil Nordland <eskil@appresso.no>
2025-07-04 17:16:39 +0200

    feat(core): #1303 - applicationstate readyness probe now checks rq redis connections for all rq queues and database healthcheck now also handle other exceptions which might occur as well

--- devilry/apps/core/api/applicationstate.py
+++ devilry/apps/core/api/applicationstate.py
@@ -1,6 +1,18 @@
+import logging
+
 from django.db import connection
 from django.http import HttpResponse
 from django.views import View
+
+from django.conf import settings
+
+logger = logging.getLogger(__name__)
+
+class HealthCheckError(Exception):
+    """
+    Raised when healthchecks fail.
+    """
+
 
 
 class ReadyCheck(View):
@@ -15,12 +27,59 @@ class ReadyCheck(View):
 
     Returns 200 OK response if ready, otherwise 503 is returned.
     """
+
+    def healthcheck_django_rq_connections(self):
+        """
+        Health check all django-rq connections (all configured RQ_QUEUES).
+
+        Raises `HealthCheckError` if the checks fail.
+        """
+        if not hasattr(settings, 'RQ_QUEUES'):
+            return
+        for config_name, config in settings.RQ_QUEUES.items():
+            try:
+                from redis.client import Redis
+                connection = Redis(
+                    host=config.get('HOST'),
+                    db=config.get('DB', 0),
+                    port=config.get('PORT'),
+                    ssl=config.get('SSL'),
+                    ssl_cert_reqs=config.get('SSL_CERT_REQS', None),
+                    password=config.get('PASSWORD', None)
+                )
+                connection.ping()
+            except Exception as e:
+                logger.exception(e)
+                raise HealthCheckError(f"django-rq: cannot connect to queue {config_name!r}.")
+
+    def healthcheck_database_connections(self):
+        """
+        Health check all database connections (all configured DATABASES).
+
+        Raises `HealthCheckError` if the checks fail.
+        """
+        db_response_error = False
+        try:
+            from django.db import connections
+            for name in connections:
+                cursor = connections[name].cursor()
+                cursor.execute("SELECT 1;")
+                row = cursor.fetchone()
+                if row is None:
+                    db_response_error = True
+        except Exception as e:
+            logger.exception(e)
+            raise HealthCheckError("db: cannot connect to database.")
+        if db_response_error:
+            raise HealthCheckError("db: invalid response")
+
     def get(self, request, *args, **kwargs):
-        with connection.cursor() as cursor:
-            cursor.execute("SELECT 1")
-            one = cursor.fetchone()[0]
-            if one != 1:
-                return HttpResponse(status=503)
+        try:
+            self.healthcheck_django_rq_connections()
+            self.healthcheck_database_connections()
+        except HealthCheckError as e:
+            logger.exception(e)
+            return HttpResponse(status=503)
         return HttpResponse()
 
 
