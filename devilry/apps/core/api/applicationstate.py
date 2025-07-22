727cc21509bc18f7f146c99dd5f4a333ec460a50
Espen Kristiansen <espen@appresso.no>
2025-07-22 14:13:07 +0200

    fix(applicationstate): Update readiness check for Redis to handle sentinel

--- devilry/apps/core/api/applicationstate.py
+++ devilry/apps/core/api/applicationstate.py
@@ -1,12 +1,12 @@
 import logging
 
+from django.conf import settings
 from django.db import connection
 from django.http import HttpResponse
 from django.views import View
 
-from django.conf import settings
-
 logger = logging.getLogger(__name__)
+
 
 class HealthCheckError(Exception):
     """
@@ -14,15 +14,14 @@ class HealthCheckError(Exception):
     """
 
 
-
 class ReadyCheck(View):
     """
     Is the application ready to accept connections?
-    
-    Checks if it's possible to fetch from the database. This endpoint 
+
+    Checks if it's possible to fetch from the database. This endpoint
     should only be called until it returns 200.
 
-    NOTE: If it's needed to continously check if the application is alive, 
+    NOTE: If it's needed to continously check if the application is alive,
     use the "devilry_core/_api/application-state/alive" endpoint.
 
     Returns 200 OK response if ready, otherwise 503 is returned.
@@ -34,19 +33,44 @@ class ReadyCheck(View):
 
         Raises `HealthCheckError` if the checks fail.
         """
-        if not hasattr(settings, 'RQ_QUEUES'):
+        if not hasattr(settings, "RQ_QUEUES"):
             return
         for config_name, config in settings.RQ_QUEUES.items():
             try:
-                from redis.client import Redis
-                connection = Redis(
-                    host=config.get('HOST'),
-                    db=config.get('DB', 0),
-                    port=config.get('PORT'),
-                    ssl=config.get('SSL'),
-                    ssl_cert_reqs=config.get('SSL_CERT_REQS', None),
-                    password=config.get('PASSWORD', None)
-                )
+                if "SENTINELS" in config:
+                    from redis.client import Sentinel
+
+                    # NOTE: Just copied from the get_redis_connection function in django_rq/queues.py, so
+                    # should match the same logic.
+                    connection_kwargs = {
+                        "db": config.get("DB"),
+                        "password": config.get("PASSWORD"),
+                        "username": config.get("USERNAME"),
+                        "socket_timeout": config.get("SOCKET_TIMEOUT"),
+                    }
+                    connection_kwargs.update(config.get("CONNECTION_KWARGS", {}))
+                    sentinel_kwargs = config.get("SENTINEL_KWARGS", {})
+                    sentinel = Sentinel(config["SENTINELS"], sentinel_kwargs=sentinel_kwargs, **connection_kwargs)
+                    connection = sentinel.master_for(
+                        service_name=config["MASTER_NAME"],
+                        redis_class=redis_cls,
+                    )
+                else:
+                    from redis.client import Redis
+
+                    # NOTE: Just copied from the get_redis_connection function in django_rq/queues.py, so
+                    # should match the same logic.
+                    connection = Redis(
+                        host=config["HOST"],
+                        port=config["PORT"],
+                        db=config.get("DB", 0),
+                        username=config.get("USERNAME", None),
+                        password=config.get("PASSWORD"),
+                        ssl=config.get("SSL", False),
+                        ssl_cert_reqs=config.get("SSL_CERT_REQS", "required"),
+                        **config.get("REDIS_CLIENT_KWARGS", {}),
+                    )
+
                 connection.ping()
             except Exception as e:
                 logger.exception(e)
@@ -61,6 +85,7 @@ class ReadyCheck(View):
         db_response_error = False
         try:
             from django.db import connections
+
             for name in connections:
                 cursor = connections[name].cursor()
                 cursor.execute("SELECT 1;")
@@ -89,5 +114,6 @@ class LiveCheck(View):
 
     Returns 200 response.
     """
+
     def get(self, request, *args, **kwargs):
         return HttpResponse()
